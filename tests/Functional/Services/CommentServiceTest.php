348601cda9c53af94a3d7dc3f2c8471f863d3eb6
roxanariza <roxana_r2000@yahoo.com>
2017-12-06 10:44:07 +0200

    Move soft and fully delete tests on Services/CommentServiceTest.php. Update tests for display_name column

--- tests/Functional/Services/CommentServiceTest.php
+++ tests/Functional/Services/CommentServiceTest.php
@@ -74,9 +74,10 @@ class CommentServiceTest extends RailcontentTestCase
             'user_id' => rand(),
             'comment' => $this->faker->text,
             'created_on' => Carbon::now()->toDateTimeString(),
-            'deleted_at' => null
+            'deleted_at' => null,
+            'display_name' => $this->faker->word
         ];
-        $result = $this->classBeingTested->create($comment['comment'], $content['id'], $comment['parent_id'], $comment['user_id']);
+        $result = $this->classBeingTested->create($comment['comment'], $content['id'], $comment['parent_id'], $comment['user_id'], $comment['display_name']);
 
         $this->assertEquals(array_add($comment, 'replies',[]), $result);
 
@@ -107,10 +108,11 @@ class CommentServiceTest extends RailcontentTestCase
             'id' => 1,
             'parent_id' => null,
             'user_id' => $userId,
+            'display_name' => $this->faker->word,
             'replies' => null
         ];
 
-        $result = $this->classBeingTested->create($comment['comment'], $content['id'], $comment['parent_id'], $userId);
+        $result = $this->classBeingTested->create($comment['comment'], $content['id'], $comment['parent_id'], $userId, $comment['display_name']);
 
         //check that the ContentCreated event was dispatched with the correct content id
         Event::assertDispatched(CommentCreated::class, function($event) use ($comment, $content) {
@@ -140,10 +142,11 @@ class CommentServiceTest extends RailcontentTestCase
             'user_id' => $userId,
             'comment' => $this->faker->text,
             'created_on' => Carbon::now()->toDateTimeString(),
-            'deleted_at' => null
+            'deleted_at' => null,
+            'display_name' => $this->faker->word
         ];
 
-        $result = $this->classBeingTested->create($reply['comment'], null, $comment['id'], $userId);
+        $result = $this->classBeingTested->create($reply['comment'], null, $comment['id'], $userId, $reply['display_name']);
 
         $this->assertEquals(array_add($reply,'replies',[]), $result);
     }
@@ -245,15 +248,98 @@ class CommentServiceTest extends RailcontentTestCase
         $limit = 3;
 
         $totalNumber = $this->faker->numberBetween($limit, ($limit+5));
-        for($i = 1; $i<=$totalNumber; $i++) {
+        for($i = 0; $i<=$totalNumber; $i++) {
             $comment[$i] = $this->commentFactory->create($this->faker->text, $content['id'], null, rand());
             $comment[$i]['replies'] = [];
         }
 
-        $results = $this->classBeingTested->getComments(1, $limit, 'id');
+        $results = $this->classBeingTested->getComments(1, $limit, 'created_on');
 
-        $this->assertEquals($totalNumber, $results['total_results']);
+        $this->assertEquals(($totalNumber+1), $results['total_results']);
         $this->assertEquals(array_slice($comment, 0, $limit, true), $results['results']);
     }
 
+    public function test_soft_delete_comment()
+    {
+        $userId = $this->createAndLogInNewUser();
+        $content = $this->contentFactory->create($this->faker->word, $this->faker->randomElement(ConfigService::$commentableContentTypes));
+        $comment = $this->commentFactory->create($this->faker->text, $content['id'], null, $userId);
+
+        $this->classBeingTested->delete($comment['id']);
+
+        $this->assertSoftDeleted(
+            ConfigService::$tableComments,
+            [
+                'id' => $comment['id'],
+            ]
+        );
+    }
+
+    public function test_soft_delete_comment_with_replies()
+    {
+
+        $userId = $this->createAndLogInNewUser();
+        $content = $this->contentFactory->create($this->faker->word, $this->faker->randomElement(ConfigService::$commentableContentTypes));
+        $comment = $this->commentFactory->create($this->faker->text, $content['id'], null, $userId);
+
+        $reply = $this->commentFactory->create($this->faker->word, $comment['content_id'], $comment['id']);
+
+        $this->classBeingTested->delete($comment['id']);
+
+        $this->assertDatabaseHas(
+            ConfigService::$tableComments,
+            [
+                'id' => $comment['id'],
+                'deleted_at' => Carbon::now()->toDateTimeString()
+            ]
+        );
+
+        $this->assertDatabaseHas(
+            ConfigService::$tableComments,
+            [
+                'id' => $reply['id'],
+                'deleted_at' => Carbon::now()->toDateTimeString()
+            ]
+        );
+    }
+
+    public function test_delete_comment()
+    {
+        CommentRepository::$softDelete = false;
+
+        $comment = $this->commentFactory->create();
+
+        $this->classBeingTested->delete($comment['id']);
+
+        $this->assertDatabaseMissing(
+            ConfigService::$tableComments,
+            [
+                'id' => $comment['id']
+            ]
+        );
+    }
+
+    public function test_delete_comment_with_replies()
+    {
+        CommentRepository::$softDelete = false;
+
+        $comment = $this->commentFactory->create();
+        $reply = $this->commentFactory->create($this->faker->word, $comment['content_id'], $comment['id']);
+
+        $this->classBeingTested->delete($comment['id']);
+
+        $this->assertDatabaseMissing(
+            ConfigService::$tableComments,
+            [
+                'id' => $comment['id']
+            ]
+        );
+
+        $this->assertDatabaseMissing(
+            ConfigService::$tableComments,
+            [
+                'id' => $reply['id']
+            ]
+        );
+    }
 }
