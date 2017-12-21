3fc4e1626ff19761add8e138b08259fabff24305
calebfavor <calebfavor@gmail.com>
2017-12-21 09:14:13 -0800

    Fixing hierarchy mgiration.

---
+++ migrations/2017_12_21_170908_add_created_on_to_content_hierarchy.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Railroad\Railcontent\Services\ConfigService;
+
+class AddCreatedOnToContentHierarchy extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableContentHierarchy,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->dateTime('created_on')->after('child_position')->index();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableContentHierarchy,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->dropColumn('created_on');
+            }
+        );
+    }
+}
