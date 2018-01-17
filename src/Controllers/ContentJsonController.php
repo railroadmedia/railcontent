railroadmedia/railcontent

commit 89f69e1b3396d5b923b755b679f56598fd12e6c7

Pass along validation results next to content on page-load rather than breaking the page-load.

src/Controllers/ContentJsonController.php
@@ -121,21 +121,23 @@ class ContentJsonController extends Controller
 
         $rules = $this->contentService->getValidationRules($content);
         if($rules === false){
-            return new JsonResponse('Application misconfiguration. Validation rules missing perhaps.', 503 );
+            return new JsonResponse('Application misconfiguration. Validation rules missing perhaps.', 503);
         }
 
         $contentPropertiesForValidation = $this->contentService->getContentPropertiesForValidation($content, $rules);
 
         $validator = $this->validationFactory->make($contentPropertiesForValidation, $rules);
 
+        $response = [ $id => $content, 'validation' => [ 'status' => 200, 'messages' => [] ] ];
+
         try{
             $validator->validate();
         }catch(ValidationException $exception){
             $messages = $exception->validator->messages()->messages();
-            return new JsonResponse($messages, $exception->status);
+            $response['validation'] = ['status' => 422, 'messages' => $messages];
         }
 
-        return new JsonResponse(array_values([$id => $content]), 200);
+        return new JsonResponse($response, 200);
     }
 
     public function slugs(Request $request, ...$slugs)

commit 973b58bba037332fab0a55cea8d897b9f24c21b9

Oops, now response can be used by front-end.
(broke it in commit 89f69e1b "Pass along validation...")

src/Controllers/ContentJsonController.php
@@ -128,16 +128,18 @@ class ContentJsonController extends Controller
 
         $validator = $this->validationFactory->make($contentPropertiesForValidation, $rules);
 
-        $response = [ $id => $content, 'validation' => [ 'status' => 200, 'messages' => [] ] ];
+        $validation = [ 'status' => 200, 'messages' => [] ];
 
         try{
             $validator->validate();
         }catch(ValidationException $exception){
             $messages = $exception->validator->messages()->messages();
-            $response['validation'] = ['status' => 422, 'messages' => $messages];
+            $validation = ['status' => 422, 'messages' => $messages];
         }
 
-        return new JsonResponse($response, 200);
+        $content = array_merge($content, ['validation' => $validation]);
+
+        return new JsonResponse(array_values([$id => $content]), 200);
     }
 
     public function slugs(Request $request, ...$slugs)