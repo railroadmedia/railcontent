0730d115f84b9d7d7663168344fb487525cb0f54
calebfavor <calebfavor@gmail.com>
2018-04-03 10:57:15 -0700

    Sorted sets now expire 60 minutes after other content rather than *60.

--- src/Helpers/CacheHelper.php
+++ src/Helpers/CacheHelper.php
@@ -70,11 +70,12 @@ class CacheHelper
                     Cache::store(ConfigService::$cacheDriver)->getPrefix() . 'content_list_' . $element,
                     Cache::store(ConfigService::$cacheDriver)->getPrefix() . $key
                 );
+
                 if (Cache::store(ConfigService::$cacheDriver)->connection()->ttl(Cache::store(ConfigService::$cacheDriver)->getPrefix() . 'content_list_' . $element)
                     == -1) {
                     Cache::store(ConfigService::$cacheDriver)->connection()->expire(
                         Cache::store(ConfigService::$cacheDriver)->getPrefix() . 'content_list_' . $element,
-                        ConfigService::$cacheTime * 60
+                        ConfigService::$cacheTime + 60
                     );
                 }
 
