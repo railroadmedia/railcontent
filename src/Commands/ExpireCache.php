9d03710c053d3a60702e782c5bb7b2d302526c24
roxanariza <roxana_r2000@yahoo.com>
2018-04-20 17:17:56 +0300

    BM-89: As a user I cannot see content where the published_on date has recently passed, since all my contents have been cached to exclude that row

---
+++ src/Commands/ExpireCache.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Railroad\Railcontent\Commands;
+
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+use Railroad\Railcontent\Helpers\CacheHelper;
+use Railroad\Railcontent\Repositories\ContentRepository;
+use Railroad\Railcontent\Services\ConfigService;
+
+class ExpireCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:expireCache';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check contents published_on date and delete cache if publish date has passed';
+
+    /**
+     * @var ContentRepository
+     */
+    protected $contentRepository;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(ContentRepository $contentRepository)
+    {
+        parent::__construct();
+
+        $this->contentRepository = $contentRepository;
+    }
+
+    /**
+     * Execute the console command. In the command we set/get a cache key(expireCacheCommand) in the Redis that contain the last execution time of the command and
+     * we check in the database if exists contents rows where the published_on date has been passed from the last execution time. If exists rows we clear the contents caches.
+     *
+     * @return boolean
+     */
+    public function handle()
+    {
+        $lastExecutionTime = Cache::store(ConfigService::$cacheDriver)->rememberForever(
+            'expireCacheCommand',
+            function ()  {
+                return Carbon::now()->toDateTimeString();
+            });
+
+        $contents = $this->contentRepository->getRecentPublishedContents($lastExecutionTime);
+
+        if(!empty($contents)){
+            CacheHelper::deleteAllCachedSearchResults('contents');
+        }
+
+        return true;
+    }
+}
