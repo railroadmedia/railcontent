161a4ea36feed217bead88ecb84e152f23d8620d
Jonathan <jonathan@drumeo.com>
2018-04-25 15:58:22 -0700

    testing wip
    big test to ensure MultipleColumnExistsValidator validation extension works, and something to make tests fail if dummy-content create fails, because that is happening a lot right now and I can't do anything.

--- tests/Validation/MultipleColumnExistsRuleTest.php
+++ tests/Validation/MultipleColumnExistsRuleTest.php
@@ -29,6 +29,16 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
         $this->contentFactory = app()->make(ContentFactory::class);
     }
 
+    private function createContent(...$arg){
+        $content = $this->contentFactory->create(...$arg);
+
+        if(is_null($content)){
+            $this->fail('Failed to generate mock content.');
+        }
+
+        return $content;
+    }
+
     /**
      * Put a description here maybe?
      *
@@ -37,7 +47,7 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
     public function test_pass()
     {
         $typeOne = $this->faker->word;
-        $contentOne = $this->contentFactory->create($this->faker->word, $typeOne);
+        $contentOne = $this->createContent($this->faker->word, $typeOne);
         $connectionName = RailcontentTestCase::getConnectionType();
         $rule = 'exists_multiple_columns:' . $connectionName . ',' . 'railcontent_content,' . 'id' . '&' .
             $connectionName . ',' . 'railcontent_content,' . 'type,' . $typeOne;
@@ -54,10 +64,10 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
     public function test_pass_and_fail()
     {
         $typeOne = $this->faker->word;
-        $contentOne = $this->contentFactory->create($this->faker->word, $typeOne);
+        $contentOne = $this->createContent($this->faker->word, $typeOne);
 
         $typeTwo = $this->faker->word;
-        $this->contentFactory->create($this->faker->word, $typeTwo);
+        $this->createContent($this->faker->word, $typeTwo);
 
         $connectionName = RailcontentTestCase::getConnectionType();
 
@@ -116,7 +126,7 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
     public function test_or_case()
     {
         $type = $this->faker->word;
-        $content = $this->contentFactory->create('testslug', $type);
+        $content = $this->createContent('testslug', $type);
 
         $connectionName = RailcontentTestCase::getConnectionType();
 
@@ -163,7 +173,7 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
         $typeForFailOne = $this->faker->word;
         $typeForFailTwo = $this->faker->word;
 
-        $content = $this->contentFactory->create('testslug', $type);
+        $content = $this->createContent('testslug', $type);
 
         $connectionName = RailcontentTestCase::getConnectionType();
 
@@ -197,5 +207,103 @@ class MultipleColumnExistsRuleTest extends RailcontentTestCase
         $this->expectException('Illuminate\Validation\ValidationException');
 
         $validator->validate()['id'];
+    }
+
+    /**
+     * Put a description here maybe?
+     *
+     * @return void
+     */
+    public function test_extreme()
+    {
+        $type = $this->faker->word;
+        $slug = $this->faker->word;
+        $status = $this->faker->word;
+
+        $content = $this->createContent($slug, $type, $status);
+
+        $this->createContent($type, $this->faker->word, $this->faker->word);
+        $this->createContent($type, $this->faker->word, $this->faker->word);
+        $this->createContent($type, $this->faker->word, $this->faker->word);
+        $this->createContent($type, $this->faker->word, $this->faker->word);
+        $this->createContent($this->faker->word, $slug, $this->faker->word);
+        $this->createContent($this->faker->word, $slug, $this->faker->word);
+        $this->createContent($this->faker->word, $slug, $this->faker->word);
+        $this->createContent($this->faker->word, $slug, $this->faker->word);
+        $this->createContent($this->faker->word, $this->faker->word, $status);
+        $this->createContent($this->faker->word, $this->faker->word, $status);
+        $this->createContent($this->faker->word, $this->faker->word, $status);
+        $this->createContent($this->faker->word, $this->faker->word, $status);
+        $this->createContent($this->faker->word, $this->faker->word, $this->faker->word);
+        $this->createContent($this->faker->word, $this->faker->word, $this->faker->word);
+        $this->createContent($this->faker->word, $this->faker->word, $this->faker->word);
+        $this->createContent($this->faker->word, $this->faker->word, $this->faker->word);
+
+        $connectionName = RailcontentTestCase::getConnectionType();
+
+        $rule = 'exists_multiple_columns:' .
+
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'id' .
+
+            // ------------------------------------------------------------------------
+
+            '&' .
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'type,' .
+            $type.
+
+            '&' .
+            'or:' . // <-- note this here... it makes all the difference
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'type,' .
+            $this->faker->word .
+
+            // ------------------------------------------------------------------------
+
+            '&' .
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'slug,' .
+            $slug.
+
+            '&' .
+            'or:' . // <-- note this here... it makes all the difference
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'slug,' .
+            $this->faker->word .
+
+            // ------------------------------------------------------------------------
+
+            '&' .
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'status,' .
+            $status.
+
+            '&' .
+            'or:' . // <-- note this here... it makes all the difference
+            $connectionName . ',' .
+            'railcontent_content,' .
+            'status,' .
+            $this->faker->word;
+
+
+
+        /**
+         * @var $validator Validator
+         */
+        $validator = Validator::make(
+            ['id' => $content['id']],
+            ['id' => $rule]
+        );
+
+        $validationResult = $validator->validate()['id'];
+
+        $this->assertEquals('1', $validationResult);
     }
 }
