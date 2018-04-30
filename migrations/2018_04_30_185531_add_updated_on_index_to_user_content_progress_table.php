57419db175327db44ceae4d3653d4795b01ed249
calebfavor <calebfavor@gmail.com>
2018-04-30 09:13:09 -0700

    Adding updated_on column index to user progress table.

---
+++ migrations/2018_04_30_185531_add_updated_on_index_to_user_content_progress_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Railroad\Railcontent\Services\ConfigService;
+
+class AddUpdatedOnIndexToUserContentProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableUserContentProgress,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->index('updated_on');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableUserContentProgress,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->dropIndex('updated_on');
+            }
+        );
+    }
+}
