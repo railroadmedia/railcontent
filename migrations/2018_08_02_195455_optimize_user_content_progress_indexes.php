a2a9a8b111aadf7ace7390635bde106a6a8d522e
calebfavor <calebfavor@gmail.com>
2018-08-02 13:17:55 -0700

    Adding index optimizations for user content progress table

---
+++ migrations/2018_08_02_195455_optimize_user_content_progress_indexes.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+use Railroad\Railcontent\Services\ConfigService;
+
+class OptimizeUserContentProgressIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableUserContentProgress,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->index(['content_id', 'state'], 'c_s');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)->table(
+            ConfigService::$tableUserContentProgress,
+            function ($table) {
+                /**
+                 * @var $table \Illuminate\Database\Schema\Blueprint
+                 */
+
+                $table->dropIndex('c_s');
+            }
+        );
+    }
+}
