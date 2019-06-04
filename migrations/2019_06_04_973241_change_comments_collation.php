b834c0ce7e2d33fe163a604b1996b9d3bca2de0b
calebfavor <calebfavor@gmail.com>
2019-06-04 11:45:38 -0700

    Changing comments collation so we can support emojis

---
+++ migrations/2019_06_04_973241_change_comments_collation.php
@@ -0,0 +1,64 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Railroad\Railcontent\Services\ConfigService;
+
+class ChangeCommentsCollation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)
+            ->table(
+                ConfigService::$tableComments,
+                function ($table) {
+
+                    DB::connection(ConfigService::$databaseConnectionName)
+                        ->table(ConfigService::$tableComments)
+                        ->where('deleted_at', '0000-00-00 00:00:00')
+                        ->update(
+                            [
+                                'deleted_at' => Carbon::now()
+                                    ->toDateTimeString()
+                            ]
+                        );
+
+                    DB::connection(ConfigService::$databaseConnectionName)
+                        ->statement(
+                            'ALTER TABLE ' .
+                            ConfigService::$tableComments .
+                            ' MODIFY comment TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;'
+                        );
+                }
+            );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection(ConfigService::$databaseConnectionName)
+            ->table(
+                ConfigService::$tableComments,
+                function ($table) {
+
+                    DB::connection(ConfigService::$databaseConnectionName)
+                        ->statement(
+                            'ALTER TABLE ' .
+                            ConfigService::$tableComments .
+                            ' MODIFY comment TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
+                        );
+                }
+            );
+    }
+}
