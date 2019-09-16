db8049cf1aaf9f739de69c5972af2ebba5de8058
roxanariza <roxana_r2000@yahoo.com>
2019-09-16 17:01:49 +0300

    Fix tests

--- migrations/2019_06_04_973241_change_comments_collation.php
+++ migrations/2019_06_04_973241_change_comments_collation.php
@@ -4,7 +4,6 @@ use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Railroad\Railcontent\Services\ConfigService;
 
 class ChangeCommentsCollation extends Migration
 {
@@ -15,27 +14,30 @@ class ChangeCommentsCollation extends Migration
      */
     public function up()
     {
-        Schema::connection(ConfigService::$databaseConnectionName)
+        Schema::connection(config('railcontent.database_connection_name'))
             ->table(
-                ConfigService::$tableComments,
+                config('railcontent.table_prefix') . 'comments',
                 function ($table) {
 
-                    DB::connection(ConfigService::$databaseConnectionName)
-                        ->table(ConfigService::$tableComments)
+                    DB::connection(config('railcontent.database_connection_name'))
+                        ->table(config('railcontent.table_prefix') . 'comments')
                         ->where('deleted_at', '0000-00-00 00:00:00')
                         ->update(
                             [
                                 'deleted_at' => Carbon::now()
-                                    ->toDateTimeString()
+                                    ->toDateTimeString(),
                             ]
                         );
 
-                    DB::connection(ConfigService::$databaseConnectionName)
-                        ->statement(
-                            'ALTER TABLE ' .
-                            ConfigService::$tableComments .
-                            ' MODIFY comment TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;'
-                        );
+                    if (config('railcontent.database_connection_name') != 'testbench') {
+                        DB::connection(config('railcontent.database_connection_name'))
+                            ->statement(
+                                'ALTER TABLE ' .
+                                config('railcontent.table_prefix') .
+                                'comments' .
+                                ' MODIFY comment TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;'
+                            );
+                    }
                 }
             );
     }
@@ -47,15 +49,16 @@ class ChangeCommentsCollation extends Migration
      */
     public function down()
     {
-        Schema::connection(ConfigService::$databaseConnectionName)
+        Schema::connection(config('railcontent.database_connection_name'))
             ->table(
-                ConfigService::$tableComments,
+                config('railcontent.table_prefix') . 'comments',
                 function ($table) {
 
-                    DB::connection(ConfigService::$databaseConnectionName)
+                    DB::connection(config('railcontent.database_connection_name'))
                         ->statement(
                             'ALTER TABLE ' .
-                            ConfigService::$tableComments .
+                            config('railcontent.table_prefix') .
+                            'comments' .
                             ' MODIFY comment TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
                         );
                 }
