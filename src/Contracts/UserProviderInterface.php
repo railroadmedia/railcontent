b81010192ea096c298af1087400aa991dad1b295
roxanariza <roxana_r2000@yahoo.com>
2019-09-23 09:51:24 +0300

    New methods in user provider: getUserByLegacyId and getUsersByIds

--- src/Contracts/UserProviderInterface.php
+++ src/Contracts/UserProviderInterface.php
@@ -7,15 +7,50 @@ use Railroad\Railcontent\Entities\User;
 
 interface UserProviderInterface
 {
+    /**
+     * @param int $id
+     * @return User|null
+     */
     public function getUserById(int $id): ?User;
 
+    /**
+     * @param User $user
+     * @return int
+     */
     public function getUserId(User $user): int;
 
+    /**
+     * @param array $ids
+     * @return User[]
+     */
+    public function getUsersByIds(array $ids): array;
+
+    /**
+     * @param int $id
+     * @param string $brand
+     * @return array
+     */
+    public function getUserByLegacyId(int $id, string $brand): array;
+
+    /**
+     * @return User|null
+     */
     public function getCurrentUser(): ?User;
 
+    /**
+     * @return int|null
+     */
     public function getCurrentUserId(): ?int;
 
+    /**
+     * @return TransformerAbstract
+     */
     public function getUserTransformer(): TransformerAbstract;
 
+    /**
+     * @param string $email
+     * @param string $password
+     * @return User|null
+     */
     public function createUser(string $email, string $password): ?User;
 }
