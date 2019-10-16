1a0c67f522ce74b261ac5acad89f142d801f94cc
roxanariza <roxana_r2000@yahoo.com>
2019-10-16 13:15:23 +0300

    Custom paginator for old style

--- src/Serializer/OldStyleSerializer.php
+++ src/Serializer/OldStyleSerializer.php
@@ -1,10 +1,16 @@
 <?php
 namespace Railroad\Railcontent\Serializer;
 
+use League\Fractal\Pagination\PaginatorInterface;
 use League\Fractal\Serializer\DataArraySerializer;
 
 class OldStyleSerializer extends DataArraySerializer
 {
+    /**
+     * @param $transformedData
+     * @param $includedData
+     * @return array
+     */
     public function mergeIncludes($transformedData, $includedData)
     {
         $includedData = array_map(function ($include) {
@@ -12,5 +18,20 @@ class OldStyleSerializer extends DataArraySerializer
         }, $includedData);
 
         return parent::mergeIncludes($transformedData, $includedData);
+    }
+
+    /**
+     * @param PaginatorInterface $paginator
+     * @return array
+     */
+    public function paginator(PaginatorInterface $paginator)
+    {
+        $pagination = [
+            'totalResults' => (int) $paginator->getTotal(),
+            'limit' => (int) $paginator->getPerPage(),
+            'page' => (int) $paginator->getCurrentPage(),
+        ];
+
+        return $pagination;
     }
 }
