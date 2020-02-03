ffeb4320462efe1b303dffe0025acb08436b5b48
roxanariza <roxana_r2000@yahoo.com>
2020-02-03 17:55:29 +0200

    Add DECORATION_MODE

--- src/Decorators/DecoratorInterface.php
+++ src/Decorators/DecoratorInterface.php
@@ -5,6 +5,10 @@ namespace Railroad\Railcontent\Decorators;
 
 interface DecoratorInterface
 {
+    const DECORATION_MODE_MAXIMUM = 'maximum';
+    const DECORATION_MODE_PARTIAL = 'partial';
+    const DECORATION_MODE_MINIMUM = 'minimum';
+
     /**
      * @param array $entities
      * @return mixed
