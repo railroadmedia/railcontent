f1c5de6d092a0c90d67cdc67da96521ba2ce58d4
Roxana Riza <roxana_r2000@yahoo.com>
2020-09-22 15:32:23 +0300

    Add length_in_seconds

--- src/Decorators/Content/ContentPositionInHierarchyDecorator.php
+++ src/Decorators/Content/ContentPositionInHierarchyDecorator.php
@@ -13,6 +13,8 @@ class ContentPositionInHierarchyDecorator implements DecoratorInterface
     public function decorate(array $entities)
     : array {
         foreach ($entities as $entity) {
+            $entity->createProperty('length_in_seconds', $entity->fetch('video.length_in_seconds', 0));
+
             if ($entity->getParent()
                     ->count() > 0) {
                 $entity->createProperty('position', $entity->getParent()[0]->getChildPosition());
