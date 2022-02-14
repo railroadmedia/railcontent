58ac7ab1167bf355a83d9df154febd6e2a438be5
Yalung Tang <yalungtang@gmail.com>
2022-02-14 17:45:44 -0300

    prototype files

---
+++ prototype/src/components/Navbar/UserIcon.vue
@@ -0,0 +1,70 @@
+<script>
+import { ref } from 'vue'
+import OptionElement from '../AvatarMenu/OptionElement.vue'
+import OptionGroup from '../AvatarMenu/OptionGroup.vue'
+import AvatarMenu from '../AvatarMenu/AvatarMenu.vue'
+import MenuHeader from '../AvatarMenu/MenuHeader.vue'
+
+export default {
+  name: 'UserIcon',
+  props: ['onColorModeToggle', 'userName', 'userPhoto'],
+  emits: ['onColorModeToggle'],
+  components: {
+    AvatarMenu,
+    OptionGroup,
+    OptionElement,
+    MenuHeader
+  },
+  setup(props, context) {
+    const isUserMenuOpen = ref(false)
+
+    const handleMenuOpen = (val) => {
+      if (typeof val === 'boolean') {
+        isUserMenuOpen.value = val
+      } else {
+        isUserMenuOpen.value = !isUserMenuOpen.value
+      }
+    }
+
+    const handleLogout = () => {
+      window.location.reload()
+    }
+
+    return {
+      handleMenuOpen,
+      handleLogout,
+      isUserMenuOpen
+    }
+  }
+}
+</script>
+
+<template>
+  <div
+    v-on:click="handleMenuOpen"
+    class="tw-ml-[36px] tw-flex tw-h-full tw-flex-row tw-items-center tw-px-[12px] tw-py-[6px] hover:tw-bg-[#002039]/80"
+  >
+    <img :src="userPhoto" class="tw-h-[42px] tw-w-[42px] tw-rounded-full" />
+    <AvatarMenu v-if="isUserMenuOpen" @closeMenu="() => handleMenuOpen(true)">
+      <MenuHeader
+        :name="userName"
+        :optionClick="() => {}"
+        :userPhoto="userPhoto"
+      />
+      <OptionGroup>
+        <OptionElement @optionClick="() => $emit('onColorModeToggle')"
+          >Toogle Color Mode</OptionElement
+        >
+        <OptionElement href="/stuff">Playlists</OptionElement>
+        <OptionElement>Stuff</OptionElement>
+      </OptionGroup>
+      <OptionGroup>
+        <OptionElement> More Stuff </OptionElement>
+        <OptionElement> Other Stuff </OptionElement>
+      </OptionGroup>
+      <OptionGroup>
+        <OptionElement @onOptionClick="handleLogout">Logout </OptionElement>
+      </OptionGroup>
+    </AvatarMenu>
+  </div>
+</template>
--- prototype/tailwind.config.js
+++ prototype/tailwind.config.js
@@ -1,7 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const stylesoraTheme = require("stylesora/theme");
+const colors = require("tailwindcss/colors");
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  content: [],
+  prefix: "tw-",
+  darkMode: "class",
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+      ...stylesoraTheme.colors,
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    //Stylesora's Base Styles
+    require("stylesora/base")(),
+    //Stylesora's Attributes
+    require("stylesora/attributes")(),
+    //Stylesora's Utility Classes
+    require("stylesora/utilities/")(),
+    //Stylesora's Component Styles
+    require("stylesora/components/")(),
+  ],
 }
