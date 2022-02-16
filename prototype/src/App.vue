85e02a33f2b0dcb135a132054c940f0dc24bb4f0
Yalung Tang <yalungtang@gmail.com>
2022-02-16 13:36:22 -0300

    header refactor

--- prototype/src/App.vue
+++ prototype/src/App.vue
@@ -1,13 +1,24 @@
 <script>
 import PageContainer from '@/components/PageContainer/PageContainer.vue'
+import HeaderTemplate from './components/CoachHeader/HeaderTemplate.vue'
 export default {
-  name: 'App',
-  components: { PageContainer }
+    name: 'App',
+    components: { PageContainer, HeaderTemplate }
 }
 </script>
 
 <template>
-  <div>
-    <PageContainer> Page Content </PageContainer>
-  </div>
+    <div>
+        <PageContainer
+            ><HeaderTemplate
+                brand="drumeo"
+                backgroundImage=""
+                topSubtitle="TOP SUBTITLE"
+                bottomSubtitle="BOTTOM SUBTITLE"
+                shortBio="shortbio"
+            >
+                <template #actionSlot> Action Slot </template></HeaderTemplate
+            >
+        </PageContainer>
+    </div>
 </template>
--- prototype/src/components/CoachHeader/HeaderTemplate.vue
+++ prototype/src/components/CoachHeader/HeaderTemplate.vue
@@ -1,110 +1,140 @@
 <script>
-import imgix from 'imgix.js'
-
+const imgix = (url) => url
 const colors = {
     drumeo: {
         solidBg: '#00101d',
         bgRgb: 'rgba(0, 16, 29, 0)',
-        subtitle: '#7E9AB1',
+        subtitle: '#7E9AB1'
     },
-    guitareo : {
+    guitareo: {
         solidBg: '#101817',
         bgRgb: 'rgba(0, 16, 29, 0)',
-        subtitle: '#717179',
+        subtitle: '#717179'
     },
     singeo: {
         solidBg: '#18131C',
         bgRgbStart: 'rgba(0, 16, 29, 0)',
-        subtitle: '#717179',
+        subtitle: '#717179'
     },
     pianote: {
         solidBg: '#201617',
         bgRgb: 'rgba(0, 16, 29, 0)',
-        subtitle: '#717179',
-    },
-};
+        subtitle: '#717179'
+    }
+}
 
 export default {
-  name: 'HeaderTemplate',
-  props: ['brand', 'backgroundImage', 'topSubtitle', 'bottomSubtitle', 'shortBio', 'actions'],
-  setup(props, context) {
-    return {
-      colors,
-      brandColors: colors[props.brand],
-      imgix
-    };
-  }
+    name: 'HeaderTemplate',
+    props: [
+        'brand',
+        'backgroundImage',
+        'topSubtitle',
+        'bottomSubtitle',
+        'shortBio',
+        'actions'
+    ],
+    setup(props, context) {
+        return {
+            colors,
+            brandColors: colors[props.brand],
+            imgix
+        }
+    }
 }
 </script>
 
 <template>
-  <section class="tw-grid xs:tw-grid-cols-1 sm:tw-grid-cols-1 md:tw-grid-cols-1 lg:tw-grid-cols-2 tw-gap-0">
-    <div class="lg:tw-hidden tw-bg-top"
-        :style="{
-            backgroundColor: colors[brand]['solidBg'],
-            backgroundImage: `url(${imgix(
-                backgroundImage,
-                []
-            )})`,
-            color: '#fff',
-            backgroundSize: 'cover',
-            minHeight: '25vh',
-            backgroundPosition: '50%',
-            }">
-        <div :style="{
-          width: '100%',
-          height: '100%',
-          background: `linear-gradient(180deg,${colors[brand]['bgRgbStart']} 50%,${ colors[brand]['solidBg'] })`,
-        }"></div>
-    </div>
-    <div class="tw-text-white" :style="{ backgroundColor: brandColors['solidBg'] }">
-        <div class="tw--mt-24 md:tw-mt-0 tw-text-center lg:tw-text-left md:tw-py-12 lg:tw-py-16 tw-py-12 md:tw-px-14 tw-px-6">
-                <h2 v-if="topSubtitle" class="tw-uppercase tw-font-semibold tw-mb-2 tw-font-bold" :style="{
-                  color: brandColors['subtitle'],
-                  fontSize: '14px',
-                  lineHeight: '24px',
-
-                }">
+    <section
+        class="xs:tw-grid-cols-1 tw-grid tw-gap-0 sm:tw-grid-cols-1 md:tw-grid-cols-1 lg:tw-grid-cols-2"
+    >
+        <div
+            class="tw-bg-top lg:tw-hidden"
+            :style="{
+                backgroundColor: colors[brand]['solidBg'],
+                backgroundImage: `url(${imgix(backgroundImage, [])})`,
+                color: '#fff',
+                backgroundSize: 'cover',
+                minHeight: '25vh',
+                backgroundPosition: '50%'
+            }"
+        >
+            <div
+                :style="{
+                    width: '100%',
+                    height: '100%',
+                    background: `linear-gradient(180deg,${colors[brand]['bgRgbStart']} 50%,${colors[brand]['solidBg']})`
+                }"
+            ></div>
+        </div>
+        <div
+            class="tw-text-white"
+            :style="{ backgroundColor: brandColors['solidBg'] }"
+        >
+            <div
+                class="tw--mt-24 tw-py-12 tw-px-6 tw-text-center md:tw-mt-0 md:tw-py-12 md:tw-px-14 lg:tw-py-16 lg:tw-text-left"
+            >
+                <h2
+                    v-if="topSubtitle"
+                    class="tw-mb-2 tw-font-semibold tw-font-bold tw-uppercase"
+                    :style="{
+                        color: brandColors['subtitle'],
+                        fontSize: '14px',
+                        lineHeight: '24px'
+                    }"
+                >
                     {{ topSubtitle }}
                 </h2>
 
-            <h1 class="tw-mb-3 tw-uppercase tw-font-normal" :style="{
-              fontSize: '40px',
-              lineHeight: '36px',
-            }">
-                {{ $title }}
-            </h1>
-                <h2 v-if="bottomSubtitle" class="tw-uppercase tw-font-semibold tw-mb-2 tw-font-bold" :style="{
-                  color: brandColors['subtitle'],
-                  fontSize: '14px',
-                  lineHeight: '24px',
-                }">
+                <h1
+                    class="tw-mb-3 tw-font-normal tw-uppercase"
+                    :style="{
+                        fontSize: '40px',
+                        lineHeight: '36px'
+                    }"
+                >
+                    {{ title }}
+                </h1>
+                <h2
+                    v-if="bottomSubtitle"
+                    class="tw-mb-2 tw-font-semibold tw-font-bold tw-uppercase"
+                    :style="{
+                        color: brandColors['subtitle'],
+                        fontSize: '14px',
+                        lineHeight: '24px'
+                    }"
+                >
                     {{ bottomSubtitle }}
                 </h2>
-            <h3 class="tw-mb-6 tw-font-normal" style="font-size: 14px; line-height: 24px;">
-                {{ shortBio }}
-            </h3>
-            <div class="tw-flex tw-flex-col lg:tw-flex-row tw-items-center xl:tw-items-start tw-flex-wrap xl:tw-flex-nowrap">
-                {{ actions }}
+                <h3
+                    class="tw-mb-6 tw-font-normal"
+                    style="font-size: 14px; line-height: 24px"
+                >
+                    {{ shortBio }}
+                </h3>
+                <div
+                    class="tw-flex tw-flex-col tw-flex-wrap tw-items-center lg:tw-flex-row xl:tw-flex-nowrap xl:tw-items-start"
+                >
+                    <slot name="actionSlot"></slot>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="tw-hidden lg:tw-flex tw-bg-top"
-        :style="{
-          backgroundColor: colors[brand]['solidBg'],
-            backgroundImage: `url(${imgix(
-                backgroundImage,
-                []
-            )})`,
-            color: '#fff',
-            backgroundSize: 'cover',
-            backgroundPosition: '50%',
-        }">
-        <div :style="{
-          width: '100%',
-          height: '100%',
-          background: `linear-gradient(268deg,${colors[brand]['bgRgbStart']} }} 50%,${colors[brand]['solidBg']})`,
-        }"></div>
-    </div>
-</section>
+        <div
+            class="tw-hidden tw-bg-top lg:tw-flex"
+            :style="{
+                backgroundColor: colors[brand]['solidBg'],
+                backgroundImage: `url(${imgix(backgroundImage, [])})`,
+                color: '#fff',
+                backgroundSize: 'cover',
+                backgroundPosition: '50%'
+            }"
+        >
+            <div
+                :style="{
+                    width: '100%',
+                    height: '100%',
+                    background: `linear-gradient(268deg,${colors[brand]['bgRgbStart']} }} 50%,${colors[brand]['solidBg']})`
+                }"
+            ></div>
+        </div>
+    </section>
 </template>
