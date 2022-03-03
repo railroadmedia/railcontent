2537e1eff2cae5b6034e57e2e58e7131fa94568c
calebfavor <calebfavor@gmail.com>
2022-03-03 22:49:11 +0000

    merge again

--- resources/marketing/views/drumeo/home.blade.php
+++ resources/marketing/views/drumeo/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="{{ _mix('marketing/css/app.css') }}">
     <title>Musora</title>
 </head>
 <body class="antialiased">
--- resources/marketing/views/musora/home.blade.php
+++ resources/marketing/views/musora/home.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
     <title>Musora</title>
 </head>
 <body class="antialiased">
-<h1>Musora home page!</h1>
+<h1 class="tw-text-2xl">Musora home page!</h1>
 </body>
 </html>
