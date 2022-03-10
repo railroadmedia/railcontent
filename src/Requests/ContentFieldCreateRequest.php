b01ad07b9b50562595bbd5a6ceb782266e1094e2
Roxana Riza <roxana_r2000@yahoo.com>
2022-03-10 17:24:29 +0200

    Update ContentFieldCreateRequest

--- src/Requests/ContentFieldCreateRequest.php
+++ src/Requests/ContentFieldCreateRequest.php
@@ -2,7 +2,6 @@
 
 namespace Railroad\Railcontent\Requests;
 
-use Railroad\Railcontent\Services\ConfigService;
 
 class ContentFieldCreateRequest extends CustomFormRequest
 {
@@ -16,14 +15,27 @@ class ContentFieldCreateRequest extends CustomFormRequest
         //set the general validation rules
         $this->setGeneralRules(
             [
-                'key' => 'required_without:id|max:255',
+                'key' => 'required|max:255',
                 'type' => 'required_without:id|max:255',
                 'position' => 'nullable|numeric|min:0',
-                'content_id' => 'required_without:id|numeric|exists:'.ConfigService::$tableContent . ',id'
+                'content_id' => 'required_without:id|numeric|exists:'.config('railcontent.database_connection_name') . '.' .
+                    config('railcontent.table_prefix'). 'content' . ',id'
             ]
         );
 
         //get all the validation rules that apply to the request
         return parent::rules();
+    }
+
+    public function onlyAllowed()
+    {
+        return
+            $this->only(
+                [
+                    'key',
+                    'value',
+                    'position'
+                ]
+            );
     }
 }
