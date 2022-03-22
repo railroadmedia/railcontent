73d6073d5a4fc6a2bb8ecdc5f7d027e1575012a0
calebfavor <calebfavor@gmail.com>
2022-03-22 17:00:26 +0000

    upload progress axios call

--- resources/platform/assets/js/vue/components/ImageUploadProgress/ImageUploadProgress.vue
+++ resources/platform/assets/js/vue/components/ImageUploadProgress/ImageUploadProgress.vue
@@ -1,9 +1,40 @@
 <script setup>
 import UploadProgress from '../UploadProgress/UploadProgress.vue'
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const FILE_UPLOAD_SERVICE = ''
+
 const props = defineProps({
   image: {
     type: String,
   },
+});
+
+const percentCompleted = ref(0);
+
+onMounted(() => {
+    (function () {
+        document.getElementById('upload').onclick = function () {
+          var data = new FormData();
+          data.append('profile-pic', 'profile-pic-img');
+          data.append('data', image);
+
+          var config = {
+            onUploadProgress: function(progressEvent) {
+              percentCompleted.value = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
+            }
+          };
+
+          axios.post(FILE_UPLOAD_SERVICE, data, config)
+            .then(function (res) {
+              console.log('success', res)
+            })
+            .catch(function (err) {
+              console.log(err)
+            });
+        };
+      })();
 });
 </script>
 
@@ -19,7 +50,7 @@ const props = defineProps({
           <h2 class="tw-text-bold tw-mb-[12px] tw-text-[16px]">Uploading in progress</h2>
           <p class="tw-italic tw-text-[14px] tw-text-[#E5E5E5]">This will take a few short seconds</p>
       </div>
-      <UploadProgress :percentage="100" />
+      <UploadProgress :percentage="percentCompleted" />
     </div>
   </div>
 </template>
