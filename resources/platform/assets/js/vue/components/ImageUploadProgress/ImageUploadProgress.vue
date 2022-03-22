d05a3de085fa167e25ad9d7268301b65a83a0402
calebfavor <calebfavor@gmail.com>
2022-03-22 17:01:18 +0000

    remove unnecessary code

--- resources/platform/assets/js/vue/components/ImageUploadProgress/ImageUploadProgress.vue
+++ resources/platform/assets/js/vue/components/ImageUploadProgress/ImageUploadProgress.vue
@@ -1,9 +1,9 @@
 <script setup>
-import UploadProgress from '../UploadProgress/UploadProgress.vue'
-import axios from 'axios'
-import { onMounted, ref } from 'vue'
+import UploadProgress from "../UploadProgress/UploadProgress.vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
 
-const FILE_UPLOAD_SERVICE = ''
+const FILE_UPLOAD_SERVICE = "";
 
 const props = defineProps({
   image: {
@@ -14,27 +14,28 @@ const props = defineProps({
 const percentCompleted = ref(0);
 
 onMounted(() => {
-    (function () {
-        document.getElementById('upload').onclick = function () {
-          var data = new FormData();
-          data.append('profile-pic', 'profile-pic-img');
-          data.append('data', image);
+  (function () {
+    var data = new FormData();
+    data.append("profile-pic", "profile-pic-img");
+    data.append("data", image);
 
-          var config = {
-            onUploadProgress: function(progressEvent) {
-              percentCompleted.value = Math.round( (progressEvent.loaded * 100) / progressEvent.total );
-            }
-          };
+    var config = {
+      onUploadProgress: function (progressEvent) {
+        percentCompleted.value = Math.round(
+          (progressEvent.loaded * 100) / progressEvent.total
+        );
+      },
+    };
 
-          axios.post(FILE_UPLOAD_SERVICE, data, config)
-            .then(function (res) {
-              console.log('success', res)
-            })
-            .catch(function (err) {
-              console.log(err)
-            });
-        };
-      })();
+    axios
+      .post(FILE_UPLOAD_SERVICE, data, config)
+      .then(function (res) {
+        console.log("success", res);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  })();
 });
 </script>
 
@@ -46,9 +47,16 @@ onMounted(() => {
         :src="image"
         alty="Profile picture thumbnail"
       />
-      <div class="tw-w-full tw-text-center tw-text-white tw-mt-[20px] tw-pb-[20px]" style="font-family: Open Sans">
-          <h2 class="tw-text-bold tw-mb-[12px] tw-text-[16px]">Uploading in progress</h2>
-          <p class="tw-italic tw-text-[14px] tw-text-[#E5E5E5]">This will take a few short seconds</p>
+      <div
+        class="tw-w-full tw-text-center tw-text-white tw-mt-[20px] tw-pb-[20px]"
+        style="font-family: Open Sans"
+      >
+        <h2 class="tw-text-bold tw-mb-[12px] tw-text-[16px]">
+          Uploading in progress
+        </h2>
+        <p class="tw-italic tw-text-[14px] tw-text-[#E5E5E5]">
+          This will take a few short seconds
+        </p>
       </div>
       <UploadProgress :percentage="percentCompleted" />
     </div>
