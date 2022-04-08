a149bc5c5ec3c3c2b2b6459d3ff5b525987d488a
Roxana Riza <roxana_r2000@yahoo.com>
2022-04-08 17:26:31 +0300

    Cleanup

--- src/Commands/MigrateContentToNewStructure.php
+++ src/Commands/MigrateContentToNewStructure.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Illuminate\Console\Command;
 
-
 class MigrateContentToNewStructure extends Command
 {
     /**
@@ -23,22 +22,14 @@ class MigrateContentToNewStructure extends Command
      */
     protected $description = 'Migrate contents from old structure to new structure';
 
-    /**
-     * @throws ORMException
-     * @throws OptimisticLockException
-     */
     public function handle()
     {
         $this->info('Starting MigrateContentToNewStructure.');
-//        $this->call('command:migrateUserPlaylists');
-//        $this->call('command:deleteOldContentForPlaylist');
+        //        $this->call('command:migrateUserPlaylists');
+        //        $this->call('command:deleteOldContentForPlaylist');
         $this->call('command:migrateFields');
         $this->call('command:migrateContentColumns');
-        $this->call('command:migrateInstructors');
         $this->call('command:migrateVideos');
-        $this->call('command:migrateStyles');
-//        $this->call('command:deleteOrphanContent');
-//        $this->call('command:calculateTotalXP');
         $this->call('command:MigrateContentToElasticsearch');
 
         $this->info('Finished MigrateContentToNewStructure.');
