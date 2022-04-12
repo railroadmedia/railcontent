62e09566be7c2030f452bde413d0e5e021a58545
peterminhlee <petereminhlee@gmail.com>
2022-04-12 15:25:39 -0700

    path changes, added  v-cloak div

--- resources/platform/views/pages/reset.blade.php
+++ resources/platform/views/pages/reset.blade.php
@@ -1,8 +1,4 @@
-@extends('members.layout', [
-    "bodyClass" => "bg-white",
-    "hideNav" => true,
-    "hideFooter" => true
-])
+@extends('partials.layout')
 
 @section('meta')
     <title>Login | Singeo</title>
@@ -40,60 +36,64 @@
 @endsection
 
 @section('content')
-    <section id="logoContainer" class="pa-2 text-center">
-        <img class="logo"
-             src="https://singeo.s3.amazonaws.com/sales/2021/singeo-logo.png">
-    </section>
+    <div v-cloak>
 
-    <p class="pb-3 text-center font-italic">Your start-to-finish guide to confident singing.</p>
+        <section id="logoContainer" class="pa-2 text-center">
+            <img class="logo"
+                src="https://singeo.s3.amazonaws.com/sales/2021/singeo-logo.png">
+        </section>
 
-    <section class="flex flex-column bg-grey-1 pa-3 mb-3 corners-10">
+        <p class="pb-3 text-center font-italic">Your start-to-finish guide to confident singing.</p>
 
-        <form method="post" action="{{ url()->route('usora.password.reset') }}" class="flex flex-column">
-            @if($useCsrfToken ?? true)
-                {{ csrf_field() }}
-            @endif
+        <section class="flex flex-column bg-grey-1 pa-3 mb-3 corners-10">
 
-            @if(!empty($errors->all()))
-                <ul class="flex flex-column mb-3 tiny text-error list-style-none">
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
+            <form method="post" action="{{ url()->route('usora.password.reset') }}" class="flex flex-column">
+                @if($useCsrfToken ?? true)
+                    {{ csrf_field() }}
+                @endif
 
-            <input type="hidden" name="token" value="{{ $token }}">
-            <input type="hidden" name="email" value="{{ $email }}">
+                @if(!empty($errors->all()))
+                    <ul class="flex flex-column mb-3 tiny text-error list-style-none">
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
 
-            <p class="tiny mb-2 text-center">Reset password for <strong>{{ $email }}</strong></p>
+                <input type="hidden" name="token" value="{{ $token }}">
+                <input type="hidden" name="email" value="{{ $email }}">
 
-            <div class="flex flex-column mb-2">
-                @include('bladesora::members.inputs.text-input', [
-                   "brand" => 'singeo',
-                   "type" => "password",
-                   "inputId" => "newPassword",
-                   "inputName" => "password",
-                   "inputLabel" => "New Password",
-                   "inputValue" => "",
-                   "inputErrors" => []
-               ])
-            </div>
+                <p class="tiny mb-2 text-center">Reset password for <strong>{{ $email }}</strong></p>
 
-            <div class="flex flex-column mb-2">
-                @include('bladesora::members.inputs.text-input', [
+                <div class="flex flex-column mb-2">
+                    @include('partials.bladesora.members.inputs.text-input', [
                     "brand" => 'singeo',
                     "type" => "password",
-                    "inputId" => "confirmNewPassword",
-                    "inputName" => "password_confirmation",
-                    "inputLabel" => "Confirm New Password",
+                    "inputId" => "newPassword",
+                    "inputName" => "password",
+                    "inputLabel" => "New Password",
                     "inputValue" => "",
                     "inputErrors" => []
                 ])
-            </div>
+                </div>
 
-            <button type="submit" class="btn">
-                <span class="text-white bg-singeo">Sign In</span>
-            </button>
-        </form>
-    </section>
+                <div class="flex flex-column mb-2">
+                    @include('partials.bladesora.members.inputs.text-input', [
+                        "brand" => 'singeo',
+                        "type" => "password",
+                        "inputId" => "confirmNewPassword",
+                        "inputName" => "password_confirmation",
+                        "inputLabel" => "Confirm New Password",
+                        "inputValue" => "",
+                        "inputErrors" => []
+                    ])
+                </div>
+
+                <button type="submit" class="btn">
+                    <span class="text-white bg-singeo">Sign In</span>
+                </button>
+            </form>
+        </section>
+
+    </div>
 @endsection
