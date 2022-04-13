11fe712a4e130d14c8da51c3042f7f000d3f9e76
peterminhlee <petereminhlee@gmail.com>
2022-04-13 13:37:28 -0700

    path and tailwind class updates

--- resources/platform/views/live/embed.blade.php
+++ resources/platform/views/live/embed.blade.php
@@ -1,26 +1,8 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1,  maximum-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+@extends('partials.layout')
 
-    <link rel="stylesheet" href="https://dpwjbsxqtam5n.cloudfront.net/fonts/font-awesome-5/fontawesome-all.min.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Roboto+Condensed:wght@400;700&display=swap"
-        rel="stylesheet"
-    >
+@section('content')
+    <div v-cloak>
 
-    @include('members.partials._favicons')
-
-    <link rel="stylesheet" href="{{ mix('css/packages.css') }}">
-
-</head>
-
-<body>
-    <div id="app">
         <div class="" style="width: 440px; height: 820px;">
             <chat
                 api-key="{{ $apiKey }}"
@@ -31,14 +13,16 @@
                 :is-administrator="{{ json_encode(boolval($isAdministrator)) }}"
                 :user-data="{{ json_encode($userData) }}"
                 embed-url="{{ $embedUrl }}"
-            ></chat>
+            />
         </div>
-    </div>
 
+    </div>
+@endsection
+
+@section('layout-scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.2.5/polyfill.js"></script>
 
-    <script src="{{ mix('assets/members/js/manifest.js') }}"></script>
-    <script src="{{ mix('assets/members/js/vendor.js') }}"></script>
-    <script src="{{ mix('assets/members/js/app.js') }}"></script>
-</body>
-</html>
+    <script src="{{ mix('platform/js/manifest.js') }}"></script>
+    <script src="{{ mix('platform/js/vendor.js') }}"></script>
+    <script src="{{ mix('platform/js/app.js') }}"></script>
+@endsection
--- resources/platform/views/live/online.blade.php
+++ resources/platform/views/live/online.blade.php
@@ -1,84 +1,82 @@
-@php
-    $bodyClass = ($bodyClass ?? '') . ' sidebar';
-    $leftSidebar = true;
-@endphp
-
-@extends('members.layout', ['excludeTailwindCss' => false])
+@extends('partials.layout')
 
 @section('meta')
-    <title>Live Lesson - {{ $lessonContent->fetch('fields.title') }} | Singeo</title>
+    <title>Live Lesson - {{ $lessonContent->fetch('fields.title') }} | {{ $brand }}</title>
 @endsection
 
-@section('styles')
+@section('layout-styles')
     <link rel="stylesheet" href="{{ mix('css/packages.css') }}">
 @endsection
 
-@section('scripts')
-
-@endsection
-
 @section('content')
-    @include('members.partials._content-sidebar')
+    <page-container>
+        <div v-cloak>
 
-    <div class="container fluid bg-grey-5 pv-3">
-        <div class="container">
-            {{-- Youtube Live Embed --}}
-            @include('bladesora::members.content.lesson-video._live-embed', [
-                "themeColor" => "drumeo",
-                "youtubeId" => $liveStreamId,
-                "lessonTitle" => $lessonContent->fetch('fields.title'),
-                "userAvatar" => current_user()->getProfilePictureUrl(),
-                "userName" => current_user()->getDisplayName(),
-                "userEmail" => current_user()->getEmail(),
-                "emailRecipient" =>  "questions@drumeo.com", // If changed, update "Email addresses set in sites" doc
-                "emailLogo" =>  "https://dmmior4id2ysr.cloudfront.net/logos/drumeo-logo.png",
-                "brand" =>  "drumeo",
-                "apiKey" => $apiKey,
-                "token" => $token,
-                "chatChannelName" => $chatChannelName,
-                "questionsChannelName" => $questionsChannelName,
-                "isAdministrator" => $isAdministrator,
-                "userData" => $userData,
+            <div class="tw-container tw-mx-auto fluid bg-grey-5 pv-3">
+                <div class="tw-container tw-mx-auto">
+                    {{-- Youtube Live Embed --}}
+                    @include('partials.bladesora.members.content.lesson-video._live-embed', [
+                        "themeColor" => "{{ $brand }}",
+                        "youtubeId" => $liveStreamId,
+                        "lessonTitle" => $lessonContent->fetch('fields.title'),
+                        "userAvatar" => current_user()->getProfilePictureUrl(),
+                        "userName" => current_user()->getDisplayName(),
+                        "userEmail" => current_user()->getEmail(),
+                        "emailRecipient" =>  "questions@drumeo.com", // If changed, update "Email addresses set in sites" doc
+                        "emailLogo" =>  "https://dmmior4id2ysr.cloudfront.net/logos/drumeo-logo.png",
+                        "brand" =>  "{{ $brand }}",
+                        "apiKey" => $apiKey,
+                        "token" => $token,
+                        "chatChannelName" => $chatChannelName,
+                        "questionsChannelName" => $questionsChannelName,
+                        "isAdministrator" => $isAdministrator,
+                        "userData" => $userData,
 
-                "lessonTitle" => $lessonContent->fetch('fields.title'),
-                "contentType" => $lessonContent->fetch('type'),
-                "instructors" => $lessonContent->fetch('*fields.instructor', []),
-                "lessonResources" => array_merge($lessonContent['resources'] ?? [], $parent['resources'] ?? []),
-            ])
-        </div>
-    </div>
-
-    <div class="container mv-3" style="margin-left: 0 !important; margin-right: 0 !important; width: 100% !important; max-width: 100%; padding-left: 87px; padding-right: 87px;">
-        <div id="lessonInfo" class="flex flex-row align-v-top">
-            <div class="flex flex-column grow bg-white shadow corners-10">
-                @if(!empty($lessonContent->fetch('*assignments')))
-                    @foreach($lessonContent->fetch('*assignments', []) as $assignment)
-                        <div class="flex flex-row">
-                            <div class="flex flex-column grow">
-                                <content-assignment
-                                        theme-color="singeo"
-                                        timecode="{{ $assignment->fetch('data.timecode', 0) }}"
-                                        id="{{ $assignment->fetch('id') }}"
-                                        xp="{{ $assignment->fetch('xp') }}"
-                                        title="{{ $assignment->fetch('fields.title') }}"
-                                        soundslice-slug="{{ $assignment->fetch('fields.soundslice_slug') }}"
-                                        :completed="{{ json_encode($assignment->fetch('completed')) }}"
-                                        :user-id="{{ auth()->id() }}"></content-assignment>
-                            </div>
-                        </div>
-                    @endforeach
-                @endif
-            </div>
-        </div>
-        <div class="flex flex-column  mv-3">
-            <div class="flex flex-row pv-3 ph">
-                <h1 class="heading">Upcoming Lessons</h1>
+                        "lessonTitle" => $lessonContent->fetch('fields.title'),
+                        "contentType" => $lessonContent->fetch('type'),
+                        "instructors" => $lessonContent->fetch('*fields.instructor', []),
+                        "lessonResources" => array_merge($lessonContent['resources'] ?? [], $parent['resources'] ?? []),
+                    ])
+                </div>
             </div>
 
-            <div class="flex flex-row">
-                <content-schedule :preloaded-content="{{ $scheduleEvents }}"
-                                  theme-color="singeo"></content-schedule>
+            <div class="tw-container tw-mx-auto tw-px-4 dark:tw-text-white tw-pt-8 tw-pb-14" style="margin-left: 0 !important; margin-right: 0 !important; width: 100% !important; max-width: 100%; padding-left: 87px; padding-right: 87px;">
+                <div id="lessonInfo" class="tw-flex tw-flex-row align-v-top">
+                    <div class="tw-flex tw-flex-coltw-grow tw-bg-white tw-shadow corners-10">
+                        @if(!empty($lessonContent->fetch('*assignments')))
+                            @foreach($lessonContent->fetch('*assignments', []) as $assignment)
+                                <div class="tw-flex tw-flex-row">
+                                    <div class="tw-flex tw-flex-coltw-grow">
+                                        <content-assignment
+                                            theme-color="{{ $brand }}"
+                                            timecode="{{ $assignment->fetch('data.timecode', 0) }}"
+                                            id="{{ $assignment->fetch('id') }}"
+                                            xp="{{ $assignment->fetch('xp') }}"
+                                            title="{{ $assignment->fetch('fields.title') }}"
+                                            soundslice-slug="{{ $assignment->fetch('fields.soundslice_slug') }}"
+                                            :completed="{{ json_encode($assignment->fetch('completed')) }}"
+                                            :user-id="{{ auth()->id() }}"
+                                        />
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+                <div class="tw-flex tw-flex-col mv-3">
+                    <div class="tw-flex tw-flex-row pv-3 ph">
+                        <h1 class="heading">Upcoming Lessons</h1>
+                    </div>
+
+                    <div class="tw-flex tw-flex-row">
+                        <content-schedule 
+                            :preloaded-content="{{ $scheduleEvents }}"
+                            theme-color="{{ $brand }}"
+                        />
+                    </div>
+                </div>
             </div>
+
         </div>
-    </div>
+    </page-container>
 @endsection
