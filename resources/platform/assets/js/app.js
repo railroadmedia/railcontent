31efced610e18fabe5891b647717e75afb8203c3
peterminhlee <petereminhlee@gmail.com>
2022-04-14 08:50:20 -0700

    added vuesora component

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -22,6 +22,7 @@ import PaymentMethods from './vue/vuesora/views/payment-methods';
 import ForumThreadsTable from './vue/vuesora/views/forum';
 import ForumThread from './vue/vuesora/views/forum/thread';
 import TextEditor from './vue/vuesora/components/TextEditor';
+import ContactMemberEmailForm from './vue/vuesora/components/ContactMemberEmailForm';
 
 const app = createApp({});
 
@@ -42,6 +43,7 @@ app.component('AppContainer', AppContainer)
    .component('ForumThreadsTable', ForumThreadsTable)
    .component('ForumThread', ForumThread)
    .component('TextEditor', TextEditor)
+   .component('ContactMemberEmailForm', ContactMemberEmailForm)
 
 app.directive('click-outside', {
     mounted(el, binding, vnode) {
