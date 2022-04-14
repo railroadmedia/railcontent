b3077ab703aa454468fd97e5a83d3e466bc98c9a
peterminhlee <petereminhlee@gmail.com>
2022-04-14 10:25:33 -0700

    tailwind update

--- resources/platform/views/partials/bladesora/members/inputs/toggle-input.blade.php
+++ resources/platform/views/partials/bladesora/members/inputs/toggle-input.blade.php
@@ -1,4 +1,4 @@
-<div class="tw-flex tw-flex-row form-group align-v-center">
+<div class="tw-flex tw-flex-row form-group tw-items-center">
     <span class="toggle-input mr-1">
         <input type="hidden"
                class="hidden-input"
