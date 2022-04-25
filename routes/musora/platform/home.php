65eda4e2728f8d64ae7799c7fa908be5848735f2
peterminhlee <petereminhlee@gmail.com>
2022-04-25 14:59:30 -0700

    added test routes

--- routes/musora/platform/home.php
+++ routes/musora/platform/home.php
@@ -2,12 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Platform\HomeController;
+use App\Models\Product;
+use App\Models\Brand;
 
 Route::domain('{subdomain}.musora.com')
     ->middleware(['web_authenticated'])
     ->group(function () {
-    Route::get('members', [
-        HomeController::class,
-        'show',
+        Route::get('members', [
+            HomeController::class,
+            'show',
+        ]);
+    });
+
+Route::get('products/{product:slug}', function ($slug){
+    $product = Product::where('slug', $slug)->first();
+
+    return view('product',[
+        'product' => $product
     ]);
 });
+
+Route::get('{brand}/products', function ($brand){
+//    $products = Product::where("brands.name", "=", $brand)->get();
+    $products = Product::join("brands", "brands.id", "=", "products.brand_id")->join("product_types", "product_types.id", "=", "products.product_type_id")->where("brands.name", "=", $brand)->get(["products.name", "product_types.name as product_type",]);
+
+    dd($products);
+
+    return view('products',[
+        'products' => $products
+    ]);
+});
+
