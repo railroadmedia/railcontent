3fdbc831142c1d051df24716a28953364c8c0bea
calebfavor <calebfavor@gmail.com>
2022-04-25 10:40:42 -0700

    Making the main catalog blade run properly so we can render the courses route. Fixing up user model accessors.

--- resources/platform/views/partials/_header-banner.blade.php
+++ resources/platform/views/partials/_header-banner.blade.php
@@ -1,10 +1,7 @@
 <!-- // todo - review & clean -->
 <div class="tw-container tw-mx-auto fluid collapsed-h pv-5 relative tw-bg-black">
     <div class="header-background-container absolute-fill tw-bg-top"
-            style="background-image: url({{ _imgix(
-                    $backgroundImage,
-                    ["q" => 80, "blur" => 40, "w" => 640]
-                ) }});" data-ix-bg="{{ $backgroundImage }}"
+            style="background-image: url({{ cf_img($backgroundImage, ["quality" => 80, "blur" => 150, "width" => 640]) }});" data-ix-bg="{{ $backgroundImage }}"
     ></div>
     <div class="header-background-container absolute-fill tw-bg-top hide-lg-down" style="background: linear-gradient(to left, #000 0%, transparent 10%, transparent 90%, #000 100%)"></div>
     <div class="header-gradient-overlay absolute-fill {{ $brand }}"></div>
@@ -15,33 +12,30 @@
             @if(empty($hideUser))
                 <div class="header-avatar tw-flex tw-flex-col hide-xs-only">
                     <div class="user-avatar tw-rounded tw-bg-black
-                                {{ in_array($currentUser['access_level'], ['edge', 'lifetime', 'team', 'guitar', 'piano']) ? 'subscriber' : '' }}
+                                {{ in_array(user()->access_level, ['edge', 'lifetime', 'team', 'guitar', 'piano']) ? 'subscriber' : '' }}
                                 {{ $brand }}
-                                {{ $currentUser['access_level'] }}"
+                                {{ user()->access_level }}"
                     >
-                        <a href="{{ $profileUrl }}" class="tw-no-underline">
+                        <a href="{{ user()->getDashboardUrl() }}" class="tw-no-underline">
                             <img
                                 class="tw-rounded inset-border"
-                                src="{{ _imgix(
-                                    $currentUser['avatar'],
-                                    ["q" => 50, "blur" => 2, "w" => 50, "h" => 50]
-                                ) }}"
-                                data-ix-src="{{ $currentUser['avatar'] }}"
+                                src="{{ user()->profile_picture_url }}"
+                                data-ix-src="{{ user()->profile_picture_url }}"
                             >
                         </a>
                     </div>
 
-                    @if($currentUser['access_level'] !== 'pack')
+                    @if(user()->access_level !== 'pack')
                         <p class="body dense tw-text-white tw-font-bold tw-text-center tw-uppercase tw-mt-1 tw-text-white">
-                            @if($currentUser['access_level'] === 'team')
+                            @if(user()->access_level === 'team')
                                 {{ $brand }} Team
                             @else
-                                {{ map_experience_rank($currentUser['xp']) }}
+                                {{ user()->getXpRank() }}
                             @endif
                         </p>
 
-                        @if($currentUser['access_level'] !== 'team')
-                            <p class="body dense tw-text-white font-compressed tw-text-center">{{ $currentUser['xp'] }} XP</p>
+                        @if(user()->access_level !== 'team')
+                            <p class="body dense tw-text-white font-compressed tw-text-center">{{ user()->total_xp }} XP</p>
                         @endif
                     @endif
                 </div>
