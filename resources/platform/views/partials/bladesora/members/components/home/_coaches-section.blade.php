3920449a88428265bc103f1e30df3d641bf8d7f4
calebfavor <calebfavor@gmail.com>
2022-04-29 13:56:06 +0000

    space fixes in homepage

--- resources/platform/views/partials/bladesora/members/components/home/_coaches-section.blade.php
+++ resources/platform/views/partials/bladesora/members/components/home/_coaches-section.blade.php
@@ -3,7 +3,7 @@
         <div class="tw-flex tw-flex-col tw-grow">
 
             <!-- Section Title -->
-            <div class="tw-flex tw-items-center tw-mb-5 tw-w-full tw-justify-between">
+            <div class="tw-flex tw-items-center tw-mb-3 tw-w-full tw-justify-between">
                 <a href="{{ $subscribedCoachesUrl }}" class="tw-text-[#00101D] dark:tw-text-white tw-pb-1 tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
                     <h2 class="tw-font-bold tw-text-2xl tw-leading-none lg:tw-leading-none lg:tw-text-3xl">Subscribed Coaches</h2>
                 </a>
