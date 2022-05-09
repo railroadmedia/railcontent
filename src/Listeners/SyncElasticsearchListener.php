02e4cc45926652588a3b4365fc6cc6ded8f9e274
Roxana Riza <roxana_r2000@yahoo.com>
2022-05-09 18:27:06 +0300

    Update SyncElasticsearchListener

--- src/Listeners/SyncElasticsearchListener.php
+++ src/Listeners/SyncElasticsearchListener.php
@@ -3,18 +3,15 @@
 namespace Railroad\Railcontent\Listeners;
 
 use Illuminate\Support\Facades\Event;
-use Railroad\Railcontent\Events\ContentUpdated;
-use Railroad\Railcontent\Services\ContentHierarchyService;
 use Railroad\Railcontent\Services\ContentService;
 use Railroad\Railcontent\Services\ElasticService;
-use Railroad\Railcontent\Services\VersionService;
 
 class SyncElasticsearchListener
 {
+    /**
+     * @var ContentService
+     */
     private $contentService;
-
-    private $contentHierarchyService;
-
     /**
      * @var ElasticService
      */
@@ -28,81 +25,9 @@ class SyncElasticsearchListener
 
     public function handleSync(Event $event)
     {
-        $content = $this->contentService->getById($event->contentId);
-
-        if (config('railcontent.use_elastic_search')){
-
-            $client = $this->elasticService->getClient();
-
-            $contentID = $content['id'];
-
-            //get progress on content
-//            $userContentPogress =
-//                $oArgs->getEntityManager()
-//                    ->getRepository(UserContentProgress::class);
-//            $allProgress = $userContentPogress->countContentProgress($contentID);
-//
-//            $lastWeekProgress = $userContentPogress->countContentProgress(
-//                $contentID,
-//                Carbon::now()
-//                    ->subWeek(1)
-//            );
-
-            $elasticData = array_merge(
-                [
-//                    'all_progress_count' => $allProgress,
-//                    'last_week_progress_count' => $lastWeekProgress,
-                ],
-                $this->contentService->getElasticData($contentID)
-            );
-
-            $paramsContent = [
-                'index' => 'content',
-                'body' => $elasticData,
-            ];
-
-            // Create indexes if not exists
-            if (!$client->indices()
-                ->exists(['index' => 'content'])) {
-                $this->elasticService->createContentIndex();
-            }
-
-            //update or create document
-            try {
-                $paramsSearch = [
-                    'index' => 'content',
-                    'body' => [
-                        'query' => [
-                            'term' => [
-                                'content_id' => "$contentID",
-                            ],
-                        ],
-                    ],
-                ];
-
-                $documents = $client->search($paramsSearch);
-
-                //delete document if exists
-                foreach ($documents['hits']['hits'] as $elData) {
-                    $paramsDelete = [
-                        'index' => 'content',
-                        'id' => $elData['_id'],
-                        'refresh' => true,
-                    ];
-
-                    $client->delete($paramsDelete);
-                }
-
-                $client->index($paramsContent);
-            } catch (Exception $exception) {
-                error_log('Can not delete elasticsearch index '.print_r($exception->getMessage(), true));
-            }
+        if (config('railcontent.use_elastic_search')) {
+            $content = $this->contentService->getById($event->contentId);
+            $this->elasticService->syncDocument($content);
         }
     }
-
-    public function handleDelete(Event $event)
-    {
-
-    }
-
 }
