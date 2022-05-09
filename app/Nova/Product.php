18b8a4f99ebce0acf58548bfa9ecc74919edfa70
peterminhlee <petereminhlee@gmail.com>
2022-05-09 14:10:21 -0700

    added more fields

--- app/Nova/Product.php
+++ app/Nova/Product.php
@@ -3,14 +3,20 @@
 namespace App\Nova;
 
 use App\Nova\Flexible\Layouts\FeatureLayout;
+use App\Nova\Flexible\Layouts\SpectLayout;
 use App\Nova\Flexible\Presets\FeaturePreset;
+use App\Nova\Flexible\Presets\SpecPreset;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\File;
+use Laravel\Nova\Fields\Heading;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Image;
 use Laravel\Nova\Fields\Markdown;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Whitecube\NovaFlexibleContent\Flexible;
 
@@ -53,13 +59,35 @@ class Product extends Resource
             BelongsTo::make('ProductType', 'productType', 'App\Nova\ProductType'),
             Text::make('Name'),
             Text::make('Slug')->hideFromIndex(),
+            Text::make('Meta Description', 'meta_desc')->hideFromIndex(),
+            Text::make('Short Description', 'short_desc')->hideFromIndex(),
             Text::make('Header Text', 'header_text')->hideFromIndex(),
-            Image::make('Logo')->hideFromIndex(),
-            Flexible::make('Features')
-                        ->addLayout(FeatureLayout::class)
-                        ->preset(FeaturePreset::class),
             Currency::make('Price')->hideFromIndex(),
             Currency::make('Discounted Price', 'discounted_price')->hideFromIndex(),
+            Text::make('Special Text', 'special_text')->hideFromIndex(),
+            Flexible::make('Features/Topics')
+                ->addLayout(FeatureLayout::class)
+                ->preset(FeaturePreset::class),
+            Flexible::make('Specs')
+                ->addLayout(SpectLayout::class)
+                ->preset(SpecPreset::class),
+            Boolean::make('Visible')->default(true)->hideFromIndex(),
+            Boolean::make('Free Bonus', 'free_bonus')->default(false)->hideFromIndex(),
+            Boolean::make('Guarantee Badge', 'guaranteed')->default(false)->hideFromIndex(),
+            Boolean::make('Lifetime Access', 'lifetime_access')->default(false)->hideFromIndex(),
+            Boolean::make('Free Shipping', 'free_shipping')->default(false)->hideFromIndex(),
+            Heading::make('Lesson'),
+            Image::make('Logo')
+                ->disk('s3')
+                ->prunable()
+                ->storeAs(function (Request $request){
+                    return $request->file('logo')->getClientOriginalName();
+                }),
+            Text::make('Video Link', 'video_src')->hideFromIndex(),
+            Text::make('Instructor Name', 'instructor_name')->hideFromIndex(),
+            Text::make('Study Text', 'study_text')->hideFromIndex(),
+            Markdown::make('Overview')->hideFromIndex(),
+            Markdown::make('Instructor Description', 'instructor_desc')->hideFromIndex(),
         ];
     }
 
