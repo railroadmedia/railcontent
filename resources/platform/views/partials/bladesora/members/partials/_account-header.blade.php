6683da550cc8e5bb798dbb4b33cc8af88d7c20ca
calebfavor <calebfavor@gmail.com>
2022-05-10 10:10:39 -0700

    Fixing user dashboard using logged in users information instead of the URL user ids information.

--- resources/platform/views/partials/bladesora/members/partials/_account-header.blade.php
+++ resources/platform/views/partials/bladesora/members/partials/_account-header.blade.php
@@ -12,9 +12,9 @@
     data-ix-bg="{{ $backgroundImage }}"
 >
     <div class="header-gradient-overlay absolute-fill {{ $brand }}"></div>
-    
+
     <div class="account-header tw-container tw-flex tw-flex-col tw-items-center lg:tw-items-end xl:tw-items-center lg:tw-flex-row tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white tw-relative tw-z-10 ">
-        
+
         {{-- Avatar Image --}}
         <div class="header-avatar tw-flex tw-flex-col tw-mb-4 lg:tw-mb-0">
             <div class="user-avatar
@@ -25,16 +25,16 @@
                 <div class="no-decoration tw-bg-cover tw-bg-top tw-inline-block tw-rounded-full tw-h-[175px] tw-w-[175px]"
                    style="background-image: url(https://musora.imgix.net/https%3A%2F%2Fs3.amazonaws.com%2Fpianote%2Fdefaults%2Favatar.png?ixlib=js-2.3.2&amp;fit=crop&amp;crop=faces%2Cedges&amp;auto=format&amp;w=171&amp;h=171&amp;dpr=1&amp;s=1bfa63f0a133082f4c2edb5f7f252f25)"
                 >
-                    @if($user->profile_picture_url)
-                        <img class="tw-inline-block tw-rounded-full" 
-                            src="{{ cf_img($user->profile_picture_url, ["quality" => 50, "blur" => 2, "width" => 175, "height" => 175, "fit" => "crop"]) }}"
-                            data-ix-src="{{ $user->profile_picture_url }}"
+                    @if($userAvatar)
+                        <img class="tw-inline-block tw-rounded-full"
+                            src="{{ cf_img($userAvatar, ["quality" => 50, "blur" => 2, "width" => 175, "height" => 175, "fit" => "crop"]) }}"
+                            data-ix-src="{{ $userAvatar }}"
                         >
                     @endif
                 </div>
             </div>
         </div>
-        
+
         <div class="tw-flex tw-flex-col xl:tw-flex-row tw-w-full tw-justify-end xl:tw-items-center tw-pl-6">
             {{-- Account Header --}}
             <div class="tw-flex tw-w-full tw-items-center">
@@ -52,19 +52,19 @@
             </div>
 
             {{-- Calls To Action --}}
-            @if($user->access_level !== 'pack')
+            @if($user->access_level !== 'pack' && ($isCurrentUsersProfile ?? true))
                 <div class="tw-flex tw-items-center tw-justify-center lg:tw-justify-start xl:tw-justify-end tw-w-full tw-flex-wrap xl:tw-flex-nowrap">
 
-                    <!-- Referral Button -->   
-                    <a href="/referral/invite-a-friend" 
+                    <!-- Referral Button -->
+                    <a href="/referral/invite-a-friend"
                         class="tw-btn-primary tw-w-auto tw-inline-flex tw-bg-{{$brand}} tw-mb-2 tw-max-w-[267px] tw-mx-2 "
                     >
-                        <i aria-hidden="true" class="fas fa-gift md:tw-mr-2"></i> 
+                        <i aria-hidden="true" class="fas fa-gift md:tw-mr-2"></i>
                         <span class="tw-leading-none tw-mt-0.5">invite a friend</span>
                     </a>
                     {{-- Complete Your Account --}}
                     @if(true) {{-- Check if User Has Finished Account --}}
-                        <a href="/referral/invite-a-friend" 
+                        <a href="/referral/invite-a-friend"
                             class="tw-btn-secondary tw-text-white tw-w-auto tw-inline-flex tw-max-w-[267px] tw-mx-2"
                         >
                             Complete Your Account
