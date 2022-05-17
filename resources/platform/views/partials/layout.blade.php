ce467396c270b69b351f0c0401723d8fe2230103
calebfavor <calebfavor@gmail.com>
2022-05-17 19:37:26 +0000

    update font declaration

--- resources/platform/views/partials/layout.blade.php
+++ resources/platform/views/partials/layout.blade.php
@@ -6,17 +6,12 @@
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=5">
         @yield('meta')
 
-        {{-- Google Fonts --}}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-
         {{-- Icons --}}
         <script src="https://kit.fontawesome.com/cf2f4c6c71.js" crossorigin="anonymous"></script>
         <link href="https://d1prhhmg8i11jr.cloudfront.net/v1.0.3/dist/icons.css" rel="stylesheet">
 
         {{-- Styles --}}
-        <link rel="stylesheet" href="{{ mix('platform/css/app.css') }}">
+        <link rel=" preload" as="style" href="{{ mix('platform/css/app.css') }}" onload="this.rel='stylesheet' ">
         @yield('layout-styles')
 
         @include('partials._favicons')
