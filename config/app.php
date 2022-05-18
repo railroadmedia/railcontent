f80bd8bc8b2d055b5708a129063d86f762434c55
calebfavor <calebfavor@gmail.com>
2022-05-18 12:16:51 -0700

    Installing laravel vapor and assets/auth.

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\VaporUiServiceProvider::class,
 
     ],
 
