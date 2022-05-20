ca03f93f6b7197af08a191565a0d4fa883bad367
peterminhlee <petereminhlee@gmail.com>
2022-05-20 09:24:55 -0700

    created models

---
+++ app/Models/Size.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Size extends Model
+{
+    use HasFactory;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    protected $guarded = [
+        'id'
+    ];
+}
