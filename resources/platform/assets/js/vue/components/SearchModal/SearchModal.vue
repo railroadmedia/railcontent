f0392ef253753e07287bdfd64790cab5f6129778
calebfavor <calebfavor@gmail.com>
2022-05-26 17:47:13 +0000

    search changes

--- resources/platform/assets/js/vue/components/SearchModal/SearchModal.vue
+++ resources/platform/assets/js/vue/components/SearchModal/SearchModal.vue
@@ -5,21 +5,20 @@ import MusoraIcon from "../MusoraIcons/MusoraIcon.vue";
 import { XIcon, SearchIcon } from "@heroicons/vue/solid";
 import { bgColor } from "../../../constants/brands"
 const props = defineProps({
-    brand: {
-        type: String,
-        default: 'drumeo'
-    }
+  brand: {
+    type: String,
+    default: 'drumeo'
+  }
 });
 const emit = defineEmits(['onClose']);
 const onSearch = () => {
-    // handle search
+  // handle search
 };
 </script>
 
 <template>
   <ModalRenderer @onClose="() => emit('onClose')">
-    <div
-      class="
+    <div class="
         lg:tw-hidden
         tw-block tw-w-[338px] tw-h-[180px]
         md:tw-w-[612px] md:tw-h-[180px]
@@ -30,22 +29,14 @@ const onSearch = () => {
         tw-border-[#445F74]
         tw-overflow-hidden
         dark:tw-text-[#9EC0DC]
-      "
-      style="
+        tw-border-[1px] tw-border-[#445f74]
+      " style="
         box-shadow: 0px 20px 25px -5px rgba(0, 0, 0, 0.1),
           0px 10px 10px -5px rgba(0, 0, 0, 0.04);
-      "
-    >
+      ">
       <div class="tw-relative tw-h-[62px] tw-flex tw-items-center tw-w-full">
-        <MusoraIcon
-          icon-name="search"
-          class="tw-absolute tw-top-6 tw-left-6 dark:tw-text-[#9EC0DC] tw-z-0"
-        />
-        <InputLabel
-          placeholder="Search"
-          @onChange="handleChange"
-          wrapperOverride="tw-flex-grow"
-          :inputOverride="`
+        <MusoraIcon icon-name="search" class="tw-absolute tw-top-6 tw-left-6 dark:tw-text-[#9EC0DC] tw-z-0" />
+        <InputLabel :showClearButton="true" placeholder="Search" @onChange="handleChange" wrapperOverride="tw-flex-grow" :inputOverride="`
         tw-outline-offset-0
         tw-relative
         tw-w-full
@@ -57,18 +48,17 @@ const onSearch = () => {
         tw-bg-transparent
         tw-shadow-none
         tw-pl-[48px]
-        `"
-          @onFocus="handleFocus"
-        />
+        tw-pr-[36px]
+        focus:tw-ring-0
+        `" @onFocus="handleFocus" />
         <div class="tw-mr-[24px]">
-          <button @click="onSearch" :class="`tw-rounded-full tw-w-[36px] tw-h-[36px] tw-flex tw-items-center tw-justify-center ${bgColor[brand]}`">
-              <SearchIcon class="tw-w-[24px] tw-h-[24px]" />
+          <button @click="onSearch"
+            :class="`tw-rounded-full tw-w-[36px] tw-h-[36px] tw-flex tw-items-center tw-justify-center ${bgColor[brand]}`">
+            <SearchIcon class="tw-w-[24px] tw-h-[24px]" />
           </button>
         </div>
       </div>
-      <div
-        class="tw-border-t-[1px] tw-border-[#445f74] tw-w-full tw-flex-grow"
-      ></div>
+      <div class="tw-border-t-[1px] tw-border-[#445f74]/40 tw-w-full tw-flex-grow"></div>
     </div>
   </ModalRenderer>
 </template>
