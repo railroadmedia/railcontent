98a39871e45c0425bb741c1bc53dd133140fae85
calebfavor <calebfavor@gmail.com>
2022-05-27 21:11:27 +0000

    method header

--- resources/platform/views/partials/_learning-path.blade.php
+++ resources/platform/views/partials/_learning-path.blade.php
@@ -26,14 +26,25 @@
                         style="width:540px;max-width:100%;"
                 >
                 {{-- todo: re-add if requested --}}
-                <a data-open-modal="surveyModal" class="tw-btn-primary tw-bg-{{ $brand }}">
-                   WHERE TO BEGIN &nbsp; <i class="fas fa-question-circle tw-text-base"></i> 
-               </a>
+                @if($brand !== "guitareo")
+                    <a data-open-modal="surveyModal" class="tw-btn-primary tw-bg-{{ $brand }}">
+                        WHERE TO BEGIN &nbsp; <i class="fas fa-question-circle tw-text-base"></i> 
+                    </a>
+                @endif
+
+               {{-- Survey Modal --}}
                <div id="surveyModal" class="modal">
                    <div class="flex flex-column corners-10">
-                       <div class="typeform-widget" data-url="https://form.typeform.com/to/huIKxOSj?typeform-medium=embed-snippet" style="width: 100%; height: 600px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
-                   </div>
+                        @if($brand === "drumeo")
+                            <div class="typeform-widget" data-url="https://form.typeform.com/to/rnPhq70s?typeform-medium=embed-snippet" style="width: 100%; height: 600px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+                        @elseif($brand === "pianote")
+                            <div class="typeform-widget" data-url="https://form.typeform.com/to/huIKxOSj?typeform-medium=embed-snippet" style="width: 100%; height: 600px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+                        @elseif($brand === "singeo")
+                            <div class="typeform-widget" data-url="https://form.typeform.com/to/huIKxOSj?typeform-medium=embed-snippet" style="width: 100%; height: 600px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
+                        @endif
+                    </div>
                </div>
+
             @elseif(!empty($learningPathSlug) && $learningPathSlug == 'foundations-2019')
                 <p class="text-white font-bold heading">Singeo Foundations</p>
                 <div class="flex flex-row align-left mt-3">
@@ -50,38 +61,15 @@
             @endif
 
             <div id="previewModal" class="modal">
-                <div class="flex flex-column corners-10">
-                    {{-- <video-player
-                       ref="learningPathPreview"
-                       theme-color="{{ $brand }}"
-                       poster="{{ $parentContent['video_poster_image_url'] ?? '' }}"
-                       :sources="{{ json_encode($parentContent['video_playback_endpoints'] ?? []) }}"
-                       hls-manifest-url="{{ $lessonContent['hlsManifestUrl'] ?? '' }}"
-                       captions="{{ $parentContent->fetch('fields.video.data.captions', $parentContent['captions'][0] ?? null) }}"
-                       :current-second="0"
-                       video-id="{{ $parentContent->fetch('fields.video.fields.vimeo_video_id') }}"
-                       content-id="{{ $parentContent->fetch('id') }}"
-                       user-id="{{ current_user()->getId() }}"
-                       cast-title="{{ $parentContent->fetch('fields.title') }}"
-                       :use-intersection-observer="true"
-                       @play="handleVideoPlay"
-                       @pause="handleVideoPause"
-                       :controls="{
-                           backward: false,
-                           forward: false,
-                           progress: true,
-                           play: true,
-                           time: true,
-                           volume: true,
-                           settings: false,
-                           fullscreen: false,
-                       }"
-                   >
-                   </video-player> --}}
+                <div class="flex flex-column corners-10">                    
                     <div class="video-wrap">
                         <div class="widescreen">
                             <div class="flex flex-column video-player user-active">
-                                <iframe style="max-width: 100%; width: 100%; height: 100%; position: absolute; top: 0; left: 0;z-index: 1;" src="//player.vimeo.com/video/494183465" frameborder="0" allowfullscreen></iframe>
+                                @if($brand==="drumeo")
+                                    <video playsinline="" preload="metadata" poster="https://i.vimeocdn.com/video/843279057-358419152f59d5352707c1c85a75724753c93c76973108da1181f0038ca10b7d-d_1280x720?r=pad" src="https://player.vimeo.com/progressive_redirect/playback/382231267/rendition/1440p/file.mp4?loc=external&amp;oauth2_token_id=1284792283&amp;signature=55e37c6052112dca72a44fb204f831b81550b8ce2f0bb00eb754cda55689ed61"><!----></video>
+                                @elseif($brand==="pianote")
+                                    <iframe style="max-width: 100%; width: 100%; height: 100%; position: absolute; top: 0; left: 0;z-index: 1;" src="//player.vimeo.com/video/494183465" frameborder="0" allowfullscreen></iframe>
+                                @endif
                             </div>
                         </div>
                     </div>
