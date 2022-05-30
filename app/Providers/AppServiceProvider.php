5b386d8f78303a7d585cb5bd30ebd7866c54f697
Roxana Riza <roxana_r2000@yahoo.com>
2022-05-30 11:40:32 +0300

    Add RailTracker provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\ViewComposers\NavigationViewComposer;
 use Illuminate\Support\ServiceProvider;
 use Railroad\MusoraApi\Contracts\ProductProviderInterface;
 use Railroad\MusoraApi\Contracts\UserProviderInterface;
+use Railroad\MusoraApi\Contracts\RailTrackerProviderInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,5 +31,6 @@ class AppServiceProvider extends ServiceProvider
 
         app()->instance(UserProviderInterface::class, app()->make(MusoraApiUserProvider::class));
         app()->instance(ProductProviderInterface::class, app()->make(MusoraApiProductProvider::class));
+        app()->instance(RailTrackerProviderInterface::class, app()->make(RailTrackerProvider::class));
     }
 }
