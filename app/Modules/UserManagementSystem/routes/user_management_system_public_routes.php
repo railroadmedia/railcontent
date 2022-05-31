78348627e2b37764add4e60f8d8f6e130038a267
Roxana Riza <roxana_r2000@yahoo.com>
2022-05-31 20:23:51 +0300

    Add mobile app middlewares after token login

--- app/Modules/UserManagementSystem/routes/user_management_system_public_routes.php
+++ app/Modules/UserManagementSystem/routes/user_management_system_public_routes.php
@@ -11,8 +11,11 @@ Route::group(
          */
         Route::post(
             'login/token',
-            AuthenticationController::class . '@loginToken'
-        )
+            AuthenticationController::class . '@loginToken',
+        )->middleware(     [
+//            \Railroad\Ecommerce\Middleware\SyncInAppPurchasedItems::class,
+            \Railroad\MusoraApi\Middleware\AddMemberData::class,
+        ])
             ->name('user_management_system.login.token');
 
         Route::get(
