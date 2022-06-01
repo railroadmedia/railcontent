b7e2369cf97cd68cf6f0ad516b3aa3e36d6d862a
calebfavor <calebfavor@gmail.com>
2022-06-01 13:15:39 -0700

    We no longer need usora session salt for any auth purposes.

--- app/Modules/UserManagementSystem/Providers/UserServiceProvider.php
+++ app/Modules/UserManagementSystem/Providers/UserServiceProvider.php
@@ -20,6 +20,8 @@ class UserServiceProvider extends EloquentUserProvider
      */
     protected $hasher;
 
+    protected array $internalIdentifierCache = [];
+
     /**
      * @param Hasher $hasher
      * @return void
@@ -37,6 +39,30 @@ class UserServiceProvider extends EloquentUserProvider
     public function createModel()
     {
         return new User();
+    }
+
+    /**
+     * Retrieve a user by their unique identifier.
+     *
+     * @param  mixed  $identifier
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function retrieveById($identifier)
+    {
+
+        if (empty($this->internalIdentifierCache[$identifier])) {
+            $model = $this->createModel();
+
+            $result = $this->newModelQuery($model)
+                ->where($model->getAuthIdentifierName(), $identifier)
+                ->first();
+        }
+
+        if (!empty($result)) {
+            $this->internalIdentifierCache[$identifier] = $result;
+        }
+
+        return $this->internalIdentifierCache[$identifier];
     }
 
     /**
@@ -101,17 +127,9 @@ class UserServiceProvider extends EloquentUserProvider
      */
     public function updateSessionSalt(Authenticatable $user, $salt)
     {
-        $user = User::query()->find($user->getAuthIdentifier());
+        // NOTE: session salt is no longer used for anything
 
-        if (!is_null($user)) {
-            $user->session_salt = $salt;
-
-            $user->save();
-
-            return true;
-        }
-
-        return false;
+        return true;
     }
 
     /**
