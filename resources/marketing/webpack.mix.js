2d5daafcf561bc636a7a14b222e7c9638d24ccfe
peterminhlee <petereminhlee@gmail.com>
2022-06-08 13:15:53 -0700

    update

--- resources/marketing/webpack.mix.js
+++ resources/marketing/webpack.mix.js
@@ -15,8 +15,11 @@ require('laravel-mix-merge-manifest');
 
 mix
     .js('resources/marketing/assets/js/app.js', 'public/marketing/js')
-    .postCss('resources/marketing/assets/css/app.css', 'public/marketing/css', [
-        tailwindcss('./resources/marketing/marketing.tailwind.config.js')
-    ])
+    .vue()
+    .sass('resources/marketing/assets/css/app.css', 'public/marketing/css')
+    .options({
+        postCss: [ tailwindcss('./resources/marketing/marketing.tailwind.config.js') ],
+    })
+    .sourceMaps()
     .version()
     .mergeManifest();
