2d2ae1eb2ea303593e8a3e450045c5eb53cff2f3
Roxana Riza <roxana_r2000@yahoo.com>
2022-06-15 20:17:56 +0300

    Replace redio butons with links, update controller to take into consideraton selected brand on user profile - notifications settings page

--- resources/platform/views/account/settings/partials/_brand-notifications-settings.blade.php
+++ resources/platform/views/account/settings/partials/_brand-notifications-settings.blade.php
@@ -1,18 +1,15 @@
-<div class="tw-flex tw-flex-row tw-flex-auto pa-3 tw-text-black dark:tw-text-white tw-py-3" >
+<div class="tw-flex tw-flex-row tw-flex-auto pa-3 tw-text-black dark:tw-text-white tw-py-3">
     @foreach($allBrands as $brand)
         <span class="tw-inline-flex tw-flex-row form-group tw-items-center mr-2">
-            <span class="radio-input tw-mr-2">
-                <input id="notifications-brand-drumeo"
-                       name="brand"
-                       {{ brand() === $brand ? 'checked' : '' }}
-                       value="{{ $brand }}"
-                       type="radio">
-                <span class="toggle"></span>
-            </span>
-
-            <label for="notifications-brand-drumeo" class="toggle-label tw-text-black dark:tw-text-white capitalize">
-                {{ $brand }}
-            </label>
+             <a class="tw-no-underline tw-transition"
+                :href="'?selected-brand={{ $brand }}'"
+             >
+                  <h3
+                      class="{{$selectedBrand == $brand ? 'capitalize tw-text-black dark:tw-text-white tw-border-0 tw-border-solid tw-border-b-[3px]' : 'capitalize tw-text-gray-400 hover:tw-text-gray-500'}}"
+                  >
+                      {{ $brand }}
+                  </h3>
+            </a>
         </span>
     @endforeach
 </div>
