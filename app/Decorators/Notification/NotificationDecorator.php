2636f73a8f47038e13fe52909d9337e51d8ddfd5
Roxana Riza <roxana_r2000@yahoo.com>
2022-06-15 20:25:04 +0300

    Add notification email template and define listeners for post create/ post like events

--- app/Decorators/Notification/NotificationDecorator.php
+++ app/Decorators/Notification/NotificationDecorator.php
@@ -14,10 +14,10 @@ class NotificationDecorator
             $url = str_replace(
                 ['www.drumeo.com', 'laravel/public/members', 'members/forums'],
                 [$domain, brand(), 'forums'],
-                $notification['content']['url']
+                $notification->getContentUrl()
             );
 
-            $notifications[$index]['content']['url'] = $url;
+            $notification->setContentUrl($url);
         }
 
         return $notifications;
