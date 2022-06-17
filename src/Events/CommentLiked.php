c338732e2fe3baf5842f59b0d047a4db5f0c501c
Roxana Riza <roxana_r2000@yahoo.com>
2022-06-17 13:41:30 +0300

    Add brand on comment liked and comment created events - for notifications

--- src/Events/CommentLiked.php
+++ src/Events/CommentLiked.php
@@ -10,21 +10,72 @@ class CommentLiked extends Event
      * @var integer
      */
     public $commentId;
-
     /**
-     * @var
+     * @var integer
      */
     public $userId;
+    /**
+     * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed|string
+     */
+    private $brand;
 
     /**
-     * CommentCreated constructor.
-     *
-     * @param integer $commentId
+     * @param $commentId
      * @param $userId
+     * @param string|null $brand
      */
-    public function __construct($commentId, $userId)
+    public function __construct($commentId, $userId, ?string $brand = null)
     {
         $this->commentId = $commentId;
         $this->userId = $userId;
+        $this->brand = $brand ?? config('railcontent.brand');
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * @param string $brand
+     */
+    public function setBrand(string $brand)
+    {
+        $this->brand = $brand;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCommentId()
+    {
+        return $this->commentId;
+    }
+
+    /**
+     * @param int $commentId
+     */
+    public function setCommentId(int $commentId)
+    {
+        $this->commentId = $commentId;
     }
 }
