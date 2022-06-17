acca0cf3d3b4142bcb8da24394c608f0071dc1c7
Roxana Riza <roxana_r2000@yahoo.com>
2022-06-17 13:03:13 +0300

    Add brand on comment liked and comment created events - for notifications

--- src/Events/CommentCreated.php
+++ src/Events/CommentCreated.php
@@ -10,35 +10,116 @@ class CommentCreated extends Event
      * @var integer
      */
     public $commentId;
-
     /**
      * @var
      */
     public $userId;
-
     /**
      * @var integer
      */
     public $parentId;
-
     /**
      * @var string
      */
     public $commentText;
+    /**
+     * @var \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed|string
+     */
+    private $brand;
 
     /**
-     * CommentCreated constructor.
-     *
-     * @param integer $commentId
+     * @param $commentId
      * @param $userId
-     * @param integer $parentId
-     * @param string $commentText
+     * @param $parentId
+     * @param $commentText
+     * @param string|null $brand
      */
-    public function __construct($commentId, $userId, $parentId, $commentText)
+    public function __construct($commentId, $userId, $parentId, $commentText, ?string $brand = null)
     {
         $this->commentId = $commentId;
         $this->userId = $userId;
         $this->parentId = $parentId;
         $this->commentText = $commentText;
+        $this->brand = $brand ?? config('railcontent.brand');
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    /**
+     * @param string $brand
+     */
+    public function setBrand(string $brand)
+    {
+        $this->brand = $brand;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCommentText()
+    {
+        return $this->commentText;
+    }
+
+    /**
+     * @param string $commentText
+     */
+    public function setCommentText(string $commentText)
+    {
+        $this->commentText = $commentText;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCommentId()
+    {
+        return $this->commentId;
+    }
+
+    /**
+     * @param int $commentId
+     */
+    public function setCommentId(int $commentId)
+    {
+        $this->commentId = $commentId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param int $parentId
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
     }
 }
