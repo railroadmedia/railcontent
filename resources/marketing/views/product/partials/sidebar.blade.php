986a632abab8a44470a36fa8257933ab4aca82be
peterminhlee <petereminhlee@gmail.com>
2022-07-03 14:21:18 -0700

    conditional statement changes

--- resources/marketing/views/product/partials/sidebar.blade.php
+++ resources/marketing/views/product/partials/sidebar.blade.php
@@ -101,14 +101,14 @@
                 </button>
             @endif
             <p class="italic text-center mx-auto my-0 text-xs">
-                @if(!empty($freeShipping))
+                @if($freeShipping)
                     <i class="fas fa-truck"></i> <strong>FREE SHIPPING!</strong><br>
                 @endif
                 You can also order by phone toll-free at<br class="hidden sm:inline">
                 <a class="text-{{ $theme }} text-xs" href="tel:1-800-439-8921">1-800-439-8921</a> or directly at
                 <a class="text-{{ $theme }} text-xs" href="tel:1-604-855-7605">1-604-855-7605</a>. </p>
         </div>
-        @if(!empty($guaranteeBadge))
+        @if($guaranteeBadge)
             <div class="flex justify-center items-center px-5 pb-5 text-center md:pt-2 md:pt-4 md:pb-6 lg:p-5 lg:-mt-1 lg:mx-auto lg:mb-0 border-t-0 lg:border-t" style="border-color: #CCD3D3; border-top-style: solid;">
                 <img class="w-24 my-0 pr-6" src="https://cdn.musora.com/image/fetch/w_170,q_auto:best/{{ $badge }}">
                 <p class="text-xs font-bold text-left my-0 text-{{ $theme }}">Your entire order is backed by<br class="lg:hidden" /> our 90-Day Money Back <br class="lg:hidden" /> Guarantee.</p>
