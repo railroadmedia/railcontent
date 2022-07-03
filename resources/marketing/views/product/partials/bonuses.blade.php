c535e3defe5fbae6b6cf504de0fac43bc5a977d6
peterminhlee <petereminhlee@gmail.com>
2022-07-03 14:17:56 -0700

    bundle partial

--- resources/marketing/views/product/partials/bonuses.blade.php
+++ resources/marketing/views/product/partials/bonuses.blade.php
@@ -1,26 +1,26 @@
-@foreach ($bonuses as $bonus)
-<div class="bonus-pic tw-mb-7 md:tw-flex md:tw-items-start md:tw-mb-10 lg:tw-items-center">
-<div class="image-wrap tw-relative tw-overflow-hidden tw-mb-4 md:tw-mb-0 md:tw-mr-5 tw-mx-auto @if($bonus['freeShipping']) shipping @endif">
-    <img class="tw-w-full tw-rounded-lg" src="{{ $bonus['img'] }}">
-</div>
-<div>
-    <strong>{{ $bonus['name'] }} - <span @if($bonus['priceColor'] == 'orange') class="orange" @endif>{{ $bonus['price']}}</span>
-            <br @if(!$bonus['lineBreak']) class="tw-hidden" @endif>
+@foreach ($product->bundles as $key => $bundle)
+    <div class="bonus-pic mb-7 md:flex md:items-start md:mb-10 lg:items-center">
+        <div class="image-wrap relative overflow-hidden mb-4 md:mb-0 md:mr-5 mx-auto @if($bundle['bundle_free_shipping']) shipping @endif">
+            <img class="w-full rounded-lg" src="@if(str_contains($bundle['thumbnail'], 'amazonaws') || str_contains($bundle['thumbnail'], 'cloudfront')){{ $bundle['thumbnail'] }} @else https://laravel-nova.s3.us-east-2.amazonaws.com/{{ $bundle['thumbnail']  }}@endif" alt="product thumbnail{{$key}}">
+        </div>
+        <div>
+            <strong>{{ $bundle['name'] }} - <span @if($bundle['priceColor'] == 'orange') class="orange" @endif>{{ $bundle['price']}}</span>
+                    <br @if(!$bundle['bundle_free_shipping'] && !$bundle['bundle_lifetime_access']) class="hidden" @endif>
 
-            @if($bonus['freeBonus'])
-                <span style="color:#0B76DB;">*FREE BONUS*</span>
-            @endif
+                    @if($bundle['free_bonus'])
+                        <span style="color:#0B76DB;">*FREE BONUS*</span>
+                    @endif
 
-            @if($bonus['lifetimeAccess'])
-                <span style="color:#E69500;">LIFETIME ACCESS</span>
-            @endif   
+                    @if($bundle['bundle_lifetime_access'])
+                        <span style="color:#E69500;">LIFETIME ACCESS</span>
+                    @endif
 
-            @if($bonus['freeShipping'])
-                <span style="color:#E69500;">FREE SHIPPING</span>
-            @endif 
-    </strong><br>
-    {{ $bonus['desc'] }}
-    <br>
-</div>
-</div>
+                    @if($bundle['bundle_free_shipping'])
+                        <span style="color:#E69500;">FREE SHIPPING</span>
+                    @endif
+            </strong><br>
+            {{ $bundle['short_desc'] }}
+            <br>
+        </div>
+    </div>
 @endforeach
