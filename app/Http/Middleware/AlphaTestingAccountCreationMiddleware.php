570291f98b3fb28e533dee6736f525d6f36e4bd3
calebfavor <calebfavor@gmail.com>
2022-07-05 15:58:10 -0700

    Fixing duplicate email check for test user creation tool. Adding docs for how to add new test users

--- app/Http/Middleware/AlphaTestingAccountCreationMiddleware.php
+++ app/Http/Middleware/AlphaTestingAccountCreationMiddleware.php
@@ -33,7 +33,7 @@ class AlphaTestingAccountCreationMiddleware
         Request $request,
         Closure $next
     ) {
-        if ($request->routeIs('user_management_system.login.cookie')) {
+        if ($request->routeIs('user_management_system.login.cookie') && !User::query()->where('email', $request->get('email'))->exists()) {
             $user = new User;
 
             $user->email = $request->get('email');
