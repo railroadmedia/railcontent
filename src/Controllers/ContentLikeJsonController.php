b8685001cee3e94c822a4b41f7ff43d129783417
Roxana Riza <roxana_r2000@yahoo.com>
2022-07-13 11:44:14 +0300

    Update ‘like_count’ content column when the authenticated user like/dislike the content

--- src/Controllers/ContentLikeJsonController.php
+++ src/Controllers/ContentLikeJsonController.php
@@ -7,25 +7,23 @@ use Illuminate\Routing\Controller;
 use Railroad\Railcontent\Requests\CommentUnLikeRequest;
 use Railroad\Railcontent\Services\ConfigService;
 use Railroad\Railcontent\Services\ContentLikeService;
+use Railroad\Railcontent\Services\ContentService;
 use Railroad\Railcontent\Transformers\DataTransformer;
 
 class ContentLikeJsonController extends Controller
 {
-    /**
-     * @var ContentLikeService
-     */
-    private $contentLikeService;
+    private ContentLikeService $contentLikeService;
+    private ContentService $contentService;
 
     /**
-     * ContentLikeJsonController constructor.
-     *
      * @param ContentLikeService $contentLikeService
+     * @param ContentService $contentService
      */
-    public function __construct(ContentLikeService $contentLikeService)
+    public function __construct(ContentLikeService $contentLikeService, ContentService $contentService)
     {
-
         $this->middleware(ConfigService::$controllerMiddleware);
         $this->contentLikeService = $contentLikeService;
+        $this->contentService = $contentService;
     }
 
     /**
@@ -53,50 +51,56 @@ class ContentLikeJsonController extends Controller
                 )
         );
 
-        return reply()->json(
-            $likes,
-            [
-                'totalResults' => $count,
-                'transformer' => DataTransformer::class,
-            ]
-        );
+        return reply()->json($likes, [
+                                       'totalResults' => $count,
+                                       'transformer' => DataTransformer::class,
+                                   ]);
     }
 
     /**
-     * Authenticated user like content.
-     *
      * @param Request $request
      * @return mixed
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \ReflectionException
      */
     public function like(Request $request)
     {
         $like = $this->contentLikeService->like($request->get('content_id'), auth()->id());
+        $content = $this->contentService->getById($request->get('content_id'));
+        $this->contentService->update($request->get('content_id'), [
+            'like_count' => $content['like_count'] + 1,
+        ]);
 
-        return reply()->json(
-            [$like],
-            [
-                'code' => $like ? 200 : 500,
-                'transformer' => DataTransformer::class,
-            ]
-        );
+        return reply()->json([$like], [
+                                        'code' => $like ? 200 : 500,
+                                        'transformer' => DataTransformer::class,
+                                    ]);
     }
 
     /**
-     * Authenticated user unlike content.
-     *
      * @param CommentUnLikeRequest $request
      * @return mixed
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \ReflectionException
      */
     public function unlike(CommentUnLikeRequest $request)
     {
         $amountDeleted = $this->contentLikeService->unlike($request->get('content_id'), auth()->id());
-
-        return reply()->json(
-            [[$amountDeleted > 0]],
-            [
-                'code' => $amountDeleted ? 200 : 500,
-                'transformer' => DataTransformer::class,
-            ]
-        );
+        $content = $this->contentService->getById($request->get('content_id'));
+        if($content['like_count'] > 0) {
+            $this->contentService->update($request->get('content_id'), [
+                'like_count' => $content['like_count'] - 1,
+            ]);
+        }
+        return reply()->json([[$amountDeleted > 0]], [
+                                                       'code' => $amountDeleted ? 200 : 500,
+                                                       'transformer' => DataTransformer::class,
+                                                   ]);
     }
 }
