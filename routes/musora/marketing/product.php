d13569a44cf6a6d6c19ea3197c6916816dde07e3
peterminhlee <petereminhlee@gmail.com>
2022-07-18 16:28:39 -0700

    added custom pages

--- routes/musora/marketing/product.php
+++ routes/musora/marketing/product.php
@@ -9,8 +9,9 @@ Route::get('{brand}/shop', [ProductPagesController::class, 'products'])
     ->name('marketing.products');
 
 //custom product pages
-
-
+Route::get('drumeo/shop/new-drummers', function(){ return view('drumeo.products.full-page-assets.new-drummers'); });
+Route::get('drumeo/shop/rock-drumming-masterclass', function(){ return view('drumeo.products.full-page-assets.rock-drumming-masterclass'); });
+Route::get('drumeo/shop/drum-technique-made-easy', function(){ return view('drumeo.products.full-page-assets.drum-technique-made-easy'); });
 
 Route::get('{brand}/shop/{product:slug}', [ProductPagesController::class, 'product'])
     ->whereIn('brand', all_brands())
