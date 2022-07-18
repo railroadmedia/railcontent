0ae480fd4f4659315ac45be04a9085e7445cab84
peterminhlee <petereminhlee@gmail.com>
2022-07-18 16:28:30 -0700

    query update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -35,7 +35,7 @@ class Product extends Model
     public function sizes()
     {
         return $this->hasManyThrough(Size::class, ProductSize::class, 'product_id', 'id', 'id', 'size_id')
-                    ->addSelect(['product_sizes.sold_out', 'product_sizes.id', 'sizes.name', 'sizes.id as sizeId'])->orderBy('sizeId');
+                    ->addSelect(['product_sizes.sold_out', 'product_sizes.id', 'sizes.name', 'sizes.id as sizeId', 'sizes.code'])->orderBy('sizeId');
     }
 
     public function product_size()
@@ -56,6 +56,43 @@ class Product extends Model
     public function sizeChart()
     {
         return $this->belongsTo(SizeChart::class, 'size_chart_id');
+    }
+
+    public function save(array $options = [])
+    {
+       Product::create([
+           'brand_id' => $this->brand_id,
+           'product_type_id' => $this->product_type_id,
+           'name' => $this->name,
+           'slug' => $this->slug,
+           'sku' => $this->sku,
+           'thumbnail' => $this->thumbnail,
+           'header_text' => $this->header_text,
+           'short_desc' => $this->short_desc,
+           'meta_desc' => $this->meta_desc,
+           'meta_img' => $this->meta_img,
+           'special_text' => $this->special_text,
+           'thumbnail_logo' => $this->thumbnail_logo,
+           'study_text' => $this->study_text,
+           'page_logo' => $this->page_logo,
+           'price' => $this->price,
+           'discounted_price' => $this->discounted_price,
+           'video_src' => $this->video_src,
+           'overview' => $this->overview,
+           'instructor_name' => $this->instructor_name,
+           'product_img' => $this->product_img,
+           'instructor_desc' => $this->instructor_desc,
+           'sold_out' => $this->sold_out,
+           'free_shipping' => $this->free_shipping,
+           'guaranteed' => $this->guaranteed,
+           'visible' => $this->visible,
+           'free_bonus' => $this->free_bonus,
+           'membership_discount' => $this->membership_discount,
+           'lifetime_access' => $this->lifetime_access,
+           'size_case_sensitive' => $this->size_case_sensitive,
+           'created_at' => now(),
+           'updated_at' => now(),
+       ]);
     }
 
     public function delete()
