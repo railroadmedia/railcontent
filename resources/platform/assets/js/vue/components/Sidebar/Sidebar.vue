e954b75c132f42486db0e2200aff4e500671380c
Yalung Tang <yalungtang@gmail.com>
2022-07-18 18:47:40 -0300

    removed console log on sidebar

--- resources/platform/assets/js/vue/components/Sidebar/Sidebar.vue
+++ resources/platform/assets/js/vue/components/Sidebar/Sidebar.vue
@@ -46,9 +46,6 @@ export default {
       let windowPathArray = window.location.pathname.split('/');
       let pathArray = path.split('/');
 
-      console.log(windowPathArray);
-      console.log(pathArray);
-
       // special case for guitareo 500 songs
       if (windowPathArray[3] === '500-songs-in-5-days') {
         return windowPathArray[3] === pathArray[3];
