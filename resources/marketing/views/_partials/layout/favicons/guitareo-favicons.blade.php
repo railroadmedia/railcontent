98cc914580e897f946a89f5b4a88ff9623477a44
calebfavor <calebfavor@gmail.com>
2022-07-21 11:21:55 -0700

    Fixing manifest file URL cors error. Also fixing up drumeo favicons.

--- resources/marketing/views/_partials/layout/favicons/guitareo-favicons.blade.php
+++ resources/marketing/views/_partials/layout/favicons/guitareo-favicons.blade.php
@@ -1,7 +1,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicons/guitareo/apple-touch-icon.png?v=2018') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicons/guitareo/favicon-32x32.png?v=2018') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicons/guitareo/favicon-16x16.png?v=2018') }}">
-<link rel="manifest" href="{{ asset('/favicons/guitareo/site.webmanifest?v=2018') }}">
+<link rel="manifest" crossorigin="use-credentials" href="{{ asset('/favicons/guitareo/site.webmanifest?v=2018') }}">
 <link rel="mask-icon" href="{{ asset('/favicons/guitareo/safari-pinned-tab.svg?v=2018') }}" color="#000000">
 <link rel="shortcut icon" href="{{ asset('/favicons/guitareo/favicon.ico?v=2018') }}">
 <meta name="msapplication-TileColor" content="#000000">
--- resources/marketing/views/_partials/layout/favicons/pianote-favicons.blade.php
+++ resources/marketing/views/_partials/layout/favicons/pianote-favicons.blade.php
@@ -1,7 +1,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicons/pianote/apple-touch-icon.png?v=2018') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicons/pianote/favicon-32x32.png?v=2018') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicons/pianote/favicon-16x16.png?v=2018') }}">
-<link rel="manifest" href="{{ asset('/favicons/pianote/site.webmanifest?v=2018') }}">
+<link rel="manifest" crossorigin="use-credentials" href="{{ asset('/favicons/pianote/site.webmanifest?v=2018') }}">
 <link rel="mask-icon" href="{{ asset('/favicons/pianote/safari-pinned-tab.svg?v=2018') }}" color="#000000">
 <link rel="shortcut icon" href="{{ asset('/favicons/pianote/favicon.ico?v=2018') }}">
 <meta name="msapplication-TileColor" content="#000000">
