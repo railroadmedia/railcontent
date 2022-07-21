97485d31b89024992c9c9b9bf196a164a5b04b63
calebfavor <calebfavor@gmail.com>
2022-07-21 15:06:05 +0000

    update chat layout

--- resources/platform/views/live/online.blade.php
+++ resources/platform/views/live/online.blade.php
@@ -34,7 +34,7 @@
         </div>
     </div>
 
-    <div class="tw-container tw-mx-auto tw-px-4 dark:tw-text-white tw-pt-8 tw-pb-14" style="margin-left: 0 !important; margin-right: 0 !important; width: 100% !important; max-width: 100%; padding-left: 87px; padding-right: 87px;">
+    <div class="tw-flex tw-w-full tw-max-w-[1703px] tw-mx-auto tw-px-4 md:tw-px-8 tw-mt-3 tw-flex-col 2xl:tw-flex-row">
         <div id="lessonInfo" class="tw-flex tw-flex-row tw-items-center">
             <div class="tw-flex tw-flex-col tw-grow tw-shadow">
                 @if(!empty($lessonContent->fetch('*assignments')))
@@ -57,12 +57,12 @@
                 @endif
             </div>
         </div>
-        <div class="tw-flex tw-flex-col mv-3">
+        <div class="tw-flex tw-flex-col mv-3 tw-w-full">
             <div class="tw-flex tw-flex-row mb-3 ph">
                 <h1 class="heading dark:tw-text-white">Live Schedule</h1>
             </div>
 
-            <div class="tw-flex tw-flex-row">
+            <div class="tw-flex tw-flex-row tw-w-full">
                 <content-schedule
                     :preloaded-content="{{ $scheduleEvents }}"
                     theme-color="{{ $brand }}"
