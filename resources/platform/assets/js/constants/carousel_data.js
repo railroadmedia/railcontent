507e6ec72a2adf907d86a0901ca90ae7a323eb8d
calebfavor <calebfavor@gmail.com>
2022-07-21 10:21:38 -0700

    Switching images used for FE assets to use s3/cdn urls.

--- resources/platform/assets/js/constants/carousel_data.js
+++ resources/platform/assets/js/constants/carousel_data.js
@@ -9,7 +9,7 @@ export const testCarousel = [
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
                 ctaUrl: "/drumeo/method",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/drumeo-method+1.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/drumeo-method+1.jpg",
             },
             {
                 topSubtitle: "Featured Coach",
@@ -17,7 +17,7 @@ export const testCarousel = [
                 ctaText: "Visit Dennis's Coach Page",
                 description: "Dennis Chambers is a drumming legend who has recorded and performed with artists such as Santana, Steely Dan, Parliament/Funkadelic, and many more.",
                 ctaUrl: "/drumeo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/drumeo-chambers-coach.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/drumeo-chambers-coach.jpg",
             },
             {
                 topSubtitle: "Popular Songs in All Genres",
@@ -25,7 +25,7 @@ export const testCarousel = [
                 ctaText: "See the latest songs",
                 description:"Full transcriptions, loops, and practice tools for music by popular bands of all eras and styles.",
                 ctaUrl: "/drumeo/songs",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/songs.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/songs.jpg",
             },
             {
                 topSubtitle: "Learn from the legends",
@@ -33,7 +33,7 @@ export const testCarousel = [
                 ctaText: "See Coaches",
                 description: "Drumming legends sharing their best advice and guiding your drumming journey.",
                 ctaUrl: "/drumeo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/coaches.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/coaches.jpg",
             },
         ]
     },
@@ -47,7 +47,7 @@ export const testCarousel = [
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
                 ctaUrl: "/pianote/method",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/pianote-method+1.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/pianote-method+1.jpg",
             },
             {
                 topSubtitle: "Featured Coach",
@@ -55,7 +55,7 @@ export const testCarousel = [
                 ctaText: "Visit Erskine's Coach Page",
                 description: "As a music director for Disney star Zendaya, producer and performer, Erskine Hawkins has done a lot. But his favorite thing in the world is playing gospel piano.",
                 ctaUrl: "/pianote/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/pianote-erskine_coach+1.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/pianote-erskine_coach+1.jpg",
             },
             {
                 topSubtitle: "Popular Songs in All Genres",
@@ -63,7 +63,7 @@ export const testCarousel = [
                 ctaText: "See the latest songs",
                 description:"Full transcriptions, loops, and practice tools for music by popular bands of all eras and styles.",
                 ctaUrl: "/pianote/songs",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/songs.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/songs.jpg",
             },
             {
                 topSubtitle: "Learn from the legends",
@@ -71,7 +71,7 @@ export const testCarousel = [
                 ctaText: "See Coaches",
                 description: "Piano legends sharing their best advice and guiding your piano journey.",
                 ctaUrl: "/pianote/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/coaches.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/coaches.jpg",
             },
         ]
     },
@@ -85,7 +85,7 @@ export const testCarousel = [
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
                 ctaUrl: "/guitareo/method",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/Guitareo-Method_Lesson+3+1.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/Guitareo-Method_Lesson+3+1.jpg",
             },
             {
                 topSubtitle: "Featured Coach",
@@ -93,7 +93,7 @@ export const testCarousel = [
                 ctaText: "Visit Sami's Coach Page",
                 description: 'Sami has been a professional musician, producer & educator for over 20 years, dedicating his life to "helping artists do what they love," as well as spreading the beautiful & universal language of music.',
                 ctaUrl: "/guitareo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/guitareo-sami_coach.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/guitareo-sami_coach.jpg",
             },
             {
                 topSubtitle: "Popular Songs in All Genres",
@@ -101,7 +101,7 @@ export const testCarousel = [
                 ctaText: "See the latest songs",
                 description:"Play popular songs faster with access to chord charts for every style, era, and skill level.",
                 ctaUrl: "/guitareo/songs",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/songs.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/songs.jpg",
             },
             {
                 topSubtitle: "Learn from the legends",
@@ -109,7 +109,7 @@ export const testCarousel = [
                 ctaText: "See Coaches",
                 description: "Guitar legends sharing their best advice and guiding your guitar journey.",
                 ctaUrl: "/guitareo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/coaches.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/coaches.jpg",
             },
         ]
     },
@@ -123,7 +123,7 @@ export const testCarousel = [
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
                 ctaUrl: "/singeo/method",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/singeo-method+1.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/singeo-method+1.jpg",
             },
             {
                 topSubtitle: "Featured Coach",
@@ -131,7 +131,7 @@ export const testCarousel = [
                 ctaText: "Visit SHELÉA's Coach Page",
                 description: "Singer, songwriter, keyboardist, producer, and actress, Sheléa is an inspiring talent who has a very important message on singing your truth.",
                 ctaUrl: "/singeo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/singeo-shelea-coach.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/singeo-shelea-coach.jpg",
             },
             {
                 topSubtitle: "Popular Songs in All Genres",
@@ -139,7 +139,7 @@ export const testCarousel = [
                 ctaText: "See the latest songs",
                 description:"Sing along to songs by popular singers of all eras and styles in our extensive Karaoke library.",
                 ctaUrl: "/singeo/songs",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/songs.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/songs.jpg",
             },
             {
                 topSubtitle: "Learn from the legends",
@@ -147,7 +147,7 @@ export const testCarousel = [
                 ctaText: "See Coaches",
                 description: "Vocal legends sharing their best advice and guiding your singing journey.",
                 ctaUrl: "/singeo/coaches",
-                img: "https://musora-web-platform.s3.amazonaws.com/carousel/coaches.jpg",
+                img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/coaches.jpg",
             },
         ]
     }
