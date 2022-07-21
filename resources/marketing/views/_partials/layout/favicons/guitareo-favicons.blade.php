fb6f2143f9d2d75244f4b12aff031d3e467752db
calebfavor <calebfavor@gmail.com>
2022-07-21 11:01:53 -0700

    Fixing the rest of the favicon links.

--- resources/marketing/views/_partials/layout/favicons/guitareo-favicons.blade.php
+++ resources/marketing/views/_partials/layout/favicons/guitareo-favicons.blade.php
@@ -1,9 +1,9 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/favicons/guitareo/apple-touch-icon.png?v=2018">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicons/guitareo/favicon-32x32.png?v=2018">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicons/guitareo/favicon-16x16.png?v=2018">
-<link rel="manifest" href="/favicons/guitareo/site.webmanifest?v=2018">
-<link rel="mask-icon" href="/favicons/guitareo/safari-pinned-tab.svg?v=2018" color="#000000">
-<link rel="shortcut icon" href="/favicons/guitareo/favicon.ico?v=2018">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicons/guitareo/apple-touch-icon.png?v=2018') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicons/guitareo/favicon-32x32.png?v=2018') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicons/guitareo/favicon-16x16.png?v=2018') }}">
+<link rel="manifest" href="{{ asset('/favicons/guitareo/site.webmanifest?v=2018') }}">
+<link rel="mask-icon" href="{{ asset('/favicons/guitareo/safari-pinned-tab.svg?v=2018') }}" color="#000000">
+<link rel="shortcut icon" href="{{ asset('/favicons/guitareo/favicon.ico?v=2018') }}">
 <meta name="msapplication-TileColor" content="#000000">
-<meta name="msapplication-config" content="/favicon/browserconfig.xml?v=2018">
+<meta name="msapplication-config" content="{{ asset('/favicon/browserconfig.xml?v=2018') }}">
 <meta name="theme-color" content="#ffffff">
