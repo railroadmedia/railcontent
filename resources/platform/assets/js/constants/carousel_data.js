7bd1376ff73bbd2082237c001e2e087bc52f3d96
calebfavor <calebfavor@gmail.com>
2022-07-21 10:37:18 -0700

    Adding correct carousel links.

--- resources/platform/assets/js/constants/carousel_data.js
+++ resources/platform/assets/js/constants/carousel_data.js
@@ -8,7 +8,7 @@ export const testCarousel = [
                 ctaText: "Start Method",
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
-                ctaUrl: "/drumeo/method",
+                ctaUrl: "/drumeo/method/drumeo-method/241247",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/drumeo-method+1.jpg",
             },
             {
@@ -16,7 +16,7 @@ export const testCarousel = [
                 title: "Dennis Chambers",
                 ctaText: "Visit Dennis's Coach Page",
                 description: "Dennis Chambers is a drumming legend who has recorded and performed with artists such as Santana, Steely Dan, Parliament/Funkadelic, and many more.",
-                ctaUrl: "/drumeo/coaches",
+                ctaUrl: "/drumeo/coaches/dennis-chambers/31980",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/drumeo-chambers-coach.jpg",
             },
             {
@@ -46,7 +46,7 @@ export const testCarousel = [
                 ctaText: "Start Method",
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
-                ctaUrl: "/pianote/method",
+                ctaUrl: "/pianote/method/pianote-method/276693",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/pianote-method+1.jpg",
             },
             {
@@ -54,7 +54,7 @@ export const testCarousel = [
                 title: "Erskine Hawkins",
                 ctaText: "Visit Erskine's Coach Page",
                 description: "As a music director for Disney star Zendaya, producer and performer, Erskine Hawkins has done a lot. But his favorite thing in the world is playing gospel piano.",
-                ctaUrl: "/pianote/coaches",
+                ctaUrl: "/pianote/coaches/erskine-hawkins/351708",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/pianote-erskine_coach+1.jpg",
             },
             {
@@ -84,7 +84,7 @@ export const testCarousel = [
                 ctaText: "Start Method",
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
-                ctaUrl: "/guitareo/method",
+                ctaUrl: "/guitareo/method/guitareo-method/333652",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/Guitareo-Method_Lesson+3+1.jpg",
             },
             {
@@ -92,7 +92,7 @@ export const testCarousel = [
                 title: "Sami Ghawi",
                 ctaText: "Visit Sami's Coach Page",
                 description: 'Sami has been a professional musician, producer & educator for over 20 years, dedicating his life to "helping artists do what they love," as well as spreading the beautiful & universal language of music.',
-                ctaUrl: "/guitareo/coaches",
+                ctaUrl: "/guitareo/coaches/sami-ghawi/350843",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/guitareo-sami_coach.jpg",
             },
             {
@@ -122,7 +122,7 @@ export const testCarousel = [
                 ctaText: "Start Method",
                 description:
                     "Exclusive curriculum so you’ll always know what to work on for maximum results.",
-                ctaUrl: "/singeo/method",
+                ctaUrl: "/singeo/method/singeo-method/308514",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/singeo-method+1.jpg",
             },
             {
@@ -130,7 +130,7 @@ export const testCarousel = [
                 title: "SHELÉA",
                 ctaText: "Visit SHELÉA's Coach Page",
                 description: "Singer, songwriter, keyboardist, producer, and actress, Sheléa is an inspiring talent who has a very important message on singing your truth.",
-                ctaUrl: "/singeo/coaches",
+                ctaUrl: "/singeo/coaches/shela/347694",
                 img: "https://musora.com/cdn-cgi/image/quality=85,width=1536,height=430,metadata=none/https://d3fzm1tzeyr5n3.cloudfront.net/carousel/singeo-shelea-coach.jpg",
             },
             {
