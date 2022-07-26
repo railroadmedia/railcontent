2c5a46361e10f303d82d9c653ff952eda101aa85
Yalung Tang <yalungtang@gmail.com>
2022-07-26 12:26:49 -0300

    login form with spinner

--- resources/platform/assets/js/vue/components/Button/RainbowButton.vue
+++ resources/platform/assets/js/vue/components/Button/RainbowButton.vue
@@ -7,12 +7,16 @@ const props = defineProps({
     type: String,
     default: "button",
   },
+  disabled: {
+    type: Boolean,
+    default: true,
+  },
 });
 const emit = defineEmits(["onButtonClick"]);
 const handleClick = (e) => {
   if (props.type === "button") {
     e.preventDefault();
-    emit("onButtonClick");
+    emit("onButtonClick", e);
   }
 };
 </script>
@@ -28,15 +32,42 @@ const handleClick = (e) => {
       tw-h-[50px]
       tw-border-[2px]
       tw-text-[#445F74]
-      hover:tw-text-white
       tw-border-[#445F74] tw-relative
       login-submit-btn
     "
+    :class="disabled ? '' : 'button-active'"
     dusk="submit-button"
     @click="handleClick"
   >
     <span class="tw-font-bebas-neue tw-uppercase">
-      {{ label }}
+      {{ label ? label : '' }}
+      <slot></slot>
     </span>
   </button>
 </template>
+
+<style type="text/css">
+.login-submit-btn:hover, .login-submit-btn.button-active {
+  border: none;
+  color: white !important;
+}
+
+.login-submit-btn:hover::before, .login-submit-btn.button-active::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  border-radius: 25px;
+  border: 2px solid transparent;
+  background: linear-gradient(
+      90.69deg,
+      #00c9ac 1.54%,
+      #0b76db 30.83%,
+      #9a00ee 72.12%,
+      #f61a30 95.24%
+    )
+    border-box;
+  -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+  -webkit-mask-composite: xor;
+  mask-composite: exclude;
+}
+</style>
--- resources/platform/assets/js/vue/components/LoginForm/LoginForm.vue
+++ resources/platform/assets/js/vue/components/LoginForm/LoginForm.vue
@@ -5,9 +5,10 @@
 */
 
 import { ref } from "vue";
-import { bgColor } from "../../../constants/brands";
 import InputLabel from "../InputLabel/InputLabel.vue";
 import RainbowButton from "../Button/RainbowButton.vue";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner.vue";
+
 const props = defineProps({
   brand: {
     type: String,
@@ -36,9 +37,27 @@ const props = defineProps({
     default: "",
   },
 });
+
 const currentForm = ref("login");
+const emailInput = ref('');
+const passwordInput = ref('');
+const isLoading = ref(false);
+
 const changeCurrentForm = (val) => {
   currentForm.value = val;
+};
+
+const handleEmailChange = (val) => {
+  emailInput.value = val;
+};
+
+const handlePasswordChange = (val) => {
+  passwordInput.value = val;
+};
+
+const handleButtonClick = (e) => {
+  isLoading.value = true;
+  document.getElementById('hidden-submit').click();
 };
 </script>
 
@@ -89,6 +108,7 @@ const changeCurrentForm = (val) => {
             labelValue="Email Address"
             placeholder="Enter your email..."
             :inputErrors="[]"
+            @onChange="handleEmailChange"
           />
         </div>
         <div class="tw-flex tw-flex-col tw-mb-[20px]">
@@ -102,10 +122,15 @@ const changeCurrentForm = (val) => {
             labelValue="Password"
             placeholder="Enter your password..."
             :inputErrors="[]"
+            @onChange="handlePasswordChange"
           />
         </div>
         
-        <RainbowButton type="submit" label="SIGN IN" />
+        <RainbowButton :disabled="!passwordInput.length || !emailInput.length" type="button" @on-button-click="handleButtonClick">
+          <span v-if="!isLoading"><LoadingSpinner /> SIGNING IN</span>
+          <span v-if="isLoading">SIGN IN</span>
+        </RainbowButton>
+        <button id="hidden-submit" type="submit" hidden>Submit</button>
         <a
           id="resetToggle"
           class="text-center text-grey-3 noselect tw-text-[16px]"
@@ -163,32 +188,9 @@ const changeCurrentForm = (val) => {
 </template>
 
 <style type="text/css">
-.login-submit-btn:hover {
-  border: none;
-}
-
 .loginEmail-label,
 .loginPassword-label {
   font-weight: 700;
   font-size: 16px;
-}
-
-.login-submit-btn:hover::before {
-  content: "";
-  position: absolute;
-  inset: 0;
-  border-radius: 25px;
-  border: 2px solid transparent;
-  background: linear-gradient(
-      90.69deg,
-      #00c9ac 1.54%,
-      #0b76db 30.83%,
-      #9a00ee 72.12%,
-      #f61a30 95.24%
-    )
-    border-box;
-  -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
-  -webkit-mask-composite: xor;
-  mask-composite: exclude;
 }
 </style>
