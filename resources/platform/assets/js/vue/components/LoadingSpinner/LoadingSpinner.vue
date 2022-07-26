b03ca430b04e53d844e7ac26e658fcf76c752809
Yalung Tang <yalungtang@gmail.com>
2022-07-26 14:09:35 -0300

    login refactor

--- resources/platform/assets/js/vue/components/LoadingSpinner/LoadingSpinner.vue
+++ resources/platform/assets/js/vue/components/LoadingSpinner/LoadingSpinner.vue
@@ -1,5 +1,4 @@
 <script setup>
-import MusoraIcon from '../MusoraIcons/MusoraIcon.vue';
 const props = defineProps({
     classOverride: {
         type: String,
@@ -9,20 +8,8 @@ const props = defineProps({
 </script>
 
 <template>
-    <MusoraIcon width="24" height="24" viewBox="0 0 24 24" icon-name="whistle-filled" class="tw-inline-block tw-w-[25px] tw-h-[25px] MusoraSpinner" :class="classOverride" />
+  <svg class="tw-animate-spin -tw-ml-1 tw-mr-2 tw-h-4 tw-w-4 tw-text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <circle class="tw-opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+    <path class="tw-opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+  </svg>
 </template>
-
-<style scoped>
-.MusoraSpinner {
-    color: white;
-    animation: spin 1s ease-in-out infinite;
-    -webkit-animation: spin 1s ease-in-out infinite;
-}
-
-@keyframes spin {
-  to { -webkit-transform: rotate(360deg); }
-}
-@-webkit-keyframes spin {
-  to { -webkit-transform: rotate(360deg); }
-}
-</style>
