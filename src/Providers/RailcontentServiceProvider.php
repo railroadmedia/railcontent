1bebbb08552aa4ae68314e2407495309d999eff6
Mircea Murasan <mircea.murasan@artsoft-consult.ro>
2022-07-27 16:08:12 +0300

    Migration and fill command for timestamps columns.

--- src/Providers/RailcontentServiceProvider.php
+++ src/Providers/RailcontentServiceProvider.php
@@ -21,6 +21,7 @@ use Railroad\Railcontent\Commands\CreateYoutubeVideoContentRecordsViaClientAPI;
 use Railroad\Railcontent\Commands\DeleteContentAndHierarchiesForUserPlaylists;
 use Railroad\Railcontent\Commands\ExpireCache;
 use Railroad\Railcontent\Commands\FillContentParentContentDataColumnFromHierarchy;
+use Railroad\Railcontent\Commands\FillTimestampsRailcontentTables;
 use Railroad\Railcontent\Commands\MigrateContentColumns;
 use Railroad\Railcontent\Commands\MigrateContentFields;
 use Railroad\Railcontent\Commands\MigrateContentToElasticsearch;
@@ -124,6 +125,7 @@ class RailcontentServiceProvider extends ServiceProvider
             CleanMetadata::class,
             CleanContentTopicsAndStyles::class,
             CalculateContentPopularity::class,
+            FillTimestampsRailcontentTables::class,
             MigrateContentToNewStructure::class,
             MigrateContentColumns::class,
             MigrateContentFields::class,
