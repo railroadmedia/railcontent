e3aa800daa47b9d40a03b1e69d2e522a9aaa9a32
calebfavor <calebfavor@gmail.com>
2022-08-13 00:35:13 +0000

    update packs background images

--- resources/platform/views/partials/content/overview-headers/_pack-bundle.blade.php
+++ resources/platform/views/partials/content/overview-headers/_pack-bundle.blade.php
@@ -1,21 +1,26 @@
+@php 
+    $packHeader = str_replace(' ', '%20', $pack->fetch('data.header_image_url'));
+@endphp
+
 @component('partials.bladesora.members.components.header-banner', [
     'hideUser' => true,
-    'backgroundImage' => 'https://musora-web-platform.s3.amazonaws.com/headers/'.$brand.'-header.jpg',
+    'backgroundImage' => $packHeader,
 ])
     @slot('content')
         <div class="flex flex-column pr-1 align-v-bottom align-h-center">
-            <img
-                    alt="{{ $pack->fetch('title') }} Logo"
-                    src="{{ $pack->fetch('data.logo_image_url') }}"
-                    class="mb-2"
-                    style="width: 100%;max-width:480px;"
+            <div class="pv-5"></div> 
+            <div class="pv-5 hide-xs-only"></div> 
+            <div class="pv-5 hide-md-down"></div> 
+            <img alt="{{ $pack->fetch('title') }} Logo"
+                 src="{{ $pack->fetch('data.logo_image_url') }}" 
+                 style="width: 100%; max-width: 480px;"
             >
 
-            @if($pack['slug'] !== 'learn-songs-faster')
+            {{-- @if($pack['slug'] !== 'learn-songs-faster')
                 <h1 class="heading text-white mb-3">
                     {{ $parentContent->fetch('fields.title') }}
                 </h1>
-            @endif
+            @endif --}}
 
             @if($parentContent->fetch('type') === 'pack-bundle' && $pack->fetch('bundle_count') > 1)
                 <a href="{{ url()->route('platform.packs') }}"
