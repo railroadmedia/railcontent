dce839cf20067dfb93dada06f52a036c7b937f76
calebfavor <calebfavor@gmail.com>
2022-08-15 21:18:46 +0000

    add brand to error pages

--- resources/platform/views/errors/missing.blade.php
+++ resources/platform/views/errors/missing.blade.php
@@ -20,8 +20,8 @@
                 <h2 class="tw-text-[#3F3F46] dark:tw-text-[#9EC0DC] tw-text-2xl lg:tw-text-3xl tw-font-bold tw-mb-4">404 ERROR</h1>
                 <p class="tw-mb-8">The Page you're looking for doesn't exist.</p>
 
-                <a href="/" class="tw-btn-primary tw-bg-[#081825] tw-text-white dark:tw-bg-white dark:tw-text-[#00101D] tw-mb-4">Go Back</a>
-                <p class="tw-mb-4">Go back or <a href="/support" class="tw-text-[#081825] dark:tw-text-white tw-underline tw-font-bold">contact support</a></p>
+                <a href="{{ $brand }}/" class="tw-btn-primary tw-bg-[#081825] tw-text-white dark:tw-bg-white dark:tw-text-[#00101D] tw-mb-4">Go Back</a>
+                <p class="tw-mb-4">Go back or <a href="{{ $brand }}/support" class="tw-text-[#081825] dark:tw-text-white tw-underline tw-font-bold">contact support</a></p>
             </div>
         </div>
     </div>
