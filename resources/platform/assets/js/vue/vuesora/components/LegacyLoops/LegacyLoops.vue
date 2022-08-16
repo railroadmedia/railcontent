86274663a1fd182f2f67001fe397bf7c29afb1db
calebfavor <calebfavor@gmail.com>
2022-08-16 19:19:23 +0000

    update modal close button and legacy loops

--- resources/platform/assets/js/vue/vuesora/components/LegacyLoops/LegacyLoops.vue
+++ resources/platform/assets/js/vue/vuesora/components/LegacyLoops/LegacyLoops.vue
@@ -7,7 +7,7 @@
         >
             <div class="flex flex-column pa-1">
                 <div class="flex flex-row">
-                    <h1 class="title">
+                    <h1 class="title dark:tw-text-white">
                         {{ loop }}
                     </h1>
                 </div>
