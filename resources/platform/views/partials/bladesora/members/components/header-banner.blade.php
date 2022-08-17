98dfaead979361c2fa20a753cffdcd1f0be465ec
calebfavor <calebfavor@gmail.com>
2022-08-17 21:38:23 +0000

    update forum background

--- resources/platform/views/partials/bladesora/members/components/header-banner.blade.php
+++ resources/platform/views/partials/bladesora/members/components/header-banner.blade.php
@@ -8,7 +8,7 @@
         >
     </div>
     {{-- Background Gradient --}}
-    <div class="header-background-container absolute-fill bg-top hide-lg-down" style="background: linear-gradient(to left, #000 0%, transparent 10%, transparent 90%, #000 100%)"></div>
+    <div class="absolute-fill bg-top hide-lg-down tw-left-0 tw-w-full" style="background: linear-gradient(to left, #000 0%, transparent 10%, transparent 90%, #000 100%)"></div>
     <div class="header-gradient-overlay absolute-fill {{ $brand }}"></div>
     <div class="relative">
         <div class="tw-relative tw-flex tw-items-center tw-container tw-mx-auto tw-px-4 md:tw-px-8">
