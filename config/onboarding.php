8607d00b23579aa288ac5027a36ebf67cf2311ab
Yalung Tang <yalungtang@gmail.com>
2022-08-18 13:36:34 -0300

    onboarding pianote classical method

--- config/onboarding.php
+++ config/onboarding.php
@@ -11,7 +11,7 @@ return [
         'pianote' => [
             'gears' => ['Electric Keyboard', 'Acoustic Piano', 'Midi Controller'],
             'experience' => ['Level 1', 'Level 2-3', 'Level 4-6', 'Level 7-10'],
-            'topics' => ['Hand Independence', 'Technique', 'Sight Reading', 'Creativity', 'Performance', 'Scales', 'Exercises', 'Improvisation', 'Chording', 'Intervals', 'Practice', 'Speed'],
+            'topics' => ['Classical', 'Hand Independence', 'Technique', 'Sight Reading', 'Creativity', 'Performance', 'Scales', 'Exercises', 'Improvisation', 'Chording', 'Intervals', 'Practice', 'Speed'],
             'genres' => ['Rock', 'Pop', 'Jazz', 'Blues', 'Country', 'Metal', 'Funk', 'Soul', 'CCM/Worship', 'Hip-Hop/Rap']
         ],
         'guitareo' => [
