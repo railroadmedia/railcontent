40f8d95e8aef27a62d0a57f848963326dda9dbd5
calebfavor <calebfavor@gmail.com>
2022-08-18 14:16:27 +0000

    update see all link text sizes

--- resources/platform/assets/js/vue/components/StatsSection/StatsSection.vue
+++ resources/platform/assets/js/vue/components/StatsSection/StatsSection.vue
@@ -87,7 +87,7 @@ export default {
             </a>
             <a href="https://www.drumeo.com/laravel/public/members/profile" 
                 aria-label="See My Dashboard" 
-                class="tw-text-base tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current"
+                class="tw-text-base tw-uppercase  xl:tw-text-lg xl:tw-leading-none tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current"
             >
                 Dashboard
             </a>
--- resources/platform/views/partials/bladesora/members/components/home/_coaches-section.blade.php
+++ resources/platform/views/partials/bladesora/members/components/home/_coaches-section.blade.php
@@ -9,7 +9,7 @@
                 </a>
                 <a href="{{ $subscribedCoachesUrl }}"
                     aria-label="See All Coaches"
-                    class="tw-text-base tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current"
+                    class="tw-text-base xl:tw-text-lg xl:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current"
                 >
                     See All
                 </a>
