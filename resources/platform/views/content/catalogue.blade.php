900f921cc6232b7023794a3bfd1657eeef9947e6
calebfavor <calebfavor@gmail.com>
2022-08-24 01:49:04 +0000

    update Podcast title

--- resources/platform/views/content/catalogue.blade.php
+++ resources/platform/views/content/catalogue.blade.php
@@ -228,7 +228,7 @@
                         <div class="tw-flex tw-flex-col tw-mb-3 tw-mr-auto">
                             <h1 class="tw-text-[#00101D] dark:tw-text-white heading tw-capitalize tw-mr-2">
                                 All
-                                @if( $catalogueMeta['name'] === 'Podcast')
+                                @if( $catalogueMeta['shortname'] === 'Podcast')
                                     Episodes {{-- Change Podcast Name}} --}}
                                 @else 
                                     {{ $catalogueMeta['shortname'] ?? $catalogueMeta['name'] }}
