2812161b4ca2617ec4b2e1928d626c54ca7d06df
calebfavor <calebfavor@gmail.com>
2022-08-30 16:18:01 +0000

    update profile details layout

--- resources/platform/views/account/dashboard.blade.php
+++ resources/platform/views/account/dashboard.blade.php
@@ -128,7 +128,7 @@ $showCompleteYourAccountButton = !$hasGear || !$hasTopics || !$hasGenres || !$ha
             <section id="editForm" class="tw-flex tw-flex-row tw-flex-wrap tw-pt-[24px]" dusk="about-user">
                 <div class="tw-flex tw-flex-col xl:tw-flex-row tw-justify-between tw-mb-3 tw-w-full">
                     {{-- User Details --}}
-                    <div class="tw-flex tw-flex-col tw-mb-4 tw-mr-[36px]">
+                    <div class="tw-flex tw-flex-col tw-mb-4 tw-mr-[36px] lg:tw-max-w-[50%]">
                         <h1
                             class="tw-font-bold tw-text-[24px] tw-leading-none tw-mb-[19px]">
                             About {{ $isCurrentUsersProfile ? 'You' : $dashboardUser->display_name }}
