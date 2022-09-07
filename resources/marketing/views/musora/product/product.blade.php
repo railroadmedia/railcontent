776757d277509bedc0b654607b35d1ddcea5655d
peterminhlee <petereminhlee@gmail.com>
2022-09-07 09:32:23 -0700

    separate scripts

--- resources/marketing/views/musora/product/product.blade.php
+++ resources/marketing/views/musora/product/product.blade.php
@@ -87,7 +87,9 @@
             </div>
         </div>
     </div>
+@stop
 
+@section('layout-scripts')
     <script src="https://kit.fontawesome.com/cf2f4c6c71.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
@@ -109,4 +111,5 @@
         })
     </script>
 @stop
+
 
