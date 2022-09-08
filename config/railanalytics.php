a7ab9182fb0861785ecda31b7244db541620d9da
Mircea Murasan <mircea.murasan@artsoft-consult.ro>
2022-09-08 16:25:49 +0300

    Update railanalytics config values

--- config/railanalytics.php
+++ config/railanalytics.php
@@ -257,46 +257,138 @@ return array(
     ],
     'production' => [
         'active-tracking-providers' => ['ga', 'gtm', 'gaw', 'fp', 'ga4', 'im'],
-
         'providers' => [
-            'google-analytics' =>
-                [
-                    'tracking-id' => 'UA-87486706-2',
+            'drumeo' => [
+                'google-analytics' =>
+                    [
+                        'tracking-id' => 'UA-304523-134',
+                        // https://support.google.com/optimize/answer/6262084
+//                    'optimise-id' => 'GTM-WP9MPV8', // if set to null, optimise code not be rendered
+                        'optimise-id' => null, // if set to null, optimise code not be rendered
+                    ],
+                'google-analytics-v4' =>
+                    [
+                        'tracking-id' => 'G-Y3L45RQH8R'
+                    ],
+                'google-tag-manager' =>
+                    [
+                        'tracking-id' => 'GTM-K8757GR'
+                    ],
+                'google-adwords' =>
+                    [
+                        'google-conversion-id' => '1071462884',
+                        'google-conversion-language' => 'en',
+                        'google-conversion-format' => '3',
+                        'google-conversion-color' => 'ffffff',
+                        'google-conversion-label' => 'NWZ6CLGKiHAQ5PP0_gM',
+                    ],
+                'facebook-pixel' =>
+                    [
+                        'pixel-id' => '713455882045507'
+                    ],
+                'impact' =>
+                    [
+                        'utt-link' => 'A3032697-2ca8-4ba5-9bad-ee08c4a7d1ca1',
+                        'sid' => 'IRzxwmnLNaMN3032697tSVoYihcdj3Qop1',
+                        'auth-token' => 'Dnetdodz2rgZeU.ECRcMmu9heSt_rhwk',
+                        'campaign-id' => '14652',
+                        'tag-action-tracker-id' => '27554',
+                        'api-action-tracker-id' => '27555',
+                        'sign-up-action-tracker-id' => '28350'
+                    ]
 
-                    // https://support.google.com/optimize/answer/6262084
+            ],
+            'pianote' => [
+                'google-analytics' =>
+                    [
+                        'tracking-id' => 'UA-87486706-2',
+                        // https://support.google.com/optimize/answer/6262084
 //                    'optimise-id' => 'GTM-NJR7J6C', // if set to null, optimise code not be rendered
-                    'optimise-id' => null, // if set to null, optimise code not be rendered
-                ],
-            'google-analytics-v4' =>
-                [
-                    'tracking-id' => 'G-H015F84TK0'
-                ],
-            'google-tag-manager' =>
-                [
-                    'tracking-id' => 'GTM-N2NTV9H'
-                ],
-            'google-adwords' =>
-                [
-                    'google-conversion-id' => '1071462884',
-                    'google-conversion-language' => 'en',
-                    'google-conversion-format' => '3',
-                    'google-conversion-color' => 'ffffff',
-                    'google-conversion-label' => 'fylwCIDGlXAQ5PP0_gM',
-                ],
-            'facebook-pixel' =>
-                [
-                    'pixel-id' => '246265805779426'
-                ],
-            'impact' =>
-                [
-                    'utt-link' => 'A3032711-54a2-463a-8dd0-dea886d450581',
-                    'sid' => 'IRrBi9uekoT63032711PiY8YYE6HVQztw1',
-                    'auth-token' => 'tFaYd3H_pvLUhkzmyuyVRzRrJ7ujv.sX',
-                    'campaign-id' => '14651',
-                    'tag-action-tracker-id' => '27558',
-                    'api-action-tracker-id' => '27559',
-                    'sign-up-action-tracker-id' => '28349',
-                ]
+                        'optimise-id' => null, // if set to null, optimise code not be rendered
+                    ],
+                'google-analytics-v4' =>
+                    [
+                        'tracking-id' => 'G-H015F84TK0'
+                    ],
+                'google-tag-manager' =>
+                    [
+                        'tracking-id' => 'GTM-N2NTV9H'
+                    ],
+                'google-adwords' =>
+                    [
+                        'google-conversion-id' => '1071462884',
+                        'google-conversion-language' => 'en',
+                        'google-conversion-format' => '3',
+                        'google-conversion-color' => 'ffffff',
+                        'google-conversion-label' => 'fylwCIDGlXAQ5PP0_gM',
+                    ],
+                'facebook-pixel' =>
+                    [
+                        'pixel-id' => '246265805779426'
+                    ],
+                'impact' =>
+                    [
+                        'utt-link' => 'A3032711-54a2-463a-8dd0-dea886d450581',
+                        'sid' => 'IRrBi9uekoT63032711PiY8YYE6HVQztw1',
+                        'auth-token' => 'tFaYd3H_pvLUhkzmyuyVRzRrJ7ujv.sX',
+                        'campaign-id' => '14651',
+                        'tag-action-tracker-id' => '27558',
+                        'api-action-tracker-id' => '27559',
+                        'sign-up-action-tracker-id' => '28349',
+                    ]
+            ],
+            'guitareo' => [
+                'google-analytics' =>
+                    [
+                        'tracking-id' => 'UA-46498312-12'
+                    ],
+                'google-analytics-v4' =>
+                    [
+                        'tracking-id' => 'G-RJTZJGP1TS'
+                    ],
+                'google-tag-manager' =>
+                    [
+                        'tracking-id' => 'GTM-5VVQJWC'
+                    ],
+                'google-adwords' =>
+                    [
+                        'google-conversion-id' => '992846266',
+                        'google-conversion-language' => 'en',
+                        'google-conversion-format' => '3',
+                        'google-conversion-color' => 'ffffff',
+                        'google-conversion-label' => 'aDXXCImz6XYQusO22QM',
+                    ],
+                'facebook-pixel' =>
+                    [
+                        'pixel-id' => '781339702238540'
+                    ],
+                'impact' =>
+                    [
+                        'utt-link' => 'A3032716-5429-4214-9065-1670fda851251',
+                        'sid' => 'IRrSkmAAdCgz3032716uDi2myMRzPJo2C1',
+                        'auth-token' => 'vwGshMUQ-pSZjyGoWCjt4K-uo9syLmQM',
+                        'campaign-id' => '14650',
+                        'tag-action-tracker-id' => '27560',
+                        'api-action-tracker-id' => '27561',
+                        'sign-up-action-tracker-id' => '28352'
+                    ]
+            ],
+            'singeo' => [
+                'google-analytics-v4' =>
+                    [
+                        'tracking-id' => 'G-RD2SJQH0N7'
+                    ],
+                'impact' =>
+                    [
+                        'utt-link' => 'A3071331-b8b1-4eb9-becc-451a063412601',
+                        'sid' => 'IRZwRzhBXQGF3071331Dnt3Rcc4PK9cMt1',
+                        'auth-token' => 'KmosMrz4TeSW7.MXqqXhQKUFNUoN_LMv',
+                        'campaign-id' => '14834',
+                        'tag-action-tracker-id' => '27792',
+                        'api-action-tracker-id' => '27793',
+                        'sign-up-action-tracker-id' => '28353'
+                    ]
+            ]
         ]
     ]
 );
