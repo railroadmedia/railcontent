10fe79bd9a7e15963a4c2d59c9c4bfd7011f1f10
peterminhlee <petereminhlee@gmail.com>
2022-09-12 14:47:35 -0700

    added auto revision record

--- app/Models/SizeChart.php
+++ app/Models/SizeChart.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class SizeChart extends Model
 {
     use HasFactory;
+    use RevisionableTrait;
 
     protected $guarded = [
         'id'
