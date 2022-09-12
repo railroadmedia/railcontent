b9dc11721476e5715d76b9d09d86a3b892161a6a
calebfavor <calebfavor@gmail.com>
2022-09-12 18:55:54 +0000

    onboarding coach search fixes

--- resources/platform/assets/js/vue/utils.js
+++ resources/platform/assets/js/vue/utils.js
@@ -10,7 +10,7 @@ export const setEndpointPrefix = () => {
 };
 
 export const searchCoaches = (brand, term) => {
-    const coachesUrl = `${setEndpointPrefix()}/railcontent/content?brand=${brand}&limit=18&statuses[]=published&sort=-published_on&required_fields[]=is_coach,1&page=1${term ? '&term='+term : ''}`
+    const coachesUrl = `${window.ENDPOINT_PREFIX}/railcontent/content?brand=${brand}&limit=18&statuses[]=published&sort=-published_on&required_fields[]=is_coach,1&page=1${term ? '&term='+term : ''}`
     return axios.get(coachesUrl);
 }
 
