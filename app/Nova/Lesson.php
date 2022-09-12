38468f751cebbbda245a9f1a4afc49bbea9f970f
peterminhlee <petereminhlee@gmail.com>
2022-09-12 15:25:37 -0700

    field changes

--- app/Nova/Lesson.php
+++ app/Nova/Lesson.php
@@ -47,14 +47,47 @@ class Lesson extends Resource
             Text::make('Name')->required(),
             //slug field for displaying to use a tag
             Text::make('Slug', function(){
-                $slug = str_replace( array('Drumeo-', 'Pianote-', 'Guitareo-', 'Singeo-'), '', $this->slug );
-
-                return '<a class="link-default" target="_blank" href="/'.strtolower($this->brand->name).'/shop/'.$slug.'">'.$slug.'</a>';
+                return '<a class="link-default" target="_blank" href="/'.strtolower($this->brand->name).'/shop/'.$this->slug.'">'.$this->slug.'</a>';
             })->asHtml()->hideWhenUpdating()->hideWhenCreating(),
             //slug field for saving
             Text::make('Slug')->required()->hideFromDetail()->hideFromIndex(),
-            Text::make('Sku')->hideFromIndex()->required(),
+            Text::make('Sku')->hideFromIndex(),
+            Text::make('Meta Description', 'meta_desc')->hideFromIndex(),
+            Image::make('Meta Image', 'meta_img')
+                ->disk('nova_s3')
+                ->prunable()
+                ->hideFromIndex()
+                ->disableDownload()
+                ->deletable(false)
+                ->storeAs(function (Request $request){
+                    $brandId = $request->brand;
+                    $brand = '';
+
+                    if($brandId === "1") {
+                        $brand = 'Drumeo';
+                    }
+                    elseif($brandId === "2"){
+                        $brand = 'Pianote';
+                    }
+                    elseif($brandId === "3"){
+                        $brand = 'Guitareo';
+                    }
+                    elseif($brandId === "4"){
+                        $brand = 'Singeo';
+                    }
+
+                    return '/'.$brand.'/'.$request->uuid.'-'.$request->file('meta_img')->getClientOriginalName();
+                })
+                ->preview(function($value){
+                    if(empty($value)) return null;
+
+                    return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
+                }),
+            Text::make('Meta Image', 'meta_img')->hideFromIndex()->hideFromDetail(),
             Text::make('Promo Code', 'promo_code')->hideFromIndex(),
+            Currency::make('Price')->required(),
+            Currency::make('Discounted Price', 'discounted_price')->hideFromIndex(),
+            Text::make('Badge Text', 'badge_text')->hideFromIndex(),
             Image::make('Thumbnail')
                 ->disk('nova_s3')
                 ->prunable()
@@ -81,7 +114,7 @@ class Lesson extends Resource
                     return '/'.$brand.'/'.$request->uuid.'-'.$request->file('thumbnail')->getClientOriginalName();
                 })
                 ->preview(function($value){
-                    if(is_null($value)) return null;
+                    if(empty($value)) return null;
 
                     return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
                 }),
@@ -112,48 +145,14 @@ class Lesson extends Resource
                     return '/'.$brand.'/'.$request->uuid.'-'.$request->file('thumbnail_logo')->getClientOriginalName();
                 })
                 ->preview(function($value){
-                    if(is_null($value)) return null;
+                    if(empty($value)) return null;
 
                     return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
                 }),
             Text::make('Thumbnail Logo', 'thumbnail_logo')->hideFromIndex()->hideFromDetail(),
-            Text::make('Badge Text', 'badge_text')->hideFromIndex(),
-            Text::make('Meta Description', 'meta_desc')->hideFromIndex()->required(),
-            Image::make('Meta Image', 'meta_img')
-                ->disk('nova_s3')
-                ->prunable()
-                ->hideFromIndex()
-                ->disableDownload()
-                ->deletable(false)
-                ->storeAs(function (Request $request){
-                    $brandId = $request->brand;
-                    $brand = '';
 
-                    if($brandId === "1") {
-                        $brand = 'Drumeo';
-                    }
-                    elseif($brandId === "2"){
-                        $brand = 'Pianote';
-                    }
-                    elseif($brandId === "3"){
-                        $brand = 'Guitareo';
-                    }
-                    elseif($brandId === "4"){
-                        $brand = 'Singeo';
-                    }
-
-                    return '/'.$brand.'/'.$request->uuid.'-'.$request->file('meta_img')->getClientOriginalName();
-                })
-                ->preview(function($value){
-                    if(is_null($value)) return null;
-
-                    return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
-                }),
-            Text::make('Meta Image', 'meta_img')->hideFromIndex()->hideFromDetail(),
             Text::make('Short Description', 'short_desc')->hideFromIndex(),
-            Text::make('Header Text', 'header_text')->hideFromIndex()->required(),
-            Currency::make('Price')->required(),
-            Currency::make('Discounted Price', 'discounted_price')->hideFromIndex(),
+            Text::make('Header Text', 'header_text')->hideFromIndex(),
             Text::make('Special Text', 'special_text')->hideFromIndex(),
             Image::make('Page Logo', 'page_logo')
                 ->disk('nova_s3')
@@ -181,11 +180,10 @@ class Lesson extends Resource
                     return '/'.$brand.'/'.$request->uuid.'-'.$request->file('page_logo')->getClientOriginalName();
                 })
                 ->preview(function($value){
-                    if(is_null($value)) return null;
+                    if(empty($value)) return null;
 
                     return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
                 }),
-            Text::make('Page Logo', 'page_logo')->hideFromIndex()->hideFromDetail(),
             Text::make('Page Logo URL', 'page_logo')->hideFromIndex()->hideFromDetail(),
             Text::make('Video Link', 'video_src')
                 ->hideFromIndex(),
@@ -221,7 +219,7 @@ class Lesson extends Resource
                     return '/'.$brand.'/'.$request->uuid.'-'.$request->file('product_img')->getClientOriginalName();
                 })
                 ->preview(function($value){
-                    if(is_null($value)) return null;
+                    if(empty($value)) return null;
 
                     return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
                 }),
@@ -235,7 +233,6 @@ class Lesson extends Resource
             Boolean::make('Free Shipping', 'free_shipping')->default(false)->hideFromIndex(),
             Boolean::make('Included Edge', 'included_edge')->default(false)->hideFromIndex(),
             Boolean::make('Bundle Free Shipping', 'bundle_free_shipping')->default(false)->hideFromIndex(),
-            Text::make('Bundle Description', 'bundle_desc')->hideFromIndex(),
             Number::make('Display order', 'display_order'),
             Flexible::make('Features/Topics')
                 ->addLayout(FeatureLayout::class)
@@ -243,6 +240,7 @@ class Lesson extends Resource
             Flexible::make('Specs')
                 ->addLayout(SpectLayout::class)
                 ->preset(SpecPreset::class),
+            Text::make('Bundle Description', 'bundle_desc')->hideFromIndex(),
         ];
     }
 
