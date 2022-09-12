bda930688cc4c6d42a885157ed8f50f3639d35da
peterminhlee <petereminhlee@gmail.com>
2022-09-12 14:50:20 -0700

    added auto revision record

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Venturecraft\Revisionable\RevisionableTrait;
 
 class ProductType extends Model
 {
     use HasFactory;
+    use RevisionableTrait;
 
     public function products()
     {
