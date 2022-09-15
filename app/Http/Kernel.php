acf7d00ca713e0f1ab8fb433b85776e75130f70c
calebfavor <calebfavor@gmail.com>
2022-09-15 09:50:25 -0700

    Removing alpha testing middleware for test data seeding.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            AlphaTestingAccountCreationMiddleware::class, // todo: remove after alpha testing
             \Modules\UserManagementSystem\Middleware\AuthenticateIfAvailable::class,
             \App\Modules\Brand\Middleware\SetLastUsedBrand::class,
             \App\Http\Middleware\SetContentPermissions::class,
