3d94ca03f29f07b43a4733eb91cbe7b3dc2908b8
calebfavor <calebfavor@gmail.com>
2022-09-15 16:44:27 -0700

    Fixing search indexes being generated too long.

--- src/Repositories/FullTextSearchRepository.php
+++ src/Repositories/FullTextSearchRepository.php
@@ -215,7 +215,7 @@ class FullTextSearchRepository extends RepositoryBase
             $values[$valueIndex] = str_replace('/', '_', $value);
         }
 
-        return implode(' ', $values);
+        return substr(preg_replace("/[^A-Za-z0-9 ]/", '', implode(' ', array_unique($values))), 0, 245);
     }
 
     /**
@@ -274,6 +274,8 @@ class FullTextSearchRepository extends RepositoryBase
         }
 
         $contentRows = $query->getToArray();
+
+        dd($contentRows);
 
         return array_column($contentRows, 'content_id');
     }
