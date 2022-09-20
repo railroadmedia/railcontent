0a0b68217a3fc03a65550fad768893a8ec9e2f18
robwillems <robert.j.willems@gmail.com>
2022-09-20 14:06:31 -0700

    Remove queue connection and name

--- app/Modules/EventDataSynchronizer/Listeners/Impact/ImpactEventListener.php
+++ app/Modules/EventDataSynchronizer/Listeners/Impact/ImpactEventListener.php
@@ -1,23 +1,20 @@
 <?php
+
 namespace App\Modules\EventDataSynchronizer\Listeners\Impact;
+
 use Carbon\Carbon;
 use Railroad\Ecommerce\Events\OrderEvent;
 use App\Modules\EventDataSynchronizer\Jobs\ImpactTrackConversion;
 
 class ImpactEventListener
 {
-    private $queueConnectionName;
+    public function __construct()
+    {
 
-    private $queueName;
-
-
-    public function __construct() {
-        $this->queueConnectionName = config('event-data-synchronizer.impact_queue_connection_name', 'database');
-        $this->queueName = config('event-data-synchronizer.impact_queue_name', 'impact');
     }
 
     /**
-     * @param  OrderEvent  $orderEvent
+     * @param OrderEvent $orderEvent
      */
     public function handleSubscriptionRenewed(OrderEvent $orderEvent)
     {
@@ -26,11 +23,8 @@ class ImpactEventListener
 
         dispatch(
             (new ImpactTrackConversion($order))
-                ->onConnection($this->queueConnectionName)
-                ->onQueue($this->queueName)
                 ->delay(Carbon::now()->addSeconds(3))
         );
-
     }
 
 }
