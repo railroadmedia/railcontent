f787a19be84253a64d4ed442399f28c9ec337280
robwillems <robert.j.willems@gmail.com>
2022-09-19 22:28:07 -0700

    Command updates

--- app/Console/Commands/Infrastructure/Command.php
+++ app/Console/Commands/Infrastructure/Command.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands\Infrastructure;
 
 use Illuminate\Console\Command as CommandBase;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 abstract class Command extends CommandBase
 {
@@ -12,15 +14,26 @@ abstract class Command extends CommandBase
     {
         $count = $query->count();
         $this->info("Processing query...");
+        $this->info("$count records found.");
 
-        $bar = $this->output->createProgressBar($count);
-        $bar->setFormat('debug');
-        $bar->start();
-        $query->chunk($chunks, function ($items) use ($function, $bar) {
+        $bar = null;
+        if (App::environment('production')) {
+            $bar = $this->output->createProgressBar($count);
+            $bar->setFormat('debug');
+            $bar->start();
+        }
+        $n = 0;
+        $query->chunk($chunks, function ($items) use ($count, &$n, $function, $bar) {
+            $last = null;
             foreach ($items as $item) {
                 call_user_func($function, $item);
-                $bar->advance();
+                if ($bar) $bar->advance();
+                $last = $item;
             }
+            $n += $items->count();
+            $message = "$n/$count processed. ";
+            if ($last?->id) $message .= "Last processed id: $last->id";
+            Log::info($message);
         });
 
         $this->info("\nProcessing Completed");
