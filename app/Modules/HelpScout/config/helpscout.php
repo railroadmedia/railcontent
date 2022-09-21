1ccf02d108266fd0f48958c9006fc97db0d9cad8
Mircea Murasan <mircea.murasan@artsoft-consult.ro>
2022-09-21 16:14:17 +0300

    Add default values for helpscout beacon ids in config files

--- app/Modules/HelpScout/config/helpscout.php
+++ app/Modules/HelpScout/config/helpscout.php
@@ -11,10 +11,10 @@ return [
 
     'helpscout_tracking_beacon_id' =>
         [
-            'drumeo' => env('HELPSCOUT_TRACKING_BEACON_ID_DRUMEO'),
-            'pianote' => env('HELPSCOUT_TRACKING_BEACON_ID_PIANOTE'),
-            'guitareo' => env('HELPSCOUT_TRACKING_BEACON_ID_GUITAREO'),
-            'singeo' => env('HELPSCOUT_TRACKING_BEACON_ID_SINGEO'),
+            'drumeo' => env('HELPSCOUT_TRACKING_BEACON_ID_DRUMEO', '9f119028-29b4-4fcd-8b9d-03c86f3d2521'),
+            'pianote' => env('HELPSCOUT_TRACKING_BEACON_ID_PIANOTE', '9f119028-29b4-4fcd-8b9d-03c86f3d2521'),
+            'guitareo' => env('HELPSCOUT_TRACKING_BEACON_ID_GUITAREO', '9f119028-29b4-4fcd-8b9d-03c86f3d2521'),
+            'singeo' => env('HELPSCOUT_TRACKING_BEACON_ID_SINGEO', '9f119028-29b4-4fcd-8b9d-03c86f3d2521'),
         ],
     'webhook_secret' => '0961023204554833aa34b4feca815a12',
 ];
