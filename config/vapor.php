4b5d10db0d4761ade0eeb5368f0f7a47b7ed4be5
Roxana Riza <roxana_r2000@yahoo.com>
2022-09-22 09:41:18 +0300

    Comment redirect

--- config/vapor.php
+++ config/vapor.php
@@ -5,5 +5,5 @@ return [
     // core library is okay for most cases but I will leave here you need to use any of them
     'middleware' => 'web_authenticated',
 
-    'redirect_to_root' => false,
+//    'redirect_to_root' => false,
 ];
