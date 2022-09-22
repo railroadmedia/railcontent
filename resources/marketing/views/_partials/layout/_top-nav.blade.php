3e6c9f63a0c71d6e3350c217cf8822e9c05fff9b
peterminhlee <petereminhlee@gmail.com>
2022-09-22 11:48:35 -0700

    commented page links

--- resources/marketing/views/_partials/layout/_top-nav.blade.php
+++ resources/marketing/views/_partials/layout/_top-nav.blade.php
@@ -8,11 +8,11 @@
     <div class="flex flex-1">
 
         <!-- Page Links -->
-        <ul class="flex items-center text-white font-bebas-neue hidden md:flex">
+        {{-- <ul class="flex items-center text-white font-bebas-neue hidden md:flex">
             <li class="lg:ml-5"><a href="/#method" alt="Go to Method Section" class="uppercase border-b-[3px] pb-0.5 transition border-transparent hover:border-gray-600">Method</a></li>
             <li class="ml-3 lg:ml-6"><a href="/#songs" alt="Go to Songs Section" class="uppercase border-b-[3px] pb-0.5 transition border-transparent hover:border-gray-600">Songs</a></li>
             <li class="ml-3 lg:ml-6"><a href="/#coaches" alt="Go to Coaches Section" class="uppercase border-b-[3px] pb-0.5 transition border-transparent hover:border-gray-600">Coaches</a></li>
-        </ul>
+        </ul> --}}
 
         <!-- Nav CTA buttons -->
         <div class="ml-auto flex items-center">
