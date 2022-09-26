07e390f2c2898174d09cdf9a5c1f9f32e98b9cf4
peterminhlee <petereminhlee@gmail.com>
2022-09-26 11:32:14 -0700

    deleted

--- resources/marketing/views/_partials/components/bg.blade.php
+++
@@ -1,7 +0,0 @@
-<img
-    class="transition-opacity opacity-0 absolute w-full h-full object-cover top-0 left-0 @if(!empty($class)) {{ $class }} @endif"
-    src="{{ $src }}"
-    alt="{{ $alt }}"
-    loading="lazy"
-    onload="this.classList.remove('opacity-0')"
-/>
--- resources/marketing/views/_partials/components/img.blade.php
+++
@@ -1,7 +0,0 @@
-<img
-    class="@if(!empty($class)) {{ $class }} @endif transition-opacity opacity-0"
-    src="{{ $src }}"
-    alt="{{ $alt }}"
-    loading="lazy"
-    onload="this.classList.remove('opacity-0')"
-/>
