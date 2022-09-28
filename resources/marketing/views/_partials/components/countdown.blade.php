9f15c15ec73e35d271ceb92bcd0f3895355c8676
peterminhlee <petereminhlee@gmail.com>
2022-09-28 10:35:49 -0700

    updates

--- resources/marketing/views/_partials/components/countdown.blade.php
+++ resources/marketing/views/_partials/components/countdown.blade.php
@@ -1,35 +1,101 @@
-const timer = () => {
-    return {
-        day: '00',
-        dayText: 'days',
-        hour: '00',
-        hourText: 'hours',
-        minute: '00',
-        minuteText: 'minutes',
-        second: '00',
-        secondText: '',
-        endTime: new Date().getTime(),
-        startTime: new Date().getTime(),
-        timeLeft: 0,
-        countdown: () => {
-            setInterval(() => {
-                this.now = new Date().getTime();
+<script>
+    /**
+         example!
+             <section x-data="timer()" x-init="countdown()">
+                 <div>
+                     <div x-show="timeLeft > 0"><span x-text="day"></span><span x-text="dayText"></span></div>
+                     <div x-show="timeLeft > 0"><span x-text="hour"></span><span x-text="hourText"></span></div>
+                     <div x-show="timeLeft > 0"><span x-text="minute"></span><span x-text="minuteText"></span></div>
+                     <div x-show="timeLeft > 0"><span x-text="second"></span><span x-text="secondText"></span></div>
+                     <div x-show="timeLeft < 0">Limited Time Left</div>
+                 </div>
+             </section>
+     */
+
+    function timer(){
+        return {
+            day: '00',
+            dayText: @if(!empty($promoVersion)) 'DAYS' @else 'days' @endif,
+            hour: '00',
+            hourText: @if(!empty($promoVersion)) 'HRS' @else 'hours' @endif,
+            minute: '00',
+            minuteText: @if(!empty($promoVersion)) 'MIN' @else 'minutes' @endif,
+            second: '00',
+            secondText: @if(!empty($promoVersion)) 'SEC' @else 'seconds' @endif,
+            endTime: new Date('{{ $date }}').getTime(),
+            startTime: new Date().getTime(),
+            timeLeft: 0,
+            smallScreen: false,
+            countdown: function (){
+                let _this = this;
+                this.startTime = new Date().getTime();
                 this.timeLeft = (this.endTime - this.startTime) / 1000;
-                this.second = this.formatTime(this.timeLeft % 60);
-                this.minute = this.formatTime(this.timeLeft / 60) % 60;
-                this.hour = this.formatTime(this.timeLeft / (60 * 60)) % 24;
-                this.days = this.formatTime(this.timeLeft / (60 * 60 * 24));
-            }, 1000);
-        },
-        formatTime: (value) => {
-            if(value < 10){
-                return "0" + Math.floor(value);
-            }
+                console.log(this.timeLeft)
+                if(this.timeLeft > 0){
+                    setInterval(() => {
+                        this.startTime = new Date().getTime();
+                        this.timeLeft = (this.endTime - this.startTime) / 1000;
+                        this.day = this.formatTime(this.timeLeft / (60 * 60 * 24));
+                        this.hour = this.formatTime(this.timeLeft / (60 * 60)) % 24;
+                        this.minute = this.formatTime(this.timeLeft / 60) % 60;
+                        this.second = this.formatTime(this.timeLeft % 60);
+                        @if(empty($promoVersion))
+                        if ( !this.smallScreen ){
+                            this.dayText = formatText(this.day, 'days');
+                            this.hourText = formatText(this.hour, 'hours');
+                            this.minuteText = formatText(this.minute, 'minutes');
+                            this.secondText = formatText(this.second, 'seconds');
+                        }
+                        @endif
+                    }, 1000);
+                }
 
-            return Math.floor(value);
-        },
-        formatText: (time, value)=>{
 
+
+                @if(empty($promoVersion))
+                    const bodyWidth = window.innerWidth;
+
+                    if ( bodyWidth < 768 ){
+                        this.dayText = 'DD';
+                        this.hourText = 'HH';
+                        this.minuteText = 'MM';
+                        this.secondText = 'SS';
+                        this.smallScreen = true;
+                    }
+
+                    window.addEventListener('resize', function(event){
+                        const bodyWidth = window.innerWidth;
+
+                        if ( bodyWidth < 768 ){
+                            _this.smallScreen = true;
+                            _this.dayText = 'DD';
+                            _this.hourText = 'HH';
+                            _this.minuteText = 'MM';
+                            _this.secondText = 'SS';
+                        }
+                        else {
+                            _this.smallScreen = false;
+                            _this.dayText = 'days';
+                            _this.hourText = 'hours';
+                            _this.minuteText = 'minutes';
+                            _this.secondText = 'seconds';
+                        }
+                    });
+                @endif
+            },
+            formatTime: function (value){
+                if(value < 10) return "0" + Math.floor(value);
+
+                return Math.floor(value);
+            },
         }
     }
-}
+
+    function formatText(time, text){
+        if(time < 2 ) return text.substring(0, text.length - 1);
+
+        return text;
+    }
+</script>
+
+
