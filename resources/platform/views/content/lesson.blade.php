d6223605f908ce0f2be1893af80464986a893f06
calebfavor <calebfavor@gmail.com>
2022-10-06 19:47:10 +0000

    youtube player chapter track feature on info section

--- resources/platform/views/content/lesson.blade.php
+++ resources/platform/views/content/lesson.blade.php
@@ -43,17 +43,15 @@
                         </youtube-player>
                     @elseif(!empty($lessonContent->fetch('fields.video.fields.youtube_video_id')))
                         <div class="widescreen mb-2 bg-black">
-                            <transition appear name="fade">
-                                <youtube-player ref="mediaElementVueInstance" brand="{{ $brand }}"
-                                    video-id="{{ $lessonContent->fetch('fields.video.fields.youtube_video_id') }}"
-                                    :current-second="{{ $lessonContent->fetch('last_watch_position_in_seconds', 0) }}"
-                                    :total-duration="{{ $lessonContent->fetch('fields.video.fields.length_in_seconds', 0) }}"
-                                    video-length="{{ $lessonContent->fetch('fields.video.fields.length_in_seconds') }}"
-                                    progress-state="{{ $lessonContent->fetch('progress_state') }}"
-                                    content-id="{{ $lessonContent->fetch('id') }}" :use-intersection-observer="true"
-                                    theme-color="{{ $brand }}" @play="handleVideoPlay" @pause="handleVideoPause">
-                                </youtube-player>lesson-sidebar
-                            </transition>
+                            <youtube-player ref="mediaElementVueInstance" brand="{{ $brand }}"
+                                video-id="{{ $lessonContent->fetch('fields.video.fields.youtube_video_id') }}"
+                                :current-second="{{ $lessonContent->fetch('last_watch_position_in_seconds', 0) }}"
+                                :total-duration="{{ $lessonContent->fetch('fields.video.fields.length_in_seconds', 0) }}"
+                                video-length="{{ $lessonContent->fetch('fields.video.fields.length_in_seconds') }}"
+                                progress-state="{{ $lessonContent->fetch('progress_state') }}"
+                                content-id="{{ $lessonContent->fetch('id') }}" :use-intersection-observer="true"
+                                theme-color="{{ $brand }}" @play="handleVideoPlay" @pause="handleVideoPause">
+                            </youtube-player>lesson-sidebar
                         </div>
                     @else
                         <div id="lessonVideoWrap">
@@ -172,7 +170,8 @@
             {{-- Assignments --}}
             <div class="tw-flex">
 
-                @if(empty($lessonContent->fetch('*assignments')) && !empty($lessonContent->fetch('*data.sheet_music_image_url.value')))
+                @if (empty($lessonContent->fetch('*assignments')) &&
+                    !empty($lessonContent->fetch('*data.sheet_music_image_url.value')))
                     <div class="container mv-3">
                         <div class="flex flex-column grow">
                             <div class="flex flex-row pv-3">
@@ -180,7 +179,7 @@
                             </div>
                             <div class="flex flex-row">
                                 <div class="flex flex-column">
-                                    @foreach($lessonContent->fetch('*data.sheet_music_image_url.value') as $sheetMusicImageUrl)
+                                    @foreach ($lessonContent->fetch('*data.sheet_music_image_url.value') as $sheetMusicImageUrl)
                                         <div class="flex flex-row bb-light-1">
                                             <div class="flex flex-column grow ph pv-3">
                                                 <img src="{{ $sheetMusicImageUrl }}" style="width:100%;">
@@ -193,7 +192,8 @@
                     </div>
                 @endif
 
-                @if(empty($lessonContent->fetch('*assignments')) && (!empty($lessonContent['stbs']) || !empty($lessonContent['bdsStbs']) || !empty($lessonContent['ds2Stbs'])))
+                @if (empty($lessonContent->fetch('*assignments')) &&
+                    (!empty($lessonContent['stbs']) || !empty($lessonContent['bdsStbs']) || !empty($lessonContent['ds2Stbs'])))
                     <div class="container mv-3">
                         <div class="flex flex-column grow">
                             <div class="flex flex-row pv-3">
@@ -272,8 +272,8 @@
                     <comments theme-color="{{ $brand }}" brand="{{ $brand }}"
                         content-id="{{ $lessonContent->fetch('id') }}" user-id="{{ user()->id }}"
                         user-name="{{ user()->display_name }}" user-avatar="{{ user()->profile_picture_url }}"
-                        user-xp="{{ user()->totalXP() }}" user-access-level="{{ user()->access_level }}" profile-base-route="/profile/"
-                        :is-admin="false">
+                        user-xp="{{ user()->totalXP() }}" user-access-level="{{ user()->access_level }}"
+                        profile-base-route="/profile/" :is-admin="false">
                     </comments>
                 </div>
             </div>
