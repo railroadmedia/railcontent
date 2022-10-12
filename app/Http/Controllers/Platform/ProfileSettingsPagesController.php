330daa819950271837f52fe468421ce5ed219090
calebfavor <calebfavor@gmail.com>
2022-10-12 08:38:37 -0700

    Adding payment method update endpoints and linking up BE functionality.

--- app/Http/Controllers/Platform/ProfileSettingsPagesController.php
+++ app/Http/Controllers/Platform/ProfileSettingsPagesController.php
@@ -152,7 +152,6 @@ class ProfileSettingsPagesController extends BaseController
             ),
             'allBrands' => all_brands(),
             'selectedBrand' => $request->get('selected-brand', $brand)
-
         ]);
     }
 
@@ -530,7 +529,7 @@ class ProfileSettingsPagesController extends BaseController
             ->respond()
             ->getContent();
 
-        $stripePublishableKey = config('ecommerce.payment_gateways.stripe.drumeo.stripe_publishable_key');
+        $stripePublishableKey = config('ecommerce.payment_gateways.stripe.' . brand() . '.stripe_publishable_key');
 
         $membershipProductIds = ProductModel::query()
             ->where([
@@ -611,7 +610,7 @@ class ProfileSettingsPagesController extends BaseController
         return view(
             'account.settings.payments',
             [
-                'sections' => $this->settingSections(),
+                'sections' => $this->settingSections('payments'),
                 'paymentMethodsJson' => $paymentMethodsJson,
                 'stripePublishableKey' => $stripePublishableKey,
                 'countries' => json_encode(array_values(CountryListService::allWithCommonDuplicatedAtTop())),
