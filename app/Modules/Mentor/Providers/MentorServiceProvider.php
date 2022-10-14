2365df224ab02a114ddb45644e99bb19503ae6e2
robwillems <robert.j.willems@gmail.com>
2022-10-14 09:40:16 -0700

    Reassign students command

--- app/Modules/Mentor/Providers/MentorServiceProvider.php
+++ app/Modules/Mentor/Providers/MentorServiceProvider.php
@@ -7,7 +7,7 @@ use App\Modules\Mentor\Console\Commands\AssignMentors;
 use App\Modules\Mentor\Console\Commands\InitializeMentors;
 use App\Modules\Mentor\Console\Commands\RecalculateMentorTotals;
 use App\Modules\Mentor\Console\Commands\RegisterHelpScoutWebHook;
-use App\Modules\Mentor\Console\Commands\UnassignMentors;
+use App\Modules\Mentor\Console\Commands\ReassignStudents;
 use App\Modules\Mentor\Console\Commands\UnregisterHelpScoutWebHook;
 use App\Modules\Mentor\Console\Commands\VerifyMentors;
 use Illuminate\Console\Scheduling\Schedule;
@@ -51,7 +51,7 @@ class MentorServiceProvider extends ServiceProvider
             RecalculateMentorTotals::class,
             RegisterHelpScoutWebHook::class,
             UnregisterHelpScoutWebHook::class,
-            UnassignMentors::class, //used for debugging can be removed after initial launch
+            ReassignStudents::class,
             AssignMentors::class,
         ]);
 
