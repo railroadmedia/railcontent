461bbdcf545296edf2b33095aa909e1ffd72e8be
calebfavor <calebfavor@gmail.com>
2022-10-17 15:25:19 +0000

    WIP still doesnt work

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -21,6 +21,7 @@ import LoginForm from './vue/components/LoginForm/LoginForm.vue';
 import ResetPassForm from './vue/components/ResetPassForm/ResetPassForm.vue';
 import MusoraIcon from './vue/components/MusoraIcons/MusoraIcon.vue'
 import GearCarousel from './vue/components/GearCarousel/GearCarousel.vue';
+import VimeoVideo from './vue/components/VimeoVideo/VimeoVideo.vue';
 
 //Vuesora Assets
 import Forms from './vue/vuesora/assets/js/classes/forms';
@@ -239,6 +240,7 @@ app.component('AppContainer', AppContainer)
     .component('ContentCatalogue', ContentCatalogue)
     .component('PlayAlongs', PlayAlongs)
     .component('GearCarousel', GearCarousel)
+    .component('VimeoVideo', VimeoVideo)
     .component('VideoResources', VideoResources)
     .component('ContentLessonActionButtons', ContentLessonActionButtons)
     .component('AssignmentsContainer', AssignmentsContainer)
