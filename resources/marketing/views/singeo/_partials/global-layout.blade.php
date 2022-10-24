a83b5f5623c124cb7cefcd6f08b10a3d5788c756
Trent <trent.harms@gmail.com>
2022-10-24 16:56:53 -0700

    fixing favicons

--- resources/marketing/views/singeo/_partials/global-layout.blade.php
+++ resources/marketing/views/singeo/_partials/global-layout.blade.php
@@ -19,7 +19,7 @@
 
     @include('singeo._partials.includes.google-optimize')
 
-    @include('singeo.sales.partials._favicons')
+    @include('_partials.layout.favicons.singeo-favicons')
     @include('singeo.sales.partials._fonts')
 
     {!! \App\Analytics\Tracker::headBottom() !!}
