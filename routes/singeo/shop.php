297c54f3b56e2f10a78ce05c6a21e226109366d8
peterminhlee <petereminhlee@gmail.com>
2022-10-24 15:12:01 -0700

    route for shop page

---
+++ routes/singeo/shop.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Singeo\ShopController;
+
+Route::domain('{singeoDomain}')->group(function () {
+    Route::get('/shop', [ShopController::class, 'shop']);
+
+    Route::get('/shop/{productslug}', [ShopController::class, 'product']);
+});
+
