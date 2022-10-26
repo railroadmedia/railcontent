ac3ebf43b446e58773ce03260b379203e96028ed
peterminhlee <petereminhlee@gmail.com>
2022-10-26 16:29:58 -0700

    path updates

--- resources/marketing/views/pianote/sales/pages/referral.blade.php
+++ resources/marketing/views/pianote/sales/pages/referral.blade.php
@@ -183,7 +183,7 @@
     </section>
 
 
-    @include("sales.partials._footer")
+    @include("pianote.sales.footer")
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/assets/members-area/js/gulp/navigation-sales.js') }}"></script>
