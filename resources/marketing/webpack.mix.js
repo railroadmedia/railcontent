cc33a86de7768fba58acb904a75732d56444f986
calebfavor <calebfavor@gmail.com>
2022-10-26 10:38:02 -0700

    Fixing up component assets not being served from CDN causing all dynamic components to fail.

--- resources/marketing/webpack.mix.js
+++ resources/marketing/webpack.mix.js
@@ -25,3 +25,20 @@ mix
     .version()
     .mergeManifest()
     .dump();
+
+if (mix.inProduction()) {
+    const ASSET_URL = process.env.ASSET_URL + "/";
+
+    mix.webpackConfig(webpack => {
+        return {
+            plugins: [
+                new webpack.DefinePlugin({
+                    "process.env.ASSET_PATH": JSON.stringify(ASSET_URL)
+                })
+            ],
+            output: {
+                publicPath: ASSET_URL
+            }
+        };
+    });
+}
