62024624837e7b6ec4fa7fc45f7f455d013ec551
calebfavor <calebfavor@gmail.com>
2022-10-27 10:12:10 -0700

    Adding bug tracking code to all home pages. Moving it to a reusable partial.

--- resources/platform/views/home/index.blade.php
+++ resources/platform/views/home/index.blade.php
@@ -147,20 +147,7 @@
 
     </div>
 
-    @if(current_user_has_recent_order())
-        @if(brand() == 'drumeo')
-            {{-- fix --}}
-            <iframe src="{{ get_legacy_brand_base_url() . '/laravel/public/railanalytics/blank-tracking-page' }}"
-                    loading="lazy"
-                    style="position: absolute; width:0; height:0; border:0;">
-            </iframe>
-        @else
-            <iframe src="{{ get_legacy_brand_base_url() . '/railanalytics/blank-tracking-page' }}"
-                    loading="lazy"
-                    style="position: absolute; width:0; height:0; border:0;">
-            </iframe>
-        @endif
-    @endif
+    @include('partials._railanalytics-brand-tracking-iframe')
 
 @endsection
 
