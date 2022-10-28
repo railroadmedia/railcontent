fb94b8767ebd9fdf112033887acf1a84c1545be5
peterminhlee <petereminhlee@gmail.com>
2022-10-28 09:00:24 -0700

    alpine updates

--- resources/marketing/views/pianote/lead-gen/personality-quiz/entertainer.blade.php
+++ resources/marketing/views/pianote/lead-gen/personality-quiz/entertainer.blade.php
@@ -58,7 +58,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="/marketing/parcel/pianote/nav-footer.js"></script>
-    <script src="{{ mix('marketing/js/app.js') }}"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/unveilhooks/ls.unveilhooks.min.js"></script>
