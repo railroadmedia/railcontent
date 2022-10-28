3429c08c8334909c5478e33f309fbceb8e1331dd
calebfavor <calebfavor@gmail.com>
2022-10-28 10:51:15 -0700

    Fixing hardcoded drumeo brand that should not have been changed to brand()

--- app/Http/Controllers/Platform/ProfileSettingsPagesController.php
+++ app/Http/Controllers/Platform/ProfileSettingsPagesController.php
@@ -352,7 +352,7 @@ class ProfileSettingsPagesController extends BaseController
         // -------------------------------------------------------------------------------------------------------------
 
          $urlParamsByBrandForTrial = [
-            brand() => 'products[DLM-Trial]=1,month,1&locked=true',
+            'drumeo' => 'products[DLM-Trial]=1,month,1&locked=true',
             'pianote' => 'products[PIANOTE-MEMBERSHIP-TRIAL]=1&redirect=%2Forder&locked=true',
             'guitareo' => 'products[GUITAREO-7-DAY-TRIAL-ONE-TIME]=1&redirect=%2Forder&locked=true',
             'singeo' => 'products[singeo-monthly-recurring-7-day-trial-membership]=1&redirect=%2Forder&locked=true',
@@ -361,7 +361,7 @@ class ProfileSettingsPagesController extends BaseController
         if($urlParamsByBrandForTrial[$brand]){
             $urlParams = $urlParamsByBrandForTrial[$brand];
             $addToCartUrlTrial = 'https://' . $brand . '.com/ecommerce/add-to-cart?' . $urlParams;
-            if ($brand == brand()) {
+            if ($brand == 'drumeo') {
                 $addToCartUrlTrial = 'https://drumeo.com/laravel/public/shopping-cart/api/query?' . $urlParams;
             }
         } else {
@@ -369,17 +369,17 @@ class ProfileSettingsPagesController extends BaseController
         }
 
 //        $annualSKUsByBrand = [
-//            brand() => 'DLM-1-year',
+//            'drumeo' => 'DLM-1-year',
 //            'pianote' => 'PIANOTE-MEMBERSHIP-1-YEAR',
 //            'guitareo' => 'GUITAREO-1-YEAR-MEMBERSHIP',
 //            'singeo' => 'singeo-annual-recurring-membership',
 //        ];
 
-        $salesPage[brand()] = 'https://www.drumeo.com/';
+        $salesPage['drumeo'] = 'https://www.drumeo.com/';
         $salesPage['pianote'] = 'https://www.pianote.com/';
         $salesPage['guitareo'] = 'https://www.guitareo.com/';
         $salesPage['singeo'] = 'https://www.singeo.com/';
-        $salesPageUrl = $salesPage[$brand] ?? $salesPage[brand()];
+        $salesPageUrl = $salesPage[$brand] ?? $salesPage['drumeo'];
 
         // -------------------------------------------------------------------------------------------------------------
 
