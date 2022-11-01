90d68b72503347b0626a1c2c3aa0343cbb609eb3
Miguel Fondeur <miguelfondeur@gmail.com>
2022-11-01 19:32:53 +0000

    update js

--- resources/marketing/assets/js/app.js
+++ resources/marketing/assets/js/app.js
@@ -1,3 +1,16 @@
 require('./bootstrap');
 
 //Libraries
+import { createApp, defineAsyncComponent } from 'vue';
+import axios from 'axios';
+//components
+import ContactEmailForm from './vuesora/components/ContactEmailForm/ContactEmailForm.vue';
+
+//Initialize Vue
+const app = createApp({});
+//Mount Components
+app.component('ContactEmailForm', ContactEmailForm )
+//Use Libraries
+app.use('VueAxios', axios);
+//Mount Vue to App ID
+app.mount('#contactPageApp');
