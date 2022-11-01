8a7a1a41d8c873c246ab03d49f2efbac2ffd7c63
Miguel Fondeur <miguelfondeur@gmail.com>
2022-11-01 18:51:17 +0000

    get rid of vue code in marketing directory

--- resources/marketing/assets/js/app.js
+++ resources/marketing/assets/js/app.js
@@ -1,16 +1,3 @@
 require('./bootstrap');
 
 //Libraries
-import { createApp, defineAsyncComponent } from 'vue';
-import axios from 'axios';
-//components
-import ContactEmailForm from './vuesora/components/ContactEmailForm/ContactEmailForm.vue';
-
-//Initialize Vue
-const app = createApp({});
-//Mount Components
-app.component('ContactEmailForm', ContactEmailForm )
-//Use Libraries
-app.use('VueAxios', axios);
-//Mount Vue to App ID
-app.mount('#contactPageApp');
--- resources/marketing/assets/js/vuesora/assets/js/services/_error-handler.js
+++
@@ -1,3 +0,0 @@
-export default function (error) {
-    console.error(error);
-}
