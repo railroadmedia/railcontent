231d5ead467438dd3d0f980b9332bfa6e2a5206e
calebfavor <calebfavor@gmail.com>
2022-11-02 18:27:34 +0000

    updated header

--- resources/platform/views/partials/_unified-header.blade.php
+++ resources/platform/views/partials/_unified-header.blade.php
@@ -1,5 +1,5 @@
 <div id="unified-header" class="tw-px-[16px] lg:tw-px-[64px] tw-flex tw-w-full tw-h-[240px] tw-relative tw-z-0 tw-bg-cover tw-items-center tw-justify-center" style="background-image: url('{{ $backgroundImage }}')">
-    <div class="tw-absolute tw-z-10 tw-h-[69%] tw-bottom-0" style="background: linear-gradient(182.73deg, rgba(0, 16, 29, 0) -0.24%, #00101D 97.72%)"></div>
+    <div class="tw-absolute tw-w-full tw-z-10 tw-h-[69%] tw-bottom-0" style="background: linear-gradient(182.73deg, rgba(0, 16, 29, 0) -0.24%, #00101D 97.72%)"></div>
     <div class="tw-z-20 tw-w-full tw-h-auto">
         <div class="tw-flex tw-flex-row tw-w-full">
             {{ $content }}
