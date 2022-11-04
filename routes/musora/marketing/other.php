ef81efbf0464fa9b06bf960f6bae1004af89ccbc
Roxana Riza <roxana_r2000@yahoo.com>
2022-11-04 13:33:50 +0200

    Access code redemption for Power-pack page

--- routes/musora/marketing/other.php
+++ routes/musora/marketing/other.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Musora\MarketingController;
 use App\Http\Controllers\Musora\ReferralJoinController;
-use App\Http\Controllers\Platform\PasswordResetController;
 use Illuminate\Support\Facades\Route;
 
 Route::domain('{musoraDomain}')
@@ -17,4 +16,9 @@ Route::domain('{musoraDomain}')
         Route::get('brand', [MarketingController::class, 'brand']);
         Route::get('unified-2022', [MarketingController::class, 'unified2022']);
         Route::get('referral-join', [ReferralJoinController::class, 'join']);
+        Route::get('power-pack', [\App\Http\Controllers\Musora\CodeRedemptionController::class, 'powerPack']);
+        Route::post('hlag-submit',
+                    [\App\Http\Controllers\Musora\CodeRedemptionController::class, 'hitLikeAGirlSubmission']);
+        Route::get('/redeem',
+                   [\App\Http\Controllers\Musora\CodeRedemptionController::class, 'renderNewAccountRedeemPage']);
     });
