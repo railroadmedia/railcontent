3a17481458ad3673086849f0ba6c735503d917e8
Miguel Fondeur <miguelfondeur@gmail.com>
2022-11-09 22:06:09 +0000

    update carousel

--- resources/platform/assets/js/constants/carousel_data.js
+++ resources/platform/assets/js/constants/carousel_data.js
@@ -30,7 +30,7 @@ export const testCarousel = [
                 topSubtitle: "",
                 title: "New Songs Releases",
                 ctaText: "Go To Songs",
-                description: "November 14th - Student Request & Peter Erskine - 21 New Songs<br>November 23rd - Drumeo's Choice - 20 New Songs",
+                description: "November 14th - Student Request - 21 New Songs<br>November 23rd - Drumeo's Choice - 20 New Songs",
                 ctaUrl: "/drumeo/songs",
                 img: "https://musora.com/cdn-cgi/image/quality=100,width=1536,height=430,fit=cover,metadata=none/https://musora-web-platform.s3.amazonaws.com/carousel/Songs-banner.jpg",
             },
