6aeb98f573170646810cdd5891503b67cf1c3c60
calebfavor <calebfavor@gmail.com>
2022-11-09 10:24:58 -0800

    Adding proper expired membership redirects and view.

---
+++ app/Http/Controllers/Platform/ExpiredMemberController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Platform;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+
+class ExpiredMemberController extends BaseController
+{
+    public function showExpiredMemberPage(Request $request)
+    {
+        return view('pages.upgrade');
+    }
+}
