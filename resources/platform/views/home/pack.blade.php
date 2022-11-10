ec6b3c5efc536db3d2ffb1879112b5dd3bbb5bdc
calebfavor <calebfavor@gmail.com>
2022-11-10 11:11:43 -0800

    Fixing pack upgrade banner not showing in some cases. Fixing restart expired membership link.

--- resources/platform/views/home/pack.blade.php
+++ resources/platform/views/home/pack.blade.php
@@ -9,7 +9,6 @@
     <div class="tw-container tw-mx-auto tw-pt-[32px] tw-px-4 md:tw-px-8 dark:tw-text-white">
 
         {{-- Add Header Slide "Welcome Username" --}}
-        @if(empty(user()->membership_expiration_date) && user()->isPackOwner())
         <div class="tw-w-full tw-pb-[36px]">
             <static-header
                 title="JOIN THE COMMUNITY"
@@ -19,7 +18,6 @@
                 img="https://musora.com/cdn-cgi/image/width=720/https://cdn.musora.com/image/fetch/c_fill,w_1920,h_1080,q_auto:good/https://musora-web-platform.s3.amazonaws.com/carousel/pre-launch-header-image-jpg.jpg"
             ></static-header>
         </div>
-        @endif
 
         {{-- Continue Section --}}
         @if($startedContentCount > 0)
