cf27869307bde32c2cbc17342d59bfa83fb10aaf
Roxana Riza <roxana_r2000@yahoo.com>
2022-11-21 16:32:20 +0200

    Support for mobile app Customer.io SDK

--- app/Providers/MusoraApiUserProvider.php
+++ app/Providers/MusoraApiUserProvider.php
@@ -18,6 +18,8 @@ use Railroad\Railcontent\Services\CommentService;
 use Railroad\Railcontent\Services\ContentService;
 use Railroad\Railforums\Repositories\PostRepository;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use \Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class MusoraApiUserProvider implements UserProviderInterface
 {
@@ -129,10 +131,19 @@ class MusoraApiUserProvider implements UserProviderInterface
             $hasCompletedMethod = $methodContent['completed'];
         }
 
-        $customerIoData = $this->customerIoService->getCustomerByUserId(config('event-data-synchronizer.customer_io_account_to_sync_all_brands'),
-                                                                        $user->id);
-        $extraData = [];
-        if (!empty($externalAttributes = $customerIoData->getExternalAttributes())) {
+        try {
+            $customerIoData = $this->customerIoService->getCustomerByUserId(config('event-data-synchronizer.customer_io_account_to_sync_all_brands'),
+                                                                            $user->id);
+        } catch (ModelNotFoundException $exception) {
+            $customerIoData = null;
+        }
+
+        $extraData = [
+            'cio_id' => null,
+            'customer_io_id' => null,
+        ];
+
+        if ($customerIoData && !empty($externalAttributes = $customerIoData->getExternalAttributes())) {
             $extraData = [
                 'cio_id' => $externalAttributes['cio_id'],
                 'customer_io_id' => $externalAttributes['id'],
@@ -148,7 +159,7 @@ class MusoraApiUserProvider implements UserProviderInterface
             'last_name' => $user->last_name,
             'avatarUrl' => $user->profile_picture_url,
             'profile_picture_url' => $user->profile_picture_url,
-            'helpscout_beacon_id' => config('railhelpscout.helpscout_tracking_beacon_id.' . brand() ),
+            'helpscout_beacon_id' => config('railhelpscout.helpscout_tracking_beacon_id.' . brand()),
             'level_rank' => $user->getMethodLevel(),
             'has_started_method' => $hasStartedMethod ?? false,
             'has_completed_method' => $hasCompletedMethod ?? false,
