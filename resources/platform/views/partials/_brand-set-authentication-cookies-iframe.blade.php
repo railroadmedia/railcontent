2b79f8867b1d28fb3fe21c21362501bce393ffc1
calebfavor <calebfavor@gmail.com>
2022-11-23 12:39:30 -0800

    Adding legacy brand auth cookie push with iframe to all logged in requests on members area pages. This ensures people are authed on the brand domains.

---
+++ resources/platform/views/partials/_brand-set-authentication-cookies-iframe.blade.php
@@ -0,0 +1,28 @@
+@if((session()->has('logged_in_recently') && !empty(user())) ||
+(!session()->has('last_set_auth_time') || (time() - session()->get('last_set_auth_time') > 1000) ))
+
+    @php
+        session()->put('last_set_auth_time', time());
+    @endphp
+
+    <iframe src="{{ get_legacy_brand_base_url('drumeo', false) . '?user_id=' . user()->id .
+            '&auth_key=' . generate_musora_cross_platform_login_key(user()->id, user()->password) }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+    <iframe src="{{ get_legacy_brand_base_url('pianote', false) . '?user_id=' . user()->id .
+            '&auth_key=' . generate_musora_cross_platform_login_key(user()->id, user()->password) }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+    <iframe src="{{ get_legacy_brand_base_url('guitareo', false) . '?user_id=' . user()->id .
+            '&auth_key=' . generate_musora_cross_platform_login_key(user()->id, user()->password) }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+    <iframe src="{{ get_legacy_brand_base_url('singeo', false) . '?user_id=' . user()->id .
+            '&auth_key=' . generate_musora_cross_platform_login_key(user()->id, user()->password) }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+@endif
