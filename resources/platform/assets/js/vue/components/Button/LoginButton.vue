a4dbdf2927aec025654f1f838399990eb4e14b26
Miguel Fondeur <miguelfondeur@gmail.com>
2022-11-23 01:57:24 +0000

    update log in button

---
+++ resources/platform/assets/js/vue/components/Button/LoginButton.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "button",
+  },
+  disabled: {
+    type: Boolean,
+    default: true,
+  },
+});
+const emit = defineEmits(["onButtonClick"]);
+const handleClick = (e) => {
+  if (props.disabled) {
+    e.preventDefault();
+  } else if (props.type === "button" && !props.disabled) {
+    e.preventDefault();
+    emit("onButtonClick", e);
+  }
+};
+</script>
+
+<template>
+  <button
+    :type="type"
+    class="tw-mb-[20px] tw-mx-4"
+    :class="disabled ? 'tw-btn-secondary tw-text-[#445F74]' : 'tw-btn-primary tw-bg-white tw-text-[#00101D]'"
+    dusk="submit-button"
+    @click="handleClick"
+  >
+    {{ label ? label : '' }}
+    <slot></slot>
+  </button>
+</template>
