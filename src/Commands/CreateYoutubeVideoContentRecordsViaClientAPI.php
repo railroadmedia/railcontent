8f999e2641512dd9ff5580143a3ed6b26ca71ff0
robwillems <robert.j.willems@gmail.com>
2022-11-24 10:03:26 -0800

    Create Youtube function updates for MWP

--- src/Commands/CreateYoutubeVideoContentRecordsViaClientAPI.php
+++ src/Commands/CreateYoutubeVideoContentRecordsViaClientAPI.php
@@ -6,29 +6,27 @@ use Carbon\Carbon;
 use Google_Client;
 use Google_Service_YouTube;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 use Railroad\Railcontent\Services\ContentFieldService;
 use Railroad\Railcontent\Services\ContentService;
+use Railroad\Railcontent\Services\ConfigService;
 
 
 class CreateYoutubeVideoContentRecordsViaClientAPI extends Command
 {
     const MAX_PAGES_ALLOWED = 5;
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:CreateYoutubeVideoContentRecordsViaClientAPI {maxPages?} {skipPages?}';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'content:CreateYoutubeVideoContentRecordsViaClientAPI {maxPages?} {skipPages?}';
     protected $description = 'Pull YT videos from channel and sync to content database.';
 
     protected $scope = 'https://www.googleapis.com/auth/youtube';
+
+
+    public function info($string, $verbosity = null)
+    {
+        Log::info($string); //also write info statements to log
+        $this->line($string, 'info', $verbosity);
+    }
 
     /**
      * Execute the console command.
@@ -37,189 +35,197 @@ class CreateYoutubeVideoContentRecordsViaClientAPI extends Command
      */
     public function handle(ContentFieldService $contentFieldService, ContentService $contentService)
     {
-        $client = new Google_Client();
-        $youtube = new Google_Service_YouTube($client);
+        $this->info("Processing $this->name");
+        $timeStart = microtime(true);
 
-        $client->setClientId(config('railcontent.video_sync.youtube_client_api.client_id'));
-        $client->setClientSecret(config('railcontent.video_sync.youtube_client_api.client_secret'));
+        foreach (ConfigService::$availableBrands as $brand) {
+            $this->info("Processing $brand videos");
+            $client = new Google_Client();
+            $youtube = new Google_Service_YouTube($client);
 
-        $client->setScopes(['https://www.googleapis.com/auth/youtube']);
-        $client->setAccessType("offline");
-        $client->setApprovalPrompt('force');
+            $client->setClientId(config("railcontent.video_sync.$brand.youtube_client_api.client_id"));
+            $client->setClientSecret(config("railcontent.video_sync.$brand.youtube_client_api.client_secret"));
 
-        $tokenData = $client->refreshToken(
-            config('railcontent.video_sync.youtube_client_api.refresh_token')
-        );
+            $client->setScopes(['https://www.googleapis.com/auth/youtube']);
+            $client->setAccessType("offline");
+            $client->setApprovalPrompt('force');
 
-        $client->setAccessToken($tokenData['access_token']);
+            $tokenData = $client->refreshToken(
+                config("railcontent.video_sync.$brand.youtube_client_api.refresh_token")
+            );
 
-        $shouldEnd = 0;
-        $items = [];
+            $client->setAccessToken($tokenData['access_token']);
 
-        if (is_numeric($this->argument('maxPages'))) {
-            $maxPages = (int)$this->argument('maxPages');
-        }
-
-        if (empty($maxPages) || $maxPages > $this::MAX_PAGES_ALLOWED) {
-            $maxPages = $this::MAX_PAGES_ALLOWED;
-        }
-
-        $skipPages = 0;
-        if (is_numeric($this->argument('skipPages'))) {
-            $skipPages = (int)$this->argument('skipPages');
-
-            // todo: maybe rename max pages since it's no longer the max *number* of pages, but rather which page to stop at
-            $maxPages = $maxPages + $skipPages;
-        }
-
-        // get the channels list for youtube user
-        // note: a refresh token generally represents a single YT channel so we should never get more
-        // than 1 channel back here
-        $channelsResponse = $youtube->channels->listChannels(
-            'contentDetails',
-            array(
-                'mine' => true,
-                'maxResults' => 50,
-            )
-        );
-
-        // for each channel get the videos from the channel playlists
-        foreach ($channelsResponse['items'] as $channel) {
-            $this->info('Processing a set of videos');
-            $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
-            $nextPageToken = '';
             $shouldEnd = 0;
-            $page = $skipPages; // default if supplied is 0
+            $items = [];
 
-            // only 50 video can be received in a call to Youtube API, so we make calls until complete
-            do {
-                $this->info('Retrieving 50 items');
+            if (is_numeric($this->argument('maxPages'))) {
+                $maxPages = (int)$this->argument('maxPages');
+            }
 
-                $playlistItemsResponse = $youtube->playlistItems->listPlaylistItems(
-                    'id,snippet,contentDetails',
-                    array(
-                        'playlistId' => $uploadsListId,
-                        'maxResults' => 50,
-                        'pageToken' => $nextPageToken,
-                    )
-                );
+            if (empty($maxPages) || $maxPages > $this::MAX_PAGES_ALLOWED) {
+                $maxPages = $this::MAX_PAGES_ALLOWED;
+            }
 
-                foreach ($playlistItemsResponse['items'] as $playlistItem) {
-                    $videoId = $playlistItem['snippet']['resourceId']['videoId'];
+            $skipPages = 0;
+            if (is_numeric($this->argument('skipPages'))) {
+                $skipPages = (int)$this->argument('skipPages');
 
-                    // get video details
-                    $videoParams = array(
-                        'id' => $videoId,
-                        'fields' => "items(contentDetails(duration))",
+                // todo: maybe rename max pages since it's no longer the max *number* of pages, but rather which page to stop at
+                $maxPages = $maxPages + $skipPages;
+            }
+
+            // get the channels list for youtube user
+            // note: a refresh token generally represents a single YT channel so we should never get more
+            // than 1 channel back here
+            $channelsResponse = $youtube->channels->listChannels(
+                'contentDetails',
+                array(
+                    'mine' => true,
+                    'maxResults' => 50,
+                )
+            );
+
+            // for each channel get the videos from the channel playlists
+            foreach ($channelsResponse['items'] as $channel) {
+                $this->info('Processing a set of videos');
+                $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
+                $nextPageToken = '';
+                $shouldEnd = 0;
+                $page = $skipPages; // default if supplied is 0
+
+                // only 50 video can be received in a call to Youtube API, so we make calls until complete
+                do {
+                    $this->info('Retrieving 50 items');
+
+                    $playlistItemsResponse = $youtube->playlistItems->listPlaylistItems(
+                        'id,snippet,contentDetails',
+                        array(
+                            'playlistId' => $uploadsListId,
+                            'maxResults' => 50,
+                            'pageToken' => $nextPageToken,
+                        )
                     );
-                    $videoDetails = $youtube->videos->listVideos("contentDetails", $videoParams);
 
-                    $duration = $videoDetails->getItems()[0]->getContentDetails()->getDuration();
-                    $duration = $this->covtime($duration);
+                    foreach ($playlistItemsResponse['items'] as $playlistItem) {
+                        $videoId = $playlistItem['snippet']['resourceId']['videoId'];
+
+                        // get video details
+                        $videoParams = array(
+                            'id' => $videoId,
+                            'fields' => "items(contentDetails(duration))",
+                        );
+                        $videoDetails = $youtube->videos->listVideos("contentDetails", $videoParams);
+
+                        $duration = $videoDetails->getItems()[0]->getContentDetails()->getDuration();
+                        $duration = $this->covtime($duration);
 
 //                    $this->info('Retrieved video ' . $videoId . ' with duration value of ' . $duration);
 
-                    $video = [
-                        'videoId' => $videoId,
-                        'duration' => $duration,
-                    ];
-                    array_push($items, $video);
-                }
-                $nextPageToken = $playlistItemsResponse->getNextPageToken();
+                        $video = [
+                            'videoId' => $videoId,
+                            'duration' => $duration,
+                        ];
+                        array_push($items, $video);
+                    }
+                    $nextPageToken = $playlistItemsResponse->getNextPageToken();
 
-                $page++;
+                    $page++;
 
-                if (is_null($nextPageToken) || $page >= $maxPages) {
-                    $shouldEnd = 1;
-                }
-            } while ($shouldEnd == 0);
-        }
-
-        $contentCreatedCount = 0;
-        $contentFieldsInsertData = [];
-        $contentCreationFailed = [];
-
-        $processed = 0;
-
-        foreach ($items as $video) {
-            if ($processed % 25 == 0) {
-                $this->info('Processing 25 items (so far processed '.$processed.' of '.count($items));
+                    if (is_null($nextPageToken) || $page >= $maxPages) {
+                        $shouldEnd = 1;
+                    }
+                } while ($shouldEnd == 0);
             }
 
-            // create if needed
-            $existingRecords = $contentService->getBySlugAndType(
-                'youtube-video-'.$video['videoId'],
-                'youtube-video'
-            );
+            $contentCreatedCount = 0;
+            $contentFieldsInsertData = [];
+            $contentCreationFailed = [];
 
-            if ($existingRecords->count() == 0 && $video['duration'] !== 0 && is_numeric($video['duration'])) {
-                $this->info('video '.$video['videoId'].' not found in our database');
+            $processed = 0;
 
-                // store a new content
-                $content = $contentService->create(
-                    'youtube-video-'.$video['videoId'],
-                    'youtube-video',
-                    ContentService::STATUS_PUBLISHED,
-                    null,
-                    null,
-                    null,
-                    Carbon::now()->toDateTimeString(),
-                    null,
-                    0,
-                    false
+            foreach ($items as $video) {
+                if ($processed % 25 == 0) {
+                    $this->info('Processing 25 items (so far processed ' . $processed . ' of ' . count($items));
+                }
+
+                // create if needed
+                $existingRecords = $contentService->getBySlugAndType(
+                    'youtube-video-' . $video['videoId'],
+                    'youtube-video'
                 );
 
-                if (empty($content)) {
-                    $contentCreationFailed[] = $video['videoId'];
+                if ($existingRecords->count() == 0 && $video['duration'] !== 0 && is_numeric($video['duration'])) {
+                    $this->info('video ' . $video['videoId'] . ' not found in our database');
+
+                    // store a new content
+                    $content = $contentService->create(
+                        'youtube-video-' . $video['videoId'],
+                        'youtube-video',
+                        ContentService::STATUS_PUBLISHED,
+                        null,
+                        $brand,
+                        null,
+                        Carbon::now()->toDateTimeString(),
+                        null,
+                        0,
+                        false
+                    );
+
+                    if (empty($content)) {
+                        $contentCreationFailed[] = $video['videoId'];
+                    } else {
+                        $contentCreatedCount++;
+                        $contentFieldsInsertData[] = $contentFieldService->create(
+                            $content['id'],
+                            'youtube_video_id',
+                            $video['videoId'],
+                            1,
+                            'string'
+                        );
+                        $contentFieldsInsertData[] = $contentFieldService->create(
+                            $content['id'],
+                            'length_in_seconds',
+                            $video['duration'],
+                            1,
+                            'integer'
+                        );
+                    }
                 } else {
-                    $contentCreatedCount++;
-                    $contentFieldsInsertData[] = $contentFieldService->create(
-                        $content['id'],
-                        'youtube_video_id',
-                        $video['videoId'],
-                        1,
-                        'string'
-                    );
-                    $contentFieldsInsertData[] = $contentFieldService->create(
-                        $content['id'],
-                        'length_in_seconds',
-                        $video['duration'],
-                        1,
-                        'integer'
-                    );
+                    if ($video['duration'] === 0) {
+                        $this->info(
+                            'Duration ' . 'for video ' . $video['videoId'] . ' is zero and thus video not added.'
+                        );
+                    } elseif (!is_numeric($video['duration'])) {
+                        $this->info(
+                            'Duration ' . 'for video ' . $video['videoId'] .
+                            ' is not numeric and thus video not added.' . $video['duration']
+                        );
+                    }
                 }
-            } else {
-                if ($video['duration'] === 0) {
-                    $this->info(
-                        'Duration '.'for video '.$video['videoId'].' is zero and thus video not added.'
-                    );
-                } elseif (!is_numeric($video['duration'])) {
-                    $this->info(
-                        'Duration '.'for video '.$video['videoId'].
-                        ' is not numeric and thus video not added.'.$video['duration']
-                    );
-                }
+                $processed++;
             }
-            $processed++;
-        }
 
-        $this->info(
-            'Processed '.count($items).' videos. '.
-            (count($contentFieldsInsertData) + $contentCreatedCount).' DB rows created.'
-        );
-
-        if (!empty($contentCreationFailed)) {
             $this->info(
-                'There was|were '.count($contentCreationFailed).' content creation failure(s):'
+                'Processed ' . count($items) . ' videos. ' .
+                (count($contentFieldsInsertData) + $contentCreatedCount) . ' DB rows created.'
             );
-            $this->info(print_r($contentCreationFailed, true));
+
+            if (!empty($contentCreationFailed)) {
+                $this->info(
+                    'There was|were ' . count($contentCreationFailed) . ' content creation failure(s):'
+                );
+                $this->info(print_r($contentCreationFailed, true));
+            }
         }
 
-        $this->info('CreateYoutubeVideoContentRecordsViaClientAPI complete.');
+        $diff = microtime(true) - $timeStart;
+        $sec = intval($diff);
+        $this->info("Finished $this->name ($sec s)");
     }
 
     /** Convert video's duration from ISO 8601 to seconds
-     * @param  string  $youtube_time  - ISO 8601
+     * @param string $youtube_time - ISO 8601
      * @return float|int
      */
     function covtime($youtube_time)
