c92a744018a90a33cf74a46d14dbfa86e69d1ab5
robwillems <robert.j.willems@gmail.com>
2022-11-29 15:01:06 -0800

    Schedule renewalDueSubscriptions

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -62,6 +62,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('content:CreateVimeoVideoContentRecords', [50])->everyThirtyMinutes();
         $schedule->command('content:CreateYoutubeVideoContentRecordsViaClientAPI', [1])->cron("0 */6 * * *");
 
+        $schedule->command('ecommerce:renewalDueSubscriptions')->cron("15 */8 * * *"); // every 8 hours
+
         $schedule->command('mentors:verify')->daily();
         //temporary measure to assign mentors until ecommerce is integrated with MWP
         $schedule->command('mentors:assign')->hourly();
