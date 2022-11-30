2f4c8c6db50b5c1bd7ee9a970e60f13fef1ccc38
peterminhlee <petereminhlee@gmail.com>
2022-11-30 10:02:12 -0800

    holiday components

--- resources/marketing/views/guitareo/_partials/holiday-logo.blade.php
+++ resources/marketing/views/guitareo/_partials/holiday-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="{{ $styles }}" src="https://cdn.musora.com/image/fetch/w_450,q_auto:best/https://drumeo-assets.s3.amazonaws.com/promos/christmas/holiday-drums.png">
+<img class="{{ $styles }}" src="https://cdn.musora.com/image/fetch/w_450,q_auto:best/https://guitareo.s3.amazonaws.com/sales/promos/november/holiday-guitar.png">
--- resources/marketing/views/pianote/_partials/holiday-logo.blade.php
+++ resources/marketing/views/pianote/_partials/holiday-logo.blade.php
@@ -1,1 +1,1 @@
-<img class="{{ $styles }}" src="https://cdn.musora.com/image/fetch/w_450,q_auto:best/https://drumeo-assets.s3.amazonaws.com/promos/christmas/holiday-drums.png">
+<img class="{{ $styles }}" src="https://cdn.musora.com/image/fetch/w_450,q_auto:best/https://pianote.s3.amazonaws.com/sales/promos/christmas/holiday-piano.png">
