1738d32da601f343c3d6b3d72c46e2ae94f6e2ee
robwillems <robert.j.willems@gmail.com>
2022-12-01 21:14:58 -0800

    Change to batch query

--- app/Modules/Ecommerce/Console/Commands/ProcessAppleExpiredSubscriptions.php
+++ app/Modules/Ecommerce/Console/Commands/ProcessAppleExpiredSubscriptions.php
@@ -7,12 +7,12 @@ use App\Modules\Ecommerce\Jobs\ProcessAppleExpiredSubscriptionsJob;
 
 class ProcessAppleExpiredSubscriptions extends Command
 {
-    protected $signature = 'ecommerce:ProcessAppleExpiredSubscriptions';
+    protected $signature = 'ecommerce:ProcessAppleExpiredSubscriptionsBatched';
 
     protected $description = 'Queries Apple to get the state of subscriptions due to expire';
 
     public function handle() {
-        $this->runChainQuery(function (int $skip, int $take) {
+        $this->runBatchQuery(function (int $skip, int $take) {
             return new ProcessAppleExpiredSubscriptionsJob($skip, $take);
         }, chunks: 100);
     }
