74b66d2123d47223bde0542a104d70b1293f7acb
peterminhlee <petereminhlee@gmail.com>
2022-12-06 11:27:15 -0800

    imported holiday global components

--- resources/marketing/views/drumeo/sales/standard.blade.php
+++ resources/marketing/views/drumeo/sales/standard.blade.php
@@ -79,7 +79,6 @@
 @section('sticky-bar')
     @include('_partials.layout.holiday.sticky-bar', [
         'text' => 'GET 10 FREE BONUSES <br> WORTH $1228.94',
-        'homepage' => true,
     ])
 
     {{--<div class="h-10 relative w-full block" style="background:linear-gradient(to bottom, #022040, #01050f);"></div>--}}
