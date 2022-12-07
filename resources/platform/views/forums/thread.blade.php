8155ddd27d864c4f1b427c5d0c353a4ee3eeb9e4
Miguel Fondeur <miguelfondeur@gmail.com>
2022-12-07 16:56:38 +0000

    update forum container width

--- resources/platform/views/forums/thread.blade.php
+++ resources/platform/views/forums/thread.blade.php
@@ -32,14 +32,16 @@
 
 @section('content')
 
-    <forum-thread
-        theme-color="{{ $brand }}"
-        brand="{{ $brand }}"
-        :thread="{{ $thread }}"
-        :current-user="{{ $currentUser }}"
-        previous-page="{{ $categoryUrl }}"
-        post-store-form-url="{{ url()->route('railforums.post.store')}}"
-        update-post-base-route="{{ url()->route('railforums.post.update',['#####']).'?redirect='.url()->route('forums.jump-to-post',['#####']) }}"
-    />
+    <div class="tw-w-full tw-max-w-[1703px] tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white">
+        <forum-thread
+            theme-color="{{ $brand }}"
+            brand="{{ $brand }}"
+            :thread="{{ $thread }}"
+            :current-user="{{ $currentUser }}"
+            previous-page="{{ $categoryUrl }}"
+            post-store-form-url="{{ url()->route('railforums.post.store')}}"
+            update-post-base-route="{{ url()->route('railforums.post.update',['#####']).'?redirect='.url()->route('forums.jump-to-post',['#####']) }}"
+        />
+    </div>
 
 @endsection
