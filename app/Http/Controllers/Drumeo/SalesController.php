47277d13b5370347b67153881518b3de732f0bb3
Trent <trent.harms@gmail.com>
2022-12-08 10:58:32 -0800

    changing directories

--- app/Http/Controllers/Drumeo/SalesController.php
+++ app/Http/Controllers/Drumeo/SalesController.php
@@ -251,7 +251,7 @@ class SalesController extends BaseController
         $products = $this->productRepository->all();
         $products = array_combine(array_entity_column($products, 'getSku'), $products);
 
-        return view('drumeo.sales.2023-home', ['products' => $products, 'theme' => 'drumeo']);
+        return view('musora.sales.subscription-homepages', ['products' => $products, 'theme' => 'drumeo']);
     }
     public function salesStudents()
     {
