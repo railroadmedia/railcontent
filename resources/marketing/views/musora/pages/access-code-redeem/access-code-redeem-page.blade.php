42dbfef02c78681b96f6d7a5955b8a062e7d887d
Trent <trent.harms@gmail.com>
2022-12-09 13:14:31 -0800

    cleaning up head

--- resources/marketing/views/musora/pages/access-code-redeem/access-code-redeem-page.blade.php
+++ resources/marketing/views/musora/pages/access-code-redeem/access-code-redeem-page.blade.php
@@ -1,19 +1,14 @@
 @extends('musora._partials.layout')
 
 @section('head-includes')
-
-    <meta property="fb:app_id" content="1772693566314871"/>
     <title>Access Pass Redeem | Musora</title>
     <meta name="description" content="To redeem your access pass enter your code below!">
-
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" />
     <link href="{{ asset('/marketing/css/pianote/tailwind-helpers.css') }}" rel="stylesheet">
     <link href="{{ asset('/marketing/parcel/pianote/nav-footer.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="/marketing/parcel/pianote/lead-gen.css">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
     <script type="text/javascript">
         $(document).ready(
             function () {
@@ -166,7 +161,7 @@
             border-bottom-color: transparent;
         }
     </style>
-    <style type="text/css">
+    <style>
         [placeholder]:focus::-webkit-input-placeholder {
             color: transparent;
         }
