7571ed94174a293e3b442fca368e0bccdc009f31
peterminhlee <petereminhlee@gmail.com>
2022-12-09 12:23:59 -0800

    changes for january

--- resources/marketing/views/_partials/layout/holiday/homepage-bottom-membership.blade.php
+++ resources/marketing/views/_partials/layout/holiday/homepage-bottom-membership.blade.php
@@ -1,29 +1,28 @@
+{{--<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;900&display=swap" rel="stylesheet">--}}
+
 <div style="background:linear-gradient(to bottom, #02010f, #010522);">
-    <section class="content-section text-center customize px-4 lg:px-6 relative z-50 overflow-hidden bg-top lazyload" style="background: url(https://drumeo-assets.s3.amazonaws.com/promos/christmas/snow-dark.gif) center center/250px;box-shadow: 0 0 100px inset #000;">
+    <section class="content-section text-center customize px-4 lg:px-6 relative z-50 overflow-hidden bg-center bg-cover lazyload" style="background: url(https://drumeo-assets.s3.amazonaws.com/sales/2023/order-bg.jpg);">
         <div class="container mx-auto relative z-50">
             <div class="mx-auto max-w-4xl" style="font-size: 0;">
                 <div class="w-full">
-                    @if(!empty($singeo))
-                        <img class="h-36 sm:h-72 lg:h-96" src="https://cdn.musora.com/image/fetch/w_1100,q_auto:best/{{$logo}}" alt="logo" />
-                    @else
+                    <h2 class="uppercase font-extrabold text-[#FFAC00] mb-8" style="font-family: 'Raleway', sans-serif;">New year. New songs.</h2>
                         <div class="bonus-wrap relative inline-block align-top mx-auto px-1 md:px-3 w-full max-w-md">
-                            <img class="h-20 absolute top-0 right-0 z-10 -mt-8 -mr-2" src="https://drumeo-assets.s3.amazonaws.com/promos/december/santa-hat.png">
+                            <p class="absolute -top-[8px] right-0 z-10 bg-[#FFAC00] rounded-full text-black text-xs px-2 py-1">SAVE __%</p>
                             <div class=" inline-block relative w-full group" style="padding-bottom: 47%;perspective: 1000px;">
                                 <div class="text-center w-full h-full absolute" style="transform-style: preserve-3d;">
-                                    <div class="border-2 border-promo front absolute z-20 overflow-hidden rounded-3xl w-full h-full transition-transform duration-700" style="backface-visibility: hidden;">
+                                    <div class="front absolute z-20 overflow-hidden rounded-3xl w-full h-full transition-transform duration-700" style="backface-visibility: hidden;">
                                         <div class="h-full w-full bg-black bg-center bg-cover lazyload" data-bg="https://cdn.musora.com/image/fetch/w_850,q_auto:best/{{ $logo }}"></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @endif
                     <h3 class="leading-tight my-3">
                         {!! $joinText !!}
                     </h3>
-                    <a class="join promo bigger my-4 md:my-6 w-full max-w-xs md:max-w-lg lg:max-w-xl" style="padding: 20px 10px;" href="{{ $annualLink }}" {{--data-open="orderModal"--}}>GET Started &raquo;</a>
+                    <a class="join bg-{{$theme}} bigger mb-2 mt-4 md:mt-6 w-full max-w-xs md:max-w-lg lg:max-w-xl" style="padding: 20px 10px;" href="{{ $annualLink }}" {{--data-open="orderModal"--}}>GET Started <i class="fas fa-arrow-right" aria-hidden="true"></i></a>
                     <p class="leading-tight text-sm"><em>Billed at ${{ Prices::$drumeoEdgeAnnual }} per year.<br class="inline sm:hidden">  Cancel anytime. 90-day guarantee.</em></p>
-                    <h4 class="leading-tight my-6 sm:my-8">
-                        <strong>{{ $bonusNum }} FREE BONUSES.</strong> <em>ONLY<br class="inline sm:hidden"> AVAILABLE UNTIL DECEMBER 26</em>
+                    <h4 class="leading-tight my-6 sm:my-8 uppercase">
+                        + get {{ $bonusNum }} free bonuses worth ${{ $worth }}.
                     </h4>
                 </div>
 
@@ -31,7 +30,7 @@
                     <div class="bonus-wrap relative inline-block align-top mx-auto mb-4 px-1 md:px-3 {{ $tileWidth }}">
                         <div class="flip-div inline-block relative w-full group" style="@if(empty($bonus['bigCard'])) padding-bottom: 133%; @else padding-bottom: 103%; @endif perspective: 1000px;">
                             <div class="text-center w-full h-full absolute cursor-pointer" style="transform-style: preserve-3d;">
-                                <div class="border-2 border-promo front absolute z-20 overflow-hidden rounded-xl w-full h-full transition-transform duration-700" style="@if(!empty($bonus['special'])) overflow: visible;border-color: #cda880; @endif backface-visibility: hidden;">
+                                <div class="border-2 border-[#FFAC00] front absolute z-20 overflow-hidden rounded-xl w-full h-full transition-transform duration-700" style="@if(!empty($bonus['special'])) overflow: visible;border-color: #cda880; @endif backface-visibility: hidden;">
                                     <div class="overflow-hidden rounded-xl h-full w-full bg-black bg-cover bg-cover lazyload" data-bg="https://cdn.musora.com/image/fetch/w_460,q_auto:best/{{ $bonus['image'] }}"></div>
                                     <div class="absolute z-40 text-center top-1/2 left-1/2 text-white transition-opacity duration-300 transform -translate-x-1/2 -translate-y-1/2 visible opacity-0 group-hover:opacity-100 text-shadow-2">
                                         <i class="fas fa-arrow-right text-4xl"></i><br>
@@ -48,7 +47,7 @@
 
                         <p class="w-full leading-normal mt-2">
                             {{--<strong class="font-black leading-tight inline-block mb-1">{!!  $bonus['title']  !!}</strong><br>--}}
-                            <span style="text-transform:uppercase; display:inline-block;"><s>${{ $bonus['price'] }}</s> <strong class="text-promo">FREE</strong></span><br>
+                            <span style="text-transform:uppercase; display:inline-block;"><s>${{ $bonus['price'] }}</s> <strong class="text-[#FFAC00]">FREE</strong></span><br>
                             <em>
                                 @if(!empty($bonus['shipping']))
                                     Free Shipping
@@ -62,7 +61,7 @@
                 @endforeach
                 {{-- <p class=" mt-4 md:mt-5" style="display:inline-block;background: -webkit-linear-gradient(20deg, #03c8ac, #0976db, #9a01ee, #f61a30);-webkit-background-clip: text;-webkit-text-fill-color: transparent;"><strong>By joining today, we’ll donate 20% of your new membership<br class="hidden sm:inline"> towards the <a target="_blank" href="https://musicounts.ca/en/take-action/ways-of-giving/fundraise-on-musicounts-behalf/fundraisers-supporting-musicounts/give-the-gift-of-music-with-musora/"><u>MusiCounts Band Aid Program</u></a>.</strong></p> --}}
                 {{--            <h4 class="leading-tight mt-4"><strong>Only $12.50/month <br class="inline sm:hidden">(billed annually at ${{ Prices::$drumeoEdgeAnnual }}).</strong></h4>--}}
-                <a class="join promo bigger my-4 md:my-5 w-full max-w-xs md:max-w-lg lg:max-w-3xl" style="padding: 20px 10px;" href="{{ $annualLink }}" {{--data-open="orderModal"--}}>GET Started &raquo;</a>
+                <a class="join bg-{{$theme}} bigger my-4 md:my-5 w-full max-w-xs md:max-w-lg lg:max-w-xl" style="padding: 20px 10px;" href="{{ $annualLink }}" {{--data-open="orderModal"--}}>GET Started <i class="fas fa-arrow-right" aria-hidden="true"></i></a>
                 {{-- <p class="leading-tight">Billed annually at <s class="opacity-60">${{ Prices::$drumeoEdgeAnnualFull }}</s>${{ Prices::$drumeoEdgeAnnual }} per year.</p> --}}
                 <br>
                 <a class="inline-block text-light-navy mt-2" href="{{ $monthlyLink }}"><p><u><em>Or start a monthly membership for <br class="inline-block md:hidden">${{ Prices::$drumeoEdgeRegular }}/month. (no bonuses)</em></u></p></a>
