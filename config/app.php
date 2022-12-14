18e374b3968640a41c01f595197b5ef3e49fcb74
robwillems <robert.j.willems@gmail.com>
2022-12-14 11:36:41 -0800

    Add reporting module with recentrequests report

--- config/app.php
+++ config/app.php
@@ -192,6 +192,7 @@ return [
         App\Modules\CustomerIO\Providers\CustomerIoServiceProvider::class,
         App\Modules\Content\Providers\ContentServiceProvider::class,
         App\Modules\Notifications\Providers\NotificationServiceProvider::class,
+        App\Modules\Reporting\Providers\ReportingServiceProvider::class,
 
         /*
          * Application Service Providers...
