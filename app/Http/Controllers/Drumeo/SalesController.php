9a7d064d5a15081191b9ceb98912ada5208356ab
peterminhlee <petereminhlee@gmail.com>
2022-12-16 11:32:22 -0800

    file path update

--- app/Http/Controllers/Drumeo/SalesController.php
+++ app/Http/Controllers/Drumeo/SalesController.php
@@ -272,7 +272,7 @@ class SalesController extends BaseController
         $products = $this->productRepository->all();
         $products = array_combine(array_entity_column($products, 'getSku'), $products);
 
-        return view('drumeo.sales.pages.method', ['products' => $products, 'theme' => 'drumeo']);
+        return view('drumeo.sales.pages.method.method', ['products' => $products, 'theme' => 'drumeo']);
     }
     public function salesStudents()
     {
