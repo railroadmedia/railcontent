38b81e3d5a5149a18b14ee4aad7a411f0cbe82c7
peterminhlee <petereminhlee@gmail.com>
2022-12-16 11:31:53 -0800

    file path changes

---
+++ resources/marketing/views/drumeo/sales/pages/method/method.blade.php
@@ -0,0 +1,301 @@
+@extends('drumeo._partials.layout-template')
+
+@section('global-head')
+    <title></title>
+    <meta property="og:title" content="">
+    <meta property="og:url" content="https://www.drumeo.com/">
+    <meta name="description" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
+
+    @include('drumeo._partials._fonts')
+
+    <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
+    <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/css/drumeo/tailwind-helpers.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/parcel/drumeo/sales-2020.css') }}" rel="stylesheet">
+    {{--<link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">--}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/css/splide.min.css">
+
+    <style>
+        .slick-slider.slick-light-buttons .slick-arrow {
+            background: #fff;
+        }
+    </style>
+
+    <style>
+        .splide__pagination__page.is-active {
+            background: #01050F;
+        }
+
+        .splide__arrow svg {
+            fill: #0B76DB !important;
+        }
+
+        .bubble:after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 50%;
+            width: 0;
+            height: 0;
+            border: 5px solid transparent;
+            border-top-color: black;
+            border-bottom: 0;
+            margin-left: -5px;
+            margin-bottom: -5px;
+        }
+    </style>
+@stop
+
+@section('body-data')
+    x-data ='{
+    trailer : false
+    }'
+@endsection
+
+@section('global-body')
+    @if(!empty($trialVersion))
+        @if(!empty($joinUrl))
+            @include("drumeo.sales.partials._nav", [
+                "edgeVersion" => true,
+                "trialVersion" => true,
+                "joinUrl" => $joinUrl
+            ])
+        @else
+            @include("drumeo.sales.partials._nav", [
+                "edgeVersion" => true,
+                "trialVersion" => true,
+                "scrollToJoin" => true,
+            ])
+        @endif
+    @else
+        @include("drumeo.sales.partials._nav", [
+            "edgeVersion" => true,
+            "scrollToJoin" => true,
+            "homepage" => true
+        ])
+    @endif
+
+    <header class="pb-12 md:pt-20 bg-[#111729] text-center text-white">
+        <img class="md:hidden mb-16" src="https://drumeo-assets.s3.amazonaws.com/sales/2023/method/method-thumb.jpg" alt="method thumb" />
+        <div class="px-4 md:px-0">
+            <div class="mb-6">
+                <img class="h-6 md:h-10" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png" alt="drumeo logo" />
+                <img class="h-6 md:h-10" src="https://dpwjbsxqtam5n.cloudfront.net/sales/2021/method-text.svg" alt="method" style="filter: invert(1) brightness(.35) sepia(1) saturate(50) hue-rotate(195deg)" />
+            </div>
+            <h2 class="font-extrabold mb-4">Always know exactly what to practice.</h2>
+            <p class="md:mb-10">10 perfectly organized levels with video lessons from the top authorities on every topic.</p>
+        </div>
+        <img class="rounded-xl md:h-72 lg:h-80 hidden md:inline-block" src="https://drumeo-assets.s3.amazonaws.com/sales/2023/method/method-thumb.jpg" alt="method thumb" />
+    </header>
+
+    @php
+        $lessons = [
+            [
+                "title" => "Getting Started On The Drums",
+                "desc" => 'This is where it starts. You’ll learn how to set up your drum-set, hold your drumsticks properly, and play your first beats. And, best of all, you’ll learn how to play your first two songs on the drums.',
+                'detail' => '30 lessons',
+                'lessons' => [
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/241249-card-thumbnail-maxres-1593029951.png',
+                        'title' => 'Gear',
+                        'desc' => 'In this course, you\'ll learn about all the different parts of your drum-set. You\'ll learn the name of each piece of drum equipment, the function and purpose of each piece, and how to set everything up properly and efficiently. ',
+                        'lessonNum' => '10',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/241260-card-thumbnail-maxres-1593030096.png',
+                        'title' => 'Technique',
+                        'desc' => 'Here you\'ll learn the most basic drum techniques to get you playing as soon as possible. You\'ll learn how to hold your drumsticks properly and how to play your hi-hat and bass drum pedals.',
+                        'lessonNum' => '5',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/241266-card-thumbnail-maxres-1593030157.png',
+                        'title' => 'Your First Song',
+                        'desc' => 'In this course, you\'ll learn how to play along to your first song! You\'ll learn how to play a drum beat, drum fill, and most importantly, how to play the drums in time.',
+                        'lessonNum' => '4',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/241271-card-thumbnail-maxres-1593030265.png',
+                        'title' => 'Your Second Song',
+                        'desc' => 'This course expands on what you learned in the previous one. Here you\'ll learn how to play along to your second song, orchestrate your drum beats and fills, and how to combine them together in different ways.',
+                        'lessonNum' => '6',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/241278-card-thumbnail-maxres-1593030445.png',
+                        'title' => 'Ears',
+                        'desc' => 'Ear training is an essential skill for any musician. In this course, you\'ll take your first steps into the world of ear training by focusing on active listening, call and response exercises, and developing your sense of time.',
+                        'lessonNum' => '5',
+                    ],
+                 ]
+            ],
+            [
+                "title" => "Basic Theory & Ear Training",
+                "desc" => 'In Level Two, you’ll continue to build your drumming foundation. You’re going to learn your first drum rudiments, develop basic reading skills (it’s not that hard, we promise!), and get introduced to three styles of drumming: rock, punk, and metal.',
+                'detail' => '51 lessons',
+                'lessons' => [
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243084-card-thumbnail-maxres-1593030792.png',
+                        'title' => 'Theory',
+                        'desc' => 'In this course, you\'ll learn about drum theory and notation, note values, and most importantly, how to read sheet music.',
+                        'lessonNum' => '12',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243085-card-thumbnail-maxres-1593031020.png',
+                        'title' => 'Practice',
+                        'desc' => 'Here you\'ll learn everything you need to know about efficient practice, stretching and warm-ups, and creating practice plans that will help you reach your drumming goals.',
+                        'lessonNum' => '5',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243086-card-thumbnail-maxres-1593031214.png',
+                        'title' => 'Rudiments',
+                        'desc' => 'This course will introduce you to your first three drum rudiments and teach you how to apply them to the drum-set.',
+                        'lessonNum' => '7',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243087-card-thumbnail-maxres-1593031335.png',
+                        'title' => 'Independence',
+                        'desc' => 'As a drummer, it\'s so important to develop independence with each of your limbs. In this course, you\'ll take your first steps into the world of drumming independence and learn how to isolate and challenge each limb.',
+                        'lessonNum' => '6',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243088-card-thumbnail-maxres-1593031467.png',
+                        'title' => 'Styles',
+                        'desc' => 'This course will introduce you to three new styles of music including rock, metal, and punk!',
+                        'lessonNum' => '12',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243089-card-thumbnail-maxres-1593031806.png',
+                        'title' => 'Ears',
+                        'desc' => 'Here you\'ll expand on what you learned in the "Ears" section of Level 1, by diving into the world of phrasing. You\'ll learn what phrasing actually is, how to hear it, and how to incorporate it into your practice.',
+                        'lessonNum' => '5',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/243090-card-thumbnail-maxres-1593031882.png',
+                        'title' => 'Music',
+                        'desc' => 'Apply the beats & fills you learned in the previous lessons to real music! ',
+                        'lessonNum' => '4',
+                    ],
+                 ]
+            ],
+            [
+                "title" => "The Motions Of Drumming	",
+                "desc" => 'Learning the motions of drumming will help you play faster, smoother, and for longer periods. This level introduces you to key drum set techniques like the three main stick grips and beginner bass drum pedal technique.',
+                'detail' => '59 lessons',
+                'lessons' => [
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246125-card-thumbnail-maxres-1593032540.png',
+                        'title' => 'Theory',
+                        'desc' => 'In this course, you\'ll expand on the theory concepts covered in Level 2. You\'ll learn about triplets, articulations, dynamics, and more!',
+                        'lessonNum' => '11',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246126-card-thumbnail-maxres-1593032720.png',
+                        'title' => 'Rudiments',
+                        'desc' => 'Here you\'ll be introduced to five new rudiments. You\'ll learn about the flam, drag, double and triplet paradiddles, and the five stroke roll.',
+                        'lessonNum' => '10',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246127-card-thumbnail-maxres-1593033143.png',
+                        'title' => 'Technique',
+                        'desc' => 'In this course, Bruce Becker will help you take your technique to the next level by introducing you to the motions of drumming. You\'ll also learn some other essential techniques that you\'ll be able to apply to all of your grooves and fills.',
+                        'lessonNum' => '11',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246128-card-thumbnail-maxres-1593034229.png',
+                        'title' => 'Independence',
+                        'desc' => 'This section will expand on the independence concepts you practiced in Level 2. You\'ll focus on developing independence with each individual limb and be introduced to some new concepts like unison dependence and dynamic independence.',
+                        'lessonNum' => '6',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246191-card-thumbnail-maxres-1593035171.png',
+                        'title' => 'Styles',
+                        'desc' => 'This course will focus on the train beat, modern country, country shuffles, and disco!',
+                        'lessonNum' => '12',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246192-card-thumbnail-maxres-1593036059.png',
+                        'title' => 'Ears',
+                        'desc' => 'Here you\'ll continue to develop your aural skills by learning how to play what you hear and create a roadmap for any song. You\'ll also expand on the timing and active listening exercises you\'ve worked on so far in The Drumeo Method.',
+                        'lessonNum' => '5',
+                    ],
+                    [
+                        'thumb' => 'https://d1923uyy6spedc.cloudfront.net/246333-card-thumbnail-maxres-1593037060.png',
+                        'title' => 'Music',
+                        'desc' => 'This course will provide you with four play-along tracks that you can use to apply everything you\'ve learned, in a musical context.',
+                        'lessonNum' => '4',
+                    ],
+                 ]
+            ],
+        ];
+    @endphp
+
+    <section class="py-12 md:py-20">
+        <div class="container mx-auto max-w-5xl px-6">
+            <h3 class="font-extrabold text-center mb-10 leading-snug">Your clear path, frustration-free <br>guide to playing the drums.</h3>
+            @foreach($lessons as $key => $lesson)
+                @include('drumeo.products.partials.question-dropdown-alt', [
+                    'num' => $key+1,
+                    "title" => $lesson['title'],
+                    "desc" => $lesson['desc'],
+                    'detail' => $lesson['detail'],
+                    'lessons' => $lesson['lessons']
+                ])
+            @endforeach
+        </div>
+    </section>
+
+{{--    @include('_partials.components.video-modal',[--}}
+{{--        'name' => 'trailer',--}}
+{{--        'video' => '772644658'--}}
+{{--    ])--}}
+
+    @include('musora.sales.order-section-collage', [
+        'header' => 'Unlimited drum lessons<br> The world’s best teachers<br> 5000+ popular songs',
+        'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Trusted by 30,000 students.</li>
+                    <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Online lessons on every topic.</li>
+                    <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Personalized feedback from real teachers.</li>
+                    <li class="leading-tight text-coaches"><i class="fa-li fas fa-check"></i> <strong>*BONUS*</strong> includes free access to Musora’s lessons for piano, guitar, and voice.</li>',
+        'buttonLink' => '/laravel/public/shopping-cart/api/query?products[DLM]=1,year,1&products[30-day-drummer]=1&products[practicepad]=1&products[Drumeo-VaterSticks]=2&products[BeginnerBook]=1&locked=true',
+        'price' => '20',
+        'image' => 'https://drumeo-assets.s3.amazonaws.com/promos/august/order_collage.png',
+    ])
+    @include('musora.sales.app-section', [
+        'appleLink' => 'https://itunes.apple.com/us/app/musora/id1619053766?ls=1',
+        'googleLink' => 'https://play.google.com/store/apps/details?id=com.musoraapp',
+        'image' => 'https://drumeo-assets.s3.amazonaws.com/sales/2023/devices.png',
+    ])
+
+    @php
+        $faqs = [
+            [
+            "title" => "What is Drumeo?",
+            "description" => 'Drumeo is an online platform that offers an organized drum curriculum, artist courses on popular topics, 5000+ songs transcribed note-for-note, and a supportive global community of students and teachers. ',
+            ],
+            [
+            "title" => "Is Drumeo good for beginners?",
+            "description" => 'Yes! You’ll always know what to practice with sequential step-by-step video lessons – plus have fun applying your new skills to your favorite songs, sorted by skill level. And if you ever need help, you’ll have unlimited personal support through live Q&A sessions, student reviews, and a helpful community. ',
+            ],
+            [
+            "title" => "Does Drumeo have anything for advanced drummers?",
+            "description" => 'Drumeo is the perfect companion for advanced drummers, giving you access to artist courses so you can gain insights and inspiration from the legends – with 200+ artist courses on a variety of topics. Plus, you’ll get note-for-note transcriptions for thousands of songs and practical playback tools, so you can take on any new challenge with confidence. ',
+            ],
+            [
+            "title" => "Am I too old to learn the drums?",
+            "description" => 'You’re never too old to learn the drums. Drumeo has a community of students of all ages, from all around the world. Whether you’re 40, 50, 60, 70, or beyond – you’ll connect with drummers just like you who are learning and applying their skills to music. ',
+            ],
+            [
+            "title" => "Do I need to be tech-savvy to learn through your app?",
+            "description" => 'Not at all! Technology is here to make your life easier, and Drumeo is designed to help you find lessons and songs easily. And if you ever get stuck, you can contact our Student Experience team by phone or email for prompt and helpful support. ',
+            ],
+        ]
+    @endphp
+    @include('musora.sales.faq-section')
+
+    @include("drumeo.sales.partials._footer")
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js"></script>
+    @yield('scripts')
+@stop
