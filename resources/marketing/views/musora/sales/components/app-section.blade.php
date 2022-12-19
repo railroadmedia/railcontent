3d8f481d1f840ec3f6c5252041080e9e9073610b
peterminhlee <petereminhlee@gmail.com>
2022-12-19 15:53:57 -0800

    added styles

--- resources/marketing/views/musora/sales/components/app-section.blade.php
+++ resources/marketing/views/musora/sales/components/app-section.blade.php
@@ -1,17 +1,21 @@
 <div class="h-5 sm:h-10 -mt-5 sm:-mt-10 relative z-10" style="background: linear-gradient(to bottom right, transparent calc(50% - 1px), transparent, #f4f8fb calc(50% + 1px));"></div>
-<section class="relative text-center px-6" style="background-color:#f6f8fc;">
-    <div class="container mx-auto max-w-6xl relative z-20">
-        <div class="sm:text-left flex flex-wrap sm:flex-nowrap justify-center items-center">
-            <img class="lg:-mt-11 mb-3 sm:mb-0 lg:-mb-4 max-w-xs lg:max-w-2xl sm:order-1 transition-opacity opacity-0"
+
+<section class="relative text-center px-6 pb-10 md:pb-0" style="background-color:#f6f8fc;">
+    <div class="container mx-auto max-w-5xl relative z-20">
+        <div class="text-center md:text-left md:flex justify-center items-center">
+            <img
+                class="lg:-mt-11 lg:-mb-4 max-w-md lg:max-w-2xl transition-opacity opacity-0 order-1 mb-6 md:mb-0 w-full"
                 loading="lazy"
                 onload="this.classList.remove('opacity-0')"
-                src="https://cdn.musora.com/image/fetch/w_1300,q_auto:best/{!! $image !!}">
-            <div class="sm:pr-4 lg:pr-7">
-                <h4 class="leading-normal sm:mb-3 lg:mb-7"><strong>Available across web,<br> tablet, & mobile.</strong></h4>
+                src="https://cdn.musora.com/image/fetch/w_1300,q_auto:best/{!! $image !!}"
+                alt="device image"
+            >
+            <div class="pr-4 lg:pr-7">
+                <h4 class="leading-normal mb-3 lg:mb-7"><strong>Available across web,<br> tablet, & mobile.</strong></h4>
                 <a class="inline-block" href="{!! $appleLink !!}" target="_blank">
-                    <img class="h-10 m-1" src="https://cdn.musora.com/image/fetch/w_260,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
+                    <img class="h-8 md:h-10 m-1" src="https://cdn.musora.com/image/fetch/w_260,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
                 <a class="inline-block" href="{!! $googleLink !!}" target="_blank">
-                    <img class="h-10 m-1" src="https://cdn.musora.com/image/fetch/w_260,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
+                    <img class="h-8 md:h-10 m-1" src="https://cdn.musora.com/image/fetch/w_260,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
             </div>
         </div>
     </div>
