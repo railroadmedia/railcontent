a489ccae3badf3dce26508247f25451af3ce3c6b
peterminhlee <petereminhlee@gmail.com>
2022-12-21 12:08:07 -0800

    extend updates

--- resources/marketing/views/_partials/layout/coaches-method-songs-layout.blade.php
+++ resources/marketing/views/_partials/layout/coaches-method-songs-layout.blade.php
@@ -1,9 +1,7 @@
-@extends('drumeo._partials.layout-template')
+@extends($theme.'._partials.global-layout')
 
 @section('global-head')
     @yield('page-meta')
-
-    @include('drumeo._partials._fonts')
 
     <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
     <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
