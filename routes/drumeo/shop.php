215e39105739debd44369c477b30d62344a44265
peterminhlee <petereminhlee@gmail.com>
2022-12-21 08:02:06 -0800

    moved drumshop pages

--- routes/drumeo/shop.php
+++ routes/drumeo/shop.php
@@ -13,6 +13,15 @@ Route::domain('{drumeoDomain}')
         function () {
             Route::get('/drumming-system', function () { return view('drumeo.drumshop.pages.drumming-system', ['theme' => 'drumeo']); });
             Route::get('/gift-card', function () { return view('drumeo.drumshop.pages.gift-card', ['theme' => 'drumeo']); });
+            Route::get('/{page?}', SalesController::class . '@products')
+                ->whereIn('page', [
+                    'beginner-book', 'better-drum-fills', 'beyond-beginner-drumming', 'comfort-cover', 'drum-technique-made-easy', 'drumming-system-discount', 'drumsticks', 'electrify-your-drumming', 'festival-videos', 'independence-made-easy', 'learn-songs-faster', 'new-drummers', 'practice-pad-full', 'quietpad', 'rock-drumming-masterclass', 'successful-drumming-discount', 'the-drummers-toolbox', 'tony-royster-jr'
+                ]);
+
+            Route::get('/30-day-drummer', [SalesController::class, 'thirtyDayDrummer'] );
+            Route::get('/eardrums', [SalesController::class, 'eardrums'] );
+            Route::get('/quietkick', [SalesController::class, 'quietKick'] );
+            Route::get('/tone-control-kit', [SalesController::class, 'toneControl'] );
         }
     );
     Route::get('/drumshop/{productslug}', [ShopController::class, 'product']);
