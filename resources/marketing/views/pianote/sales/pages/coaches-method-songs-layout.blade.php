087479005bbc14f0226bbb506da63ae69a605052
peterminhlee <petereminhlee@gmail.com>
2022-12-22 10:36:58 -0800

    revisions

--- resources/marketing/views/pianote/sales/pages/coaches-method-songs-layout.blade.php
+++ resources/marketing/views/pianote/sales/pages/coaches-method-songs-layout.blade.php
@@ -1,25 +1,49 @@
 @extends('_partials.layout.coaches-method-songs-layout')
 
+@section('page-styles')
+    <link rel="stylesheet" href="{{ asset('/marketing/parcel/pianote/nav-footer.css') }}">
+@endsection
+
 @section('page-nav')
-    @include('pianote._partials._nav')
+    @if(!empty($trialVersion))
+        @if(!empty($joinUrl))
+            @include("pianote._partials._nav", [
+                "edgeVersion" => true,
+                "trialVersion" => true,
+                "joinUrl" => $joinUrl
+            ])
+        @else
+            @include("pianote._partials._nav", [
+                "edgeVersion" => true,
+                "trialVersion" => true,
+                "scrollToJoin" => true,
+            ])
+        @endif
+    @else
+        @include("pianote._partials._nav", [
+            "edgeVersion" => true,
+            "scrollToJoin" => true,
+            "homepage" => true
+        ])
+    @endif
 @endsection
 
 @section('page-footer')
     @include('musora.sales.components.order-section-collage', [
-        'header' => '',
-        'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Trusted by 30,000 students.</li>
-                    <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Online lessons on every topic.</li>
+        'header' => 'Unlimited piano lessons.<br> Direct access to real teachers.<br> Personalized feedback from real teachers.',
+        'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Trusted by 30,000 happy students.</li>
+                    <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Online piano lessons on every topic.</li>
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Personalized feedback from real teachers.</li>
-                    <li class="leading-tight text-coaches"><i class="fa-li fas fa-check"></i> <strong>*BONUS*</strong> includes free access to Musora’s lessons for piano, guitar, and voice.</li>',
-        'buttonLink' => '/laravel/public/shopping-cart/api/query?products[DLM]=1,year,1&products[30-day-drummer]=1&products[practicepad]=1&products[Drumeo-VaterSticks]=2&products[BeginnerBook]=1&locked=true',
-        'price' => '20',
-        'image' => 'https://drumeo-assets.s3.amazonaws.com/promos/august/order_collage.png',
+                    <li class="leading-tight text-coaches"><i class="fa-li fas fa-check"></i> <strong>*BONUS*</strong> voice, guitar, and drums lessons with full access to all Musora communities.</li>',
+        'buttonLink' => '',
+        'price' => '',
+        'image' => 'https://pianote.s3.amazonaws.com/sales/2023/pianote-spread.png',
     ])
 
     @include('musora.sales.components.app-section', [
         'appleLink' => 'https://itunes.apple.com/us/app/musora/id1619053766?ls=1',
         'googleLink' => 'https://play.google.com/store/apps/details?id=com.musoraapp',
-        'image' => 'https://drumeo-assets.s3.amazonaws.com/sales/2023/devices.png',
+        'image' => 'https://pianote.s3.amazonaws.com/sales/2023/devices.png',
     ])
 
     @php
