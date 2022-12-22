083e4f8634dd1d468baf4e05198f9f51bc33a3aa
peterminhlee <petereminhlee@gmail.com>
2022-12-22 10:36:45 -0800

    faq component

---
+++ resources/marketing/views/drumeo/_partials/faq.blade.php
@@ -0,0 +1,37 @@
+@php
+    $faqs = [
+        [
+        "title" => "What is Drumeo?",
+        "desc" => 'Drumeo is an online platform that offers an organized drum curriculum, artist courses on popular topics, 5000+ songs transcribed note-for-note, and a supportive global community of students and teachers. ',
+        ],
+        [
+        "title" => "Is Drumeo good for beginners?",
+        "desc" => 'Yes! You’ll always know what to practice with sequential step-by-step video lessons – plus have fun applying your new skills to your favorite songs, sorted by skill level. And if you ever need help, you’ll have unlimited personal support through live Q&A sessions, student reviews, and a helpful community. ',
+        ],
+        [
+        "title" => "Does Drumeo have anything for advanced drummers?",
+        "desc" => 'Drumeo is the perfect companion for advanced drummers, giving you access to artist courses so you can gain insights and inspiration from the legends – with 200+ artist courses on a variety of topics. Plus, you’ll get note-for-note transcriptions for thousands of songs and practical playback tools, so you can take on any new challenge with confidence. ',
+        ],
+        [
+        "title" => "Am I too old to learn the drums?",
+        "desc" => 'You’re never too old to learn the drums. Drumeo has a community of students of all ages, from all around the world. Whether you’re 40, 50, 60, 70, or beyond – you’ll connect with drummers just like you who are learning and applying their skills to music. ',
+        ],
+        [
+        "title" => "Do I need to be tech-savvy to learn through your app?",
+        "desc" => 'Not at all! Technology is here to make your life easier, and Drumeo is designed to help you find lessons and songs easily. And if you ever get stuck, you can contact our Student Experience team by phone or email for prompt and helpful support. ',
+        ],
+    ]
+@endphp
+
+<section class="py-12 md:py-20">
+    <div class="container mx-auto max-w-5xl px-6">
+        <h2 class="font-extrabold mb-10 text-center">Frequently Asked Questions</h2>
+        @foreach($faqs as $faq)
+            @include('_partials.components.question-dropdown', [
+                'num' => '?',
+                "title" => $faq['title'],
+                "desc" => $faq['desc'],
+            ])
+        @endforeach
+    </div>
+</section>
