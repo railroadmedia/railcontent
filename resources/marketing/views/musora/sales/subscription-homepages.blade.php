59319305735dd12c34081cc4553ccbc50a5ac039
Trent <trent.harms@gmail.com>
2022-12-22 14:09:40 -0800

    changing wording

--- resources/marketing/views/musora/sales/subscription-homepages.blade.php
+++ resources/marketing/views/musora/sales/subscription-homepages.blade.php
@@ -617,7 +617,7 @@
         'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Trusted by ' . number_format(31856) . ' students.</li>
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Online lessons on every topic.</li>
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-drumeo"></i> Personalized feedback from real teachers.</li>
-                    <li class="leading-tight text-coaches max-w-xs mx-0"><i class="fa-li fas fa-check"></i> <strong>*BONUS*</strong> includes free access to Musora’s lessons for piano, guitar, and voice.</li>',
+                    <li class="leading-tight text-coaches max-w-xs mx-0"><i class="fa-li fas fa-check"></i> <strong>PLUS</strong> piano, guitar, and voice lessons with full access to all Musora communities.</li>',
         'buttonLink' => '/laravel/public/shopping-cart/api/query?products[DLM]=1,year,1&products[30-day-drummer]=1&products[practicepad]=1&products[Drumeo-VaterSticks]=2&products[BeginnerBook]=1&locked=true',
         'price' => '20',
         'image' => 'https://drumeo-assets.s3.amazonaws.com/sales/2023/drumeo-spread.png',
