7d347a9f31915ca0121bf43aa5dc399360ee4a84
peterminhlee <petereminhlee@gmail.com>
2022-12-22 17:33:41 -0800

    removed flipped code

--- resources/marketing/views/musora/sales/subscription-homepages.blade.php
+++ resources/marketing/views/musora/sales/subscription-homepages.blade.php
@@ -93,16 +93,6 @@
             margin-left: -5px;
             margin-bottom: -5px;
         }
-
-        .flip-div .back {
-            transform: rotateY(-180deg);
-        }
-        .flip-div.flipped .back {
-            transform: rotateY(0deg);
-        }
-        .flip-div.flipped .front {
-            transform: rotateY(180deg);
-        }
     </style>
 @stop
 
@@ -633,14 +623,6 @@
     @endif
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
-    <script>
-        $(document).ready(function () {
-            $('.flip-div').click(function (e) {
-                $(this).toggleClass('flipped');
-            });
-        });
-    </script>
-
     <script type="text/javascript" src="{{ asset('/marketing/js/sliding-anchor.js') }}"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/unveilhooks/ls.unveilhooks.min.js"></script>
