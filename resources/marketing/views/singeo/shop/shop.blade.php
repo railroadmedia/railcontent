e25340708f35536a08a6f0ab5b82726fe86a6b5e
Trent <trent.harms@gmail.com>
2022-12-23 14:51:01 -0800

    unhiding countdowns

--- resources/marketing/views/singeo/shop/shop.blade.php
+++ resources/marketing/views/singeo/shop/shop.blade.php
@@ -186,28 +186,28 @@
                 {{-- <img class="logo" src="https://cdn.musora.com/image/fetch/w_200,q_auto:best/https://singeo.s3.amazonaws.com/sales/2021/singeo-logo.png" alt="singeo logo"> --}}
                 <h1><strong>SINGEO SHOP</strong></h1>
                 <p>Save up to <span class="">74%</span> on lessons, accessories, and merch.</p>
-                {{--                <div class="rounded-xl px-6 py-2 inline-flex flex-wrap mx-auto mt-5 justify-center items-center" style="background-color:#181515;">--}}
-                {{--                    <p class="leading-none m-0"><strong>DEALS END IN:</strong></p>--}}
-                {{--                    <div class="h-12 mx-5 bg-promo" style="width:2px;"></div>--}}
-                {{--                    <div class="tzcd-big">--}}
-                {{--                        <div class="inline-block">--}}
-                {{--                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>--}}
-                {{--                            <p class="leading-none uppercase font-extrabold text-xs text-promo">days</p>--}}
-                {{--                        </div>--}}
-                {{--                        <div class="inline-block mx-2">--}}
-                {{--                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>--}}
-                {{--                            <p class="leading-none uppercase font-extrabold text-xs text-promo">hrs</p>--}}
-                {{--                        </div>--}}
-                {{--                        <div class="inline-block mr-2">--}}
-                {{--                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>--}}
-                {{--                            <p class="leading-none uppercase font-extrabold text-xs text-promo">mins</p>--}}
-                {{--                        </div>--}}
-                {{--                        <div class="inline-block">--}}
-                {{--                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>--}}
-                {{--                            <p class="leading-none uppercase font-extrabold text-xs text-promo">secs</p>--}}
-                {{--                        </div>--}}
-                {{--                    </div>--}}
-                {{--                </div>--}}
+                                <div class="rounded-xl px-6 py-2 inline-flex flex-wrap mx-auto mt-5 justify-center items-center" style="background-color:#181515;">
+                                    <p class="leading-none m-0"><strong>DEALS END IN:</strong></p>
+                                    <div class="h-12 mx-5 bg-promo" style="width:2px;"></div>
+                                    <div class="tzcd-big">
+                                        <div class="inline-block">
+                                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>
+                                            <p class="leading-none uppercase font-extrabold text-xs text-promo">days</p>
+                                        </div>
+                                        <div class="inline-block mx-2">
+                                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>
+                                            <p class="leading-none uppercase font-extrabold text-xs text-promo">hrs</p>
+                                        </div>
+                                        <div class="inline-block mr-2">
+                                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>
+                                            <p class="leading-none uppercase font-extrabold text-xs text-promo">mins</p>
+                                        </div>
+                                        <div class="inline-block">
+                                            <h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">00</h2>
+                                            <p class="leading-none uppercase font-extrabold text-xs text-promo">secs</p>
+                                        </div>
+                                    </div>
+                                </div>
             </div>
         </div>
     </header>
