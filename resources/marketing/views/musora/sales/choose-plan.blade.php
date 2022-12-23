cff6ac41a893075c67f948530288e8764d616d09
Trent <trent.harms@gmail.com>
2022-12-22 20:20:36 -0800

    pricing page

--- resources/marketing/views/musora/sales/choose-plan.blade.php
+++ resources/marketing/views/musora/sales/choose-plan.blade.php
@@ -1,22 +1,19 @@
 @extends('drumeo._partials.global-layout')
 
 @section('global-head')
-    <title>Drumeo Trial</title>
-    <meta property="og:title" content="Drumeo Trial">
-    <meta property="og:url" content="https://www.drumeo.com/trial/">
-    <meta name="description" content="Learn the drums faster with organized video lessons, legendary teachers, and better practice tools. 90-Day Guarantee. ">
-    <meta property="og:image" content="https://dpwjbsxqtam5n.cloudfront.net/sales/2022/og-image.jpg" style="display: none;">
-    <meta property="og:description" content="Reach your drumming goals with organized video lessons, legendary teachers, and better practice tools. 90-Day Guarantee.">
-    @if(empty($weekly))
-        <meta name="robots" content="noindex">
-    @endif
+    <title>Drumeo | Reach your drumming goals.</title>
+    <meta property="og:title" content="Drumeo | Reach your drumming goals.">
+    <meta name="description" content="Learn the drums faster with organized video lessons, legendary teachers, and better practice tools. 90-Day Guarantee.">
+    <meta property="og:description" content="Learn the drums faster with organized video lessons, legendary teachers, and better practice tools. 90-Day Guarantee.">
+    <meta property="og:image" content="https://drumeo-assets.s3.amazonaws.com/sales/2023/share-image-drumeo.jpg" style="display: none;">
+    <meta property="og:url" content="https://www.drumeo.com/">
 
     @include('drumeo._partials._fonts')
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" />
+    <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
+    <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
     <link href="{{ asset('/marketing/css/drumeo/tailwind-helpers.css') }}" rel="stylesheet">
     <link href="{{ asset('/marketing/parcel/drumeo/sales-2020.css') }}" rel="stylesheet">
-    <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
 @stop
 
 @section('global-body')
@@ -24,132 +21,86 @@
             "edgeVersion" => true
         ])
 
-    <style>
-        .content-section.customize .logo {
-            max-width: 200px;
-        }
-        @media (min-width: 40em) {
-            .content-section.customize .logo {
-                max-width: 240px;
-            }
-        }
-    </style>
 
     <div id="customize-anchor" class="anchor anchor-slide"></div>
-    <section class="content-section grey text-center customize trial" style="background-color:#000318;">
-        <div class="container mx-auto max-w-6xl">
-            <img class="h-7 md:h-9 lg:h-14 mb-4" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png">
-            <br>
-            <h1><strong>Your first
-                    @if(!empty($weekly))
-                        week
-                    @else
-                        month
-                    @endif
-                    <br class="inline sm:hidden"> is free.</strong></h1>
-            <h5 class="text-light-navy mt-2 md:mt-4 mb-10 md:mb-20"><em>Choose the plan that will continue on <br class="inline lg:hidden">
-                    @if(!empty($weekly))
-                        {{ Carbon\Carbon::now()->addDays(7)->format('F jS') }}
-                    @else
-                        {{ Carbon\Carbon::now()->addDays(30)->format('F jS') }}
-                    @endif
+    <section class="text-center text-white px-5 sm:px-6 py-10 sm:py-14 lg:py-20" style="background-color:#0c1524;">
+        <div class="container max-w-6xl mx-auto">
+            <h1><strong>Your first week<br class="inline sm:hidden"> is free.</strong></h1>
+            <h5 class="mt-2 md:mt-4">Choose the plan that will continue on <br class="inline lg:hidden">
+                    {{ Carbon\Carbon::now()->addDays(7)->format('F jS') }} (after your free trial). Cancel anytime.</h5>
 
-                    (after your free trial). Cancel anytime.</em></h5>
+            <div class="flex items-end justify-center">
+                <div class="px-2">
+                    <p class="inline-block relative -bottom-1 mb-1 px-3 py-0.5 z-10 leading-tight text-black text-xs rounded-full" style="background-color:#00c9ac;">MOST POPULAR</p>
+                    <div class="flex items-center rounded-xl border-2 border-drumeo px-3 py-2" style="background-color:#273040;">
+                        <div class="text-left">
+                            <img class="h-5 sm:h-6" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png">
+                            <p class="opacity-60 text-sm"><em>Lessons + Songs</em></p>
+                        </div>
+                        <i class="fas fa-check-circle text-drumeo text-2xl ml-1 sm:ml-10"></i>
+                    </div>
+                </div>
+                <div class="px-2">
+                    <div class="flex items-center rounded-xl px-3 py-2" style="background-color:#273040;">
+                        <div class="text-left">
+                            <img class="h-5 sm:h-6" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png">
+                            <p class="opacity-60 text-sm"><em>Lessons only.</em></p>
+                        </div>
+                        <i class="far fa-circle text-2xl ml-1 sm:ml-10"></i>
+                    </div>
+                </div>
 
+            </div>
 
-            <div class="inline-block w-full px-2 md:px-0 max-w-xs md:max-w-3xl">
-                <div class="float-left px-2 w-full md:w-1/2">
-                    <a class="border-2 overflow-hidden rounded-2xl block mx-auto mb-10 md:mb-0 px-4 md:px-6 py-7 md:py-10"
-                            @hasSection('month-url')
-                                @yield('month-url')
-                            @else
-                                href="/laravel/public/shopping-cart/api/query?products[DLM-Trial-30-Day]=1&promo-code=melodics-special-offer&locked=true"
-                            @endif
-                    >
-                        <h2><strong>MONTHLY</strong></h2>
-                        <h1 class="my-2 md:my-4"><strong>$29</strong><sub class="bottom-0 text-xs -mr-10" st>/month</sub></h1>
-                        <p class="mb-3 text-light-navy"><em>If you're just giving it a test-drive.</em></p>
-                        <ul class="fa-ul text-left my-4 md:my-6">
-                            <li><i class="fa-li fal fa-check"></i> DrumeoMETHOD</li>
-                            <li><i class="fa-li fal fa-check"></i> DrumeoSONGS</li>
-                            <li><i class="fa-li fal fa-check"></i> DrumeoCOACHES</li>
-                            <li><i class="fa-li fal fa-check"></i> 90-day money back guarantee.</li>
-                            <li><i class="fa-li fal fa-check"></i> Unlimited personal support.</li>
-                            <li><i class="fa-li fal fa-check"></i> Billed Monthly</li>
-                        </ul>
-                        <div class="join smaller outline white">Start My Free Trial &raquo;</div>
+            <div class="flex flex-wrap items-end justify-center 2-full max-w-sm md:max-w-2xl lg:max-w-3xl my-5 sm:my-7 mx-auto">
+                <div class="w-full md:w-1/2 px-2 md:px-3 relative">
+                    <p class="inline-block relative -bottom-1 mb-1 px-5 py-1 z-10 leading-tight text-black text-sm rounded-full" style="background-color:#ffac00;">SAVE 33%</p>
+                    <a href="todo" class="text-black overflow-hidden rounded-2xl block mx-auto mb-4 md:mb-0 group">
+                        <div class="bg-white px-3 pt-6 md:pt-8 pb-5 md:pb-8">
+                            <h2 class="leading-none mb-3"><strong>Annual</strong></h2>
+                            <h4 class="inline-block leading-none"><strong>${{ number_format(Prices::$drumeoEdgeAnnual / 12) }}/month</strong></h4>
+                            <p class="text-sm mb-4"><em>Billed at ${{Prices::$drumeoEdgeAnnual}} per year.</em></p>
+                            <div class="join blue smaller w-full transition-opacity duration-300 group-hover:opacity-80" style="max-width: 230px;">Free for 7 days</div>
+                        </div>
+                        <div class="px-3 pt-5 md:pt-6 pb-9 md:pb-10" style="background: #f1f8ff;border-top: 2px solid #e6f2ff;">
+                            <p class="mb-1"><strong>The world’s best drum lessons.</strong></p>
+                            <p class="mb-1"><strong>5000+ popular songs.</strong></p>
+                            <p class="mb-1"><strong>Unlimited personal support</strong></p>
+                            <p class="mb-1">Join a community of {{ number_format(31856) }} drum students.</p>
+                            <p class="mb-1">Lesson access for piano, guitar, and singing.</p>
+                            <p>90-day money back guarantee.</p>
+                        </div>
                     </a>
                 </div>
-                <div class="float-left px-2 w-full md:w-1/2 relative">
-                    <p class="bg-{{ $theme }} rounded-t-2xl absolute left-1/2 inline-block w-4/5 transform -translate-x-1/2" style="top: -23px;">
-                        @hasSection('badge-text')
-                            @yield('badge-text')
-                        @else
-                            Most Popular
-                        @endif
-                    </p>
-                    <a class="border-2 overflow-hidden rounded-2xl block mx-auto mb-10 md:mb-0 px-4 md:px-6 py-7 md:py-10 border-{{ $theme }} text-{{ $theme }}"
-                            @hasSection('annual-url')
-                                @yield('annual-url')
-                            @else
-                                href="/laravel/public/shopping-cart/api/query?products[DLM-Trial-Annual-30-Day]=1&locked=true"
-                            @endif
-                    >
-                        <h2><strong>ANNUAL</strong></h2>
-                        @hasSection('extra-savings')
-                            <h1 class="my-2 md:my-4"><strong>@yield('extra-savings-divided')</strong><sub class="bottom-0 text-xs -mr-10" st>/month</sub></h1>
-                            <p class="mb-3 text-coaches"><em>Billed as @yield('extra-savings') per year. </em></p>
-                        @else
-                            <h1 class="my-2 md:my-4"><strong>$20</strong><sub class="bottom-0 text-xs -mr-10" st>/month</sub></h1>
-                            <p class="mb-3 text-coaches"><em>Billed as $240 per year. </em></p>
-                        @endif
-                        <ul class="fa-ul text-left my-4 md:my-6">
-                            <li><i class="fa-li fal fa-check"></i> DrumeoMETHOD</li>
-                            <li><i class="fa-li fal fa-check"></i> DrumeoSONGS</li>
-                            <li><i class="fa-li fal fa-check"></i> DrumeoCOACHES</li>
-                            <li><i class="fa-li fal fa-check"></i> 90-day money back guarantee.</li>
-                            <li><i class="fa-li fal fa-check"></i> Unlimited personal support.</li>
-                            <li><i class="fa-li fal fa-check"></i> Billed Annually</li>
-                        </ul>
-                        <div class="join smaller blue">Start My Free Trial &raquo;</div>
-                        <p class="text-light-navy mt-1 -mb-7"><em>Save
-                                @hasSection('extra-savings-amount')
-                                    @yield('extra-savings-amount')
-                                @else
-                                    $108
-                                @endif
-                                per year vs. monthly.</em></p>
+                <div class="w-full md:w-1/2 px-2 md:px-3 relative">
+                    <a href="todo" class="text-black overflow-hidden rounded-2xl block mx-auto mb-4 md:mb-0 group">
+                        <div class="bg-white px-3 pt-6 md:pt-8 pb-5 md:pb-8">
+                            <h2 class="leading-none mb-3"><strong>Monthly</strong></h2>
+                            <h4 class="inline-block leading-none"><strong>${{ Prices::$drumeoEdgeRegular }}/month</strong></h4>
+                            <p class="text-sm mb-4"><em>Pay as you go.</em></p>
+                            <div class="join blue smaller w-full transition-opacity duration-300 group-hover:opacity-80" style="max-width: 230px;">Free for 7 days</div>
+                        </div>
+                        <div class="px-3 pt-5 md:pt-6 pb-9 md:pb-10" style="background: #f1f8ff;border-top: 2px solid #e6f2ff;">
+                            <p class="mb-1"><strong>The world’s best drum lessons.</strong></p>
+                            <p class="mb-1"><strong>5000+ popular songs.</strong></p>
+                            <p class="mb-1"><strong>Unlimited personal support</strong></p>
+                            <p class="mb-1">Join a community of {{ number_format(31856) }} drum students.</p>
+                            <p class="mb-1">Lesson access for piano, guitar, and singing.</p>
+                            <p>90-day money back guarantee.</p>
+                        </div>
                     </a>
                 </div>
             </div>
-
-            <p class="text-coaches md:mt-5 mb-2 uppercase"><strong>You will not be billed until <br class="inline md:hidden">
-                    @if(!empty($weekly))
-                        {{ Carbon\Carbon::now()->addDays(7)->format('F jS') }}
-                    @else
-                        {{ Carbon\Carbon::now()->addDays(30)->format('F jS') }}
-                    @endif
-                    (After your free trial.)</strong></p>
-
-
-            <div class="inline-block w-full px-3 md:px-4 my-5 text-light-navy">
-                <p><strong>Any questions?</strong><br class="inline-block md:hidden"> Call us toll-free at
-                    <a href="tel:+18004398921">1-800-439-8921</a> <br class="inline-block md:hidden"> or directly at
-                    <a href="tel:+16048557605">1-604-855-7605</a>.<br> All prices listed in USD. </p>
-            </div>
-            <div class="inline-block w-full px-3 md:px-4 text-light-navy" style="margin-top: 0;">
-                <i class="mx-0.5 text-3xl md:text-4xl fab fa-cc-visa"></i>
-                <i class="mx-0.5 text-3xl md:text-4xl fab fa-cc-mastercard"></i>
-                <i class="mx-0.5 text-3xl md:text-4xl fab fa-cc-amex"></i>
-                <i class="mx-0.5 text-3xl md:text-4xl fab fa-cc-paypal"></i>
-                <i class="mx-0.5 text-3xl md:text-4xl fab fa-cc-discover"></i>
-            </div>
+            <p><em>All prices listed in USD.</em></p>
         </div>
     </section>
 
+    @include('musora.sales.components.plans-different-section')
+    @include('drumeo._partials.faq')
 
     @include("drumeo.sales.partials._footer")
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 @stop
---
+++ resources/marketing/views/musora/sales/components/plans-different-section.blade.php
@@ -0,0 +1,72 @@
+<section class="text-center px-5 sm:px-6 py-10 sm:py-14 lg:py-20">
+    <div class="container max-w-5xl mx-auto">
+        <h2 class="text-center mb-4 sm:mb-10"><strong>How are the plans different?</strong></h2>
+        <div class="flex items-center rounded-xl py-3 px-3 sm:px-6">
+            <div class="w-1/2 text-left"></div>
+            <div class="w-1/4">
+                <img class="h-3 sm:h-6 lg:h-8" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png">
+            </div>
+            <div class="w-1/4">
+                <img class="h-3 sm:h-6 lg:h-8" src="https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png">
+            </div>
+        </div>
+        <div class="flex items-center rounded-xl py-4 px-3 sm:px-6" style="background-color:#f5f8fc;">
+            <div class="w-1/2 text-left">
+                <h5 class="mb-1 sm:mb-2 leading-tight"><strong>Method</strong></h5>
+                <p class="leading-tight text-xs sm:text-sm">10-level step-by-step curriculum</p>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+        </div>
+        <div class="flex items-center rounded-xl py-4 px-3 sm:px-6">
+            <div class="w-1/2 text-left">
+                <h5 class="mb-1 sm:mb-2 leading-tight"><strong>Coaches</strong></h5>
+                <p class="leading-tight text-xs sm:text-sm">200+ courses with legendary teachers</p>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+        </div>
+        <div class="flex items-center rounded-xl py-4 px-3 sm:px-6" style="background-color:#f5f8fc;">
+            <div class="w-1/2 text-left">
+                <h5 class="mb-1 sm:mb-2 leading-tight"><strong>Instruments</strong></h5>
+                <p class="leading-tight text-xs sm:text-sm">Go beyond drums with lessons for piano, guitar, and singing.</p>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+        </div>
+        <div class="flex items-center rounded-xl py-4 px-3 sm:px-6">
+            <div class="w-1/2 text-left">
+                <h5 class="mb-1 sm:mb-2 leading-tight"><strong>Mentors</strong></h5>
+                <p class="leading-tight text-xs sm:text-sm">Student plans, weekly live Q&As, and unlimited personal support</p>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+        </div>
+        <div class="flex items-center rounded-xl py-4 px-3 sm:px-6" style="background-color:#f5f8fc;">
+            <div class="w-1/2 text-left">
+                <h5 class="mb-1 sm:mb-2 leading-tight"><strong>Songs</strong></h5>
+                <p class="leading-tight text-xs sm:text-sm">Thousands of songs transcribed w/ playback tools for all instruments.</p>
+            </div>
+            <div class="w-1/4">
+                <i class="fas fa-check text-2xl sm:text-3xl text-drumeo"></i>
+            </div>
+            <div class="w-1/4"></div>
+        </div>
+    </div>
+</section>
