589c74d9698dd7d8d5fe8a51b66e3a1cb4ab3d9c
Caleb Favor <calebfavor@gmail.com>
2022-12-28 12:36:37 -0800

    Now always 404ing if somone tries to access a non-existing content catalog page.

--- app/Http/Controllers/Platform/ContentPagesController.php
+++ app/Http/Controllers/Platform/ContentPagesController.php
@@ -103,7 +103,7 @@ class ContentPagesController extends BaseController
                 [ContentService::STATUS_PUBLISHED, ContentService::STATUS_SCHEDULED, ContentService::STATUS_DRAFT];
         }
 
-        if (empty($lessonType)) {
+        if (empty($lessonType) || empty($catalogueMeta)) {
             throw new NotFoundHttpException();
         }
 
@@ -1548,7 +1548,7 @@ class ContentPagesController extends BaseController
         if (!user()->isAdmin()) {
             throw new NotFoundHttpException();
         }
-        
+
         return view('content.comments');
     }
 }
