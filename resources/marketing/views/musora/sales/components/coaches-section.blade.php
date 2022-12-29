644c2e05721d5694026a94e58fa5fab9cacb6c24
Trent <trent.harms@gmail.com>
2022-12-28 18:32:45 -0800

    workin on pianote pages

--- resources/marketing/views/musora/sales/components/coaches-section.blade.php
+++ resources/marketing/views/musora/sales/components/coaches-section.blade.php
@@ -147,9 +147,9 @@
 
     <div class="text-center my-10 px-4 sm:px-0">
         @if(empty($promoVersion))
-            <a href="/courses" class="sm:mx-1 mb-2 sm:mb-0 w-full sm:w-64 join outline {{ $theme }}-blue smaller">EXPLORE 200+ COURSES <i class="fas fa-info-circle"></i> </a>
+            <a href="/courses" class="sm:mx-1 mb-2 sm:mb-0 w-full sm:w-64 join outline {{ $theme }} smaller">EXPLORE THE COURSES <i class="fas fa-info-circle"></i> </a>
         @endif
-        <a class="sm:mx-1 w-full sm:w-64 join {{ $theme }}-blue smaller @if(!empty($promoVersion)) anchor-slide @endif"
+        <a class="sm:mx-1 w-full sm:w-64 join {{ $theme }} smaller @if(!empty($promoVersion)) anchor-slide @endif"
             @if(!empty($promoVersion))
                 href="#customize-anchor"
             @else
--- resources/marketing/views/musora/sales/components/trailer-grid-section.blade.php
+++ resources/marketing/views/musora/sales/components/trailer-grid-section.blade.php
@@ -24,9 +24,9 @@
             @endforeach
         </div>
         @if(empty($promoVersion))
-            <a href="/method" class="sm:mx-1 mb-2 sm:mb-0 w-full sm:w-64 join outline {{ $theme }}-blue smaller">EXPLORE THE METHOD <i class="fas fa-info-circle"></i> </a>
+            <a href="/method" class="sm:mx-1 mb-2 sm:mb-0 w-full sm:w-64 join outline {{ $theme }} smaller">EXPLORE THE METHOD <i class="fas fa-info-circle"></i> </a>
         @endif
-        <a class="sm:mx-1 w-full sm:w-64 join {{ $theme }}-blue smaller @if(!empty($promoVersion)) anchor-slide @endif"
+        <a class="sm:mx-1 w-full sm:w-64 join {{ $theme }} smaller @if(!empty($promoVersion)) anchor-slide @endif"
             @if(!empty($promoVersion))
                 href="#customize-anchor"
             @else
