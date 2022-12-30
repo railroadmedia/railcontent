4b84bc4b69a423569c3f0fbc312f4865f1d02f6e
peterminhlee <petereminhlee@gmail.com>
2022-12-30 09:56:47 -0800

    optimisation

--- resources/marketing/views/_partials/components/question-dropdown.blade.php
+++ resources/marketing/views/_partials/components/question-dropdown.blade.php
@@ -1,39 +1,19 @@
 <div
     class="dropdown text-center rounded-xl mb-3 select-none text-black border-2 border-[#EFF3F5]"
-    x-data="{ open: @if(!empty($open) && $open) true @else false @endif}"
+    x-data="{ open: @if(!empty($open) && $open) true @else false @endif }"
 >
     <div class="flex">
         <div
             class="py-4 sm:py-6 pr-4 sm:pr-5 pl-8 sm:pl-12 text-left flex-grow relative cursor-pointer"
-            x-on:click="
-                open = !open;
-                if(open){
-                    $refs.dropdown1.classList.remove('hidden', 'h-0');
-                    $refs.dropdown1.classList.add('h-auto');
-                    @if(!empty($lessonInfo))
-                    $refs.dropdown2.classList.remove('hidden', 'h-0');
-                    $refs.dropdown2.classList.add('h-auto');
-                    @endif
-                    $refs.icon.classList.add('rotate-45');
-                } else {
-                    $refs.dropdown1.classList.add('h-0');
-                    $refs.dropdown1.classList.remove('h-auto');
-                    @if(!empty($lessonInfo))
-                    $refs.dropdown2.classList.add('h-0');
-                    $refs.dropdown2.classList.remove('h-auto');
-                    @endif
-                    $refs.icon.classList.remove('rotate-45');
-                }
-            "
+            x-on:click="open = !open;"
         >
             <h5 class="leading-tight sm:leading-loose font-black relative" :class="open && 'mb-2'">
                 @if(!empty($num))<span class="text-white rounded-full py-1 px-2 md:px-2.5 text-xs md:text-sm absolute -left-11 md:-left-16 -top-0.5 md:top-0.5" :class="open ? 'bg-{{$theme}}' : 'bg-[#838C98]'">{{$num}}</span>@endif
                 {!! $title !!}
             </h5>
             <p
-                x-ref="dropdown1"
-                class="transition-all duration-300 text-xs sm:text-sm @if(empty($open) || !$open) h-0 @endif leading-relaxed sm:leading-relaxed overflow-hidden"
-                :class="open ? '' : ''"
+                class="transition-all duration-300 text-xs sm:text-sm leading-relaxed sm:leading-relaxed overflow-hidden"
+                x-bind:class="{ 'hidden h-0': !open, 'h-auto': open  }"
             >
                 {!! nl2br( $desc) !!}
                 @if(!empty($detail))<br><span class="block mt-4 text-[#838C98]">{{$detail}}</span>@endif
@@ -41,48 +21,34 @@
         </div>
         <div
             class="ml-auto text-{{$theme}} pt-3 sm:pt-6 pr-4 sm:pr-5 cursor-pointer @if(!empty($customArrow)) {{ $customArrow }} @endif"
-            x-on:click="
-                open = !open;
-                if(open){
-                    $refs.dropdown1.classList.remove('hidden', 'h-0');
-                    $refs.dropdown1.classList.add('h-auto');
-                    @if(!empty($lessonInfo))
-                    $refs.dropdown2.classList.remove('hidden', 'h-0');
-                    $refs.dropdown2.classList.add('h-auto');
-                    @endif
-                    $refs.icon.classList.add('rotate-45');
-                } else {
-                    $refs.dropdown1.classList.add('h-0');
-                    $refs.dropdown1.classList.remove('h-auto');
-                    @if(!empty($lessonInfo))
-                    $refs.dropdown2.classList.add('h-0');
-                    $refs.dropdown2.classList.remove('h-auto');
-                    @endif
-                    $refs.icon.classList.remove('rotate-45');
-                }
-            "
+            x-on:click="open = !open"
         >
-            <i x-ref="icon" class="fas fa-plus transform transition-all duration-300 text-lg md:text-2xl lg:text-3xl"></i>
+            <i class="fas fa-plus transform transition-all duration-300 text-lg md:text-2xl lg:text-3xl" x-bind:class="{ 'rotate-45': open }"></i>
         </div>
     </div>
     @if(!empty($lessonInfo))
-        <div x-ref="dropdown2" class="transition-all duration-300 text-xs sm:text-sm text-left bg-[#F5F8FC] @if(empty($open) || !$open) h-0 @endif overflow-hidden" :class="open ? 'py-4 sm:py-6 pl-4 sm:pl-5 pr-8 sm:pr-12' : ''">
-            @foreach($lessonInfo as $key => $info)
-                <div class="bg-white p-4 rounded-xl flex flex-col md:flex-row mb-2 @if(empty($info['desc'])) md:items-center @endif">
-                    <img
-                        class="rounded-xl @if(empty($info['desc'])) md:h-24 @else md:h-32 @endif mb-6 md:mb-0 transition-opacity opacity-0"
-                        src="https://cdn.musora.com/image/fetch/w_650,q_auto:best/{{$info['thumb']}}"
-                        alt="lesson{{$key+1}} thumb"
-                        loading="lazy"
-                        onload="this.classList.remove('opacity-0')"
-                    />
-                    <div class="md:pl-6">
-                        <h6 class="font-black">{{$info['title']}}</h6>
-                        @if(!empty($info['desc']))<p class="my-2">{{$info['desc']}}</p>@endif
-                        @if(!empty($info['lessonNum']))<p class="text-[#838C98]">{{ $info['lessonNum'] }} lessons</p>@endif
+        <div
+            class="transition-all duration-300 text-xs sm:text-sm text-left bg-[#F5F8FC] overflow-hidden"
+            x-bind:class="{ 'hidden h-0': !open, 'h-auto': open }">
+            <div class="py-4 sm:py-6 pl-4 sm:pl-5 pr-8 sm:pr-12">
+                @foreach($lessonInfo as $key => $info)
+                    <div class="bg-white p-4 rounded-xl flex flex-col md:flex-row mb-2 @if(empty($info['desc'])) md:items-center @endif">
+                        <img
+                            class="rounded-xl @if(empty($info['desc'])) md:h-24 @else md:h-32 @endif mb-6 md:mb-0 transition-opacity opacity-0"
+                            src="https://cdn.musora.com/image/fetch/w_650,q_auto:best/{{$info['thumb']}}"
+                            alt="lesson{{$key+1}} thumb"
+                            loading="lazy"
+                            onload="this.classList.remove('opacity-0')"
+                        />
+                        <div class="md:pl-6">
+                            <h6 class="font-black">{{$info['title']}}</h6>
+                            @if(!empty($info['desc']))<p class="my-2">{{$info['desc']}}</p>@endif
+                            @if(!empty($info['lessonNum']))<p class="text-[#838C98]">{{ $info['lessonNum'] }} lessons</p>@endif
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     @endif
 </div>
+
