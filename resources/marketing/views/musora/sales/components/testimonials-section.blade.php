6fecf9c5f2c0b58e70204d8c89788460e37322a8
peterminhlee <petereminhlee@gmail.com>
2022-12-29 16:13:35 -0800

    breakpoints updates

--- resources/marketing/views/musora/sales/components/testimonials-section.blade.php
+++ resources/marketing/views/musora/sales/components/testimonials-section.blade.php
@@ -63,8 +63,8 @@
                         focus: 0,
                         interval: 2000,
                         breakpoints: {
-                            720: {
-                                perPage: 4.5,
+                            1000: {
+                                perPage: 3.5,
                             },
                             620: {
                                 perPage: 1.5,
