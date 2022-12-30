d54f242e2f3351da90998b6942b5e46b4bbf0ba2
peterminhlee <petereminhlee@gmail.com>
2022-12-30 08:33:28 -0800

    collage updates

--- resources/marketing/views/singeo/sales/pages/coaches-method-songs-layout.blade.php
+++ resources/marketing/views/singeo/sales/pages/coaches-method-songs-layout.blade.php
@@ -35,11 +35,11 @@
 
 @section('page-footer')
     @include('musora.sales.components.order-section-collage', [
-        'header' => 'Unlimited piano lessons.<br> Direct access to real teachers.<br> Personalized feedback from real teachers.',
-        'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-singeo"></i> Trusted by 30,000 happy students.</li>
+        'header' => 'Unlimited singing lessons.<br> Vocal coaches and support.<br>1000+ popular songs.',
+        'list' => '<li class="leading-tight mb-3"><i class="fa-li fas fa-check text-singeo"></i> Trusted by ' . number_format(Prices::$students) . ' students.</li>
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-singeo"></i> Online singing lessons on every topic.</li>
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-singeo"></i> Personalized feedback from vocal coaches.</li>
-                    <li class="leading-tight text-coaches"><i class="fa-li fas fa-check"></i> <strong>*BONUS*</strong> voice, guitar, and drum lessons with full access to all Musora communities.</li>',
+                    <li class="leading-tight text-coaches max-w-xs mx-0"><i class="fa-li fas fa-check"></i> <strong>PLUS</strong> piano, guitar, and drum lessons with full access to all Musora communities.</li>',
         'buttonLink' => '/choose-plan',
         'price' => '20',
         'image' => 'https://singeo.s3.amazonaws.com/sales/2023/singeo-spread.png',
