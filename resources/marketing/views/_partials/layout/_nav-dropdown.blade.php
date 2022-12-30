999a5fb801b57d3ce2a618e4191795e460fee7e2
Miguel Fondeur <miguelfondeur@gmail.com>
2022-12-30 17:52:30 +0000

    update the nav hover states

--- resources/marketing/views/_partials/layout/_nav-dropdown.blade.php
+++ resources/marketing/views/_partials/layout/_nav-dropdown.blade.php
@@ -6,7 +6,7 @@
      x-on:click.away="dropdown_{{ $id }} = false"
 >
     {{-- DROPDOWN TRIGGER --}}
-    <div class="flex py-2 mr-4 lg:mr-8 items-center text-white hover:text-{{ $brand }} tracking-widest text-base leading-none"
+    <div class="flex py-2 mr-4 lg:mr-8 items-center text-white hover:text-{{ $brand }} tracking-widest text-base leading-none transition cursor-pointer"
           dusk="parent-button-{{ strtolower(str_replace(' ', '-', $page)) }}"
           x-on:click.prevent="dropdown_{{ $id }} = !dropdown_{{ $id }}"      
     >
@@ -20,7 +20,7 @@
          x-bind:class="dropdown_{{ $id }} ? '' : 'hidden' "
     >
         @foreach($children as $page => $info)
-            <a href="{{ $info['url'] }}" class="flex items-center text-base tracking-widest hover:text-{{ $brand }} px-2 py-1 hover:bg-gray-100 w-full"
+            <a href="{{ $info['url'] }}" class="flex items-center text-base tracking-widest px-2 py-1 hover:bg-gray-100 w-full rounded-lg"
                 dusk="page-link-{{ strtolower(str_replace(' ', '-', $page)) }}">
                 @if(!empty($info['iconClass']))
                     <i class="no-events {{ $info['iconClass'] }} mr-1"></i>
@@ -30,3 +30,4 @@
         @endforeach
     </div>
 </div>
+
