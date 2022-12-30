79d2f8f4f0be0125461ead68071ab9ed52bf1423
peterminhlee <petereminhlee@gmail.com>
2022-12-30 12:27:43 -0800

    added z index on close button

--- resources/marketing/views/_partials/components/modal.blade.php
+++ resources/marketing/views/_partials/components/modal.blade.php
@@ -5,7 +5,6 @@
     x-on:keydown.escape.prevent.stop="{{ $name }} = false"
     role="dialog"
     aria-modal="true"
-    x-id="['modal-title']"
     :aria-labelledby="['modal-title']"
     class="fixed inset-0 overflow-y-auto"
 >
@@ -20,7 +19,7 @@
         style="z-index: 1006;"
     >
         <!-- Close button -->
-        <i class="fal fa-times fa-2x fixed top-16 right-2 text-white cursor-pointer text-5xl"></i>
+        <i class="fal fa-times fa-2x fixed top-16 right-2 text-white cursor-pointer text-5xl z-150"></i>
         <div
             x-on:click.stop
             x-trap.noscroll.inert="{{ $name }}"
