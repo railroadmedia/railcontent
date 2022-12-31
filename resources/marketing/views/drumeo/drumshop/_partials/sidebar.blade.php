c9058868869c5aa5d4cc90f0ec54f77bd561977f
Trent <trent.harms@gmail.com>
2022-12-30 20:00:21 -0800

    removing sidebars

--- resources/marketing/views/drumeo/drumshop/_partials/sidebar.blade.php
+++ resources/marketing/views/drumeo/drumshop/_partials/sidebar.blade.php
@@ -10,7 +10,7 @@
     <div class="lg:h-0">
     <div id="order" class="anchor"></div>
     <div class="side-slide overflow-hidden rounded border border-solid" style="border-color: #CCD3D3;">
-        @include('_partials.layout.holiday.shop-sidebar-banner')
+{{--        @include('_partials.layout.holiday.shop-sidebar-banner')--}}
 
         <div class="buy-section active px-5 pt-2 pb-6 text-center lg:py-6">
             @if(!empty($instructor))
