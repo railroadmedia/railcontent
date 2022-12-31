18900c6e47899129958e114a078680c44c7707eb
Trent <trent.harms@gmail.com>
2022-12-30 17:22:06 -0800

    fixing button links

--- resources/marketing/views/musora/sales/components/header-section.blade.php
+++ resources/marketing/views/musora/sales/components/header-section.blade.php
@@ -29,7 +29,7 @@
                             @if(!empty($promoVersion))
                                 href="#customize-anchor"
                             @else
-                                href="/choose-your-trial"
+                                href="/choose-plan"
                             @endif
                         >
                             @if(!empty($promoVersion))
