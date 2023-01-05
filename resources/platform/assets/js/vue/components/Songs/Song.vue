af2ef49e2685497a03bd1ff39ab4ad20fba9b4ce
calebfavor <calebfavor@gmail.com>
2023-01-05 21:35:27 +0000

    added songs update banner on account page

--- resources/platform/assets/js/vue/components/Songs/Song.vue
+++ resources/platform/assets/js/vue/components/Songs/Song.vue
@@ -11,9 +11,7 @@ import ContentLessonActionButtons from '../../vuesora/components/VideoResources/
 import SoundSlice from "../SoundSlice/SoundSlice.vue"
 import SoundSliceControls from "../SoundSlice/SoundSliceControls.vue";
 import ContentService from "../../vuesora/assets/js/services/content";
-import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.vue';
 import { ArrowSmLeftIcon } from '@heroicons/vue/solid';
-import SelectModal from '../Membership/SelectModal.vue';
 
 const props = defineProps({
     brand: {
@@ -97,10 +95,6 @@ const props = defineProps({
         type: Boolean,
     }
 });
-
-onMounted(() => {
-    console.log(props.hasInstrumentless)
-})
 
 const soundsliceObject = ref(props.assignments.length ? props.assignments[0] : {});
 const openSoundslice = ref(null);
@@ -189,7 +183,6 @@ const getBrandSpecificParams = () => {
 
 <template>
     <div class="tw-w-full tw-max-w-[1703px] tw-mx-auto tw-px-4 md:tw-px-8 mv-3">
-        <SelectModal />
         <div id="lessonInfo" class="tw-flex xl:tw-flex-row tw-flex-col align-v-top ">
             <div class="tw-flex tw-flex-col tw-pr-0 xl:tw-pr-8 tw-grow tw-w-full">
                 <a :href="backUrl ? backUrl : null"
