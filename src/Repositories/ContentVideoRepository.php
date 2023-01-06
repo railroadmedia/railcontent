1bd1ec482a488d6d0ad52adb66a08eace2e6666c
Roxana Riza <roxana_r2000@yahoo.com>
2023-01-06 18:04:41 +0200

    Get lessons based on the new column: external_video_id

--- src/Repositories/ContentVideoRepository.php
+++ src/Repositories/ContentVideoRepository.php
@@ -53,18 +53,11 @@ class ContentVideoRepository extends RepositoryBase
 
     public function getContentWithExternalVideoId($videoId)
     {
-        $results =  $this->query()
-            ->select('lesson.id as content_id', 'lesson.*')
-            ->join(
-                config('railcontent.table_prefix').'content as lesson',
-                'lesson.video',
-                '=',
-                config('railcontent.table_prefix').'content.id'
-            )
-            ->where(config('railcontent.table_prefix').'content.vimeo_video_id','=', $videoId)
-            ->orWhere(config('railcontent.table_prefix').'content.youtube_video_id','=', $videoId)
+        $lessonRows = $this->query()
+            ->select('id as content_id')
+            ->where('external_video_id', $videoId)
             ->get();
 
-        return $results;
+        return $lessonRows->toArray();
     }
 }
