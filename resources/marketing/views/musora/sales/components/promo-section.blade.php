4f7afdcbc28f84d67a5e1409d6dc247671ce82df
Trent <trent.harms@gmail.com>
2023-01-09 10:11:17 -0800

    changing sticky bar logic

--- resources/marketing/views/musora/sales/components/promo-section.blade.php
+++ resources/marketing/views/musora/sales/components/promo-section.blade.php
@@ -1,6 +1,7 @@
 @if(!empty($countdown))
+    <div class="block h-10 w-full"></div>
 <a href="#customize-anchor" style="background: #000;"
-    class="anchor-slide promo-banner block text-center w-full transition-opacity duration-300 overflow-hidden whitespace-nowrap bg-cover bg-center shadow-md py-1 mx-auto text-xs fixed">
+    class="anchor-slide promo-banner block text-center w-full transition-opacity duration-300 overflow-hidden whitespace-nowrap bg-cover bg-center shadow-md py-1 mx-auto text-xs -mt-10">
     <div class="container mx-auto relative">
         <div class="inline-block align-middle text-center">
             <p class="inline-block align-middle mx-auto font-bebas text-white text-sm leading-none sm:text-lg sm:leading-none text-left uppercase mr-2">
@@ -28,6 +29,7 @@
         </div>
     </div>
 </a>
+    <div class="sticky-trigger block"></div>
 @endif
 <div style="background:linear-gradient(30deg, #0a3761, #0c1526);">
 <section class="text-center text-white px-5 sm:px-6 py-10 sm:py-14 lg:py-20" style="background:url(https://drumeo-assets.s3.amazonaws.com/sales/2023/order-bg-tile-2.png) center center/160px;">
