ed7bb3b380584ae5b62d717d98c3715ac11e8034
calebfavor <calebfavor@gmail.com>
2023-01-09 16:28:38 -0800

    Now using cache to pass railanalytics data to iframe instead of params.

--- resources/platform/views/partials/_railanalytics-brand-tracking-iframe.blade.php
+++ resources/platform/views/partials/_railanalytics-brand-tracking-iframe.blade.php
@@ -1,5 +1,36 @@
 @if(current_user_has_recent_order())
-    <iframe src="{{ get_legacy_brand_base_url('drumeo') . '/railanalytics/blank-tracking-page' }}?{{ http_build_query(\App\Analytics\Tracker::getQueueForBrand('drumeo')) }}"
+    @php
+        cache()->set(auth()->id() . '_recent_order_analytics_data_drumeo', \App\Analytics\Tracker::getQueueForBrand('drumeo'), 60)
+    @endphp
+
+    <iframe src="{{ get_legacy_brand_base_url('drumeo') . '/railanalytics/blank-tracking-page?cache_key=' . auth()->id() . '_recent_order_analytics_data_drumeo' }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+
+    @php
+        cache()->set(auth()->id() . '_recent_order_analytics_data_pianote', \App\Analytics\Tracker::getQueueForBrand('pianote'), 60)
+    @endphp
+
+    <iframe src="{{ get_legacy_brand_base_url('pianote') . '/railanalytics/blank-tracking-page?cache_key=' . auth()->id() . '_recent_order_analytics_data_pianote' }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+
+    @php
+        cache()->set(auth()->id() . '_recent_order_analytics_data_guitareo', \App\Analytics\Tracker::getQueueForBrand('guitareo'), 60)
+    @endphp
+
+    <iframe src="{{ get_legacy_brand_base_url('guitareo') . '/railanalytics/blank-tracking-page?cache_key=' . auth()->id() . '_recent_order_analytics_data_guitareo' }}"
+            loading="lazy"
+            style="position: absolute; width:0; height:0; border:0;">
+    </iframe>
+
+    @php
+        cache()->set(auth()->id() . '_recent_order_analytics_data_singeo', \App\Analytics\Tracker::getQueueForBrand('singeo'), 60)
+    @endphp
+
+    <iframe src="{{ get_legacy_brand_base_url('singeo') . '/railanalytics/blank-tracking-page?cache_key=' . auth()->id() . '_recent_order_analytics_data_singeo' }}"
             loading="lazy"
             style="position: absolute; width:0; height:0; border:0;">
     </iframe>
