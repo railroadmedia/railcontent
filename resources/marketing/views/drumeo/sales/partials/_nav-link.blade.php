747bb2c8532f2c40db9a45de4864e62f1bac528c
Trent <trent.harms@gmail.com>
2023-01-12 17:48:46 -0800

    Revert "Merge branch 'feature-marketing-optimisation'"
    This reverts commit d07ba681fbf267a849ef28568e93dc64b49a390a, reversing
    changes made to 03c372a32fcc0ec552b5765c2f18c9e9852ad374.
    
    # Conflicts:
    #	app/Http/Controllers/Drumeo/ShopController.php
    #	app/Http/Controllers/Guitareo/ShopController.php
    #	app/Http/Controllers/Pianote/ShopController.php
    #	app/Http/Controllers/Singeo/ShopController.php

--- resources/marketing/views/drumeo/sales/partials/_nav-link.blade.php
+++ resources/marketing/views/drumeo/sales/partials/_nav-link.blade.php
@@ -5,14 +5,14 @@
    target="{{ !empty($externalLink)? '_blank' : '_parent' }}" rel="{{ !empty($externalLink)? 'noopener' : '' }}">
     <div class="nav-link">
         @if(!empty($linkIcon))
-            <i class="{{ $linkIcon }} text-{{ $brand }}"></i>
+            <i class="{{ $linkIcon }}"></i>
         @endif
         {!! $linkName !!}
 
         @if(!empty($hasDropdown))
             <div class="drop-down-arrow {{ !empty($defaultOpen)? 'active' : '' }}">
-                <span class="bg-{{ $brand }}"></span>
-                <span class="bg-{{ $brand }}"></span>
+                <span></span>
+                <span></span>
             </div>
         @endif
     </div>
