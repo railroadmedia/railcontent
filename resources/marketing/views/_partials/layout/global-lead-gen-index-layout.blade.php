5f7bf2224f22137091e755306abd125e43ab4842
peterminhlee <petereminhlee@gmail.com>
2023-01-17 14:30:24 -0800

    testttttttttt

--- resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
@@ -11,5 +11,36 @@
 @stop
 
 @section('layout-styles')
+    <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/parcel/drumeo/lead-gen-course.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/css/global-styles.css') }}" rel="stylesheet">
+@endsection
 
+@section('layout-header')
+    @include($theme.".sales.partials._nav")
+@endsection
+
+@section('layout-body')
+    <div class="flex-1">
+        <div class="lesson-catalogue max-w-5xl mx-auto py-7 md:py-12">
+            <div class="list-wrapper px-4">
+                <h3 class="font-bold mb-4 md:mb-6">Course Lessons</h3>
+                @foreach($lessons as $key => $lesson)
+                    @include('_partials.components.leadgen-lesson',[
+                        'lessonURL' => $lesson->slug,
+                        'lessonNumber' => $key + 1,
+                        'lessonImg' => $lesson->thumbnail,
+                        'lessonTitle' => $lesson->title,
+                        'duration' => $lesson->duration
+                    ])
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('layout-footer')
+    @include($theme.".sales.partials._footer", [
+            "minimal" => true
+    ])
 @endsection
