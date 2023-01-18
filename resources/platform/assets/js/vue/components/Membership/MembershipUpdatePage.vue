e008e94762b1bfe74b2395e40e9c9afe9bf075e0
calebfavor <calebfavor@gmail.com>
2023-01-18 17:19:13 +0000

    songs upgrade page blade template

---
+++ resources/platform/assets/js/vue/components/Membership/MembershipUpdatePage.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import MembershipSelectModal from './MembershipSelectModal.vue';
+
+const props = defineProps({
+    brand: {
+        type: String,
+        default: 'drumeo'
+    },
+    upgradeCost: {
+        type: String,
+        default: null,
+    },
+    currentTier: {
+        type: String,
+        default: 'plus'
+    },
+    isLifetimeMember: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+</script>
+<template>
+    <MembershipSelectModal @onCloseModal="() => window.location = '/'" :upgradeCost="upgradeCost" :currentTier="currentTier"
+        :isLifetimeMember="isLifetimeMember" />
+</template>
