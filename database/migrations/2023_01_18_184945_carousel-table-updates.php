99f834e34d4a195ea928766f530d739c88534d61
peterminhlee <petereminhlee@gmail.com>
2023-01-18 11:35:16 -0800

    carousel table updates

---
+++ database/migrations/2023_01_18_184945_carousel-table-updates.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('carousels', function (Blueprint $table) {
+            $table->boolean('is_featured');
+            $table->integer('product_id')->nullable();
+            $table->string('product_url')->nullable();
+            $table->string('endpoint')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
