35223cc76ea6962aaa63d5d676aeeda8b3842901
peterminhlee <petereminhlee@gmail.com>
2023-01-20 09:07:33 -0800

    Revert "image updates"
    This reverts commit a13e6a58898df1753d2e21ca98162a1b14008ef4.

--- app/Nova/Flexible/Layouts/LeadgenLessonLayout.php
+++ app/Nova/Flexible/Layouts/LeadgenLessonLayout.php
@@ -46,7 +46,7 @@ class LeadgenLessonLayout extends Layout
             Text::make('slug')->required(),
             Text::make('title')->required(),
             Text::make('Description', 'desc'),
-            Image::make('thumbnail', 'thumbnail_path')
+            Image::make('thumbnail')
                 ->disk('nova_s3')
                 ->prunable()
                 ->hideFromIndex()
@@ -74,9 +74,9 @@ class LeadgenLessonLayout extends Layout
                 ->preview(function($value){
                     if(empty($value)) return null;
 
-                    return $value;
+                    return str_contains($value, 'amazonaws') || str_contains($value, 'cloudfront') || str_contains($value, 'vimeocdn') ? $value : 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$value;
                 }),
-            Text::make('thumbnail', 'thumbnail_text')->hideFromIndex()->hideFromDetail()->required(),
+            Text::make('thumbnail')->hideFromIndex()->hideFromDetail()->required(),
             Text::make('Video Src', 'video_src')->hideFromIndex()->required(),
             Number::make('duration')->help('In minutes'),
             Flexible::make('Assets')
--- app/Nova/Flexible/Resolvers/LeadgenLessonResolver.php
+++ app/Nova/Flexible/Resolvers/LeadgenLessonResolver.php
@@ -28,8 +28,7 @@ class LeadgenLessonResolver implements ResolverInterface
             return $layout->duplicateAndHydrate($lesson->id, [
                 'title' => $lesson->title,
                 'desc' => $lesson->desc,
-                'thumbnail_path' => $lesson->thumbnail,
-                'thumbnail_text' => $lesson->thumbnail,
+                'thumbnail' => $lesson->thumbnail,
                 'video_src' => $lesson->video_src,
                 'display_order' => $lesson->display_order,
                 'slug' => $lesson->slug,
@@ -50,11 +49,14 @@ class LeadgenLessonResolver implements ResolverInterface
      */
     public function set($model, $attribute, $groups)
     {
-        $lessons = $groups->map(function($group, $index) use($model){
+        $class = get_class($model);
+
+        $class::saved(function ($model) use ($groups){
+            $lessons = $groups->map(function($group, $index) use($model){
                return [
                    'title' => $group->getAttributes()['title'],
                    'desc' => $group->getAttributes()['desc'],
-                   'thumbnail' => !empty($group->getAttributes()['thumbnail_text']) ? $group->getAttributes()['thumbnail_text'] : $group->getAttributes()['thumbnail_path'],
+                   'thumbnail' => $group->getAttributes()['thumbnail'],
                    'video_src' => $group->getAttributes()['video_src'],
                    'slug' => $group->getAttributes()['slug'],
                    'display_order' => $index+1,
@@ -63,11 +65,11 @@ class LeadgenLessonResolver implements ResolverInterface
                 ];
             });
 
+            $total = count(LeadgenLesson::where('leadgen_id', $model['id'])->get());
+
             foreach($lessons as $key => $lesson){
                 //insert
                 if(is_null($lesson['id'])){
-                    if(!str_contains($lesson['thumbnail'], 'https')) $lessons['thumbnail'] = 'https://laravel-nova.s3.us-east-2.amazonaws.com/'.$lessons['thumbnail'];
-
                     $addLesson = new LeadgenLesson();
                     $addLesson->leadgen_id = $model['id'];
                     $addLesson->title = $lesson['title'];
@@ -129,6 +131,7 @@ class LeadgenLessonResolver implements ResolverInterface
                     $deleteLessons->delete();
                 }
 
-            };
+            }
+        });
     }
 }
