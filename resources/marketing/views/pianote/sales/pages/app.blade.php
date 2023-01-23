afcdf36c06b63035347b6997427b7a61632a847b
Trent <trent.harms@gmail.com>
2023-01-23 15:32:56 -0800

    updating app links

--- resources/marketing/views/pianote/sales/pages/app.blade.php
+++ resources/marketing/views/pianote/sales/pages/app.blade.php
@@ -41,8 +41,8 @@
         <div class="container mx-auto">
             <p>Put your piano teacher in your pocket. Take your video lessons, <br class="hidden md:inline">
                 song tutorials, and practice sessions anywhere you go.</p>
-            <a class="download-badge" href="https://itunes.apple.com/us/app/pianote-mobile/id1500496457?ls=1" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
-            <a class="download-badge" href="https://play.google.com/store/apps/details?id=com.pianote2" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
+            <a class="download-badge" href="https://apps.apple.com/us/app/musora/id1619053766?ppid=afddd5f6-fbc3-46c9-b6e4-6c9e6a6936af" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
+            <a class="download-badge" href="https://play.google.com/store/apps/details?id=com.musoraapp&listing=pianote_previews" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
             <p class="small">Not a member? <a class="text-blue" href="/">Click here to join Pianote</a>.</p>
         </div>
     </section>
@@ -82,8 +82,8 @@
             <div class="float-left w-full md:w-1/2 px-2 md:px-3 option-wrap">
                 <p>MEMBERS</p>
                 <h4>Take your membership everywhere<br> with the Pianote app.</h4>
-                <a class="download-badge" href="https://itunes.apple.com/us/app/pianote-mobile/id1500496457?ls=1" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
-                <a class="download-badge" href="https://play.google.com/store/apps/details?id=com.pianote2" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
+                <a class="download-badge" href="https://apps.apple.com/us/app/musora/id1619053766?ppid=afddd5f6-fbc3-46c9-b6e4-6c9e6a6936af" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/download-on-app-store-button.png"></a>
+                <a class="download-badge" href="https://play.google.com/store/apps/details?id=com.musoraapp&listing=pianote_previews" target="_blank"><img src="https://dpwjbsxqtam5n.cloudfront.net/app/google-play-button.png"></a>
             </div>
             <div class="float-left w-full md:w-1/2 px-2 md:px-3 option-wrap">
                 <p>NOT A MEMBER?</p>
