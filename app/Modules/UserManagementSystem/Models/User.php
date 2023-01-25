4e92e353eed80b157452b43979c71defd882435f
robwillems <robert@musora.com>
2023-01-25 15:19:59 -0800

    Enable songs access for everyone

--- app/Modules/UserManagementSystem/Models/User.php
+++ app/Modules/UserManagementSystem/Models/User.php
@@ -826,10 +826,11 @@ class User extends Model implements Authenticatable, CanResetPassword, Authoriza
 
     public function hasSongsAccess(?string $brand = null): bool
     {
-        if (!$brand) {
-            $brand = brand();
-        }
-        return $this->isAPlusMember() ||
-            ($brand == 'drumeo' && $this->is_drumeo_lifetime_member);
+        return true;
+//        if (!$brand) {
+//            $brand = brand();
+//        }
+//        return $this->isAPlusMember() ||
+//            ($brand == 'drumeo' && $this->is_drumeo_lifetime_member);
     }
 }
