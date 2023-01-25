bfcf85849bf6cb61684f17df336cdac68a2bd20c
Roxana Riza <roxana_r2000@yahoo.com>
2023-01-25 14:32:33 +0200

    Update soft deleted guitareo songs

---
+++ app/Console/Commands/UpdateOldGuitareoSongs.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Collection;
+use Railroad\Railcontent\Services\ContentService;
+
+class UpdateOldGuitareoSongs extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'UpdateOldGuitareoSongs';
+
+    protected $signature = 'UpdateOldGuitareoSongs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update "soft-deleted" Guitareo Songs. Set status as draft';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(
+        DatabaseManager $databaseManager,
+        ContentService $contentService
+    ) {
+        $this->info("UpdateOldGuitareoSongs command starts now \n");
+
+        $dbConn = $databaseManager->connection(config('railcontent.database_connection_name'));
+
+        $query =
+            $dbConn->table('railcontent_content')
+                ->select('railcontent_content.id', 'railcontent_content.type')
+                ->join('railcontent_content_instructors','railcontent_content.id','=','railcontent_content_instructors.content_id')
+                ->where('brand', 'guitareo')
+                ->where('type', 'song')
+                ->where('railcontent_content.id','<',377661)
+                ->where('railcontent_content_instructors.instructor_id','=',191339)
+              //  ->where('status','=','deleted')
+                ->orderBy('railcontent_content.id', 'asc');
+
+        $query->chunk(200, function (Collection $rows) use ($dbConn, $contentService) {
+            $dbConn->table('railcontent_content')
+                ->whereIn('id', $rows->pluck('id'))
+                ->update([
+                    'status' => 'draft',
+                    'type' => 'course-part'
+                         ]);
+            $contentService->fillCompiledViewContentDataColumnForContentIds(
+                $rows->pluck('id')
+                    ->toArray()
+            );
+            $contentService->fillParentContentDataColumnForContentIds(
+                $rows->pluck('id')
+                    ->toArray()
+            );
+        });
+
+        $this->info("UpdateOldGuitareoSongs command has finished");
+    }
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,6 +23,7 @@ use App\Console\Commands\RepairVimeoDurations;
 use App\Console\Commands\SeedLiveAndScheduledContent;
 use App\Console\Commands\SeedUserContentData;
 use App\Console\Commands\TestLessonsDescriptionUrls;
+use App\Console\Commands\UpdateOldGuitareoSongs;
 use App\Console\Commands\VaporEnvManager;
 use App\Console\Commands\UpdateRoutines;
 use Illuminate\Console\Scheduling\Schedule;
@@ -59,6 +60,7 @@ class Kernel extends ConsoleKernel
         AddTimeToUsersAccountsJan2023::class,
         UpdateRoutines::class,
         CreateSongs24Jan2022::class,
+        UpdateOldGuitareoSongs::class,
     ];
 
     /**
