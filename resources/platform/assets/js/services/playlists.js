9562d5ac902ae8d2c9fbfb7311f1f5de4431d107
Miguel Fondeur <miguelfondeur@gmail.com>
2023-01-26 16:42:13 +0000

    add playlist services

---
+++ resources/platform/assets/js/services/playlists.js
@@ -0,0 +1,40 @@
+//Playlists Services
+
+export default {
+
+    //GET Playlists
+    getCurrentUserPlaylists() {
+
+    },
+
+    //GET Pinned Playlists
+    getPinnedPlaylists() {
+
+    },
+
+    //GET Public Playlists
+    getPublicPlaylists() {
+
+    },
+
+    //CREATE Playlist
+    createPlaylist() {
+
+    },
+
+    //PIN Playlist 
+    pinPlaylist() {
+
+    },
+
+    //UNPIN Playlist
+    unpinPlaylist() {
+
+    },
+
+    //DELETE Playlist
+    deletePlaylist() {
+
+    }
+
+};
