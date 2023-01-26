b2b5c394aeee4b1f0bd95e6cd3ac493322c40130
peterminhlee <petereminhlee@gmail.com>
2023-01-26 08:06:21 -0800

    made assignment table

---
+++ database/migrations/2023_01_25_221019_create_leadgen_lesson_assignments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leadgen_lesson_assignments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('leadgen_lesson_id');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leadgen_lesson_assignments');
+    }
+};
