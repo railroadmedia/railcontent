1c2949cae9ec48193a62c19b7f684b0aa8b23d81
Jonathan Maingot <jonathan@drumeo.com>
2023-02-01 13:56:32 -0800

    Merge branch 'remove-temp-songs-access-lifetime-members' into beta-testing
    # Conflicts:
    #	app/Console/Kernel.php (kept all changes, just removed the conflict markup)
    #	public/mix-manifest.json (discarded changes, kept "our" version)

---
+++ app/Console/Commands/MembershipFieldsSync.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Modules\EventDataSynchronizer\Services\UserMembershipFieldsService;
+use Illuminate\Console\Command;
+use Illuminate\Database\DatabaseManager;
+
+class MembershipFieldsSync extends Command
+{
+    protected $name = 'MembershipFieldsSync';
+
+    protected $signature = 'MembershipFieldsSync {productId} {skip?} {chunkSize?}';
+
+    protected $description = 'run UserMembershipFieldsSync->sync() for all users owning a product';
+
+    private $connection;
+
+    public function handle(
+        DatabaseManager $databaseManager,
+        UserMembershipFieldsService $userMembershipFieldsService
+    )
+    {
+        // ----------------------------------------------------------------
+        // this matters
+        // ----------------------------------------------------------------
+
+        $connection = $databaseManager->connection('musora_laravel_mysql');
+        $this->connection = $connection;
+
+        $productId = $this->argument('productId');
+
+        // ----------------------------------------------------------------
+        // this is basically fluff
+        // ----------------------------------------------------------------
+
+        $product = $this->connection->table('ecommerce_products')
+            ->where('id', $productId)
+            ->get()
+            ->first();
+
+        $skip = $this->argument('skip');
+        $chunkSize = $this->argument('chunkSize');
+
+        $this->info('product id ' . $productId . ' is "' . $product->name . '" (sku: "' . $product->sku . '")');
+
+        if (!empty($skip)) {
+            $this->info('"skip" argument supplied: ' . $skip);
+        } else {
+            $skip = 0;
+            $this->info('"skip" is default value of ' . $skip);
+        }
+
+        if (!empty($chunkSize)) {
+            $this->info('"chunkSize" argument supplied: ' . $chunkSize);
+        } else {
+            $chunkSize = 100;
+            $this->info('"chunkSize" is default value of ' . $chunkSize);
+        }
+
+        // ----------------------------------------------------------------
+        // this matters
+        // ----------------------------------------------------------------
+
+        $userIds = $this->connection->table('ecommerce_user_products')
+            ->where('product_id', $productId)
+            ->pluck('user_id');
+
+        // ----------------------------------------------------------------
+        // this is basically fluff
+        // ----------------------------------------------------------------
+
+        $this->info('Number of users to process here: ' . count($userIds));
+
+        $answer = $this->ask('If this is correct enter "y" to continue');
+
+        $continue = in_array($answer, ['y', 'yes', 'Y', 'YES']);
+
+        if(!$continue){
+            $this->info('Ok fine be like that. Exiting now.');
+            return true;
+        }
+
+        // ----------------------------------------------------------------
+        // this matters
+        // ----------------------------------------------------------------
+
+        $chunkCounter = 0;
+
+        foreach($userIds->chunk($chunkSize) as $chunk){
+            $chunkCounter++;
+            if($skip){
+                if($chunkCounter < $skip){
+                    continue;
+                }
+            }
+            $this->info('starting chunk ' . $chunkCounter . ' of ' . (ceil($userIds->count()/$chunkSize)));
+            foreach($chunk as $userId) {
+                $result = $userMembershipFieldsService->sync($userId);
+                if(!$result){
+                    $this->info('$userMembershipFieldsService->sync for user id ' . $userId . ' returned a non true value');
+                }
+            }
+        }
+
+        return true;
+    }
+}
