e6b2f32cbb095c3f9dc392e81001714c6a1e8240
peterminhlee <petereminhlee@gmail.com>
2023-02-03 10:11:16 -0800

    typo fix

--- resources/marketing/views/_partials/components/shop/sidebar.blade.php
+++ resources/marketing/views/_partials/components/shop/sidebar.blade.php
@@ -44,7 +44,7 @@
             @else
                 @if($category !== 'bundles' && count($sizes) === 0 && !$bundle)
                     <a
-                        class="online-atc @if($them !== 'musora') vue-add-to-cart @endif"
+                        class="online-atc @if($theme !== 'musora') vue-add-to-cart @endif"
                         href="/ecommerce/add-to-cart?go-back-to-shop=true&products[{!! $sku !!}]=1"
                         data-base-url="/ecommerce/add-to-cart?go-back-to-shop=true&products[{!! $sku !!}]=1"
                         data-product-json='{"{!! $sku !!}": 1}'
@@ -73,7 +73,7 @@
                     </select>
 
                     <a
-                        class="online-atc merch @if($them !== 'musora') vue-add-to-cart @endif selected-pack"
+                        class="online-atc merch @if($theme !== 'musora') vue-add-to-cart @endif selected-pack"
                         href="#"
                         @if(!empty($promoCode))
                         data-base-url="/ecommerce/add-to-cart?go-back-to-shop=true&promo-code={{$promoCode}}"
