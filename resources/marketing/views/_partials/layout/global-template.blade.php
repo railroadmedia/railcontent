8f928e605dcaf4053674375efac1b9a6691b3aed
Trent <trent.harms@gmail.com>
2023-02-03 12:10:51 -0800

    fixing countdown

--- resources/marketing/views/_partials/layout/global-template.blade.php
+++ resources/marketing/views/_partials/layout/global-template.blade.php
@@ -89,7 +89,7 @@
                 $(this).html('<div class="inline-block"><h2 class="font-extrabold leading-none text-lg">LIMITED</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">TIME LEFT</p></div>');
             });
 
-        $('.tzcd-big').countdown('2023/03/01')
+        $('.tzcd-big').countdown('2023/02/17')
             .on('update.countdown', function (event) {
                 var format = '' + '<div class="inline-block mr-2"><h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">%M</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">min%!M</p></div> ' + '<div class="inline-block"><h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">%S</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">sec%!S</p></div>';
                 if (event.offset.totalHours > 0) {
--- resources/marketing/views/musora/shop/shop.blade.php
+++ resources/marketing/views/musora/shop/shop.blade.php
@@ -1,13 +1,12 @@
 @extends('_partials.layout.global-template')
 
 @section('meta')
-    <title></title>
-    <meta property="og:title" content="">
+    <title>Musora Shop</title>
+    <meta property="og:title" content="Musora Shop">
     <meta name="description" content="">
     <meta property="og:description" content="">
-    <meta property="og:image" content="">
-    <meta property="og:url" content="https://www.musora.com/merch/">
-    <meta name="robots" content="noindex">
+    <meta property="og:image" content="https://musora-center.s3.amazonaws.com/shop/header-m.jpg">
+    <meta property="og:url" content="https://www.musora.com/merch">
 @endsection
 
 @section('layout-styles')
@@ -528,7 +527,7 @@
                     <div class="py-10 px-4 md:px-20">
                         <h4 class="font-bold mb-2">Just a heads up!</h4>
                         <p class="mb-4">
-                            I understand that these items are preorders<br class="hidden md:inline-block"> and will not ship until <b>March 1st, 2023</b>.
+                            I understand that these items are preorders<br class="hidden md:inline-block"> and will not ship until after <b>March 1st, 2023</b>.
                         </p>
                         <div class="flex gap-2">
                             <a class="flex-1 rounded-full border-2 border-black uppercase text-black py-2 font-bold uppercase text-sm md:text-base" @click="orderModal = false">Cancel</a>
