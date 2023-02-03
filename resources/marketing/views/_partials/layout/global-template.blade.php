223d2ff8905b757e779a2ba9b177e893fce25800
Trent <trent.harms@gmail.com>
2023-02-03 11:32:29 -0800

    fixing countdown

--- resources/marketing/views/_partials/layout/global-template.blade.php
+++ resources/marketing/views/_partials/layout/global-template.blade.php
@@ -88,12 +88,8 @@
             .on('finish.countdown', function (event) {
                 $(this).html('<div class="inline-block"><h2 class="font-extrabold leading-none text-lg">LIMITED</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">TIME LEFT</p></div>');
             });
-    });
-</script>
 
-<script>
-    $(document).ready(function () {
-        $('.tzcd-big').countdown('2022/12/27')
+        $('.tzcd-big').countdown('2023/03/01')
             .on('update.countdown', function (event) {
                 var format = '' + '<div class="inline-block mr-2"><h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">%M</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">min%!M</p></div> ' + '<div class="inline-block"><h2 class="font-extrabold leading-none text-2xl md:text-3xl lg:text-4xl">%S</h2> <p class="leading-none uppercase font-extrabold text-xs text-promo">sec%!S</p></div>';
                 if (event.offset.totalHours > 0) {
