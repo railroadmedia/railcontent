f547f4aa0ceaf6841ae7fffac0a954426cdaf020
peterminhlee <petereminhlee@gmail.com>
2023-02-08 13:15:04 -0800

    removed required on description

--- app/Nova/Carousel.php
+++ app/Nova/Carousel.php
@@ -88,7 +88,7 @@ class Carousel extends Resource
                     return $value;
                 }),
             Text::make('logo')->hideFromIndex()->hideFromDetail()->help('Use this field if you have a hosted image link. (Google Drive links will NOT work.)'),
-            Markdown::make('Description')->required()->help('If a description exceeds 316 the last three characters will be replaced with an ellipses.<br> Use &lt;br&gt; for a line break.'),
+            Markdown::make('Description')->help('If a description exceeds 316 the last three characters will be replaced with an ellipses.<br> Use &lt;br&gt; for a line break.'),
             Text::make('CTA Button Text', 'cta_text')->hideFromIndex(),
             Text::make('CTA URL', 'cta_url'),
             Image::make('Image', 'img')
