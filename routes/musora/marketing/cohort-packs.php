e786345e7ea05185ab8980698024eef5c49f8bf3
peterminhlee <petereminhlee@gmail.com>
2023-02-08 13:09:40 -0800

    separated endpoints

--- routes/musora/marketing/cohort-packs.php
+++ routes/musora/marketing/cohort-packs.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\Musora\CohortPackController;
 use Illuminate\Support\Facades\Route;
 
-Route::domain('{musoraDomain}')->middleware(['web_public'])
+Route::domain('{drumeoDomain}')->middleware(['web_public'])
     ->group(function () {
         //Route::get('/cohort-packs/register/30-day-drummer-2', [CohortPackController::class, 'registerFor30DayDrummer2'] );
+    });
+
+Route::domain('{pianoteDomain}')->middleware(['web_public'])
+    ->group(function () {
         //Route::get('/cohort-packs/register/new-piano-players-start-here', [CohortPackController::class, 'registerNewPianoPlayersStartHere'] );
     });
 
