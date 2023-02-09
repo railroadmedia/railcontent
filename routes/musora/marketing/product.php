4d121ed1b5f48be44ae9a23306b8b8a4c26c462f
peterminhlee <petereminhlee@gmail.com>
2023-02-09 10:17:01 -0800

    added 30DD2

--- routes/musora/marketing/product.php
+++ routes/musora/marketing/product.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Musora\ProductPagesController;
 use App\Modules\Brand\Enums\Brand;
+use App\Http\Controllers\Drumeo\SalesController;
 
 Route::domain('{musoraDomain}')
     ->group(function () {
@@ -10,6 +11,8 @@ Route::domain('{musoraDomain}')
 //            ->whereIn('brand', ['pianote', 'drumeo', 'singeo', 'guitareo'])
 //            ->whereIn('category', ['shop', 'lessons', 'accessories', 'clothing'])
 //            ->name('marketing.products');
+
+        Route::get('/30-day-drummer', [SalesController::class, 'thirtyDayDrummer']);
 
         Route::get('/merch', [ProductPagesController::class, 'shopAlt']);
         Route::get('/shop/{slug}', [ProductPagesController::class, 'productAlt']);
