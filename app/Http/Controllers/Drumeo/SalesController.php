2fe28c278e1d4e81532b3e3d4f232a7b16f69559
peterminhlee <petereminhlee@gmail.com>
2023-02-11 13:35:56 -0800

    quietkick fix

--- app/Http/Controllers/Drumeo/SalesController.php
+++ app/Http/Controllers/Drumeo/SalesController.php
@@ -171,7 +171,7 @@ class SalesController extends BaseController
         $products = $this->productRepository->all();
         $products = array_combine(array_entity_column($products, 'getSku'), $products);
 
-        return view('drumeo.products.quietkick', ['products' => $products]);
+        return view('drumeo.products.quietkick', ['products' => $products, 'theme' => 'drumeo']);
     }
     public function eardrums()
     {
