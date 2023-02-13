0ff58e64d64dc72416fc07f2cda7ff96fcdd0934
robwillems <robert@musora.com>
2023-02-13 11:49:33 -0800

    Added pianote domain route

--- routes/musora/marketing/cohort-packs.php
+++ routes/musora/marketing/cohort-packs.php
@@ -8,6 +8,11 @@ Route::domain('{drumeoDomain}')->middleware(['web_public'])
         Route::get('/cohort-packs/register/30-day-drummer-2', [CohortPackController::class, 'registerFor30DayDrummer2'] );
     });
 
+Route::domain('{pianoteDomain}')->middleware(['web_public'])
+    ->group(function () {
+        Route::get('/cohort-packs/register/new-piano-players-start-here', [CohortPackController::class, 'registerNewPianoPlayersStartHere'] );
+    });
+
 Route::domain('{musoraDomain}')->middleware(['web_public'])
     ->group(function () {
         Route::get('/cohort-packs/register/30-day-drummer-2', [CohortPackController::class, 'registerFor30DayDrummer2'] );
