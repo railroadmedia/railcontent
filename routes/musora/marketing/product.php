f8dfbbd01c147ae02d6d49a3a5f7fa21a7d85bae
robwillems <robert@musora.com>
2023-02-13 09:43:15 -0800

    Make musora.com/30-day-drummer an authenticated route

--- routes/musora/marketing/product.php
+++ routes/musora/marketing/product.php
@@ -13,10 +13,14 @@ Route::domain('{musoraDomain}')
 //            ->whereIn('category', ['shop', 'lessons', 'accessories', 'clothing'])
 //            ->name('marketing.products');
 
-        Route::get('/30-day-drummer', [SalesController::class, 'thirtyDayDrummer']);
 
         Route::get('/merch', [ProductPagesController::class, 'shopAlt']);
         Route::get('/shop/{slug}', [ProductPagesController::class, 'productAlt']);
 
     });
 
+Route::domain('{musoraDomain}')
+    ->middleware(['web_authenticated'])
+    ->group(function () {
+        Route::get('/30-day-drummer', [SalesController::class, 'thirtyDayDrummer']);
+    });
