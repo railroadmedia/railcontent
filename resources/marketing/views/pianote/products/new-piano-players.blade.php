ebb2bc513b535700d700f89923af862528255f2f
Trent <trent.harms@gmail.com>
2023-02-14 13:18:33 -0800

    fixing text

--- resources/marketing/views/pianote/products/new-piano-players.blade.php
+++ resources/marketing/views/pianote/products/new-piano-players.blade.php
@@ -269,7 +269,7 @@
                     @endif
 
                     @if(!$hasProduct && is_current_user_a_member())
-                        <p class="mb-3 lg:-mb-7 mt-5 mb-2 text-drumeo text-left"><strong>You are a Pianote member! <br>Enroll for free by clicking on the button below.</strong></p>
+                        <p class="mb-3 lg:-mb-7 mt-5 mb-2 text-pianote text-left"><strong>You are a Pianote member! <br>Enroll for free by clicking on the button below.</strong></p>
                     @endif
 
                     <div class="flex flex-wrap sm:flex-nowrap items-center mt-6 sm:mt-5 lg:mt-10">
