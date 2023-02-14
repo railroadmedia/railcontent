d2a39f7dcd8c58343ba3fc72e06698298224f5f6
robwillems <robert@musora.com>
2023-02-14 15:21:49 -0800

    Sync all digital products to _owned_pack_product_skus

--- app/Modules/EventDataSynchronizer/Services/CustomerIoSyncService.php
+++ app/Modules/EventDataSynchronizer/Services/CustomerIoSyncService.php
@@ -488,8 +488,6 @@ class CustomerIoSyncService
             $brands = config('event-data-synchronizer.customer_io_brands_to_sync');
         }
 
-        $packSkusToSync = config('event-data-synchronizer.customer_io_pack_skus_to_sync_ownership');
-
         /**
          * @var $userProducts UserProduct[]
          */
@@ -506,7 +504,9 @@ class CustomerIoSyncService
                     continue;
                 }
 
-                if (in_array($userProduct->getProduct()->getSku(), $packSkusToSync) && $userProduct->isValid()) {
+                if (($userProduct->getProduct()->getType() == Product::TYPE_DIGITAL_SUBSCRIPTION
+                        || $userProduct->getProduct()->getType() == Product::TYPE_DIGITAL_ONE_TIME
+                    ) && $userProduct->isValid()) {
                     $productSkuArray[] = "_" . $userProduct->getProduct()->getSku() . "_";
                     $idArray[] = "_" . $userProduct->getProduct()->getId() . "_";
                 }
