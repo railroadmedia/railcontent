cb1be1c4f659e9323ebb2090a0818b1c74f7b8e0
robwillems <robert@musora.com>
2023-02-14 16:00:40 -0800

    Revert "Sync all digital products to _owned_pack_product_skus"
    This reverts commit d2a39f7dcd8c58343ba3fc72e06698298224f5f6.

--- app/Modules/EventDataSynchronizer/Services/CustomerIoSyncService.php
+++ app/Modules/EventDataSynchronizer/Services/CustomerIoSyncService.php
@@ -488,6 +488,8 @@ class CustomerIoSyncService
             $brands = config('event-data-synchronizer.customer_io_brands_to_sync');
         }
 
+        $packSkusToSync = config('event-data-synchronizer.customer_io_pack_skus_to_sync_ownership');
+
         /**
          * @var $userProducts UserProduct[]
          */
@@ -504,9 +506,7 @@ class CustomerIoSyncService
                     continue;
                 }
 
-                if (($userProduct->getProduct()->getType() == Product::TYPE_DIGITAL_SUBSCRIPTION
-                        || $userProduct->getProduct()->getType() == Product::TYPE_DIGITAL_ONE_TIME
-                    ) && $userProduct->isValid()) {
+                if (in_array($userProduct->getProduct()->getSku(), $packSkusToSync) && $userProduct->isValid()) {
                     $productSkuArray[] = "_" . $userProduct->getProduct()->getSku() . "_";
                     $idArray[] = "_" . $userProduct->getProduct()->getId() . "_";
                 }
