bd5aa0739cebea419d38a3fd0c8a3aa0d421b80a
robwillems <robert@musora.com>
2023-02-15 11:26:21 -0800

    Add sitemap routes

---
+++ routes/misc/sitemap_routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Misc\SiteMapController;
+
+Route::middleware(['web_public'])
+    ->group(function () {
+        Route::get(
+            '/sitemap.xml',
+            SiteMapController::class . '@sitemap'
+        );
+    });
+
+
+
