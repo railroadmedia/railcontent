c55b61ee8d81e9d521ce79a04e37627252669c90
robwillems <robert@musora.com>
2023-02-15 11:26:21 -0800

    Add sitemap routes

---
+++ app/Http/Controllers/Misc/SiteMapController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Misc;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SiteMapController extends BaseController
+{
+    public function sitemap(Request $request)
+    {
+        $root = $request->root();
+        if (str_contains('drumeo.com', $root)) {
+            return file_get_contents(resource_path('/sitemap/drumeo/sitemap.xml'));
+        }
+
+        abort(404)
+    }
+}
