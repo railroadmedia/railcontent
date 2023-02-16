3ca0f868f5d89ba9b7e01670141084432e9014d8
robwillems <robert@musora.com>
2023-02-15 16:13:27 -0800

    Change to load from file instead of redirect

--- app/Http/Controllers/Misc/SiteMapController.php
+++ app/Http/Controllers/Misc/SiteMapController.php
@@ -5,17 +5,35 @@ namespace App\Http\Controllers\Misc;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use Illuminate\Support\Str;
+use Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SiteMapController extends BaseController
 {
     public function sitemap(Request $request)
     {
-        $root = $request->root();
-        if (str_contains($root, 'drumeo.com')) {
-            return file_get_contents(resource_path('/sitemap/drumeo/sitemap.xml'));
+        $domain = $this->getDomain($request);
+        try {
+            $content = file_get_contents(resource_path("/sitemap/$domain/sitemap.xml"));
+            return Response::make($content, 200)->header('Content-Type', 'text/xml');
+        } catch (\Throwable $e) {
+            throw new NotFoundHttpException();
         }
+    }
 
-        abort(404);
+    public function getDomain(Request $request): string
+    {
+        if (Str::endsWith(request()->host(), 'musora.com')) {
+            return "musora";
+        } elseif (Str::endsWith(request()->host(), 'drumeo.com')) {
+            return "drumeo";
+        } elseif (Str::endsWith(request()->host(), 'pianote.com')) {
+            return "pianote";
+        } elseif (Str::endsWith(request()->host(), 'guitareo.com')) {
+            return "guitareo";
+        } elseif (Str::endsWith(request()->host(), 'singeo.com')) {
+            return "singeo";
+        }
+        throw new NotFoundHttpException();
     }
 }
