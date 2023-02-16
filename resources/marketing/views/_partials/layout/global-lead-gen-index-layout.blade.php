117797b073323546741af0f0a9dc40f38da9add5
peterminhlee <petereminhlee@gmail.com>
2023-02-15 16:01:51 -0800

    added header on index page

--- resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
@@ -22,6 +22,34 @@
 
 @section('layout-body')
     <div class="flex-1">
+        <div class="relative py-12 md:py-24">
+            <div class="absolute inset-0 z-10">
+                <img
+                    src="@if($theme === 'drumeo') https://musora-web-platform.s3.amazonaws.com/headers/drumeo-header.jpg @elseif($theme === 'pianote') https://musora-web-platform.s3.amazonaws.com/headers/pianote-header.jpg @elseif($theme === 'guitareo') https://musora-web-platform.s3.amazonaws.com/headers/guitareo-header.jpg @elseif($theme === 'singeo') https://musora-web-platform.s3.amazonaws.com/headers/singeo-header.jpg @endif"
+                    class="h-full w-full object-top object-cover transition-opacity opacity-0"
+                    alt="{{ $theme }} background"
+                    loading="lazy"
+                    onload="this.classList.remove('opacity-0')"
+                />
+            </div>
+            <div class="absolute inset-0 z-20" style="background: @if($theme === 'drumeo')linear-gradient(180deg,rgba(0,0,0,.05) 55%,rgba(9,92,170,.7)); @elseif($theme === 'pianote') linear-gradient(180deg,rgba(0,0,0,.05) 55%,rgba(213,8,29,.7)); @elseif($theme === 'guitareo') linear-gradient(180deg,rgba(0,0,0,.05) 55%,rgba(0,150,128,.7)); @elseif($theme === 'singeo') linear-gradient(180deg,rgba(0,0,0,.05) 55%,rgba(102,0,182,.7)); @endif">
+            </div>
+            <div class="text-center">
+                @if(!empty($leadgen->logo))
+                    <img
+                        class="transition-opacity opacity-0 h-16 md:h-24 lg:h-40 z-30 relative"
+                        src="{{$leadgen->logo}}"
+                        alt="leadgen logo"
+                        loading="lazy"
+                        onload="this.classList.remove('opacity-0')"
+                    />
+                @else
+                    <h1 class="text-white z-30 relative font-bold">
+                        {{ $leadgen->title }}
+                    </h1>
+                @endif
+            </div>
+        </div>
         <div class="lesson-catalogue max-w-5xl mx-auto py-7 md:py-12">
             <div class="list-wrapper px-4">
                 <h3 class="font-bold mb-4 md:mb-6">Course Lessons</h3>
