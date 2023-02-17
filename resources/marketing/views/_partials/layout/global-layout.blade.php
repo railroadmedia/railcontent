c1575b964d9e42e37beabd8f7230a18f051728cf
peterminhlee <petereminhlee@gmail.com>
2023-02-17 12:45:03 -0800

    alpine version switch

--- resources/marketing/views/_partials/layout/global-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-layout.blade.php
@@ -17,9 +17,9 @@
 
         {!! \App\Analytics\Tracker::trackPageView() !!}
 
-{{--        {!! \App\Analytics\Tracker::headBottom() !!}--}}
+        {!! \App\Analytics\Tracker::headBottom() !!}
 
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
 
     <body class="flex flex-col w-full min-h-screen pt-[40px] lg:pt-[56px] @yield('body-class')"
