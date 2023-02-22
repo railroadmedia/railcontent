ea4ad7377ec598f83520b9df4fc58def22bf16e0
peterminhlee <petereminhlee@gmail.com>
2023-02-22 10:00:25 -0800

    sticky bar comp

---
+++ resources/marketing/views/_partials/components/sticky-bar.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ $link }}" class="flex items-center justify-center py-2 px-2 sm:px-0 w-full z-[100] fixed" style="background: linear-gradient(180deg, #EBF0FF 0%, #DEE6FF 100%);">
+    <img class="h-8 sm:h-10 mr-4" src="https://cdn.musora.com/image/fetch/w_300,q_auto:best/{{ $logo }}" alt="sticky bar logo" />
+    <p class="text-sm sm:text-lg font-bebas uppercase mx-0 leading-none sm:leading-none">
+        {!! $text !!}
+    </p>
+</a>
