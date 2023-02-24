1271ed2a22b17f09aabff55fd13e41a6b110a378
Trent <trent.harms@gmail.com>
2023-02-24 13:20:12 -0800

    adjusting list styling

--- resources/marketing/views/_partials/components/leadgen-lesson.blade.php
+++ resources/marketing/views/_partials/components/leadgen-lesson.blade.php
@@ -1,6 +1,6 @@
 <a class="flex flex-row relative text-[#3F3F46] border-b border-[#E4E4E7] no-underline hover:bg-[#E7EFF6] group py-2 px-3" href="/{{ $lessonURL }}">
-    <div class="flex flex-col text-[#00101D] align-left justify-center hide-xs-only font-black text-lg xl:text-xl" style="flex: 0 0 35px;">{{ $lessonNumber }}</div>
-    <div class="flex flex-col justify-center" style="flex: 0 0 142px;">
+    <div class="flex flex-col text-[#00101D] align-left justify-center hide-xs-only font-black text-lg xl:text-xl flex-shrink-0 w-7 sm:w-9">{{ $lessonNumber }}</div>
+    <div class="flex flex-col justify-center flex-shrink-0 w-20 sm:w-36">
         <div class="relative aspect-16:9 overflow-hidden rounded-xl">
             <img src="https://musora.com/cdn-cgi/image/width=500/{{ $lessonImg }}" alt="Lesson Thumbnail {{ $lessonNumber }}" class="transition-opacity duration-500 opacity-1 absolute object-cover h-full w-full" loading="lazy">
             <div class="flex absolute inset-0 bg-black opacity-0 group-hover:opacity-40 z-20 flex justify-center items-center transition-opacity"></div>
@@ -9,7 +9,7 @@
             </div>
         </div>
     </div>
-    <div class="flex flex-col justify-center mr-auto overflow px-2">
+    <div class="flex flex-col justify-center mr-auto overflow px-3">
         <p class="text-[#00101D] font-black text-sm lg:text-base">
             {{ $lessonTitle }}
         </p>
@@ -18,7 +18,7 @@
         </p>
     </div>
     <div class="hidden xl:flex uppercase items-center justify-center text-xs" data-test="3 mins" style="flex: 0 0 80px;">{{ $duration }} @if($duration > 1) Mins @else Min @endif</div>
-    <div class="flex flex-col justify-center" style="flex:0 0 35px;">
+    <div class="flex flex-col justify-center">
         <i class="fas fa-play-circle text-[#D4D4D8] group-hover:text-[#00101D] text-[28px]" aria-hidden="true"></i>
     </div>
 </a>
