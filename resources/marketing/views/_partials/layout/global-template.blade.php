2d13023ddcb866a1f106e8a799d5588ced885460
peterminhlee <petereminhlee@gmail.com>
2023-02-24 14:12:48 -0800

    jquery error fix

--- resources/marketing/views/_partials/layout/global-template.blade.php
+++ resources/marketing/views/_partials/layout/global-template.blade.php
@@ -71,7 +71,7 @@
 </script>
 
 @yield('layout-scripts')
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script type="text/javascript" src="{{ asset('marketing/js/jquery.countdown-2.min.js') }}"></script>
 
 <script>
