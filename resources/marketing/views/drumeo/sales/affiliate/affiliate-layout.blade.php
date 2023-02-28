61ddbb5a7f763e428ab4ef060151ccceafccb35d
Trent <trent.harms@gmail.com>
2023-02-28 15:10:05 -0800

    affiliate updates

--- resources/marketing/views/drumeo/sales/affiliate/affiliate-layout.blade.php
+++ resources/marketing/views/drumeo/sales/affiliate/affiliate-layout.blade.php
@@ -12,14 +12,14 @@
 @endsection
 
 @section('promo-banner')
-    <div class="text-white px-4 py-8 md:py-16 relative z-10" style="background-color:#000318;">
-        <div class="container mx-auto">
-            <div class="sm:flex mx-auto items-start text-center" style="max-width: 1050px;">
-                <img class="avatar rounded-full border-4 lg:border-8 mx-auto mb-3 sm:mb-0 w-36 md:w-48 lg:w-72" style="border-color:#fe9f13; background-color:#fe9f13;" src="https://cdn.musora.com/image/fetch/w_540,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/sales/trials/@yield('url').jpg">
-                <div class="md:text-left px-2 md:pl-7 lg:pl-10">
+    <div class="text-white px-4 sm:px-6 py-6 sm:py-8 relative z-10" style="background-color:#000318;">
+        <div class="container max-w-4xl mx-auto">
+            <div class="sm:flex mx-auto items-start text-center">
+                <img class="rounded-full bg-drumeo border-drumeo border-4 sm:border-8 mx-auto mb-3 sm:mb-0 w-36 sm:w-56 lg:w-72" src="https://cdn.musora.com/image/fetch/w_540,q_auto:best/https://dpwjbsxqtam5n.cloudfront.net/sales/trials/@yield('url').jpg">
+                <div class="sm:text-left px-2 sm:pr-0 sm:pl-7 lg:pl-10">
                     <h2 class="uppercase"><strong>@yield('name') FANS</strong></h2>
-                    <h3 class="text-coaches">YOUR FIRST MONTH IS FREE!</h3>
-                    <h6 class="leading-normal my-3 md:my-5">
+                    <h3 class="text-drumeo">YOUR FIRST MONTH IS FREE!</h3>
+                    <h6 class="leading-normal my-3 sm:my-5">
                         <em>@yield('text')</em>
                     </h6>
                     <a href="/choose-your-trial-month/" class="join drumeo smaller">START MY FREE TRIAL</a>
