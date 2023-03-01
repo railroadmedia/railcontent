005e2724817cdb84a4e45c4839b61d47e7f2691a
peterminhlee <petereminhlee@gmail.com>
2023-03-01 12:26:51 -0800

    jquery optimisation

--- resources/marketing/views/_partials/layout/global-template.blade.php
+++ resources/marketing/views/_partials/layout/global-template.blade.php
@@ -70,10 +70,8 @@
     });
 </script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="{{ asset('marketing/js/jquery.countdown-2.min.js') }}"></script>
-
 @yield('layout-scripts')
+<script type="text/javascript" src="{{ asset('marketing/js/jquery.countdown-2.min.js') }}"></script>
 
 <script>
     $(document).ready(function () {
