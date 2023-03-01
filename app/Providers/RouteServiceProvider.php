46049f1cd2c3016a14cab1b22730c424363c8766
Mircea Murasan <mircea.murasan@artsoft-consult.ro>
2023-03-01 17:14:40 +0200

    Rearrange order routes.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $subDomain = current(explode('.', request()->getHost()));
-
+//dd($subDomain);
         /*
          * Domain patterns for usage in routes. This allows routes to accept any subdomain OR no subdomain for a given domain.
          */
@@ -55,7 +55,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::group([], base_path('routes/misc/legacy_brand_members_redirects_to_up.php'));
+
             Route::group([], base_path('routes/routes.php')); // not actually needed
             Route::group([], base_path('routes/misc/manifest_files_routes.php'));
             Route::group([], base_path('routes/misc/mobile_app_store_api_keys_json_routes.php'));
@@ -71,6 +71,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::group([], base_path('routes/musora/platform/home.php'));
             Route::group([], base_path('routes/musora/platform/search.php'));
 
+            Route::group([], base_path('routes/misc/legacy_brand_members_redirects_to_up.php'));
             Route::group([], base_path('routes/drumeo/sales.php'));
             Route::group([], base_path('routes/drumeo/shop.php'));
             Route::group([], base_path('routes/drumeo/lead-gen.php'));
