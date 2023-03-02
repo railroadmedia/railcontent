8e38d28897f5f06294c92ab42f686ee7ef60c99f
Roxana Riza <roxana_r2000@yahoo.com>
2023-03-02 14:30:14 +0200

    Fix upload playlist thumbnail

--- app/Decorators/Content/PlaylistItemDecorator.php
+++ app/Decorators/Content/PlaylistItemDecorator.php
@@ -2,32 +2,27 @@
 
 namespace App\Decorators\Content;
 
-use Railroad\Railcontent\Entities\ContentEntity;
 use Railroad\Railcontent\Support\Collection;
 
 class PlaylistItemDecorator extends TypeDecoratorBase
 {
     /**
      * @param Collection $contents
-     * @return Collection
+     * @return mixed|Collection
      */
     public function decorate(Collection $contents)
     {
-//        dd($contents);
-        // url
         $contentsOfType = $contents->whereNotNull('user_playlist_item_id');
-//
+
         if ($contentsOfType->isEmpty()) {
-//            dd($contentsOfType);
             return $contents;
         }
-//
-//        // lesson count first and add lessons
-        foreach ($contentsOfType as $contentIndex => $content) {
-            $contentsOfType[$contentIndex]['set_start_end_time'] = $content['type'] != 'assignment';
 
+        foreach ($contentsOfType as $contentIndex => $content) {
+            //set start/end time should not be displayed on assignments and song playlist items
+            $contentsOfType[$contentIndex]['set_start_end_time'] = $content['type'] != 'assignment';
         }
-//
+
         return $this->mergeDecorated($contents, $contentsOfType);
     }
 }
