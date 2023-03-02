7e7db6b6c46617ba8ae5ba2942dfc4954ad7c744
peterminhlee <petereminhlee@gmail.com>
2023-03-02 12:40:36 -0800

    added classical piano

--- resources/marketing/views/pianote/sales/pages/lifetime-members.blade.php
+++ resources/marketing/views/pianote/sales/pages/lifetime-members.blade.php
@@ -371,7 +371,7 @@
             </div>
 
             @if($products['PIANOTE-MEMBERSHIP-LIFETIME']->getStockAvailability() > 0)
-                <a  href="/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-LIFETIME]=1&products[pianote-headphones]=1&products[piano-chords-and-scales-guide]=1&products[pianote-practice-planner]=1&products[christmas-song-book-digital]=1&products[poster-chords]=1&products[poster-scales]=1&products[the-power-of-chords]=1&products[classical-piano]=1&products[jesus-molina-improvisation-and-musical-freedom-pack]=1&products[play-beautiful-piano]=1&products[piano-riffs-and-fills]=1&products[piano-technique-made-easy]=1&products[destupefy-your-left-hand]=1&products[worship-piano]=1&products[faster-fingers]=1&redirect=/order&locked=true"
+                <a  href="/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-LIFETIME]=1&products[pianote-headphones]=1&products[piano-chords-and-scales-guide]=1&products[pianote-practice-planner]=1&products[classical-book]=1&products[christmas-song-book-digital]=1&products[poster-chords]=1&products[poster-scales]=1&products[the-power-of-chords]=1&products[classical-piano]=1&products[jesus-molina-improvisation-and-musical-freedom-pack]=1&products[play-beautiful-piano]=1&products[piano-riffs-and-fills]=1&products[piano-technique-made-easy]=1&products[destupefy-your-left-hand]=1&products[worship-piano]=1&products[faster-fingers]=1&redirect=/order&locked=true"
                                         class="join methodcta my-4" style="background:#FB0188;">Become A lifetime Member &raquo;</a>
                 <p>Only <s>500</s> {{ $products['PIANOTE-MEMBERSHIP-LIFETIME']->getPublicStockCount() }} spots remaining!</p>
             {{--<p>(Or choose a payment plan on the next page.)</p>--}}
