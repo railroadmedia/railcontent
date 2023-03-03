7b9dbc858a594ec1fb7c5a3730be0e3ce97d1512
peterminhlee <petereminhlee@gmail.com>
2023-03-02 20:55:41 -0800

    img alt update

--- resources/marketing/views/drumeo/sales/march-promo.blade.php
+++ resources/marketing/views/drumeo/sales/march-promo.blade.php
@@ -133,7 +133,7 @@
     @endif
 
     <a href="#customize-anchor" class="anchor-slide flex items-center justify-center py-2 px-2 sm:px-0 w-full z-[100]" style="background: linear-gradient(180deg, #EBF0FF 0%, #DEE6FF 100%);">
-        <img class="h-8 sm:h-10 mr-4" src="https://cdn.musora.com/image/fetch/w_300,q_auto:best/https://drumeo-assets.s3.amazonaws.com/promos/anniversary/2023/logo.png" alt="30 day drummer logo" />
+        <img class="h-8 sm:h-10 mr-4" src="https://cdn.musora.com/image/fetch/w_300,q_auto:best/https://drumeo-assets.s3.amazonaws.com/promos/anniversary/2023/logo.png" alt="11th anniversary logo" />
         <p class="text-sm sm:text-lg font-bebas uppercase mx-0 leading-none sm:leading-none">
             Celebrate 11 Years Of Drumeo With<br>
             11 FREE Bonuses 🥳 (worth $1198.95)
