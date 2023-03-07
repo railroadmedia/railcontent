1f4b4cce5f98008895bb8f6fef1a9f777a17b47a
peterminhlee <petereminhlee@gmail.com>
2023-03-07 13:57:45 -0800

    handbook updates

--- resources/marketing/views/musora/handbook.blade.php
+++ resources/marketing/views/musora/handbook.blade.php
@@ -6,6 +6,7 @@
         p {
             color: #44444F;
             padding-bottom: 60px;
+            line-height: 30px;
         }
 
         .header {
@@ -28,11 +29,55 @@
         <p>
             Empowering music students around the world to achieve musical freedom through our inspiring social learning communities. We do this by partnering with the best teachers, filming entertaining and educational content, hiring an amazing staff, and utilizing cutting edge web and mobile technology.
         </p>
-        <div class="header">Our vision (What are we going to do?) </div>
+        <div class="header">Our vision (What are we going to do?)</div>
         <p>
             To Be The #1 Music Education Company In The World!<br><br>
             We will do this by publishing online courses, books, apps, and membership websites. Our full-time staff will grow to more than 100 people and we will establish a presence in the piano, guitar, voice, bass, drum, and recording markets.
-
         </p>
+        <div class="header">Our values (How are we going to achieve our vision?)</div>
+        <p>
+            Below are the values that guide management and employees to make decisions autonomously. This is how we prioritize new projects, make the best staff hiring decisions, and gives us a guideline on how we treat our students and staff. <br><br>
+            1. We believe that all of our students should be treated like family members.<br>
+            2. We believe that staff should love what they do, even if it means not working here.<br>
+            3. We believe that staff should always keep pushing, in our successes and failures.<br>
+            4. We believe in sticking to deadlines and will work extra hours to get the job done right.<br>
+            5. We believe in connecting with our students, regardless of whether they’ve paid us or not.<br>
+            6. We believe that strong relationships are more important than powerful technology.<br>
+            7. We believe that every day is an opportunity to do a better job than the previous day.<br>
+            8. We believe that music education can have a positive effect on people’s lives.<br>
+            9. We believe in an open door policy; staff and instructors should be approachable.<br>
+            10. We believe in a clean workspace, even if we have to clean up someone else’s mess.
+        </p>
+        <div class="header">Holiday</div>
+        <p>
+            At Musora Media Inc we believe in taking a break from work from time to time. It’s important to get time away from the office to just recharge and connect closer with family and friends.
+        </p>
+        <div class="header">Schedule</div>
+        <p>
+            We value an autonomous work mindset and have a “Work from Wherever Just Get Your Sh*t Done” policy. Your leadership team loves working with you in the office in person. However, you are welcome to work in-office or from home. A typical work-day will be 8am - 4pm however if you need to make alternate arrangements, that can be discussed with your manager. You are welcome to work from wherever you want. We will only question the process if we’re not getting the result.
+        </p>
+        <div class="header">Focus</div>
+        <p>
+            Working on a computer all day can be tiring on the eyes and easy to get distracted. We suggest taking a quick 5-minute break or standing up to grab a glass of water if you need to. However, it’s important to stay focused and do your best to not distract other team members by talking loudly or sending random messages through chat.<br><br>
+            There are exceptions to this if what you have to say is very funny. However, if it’s only a 6/10 on the funny scale you should probably wait until lunch break or after hours to discuss.<br><br>
+            Personal mobile devices are fine to use during work hours, however, situations will be treated on a case-by-case basis. If a team member is on their phone too frequently, disciplinary action will be taken in the form of a public shaming.<br><br>
+            Logging into social media sites like Reddit, Facebook, Instagram, and Snapchat can be a huge distraction from your main focus and Musora Media Inc’s Mission. We ask that you keep all of this activity to a minimum unless it’s in your job description or you need to check something very important.
+        </p>
+        <div class="header">Payroll</div>
+        <p>
+            All team members are paid every two weeks on Friday. This is generally done by direct deposit (Note: you must give Elizabeth a direct deposit form or VOID cheque to initiate this). For all questions relating to payroll and deductions you can email Elizabeth at: elizabeth@musora.com
+        </p>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
+        <div class="header"></div>
     </div>
 @stop
