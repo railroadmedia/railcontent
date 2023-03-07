8b6f88fbb638a544ffff7d9efbb6c02dd9fee090
peterminhlee <petereminhlee@gmail.com>
2023-03-07 09:46:07 -0800

    added tailwind helper

--- resources/marketing/views/_partials/layout/global-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-layout.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
+        <link href="{{ asset('/marketing/css/tailwind-helpers.css') }}" rel="stylesheet">
 
         @yield('head-includes')
 
@@ -43,7 +44,7 @@
              x-on:click="showOverlay = !showOverlay, sidebarOpen = !sidebarOpen"
              x-bind:class="{ 'fixed h-screen w-screen z-30 bg-black bg-opacity-20': showOverlay }"
         ></div>
-        
+
          @yield('layout-scripts')
         {!! \App\Analytics\Tracker::bodyBottom() !!}
     </body>
