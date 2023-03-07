088ebb4f30b0404084965a0f23f679c0be9fc8f1
peterminhlee <petereminhlee@gmail.com>
2023-03-07 10:18:19 -0800

    updates

--- resources/marketing/views/musora/handbook.blade.php
+++ resources/marketing/views/musora/handbook.blade.php
@@ -2,6 +2,18 @@
 
 @section('head-includes')
     <meta name="robots" content="noindex">
+    <style>
+        p {
+            color: #44444F;
+            padding-bottom: 60px;
+        }
+
+        .header {
+            font-weight: 900;
+            font-size: 20px;
+            padding-bottom: 10px;
+        }
+    </style>
 @endsection
 
 <!-- Main -->
@@ -12,11 +24,11 @@
         <p class="text-[#44444F]">
             This Employee Handbook outlines some basic guidelines for all staff working at Musora Media Inc. Keep in mind, these are simply guidelines and not meant to be a strict set of rules. Occasionally, you may operate outside of these guidelines, but it’s important to use these as a baseline set of standards.
         </p>
-        <div class="font-extrabold text-xl">Our mission (Why does this company exist?)</div>
+        <div class="header">Our mission (Why does this company exist?)</div>
         <p>
             Empowering music students around the world to achieve musical freedom through our inspiring social learning communities. We do this by partnering with the best teachers, filming entertaining and educational content, hiring an amazing staff, and utilizing cutting edge web and mobile technology.
         </p>
-        <div class="font-extrabold text-xl">Our vision (What are we going to do?) </div>
+        <div class="header">Our vision (What are we going to do?) </div>
         <p>
             To Be The #1 Music Education Company In The World!<br><br>
             We will do this by publishing online courses, books, apps, and membership websites. Our full-time staff will grow to more than 100 people and we will establish a presence in the piano, guitar, voice, bass, drum, and recording markets.
