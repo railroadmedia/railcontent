2802107b1a161ccbbc4837ab8dd5d96bc236efd8
peterminhlee <petereminhlee@gmail.com>
2023-03-09 10:36:05 -0800

    carousel v2 table updates

---
+++ database/migrations/2023_03_08_203203_carousel_v2.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('carousels', function (Blueprint $table) {
+            $table->renameColumn('cta_text', 'primary_cta_text');
+            $table->renameColumn('cta_url', 'primary_cta_url');
+            $table->renameColumn('img', 'desktop_img');
+            $table->string('primary_cta_text_alt')->nullable();
+            $table->string('secondary_cta_text')->nullable();
+            $table->string('secondary_cta_url')->nullable();
+            $table->string('tablet_img');
+            $table->string('mobile_img');
+            $table->dropColumn('endpoint');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('carousels', function (Blueprint $table) {
+            $table->renameColumn('primary_cta_text', 'cta_text');
+            $table->renameColumn('primary_cta_url', 'cta_url');
+            $table->renameColumn('desktop_img', 'img');
+            $table->dropColumn('primary_cta_text_alt');
+            $table->dropColumn('secondary_cta_text');
+            $table->dropColumn('secondary_cta_url');
+            $table->dropColumn('tablet_img');
+            $table->dropColumn('mobile_img');
+            $table->string('endpoint');
+        });
+    }
+};
