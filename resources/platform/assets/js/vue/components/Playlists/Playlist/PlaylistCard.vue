d44cdffa4871678a1bea1abc229d93469641b48b
Miguel Fondeur <miguelfondeur@gmail.com>
2023-03-13 22:37:45 +0000

    revert fixes, data is too broken

--- resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
+++ resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
@@ -59,7 +59,6 @@
 
     //-----------Refs-----------//
     const dropdownTarget = ref(null)
-    const parentContent = ref({});
 
     //-----------Reactive Data-----------//
     const state = reactive({
@@ -139,14 +138,10 @@
         state.dropdownOpen = !state.dropdownOpen;
     }
 
-    //-----------Lifecycle Methods-----------//gps
+    //-----------Lifecycle Methods-----------//
 
     onBeforeMount(() => {
-        console.log('lesson', { ...props.lesson })
-        if (props.lesson && props.lesson.parent_content_data) {
-            parentContent.value = JSON.parse(props.lesson.parent_content_data);
-            console.log('parsed parent content data',{...parentContent.value})
-        }
+        console.log(props.lesson)
         state.isPinned = props.lesson.pinned;
         state.isPrivate = props.lesson.private;
     });
@@ -199,7 +194,7 @@
             <div class="tw-truncate tw-w-full lg:tw-w-[calc(100%-350px)] tw-px-3 lg:tw-pl-[15px]">
                 <!--name-->
                 <p class="tw-truncate">
-                    <template v-if="lesson.type !== 'assignment' && lesson.instructors && lesson.instructors.length">
+                    <template v-if="lesson.type !== 'assignment'">
                         <span class="tw-uppercase tw-mr-1 tw-text-[#3F3F46] dark:tw-text-[#9EC0DC] tw-text-sm"
                                 v-for="(instructor, i) in lesson.instructors"
                                 :key="i">
@@ -236,7 +231,7 @@
                     </button>
                 </div>
                 <!-- Assignment Instructors -->
-                <p v-if="lesson.type === 'assignment' && lesson.parent && lesson.parent.instructors" class="tw-truncate">   
+                <p v-if="lesson.type === 'assignment'" class="tw-truncate">   
                     <span class="tw-uppercase tw-mr-1 tw-text-[#3F3F46] dark:tw-text-[#9EC0DC] tw-text-sm"
                             v-for="(instructor, i) in lesson.parent.instructors"
                             :key="i">
