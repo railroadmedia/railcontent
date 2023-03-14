3e0f9058cf88c6efcc903a23cd5a2501f4681caf
Miguel Fondeur <miguelfondeur@gmail.com>
2023-03-14 11:23:39 +0000

    fix playlist page conflict

--- resources/platform/views/account/playlists.blade.php
+++ resources/platform/views/account/playlists.blade.php
@@ -6,21 +6,11 @@
 
 @section('content')
 
-<<<<<<< HEAD
     {{-- Playlist Header --}}
     <playlist-collection-header
         :brand="{{ $brand }}"
         :playlist-count="{{ $playlistsNumber }}"
     /></playlist-collection-header>
-=======
-    @include('partials.bladesora.members.partials._account-header', [
-        'backgroundImage' => 'https://d3fzm1tzeyr5n3.cloudfront.net/headers/'.$brand.'-header.jpg',
-        'userAvatar' => user()->profile_picture_url,
-        'userName' => user()->display_name,
-        'appName' => 'Musora',
-        'memberSince' => user()->created_at,
-    ])
->>>>>>> master
 
     <div class="tw-container tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white tw-pb-14">
         <div class="tw-flex tw-flex-col">
