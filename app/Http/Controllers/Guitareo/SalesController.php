66a0c32f61f57597b2fd5b007cb143a8f398a494
peterminhlee <petereminhlee@gmail.com>
2023-03-14 13:46:34 -0700

    added them variable

--- app/Http/Controllers/Guitareo/SalesController.php
+++ app/Http/Controllers/Guitareo/SalesController.php
@@ -60,7 +60,7 @@ class SalesController extends BaseController
 
     public function lifetime()
     {
-        return view('guitareo.shop.pages.lifetime-bundle');
+        return view('guitareo.shop.pages.lifetime-bundle', [ 'theme' => 'guitareo' ]);
     }
 
 
