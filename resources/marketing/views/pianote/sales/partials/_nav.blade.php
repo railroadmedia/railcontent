eeced427ed1555accc801d782f9ce1f7c5e54ab8
Trent <trent.harms@gmail.com>
2023-03-13 18:05:22 -0700

    changing nav cta

--- resources/marketing/views/pianote/sales/partials/_nav.blade.php
+++ resources/marketing/views/pianote/sales/partials/_nav.blade.php
@@ -71,7 +71,7 @@
                 >
 
                 @if(!empty($trialVersion))
-                    Start Trial
+                    Start for free <i class="fas fa-arrow-right" style="line-height: 0;" aria-hidden="true"></i>
                 @else
                     Join<span class="show-for-medium"> Pianote</span>
                 @endif
