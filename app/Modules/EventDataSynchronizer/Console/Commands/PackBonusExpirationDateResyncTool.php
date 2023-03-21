e459659ce19a6ac422d1126b6edc14296ac8d9fc
robwillems <robert@musora.com>
2023-03-20 19:56:03 -0700

    Fix chunking issue

--- app/Modules/EventDataSynchronizer/Console/Commands/PackBonusExpirationDateResyncTool.php
+++ app/Modules/EventDataSynchronizer/Console/Commands/PackBonusExpirationDateResyncTool.php
@@ -3,6 +3,7 @@
 namespace App\Modules\EventDataSynchronizer\Console\Commands;
 
 use App\Modules\Ecommerce\Models\UserProduct;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Collection;
 use App\Modules\EventDataSynchronizer\Listeners\UserProductToUserContentPermissionListener;
@@ -20,30 +21,27 @@ class PackBonusExpirationDateResyncTool extends Command
 
         $query = UserProduct::query()->select('ecommerce_user_products.*')
             ->join('ecommerce_products', 'ecommerce_products.id', '=', 'ecommerce_user_products.product_id')
-            ->where('ecommerce_user_products.product_id', '=', $productId)
-            ->where('expiration_date', '=', '2023-03-28');
+            ->where('ecommerce_user_products.product_id', '=', $productId);
 
         $count = $query->count();
 
-        $this->info("$count records to be updated.");
+        $this->info("$count records to be processed.");
         $done = 0;
 
-        $query->chunk(250, function (Collection $userProducts) use (
+        $query->chunk(1000, function (Collection $userProducts) use (
             $userProductToUserContentPermissionListener,
             &$done
         ) {
-            $newDate = '2023-04-01';
+            $oldDate = new Carbon('2023-03-28');
+            $newDate = new Carbon('2023-04-01');
             /** @var UserProduct $userProduct */
             foreach ($userProducts as $userProduct) {
-                if ($userProduct->expiration_date < $newDate) {
+                if ($userProduct->expiration_date == $oldDate) {
                     $userProduct->expiration_date = $newDate;
                     $userProduct->save();
+                    $userProductToUserContentPermissionListener->syncUserId($userProduct->user_id);
+                    $done++;
                 }
-            }
-
-            foreach ($userProducts as $userProduct) {
-                $userProductToUserContentPermissionListener->syncUserId($userProduct->user_id);
-                $done++;
             }
 
             $this->info('Done ' . $done);
