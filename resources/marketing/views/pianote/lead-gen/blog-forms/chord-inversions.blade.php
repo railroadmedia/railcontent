c65bed23ebfd1a8cdb7b55f3d43410627d1bec5e
Trent <trent.harms@gmail.com>
2023-03-21 13:36:26 -0700

    cleaning up blog forms + new form

--- resources/marketing/views/pianote/lead-gen/blog-forms/chord-inversions.blade.php
+++ resources/marketing/views/pianote/lead-gen/blog-forms/chord-inversions.blade.php
@@ -1,11 +1,11 @@
 @extends('pianote.lead-gen.blog-forms.blog-form-layout')
 
-@section('title', 'F#m')
+@section('title', 'Practice Chord Inversions')
 
 @section('form')
     @include("pianote._partials._sign-up-form", [
-        "formName" => 'F Sharp Minor',
-        "formId" => "Pianote - Engagement - Trigger - F Sharp Minor - Web Form",
+        "formName" => 'Practice Chord Inversions',
+        "formId" => "Pianote - Engagement - Trigger - Practice Chord Inversions - Web Form",
         "buttonText" => "Get Backing Track",
         "minimalForm" => true,
     ])
