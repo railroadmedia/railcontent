2f7d47ce227ab619b61f7c006e10dc3045053598
Trent <trent.harms@gmail.com>
2023-03-23 12:01:53 -0700

    fixing gift card

--- resources/marketing/views/drumeo/drumshop/pages/gift-card.blade.php
+++ resources/marketing/views/drumeo/drumshop/pages/gift-card.blade.php
@@ -29,10 +29,6 @@
         <div class="lg:h-0">
             <div id="order" class="anchor"></div>
             <div class="side-slide overflow-hidden rounded border border-solid fixedSlider" style="border-color: rgb(204, 211, 211); width: 352px;">
-                <div class="promo-tab hidden lg:block text-center py-4 px-3" style="position:relative;background: #000 url(https://dpwjbsxqtam5n.cloudfront.net/promos/christmas/snow-dark.gif) center center/250px;box-shadow: 0 0 10px inset #000;">
-                    <img class="w-auto max-h-12" src="https://www.musora.com/musora-cdn/image/width=200,quality=85/https://dpwjbsxqtam5n.cloudfront.net/promos/christmas/holiday-drums.png">
-                </div>
-
                 <div class="buy-section active px-5 pt-2 pb-6 text-center lg:py-6">
 
                     <h1 class="text-center text-3xl uppercase md:text-4xl"><strong class="font-black text-drumeo">$<span class="chosen-variant-price-float">29</span></strong></h1>
