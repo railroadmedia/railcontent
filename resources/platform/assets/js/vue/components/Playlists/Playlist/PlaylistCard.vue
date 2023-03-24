0e60c94eab820377059bf4dd5d5e9e5fc960fd60
calebfavor <calebfavor@gmail.com>
2023-03-24 23:24:51 +0000

    WIP Playlistcard

--- resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
+++ resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
@@ -260,7 +260,7 @@
                 <p v-if="lesson.type === 'assignment' && !cueVersion" class="tw-hidden md:tw-flex tw-text-xs md:tw-text-sm tw-truncate tw-capitalize tw-mr-1 tw-text-[#3F3F46] dark:tw-text-[#9EC0DC] tw-mt-1 md:tw-mt-0">   
                     <span v-for="(instructor, i) in lesson.instructors"
                         :key="i">
-                        {{ instructor }}<span v-if="i != (lesson.instructors.length - 1)">,</span>
+                        {{ instructor }}<span v-if="i != (lesson.instructors.length - 1)">,&nbsp;</span>
                     </span>
                 </p>   
                 <!-- Cue Version Data -->
@@ -306,6 +306,35 @@
                     <span>{{ duration_formatted(lesson.duration) || '0:00:00' }}</span>
                 </div>
             </template>
+
+            <!-- PLAYBACK CUE OPTIONS DROPDOWN -->
+        <div v-if="cueVersion" class="tw-inline-flex tw-items-center tw-justify-end tw-shrink-0 tw-w-[35px] lg:tw-w-[100px] md:tw-justify-center" >
+            <div class="tw-relative "
+                 v-click-outside="()=>{ state.dropdownOpen = false }"
+            >
+                <button v-if="!needAccess && !playlistsStore.sortingPlaylist" class="tw-text-[#445F74] dark:tw-text-[#7E9AB1] tw-bg-transparent dark:hover:tw-bg-[#102230] hover:tw-bg-[#F5F5F6] tw-transition-colors tw-p-0 tw-mb-0 focus-visible:tw-outline focus-visible:tw-outline-[#111827] dark:focus:tw-outline-[#9EC0DC] focus-visible:tw-outline-2 tw-rotate-0 lg:tw-rotate-90"
+                        title="Playlist Options"
+                        @click.prevent="dropdownTriggerHandler($event)"
+                >
+                    <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.5 5.20768L12.5 5.2181M12.5 12.4993L12.5 12.5098M12.5 19.791L12.5 19.8014M12.5 6.24935C11.9247 6.24935 11.4583 5.78298 11.4583 5.20768C11.4583 4.63239 11.9247 4.16602 12.5 4.16602C13.0753 4.16602 13.5417 4.63239 13.5417 5.20768C13.5417 5.78298 13.0753 6.24935 12.5 6.24935ZM12.5 13.541C11.9247 13.541 11.4583 13.0746 11.4583 12.4993C11.4583 11.9241 11.9247 11.4577 12.5 11.4577C13.0753 11.4577 13.5417 11.9241 13.5417 12.4993C13.5417 13.0746 13.0753 13.541 12.5 13.541ZM12.5 20.8327C11.9247 20.8327 11.4583 20.3663 11.4583 19.791C11.4583 19.2157 11.9247 18.7493 12.5 18.7493C13.0753 18.7493 13.5417 19.2157 13.5417 19.791C13.5417 20.3663 13.0753 20.8327 12.5 20.8327Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+
+                <!-- Dropdown-->
+                <PlaylistDropdown
+                    :brand="brand"
+                    :dropdownTop="state.dropdownTop"
+                    :is-open="state.dropdownOpen"
+                    :dropdownOptions="dropdownOptions"
+                    :data="lesson"
+                    :index="index"
+                    type="lesson"
+                    @closeDropdown="state.dropdownOpen = false"
+                    @pinItem="(val) => state.isPinned = val"
+                />
+            </div>
+        </div>
         </a>
 
         <!-- PLAYLISTS OPTIONS DROPDOWN -->
