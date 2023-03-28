6aae01779c5a56e271bc0029c6c331583a7bc687
Trent <trent.harms@gmail.com>
2023-03-28 09:55:57 -0700

    adding stock count

--- resources/marketing/views/pianote/products/new-piano-players-deal.blade.php
+++ resources/marketing/views/pianote/products/new-piano-players-deal.blade.php
@@ -160,15 +160,7 @@
         <div class="max-w-2xl lg:max-w-5xl mx-auto text-center text-white">
             <img class="h-12 mb-4" src="https://d2vyvo0tyx8ig5.cloudfront.net/logo/pianote-logo-red.png" alt="pianote logo">
             <h3 class="font-extrabold">Join Pianote today and get 12 free bonuses!</h3>
-            <h5 class="uppercase my-8 text-yellow">Only
-                    <span x-cloak x-data="timer()" x-init="countdown()">
-                         <span x-cloak x-show="timeLeft > 0 && day !== '00'"><span x-text="day"></span><span x-text="dayText"></span></span>
-                         <span x-cloak x-show="timeLeft > 0 && hour !== '00'"><span x-text="hour"></span><span x-text="hourText"></span></span>
-                         <span x-cloak x-show="timeLeft > 0"><span x-text="minute"></span><span x-text="minuteText"></span></span>
-                         <span x-cloak x-show="timeLeft > 0"><span x-text="second"></span><span x-text="secondText"></span></span>
-                         <span x-cloak x-show="timeLeft < 0">A Limited Time</span>
-                     </span>
-                left!</h5>
+            <h5 class="uppercase my-8 text-yellow">Only @if($products['PIANOTE-MEMBERSHIP-LIFETIME']->getPublicStockCount() < 500)<s>500</s>@endif {{ $products['PIANOTE-MEMBERSHIP-LIFETIME']->getPublicStockCount() }} Headphones left!</h5>
             <div class="max-w-md mx-auto mb-10">
                 <img
                     src="https://cdn.musora.com/image/fetch/w_800,q_auto:best/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/november/pianote-annual-card.jpg"
