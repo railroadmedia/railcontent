08b15a4ec6bc7d743f65cd6267bedbca03f6a358
Trent <trent.harms@gmail.com>
2023-03-29 11:46:01 -0700

    wip

--- resources/marketing/views/drumeo/lead-gen/getting-started/signup.blade.php
+++ resources/marketing/views/drumeo/lead-gen/getting-started/signup.blade.php
@@ -58,7 +58,7 @@
                     </h6>
                 </div>
 
-                @include("drumeo.lead-gen.partials.sign-up-form-tw", [
+                @include("drumeo.lead-gen.partials.sign-up-form-rc", [
                     "formId" => "Drumeo - Engagement - Trigger - GSOTD - Web Form",
                     "formName" => 'Getting Started On The Drums',
                     "buttonText" => "Get started for free ",
@@ -140,7 +140,7 @@
             <h3 class="leading-tight font-extrabold my-10">
                 Enter your email address<br class="inline sm:hidden"> below to get started:
             </h3>
-            @include("drumeo.lead-gen.partials.sign-up-form-tw", [
+            @include("drumeo.lead-gen.partials.sign-up-form-rc", [
                 "formId" => "Drumeo - Engagement - Trigger - GSOTD - Web Form",
                 "formName" => 'Getting Started On The Drums',
                 "buttonText" => "Get started for free ",
@@ -153,7 +153,7 @@
     <div class="reveal max-w-xl text-center" id="signUpModal" data-reveal data-reset-on-close="false">
         <div class="p-4">
             <h2 class="mb-2"><strong>Enter your email address<br class="inline sm:hidden"> below to get started:</strong></h2>
-            @include("drumeo.lead-gen.partials.sign-up-form-tw", [
+            @include("drumeo.lead-gen.partials.sign-up-form-rc", [
                 "stacked" => true,
                 "formId" => "Drumeo - Engagement - Trigger - GSOTD - Web Form",
                 "formName" => 'Getting Started On The Drums',
@@ -177,4 +177,10 @@
     <script defer src="{{ asset('/marketing/js/drumeo/infusionsoft-tracking.js') }}"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/unveilhooks/ls.unveilhooks.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script>
+        function onSubmit(token) {
+            document.getElementById("DrumeoEngagementTriggerGSOTDWebForm").submit();
+        }
+    </script>
 @stop
