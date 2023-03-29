b07dacaec541c58de66db6766383a0fc38b64338
peterminhlee <petereminhlee@gmail.com>
2023-03-29 16:24:44 -0700

    added unlike

--- resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistHeader.vue
+++ resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistHeader.vue
@@ -93,13 +93,24 @@
     //Handle Like
     const likePlaylist = () => {
         state.isLiked = !state.isLiked;
-        PlaylistService.likePlaylist({ "brand": props.brand, "playlist_id": props.playlist.id }, token)
-            .then((response) => {
-                console.log('successfully liked')
-            })
-            .catch(function (error) {
-                console.log('failed to like')
-            });
+        if(state.isLiked){
+            PlaylistService.likePlaylist({ "brand": props.brand, "playlist_id": props.playlist.id }, token)
+                .then((response) => {
+                    console.log('successfully liked')
+                })
+                .catch(function (error) {
+                    console.log('failed to like')
+                });
+        }
+        else {
+            PlaylistService.unlikePlaylist(props.playlist.id, token)
+                .then((response) => {
+                    console.log('successfully unliked')
+                })
+                .catch(function (error) {
+                    console.log('failed to unlike')
+                });
+        }
     }
 
     //TODO: add unlike function
