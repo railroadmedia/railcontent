773675cb9ba20374bc05004204f2a06c25c5e151
peterminhlee <petereminhlee@gmail.com>
2023-03-30 08:28:05 -0700

    button updates

--- resources/platform/assets/js/vue/components/Playlists/Modals/AddDuplicate.vue
+++ resources/platform/assets/js/vue/components/Playlists/Modals/AddDuplicate.vue
@@ -34,13 +34,13 @@ const props = defineProps({
 
             <div class="tw-pt-8 tw-flex tw-flex-col tw-items-center tw-justify-center tw-w-full">
                 <!-- Delete Lesson -->
-                <button :class="`tw-mb-2 tw-btn-primary tw-btn-small tw-text-base tw-bg-${brand} tw-px-[30px] tw-w-[218px]`"
+                <button :class="`tw-mb-2 tw-btn-primary tw-text-base tw-bg-${brand} tw-px-[30px] tw-w-[218px]`"
                     @click.prevent="() => emit('onConfirm')">
                     CONFIRM
                 </button>
                 <!-- Close Modal -->
                 <button @click="() => emit('onClose')"
-                    class="tw-btn-primary tw-btn-small tw-text-base dark:tw-text-white tw-text-[#0D0D0D] hover:tw-bg-slate-200/50 dark:hover:tw-bg-white/10 tw-w-[218px]">
+                    class="tw-btn-secondary tw-text-base dark:tw-text-white tw-text-[#0D0D0D] hover:tw-bg-slate-200/50 dark:hover:tw-bg-white/10 tw-w-[218px]">
                     CANCEL
                 </button>
             </div>
