a822b5c91fede86fa274a01c3f74da3bcef62377
peterminhlee <petereminhlee@gmail.com>
2023-03-30 09:55:54 -0700

    props name changes

--- resources/platform/assets/js/vue/components/Modal/InfoModal.vue
+++ resources/platform/assets/js/vue/components/Modal/InfoModal.vue
@@ -2,7 +2,7 @@
 import { onMounted, onUnmounted, ref } from "vue";
 import { XIcon } from "@heroicons/vue/solid";
 const isContainerCreated = ref(false);
-const props = defineProps(["modalId", "title", "selfContained", "classOverride", "overlay"]);
+const props = defineProps(["modalId", "title", "selfContained", "classOverride", "showOverlay"]);
 const emit = defineEmits(["onClose"]);
 
 const onClose = () => {
@@ -50,7 +50,7 @@ onUnmounted(() => {
         tw-items-center
         tw-justify-center
       "
-      :class="overlay && 'tw-bg-[rgba(0,0,0,0.8)] tw-z-30'"
+      :class="showOverlay && 'tw-bg-[rgba(0,0,0,0.8)] tw-z-30'"
     >
       <div
         class="
