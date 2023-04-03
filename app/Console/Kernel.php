0cd1dbfbb00e3d469c2e73127532fa6443cae098
Roxana Riza <roxana_r2000@yahoo.com>
2023-04-03 17:46:20 +0300

    MT-396: Migrate old Guitareo songs to new courses

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,6 +9,7 @@ use App\Console\Commands\AssignSongsPermissionsToProducts;
 use App\Console\Commands\CreateSongs24Jan2023;
 use App\Console\Commands\CreateSongsDecember2022;
 use App\Console\Commands\FixSongsTemp;
+use App\Console\Commands\MigrateOldGuitareoDeletedSongs;
 use App\Console\Commands\MigratePianoteSongTutorial;
 use App\Console\Commands\RemoveTemporarySongsAccessForLifetimeMembersJanuary2023;
 use App\Console\Commands\RepairUserProgressOn30DD;
@@ -69,6 +70,7 @@ class Kernel extends ConsoleKernel
         UpdateRoutinesFebruary2023::class,
         RepairUserProgressOn30DD::class,
         RepairUserProgressOnNPPSH::class,
+        MigrateOldGuitareoDeletedSongs::class,
     ];
 
     /**
