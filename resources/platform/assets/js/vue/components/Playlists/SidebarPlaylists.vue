cf48b8dbc746dd659de4a3bd373ac61ea72dae96
peterminhlee <petereminhlee@gmail.com>
2023-04-04 11:58:47 -0700

    removed hover bg and added hover underline

--- resources/platform/assets/js/vue/components/Playlists/SidebarPlaylists.vue
+++ resources/platform/assets/js/vue/components/Playlists/SidebarPlaylists.vue
@@ -118,10 +118,10 @@ onBeforeMount(()=> {
     <section>
         <div class="tw-h-[42px] tw-flex tw-items-center tw-w-full">
             <a :href="`/${brand}/playlists`" :title="[isSidebarCollapsed ? 'Playlists' : '']"
-                class="tw-text-sm tw-h-[42px] tw-flex tw-items-center tw-pl-1 tw-border-l-4 dark:hover:tw-bg-[#102230] hover:tw-bg-[#F5F5F6] tw-w-full"
+                class="tw-text-sm tw-h-[42px] tw-flex tw-items-center tw-pl-1 tw-border-l-4 tw-w-full tw-group"
                 :class="[isActivePath ? `tw-font-bold ${textColor[brand]} ${borderColor[brand]}` : 'tw-border-transparent tw-text-[#00101D] dark:tw-text-white']">
                 <musora-icon icon-name="playlist" class="tw-w-[24px] tw-mx-4" />
-                <span class="tw-transition tw-whitespace-nowrap tw-font-bold tw-text-sm tw-uppercase"
+                <span class="tw-transition tw-whitespace-nowrap tw-font-bold tw-text-sm tw-uppercase group-hover:tw-underline"
                     :class="[isSidebarCollapsed ? 'md:tw-opacity-0' : 'tw-opacity-100']">Playlists</span>
             </a>
 
