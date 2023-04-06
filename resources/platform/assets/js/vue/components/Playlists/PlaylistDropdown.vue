76bfdd5212bc66320309d9dd91fdf99c62d68cf7
calebfavor <calebfavor@gmail.com>
2023-04-06 17:45:45 +0000

    hover states on playlist dropdown

--- resources/platform/assets/js/vue/components/Playlists/PlaylistDropdown.vue
+++ resources/platform/assets/js/vue/components/Playlists/PlaylistDropdown.vue
@@ -243,7 +243,7 @@ onMounted(() => {
             <li v-for="(item, i) in dropdownOptions" :key="i" class="tw-w-full"
                 :class="{ 'tw-hidden': item.name === 'Share' && state.isPrivate }">
                 <button
-                    class="tw-flex tw-w-full tw-items-center tw-px-4 tw-py-2 tw-z-30 tw-transition-colors hover:tw-bg-[#E6E7E9]/40 dark:hover:tw-bg-black/20"
+                    class="tw-flex tw-w-full tw-items-center tw-px-4 tw-py-2 tw-z-30 tw-transition-colors dark:hover:tw-bg-[#102230] hover:tw-bg-[#F5F5F6]"
                     :class="[{'tw-pointer-events-none tw-cursor-default tw-opacity-60' : item.name === 'Re-Order' && lessonsLength === 1 }]"
                     @click.prevent="clickHandler(item.action)">
                     <template v-if="item.name === 'Private'">
