c4cd93bf952672a5f95512af3f6ad9f15afcf9ce
peterminhlee <petereminhlee@gmail.com>
2023-04-10 16:25:10 -0700

    video modal

--- resources/platform/views/partials/modals/_video-modal.blade.php
+++ resources/platform/views/partials/modals/_video-modal.blade.php
@@ -1,16 +1,3 @@
-@php
-    if(!empty($vimeo)){
-        $video = '//player.vimeo.com/video/'.$video.'?autoplay=1';
-    }
-    elseif(!empty($soundslice)){
-        if(!empty($score)){
-            $video = 'https://www.soundslice.com/scores/'.$video.'/embed/?api=1&scroll_type=2&branding=0&top_controls=1&show_chords=0&layout=3&recording_idx=1&enable_metronome=0';
-        }
-        else{
-            $video = 'https://www.soundslice.com/slices/'.$video.'/embed/?api=1&scroll_type=2&branding=0&top_controls=1&show_chords=0&layout=3&recording_idx=1&enable_metronome=0';
-        }
-    }
-@endphp
 <!-- Modal -->
 <div
     x-show="{{ $name }}"
@@ -18,29 +5,30 @@
     x-on:keydown.escape.prevent.stop="{{ $name }} = false; "
     role="dialog"
     aria-modal="true"
-    class="fixed inset-0 overflow-y-auto"
+    class="tw-fixed tw-inset-0 tw-overflow-y-auto"
 >
     <!-- Overlay -->
-    <div x-show="{{ $name }}" x-transition.opacity class="fixed inset-0 bg-black bg-opacity-80" style="z-index: 1005;"></div>
+    <div x-show="{{ $name }}" x-transition.opacity class="tw-fixed tw-inset-0 tw-bg-black tw-bg-opacity-80" style="z-index: 1005;"></div>
 
     <!-- Panel -->
     <div
         x-show="{{ $name }}" x-transition
         x-on:click="{{ $name }} = false;"
-        class="relative flex min-h-screen items-center justify-center p-4"
+        class="tw-relative tw-flex tw-min-h-screen tw-items-center tw-justify-center tw-p-4"
         style="z-index: 1006;"
     >
         <!-- Close button -->
-        <i class="fal fa-times fa-2x fixed top-16 right-2 text-white cursor-pointer text-5xl"></i>
+        <i class="fal fa-times fa-2x tw-fixed tw-top-16 tw-right-2 tw-text-white tw-cursor-pointer tw-text-5xl"></i>
         <div
             x-on:click.stop
-            class="relative w-full overflow-y-visible max-w-6xl"
+            class="tw-relative tw-w-full tw-overflow-y-visible tw-max-w-6xl"
         >
             <!-- Content -->
             <div
-                class="@if(!empty($styles)) {{ $styles }} @endif overflow-hidden rounded-xl w-full relative @if(!empty($vimeo)) aspect-16:9 @elseif(!empty($soundslice)) pb-[66vh] bg-white @endif"
+                class="@if(!empty($styles)) {{ $styles }} @endif tw-overflow-hidden tw-rounded-xl tw--full tw-relative"
+                style="padding-bottom: 56.25%;"
             >
-                <iframe class="z-10 absolute w-full h-full reset-on-close" x-bind:src="{{ $name }} && '{{ $video }}'" frameborder="0" allowfullscreen allow="autoplay" title="{{ $name }}"></iframe>
+                <iframe class="tw-z-10 tw-absolute tw-w-full tw-h-full reset-on-close" x-bind:src="{{ $name }} && '{{ $video }}'" frameborder="0" allowfullscreen allow="autoplay" title="{{ $name }}"></iframe>
             </div>
         </div>
     </div>
