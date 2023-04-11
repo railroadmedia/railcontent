ff0e9bdfb8d545bd001cd9bda47d6933c1fdb27a
Trent <trent.harms@gmail.com>
2023-04-11 16:55:15 -0700

    new bundle

--- routes/drumeo/sales.php
+++ routes/drumeo/sales.php
@@ -20,6 +20,7 @@ Route::domain('{drumeoDomain}')
     Route::get('/lifetime', [SalesController::class, 'salesUpgradeLifetime'] );
     Route::get('/festival/', [SalesController::class, 'Festival'] );
     Route::get('/anniversary', [SalesController::class, 'home'] );
+    Route::get('/faster-feet', [SalesController::class, 'fasterFeet'] );
 
     //    sales pages
     Route::get('/method', [SalesController::class, 'method'] );
