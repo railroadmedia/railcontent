ef8a5bc674c0d165dead08dbfb868c1b840ade18
robwillems <robert@musora.com>
2023-04-11 09:39:55 -0700

    Points optimization - Reduce to user/content records to single row

--- app/Modules/EventDataSynchronizer/Listeners/ContentProgressEventListener.php
+++ app/Modules/EventDataSynchronizer/Listeners/ContentProgressEventListener.php
@@ -324,9 +324,9 @@ class ContentProgressEventListener
                             'content_id' => $content['id'],
                             'progress_state' => 'completed',
                         ],
-                        str_replace('-', '_', $content['type']).'_content_completed',
+                        str_replace('-', '_', $content['type']) . '_content_completed',
                         $pointAmount,
-                        'Awarded per complete '.str_replace('-', ' ', $content['type']).'.',
+                        'Awarded per complete ' . str_replace('-', ' ', $content['type']) . '.',
                         brand()
                     );
                 } else {
@@ -349,15 +349,17 @@ class ContentProgressEventListener
     public function handleMediaPlaybackTracked(MediaPlaybackTracked $mediaPlaybackTracked)
     {
         $assignmentTypeIds = $this->mediaPlaybackRepository->getAssignmentTypeIds();
-        if (in_array($mediaPlaybackTracked->typeId, $assignmentTypeIds))  {
-            $min = $this->userMetricsService->getTotalMinutesPracticed($mediaPlaybackTracked->userId, $assignmentTypeIds);
+        if (in_array($mediaPlaybackTracked->typeId, $assignmentTypeIds)) {
+            $min = $this->userMetricsService->getTotalMinutesPracticed(
+                $mediaPlaybackTracked->userId,
+                $assignmentTypeIds
+            );
             $userBrandMinutesPracticed = user()->brand_minutes_practiced;
             $brand = config('railcontent.brand');
             $userBrandMinutesPracticed[$brand] = $min;
 
             user()->brand_minutes_practiced = $userBrandMinutesPracticed;
             user()->save();
-
         }
 
         // sound slice assignment
@@ -447,7 +449,7 @@ class ContentProgressEventListener
             $this->contentService->getContentWithExternalVideoId($mediaPlaybackTracked->mediaId)
                 ->toArray();
 
-        $lengthInSeconds = (integer)$mediaPlaybackTracked->mediaLengthInSeconds;
+        $lengthInSeconds = $mediaPlaybackTracked->mediaLengthInSeconds;
 
         foreach ($vimeoIdFields as $content) {
             $totalTimeWatched = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
@@ -456,30 +458,37 @@ class ContentProgressEventListener
                 $mediaPlaybackTracked->typeId
             );
 
-            if ($lengthInSeconds > 0 && $totalTimeWatched < $lengthInSeconds) {
-                $minutes = floor($totalTimeWatched / 60);
-                $totalAmount = 0;
-                $contentId = $content['content_id'];
-                Log::debug("handleMediaPlaybackTracked userId=$mediaPlaybackTracked->userId contentId=$contentId minutes=$minutes");
-                $start = microtime(true);
+            if ($lengthInSeconds > 0) {
+                $minutes = min(floor($totalTimeWatched / 60), $lengthInSeconds);
+                $points = $minutes * config('xp_ranks.per_minute_content_watched');
+
+                $this->userPointsService->setPoints(
+                    $mediaPlaybackTracked->userId,
+                    [
+                        'content_id' => $content['content_id'],
+                        'minutes_watched' => 'all',
+                    ],
+                    'minutes_of_content_watched2', //Change back after migration complete
+                    $points,
+                    'Awarded for every minute of video watched.',
+                    $mediaPlaybackTracked->brand
+                );
+
+                //******************************************************************************************//
+                //Remove this after migration completed
                 while ($minutes > 0) {
-                    $this->userPointsService->setPoints(
+                    $this->userPointsService->deletePoints(
                         $mediaPlaybackTracked->userId,
                         [
                             'content_id' => $content['content_id'],
                             'minutes_watched' => $minutes,
                         ],
-                        'minutes_of_content_watched',
-                        config('xp_ranks.per_minute_content_watched'),
-                        'Awarded for every minute of video watched.',
                         $mediaPlaybackTracked->brand
                     );
-
-                    $totalAmount = $totalAmount + config('xp_ranks.per_minute_content_watched');
                     $minutes--;
                 }
-                $time = microtime(true) - $start;
-                Log::debug("handleMediaPlaybackTracked finished($time s)");
+                //******************************************************************************************//
+
                 $this->userProvider->saveExperiencePoints(
                     $mediaPlaybackTracked->userId,
                     $this->userPointsService->countUserPointsPerBrand(
@@ -516,8 +525,8 @@ class ContentProgressEventListener
         $pointsPerBrand = $this->userPointsService->countUserPointsPerBrand(
             $userContentsProgressReset->userId
         );
-        foreach (config('railcontent.available_brands',[]) as $brand){
-            if(!isset($pointsPerBrand[$brand])){
+        foreach (config('railcontent.available_brands', []) as $brand) {
+            if (!isset($pointsPerBrand[$brand])) {
                 $pointsPerBrand[$brand] = 0;
             }
         }
@@ -585,9 +594,9 @@ class ContentProgressEventListener
 
             foreach ($commentLikes as $commentLike) {
                 $hashes[] = $this->userPointsService->hash([
-                                                               'comment_id' => $comment['id'],
-                                                               'comment_liker_user_id' => $commentLike['user_id'],
-                                                           ]);
+                    'comment_id' => $comment['id'],
+                    'comment_liker_user_id' => $commentLike['user_id'],
+                ]);
             }
 
             $this->userPointsService->repository()
