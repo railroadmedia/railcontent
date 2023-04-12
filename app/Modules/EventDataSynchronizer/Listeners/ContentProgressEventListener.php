23d25d6777e1d011af0fed0f4f1faabb6129bdec
robwillems <robert@musora.com>
2023-04-12 09:55:42 -0700

    Fix issue with conversion

--- app/Modules/EventDataSynchronizer/Listeners/ContentProgressEventListener.php
+++ app/Modules/EventDataSynchronizer/Listeners/ContentProgressEventListener.php
@@ -366,14 +366,14 @@ class ContentProgressEventListener
         if ($mediaPlaybackTracked->typeId == 4) {
             $maxMinutesToTrack = 600;
 
-            $totalTimeWatched = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
+            $totalTimeWatchedSeconds = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
                 $mediaPlaybackTracked->userId,
                 $mediaPlaybackTracked->mediaId,
                 $mediaPlaybackTracked->typeId
             );
 
-            if ($totalTimeWatched <= $maxMinutesToTrack) {
-                $minutes = floor($totalTimeWatched / 60);
+            if ($totalTimeWatchedSeconds <= $maxMinutesToTrack) {
+                $minutes = floor($totalTimeWatchedSeconds / 60);
                 $totalAmount = 0;
                 while ($minutes > 0) {
                     $this->userPointsService->setPoints(
@@ -407,14 +407,14 @@ class ContentProgressEventListener
         if ($mediaPlaybackTracked->typeId == 5) {
             $maxMinutesToTrack = 600;
 
-            $totalTimeWatched = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
+            $totalTimeWatchedSeconds = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
                 $mediaPlaybackTracked->userId,
                 $mediaPlaybackTracked->mediaId,
                 $mediaPlaybackTracked->typeId
             );
 
-            if ($totalTimeWatched <= $maxMinutesToTrack) {
-                $minutes = floor($totalTimeWatched / 60);
+            if ($totalTimeWatchedSeconds <= $maxMinutesToTrack) {
+                $minutes = floor($totalTimeWatchedSeconds / 60);
                 $totalAmount = 0;
                 while ($minutes > 0) {
                     $this->userPointsService->setPoints(
@@ -452,14 +452,14 @@ class ContentProgressEventListener
         $lengthInSeconds = (integer)$mediaPlaybackTracked->mediaLengthInSeconds;
 
         foreach ($vimeoIdFields as $content) {
-            $totalTimeWatched = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
+            $totalTimeWatchedSeconds = (integer)$this->mediaPlaybackRepository->sumTotalPlayed(
                 $mediaPlaybackTracked->userId,
                 $mediaPlaybackTracked->mediaId,
                 $mediaPlaybackTracked->typeId
             );
 
             if ($lengthInSeconds > 0) {
-                $minutes = min(floor($totalTimeWatched / 60), $lengthInSeconds);
+                $minutes = floor(min($totalTimeWatchedSeconds, $lengthInSeconds) / 60);
                 $points = $minutes * config('xp_ranks.per_minute_content_watched');
 
                 $this->userPointsService->setPoints(
