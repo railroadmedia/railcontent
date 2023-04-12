f60fd7533b3189a22548bf87831dc1c864aaef9b
peterminhlee <petereminhlee@gmail.com>
2023-04-12 16:12:26 -0700

    added cohort banner

--- resources/platform/views/home/index.blade.php
+++ resources/platform/views/home/index.blade.php
@@ -19,6 +19,8 @@
         >
         </header-carousel>
 
+        <cohort-banner></cohort-banner>
+
         {{-- Invite Email Message --}}
         @if(session()->has('email-invite-message'))
             <div class="tw-container tw-mx-auto tw-rounded tw-px-4 md:tw-px-8 tw-my-4">
@@ -153,4 +155,9 @@
 @endsection
 
 
-
+<script>
+    import CohortBanner from "../../assets/js/vue/components/CohortBanner/CohortBanner";
+    export default {
+        components: {CohortBanner}
+    }
+</script>
