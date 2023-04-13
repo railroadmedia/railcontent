17f089c92ab825080d69a4e6d885a8a398fc6b96
robwillems <robert@musora.com>
2023-04-13 10:20:38 -0700

    Add error logging

--- app/Modules/EventDataSynchronizer/Jobs/MigrateMinutesWatchedPointsJob.php
+++ app/Modules/EventDataSynchronizer/Jobs/MigrateMinutesWatchedPointsJob.php
@@ -5,6 +5,7 @@ namespace App\Modules\EventDataSynchronizer\Jobs;
 use App\Console\Commands\Infrastructure\Job;
 use App\Modules\Points\Models\ExperiencePoints;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class MigrateMinutesWatchedPointsJob extends Job
 {
@@ -15,34 +16,39 @@ class MigrateMinutesWatchedPointsJob extends Job
                 10000
             )->get();
         foreach ($points as $point) {
-            /** @var ExperiencePoints $point */
-            $unserialized = unserialize($point->trigger_hash_data);
-            $contentId = $unserialized["content_id"];
-            $data = serialize([
-                'content_id' => $contentId,
-                'minutes_watched' => 'all'
-            ]);
-            $hash = md5($data);
+            try {
+                /** @var ExperiencePoints $point */
+                $unserialized = unserialize($point->trigger_hash_data);
+                $contentId = $unserialized["content_id"];
+                $data = serialize([
+                    'content_id' => $contentId,
+                    'minutes_watched' => 'all'
+                ]);
+                $hash = md5($data);
 
-            /** @var ExperiencePoints $existingPoint */
-            $existingPoint = ExperiencePoints::query()
-                ->where('user_id', '=', $point->user_id)
-                ->where('trigger_name', '=', 'minutes_of_content_watched_v2')
-                ->where('trigger_hash', '=', $hash)->first();
+                /** @var ExperiencePoints $existingPoint */
+                $existingPoint = ExperiencePoints::query()
+                    ->where('user_id', '=', $point->user_id)
+                    ->where('trigger_name', '=', 'minutes_of_content_watched_v2')
+                    ->where('trigger_hash', '=', $hash)->first();
 
-            if ($existingPoint == null) {
-                $point->trigger_name = 'minutes_of_content_watched_v2';
-                $point->trigger_hash = $hash;
-                $point->trigger_hash_data = $data;
-                $point->points_description = null;
-                $point->save();
-            } else {
-                if ($point->trigger_name != 'minutes_of_content_watched2') {
-                    $existingPoint->points += $point->points;
-                    $existingPoint->updated_at = Carbon::now();
-                    $existingPoint->save();
+                if ($existingPoint == null) {
+                    $point->trigger_name = 'minutes_of_content_watched_v2';
+                    $point->trigger_hash = $hash;
+                    $point->trigger_hash_data = $data;
+                    $point->points_description = null;
+                    $point->save();
+                } else {
+                    if ($point->trigger_name != 'minutes_of_content_watched2') {
+                        $existingPoint->points += $point->points;
+                        $existingPoint->updated_at = Carbon::now();
+                        $existingPoint->save();
+                    }
+                    $point->delete();
                 }
-                $point->delete();
+            } catch (\Throwable $e) {
+                Log::error("Error migrating point $point->id");
+                Log::error($e);
             }
         }
     }
