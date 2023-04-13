ea1d88cb48452cf5bbf39414c4a54e49e664c70c
Trent <trent.harms@gmail.com>
2023-04-13 10:12:55 -0700

    hiding holida logos

--- resources/marketing/views/_partials/layout/holiday/shop-sidebar-banner.blade.php
+++ resources/marketing/views/_partials/layout/holiday/shop-sidebar-banner.blade.php
@@ -1,5 +1,5 @@
-<div class="promo-tab hidden lg:block text-center py-2 px-3" style="position:relative; background: #000 url(https://dpwjbsxqtam5n.cloudfront.net/promos/christmas/snow-dark.gif) center center/250px;box-shadow: 0 0 10px inset #000;">
-    @include($theme.'._partials.holiday-logo',[
-        'styles' => 'w-auto max-h-12'
-    ])
-</div>
+{{--<div class="promo-tab hidden lg:block text-center py-2 px-3" style="position:relative; background: #000 url(https://dpwjbsxqtam5n.cloudfront.net/promos/christmas/snow-dark.gif) center center/250px;box-shadow: 0 0 10px inset #000;">--}}
+{{--    @include($theme.'._partials.holiday-logo',[--}}
+{{--        'styles' => 'w-auto max-h-12'--}}
+{{--    ])--}}
+{{--</div>--}}
