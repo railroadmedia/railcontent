8a8c233f77f278bf6df520e0895e381a2b4e51eb
peterminhlee <petereminhlee@gmail.com>
2023-04-17 10:58:51 -0700

    added comments

--- resources/platform/views/content/cohort-template.blade.php
+++ resources/platform/views/content/cohort-template.blade.php
@@ -19,10 +19,15 @@
             <div class="tw-max-w-5xl tw-mx-auto">
                 <div class="md:tw-flex tw-items-center tw-gap-6 tw-mb-12 tw-text-center md:tw-text-left">
                     <div class="md:tw-flex-1">
+                        {{--  Header logo  --}}
                         <img class="tw-h-14 sm:tw-h-18 lg:tw-h-20 tw-mb-2 tw-inline-block " alt="header logo" src="https://www.musora.com/musora-cdn/image/width=440,quality=85/{{ $cohort['light_mode_logo']??'' }}" />
+                        {{--  Headline  --}}
                         <h1 class="tw-font-extrabold tw-text-3xl lg:tw-text-4xl">{{ $cohort['headline'] }}</h1>
+                        {{--  Subheadline  --}}
                         <h2 class="tw-font-normal tw-text-2xl lg:tw-text-3xl">{{ $cohort['subheadline'] }}</h2>
+                        {{--  Header description  --}}
                         <p class="tw-font-bold tw-mt-4 lg:tw-mt-6 tw-mb-7 lg:tw-mb-9">{{ $cohort['header_description'] }}</p>
+                        {{--  Mobile Header Image --}}
                         <div class="tw-relative md:tw-hidden">
                             <img
                                 class="tw-rounded-xl tw-mb-6 tw-transition-opacity tw-opacity-0"
@@ -34,6 +39,7 @@
                             <div class="tw-absolute tw-bottom-4 tw-left-4 tw-bg-white tw-rounded-full tw-uppercase tw-font-bebas-neue tw-px-5 tw-py-1 tw-flex tw-items-center tw-cursor-pointer" x-on:click="trailer = true"><i class="fas fa-play tw-mr-2" aria-hidden="true"></i> <div class="tw-mt-1">Watch Trailer</div></div>
                         </div>
                         <div class="md:tw-flex @if($hasProduct) md:tw-items-start @else md:tw-items-center @endif">
+                            {{--  Buttons  --}}
                             @if($enrollmentClosed)
                                 <span class="tw-btn-primary tw-bg-[#65656B] tw-w-full md:tw-w-1/2 md:tw-mr-2 tw-text-white tw-cursor-default">Enrollment Closed</span>
                             @elseif($hasProduct)
@@ -44,7 +50,6 @@
                             @else
                                 <button onclick="enroll('{{ $registerButtonUrl }}');" class="tw-btn-primary tw-bg-{{ $brand }} tw-w-full md:tw-w-1/2 md:tw-mr-2 tw-max-w-[415px] tw-mb-5 md:tw-mb-0 hover:tw-bg-{{ $brand }}-600">Enroll Now</button>
                             @endif
-
                             <div class="md:tw-w-1/2 tw-flex tw-items-center tw-justify-center md:tw-justify-start @if($hasProduct) md:tw-mt-2 @endif">
                                 <img
                                     class="tw-h-8 md:tw-h-6 tw-mr-1 tw-transition-opacity tw-opacity-0"
@@ -67,6 +72,7 @@
                             </div>
                         </div>
                     </div>
+                    {{--  Tablet/Desktop Header Image  --}}
                     <div class="md:tw-flex-1 tw-hidden md:tw-block tw-relative">
                         <img
                             class="tw-rounded-xl tw-transition-opacity tw-opacity-0"
@@ -84,6 +90,7 @@
                         <h4 class="tw-px-3 lg:tw-px-5"><strong>{{date('F dS', strtotime($cohort['enrollment_start_date']))}}</strong></h4>
                         <hr class="tw-border-gray-300 tw-my-4 sm:tw-my-2 lg:tw-my-4">
                         <p class="tw-text-sm tw-px-3 lg:tw-px-5">
+                            {{--  Countdown  --}}
                              <span x-cloak x-data="timer()" x-init="countdown()">
                                  <span x-cloak x-show="timeLeft > 0">Enrollment closes in </span>
                                  <span class="tw-text-pianote">
@@ -93,6 +100,7 @@
                              </span>
                         </p>
                     </div>
+                    {{--  Icons/Title/Copies  --}}
                     <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-evenly tw-w-full sm:tw-w-auto sm:tw-flex-grow tw-py-4 sm:tw-py-3 lg:tw-py-4 tw-text-left sm:tw-text-center">
                         <div class="tw-flex sm:tw-block tw-w-full sm:tw-w-1/3 tw-px-4 sm:tw-px-3 tw-mb-4 sm:tw-mb-0">
                             <i class="far fa-fw {{ $cohort['icon1'] }} tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-2xl" aria-hidden="true"></i>
@@ -116,13 +124,16 @@
 
         <section class="tw-bg-white tw-pt-7">
             <div class="tw-max-w-5xl tw-mx-auto tw-px-4 md:tw-px-10">
+                {{--  Body title  --}}
                 <h3 class="tw-font-extrabold tw-text-center tw-mb-7">{{ $cohort['body_title'] }}</h3>
+                {{--  Body top description  --}}
                 <p class="md:tw-px-10 tw-mb-52">{!! $cohort['body_top_description'] !!}</p>
             </div>
         </section>
         <section class="tw-bg-[#F1F7FE] tw-pb-7 ">
             <div class="tw-max-w-5xl tw-mx-auto tw-px-4 md:tw-px-10 -tw-mt-40">
                 <div class="tw-relative tw-cursor-pointer tw-mb-10" x-on:click="trailer = true">
+                    {{--  Body Image  --}}
                     <img
                         class="tw-rounded-xl tw-transition-opacity tw-opacity-0"
                         src="https://www.musora.com/musora-cdn/image/width=1200,quality=85/{{ $cohort['body_image_url'] }}"
@@ -134,6 +145,7 @@
                         <i class="fa-regular fa-play tw-z-10 tw-border-white tw-border-4 tw-text-white tw-py-3 md:tw-py-6 tw-pl-5 md:tw-pl-8 tw-pr-4 md:tw-pr-6 tw-rounded-full tw-text-3xl md:tw-text-5xl tw-bg-[rgba(0,0,0,0.2)]"></i>
                     </div>
                 </div>
+                {{--  Body logo  --}}
                 <img
                     class="tw-h-10 sm:tw-h-16 tw-mb-8 tw-mx-auto tw-transition-opacity tw-opacity-0"
                     alt="just play logo"
@@ -141,35 +153,12 @@
                     loading="lazy"
                     onload="this.classList.remove('tw-opacity-0')"
                 />
+                {{--  Body bottom description  --}}
                 <p class="tw-text-center tw-px-6">{!! $cohort['body_bottom_description'] !!}</p>
             </div>
         </section>
 
-        @php
-            $faqs = [
-                [
-                    "title" => "What is Pianote?",
-                    "desc" => 'Pianote is an online platform that offers an organized piano lesson curriculum, artist courses on popular topics, 1000+ songs transcribed note-for-note, and a supportive global community of students and teachers.',
-                ],
-                [
-                    "title" => "Is Pianote good for beginners?",
-                    "desc" => 'Yes! You’ll always know what to practice with step-by-step video lessons – plus have fun applying your new skills to your favorite songs, sorted by skill level. And if you ever need help, you’ll have unlimited personal support through live Q&A sessions, student reviews, and a helpful community. ',
-                ],
-                [
-                    "title" => "Does Pianote have anything for advanced pianists?",
-                    "desc" => 'Pianote is the perfect companion for advanced pianists, giving you access to artist courses so you can gain insights and inspiration from professionals. Plus, you’ll get note-for-note sheet music for thousands of songs and practical playback tools, so you can take on any new challenge with confidence.',
-                ],
-                [
-                    "title" => "Am I too old to learn piano?",
-                    "desc" => 'You’re never too old to learn piano. Pianote has a community of students of all ages, from all around the world. Whether you’re 40, 50, 60, 70, or beyond – you’ll connect with aspiring pianists just like you who are learning and applying their skills to music.',
-                ],
-                [
-                    "title" => "Do I need to be tech-savvy to learn through your app?",
-                    "desc" => 'Not at all! Technology is here to make your life easier, and Pianote is designed to help you find lessons and songs easily. And if you ever get stuck, you can contact our Student Experience team by phone or email for prompt and helpful support.',
-                ],
-            ]
-        @endphp
-
+        {{--  Dropdown  --}}
         <section class="tw-bg-white tw-py-7">
             <div class="tw-max-w-4xl tw-mx-auto tw-pl-6 tw-pr-4">
                 <h3 class="tw-font-extrabold tw-text-center tw-mb-7">{{ $cohort['dropdown_title'] }}</h3>
@@ -180,9 +169,12 @@
                         "desc" => $dropdown['description'],
                     ])
                 @endforeach
+                {{--  Bottom title  --}}
                 <h3 class="tw-font-extrabold tw-text-center tw-mt-10">{{ $cohort['bottom_title'] }}</h3>
+                {{--  Bottom description  --}}
                 <p class="tw-font-bold tw-text-center tw-mt-4 tw-mb-6">{{ $cohort['bottom_description'] }}</p>
                 <div class="tw-max-w-[415px] md:tw-max-w-xl tw-mx-auto tw-flex tw-flex-col md:tw-flex-row md:tw-gap-2 tw-mb-4 tw-justify-center">
+                    {{--  Buttons  --}}
                     @if($enrollmentClosed)
                         <span class="tw-btn-primary tw-bg-[#65656B] tw-w-full tw-text-white tw-cursor-default">Enrollment Closed</span>
                     @elseif($hasProduct)
@@ -215,6 +207,7 @@
             </div>
         </section>
 
+        {{--  Signup modal  --}}
         <div id="signupModal" class="modal">
             <div class="tw-flex tw-justify-center tw-items-center">
                 <div class="tw-max-w-xl tw-bg-white dark:tw-bg-[#081825] tw-text-center dark:tw-text-white tw-rounded-xl tw-px-8 tw-py-10 dark:tw-border-[#445F74] dark:tw-border">
