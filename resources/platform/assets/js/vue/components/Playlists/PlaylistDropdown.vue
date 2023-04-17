ee89fe999b8ab23e383e14c8748f16d2171d0505
peterminhlee <petereminhlee@gmail.com>
2023-04-17 13:26:32 -0700

    updatedPinned when pinning/unpining playlist

--- resources/platform/assets/js/vue/components/Playlists/PlaylistDropdown.vue
+++ resources/platform/assets/js/vue/components/Playlists/PlaylistDropdown.vue
@@ -12,7 +12,7 @@ const token = inject('csrf_token');
 const playlistsStore = usePlaylistsStore();
 
 //Emits
-const emit = defineEmits(['closeDropdown', 'pinItem', 'makePublic']);
+const emit = defineEmits(['closeDropdown', 'pinItem', 'makePublic', 'updatePinned']);
 
 //-----------Props-----------//
 const props = defineProps({
@@ -119,6 +119,7 @@ const pinPlaylist = () => {
                             icon: 'playlist',
                             text: `'${props.data.name}' has been pinned within the sidebar.`
                         })
+                        emit('updatePinned', true);
                     }
                 })
                 .catch(function (error) {
@@ -160,6 +161,7 @@ const pinPlaylist = () => {
                     })
                 }
             });
+        emit('updatePinned', false);
     }
     //close after click
     emit('closeDropdown');
