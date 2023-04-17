0cb774c7611cf185234097b52bd71f68b4cbf30f
peterminhlee <petereminhlee@gmail.com>
2023-04-17 12:02:34 -0700

    added prefix

--- resources/platform/views/partials/_question-dropdown.blade.php
+++ resources/platform/views/partials/_question-dropdown.blade.php
@@ -10,7 +10,7 @@
             >
                 <h5
                     class="tw-leading-tight sm:tw-leading-loose tw-font-black tw-relative"
-                    x-bind:class="open && 'mb-2'"
+                    x-bind:class="open && 'tw-mb-2'"
                 >
                     @if(!empty($num))<span class="tw-text-white tw-rounded-full tw-py-1 tw-px-2 md:tw-px-2.5 tw-text-xs md:tw-text-sm tw-absolute -tw-left-11 md:-tw-left-16 -tw-top-0.5 md:tw-top-0.5 tw-min-w-0" x-bind:class="open ? 'tw-bg-{{$brand}}' : 'tw-bg-[#838C98]'">{{$num}}</span>@endif
                     {!! $title !!}
