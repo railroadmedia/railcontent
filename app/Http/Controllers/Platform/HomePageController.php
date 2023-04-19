cb852a47f6e0fd47a4718f52b6c3cd93cceba9a7
Roxana Riza <roxana_r2000@yahoo.com>
2023-04-19 20:18:06 +0300

    Do not display Close and Continue buttons for unreleased lessons

--- app/Http/Controllers/Platform/HomePageController.php
+++ app/Http/Controllers/Platform/HomePageController.php
@@ -313,6 +313,10 @@ class HomePageController extends BaseController
                     $cohortBanner['title'] = $nextLesson->fetch('title');
                     $cohortBanner['thumbnail'] = $nextLesson->fetch('data.thumbnail_url');
                     $cohortBanner['continue_visible'] = true;
+                    if(Carbon::parse($nextLesson->fetch('published_on')) > Carbon::now()){
+                        $cohortBanner['continue_visible'] = false;
+                        $cohortBanner['close_visible'] = false;
+                    }
                 }
                 if ($content['completed']) {
                     $cohortBanner['completed'] = true;
