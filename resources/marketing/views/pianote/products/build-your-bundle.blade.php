8a0b71547bb6449c8edb525c424438f653d26739
Trent <trent.harms@gmail.com>
2023-04-18 17:05:59 -0700

    fixing bundle

--- resources/marketing/views/pianote/products/build-your-bundle.blade.php
+++ resources/marketing/views/pianote/products/build-your-bundle.blade.php
@@ -1,13 +1,13 @@
 @extends('pianote._partials.global-layout')
 
 @section('global-head')
-    <title>The Beautiful Beginner Piano Bundle</title>
-    <meta property="og:title" content="The Beautiful Beginner Piano Bundle">
+    <title>Build Your Bundle</title>
+    <meta property="og:title" content="Build Your Bundle">
     <meta property="og:url" content="https://www.pianote.com/{{ Request::path() }}">
 
-    <meta name="description" content="Because starting out should sound this good.">
-    <meta property="og:description" content="Because starting out should sound this good.">
-    <meta property="og:image" content="https://www.musora.com/musora-cdn/image/width=1200,quality=85/https://pianote.s3.amazonaws.com/shop/products/beautiful-beginner-bundle/banner.jpg" style="display: none;">
+    <meta name="description" content="Build your own bundle and save up to 50% on Pianote books!">
+    <meta property="og:description" content="Build your own bundle and save up to 50% on Pianote books!">
+    <meta property="og:image" content="https://www.musora.com/musora-cdn/image/width=1200,quality=85/https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/books.png" style="display: none;">
 
     <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
     <link href="{{ asset('/marketing/css/tailwind-helpers.css') }}" rel="stylesheet">
@@ -23,69 +23,6 @@
             opacity: 1;
             transition: opacity 300ms;
         }
-
-        .tooltip {
-            position: absolute;
-        }
-        .tooltip:after, .tooltip:before {
-            position: absolute;
-            transform: translate(-50%, 0);
-            height: auto;
-            max-height: 0;
-            visibility: hidden;
-            opacity: 0;
-            transition: all .3s;
-            overflow: hidden;
-            font-size: 14px;
-        }
-        .tooltip:before {
-            z-index: 100;
-            content: "";
-            bottom: 23px;
-            left: 50%;
-            border-right: 7px transparent solid;
-            border-left: 7px transparent solid;
-            border-top: 7px solid #fff;
-        }
-        .tooltip:after {
-            padding: 5px 8px;
-            content: attr(tip);
-            font-size: 14px;
-            text-align: left;
-            color: #000;
-            width: 220px;
-            border-radius: 8px;
-            background: #fff;
-            box-shadow: 0 0 15px #000;
-            bottom: 30px;
-            left: -300%;
-        }
-        .tooltip:hover, .tooltip:active, .tooltip:focus {
-            z-index: 100;
-        }
-        .tooltip:hover:after, .tooltip:hover:before, .tooltip:active:after, .tooltip:active:before, .tooltip:focus:after, .tooltip:focus:before {
-            max-height: 1000px;
-            visibility: visible;
-            opacity: 1;
-            display: block;
-        }
-
-        .content-section.customize .horizontal-bonuses .bonus-wrap .flip-div.special {
-            max-width:240px;
-            padding-bottom: 51%;
-        }
-        @media (min-width: 768px) {
-            .content-section.customize .horizontal-bonuses .bonus-wrap .flip-div.special {
-                max-width:370px;
-                padding-bottom: 35%;
-            }
-        }
-        @media (min-width: 1024px) {
-            .content-section.customize .horizontal-bonuses .bonus-wrap .flip-div.special {
-                max-width:400px;
-                padding-bottom: 30%;
-            }
-        }
     </style>
 @stop
 
@@ -95,77 +32,82 @@
         "scrollToJoin" => true
     ])
 
-    <section class="content-section text-center upgrade-video-header text-black" style="background-color:#EFF7FF;">
+    <section class="content-section text-center upgrade-video-header text-black" style="background-color:#EFF7FF;overflow:visible">
         <div class="container mx-auto">
-            <h1 data-aos="fade-down" class="font-bebas leading-none text-5xl md:text-7xl mb-1" style="text-shadow: 2px 3px #f61a30;"><strong>BUILD YOUR BUNDLE</strong></h1>
+            <img class="h-20 mb-2" src="https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/books-bundle-logo.png">
             <h6 class="leading-tight">Build your own bundle and save up to 50% on Pianote books! Simply select the books <br class="hidden sm:inline">
                     you want at the bottom of this page and the discount will automatically be applied.</h6>
-            <a href="#customize-anchor" class="join smaller pianote anchor-slide my-5 sm:my-10">START BUILDING MY BUNDLE</a>
-{{--            <div class="w-full mx-auto px-4" style="max-width:920px;">--}}
-{{--                <img--}}
-{{--                    class="rounded-xl"--}}
-{{--                    src="https://www.musora.com/musora-cdn/image/width=1300,quality=85/https://pianote.s3.amazonaws.com/shop/products/beautiful-beginner-bundle/banner.jpg"--}}
-{{--                    alt="Video thumbnail"--}}
-{{--                    loading="lazy"--}}
-{{--                    onload="this.classList.remove('opacity-0')"--}}
-{{--                />--}}
-{{--            </div>--}}
+            <a href="#customize-anchor" class="join smaller pianote anchor-slide mt-7 mb-5 sm:mb-10">START BUILDING MY BUNDLE</a>
+            <div class="w-full mx-auto px-4 max-w-2xl">
+                <img
+                    class="relative z-10 -mb-32"
+                    src="https://www.musora.com/musora-cdn/image/width=1300,quality=85/https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/books.png"
+                    alt="Video thumbnail"
+                    loading="lazy"
+                    onload="this.classList.remove('opacity-0')"
+                />
+            </div>
         </div>
     </section>
     <section class="content-section text-center upgrade-video-header bg-white text-black">
-        <div class="container mx-auto max-w-5xl">
-            <div class="text-left">
-                <h3><strong>Piano Chords & Scales The Ultimate Guide</strong></h3>
-                <p class="leading-tight mt-2">Master Every Chord. Every Scale. In Every Key.
-                    <br><br>
-                    This book will help you learn every chord shape, chord variation, and scale in EVERY key.
-                    <br><br>
-                    It’s the ultimate guide to mastering the building blocks of music on the piano.
-                    <br><br>
-                    The handy tabs on the side will make it easy to look up any key signature and quickly find all the different scales and chords you need when it comes time to practice or learn a new song.</p>
+        <div class="container mx-auto max-w-6xl">
+            <div class="text-left flex flex-wrap items-center mb-20">
+                <div class="w-1/2 pr-10">
+                    <h3><strong>Piano Chords & Scales The Ultimate Guide</strong></h3>
+                    <p class="leading-normal mt-2">Master Every Chord. Every Scale. In Every Key.
+                        <br><br>
+                        This book will help you learn every chord shape, chord variation, and scale in EVERY key.
+                        <br><br>
+                        It’s the ultimate guide to mastering the building blocks of music on the piano.
+                        <br><br>
+                        The handy tabs on the side will make it easy to look up any key signature and quickly find all the different scales and chords you need when it comes time to practice or learn a new song.</p>
+                </div>
+                <img class="w-1/2" src="https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/chords-scales-collage.png">
             </div>
-            <div class="text-right my-10">
-                <h3><strong>Classical Piano Pieces (You Can Actually Play)</strong></h3>
-                <p class="leading-tight mt-2">Let’s be honest…
-                    <br><br>
-                    Classical piano can seem a little elitist, even snobby. And that’s a shame. Because classical music is so beautiful.
-                    <br><br>
-                    So we’re out to change that reputation.
-                    <br><br>
-                    Welcome to the world of classical piano music (you can actually play)! This book is your gateway to famous composers, stunning piano pieces, and an entirely new and rewarding experience on the piano.
-                    <br><br>
-                    It’s your repertoire of beautiful classical pieces that you can actually play (and that people will want to hear)!
-                    <br><br>
-                    <strong><em>Here are some of our favorites:</em></strong><br>
-                    • Ukrainian Folk Song by Ludwig van Beethoven<br>
-                    • Minuet in F Major by Wolfgang Amadeus Mozart<br>
-                    • Prelude in C Major by Johann Sebastian Bach<br>
-                    • Sonatina in B-flat Major by George Frideric Handel<br>
-                    • Waltz in A Minor by Frédéric Chopin<br>
-                    And so many more!
-                </p>
+            <div class="text-right flex flex-wrap items-center mb-20">
+                <img class="w-1/2" src="https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/classical-collage.png">
+                <div class="w-1/2 pl-10">
+                    <h3><strong>Classical Piano Pieces (You Can Actually Play)</strong></h3>
+                    <p class="leading-normal mt-2">Let’s be honest…
+                        <br><br>
+                        Classical piano can seem a little elitist, even snobby. And that’s a shame. Because classical music is so beautiful.
+                        <br><br>
+                        So we’re out to change that reputation.
+                        <br><br>
+                        Welcome to the world of classical piano music (you can actually play)! This book is your gateway to famous composers, stunning piano pieces, and an entirely new and rewarding experience on the piano.
+                        <br><br>
+                        It’s your repertoire of beautiful classical pieces that you can actually play (and that people will want to hear)!
+                        <br><br>
+                        <strong><em>Here are some of our favorites:</em></strong><br>
+                        • Ukrainian Folk Song by Ludwig van Beethoven<br>
+                        • Minuet in F Major by Wolfgang Amadeus Mozart<br>
+                        • Prelude in C Major by Johann Sebastian Bach<br>
+                        • Sonatina in B-flat Major by George Frideric Handel<br>
+                        • Waltz in A Minor by Frédéric Chopin<br>
+                        And so many more!
+                    </p>
+                </div>
             </div>
-            <div class="text-left">
-                <h3><strong>The Pianote Practice Planner</strong></h3>
-                <p class="leading-tight mt-2">They say practice makes perfect.
-                    <br><br>
-                    It’s a cliche – but it’s not entirely true. Because if you’re not practicing the RIGHT things – the RIGHT way....
-                    <br><br>
-                    You won’t be perfect.
-                    <br><br>
-                    Worse – you could be wasting your time.
-                    <br><br>
-                    The new Pianote Practice Planner is your written guide to making every practice perfect, so you get the results you deserve.
-                    <br><br>
-                    This is Lisa Witt’s personal practice guide. Written by her, exclusively for piano players.
-                </p>
+            <div class="text-left flex flex-wrap items-center">
+                <div class="w-1/2 pr-10">
+                    <h3><strong>The Pianote Practice Planner</strong></h3>
+                    <p class="leading-normal mt-2">They say practice makes perfect.
+                        <br><br>
+                        It’s a cliche – but it’s not entirely true. Because if you’re not practicing the RIGHT things – the RIGHT way....
+                        <br><br>
+                        You won’t be perfect.
+                        <br><br>
+                        Worse – you could be wasting your time.
+                        <br><br>
+                        The new Pianote Practice Planner is your written guide to making every practice perfect, so you get the results you deserve.
+                        <br><br>
+                        This is Lisa Witt’s personal practice guide. Written by her, exclusively for piano players.
+                    </p>
+                </div>
+                <img class="w-1/2" src="https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/planner-collage.png">
             </div>
         </div>
     </section>
-
-
-
-
     <div id="customize-anchor" class="anchor anchor-slide"></div>
     <section
         class="content-section text-center customize relative z-50 bg-top bg-no-repeat lazyload"
@@ -176,8 +118,7 @@
         }"
     >
         <div class="container mx-auto max-w-6xl relative z-50">
-            <h1 data-aos="fade-down" class="font-bebas leading-none text-5xl md:text-7xl" style="text-shadow: 2px 3px #f61a30;"><strong>BUILD YOUR BUNDLE</strong></h1>
-            {{--<h2 data-aos-once="true" data-aos="fade-up" class="my-3 md:my-5 leading-normal"><strong>Improve your drumming for<br> just <span class="text-drumeo">${{ round((Prices::$plusSubscriptionAnnual / 12), 2) }}</span> per month.</strong></h2>--}}
+            <img class="h-20" src="https://d2vyvo0tyx8ig5.cloudfront.net/products/build-your-bundle/books-bundle-logo.png">
             <h5 class="mt-1 sm:mt-3 mb-6" style="line-height: 1.4em;"><strong>Select the books that you would <br class="inline lg:hidden"> like to add to your bundle below.</strong><br>
                 <em class="text-coaches">(the more books you add – the more you’ll save!)</em>
             </h5>
