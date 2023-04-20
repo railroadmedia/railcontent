c642db6ecec5584a6d5cc20d1241d2b47924da0a
robwillems <robert@musora.com>
2023-04-20 16:46:27 -0700

    Resync points job

---
+++ app/Modules/EventDataSynchronizer/Console/Commands/SyncPoints.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Modules\EventDataSynchronizer\Console\Commands;
+
+use App\Console\Commands\Infrastructure\Command;
+use App\Modules\EventDataSynchronizer\Jobs\SyncPointsJob;
+
+class SyncPoints extends Command
+{
+    protected $signature = 'points:sync';
+
+    public function handle()
+    {
+        $this->runChainQuery(function (int $skip, int $take) {
+            return new SyncPointsJob($skip, $take);
+        }, chunks: 1000);
+    }
+}
