f11d348d963bc90a4912d60822880bf8c9ad144f
peterminhlee <petereminhlee@gmail.com>
2023-04-21 12:12:30 -0700

    made icons static

--- resources/platform/views/content/cohort-template.blade.php
+++ resources/platform/views/content/cohort-template.blade.php
@@ -103,38 +103,38 @@
                     {{--  Icons/Title/Copies  --}}
                     <div class="tw-flex tw-flex-wrap sm:tw-flex-nowrap tw-items-center tw-justify-evenly tw-w-full sm:tw-w-auto sm:tw-flex-grow tw-py-4 sm:tw-py-3 lg:tw-py-4 tw-text-left sm:tw-text-center">
                         <div class="tw-flex sm:tw-block tw-w-full sm:tw-w-1/3 tw-px-4 sm:tw-px-3 tw-mb-4 sm:tw-mb-0">
-                            <img
-                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"
-                                alt="icon 1"
-                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon1_url'] }}"
-                                loading="lazy"
-                                onload="this.classList.remove('tw-opacity-0')"
-                            />
-{{--                            <i class="far fa-fw {{ $cohort['icon1'] }} tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-2xl" aria-hidden="true"></i>--}}
+                            <i class="far fa-fw fa-calendar-day tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-3xl sm:tw-mb-1" aria-hidden="true"></i>
+{{--                            <img--}}
+{{--                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"--}}
+{{--                                alt="icon 1"--}}
+{{--                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon1_url'] }}"--}}
+{{--                                loading="lazy"--}}
+{{--                                onload="this.classList.remove('tw-opacity-0')"--}}
+{{--                            />--}}
                             <p class="tw-leading-tight tw-mx-0"><strong class="tw-font-black">{{ $cohort['icon1_title'] }}</strong><br>
                                 <span class="tw-text-sm">{!!$cohort['icon1_copy']!!}</span></p>
                         </div>
                         <div class="tw-flex sm:tw-block tw-w-full sm:tw-w-1/3 tw-px-4 sm:tw-px-3 tw-mb-4 sm:tw-mb-0">
-{{--                            <i class="far fa-fw {{ $cohort['icon2'] }} tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-2xl" aria-hidden="true"></i>--}}
-                            <img
-                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"
-                                alt="icon 2"
-                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon2_url'] }}"
-                                loading="lazy"
-                                onload="this.classList.remove('tw-opacity-0')"
-                            />
+                            <i class="far fa-fw fa-clock tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-3xl sm:tw-mb-1" aria-hidden="true"></i>
+{{--                            <img--}}
+{{--                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"--}}
+{{--                                alt="icon 2"--}}
+{{--                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon2_url'] }}"--}}
+{{--                                loading="lazy"--}}
+{{--                                onload="this.classList.remove('tw-opacity-0')"--}}
+{{--                            />--}}
                             <p class="tw-leading-tight tw-mx-0"><strong class="tw-font-black">{{ $cohort['icon2_title'] }}</strong><br>
                                 <span class="tw-text-sm">{{ $cohort['icon2_copy'] }}</span></p>
                         </div>
                         <div class="tw-flex sm:tw-block tw-w-full sm:tw-w-1/3 tw-px-4 sm:tw-px-3" >
-{{--                            <i class="far fa-fw {{ $cohort['icon3'] }} tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-2xl" aria-hidden="true"></i>--}}
-                            <img
-                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"
-                                alt="icon 3"
-                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon3_url'] }}"
-                                loading="lazy"
-                                onload="this.classList.remove('tw-opacity-0')"
-                            />
+                            <i class="far fa-fw fa-trophy tw-mr-3 sm:tw-mr-0 tw-text-{{ $brand }} tw-text-3xl sm:tw-mb-1" aria-hidden="true"></i>
+{{--                            <img--}}
+{{--                                class="tw-mr-3 tw-h-[30px] sm:tw-mb-1 sm:tw-mx-auto"--}}
+{{--                                alt="icon 3"--}}
+{{--                                src="https://www.musora.com/musora-cdn/image/width=60,quality=85/{{ $cohort['icon3_url'] }}"--}}
+{{--                                loading="lazy"--}}
+{{--                                onload="this.classList.remove('tw-opacity-0')"--}}
+{{--                            />--}}
                             <p class="tw-leading-tight tw-mx-0"><strong class="tw-font-black">{!! $cohort['icon3_title'] !!}</strong><br>
                                 <span class="tw-text-sm">{!! $cohort['icon3_copy'] !!}</span></p>
                         </div>
