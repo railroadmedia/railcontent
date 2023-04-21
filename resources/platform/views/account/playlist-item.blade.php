2374f31afa251a499358934c7b52bcb830695788
calebfavor <calebfavor@gmail.com>
2023-04-21 16:35:50 +0000

    playlist link on cue header

--- resources/platform/views/account/playlist-item.blade.php
+++ resources/platform/views/account/playlist-item.blade.php
@@ -62,7 +62,7 @@
         :playlist-item-id="{{ $playlistItem['id'] }}"
         :start-second="{{ $playlistItem['start_second'] ?? 0 }}"
         :end-second="{{ $playlistItem['end_second'] ?? $lessonContent->fetch('fields.video.fields.length_in_seconds', 0) }}"
-        user-access-level="{{ user()->access_level }}">
+        user-access-level="{{ user()->access_level }}" playlist-url="{{ $playlist['url'] }}">
     </playlist-playback-wrapper>
 @endsection
 
