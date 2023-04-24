523430bed692a54a3e72a5c11309094ffc4470a7
Miguel Fondeur <miguelfondeur@gmail.com>
2023-04-24 15:37:57 +0000

    Change My List to Playlist

--- resources/platform/views/partials/bladesora/members/components/home/_list-section.blade.php
+++ resources/platform/views/partials/bladesora/members/components/home/_list-section.blade.php
@@ -7,7 +7,7 @@
                 <h2 class="tw-font-bold tw-text-2xl tw-leading-none lg:tw-leading-none lg:tw-text-3xl">Playlists</h2>
             </a>
             <a href="{{ $myListUrl }}"
-                aria-label="See All Lessons In My List"
+                aria-label="See All Playlists"
                 class="tw-text-base xl:tw-text-lg xl:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current"
             >
                 See All
