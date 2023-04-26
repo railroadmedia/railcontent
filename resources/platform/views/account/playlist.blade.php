2464f17a02564aed05f2b815579cee226443bf76
Roxana Riza <roxana_r2000@yahoo.com>
2023-04-26 16:27:00 +0300

    Testing

--- resources/platform/views/account/playlist.blade.php
+++ resources/platform/views/account/playlist.blade.php
@@ -17,7 +17,7 @@
     {{-- Playlist Catalog --}}
     <playlist-catalog
         :has-access="{{$playlist['has_access'] ?? 0}}"
-        :is-my-playlist="{{$playlist['is_my_playlist']}}"
+        :is-my-playlist="{{$playlist['is_my_playlist'] ?? 0}}"
         :lessons="{{ $listLessons }}"
         brand="{{ $brand }}"
     ></playlist-catalog>
