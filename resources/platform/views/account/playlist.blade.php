52987b789afe098879ff4eb25c63ae5fcfdda46a
Roxana Riza <roxana_r2000@yahoo.com>
2023-04-26 16:16:34 +0300

    Testing

--- resources/platform/views/account/playlist.blade.php
+++ resources/platform/views/account/playlist.blade.php
@@ -9,14 +9,14 @@
     {{-- Playlist Header --}}
     <playlist-header
         :playlist="{{ json_encode($playlist) }}"
-        :has-access="{{$playlist['has_access']}}"
+        :has-access="{{$playlist['has_access'] ?? 0}}"
         :lessons="{{ $listLessons }}"
         brand="{{ $brand }}"
     /></playlist-header>
 
     {{-- Playlist Catalog --}}
     <playlist-catalog
-        :has-access="{{$playlist['has_access']}}"
+        :has-access="{{$playlist['has_access'] ?? 0}}"
         :is-my-playlist="{{$playlist['is_my_playlist']}}"
         :lessons="{{ $listLessons }}"
         brand="{{ $brand }}"
