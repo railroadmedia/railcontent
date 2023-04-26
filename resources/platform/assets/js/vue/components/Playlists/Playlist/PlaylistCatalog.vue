60e1043a3868ab1fd99be187aa522e298999b79e
Miguel Fondeur <miguelfondeur@gmail.com>
2023-04-26 14:08:51 +0000

    add no access state updates to the playlist ui

--- resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCatalog.vue
+++ resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCatalog.vue
@@ -38,6 +38,10 @@
             type: Boolean,
             default: false,
         },
+        hasAccess: {
+            type: Boolean,
+            default: true,
+        }
     })
 
     //-----------Refs-----------//
@@ -117,7 +121,7 @@
             const index = state.newSortPositions.findIndex(object => object.id === state.startID);
             if(index == -1) {
                 state.newSortPositions.push({ id: state.startID, position: state.endPosition + 1 })
-            } else{
+            } else {
                 state.newSortPositions[index].position =  state.endPosition + 1;
             }
         }
@@ -190,79 +194,91 @@
 <template>
     <main class="tw-w-full ">
 
-        <div class="tw-w-full tw-transition-all dark:tw-bg-[#002039] tw-bg-[#e5e7ea] tw-overflow-hidden"
-             :class="playlistsStore.sortingPlaylist ? 'tw-max-h-[200px] tw-z-20 tw-sticky tw-top-0 tw-drop-shadow-md' : 'tw-max-h-0'">
-            <div class="tw-w-full tw-container tw-mx-auto tw-pt-4 md:tw-pt-0 tw-px-4 md:tw-px-8 dark:tw-text-white tw-flex tw-flex-col md:tw-flex-row tw-items-center">
-                <p class="tw-text-center md:tw-text-left  md:tw-mr-auto">Click <span class="tw-font-bold tw-mx-0.5">save changes</span> to save your changes or <span class="tw-font-bold tw-mx-0.5">cancel</span> to exit re-ordering</p>
-                <div class="tw-ml-6 tw-flex tw-py-[15px]">
-                    <button class="tw-btn-primary tw-mr-[10px] tw-px-[30px] xl:tw-w-[250px]" :class="`tw-bg-${brand}`" @click.prevent="handleSort">
-                        Save changes
-                    </button>
-                    <button class="tw-btn-secondary tw-bg-transparent dark:tw-text-white tw-text-[#00101D] tw-px-[30px] hover:tw-bg-black/10 xl:tw-w-[200px]"
-                            @click.prevent="handleCancelSort">
-                        Cancel
-                    </button>
+        <template v-if="hasAccess">
+            <div class="tw-w-full tw-transition-all dark:tw-bg-[#002039] tw-bg-[#e5e7ea] tw-overflow-hidden"
+                :class="playlistsStore.sortingPlaylist ? 'tw-max-h-[200px] tw-z-20 tw-sticky tw-top-0 tw-drop-shadow-md' : 'tw-max-h-0'">
+                <div class="tw-w-full tw-container tw-mx-auto tw-pt-4 md:tw-pt-0 tw-px-4 md:tw-px-8 dark:tw-text-white tw-flex tw-flex-col md:tw-flex-row tw-items-center">
+                    <p class="tw-text-center md:tw-text-left  md:tw-mr-auto">Click <span class="tw-font-bold tw-mx-0.5">save changes</span> to save your changes or <span class="tw-font-bold tw-mx-0.5">cancel</span> to exit re-ordering</p>
+                    <div class="tw-ml-6 tw-flex tw-py-[15px]">
+                        <button class="tw-btn-primary tw-mr-[10px] tw-px-[30px] xl:tw-w-[250px]" :class="`tw-bg-${brand}`" @click.prevent="handleSort">
+                            Save changes
+                        </button>
+                        <button class="tw-btn-secondary tw-bg-transparent dark:tw-text-white tw-text-[#00101D] tw-px-[30px] hover:tw-bg-black/10 xl:tw-w-[200px]"
+                                @click.prevent="handleCancelSort">
+                            Cancel
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="tw-container tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white tw-pb-14 tw-pt-[28px]">
-            <div class="tw-flex tw-flex-col">
-                <!-- Empty State -->
-                <section v-if="playlistsStore.lessons.length === 0" class="tw-w-full tw-flex tw-flex-col dark:tw-text-white tw-items-center tw-mt-[58px]">
-                    <div class="tw-h-[84px] tw-w-[84px] tw-rounded-full tw-inline-flex tw-items-center tw-justify-center tw-text-white dark:tw-text-[#9EC0DC] tw-transition-colors tw-bg-[#3F3F46] dark:tw-bg-[#445F74] tw-mb-6">
-                        <musora-icon icon-name="playlist" width="45" height="45" />
-                    </div>
-                    <h1 class="tw-text-3xl tw-font-bold tw-mb-[15px]">No lessons here yet</h1>
-                    <p>Go to a lesson and click the plus icon to add to this playlist. </p>
-                </section>
-
-                <!-- Catalog -->
-                <div v-if="playlistsStore.lessons.length" class="tw-w-full">
-
-                    <!-- List View Header -->
-                    <header class="tw-hidden lg:tw-flex tw-w-full tw-font-bold tw-items-center tw-transition-colors tw-bg-[#E6E7E9] dark:tw-bg-[#002039] tw-text-[#0D0D0D] dark:tw-text-white tw-mb-1 tw-rounded-t-md tw-p-3">
-                        <div class="tw-w-[40px]">#</div>
-                        <p class="tw-relative tw-inline-block tw-mr-auto tw-w-full">Name</p>
-                        <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[128px]">Type</p>
-                        <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[100px]">Time</p>
-                        <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[100px]">Actions</p>
-                    </header>
-
-                    <!-- Cards -->
-                    <section v-if="playlistsStore.lessons.length"
-                             class="tw-w-full tw-relative tw-mb-5 tw-flex tw-flex-col"
-                    >
-                        <!-- Print Each Card -->
-                        <playlist-card
-                            v-for="(lesson,i) in playlistsStore.lessons"
-                            :key="i"
-                            :index="i"
-                            :lesson="lesson"
-                            :token="token"
-                            :brand="brand"
-                            :draggable="playlistsStore.sortingPlaylist"
-                            @dragstart="handleDragStart($event, i,lesson.user_playlist_item_id)"
-                            @dragleave="handleDragLeave($event)"
-                            @dragover.prevent="handleDragOver($event)"
-                            @dragend="handleDragEnd"
-                            @drop="handleDrop($event, i)"
-                        />
-                        <!-- Skeleton Loader For Infinite Scroll -->
-                        <div v-if="playlistsStore.loadingLessons && infiniteScroll"
-                            class="tw-w-full tw-animate-pulse tw-flex tw-flex-col">
-                            <div v-for="n in limit"
-                                :key="n"
-                                class="tw-flex tw-w-full tw-h-[90px] tw-flex-row tw-items-center tw-transition-colors tw-py-0.5 even:tw-bg-[#E6E7E9] dark:even:tw-bg-[#081825]"
-                            >
-                            </div>
+            <div class="tw-container tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white tw-pb-14 tw-pt-[28px]">
+                <div class="tw-flex tw-flex-col">
+                    <!-- Empty State -->
+                    <section v-if="playlistsStore.lessons.length === 0" class="tw-w-full tw-flex tw-flex-col dark:tw-text-white tw-items-center tw-mt-[58px]">
+                        <div class="tw-h-[84px] tw-w-[84px] tw-rounded-full tw-inline-flex tw-items-center tw-justify-center tw-text-white dark:tw-text-[#9EC0DC] tw-transition-colors tw-bg-[#3F3F46] dark:tw-bg-[#445F74] tw-mb-6">
+                            <musora-icon icon-name="playlist" width="45" height="45" />
                         </div>
-
+                        <h1 class="tw-text-3xl tw-font-bold tw-mb-[15px]">No lessons here yet</h1>
+                        <p>Go to a lesson and click the plus icon to add to this playlist. </p>
                     </section>
 
+                    <!-- Catalog -->
+                    <div v-if="playlistsStore.lessons.length" class="tw-w-full">
+
+                        <!-- List View Header -->
+                        <header class="tw-hidden lg:tw-flex tw-w-full tw-font-bold tw-items-center tw-transition-colors tw-bg-[#E6E7E9] dark:tw-bg-[#002039] tw-text-[#0D0D0D] dark:tw-text-white tw-mb-1 tw-rounded-t-md tw-p-3">
+                            <div class="tw-w-[40px]">#</div>
+                            <p class="tw-relative tw-inline-block tw-mr-auto tw-w-full">Name</p>
+                            <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[128px]">Type</p>
+                            <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[100px]">Time</p>
+                            <p class="tw-inline-flex tw-shrink-0 tw-justify-center tw-w-[100px]">Actions</p>
+                        </header>
+
+                        <!-- Cards -->
+                        <section v-if="playlistsStore.lessons.length"
+                                class="tw-w-full tw-relative tw-mb-5 tw-flex tw-flex-col"
+                        >
+                            <!-- Print Each Card -->
+                            <playlist-card
+                                v-for="(lesson,i) in playlistsStore.lessons"
+                                :key="i"
+                                :index="i"
+                                :lesson="lesson"
+                                :token="token"
+                                :brand="brand"
+                                :draggable="playlistsStore.sortingPlaylist"
+                                @dragstart="handleDragStart($event, i,lesson.user_playlist_item_id)"
+                                @dragleave="handleDragLeave($event)"
+                                @dragover.prevent="handleDragOver($event)"
+                                @dragend="handleDragEnd"
+                                @drop="handleDrop($event, i)"
+                            />
+                            <!-- Skeleton Loader For Infinite Scroll -->
+                            <div v-if="playlistsStore.loadingLessons && infiniteScroll"
+                                class="tw-w-full tw-animate-pulse tw-flex tw-flex-col">
+                                <div v-for="n in limit"
+                                    :key="n"
+                                    class="tw-flex tw-w-full tw-h-[90px] tw-flex-row tw-items-center tw-transition-colors tw-py-0.5 even:tw-bg-[#E6E7E9] dark:even:tw-bg-[#081825]"
+                                >
+                                </div>
+                            </div>
+
+                        </section>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </template>
+        
+        <!-- No Access -->
+        <template v-else>
+            <div class="tw-container tw-mx-auto tw-px-4 md:tw-px-8 dark:tw-text-white tw-pb-14 tw-pt-[80px]">
+                <div class="tw-flex tw-flex-col tw-items-center">
+                    <h1 class="tw-text-3xl tw-font-bold tw-mb-[15px]">Oops!</h1>
+                    <p>This playlist is private and you don’t have access to view it</p>
+                </div>
+            </div>
+        </template>
 
     </main>
 </template>
