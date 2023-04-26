2a0d5a7aae63fbe4293106aec7e103c0c609f436
Miguel Fondeur <miguelfondeur@gmail.com>
2023-04-26 19:33:36 +0000

    add boolean value for has-access

--- resources/platform/views/account/playlist.blade.php
+++ resources/platform/views/account/playlist.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     <p class="tw-font-bold dark:tw-text-white"> Playlist Data: <br> {{ json_encode($playlist) }}</p>
-    <p class="tw-font-bold dark:tw-text-white"> Has Access: <br> {{$playlist['has_access'] ?? 0}}</p>
+    <p class="tw-font-bold dark:tw-text-white"> Has Access: {{ $playlist['has_access'] ?? false }}</p>
     <p class="tw-font-bold tw-text-white"> Lessons Data: <br>{{ $listLessons }}</p>
 
 @endsection
