9a517e65425537cc2d6f8d8aa5c38f5fb42ae266
Miguel Fondeur <miguelfondeur@gmail.com>
2023-04-28 14:55:39 +0000

    update breadcrumb styles for light mode

--- resources/platform/assets/js/vue/components/Playlists/Breadcrumb.vue
+++ resources/platform/assets/js/vue/components/Playlists/Breadcrumb.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="tw-text-black tw-text-[#A1A1A9] tw-flex tw-justify-center tw-items-center tw-w-full tw-h-[40px] dark:tw-bg-black tw-bg-[#e5e7eb] tw-text-center tw-uppercase tw-text-sm">
-      <a :href="homeLink" class="tw-text-black tw-text-[#A1A1A9]">
+    <div class="tw-text-black tw-text-[#3F3F46] dark:tw-text-[#A1A1A9]  tw-flex tw-justify-center tw-items-center tw-w-full tw-h-[40px] dark:tw-bg-black tw-bg-[#e5e7eb] tw-text-center tw-uppercase tw-text-sm">
+      <a :href="homeLink" class="tw-text-black tw-text-[#3F3F46] dark:tw-text-[#A1A1A9] ">
         <HomeIcon class="tw-w-[12px] tw-h-[12px]" />
       </a>
       &nbsp;/&nbsp;
-      <a class="tw-text-black tw-text-[#A1A1A9] tw-font-normal tw-text-sm" :href="playlistsLink">PLAYLISTS</a>
+      <a class="tw-text-black tw-text-[#3F3F46] dark:tw-text-[#A1A1A9]  tw-font-normal tw-text-sm" :href="playlistsLink">PLAYLISTS</a>
       &nbsp;/&nbsp;
-      <a class="tw-font-bold tw-text-[#A1A1A9] tw-text-sm" :href="playlistLink">{{ playlistName }}</a>
+      <a class="tw-font-bold tw-text-[#3F3F46] dark:tw-text-[#A1A1A9]  tw-text-sm" :href="playlistLink">{{ playlistName }}</a>
     </div>
   </template>
   
