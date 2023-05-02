a45ef0e7b0faee7a79e40e6117e46deb493f13d0
Roxana Riza <roxana_r2000@yahoo.com>
2023-05-02 13:05:06 +0300

    Update product id on cohort

--- app/Nova/Cohort.php
+++ app/Nova/Cohort.php
@@ -324,13 +324,13 @@ class Cohort extends Resource
 
             DateTime::make(__('Enrollment Start Time'), 'enrollment_start_date')->hideFromIndex()->help('Ignore UTC. It is actually PST.<br>This does NOT account for Daylight Savings between Mar-Nov. Make sure you offset by an hour during PDT'),
             DateTime::make(__('Enrollment End Time'), 'enrollment_end_date')->hideFromIndex()->help('Ignore UTC. It is actually PST.<br>This does NOT account for Daylight Savings between Mar-Nov. Make sure you offset by an hour during PDT'),
-            Number::make('Product ID', 'product_id')->hideFromIndex(),
+            Text::make('Product ID', 'product_id')->hideFromIndex()->required(),
             DateTime::make(__('Cohort Start Time'), 'cohort_start_date')->hideFromIndex()->help("Controls whether cohort is currently active.  Cohort banners/pinned packs are a couple of functions tied to this."),
             DateTime::make(__('Cohort End Time'), 'cohort_end_date')->hideFromIndex()->help("Controls whether cohort is currently active.  Cohort banners/pinned packs are a couple of functions tied to this."),
 
-            Number::make('Course ID','content_id')->hideFromIndex()->required(),
+            Text::make('Course ID','content_id')->hideFromIndex()->required(),
 
-            Number::make('Conversation Thread ID','conversation_thread_id')->hideFromIndex(),
+            Text::make('Conversation Thread ID','conversation_thread_id')->hideFromIndex(),
         ];
     }
 
