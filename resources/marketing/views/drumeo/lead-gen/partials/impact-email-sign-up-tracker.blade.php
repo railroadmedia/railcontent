4ddcf7c61be76c842437be35fc08fce965bc606b
Mircea Murasan <mircea.murasan@artsoft-consult.ro>
2023-05-03 12:02:06 +0300

    Update getSignUpActionTrackerId method.

--- resources/marketing/views/drumeo/lead-gen/partials/impact-email-sign-up-tracker.blade.php
+++ resources/marketing/views/drumeo/lead-gen/partials/impact-email-sign-up-tracker.blade.php
@@ -4,17 +4,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/js-sha1/0.6.0/sha1.min.js"></script>
 <script>
     function getSignUpActionTrackerId(environment) {
-        var signUpActionTrackerId =
-            "{{ config('railanalytics.drumeo.production.providers.impact.sign-up-action-tracker-id') }}"
-        {{--if (environment == "development") {--}}
-        {{--    signUpActionTrackerId =--}}
-        {{--        "{{ config('railanalytics.drumeo.local.providers.impact.sign-up-action-tracker-id') }}"--}}
-        {{--} else if (environment == "staging") {--}}
-        {{--    signUpActionTrackerId = "{{ config('railanalytics.drumeo.beta-testing.providers.impact.sign-up-action-tracker-id') }}"--}}
-        {{--} else if (environment == "testing") {--}}
-        {{--    signUpActionTrackerId = "{{ config('railanalytics.drumeo.beta-testing.providers.impact.sign-up-action-tracker-id') }}"--}}
-        {{--}--}}
-        return signUpActionTrackerId;
+        return (environment == 'production') ?
+            "{{ config('railanalytics.drumeo.production.providers.impact.sign-up-action-tracker-id') }}" :
+            "{{ config('railanalytics.drumeo.local.providers.impact.sign-up-action-tracker-id') }}";
     }
 
     function emailSignUpConversionTrackerForImpactProvider() {
