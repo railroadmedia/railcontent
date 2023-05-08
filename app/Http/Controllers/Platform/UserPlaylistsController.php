35f0344e6406b467c082585a2db2537737a310fe
Roxana Riza <roxana_r2000@yahoo.com>
2023-05-08 16:41:11 +0300

    Add published_on and published_on_in_timezone on playlist items

--- app/Http/Controllers/Platform/UserPlaylistsController.php
+++ app/Http/Controllers/Platform/UserPlaylistsController.php
@@ -163,6 +163,8 @@ class UserPlaylistsController extends BaseController
                 $playlistItems[$index]['title'] = $item['title'];
                 $playlistItems[$index]['artist'] = $item['artist'];
                 $playlistItems[$index]['status'] = $item['status'];
+                $playlistItems[$index]['published_on'] = $item['published_on'];
+                $playlistItems[$index]['published_on_in_timezone'] = $item['published_on_in_timezone'] ?? null;
                 $playlistItems[$index]['need_access'] = empty(
                     array_intersect(
                         $userPermissionIds,
