835a1d1b1aa37b043e6359d3f55d8eaa61e80bc3
Roxana Riza <roxana_r2000@yahoo.com>
2023-05-11 15:10:11 +0300

    Update route name for website associations JSON file

--- routes/platform/platform_pages_routes.php
+++ routes/platform/platform_pages_routes.php
@@ -661,7 +661,7 @@ Route::get(
     ->name('platform.apple-association-file');
 
 Route::get(
-    '/assetlinks',
+    '/.well-known/assetlinks.json',
     [\App\Http\Controllers\Misc\ManifestFilesController::class, 'androidAssociationFile']
 )
     ->name('platform.android-association-file');
