add63433c29d7811cc03a792b374c589c131474d
peterminhlee <petereminhlee@gmail.com>
2023-05-11 19:45:07 -0700

    method data

---
+++ resources/marketing/views/musora/pages/method-data.php
@@ -0,0 +1,245 @@
+<?php
+    $pianoteMethod = [
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/276694-card-thumbnail-maxres-1607968717.png',
+            'lessonNum' => 16,
+            'title' => 'Getting Started On The Piano',
+            'desc' => 'Welcome to the piano. This level will get you acquainted with this beautiful instrument, and teach you the fundamental skills to be successful on the keys, no matter what style you want to play.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/276744-card-thumbnail-maxres-1607968746.png',
+            'lessonNum' => 12,
+            'title' => 'Developing Dexterity And Keyboard Confidence',
+            'desc' => 'Scales can get real boring, real fast. In Level 2, you’ll learn how to make scale practice musical so you not only develop greater control and confidence at the keyboard, you also have a ton of fun (and sound great).',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/276781-card-thumbnail-maxres-1607968758.png',
+            'lessonNum' => 15,
+            'title' => 'Chording',
+            'desc' => 'Chords make songs. It’s as simple as that. In Level 3 you’ll dive deeper into the concepts of piano chords and you’ll learn how to play them better, faster, and more smoothly.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/276826-card-thumbnail-maxres-1607968769.png',
+            'lessonNum' => 15,
+            'title' => 'Playing Chords Like A Pro',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/286045-card-thumbnail-maxres-1611613282.JPG',
+            'lessonNum' => 16,
+            'title' => 'Sight Reading',
+            'desc' => '.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/289378-card-thumbnail-maxres-1613004051.JPG',
+            'lessonNum' => 19,
+            'title' => 'Developing Your Musicality',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/293686-card-thumbnail-maxres-1618852882.jpg',
+            'lessonNum' => 20,
+            'title' => 'Applying Technique & Solving Piano Player Problems',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/295875-card-thumbnail-maxres-1616601546.JPG',
+            'lessonNum' => 30,
+            'title' => 'Exploring Musical Styles',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/299115-card-thumbnail-maxres-1618852615.jpg',
+            'lessonNum' => 15,
+            'title' => 'Composition & Songwriting',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/301572-card-thumbnail-maxres-1620751089.jpg',
+            'lessonNum' => 18,
+            'title' => 'The Next Steps',
+            'desc' => '',
+        ],
+    ];
+
+    $guitareoMethod = [
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level1-1640823137.jpg',
+            'lessonNum' => 8,
+            'title' => 'Get To Know The Guitar',
+            'desc' => 'From tuning to striking the strings to learning what a note actually is, get to know your guitar and understand how it works.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level2-1640823206.jpg',
+            'lessonNum' => 8,
+            'title' => 'Let\'s Get You Playing Chords (Songs), Like Now!',
+            'desc' => 'Music is a language and a powerful communication tool. You\'ll learn to read and understand chord diagrams and play progressions used in thousands of songs.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level3-1640823216.jp',
+            'lessonNum' => 8,
+            'title' => 'Let\'s Get Rockin\' With Rhythm!',
+            'desc' => 'Rhythm is an important part of learning the guitar. Add groove to your playing by understanding beat, timing, and tempo.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level4-1640823229.jpg',
+            'lessonNum' => 9,
+            'title' => 'What Are These Tabs And Chord Charts You Speak Of?',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level5-1640823238.jpg',
+            'lessonNum' => 7,
+            'title' => 'Time To Get Fancy: Add Color To Your Chords!!',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level6-1641917824.jpg',
+            'lessonNum' => 8,
+            'title' => 'You Can Start Soloing, Like Now!',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/MethodLevel7-09-1643789795.png',
+            'lessonNum' => 8,
+            'title' => 'Don’t Be Afraid Of Bar Chords… Embrace Them!',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/MethodLevel8-SquareThumb-1646723963.jpg',
+            'lessonNum' => 9,
+            'title' => 'Expressive Techniques: Exploring Emotions In Each Note',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitaro-Method-Level9-1650358480.jpg',
+            'lessonNum' => 8,
+            'title' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Guitareo-Method-Level10-1663141571.jpg',
+            'lessonNum' => 9,
+            'title' => 'You Made It!  But Let\'s Push You A Bit Further!',
+            'desc' => '',
+        ],
+    ];
+
+    $drumeoMethod = [
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241248-card-thumbnail-1577144969.jpg',
+            'lessonNum' => 30,
+            'title' => 'Getting Started On The Drums',
+            'desc' => 'This is where it starts. You’ll learn how to set up your drum-set, hold your drumsticks properly, and play your first beats. And, best of all, you’ll learn how to play your first two songs on the drums.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241284-card-thumbnail-1577144975.jpg',
+            'lessonNum' => 49,
+            'title' => 'Elevate Your Drum Sound',
+            'desc' => 'In Level Two, you’ll continue to build your drumming foundation. You’re going to learn your first drum rudiments, develop basic reading skills (it’s not that hard, we promise!), and get introduced to three styles of drumming: rock, punk, and metal.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241285-card-thumbnail-1577144979.jpg',
+            'lessonNum' => 48,
+            'title' => 'The Motions Of Drumming',
+            'desc' => 'Learning the motions of drumming will help you play faster, smoother, and for longer periods. This level introduces you to key drum set techniques like the three main stick grips and beginner bass drum pedal technique.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241286-card-thumbnail-1577144984.jpg',
+            'lessonNum' => 66,
+            'title' => 'The Moeller Method & Essential Grooves',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241287-header-image-1577145376.jpg',
+            'lessonNum' => 63,
+            'title' => 'Rhythmic Groupings & Independence',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241288-card-thumbnail-1577144999.jpg',
+            'lessonNum' => 51,
+            'title' => 'Odd Time & Improvisation',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241289-card-thumbnail-1577145007.jpg',
+            'lessonNum' => 34   ,
+            'title' => 'Foot Technique & Combinations',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241290-card-thumbnail-1577145012.jpg',
+            'lessonNum' => 43,
+            'title' => 'Brushes, Texture & Articulation',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241291-card-thumbnail-1577145017.jpg',
+            'lessonNum' => 39,
+            'title' => 'Advanced Styles & Musical Decisions',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/241292-card-thumbnail-1577145021.jpg',
+            'lessonNum' => 38,
+            'title' => 'Go Anywhere On The Drums',
+            'desc' => '',
+        ],
+    ];
+
+    $singeoMethod = [
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-1.png',
+            'lessonNum' => 6,
+            'title' => 'Understanding Your Voice',
+            'desc' => 'This is where it starts. You’ll learn how to set up your drum-set, hold your drumsticks properly, and play your first beats. And, best of all, you’ll learn how to play your first two songs on the drums.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-2.png',
+            'lessonNum' => 11,
+            'title' => 'Build Strength & Gain Control',
+            'desc' => 'In Level Two, you’ll continue to build your drumming foundation. You’re going to learn your first drum rudiments, develop basic reading skills (it’s not that hard, we promise!), and get introduced to three styles of drumming: rock, punk, and metal.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-3.png',
+            'lessonNum' => 7,
+            'title' => 'Vowel Shapes & Sounds',
+            'desc' => 'Learning the motions of drumming will help you play faster, smoother, and for longer periods. This level introduces you to key drum set techniques like the three main stick grips and beginner bass drum pedal technique.',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-4.png',
+            'lessonNum' => 8,
+            'title' => 'Start To Finish',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-5-1635616364.pn',
+            'lessonNum' => 9,
+            'title' => 'Finding Your Unique Style',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-6-1637292952.jpg',
+            'lessonNum' => 10,
+            'title' => 'Riffs & Runs',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-7-1643042123.jpg',
+            'lessonNum' => 9,
+            'title' => 'Music Theory For Singers',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-8-1647890731.jpg',
+            'lessonNum' => 7,
+            'title' => 'Fine Tuning Your Voice',
+            'desc' => '',
+        ],
+        [
+            'img' => 'https://d1923uyy6spedc.cloudfront.net/Level-9-1653595021.jpg',
+            'lessonNum' => 60,
+            'title' => 'Harmony And Ear Training',
+            'desc' => '',
+        ],
+    ];
