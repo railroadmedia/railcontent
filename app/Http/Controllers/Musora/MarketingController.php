adb58ec1dcabffcc0596980bd0b63ed8d862500c
peterminhlee <petereminhlee@gmail.com>
2023-05-11 19:45:03 -0700

    added method

--- app/Http/Controllers/Musora/MarketingController.php
+++ app/Http/Controllers/Musora/MarketingController.php
@@ -56,4 +56,8 @@ class MarketingController extends BaseController
     {
         return view('musora.pages.giftcard-page');
     }
+    public function method()
+    {
+        return view('musora.pages.method', [ 'theme' => 'musora', 'page' => 'method' ]);
+    }
 }
--- routes/musora/marketing/other.php
+++ routes/musora/marketing/other.php
@@ -20,6 +20,7 @@ Route::domain('{musoraDomain}')
         Route::get('referral-join', [ReferralJoinController::class, 'join']);
         Route::get('recitals', [MarketingController::class, 'recitals']);
         Route::get('gift-card', [MarketingController::class, 'giftcard']);
+        Route::get('method', [MarketingController::class, 'method']);
 
         Route::get('redeem', [CodeRedemptionController::class, 'renderNewAccountRedeemPage']);
         Route::get('pass', [CodeRedemptionController::class, 'passRedeem']);
