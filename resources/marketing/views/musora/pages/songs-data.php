d2deb0d5a12bdac5aae3c6d0b82ddf0fd1d0fdf3
peterminhlee <petereminhlee@gmail.com>
2023-05-16 10:13:30 -0700

    songs data

--- resources/marketing/views/musora/pages/songs-data.php
+++ resources/marketing/views/musora/pages/songs-data.php
@@ -1,1 +1,537 @@
 <?php
+    $classicRock = [
+        [
+            'img' => '',
+            'title' => 'Back In Black',
+            'artist' => 'AC/DC',
+        ],
+        [
+            'img' => '',
+            'title' => 'Livin\' On A Prayer',
+            'artist' => 'Bon Jovi',
+        ],
+        [
+            'img' => '',
+            'title' => 'Summer Of \'69',
+            'artist' => 'Bryan Adams',
+        ],
+        [
+            'img' => '',
+            'title' => 'Bad Moon Rising',
+            'artist' => 'Creedence Clearwater Revival',
+        ],
+        [
+            'img' => '',
+            'title' => 'Don\'t Stop Believin\'',
+            'artist' => 'Journey',
+        ],
+        [
+            'img' => '',
+            'title' => 'Good Times Bad Times',
+            'artist' => 'Led Zeppelin',
+        ],
+        [
+            'img' => '',
+            'title' => '50 Ways To Leave Your Lover',
+            'artist' => 'Paul Simon',
+        ],
+        [
+            'img' => '',
+            'title' => 'Another One Bites The Dust',
+            'artist' => 'Queen',
+        ],
+        [
+            'img' => '',
+            'title' => 'Tom Sawyer',
+            'artist' => 'Rush',
+        ],
+        [
+            'img' => '',
+            'title' => 'Hot For Teacher',
+            'artist' => 'Van Halen',
+        ],
+    ];
+
+    $popClassics = [
+        [
+            'img' => '',
+            'title' => 'Tom Petty And The Heartbreakers',
+            'artist' => 'American Girl',
+        ],
+        [
+            'img' => '',
+            'title' => 'Andrea Bocelli',
+            'artist' => 'Because We Believe',
+        ],
+        [
+            'img' => '',
+            'title' => 'Elton John',
+            'artist' => 'Candle In The Wind',
+        ],
+        [
+            'img' => '',
+            'title' => 'Simon & Garfunkel',
+            'artist' => 'Cecilia',
+        ],
+        [
+            'img' => '',
+            'title' => 'David Bowie',
+            'artist' => 'Changes',
+        ],
+        [
+            'img' => '',
+            'title' => 'ABBA',
+            'artist' => 'Dancing Queen',
+        ],
+        [
+            'img' => '',
+            'title' => 'Leonard Cohen',
+            'artist' => 'Hallelujah',
+        ],
+        [
+            'img' => '',
+            'title' => 'John Lennon',
+            'artist' => 'Imagine',
+        ],
+        [
+            'img' => '',
+            'title' => 'Aretha Franklin',
+            'artist' => 'Respect',
+        ],
+        [
+            'img' => '',
+            'title' => 'Tina Turner',
+            'artist' => 'What’s Love Got To Do With It',
+        ],
+    ];
+
+    $modernRock = [
+        [
+            'img' => '',
+            'title' => 'Don\'t Wanna Fight',
+            'artist' => 'Alabama Shakes',
+        ],
+        [
+            'img' => '',
+            'title' => 'Do I Wanna Know?',
+            'artist' => 'Arctic Monkeys',
+        ],
+        [
+            'img' => '',
+            'title' => 'Little Talks',
+            'artist' => 'Of Monsters and Men',
+        ],
+        [
+            'img' => '',
+            'title' => 'The Scientist',
+            'artist' => 'Coldplay',
+        ],
+        [
+            'img' => '',
+            'title' => 'Pumped Up Kicks',
+            'artist' => 'Foster The People',
+        ],
+        [
+            'img' => '',
+            'title' => 'Highway Tune',
+            'artist' => 'Greta Van Fleet',
+        ],
+        [
+            'img' => '',
+            'title' => 'Believer',
+            'artist' => 'Imagine Dragons',
+        ],
+        [
+            'img' => '',
+            'title' => 'Take Me To Church',
+            'artist' => 'Hozier',
+        ],
+        [
+            'img' => '',
+            'title' => 'The Night We Met',
+            'artist' => 'Lord Huron',
+        ],
+        [
+            'img' => '',
+            'title' => 'Little Lion Man',
+            'artist' => 'Mumford & Sons',
+        ],
+    ];
+
+    $jazz = [
+        [
+            'img' => '',
+            'title' => 'Moanin\'',
+            'artist' => 'Art Blakey & The Jazz Messengers',
+        ],
+        [
+            'img' => '',
+            'title' => 'Sing, Sing, Sing',
+            'artist' => 'Benny Goodman',
+        ],
+        [
+            'img' => '',
+            'title' => 'Spectrum',
+            'artist' => 'Billy Cobham',
+        ],
+        [
+            'img' => '',
+            'title' => 'Caravan',
+            'artist' => 'Buddy Rich Big Band',
+        ],
+        [
+            'img' => '',
+            'title' => 'Beneath The Mask',
+            'artist' => 'Chick Corea Elektric Band',
+        ],
+        [
+            'img' => '',
+            'title' => 'Take Five',
+            'artist' => 'Dave Brubeck Quartet',
+        ],
+        [
+            'img' => '',
+            'title' => 'Ella Fitzgerald',
+            'artist' => 'Dream A Little Dream',
+        ],
+        [
+            'img' => '',
+            'title' => 'Ray Charles',
+            'artist' => 'Georgia On My Mind',
+        ],
+        [
+            'img' => '',
+            'title' => 'Milt Gabler, Duke Ellington',
+            'artist' => 'In A Mellow Tone',
+        ],
+        [
+            'img' => '',
+            'title' => 'Duke Ellington, Manny Kurtz, Irving Mills',
+            'artist' => 'In A Sentimental Mood',
+        ],
+        [
+            'img' => '',
+            'title' => 'Judy Garland',
+            'artist' => 'Over The Rainbow',
+        ],
+    ];
+
+    $metal = [
+        [
+            'img' => '',
+            'title' => 'Hail To The King',
+            'artist' => 'Avenged Sevenfold',
+        ],
+        [
+            'img' => '',
+            'title' => 'Paranoid',
+            'artist' => 'Black Sabbath',
+        ],
+        [
+            'img' => '',
+            'title' => 'Pull Me Under',
+            'artist' => 'Dream Theater',
+        ],
+        [
+            'img' => '',
+            'title' => 'Bleed',
+            'artist' => 'Meshuggah',
+        ],
+        [
+            'img' => '',
+            'title' => 'Walk',
+            'artist' => 'Pantera',
+        ],
+        [
+            'img' => '',
+            'title' => 'Marigold',
+            'artist' => 'Periphery',
+        ],
+        [
+            'img' => '',
+            'title' => 'Bulls On Parade',
+            'artist' => 'Rage Against The Machine',
+        ],
+        [
+            'img' => '',
+            'title' => 'Du Hast',
+            'artist' => 'Rammstein',
+        ],
+        [
+            'img' => '',
+            'title' => 'Chop Suey!',
+            'artist' => 'System Of A Down',
+        ],
+        [
+            'img' => '',
+            'title' => 'The Pot',
+            'artist' => 'Tool',
+        ],
+    ];
+
+    $modernPop = [
+        [
+            'img' => '',
+            'title' => 'Lady Gaga',
+            'artist' => 'A Million Reasons',
+        ],
+        [
+            'img' => '',
+            'title' => 'Vanessa Carlton',
+            'artist' => 'A Thousand Miles',
+        ],
+        [
+            'img' => '',
+            'title' => 'John Legend',
+            'artist' => 'All Of Me',
+        ],
+        [
+            'img' => '',
+            'title' => 'OneRepublic',
+            'artist' => 'Apologize',
+        ],
+        [
+            'img' => '',
+            'title' => 'Christina Perri',
+            'artist' => 'A Thousand Years',
+        ],
+        [
+            'img' => '',
+            'title' => 'Coldplay',
+            'artist' => 'Clocks',
+        ],
+        [
+            'img' => '',
+            'title' => 'Snow Patrol',
+            'artist' => 'Chasing Cars',
+        ],
+        [
+            'img' => '',
+            'title' => 'The Chainsmokers, Halsey',
+            'artist' => 'Closer',
+        ],
+        [
+            'img' => '',
+            'title' => 'Anna Kendrick',
+            'artist' => 'Cups (When I\'m Gone)',
+        ],
+        [
+            'img' => '',
+            'title' => 'Adele',
+            'artist' => 'Someone Like You',
+        ],
+    ];
+
+    $country = [
+        [
+            'img' => '',
+            'title' => 'I Saw The Light',
+            'artist' => 'Hank Williams',
+        ],
+        [
+            'img' => '',
+            'title' => 'Sangría',
+            'artist' => 'Blake Shelton',
+        ],
+        [
+            'img' => '',
+            'title' => 'Mr. Bojangles',
+            'artist' => 'Nitty Gritty Dirt Band',
+        ],
+        [
+            'img' => '',
+            'title' => 'From This Moment',
+            'artist' => 'Shania Twain',
+        ],
+        [
+            'img' => '',
+            'title' => 'Hicktown',
+            'artist' => 'Jason Aldean',
+        ],
+        [
+            'img' => '',
+            'title' => 'Jesus, Take The Wheel',
+            'artist' => 'Carrie Underwood',
+        ],
+        [
+            'img' => '',
+            'title' => 'Tennessey Whiskey',
+            'artist' => 'Chris Stapleton',
+        ],
+        [
+            'img' => '',
+            'title' => 'Tequila',
+            'artist' => 'Dan + Shay',
+        ],
+        [
+            'img' => '',
+            'title' => 'Cruise',
+            'artist' => 'Florida Georgia Line',
+        ],
+        [
+            'img' => '',
+            'title' => 'Friends In Low Places',
+            'artist' => 'Garth Brooks',
+        ],
+    ];
+
+    $musicals = [
+        [
+            'img' => '',
+            'title' => 'Lin-Manuel Miranda',
+            'artist' => 'Alexander Hamilton ',
+        ],
+        [
+            'img' => '',
+            'title' => 'Kander & Ebb',
+            'artist' => 'And All That Jazz',
+        ],
+        [
+            'img' => '',
+            'title' => 'Boublil and Schonberg',
+            'artist' => 'Castle On A Cloud',
+        ],
+        [
+            'img' => '',
+            'title' => 'Andrew Lloyd Webber',
+            'artist' => 'Memory ',
+        ],
+        [
+            'img' => '',
+            'title' => 'Frederick Loewe',
+            'artist' => 'I Could Have Danced All Night',
+        ],
+        [
+            'img' => '',
+            'title' => 'Andrew Lloyd Webber',
+            'artist' => 'The Phantom Of The Opera',
+        ],
+        [
+            'img' => '',
+            'title' => 'Charles Strouse',
+            'artist' => 'Tomorrow',
+        ],
+        [
+            'img' => '',
+            'title' => 'Rodgers & Hammerstein',
+            'artist' => 'The Sound Of Music',
+        ],
+        [
+            'img' => '',
+            'title' => 'Nathan Tysen & Daniel Messé',
+            'artist' => 'Where Do We Go From Here ',
+        ],
+        [
+            'img' => '',
+            'title' => 'Marc Shaiman',
+            'artist' => 'You Can\'t Stop The Beat',
+        ],
+    ];
+
+    $worship = [
+        [
+            'img' => '',
+            'title' => '10,000 Reasons (Bless The Lord)',
+            'artist' => 'Matt Redman',
+        ],
+        [
+            'img' => '',
+            'title' => 'Amazing Grace',
+            'artist' => 'Traditional Hymn ',
+        ],
+        [
+            'img' => '',
+            'title' => 'Balm In Gilead',
+            'artist' => 'African-American Spiritual',
+        ],
+        [
+            'img' => '',
+            'title' => 'Every Time I Feel The Spirit',
+            'artist' => 'African-American Spiritual',
+        ],
+        [
+            'img' => '',
+            'title' => 'Go, Tell It On The Mountain',
+            'artist' => 'African-American Spiritual',
+        ],
+        [
+            'img' => '',
+            'title' => 'How Great Thou Art',
+            'artist' => 'Hymn',
+        ],
+        [
+            'img' => '',
+            'title' => 'Mighty To Save',
+            'artist' => 'Hillsong Worship',
+        ],
+        [
+            'img' => '',
+            'title' => 'This Little Light Of Mine',
+            'artist' => 'African-American Spiritual',
+        ],
+        [
+            'img' => '',
+            'title' => 'Were You There?',
+            'artist' => 'African-American Spiritual',
+        ],
+        [
+            'img' => '',
+            'title' => 'When The Saints Go Marching In',
+            'artist' => 'Louis Armstrong',
+        ],
+    ];
+
+    $hiphop = [
+        [
+            'img' => '',
+            'title' => 'Fight For Your Right',
+            'artist' => 'Beastie Boys',
+        ],
+        [
+            'img' => '',
+            'title' => 'Gangsta\' Paradise',
+            'artist' => 'Coolio',
+        ],
+        [
+            'img' => '',
+            'title' => 'I Need A Doctor',
+            'artist' => 'Dr. Dre',
+        ],
+        [
+            'img' => '',
+            'title' => 'Lose Yourself',
+            'artist' => 'Eminem',
+        ],
+        [
+            'img' => '',
+            'title' => 'Empire State Of Mind',
+            'artist' => 'Jay-Z',
+        ],
+        [
+            'img' => '',
+            'title' => 'Straight Outta Compton',
+            'artist' => 'N.W.A.',
+        ],
+        [
+            'img' => '',
+            'title' => 'Hey Ya!',
+            'artist' => 'OutKast',
+        ],
+        [
+            'img' => '',
+            'title' => 'Fight The Power',
+            'artist' => 'Public Enemy',
+        ],
+        [
+            'img' => '',
+            'title' => 'I Gotta Feeling',
+            'artist' => 'The Black Eyed Peas',
+        ],
+        [
+            'img' => '',
+            'title' => 'Mellow My Man',
+            'artist' => 'The Roots',
+        ],
+    ];
+
+
