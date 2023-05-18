0b2bdc697b72b34f5ea2a61871ca4449929b03d1
Roxana Riza <roxana_r2000@yahoo.com>
2023-05-18 12:00:08 +0300

    Test Cohort validation on Nova

--- app/Nova/Cohort.php
+++ app/Nova/Cohort.php
@@ -32,7 +32,7 @@ class Cohort extends Resource
             ID::make()->sortable(),
 
             BelongsTo::make('Brand', 'brand', 'App\Nova\Brand')->sortable(),
-            Text::make('slug')->required(),
+            Text::make('slug')->required()->rules('required', 'max:255'),
             Hidden::make('Uuid')->withMeta(["value" => $uuid]),
             Text::make('Cohort Title', 'cohort_title')->required()->help("For easy reference to this banner in the CMS. This info won't show on the banner."),
 
