679cbc6c2a28d360812ba5e008ba4e12394f6e39
robwillems <robert@musora.com>
2023-05-19 14:02:48 -0700

    Add service provider

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         App\Modules\Content\Providers\ContentServiceProvider::class,
         App\Modules\Notifications\Providers\NotificationServiceProvider::class,
         App\Modules\Reporting\Providers\ReportingServiceProvider::class,
+        App\Modules\AddEventCalendars\Providers\AddEventCalendarsServiceProvider::class,
 
         /*
          * Application Service Providers...
