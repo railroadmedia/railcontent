8aab3cbafee15e5e5c5c6e4ba4430b8370f5888f
Trent <trent.harms@gmail.com>
2023-05-25 13:47:49 -0700

    fixing urls

--- resources/marketing/views/guitareo/products/rhythm-and-groove.blade.php
+++ resources/marketing/views/guitareo/products/rhythm-and-groove.blade.php
@@ -16,7 +16,7 @@
     @parent
     @include('_partials.layout._tailwindcdn')
     <link rel="stylesheet" href="{{ asset('/marketing/css/tailwind-helpers.css') }}">
-    <link href="{{ asset('/marketing/parcel/drumeo/sales-page-guitareo.css.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/parcel/drumeo/sales-page-guitareo.css') }}" rel="stylesheet">
     <link href="{{ asset('/marketing/parcel/drumeo/nav-footer-guitareo.css') }}" rel="stylesheet">
 @stop()
 
