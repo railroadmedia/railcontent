92380e304245df58ab19b620e132a432c78a79cc
Miguel Fondeur <miguelfondeur@gmail.com>
2023-05-26 12:06:25 +0000

    thumbnail overlay

--- resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
+++ resources/platform/assets/js/vue/components/Playlists/Playlist/PlaylistCard.vue
@@ -234,7 +234,9 @@ onBeforeMount(() => {
                     </div>
 
                     <!-- Overlay -->
-                    <div v-if="lesson.progress_percent === 100 || !released" class="tw-z-10 tw-absolute tw-top-0 tw-w-full tw-h-full tw-left-0 tw-bg-black/80 tw-flex tw-flex-col tw-items-center tw-justify-center">
+                    <div v-if="lesson.progress_percent === 100 || !released" 
+                         class="tw-z-10 tw-absolute tw-top-0 tw-w-full tw-h-full tw-left-0 tw-bg-black/70 tw-flex tw-flex-col tw-items-center tw-justify-center"
+                    >
                         <template v-if="!released">
                             <p class="tw-text-xs text-white font-bold">
                                 {{ day }},
