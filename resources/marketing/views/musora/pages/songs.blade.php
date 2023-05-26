e028103b014dc4e534c96da75623270a66a2d3b3
peterminhlee <petereminhlee@gmail.com>
2023-05-26 10:01:36 -0700

    badge url updates

--- resources/marketing/views/musora/pages/songs.blade.php
+++ resources/marketing/views/musora/pages/songs.blade.php
@@ -790,7 +790,7 @@
     </section>
 
     @include('musora.sales.components.guarantee-section', [
-        'badge' => 'https://musora-center.s3.amazonaws.com/logos/guarantee.jpg',
+        'badge' => 'https://dmmior4id2ysr.cloudfront.net/logos/guarantee+1.png',
         'header' => '<strong>Happy student guarantee.</strong><br>Test-drive your lessons for 90 days. Zero risk.',
         'desc' => 'Online lessons can be intimidating. Maybe you’re wondering if they work, or if you’ll use them enough – or if you’ll even enjoy the experience. So we’re removing the risk with our 90-day guarantee. More than anything, we want to make sure you have a POSITIVE experience developing new skills and gaining confidence on the drums.',
     ])
