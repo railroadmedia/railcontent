90a7b37929e15957410682568f76a209c385cf89
peterminhlee <petereminhlee@gmail.com>
2023-06-07 08:55:15 -0700

    removed modal js and foundation

--- resources/marketing/views/drumeo/products/misc-products-layout.blade.php
+++ resources/marketing/views/drumeo/products/misc-products-layout.blade.php
@@ -24,13 +24,6 @@
     <script type="text/javascript" src="{{ asset('/marketing/js/drumeo/ba-bbq.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/marketing/js/drumeo/misc.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
-    <script src="{{ asset('/marketing/js/modal-autoplay.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/marketing/js/modal.js') }}"></script>
-    <script>
-        $(document).ready(function () {
-            $(document).foundation();
-        });
-    </script>
 
     {{-- Platform --}}
     <script src="{{ mix('/platform/js/manifest.js') }}"></script>
