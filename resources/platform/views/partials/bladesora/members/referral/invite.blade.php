ecb5108f15e4c4a7a360f300a036a7603badce8b
peterminhlee <petereminhlee@gmail.com>
2023-06-07 13:40:33 -0700

    moved submit function to form

--- resources/platform/views/partials/bladesora/members/referral/invite.blade.php
+++ resources/platform/views/partials/bladesora/members/referral/invite.blade.php
@@ -69,7 +69,7 @@
                         class="tw-flex tw-flex-col lg:tw-h-full 2xl:tw-pl-10 tw-w-full tw-max-w-lg 2xl:tw-max-w-none tw-mx-auto">
                         <h5 class="tw-leading-tight tw-py-4 tw-text-lg">Give a friend unlimited access to {{ ucfirst($brand) }}, free for 30 days
                         </h5>
-                        <form id="invite-email-form" name="invite-email-form">
+                        <form id="invite-email-form" name="invite-email-form" onsubmit="sendPass(event)">
                             <label class="tw-inline-block tw-w-full tw-text-left tw-pt-6 tw-ml-6"
                                 for="email"><strong>Invite via email</strong></label>
                             <div
@@ -81,7 +81,7 @@
                                 <input
                                     class="tw-inline-block tw-text-black tw-w-full tw-mb-4 sm:tw-mb-0 sm:tw-mr-4 tw-default-form-field sm:tw-flex-grow tw-py-0 tw-px-[25px] tw-h-[50px] tw-rounded-[25px] tw-border"
                                     type="email" id="email" name="email" placeholder="Email address..." value="">
-                                <input name="button" type="button" id="button"
+                                <input name="button" type="submit" id="button"
                                     class=" tw-btn-primary tw-bg-{{ $brand }} hover:tw-bg-{{ $brand }}-600 tw-leading-none tw-text-lg tw-border-0 tw-rounded-full tw-select-none tw-cursor-pointer tw-text-center tw-py-4 tw-px-6 tw-uppercase tw-font-bebas-neue tw-text-white tw-flex-none tw-w-full sm:tw-w-52"
                                     value="Send Guest Pass" onclick="sendPass(event)" />
                             </div>
