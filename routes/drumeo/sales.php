6b212f694f0cd5c1284fc1955b84d3a9a995dd57
Trent <trent.harms@gmail.com>
2023-06-09 09:37:11 -0700

    updating url

--- routes/drumeo/sales.php
+++ routes/drumeo/sales.php
@@ -41,6 +41,7 @@ Route::domain('{drumeoDomain}')
     Route::get('/alesis', [SalesController::class, 'alesis']);
 
     Route::get('/june', [SalesController::class, 'trialJune']);
+    Route::get('/trial-key', [SalesController::class, 'trialJune']);
     Route::get('/trial', [SalesController::class, 'trial']);
     Route::get('/trial2', [SalesController::class, 'trial'] );
     Route::get('/trial-beginner', [SalesController::class, 'trialBeginner'] );
