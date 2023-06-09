2621668944fd2c934e8a6296bdf752d3658f69c1
peterminhlee <petereminhlee@gmail.com>
2023-06-09 13:16:48 -0700

    added draft field

--- app/Nova/Carousel.php
+++ app/Nova/Carousel.php
@@ -58,6 +58,7 @@ class Carousel extends Resource
             BelongsTo::make('Brand', 'brand', 'App\Nova\Brand')->sortable(),
             Text::make('name')->sortable()->help('For easy reference to this banner in the CMS. This info won\'t show on the banner.')->required()->rules('required'),
             Boolean::make('visible')->hideFromIndex()->default(true),
+            Boolean::make('draft')->hideFromIndex()->default(true),
             Number::make('Display Order', 'display_order')->hideFromIndex(),
             DateTime::make('Start Time', 'start_date')->hideFromIndex()->help('Ignore UTC. It is actually PST.<br>This does NOT account for Daylight Savings between Mar-Nov. Make sure you offset by an hour during PDT'),
             DateTime::make(__('End Time'), 'end_date')->hideFromIndex()->help('Ignore UTC. It is actually PST.<br>This does NOT account for Daylight Savings between Mar-Nov. Make sure you offset by an hour during PDT'),
@@ -241,8 +242,10 @@ class Carousel extends Resource
             Text::make('Video Source', 'video_src')->hideFromIndex()
                 ->hideFromDetail()
                 ->help('Automatically replaces the second button URL with the pop-up video player. Use Vimeo links only, e.g. //player.vimeo.com/video/798501810?autoplay=1. In Vimeo, video permissions must be at least set to "Hidden from Vimeo", and cannot be set to "Unlisted".'),
+
             //duplicated fields for displaying index page purpose
             Number::make('Display Order', 'display_order')->hideFromDetail()->hideWhenUpdating()->hideWhenCreating(),
+            Boolean::make('draft')->hideFromDetail()->hideWhenUpdating()->hideWhenCreating()->sortable(),
             Boolean::make('visible')->hideFromDetail()->hideWhenUpdating()->hideWhenCreating()->sortable(),
         ];
     }
