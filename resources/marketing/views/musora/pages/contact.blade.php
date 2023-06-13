296d842fb296dde03a1096da2c6e069357119d30
peterminhlee <petereminhlee@gmail.com>
2023-06-13 15:57:46 -0700

    text updates

--- resources/marketing/views/musora/pages/contact.blade.php
+++ resources/marketing/views/musora/pages/contact.blade.php
@@ -89,7 +89,7 @@
         <div class="container max-w-3xl mx-auto text-center">
             <h3><strong>Reach Out Directly</strong></h3>
             <p class="mt-4 mb-5 text-sm md:text-base max-w-3xl mx-auto">
-                Get in touch with your dedicated Musora Mentor, [dynamic]!
+                Get in touch with your dedicated Musora Mentor!
             </p>
             <div class="text-left">
                 <contact-email-form-marketing
