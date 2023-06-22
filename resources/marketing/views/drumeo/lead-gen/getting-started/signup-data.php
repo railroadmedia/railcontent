ae9fab59aa3349c58964fa61d685ec628e0d99a9
peterminhlee <petereminhlee@gmail.com>
2023-06-22 11:04:43 -0700

    sign up data

--- resources/marketing/views/drumeo/lead-gen/getting-started/signup-data.php
+++ resources/marketing/views/drumeo/lead-gen/getting-started/signup-data.php
@@ -1,1 +1,81 @@
 <?php
+$lessons = [
+    [
+        "title" => "Pre-beginner",
+        'lessonNum' => 3,
+        'lessonInfo' => [
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/efe2fb6c-00be-40f5-a912-01c027b0ede5-GettingStartedOnTheDrums-01.jpg',
+                'title' => ' Listening For The Drums In Music',
+                'desc' => 'Domino shows you how to identify the key pieces of a drum set in a real song so that you can start learning how to play them.',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/efe2fb6c-00be-40f5-a912-01c027b0ede5-GettingStartedOnTheDrums-02.jpg',
+                'title' => 'Drumming Coordination',
+                'desc' => 'Domino shows you how to learn basic drumming coordination by tapping on your legs – this one skill will help you play hundreds of songs.',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/efe2fb6c-00be-40f5-a912-01c027b0ede5-GettingStartedOnTheDrums-03.jpg',
+                'title' => 'Adding Your Foot',
+                'desc' => 'In this video, you’ll learn how to add your foot into a real drum beat simply by tapping on the floor. You’ll be playing 3 things at once!',
+            ],
+        ]
+    ],
+    [
+        "title" => "Buying guide for drummers",
+        'lessonNum' => 4,
+        'lessonInfo' => [
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/d7b38fa6-81b1-4dda-ad69-bbc90a59a582-GettingStartedOnTheDrums-04.jpg',
+                'title' => 'Practice Pad',
+                'desc' => 'A practice pad is a practical, inexpensive option to help you get started on the drums. Domino discusses how to use one and where you can find your first practice pad.',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/f2c80abe-d94f-458b-9f30-172a51e6c8a9-GettingStartedOnTheDrums-05.jpg',
+                'title' => 'Electronic Drum Set',
+                'desc' => 'If you’re ready for your first E-Kit, Domino will help you navigate the pros and cons of using an electronic drum set. If you’re in an apartment or townhouse, this is for you!',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-06.jpg',
+                'title' => 'Acoustic Drum Set',
+                'desc' => 'Every drummer’s dream! In this video, you’ll learn how to shop for an acoustic drum set – what to look for and how much you can expect to pay for your first kit.',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-07.jpg',
+                'title' => 'Setting Up Your Kit',
+                'desc' => 'Domino gives you a simple overview of how to set up and position your drums. These tips will help you ensure you’re comfortable sitting behind the drums.',
+            ],
+        ]
+    ],
+    [
+        "title" => "Beginner drummer",
+        'lessonNum' => 5,
+        'lessonInfo' => [
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-08.jpg',
+                'title' => 'Your First Drum Beat',
+                'desc' => 'Domino shows you the basic coordination to start playing your first beat on the drums. This one drum beat will unlock hundreds of songs you can play!',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-09.jpg',
+                'title' => 'Beginner Drum Workout',
+                'desc' => 'In this 5-minute lesson, Domino plays along with you as you play your first drum beat. This lesson helps you learn the groove & coordination you need to fall in love with playing the drums.',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-10.jpg',
+                'title' => 'How To Read Drum Music',
+                'desc' => 'Domino teaches you the alphabet of drum music so you can start learning any song you want!',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-11.jpg',
+                'title' => 'Song Tutorial',
+                'desc' => 'Domino shows you how to use a handy practice tool to learn your favorite songs. This lesson includes a special invitation!',
+            ],
+            [
+                'thumb' => 'https://d1fyshwdvi6fth.cloudfront.net/Lead-gens/Thumbnails/4c2dcbb9-11dd-41cf-b9a9-69d0ce385196-GettingStartedOnTheDrums-12.jpg',
+                'title' => 'What’s Next? (BONUS)',
+                'desc' => 'Domino gives you an example of all the songs you can play with the skills you’ve learned in this course.',
+            ],
+        ]
+    ],
+];
