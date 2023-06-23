caa413278bb8e0140f9d98dcc159f2f380077293
Miguel Fondeur <miguelfondeur@gmail.com>
2023-06-23 19:15:24 +0000

    update

--- resources/platform/assets/js/vue/components/Playlists/Modals/CreatePlaylist.vue
+++ resources/platform/assets/js/vue/components/Playlists/Modals/CreatePlaylist.vue
@@ -246,7 +246,7 @@
     onBeforeMount(()=> {
         console.log('additional items', typeof props.playlist.additionalItems, props.playlist.additionalItems)
         //Load Data
-        // state.thumb = props.playlist.thumbnail_url;
+        state.thumb = props.playlist.thumbnail_url;
         state.name = props.mode === 'duplicate' ? props.playlist.name + ' (Duplicate)' : props.playlist.name;
         state.description = props.playlist.description;
         state.category = props.playlist.category || 'General';
