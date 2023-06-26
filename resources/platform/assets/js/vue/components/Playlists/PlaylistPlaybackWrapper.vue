028b5370b6bb004b78fea57463b1d5bad2ef4770
calebfavor <calebfavor@gmail.com>
2023-06-26 17:51:43 +0000

    changed soundslice container height in playback mode

--- resources/platform/assets/js/vue/components/Playlists/PlaylistPlaybackWrapper.vue
+++ resources/platform/assets/js/vue/components/Playlists/PlaylistPlaybackWrapper.vue
@@ -297,7 +297,7 @@ onBeforeMount(() => {
                         :message="needAccessMessage" />
                     <!-- Soundslice Player -->
                     <div v-if="(lessonType === 'song' || lessonType === 'assignment' || lessonType === 'routine') && !needAccess"
-                        class="tw-w-full tw-aspect-video"
+                        class="tw-w-full tw-aspect-video tw-max-h-[90vh]"
                         :class="{ 'tw-max-w-[1280px]' : !playlistsStore.playerExpanded }"
                     >
                         <SoundSlice :user-id="userId" :theme-color="brand" :additional-params="additionalSoundsliceParams"
