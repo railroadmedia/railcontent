20173b56afbc09c03020fb4918b1207bfe9f0f3e
peterminhlee <petereminhlee@gmail.com>
2023-06-28 09:54:59 -0700

    lesson index tile updates

--- resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-lead-gen-index-layout.blade.php
@@ -59,9 +59,8 @@
             </div>
 
         </div>
-        <div class="lesson-catalogue max-w-5xl mx-auto py-7 md:py-12">
-            <div class="list-wrapper px-2 sm:px-4">
-                <h3 class="font-black mb-4 md:mb-6 px-1">Course Lessons</h3>
+        <div class="lesson-catalogue max-w-5xl mx-auto py-12 md:py-20">
+            <div class="list-wrapper px-2 sm:px-4 grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-4">
                 @foreach($lessons as $key => $lesson)
                     @include('_partials.components.leadgen-lesson',[
                         'lessonURL' => $lesson->slug,
