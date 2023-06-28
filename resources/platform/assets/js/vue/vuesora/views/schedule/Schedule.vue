c6e3af865b6a1987cfd2b1f8abdf2aa0fa2a0608
calebfavor <calebfavor@gmail.com>
2023-06-28 21:11:36 +0000

    preloaded content

--- resources/platform/assets/js/vue/vuesora/views/schedule/Schedule.vue
+++ resources/platform/assets/js/vue/vuesora/views/schedule/Schedule.vue
@@ -73,6 +73,8 @@ export default {
 
     },
     mounted() {
+        console.log('preloaded content', this.preloadedContent);
+
         const monthsSelector = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
         monthsSelector.forEach((month) => {
@@ -83,7 +85,7 @@ export default {
         });
     },
     beforeMount() {
-        console.log(this.item);
+        console.log('preloaded content', this.preloadedContent);
     },
     methods: {
         addEventToDropdown(payload) {
