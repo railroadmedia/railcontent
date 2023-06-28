3167831d41386407bcb6e9c0831152b170012d22
Miguel Fondeur <miguelfondeur@gmail.com>
2023-06-28 21:06:28 +0000

    added customer io sdk snipper to layout

--- resources/platform/views/partials/layout.blade.php
+++ resources/platform/views/partials/layout.blade.php
@@ -87,6 +87,27 @@
             window.userNavigationDropdownLinks = {!! $userNavigationDropdownLinksJson ?? '' !!};
         </script>
 
+        {{-- Customer.io --}}
+        <script type="text/javascript">
+            var _cio = _cio || [];
+            (function() {
+                var a,b,c;a=function(f){return function(){_cio.push([f].
+                concat(Array.prototype.slice.call(arguments,0)))}};b=["load","identify",
+                "sidentify","track","page","on","off"];for(c=0;c<b.length;c++){_cio[b[c]]=a(b[c])};
+                var t = document.createElement('script'),
+                    s = document.getElementsByTagName('script')[0];
+                t.async = true;
+                t.id    = 'cio-tracker';
+                t.setAttribute('data-site-id', 'YOUR_SITE_ID');
+                t.setAttribute('data-use-array-params', 'true');
+                t.setAttribute('data-use-in-app', 'true');
+                t.src = 'https://assets.customer.io/assets/track.js';
+                //If your account is in the EU, use:
+                //t.src = 'https://assets.customer.io/assets/track-eu.js'
+                s.parentNode.insertBefore(t, s);
+            })();
+        </script>
+
         @yield('layout-scripts')
             <script src="{{ mix('platform/js/manifest.js') }}"></script>
             <script src="{{ mix('platform/js/vendor.js') }}"></script>
