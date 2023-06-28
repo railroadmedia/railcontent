9ae56f38b22558e750a7a044cfb5112113e23a05
yalungtang <yalungtang@gmail.com>
2023-06-28 08:21:07 -0700

    debug

--- resources/platform/assets/js/vue/vuesora/views/schedule/Schedule.vue
+++ resources/platform/assets/js/vue/vuesora/views/schedule/Schedule.vue
@@ -73,6 +73,7 @@ export default {
 
     },
     mounted() {
+        console.log('schedule content', this.content);
         const monthsSelector = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
         monthsSelector.forEach((month) => {
