c1e67080d3adb7c7b5da736590ab5e19ca37a380
Trent <trent.harms@gmail.com>
2023-07-04 17:09:28 -0700

    temp revert

--- resources/platform/assets/js/vue/components/PageContainer/PageContainer.vue
+++ resources/platform/assets/js/vue/components/PageContainer/PageContainer.vue
@@ -273,7 +273,7 @@ onUpdated(() => {
         :forceSidebarHidden="forceSidebarHidden" :user-id="userId" />
 
       <!-- Content Container -->
-      <section class="
+      <main class="
           tw-flex
           tw-w-full
           tw-h-full
@@ -288,10 +288,11 @@ onUpdated(() => {
         <h2 v-if="adminMessage && adminMessage.length" class="tw-text-[18px] tw-w-full tw-bg-yellow-200 tw-p-[20px] tw-text-center">
           {{ adminMessage }}
         </h2>
-        <!-- Content | Accounting for footer heithg in min height -->
-        <div class="tw-flex tw-flex-col tw-grow tw-h-full tw-w-full tw-min-h-[calc(100vh-152px)] lg:tw-min-h-[calc(100vh-128px)]">
+        <!-- Content -->
+        <section class="tw-flex tw-flex-col tw-grow tw-w-full">
           <slot :is-dark-mode="isDarkModeSelected" />
-        </div>
+        </section>
+
         <!-- Footer -->
         <Footer :brand="brand" />
 
@@ -303,7 +304,7 @@ onUpdated(() => {
               tw-top-0 tw-left-0 tw-w-full tw-h-full tw-z-10 tw-bg-black/30
             "></div>
         </Transition>
-      </section>
+      </main>
     </div>
   </main>
 </template>
