93fd2968046b415cd1c1098031dd4db000d09c4d
peterminhlee <petereminhlee@gmail.com>
2023-07-06 16:00:32 -0700

    bundle data

--- resources/marketing/views/drumeo/drumshop/bundles.php
+++ resources/marketing/views/drumeo/drumshop/bundles.php
@@ -1,1 +1,27 @@
 <?php
+    $bundles = [
+        [
+            'slug' => '/drumshop/hands-and-feet-bundle',
+            'badgeText' => 'Free quietkick & quietpad',
+            'title' => 'Improve Your Hands & Feet Anywhere',
+            'desc' => 'Annual Drumeo membership + QuietKick, QuietPad, sticks, and more!',
+            'priceColor' => '#1370DC',
+            'price' => '691',
+            'discountedPrice' => '200',
+            'img' => 'https://dpwjbsxqtam5n.cloudfront.net/promos/july/hands-feet-bundle-shop.jpg',
+            'buttonColor' => '#0F73DC',
+            'visible' => 1,
+        ],
+        [
+            'slug' => '/drumshop/beginner-bundle',
+            'badgeText' => 'Free practice & drumsticks',
+            'title' => 'Everything You Need To Get Started',
+            'desc' => 'Annual Drumeo membership + a FREE book, practice pad, and more!',
+            'priceColor' => '#BC0E90',
+            'price' => '682',
+            'discountedPrice' => '200',
+            'img' => 'https://dpwjbsxqtam5n.cloudfront.net/promos/july/beginner-shop.jpg',
+            'buttonColor' => '#BC0E90',
+            'visible' => 1,
+        ],
+    ];
--- resources/marketing/views/pianote/shop/bundles.php
+++ resources/marketing/views/pianote/shop/bundles.php
@@ -1,1 +1,28 @@
 <?php
+
+    $bundles = [
+        [
+            'slug' => '/shop/bundle-unlimited-lessons',
+            'badgeText' => '7 free gifts worth $460',
+            'title' => 'The Unlimited Lessons Bundle',
+            'desc' => 'Annual Pianote membership + books, posters, and more!',
+            'priceColor' => '#F16B1C',
+            'price' => '699',
+            'discountedPrice' => '200',
+            'img' => 'https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/july/unlimited-lessons-bundle-shop.jpg',
+            'buttonColor' => '#F16B1C',
+            'visible' => 1,
+        ],
+        [
+            'slug' => '/shop/5-for-3-bundle',
+            'badgeText' => '13 free gifts worth $1007',
+            'title' => 'Pianote 5-For-3 Bundle',
+            'desc' => 'A 5-Year Membership for the price of 3, and more!',
+            'priceColor' => '#63053A',
+            'price' => '2207',
+            'discountedPrice' => '720',
+            'img' => 'https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/july/pianote-5-for-3-shop.jpg',
+            'buttonColor' => '#63053A',
+            'visible' => 1,
+        ],
+    ];
