7449a5f84acc0f3379900badec3b2e42f3def8e1
peterminhlee <petereminhlee@gmail.com>
2023-07-10 11:57:29 -0700

    fix

--- resources/platform/assets/js/vue/components/HeaderCarousel/CarouselSlide.vue
+++ resources/platform/assets/js/vue/components/HeaderCarousel/CarouselSlide.vue
@@ -150,7 +150,7 @@ const secondaryVideoModal = ref(false);
             {{ title }}
           </h2>
             <!-- Logo -->
-            <img v-if="logo" class="tw-h-24 md:tw-h-28 3xl:tw-h-32 mb-1 tw-mr-auto" :src="`https://www.musora.com/musora-cdn/image/width=800,quality=85${logo}`" alt="pack logo" />
+            <img v-if="logo" class="tw-h-24 md:tw-h-28 3xl:tw-h-32 mb-1 tw-mr-auto" :src="`https://www.musora.com/musora-cdn/image/width=800,quality=85/${logo}`" alt="pack logo" />
           <p :class="`tw-hidden xl:tw-block xl:tw-line-clamp-3 tw-text-lg tw-max-w-[520px] ${ descriptionColor && `tw-text-${descriptionColor}` }`" v-html="description"></p>
           <!-- CTA -->
             <div class="tw-mt-2 md:tw-mt-4 xl:tw-mt-6 tw-flex tw-items-center md:tw-block">
