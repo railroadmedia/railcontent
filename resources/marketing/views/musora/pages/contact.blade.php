d3d1b2525cba1233da5de167a40a116cf85965ff
peterminhlee <petereminhlee@gmail.com>
2023-07-17 13:34:47 -0700

    beacon updates

--- resources/marketing/views/musora/pages/contact.blade.php
+++ resources/marketing/views/musora/pages/contact.blade.php
@@ -17,13 +17,9 @@
         }
     </style>
 
-    <script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});</script>
-    <script type="text/javascript">
-        window.Beacon('init', '9f119028-29b4-4fcd-8b9d-03c86f3d2521')
-        Beacon('on', 'ready', () => {
-            document.querySelector('.BeaconFabButtonFrame').style.bottom ="50px";
-        })
+    <script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
     </script>
+    <script type="text/javascript">window.Beacon('init', 'e8d02e5b-8d06-4b6c-ae78-f2ca95d81b59')</script>
 @endsection
 
 <!-- Main -->
