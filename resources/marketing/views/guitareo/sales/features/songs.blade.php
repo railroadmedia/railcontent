56b1b3383a75850d25a61976c2cf9b952247e683
Trent <trent.harms@gmail.com>
2023-07-19 10:21:27 -0700

    fixing songs page

--- resources/marketing/views/guitareo/sales/features/songs.blade.php
+++ resources/marketing/views/guitareo/sales/features/songs.blade.php
@@ -284,57 +284,6 @@
                 @endcomponent
             </div>
             <div class="mb-6">
-                <h4 class="font-extrabold mb-4">Surf</h4>
-                @component('_partials.components.carousel',[
-                    'xdata' => "
-                        classes: {
-                            arrow: 'splide__arrow bg-white opacity-100 top-[38%] shadow-lg h-11 w-11',
-                            prev: 'hidden',
-                            next: 'splide__arrow--next your-class-next hidden sm:flex z-50 -right-1',
-                            pagination: 'hidden',
-                        },
-                        perPage: 6.5,
-                        perMove: 1,
-                        type: 'loop',
-                        interval: 2000,
-                        breakpoints: {
-                            1160: {
-                                perPage: 5.5,
-                            },
-                            900: {
-                                perPage: 4.5,
-                            },
-                            760: {
-                                perPage: 3.5,
-                                drag   : 'free',
-                                snap   : false,
-                            },
-                            590: {
-                                perPage: 2.5,
-                            },
-                        },
-                    ",
-                ])
-                    @slot('content')
-                        @foreach ($surf as $slide)
-                            <li class="splide__slide px-1">
-                                <div>
-                                    <img
-                                        class="rounded-xl mb-1 transition-opacity opacity-0"
-                                        src="https://www.musora.com/musora-cdn/image/width=250,quality=95/{{$slide['img']}}"
-                                        alt="{{$slide['title']}} img"
-                                        loading="lazy"
-                                        onload="this.classList.remove('opacity-0')"
-                                    >
-                                    <p class="font-bold mb-1 leading-tight">{{$slide['title']}}</p>
-                                    <p class="text-[#838C98] leading-tight">{{$slide['artist']}}</p>
-                                </div>
-                            </li>
-                        @endforeach
-                    @endslot
-                @endcomponent
-            </div>
-            <div class="mb-6">
                 <h4 class="font-extrabold mb-4">Blues</h4>
                 @component('_partials.components.carousel',[
                     'xdata' => "
