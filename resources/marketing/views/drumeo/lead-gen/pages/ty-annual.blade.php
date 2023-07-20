b0523f2d6b10928ef79dc8c26e26688ebcde9d81
Trent <trent.harms@gmail.com>
2023-07-20 14:41:43 -0700

    testing redirect

---
+++ resources/marketing/views/drumeo/lead-gen/pages/ty-annual.blade.php
@@ -0,0 +1,5 @@
+@extends('drumeo.lead-gen.lead-gen-layout-tw')
+
+@section('meta')
+    <meta http-equiv="Refresh" content="0; url='https://www.drumeo.com/ecommerce/add-to-cart?products[DLM-Trial-Annual-30-Day]=1&locked=true'" />
+@stop
