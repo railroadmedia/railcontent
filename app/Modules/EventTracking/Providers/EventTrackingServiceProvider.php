b6d2213269f9dd0483c4ce155dfa773d99dd507f
robwillems <robert@musora.com>
2023-08-03 16:53:16 -0700

    Try to fix logging with new logger class

--- app/Modules/EventTracking/Providers/EventTrackingServiceProvider.php
+++ app/Modules/EventTracking/Providers/EventTrackingServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Modules\EventTracking\Providers;
 
 use App\Modules\EventTracking\Destinations\RudderDestination;
 use App\Modules\EventTracking\Listeners\EventTrackerListener;
+use App\Modules\EventTracking\Logger;
 use App\Providers\EventServiceProvider;
 use Avo;
 use Illuminate\Support\ServiceProvider;
@@ -28,7 +29,7 @@ class EventTrackingServiceProvider extends EventServiceProvider
 
         Avo::init_avo([
             "env" => app()->isProduction() ? "prod" : "dev",
-            "logger" => \Log::getLogger(),
+            "logger" => new Logger(),
             "rudder_stack_instance" => new RudderDestination(),
         ]);
     }
