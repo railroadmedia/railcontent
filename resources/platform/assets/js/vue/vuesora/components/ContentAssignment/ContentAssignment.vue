688ddc602124d1764e3da75552d2054e2d28d350
peterminhlee <petereminhlee@gmail.com>
2023-08-03 15:40:34 -0700

    add to playlist and expand/collapse icon updates

--- resources/platform/assets/js/vue/vuesora/components/ContentAssignment/ContentAssignment.vue
+++ resources/platform/assets/js/vue/vuesora/components/ContentAssignment/ContentAssignment.vue
@@ -5,7 +5,7 @@
                 <div class="flex flex-row align-v-center">
                     <div class="flex flex-column arrow-column hide-xs-only">
                         <button class="btn collapse-square" @click="openAssignment">
-                            <span class="bg-grey-3" :class="accordionButtonClasses">
+                            <span class="tw-border-2 tw-border-solid tw-border-[#000C17] tw-text-[#000C17] dark:tw-text-white dark:tw-border-white tw-h-[50px] tw-w-[50px] tw-rounded-full tw-flex tw-justify-center tw-items-center">
                                 <i class="fas" :class="accordionButtonIconClasses"></i>
                             </span>
                         </button>
@@ -25,10 +25,13 @@
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-column flex-auto body ph-2 hide-sm-up pointer dark:tw-text-white"
-                        @click="openAssignment">
-                        <i class="fas" :class="accordionButtonIconClasses"></i>
-                    </div>
+                    <button v-if="soundsliceSlug"
+                            title="Add To Playlist"
+                            class="dark:tw-text-white tw-text-[#00101D] md:tw-hidden"
+                            @click.stop.prevent="addToPlaylist({ content_id: id, brand: this.brand, type: 'General', name: lessonTitle, description: '', thumbnail_url: lessonThumbnail })"
+                    >
+                        <musora-icon icon-name="plus" class="tw-h-8 tw-w-8 font-bold" />
+                    </button>
                 </div>
             </div>
             <div class="flex flex-column tw-ml-auto complete-column tw-items-start">
@@ -40,7 +43,7 @@
                         <i class="fas fa-play mr-1"></i> Practice
                     </button>
 
-                    <button class="tw-w-full md:tw-w-[250px] md:tw-mr-2 tw-mb-2"
+                    <button class="tw-w-full md:tw-w-[250px] md:tw-mr-2 tw-mb-2 md:tw-mb-0"
                         :class="isComplete ? `tw-btn-primary ${brandBgColor}` : `tw-btn-secondary ${brandTextColor}`"
                         :disabled="isRequesting" @click.stop="markAsComplete">
                         <i class="fas fa-check mr-1"></i>
@@ -49,11 +52,11 @@
 
                     <!-- New Add To Playlist Button -->
                     <button v-if="soundsliceSlug"
-                            title="Add To Playlist" 
-                            class="tw-btn-secondary tw-btn-circle dark:tw-text-white tw-text-[#00101D]"
+                            title="Add To Playlist"
+                            class="dark:tw-text-white tw-text-[#00101D] tw-hidden md:tw-block"
                             @click.stop.prevent="addToPlaylist({ content_id: id, brand: this.brand, type: 'General', name: lessonTitle, description: '', thumbnail_url: lessonThumbnail })"
                     >
-                        <musora-icon icon-name="plus" class="tw-h-7 tw-w-7" />
+                        <musora-icon icon-name="plus" class="tw-h-10 tw-w-10 font-bold" />
                     </button>
                 </div>
             </div>
