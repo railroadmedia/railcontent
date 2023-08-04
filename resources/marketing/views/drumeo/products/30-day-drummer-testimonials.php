aa6a08f836b4ca6331242989d7432921fdd799d5
peterminhlee <petereminhlee@gmail.com>
2023-08-04 10:53:29 -0700

    30dd3 updates

---
+++ resources/marketing/views/drumeo/products/30-day-drummer-testimonials.php
@@ -0,0 +1,52 @@
+<?php
+ $testimonials = [
+     [
+         'name' => 'Erin W.',
+         'comment' => 'I am shocked at how much I learned in these last 30 days. My husbands a drummer so he watched me go <b>from knowing NOTHING to being able to make it through a song.</b>',
+         'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/user-profile-picture-1678130486-572603.jpg',
+     ],
+     [
+         'name' => 'Kristyn T.',
+         'comment' => '<b>I\'m SOO EXCITED to have actually learned my first entire song ever!!!</b>  Such a HIGH!!!  Domino is like having a dear sweet encouraging friend and mentor to work with daily! PLEASE make more 30 Day Series!!! Don\'t let the FUN and Magic end!!!',
+         'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/user-profile-picture-1679111185-496776.jpg',
+     ],
+     [
+         'name' => 'Tobias W.',
+         'comment' => 'This was awesome! Thank you so much Domino for the last 30 days.<br/>
+         The idea behind this format is really great. These short sessions can easily be fit into everyday life, no need to think or prepare what\'s next. <br/>
+        It is like having a personal trainer but for drums, telling you exactly what to do. <b>It keeps the guesswork out of learning the drums so you can focus on the most important: Playing the drums and having fun.</b>',
+         'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/user-profile-picture-1669379419-276550.jpg',
+     ],
+     [
+         'name' => 'Stephiekitty (Estephania E.)',
+         'comment' => '<b>I never thought I’d be a drummer but here I am! I went from zero to hero all thanks to Domino and Drumeo!</b> I would have never tried drums had it not been for this 30 day drummer program💕🥁🎵🎶',
+         'img' => 'https://dzryyo1we6bm3.cloudfront.net/avatars/526989_1660712845376-1660712847-526989.jpg',
+     ],
+     [
+         'name' => 'Alan C.',
+         'comment' => '<b>This has been an amazing journey. Five weeks ago I didn\'t own a drum kit – Today I played a whole song twice!</b> I wouldn\'t have believed it possible if I hadn\'t just experienced it. Big thanks to Domino and all at Drumeo',
+         'img' => 'https://dzryyo1we6bm3.cloudfront.net/avatars/412320_1662361641020-1662361646-412320.jpg',
+     ],
+     [
+         'name' => 'Shae C.',
+         'comment' => '👏👏👏Round of applause and standing ovation to you Domino, Kyle and the entire Drumeo team. <b>Yes! I’m so happy and so proud. I still can’t believe I played my first ever drum song. And I did it twice!! So very happy right now, it’s making me tear up.</b> Thank you Domino, you’re an awesome coach. You made this course so enjoyable even when it is challenging at times for a newbie like me.',
+         'img' => 'https://dzryyo1we6bm3.cloudfront.net/avatars/521912_1663974802851-1663974809-521912.jpg',
+     ],
+     [
+         'name' => 'Monique G.',
+         'comment' => 'When I was in the last 30 seconds I got really emotional. This was such an incredible experience! I\'m so proud of myself! Thanks to the Drumeo team and all the wonderful mods, and the community. Most importantly--thank you to Domino. <br>
+          Your wonderful spirit and enthusiasm, combined with this method of teaching really worked for me and (obviously) so many others. <b>I\'m thrilled that I\'m back behind a drum kit after 30 years and not only loving it, but genuinely understanding it. Thank you, thank you, thank you! 🥁</b>',
+         'img' => 'https://dzryyo1we6bm3.cloudfront.net/avatars/528540_1661460541688-1661460545-528540.jpg',
+     ],
+     [
+         'name' => 'David Stanley',
+         'comment' => '<b>This has just been the best experience.</b> In less than 30 days I\'ve gone from the occasional uncoordinated bash on my son\'s acoustic kit to sounding like I almost know what I\'m doing. <br/>
+            I\'m really going to miss my daily dose of Domino. She\'s been the perfect teacher: fun, eternally encouraging, and just so authentic. And was I the only person to get that tingly feeling of nervous excitement as the daily video intro music played, knowing that in a few seconds hence, we\'d be launching into 11 minutes of amazing?!',
+         'img' => 'https://s3.amazonaws.com/pianote/defaults/avatar.png',
+     ],
+     [
+         'name' => 'Evan W.',
+         'comment' => 'An absolutely fabulous program… for yourself, your son, your daughter or otherwise. Filled with great energy, and sound fundamentals. I purchased this program for my daughter…and she had soooooooo much fun! 🤩 If you’re thinking about it - don’t think, just do! ⚡️',
+         'img' => 'https://s3.amazonaws.com/pianote/defaults/avatar.png',
+     ],
+ ];
