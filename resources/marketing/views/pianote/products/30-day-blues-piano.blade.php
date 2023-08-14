8cab9d8db02b5316b97e75b55189f58c3b4e0764
Trent <trent.harms@gmail.com>
2023-08-14 14:11:42 -0700

    fixing countdown date

--- resources/marketing/views/pianote/products/30-day-blues-piano.blade.php
+++ resources/marketing/views/pianote/products/30-day-blues-piano.blade.php
@@ -542,7 +542,7 @@
     ])
 
     @include('_partials.components.countdown',[
-        'countdownDate' => '2023-09-04 00:00:00',
+        'countdownDate' => '2023-09-01 00:00:00',
         'promoVersion' => false
     ])
     @include("pianote.sales.partials._footer")
