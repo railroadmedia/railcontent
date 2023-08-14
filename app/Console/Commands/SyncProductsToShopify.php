f223deea64555668026a5f22ce411d942a1af7f6
Brandon Kerr <brandonk@musora.com>
2023-08-14 16:39:01 -0400

    Shopify sync support for digital products

--- app/Console/Commands/SyncProductsToShopify.php
+++ app/Console/Commands/SyncProductsToShopify.php
@@ -37,6 +37,10 @@ class SyncProductsToShopify extends Command
     protected ProductRepository $productRepository;
     protected EcommerceEntityManager $entityManager;
 
+    const PRODUCT_TYPE_DIGITAL_ONE_TIME = "digital one time";
+    const PRODUCT_TYPE_DIGITAL_SUBSCRIPTION = "digital subscription";
+    const PRODUCT_TYPE_PHYSICAL_ONE_TIME = "physical one time";
+
     /**
      * Execute the console command.
      *
@@ -70,7 +74,7 @@ class SyncProductsToShopify extends Command
         $shopifyIds = collect();
 
         // STEP 2: get all the products that need to be synced
-        $products = $this->getEcommerceEntities($fresh)->take(2);
+        $products = $this->getEcommerceEntities($fresh);
 
         $this->info("Found {$products->count()} products to be synced");
 
@@ -82,57 +86,22 @@ class SyncProductsToShopify extends Command
 
         $products->each(function (Product $product) use ($fresh, $bar, $simulate, $shopifyIds, $location, &$tableRows) {
             // STEP 3: determine if updating or creating
-            $isCreating = is_null($product->getShopifyId());
+            $isCreating = $fresh || is_null($product->getShopifyId());
 
             // STEP 4: build up the data structure depending on product type
-            //TODO do whatever special stuff for clothes. handle each type specially to make the nested variants and whatnot
-            // and review what sku to use (maybe it varies by product type)?
-
-            //TODO: check if there's a shopifyId (and not doing fresh) so we know if we're updating or creating
-
-            $postData = [
-                "title" => $product->getName(),
-                "body_html" => $product->getDescription(),
-                "vendor" => Str::ucfirst($product->getBrand()),
-                "product_type" => Str::ucfirst($product->getType()),
-                //TODO maybe need to do this separately
-                "images" => [
-                    [
-                        "src" => $product->getThumbnailUrl(),
-                    ]
-                ],
-                // TODO refer to https://shopify.dev/docs/apps/custom-data/metafields/types
-                // we can use meta fields for stuff like our product id, etc
-                //NOTE that updating a product with metafields seems to break, because keys "must be unique within this namespace on this resource"
-                // "metafields" => [
-                //     [
-                //         // "product_id" => $product->getId()
-                //         "key" => "_id",
-                //         "value" => $product->getId(),
-                //         "type" => "number_integer",
-                //         "namespace" => "products"
-                //     ]
-                // ],
-                // TODO?
-                // "tags" => "",
-                // TODO: need something to determine if draft or archived when it's not active
-                "status" => $product->getActive() ? "active" : "draft",
-                //TODO: this creates a new variant (and removes the old one), so be careful when doing updates
-                "variants" => [
-                    [
-                        "price" => number_format($product->getPrice(), 2),
-                        "sku" => $product->getInventoryControlSku(),
-                        //TODO?
-                        "taxable" => true,
-                        "weight" => $product->getWeight(),
-                        "weight_unit" => "lb",
-                        "inventory_management" => "shopify",
-                        "inventory_quantity" => $product->getStockAvailability(),
-                        "requires_shipping" => $product->getIsPhysical(),
-                        "location_id" => $location->getAttributes()["id"]
-                    ]
-                ]
-            ];
+            if ($product->getType() === self::PRODUCT_TYPE_DIGITAL_SUBSCRIPTION
+            || $product->getType() === self::PRODUCT_TYPE_DIGITAL_ONE_TIME) {
+                $postData = $isCreating ? $this->createProductData($product, $location->getAttributes()["id"])
+                    : $this->updateProductData($product, $location->getAttributes()["id"]);
+            } elseif($product->getType() === self::PRODUCT_TYPE_PHYSICAL_ONE_TIME){
+                //TODO do whatever special stuff for clothes. handle each type specially to make the nested variants and whatnot
+                $postData = [];
+                return;
+            } else {
+                throw new \Exception(sprintf("Unknown product type %s. Expected types are: %s",
+                        $product->getType(), implode(", ", [$this->PRODUCT_TYPE_DIGITAL_ONE_TIME,
+                        $this->PRODUCT_TYPE_DIGITAL_SUBSCRIPTION, $this->PRODUCT_TYPE_PHYSICAL_ONE_TIME])));
+            }
 
             // STEP 5: send the data to Shopify
             if (!$simulate) {
@@ -143,6 +112,10 @@ class SyncProductsToShopify extends Command
                     $variant = $this->shopify->getVariant($product->getShopifyId());
                     $productID = $variant->getAttributes()["product_id"];
                     $productResource = $this->shopify->updateProduct($productID, $postData);
+                    // if we're updating something in the variant, also call Shopify to update the variant
+                    if (array_key_exists("variants", $postData)) {
+                        $this->shopify->updateVariant($product->getShopifyId(), $postData["variants"]);
+                    }
                 }
 
                 /** @var VariantResource $variantData */
@@ -151,14 +124,16 @@ class SyncProductsToShopify extends Command
                 $shopifyIds->push($shopifyId);
 
                 // record the shopify ID
-                $product->setShopifyId($shopifyId);
-                $this->entityManager->persist($product);
-                $this->entityManager->flush();
+                if ($product->getShopifyId() !== $shopifyId) {
+                    $product->setShopifyId($shopifyId);
+                    $this->entityManager->persist($product);
+                    $this->entityManager->flush();
+                }
             } else {
                 $shopifyId = "";
             }
 
-            $tableRows[] = [$product->getId(), $product->getInventoryControlSku(), $shopifyId];
+            $tableRows[] = [$product->getId(), $product->getSku(), $shopifyId];
             $bar->advance();
         })->chunk(100);
 
@@ -168,6 +143,185 @@ class SyncProductsToShopify extends Command
         $this->table($tableHeaders, $tableRows);
 
         return $shopifyIds;
+    }
+
+    /**
+     * Create the data to post to Shopify to create a product
+     *
+     * @param Product $product
+     * @param int $locationId
+     * @return array
+     */
+    private function createProductData(Product $product, int $locationId): array
+    {
+        $postData = $this->formatProductData($product, true);
+
+        // DEV NOTE: different types of products will do this differently, but this simple case is a 1-1 product-variant
+        $postData["variants"] = [$this->createVariantData($product, $locationId, true)];
+
+        return $postData;
+    }
+
+    /**
+     * Build up the payload data to update a Product
+     *
+     * @param Product $product
+     * @return array
+     */
+    private function updateProductData(Product $product): array
+    {
+        // first, we need to get the existing product from Shopify, so we know what to update
+        $shopifyVariantId = $product->getShopifyId();
+        $shopifyVariant = $this->shopify->getVariant($shopifyVariantId);
+        $shopifyProductId = $shopifyVariant->getAttributes()["product_id"];
+        $shopifyProduct = $this->shopify->getProduct($shopifyProductId);
+
+        // now check for any applicable changes
+        $productData = collect($this->formatProductData($product, false));
+        $shopifyProductAttributes = collect($shopifyProduct->getAttributes())->only($productData->keys());
+
+        $productChanges = $productData->diff($shopifyProductAttributes);
+
+        // make sure we include the product ID so shopify knows to update (and not replace)
+        $postData = ["id" => $shopifyProductId];
+        $postData = array_merge($postData, $productChanges->toArray());
+
+        // check for changes to the image, so we don't keep creating new images every time
+        $shopifyImageData = $shopifyProduct->getAttributes()["images"];
+        if (count($shopifyImageData)) {
+            // shopify had one before ...
+            if ($product->getThumbnailUrl()) {
+                // ... and we have one now
+                // DEV NOTE: Shopify hosts the image with its own cdn, so we have to just check if the actual file name is in there
+                if (!Str::of(basename($shopifyImageData[0]["src"]))->contains(basename($product->getThumbnailUrl()))) {
+                    // it's different, so replace it
+                    $image = [
+                        "src" => $product->getThumbnailUrl()
+                    ];
+                } else {
+                    // it's the same, so do nothing
+                    $image = null;
+                }
+            } else {
+                // ... but we've removed it, so clear it out
+                $image = [];
+            }
+        } else {
+            // shopify didn't have one before, so add it if we have one now
+            if ($product->getThumbnailUrl()) {
+                $image = ["src" => $product->getThumbnailUrl()];
+            } else {
+                $image = null;
+            }
+        }
+
+        if ($image !== null) {
+            $postData["images"] = $image;
+        }
+
+        // check for changes to the variant, so we know to update it or not
+        // DEV NOTE: there is always a variant, so we don't have to worry about checking for that or creating one,
+        // and if we didn't supply the variant's ID, Shopify would end up replacing the existing one, so make sure to include it
+        $variantData = collect($this->createVariantData($product, null, false));
+        $shopifyVariantAttributes = collect($shopifyVariant->getAttributes())->only($variantData->keys());
+
+        /*
+          TODO: leaving this as an example, for now. We (currently) only use the metafields for the product ID, which
+           won't ever change, so there's no need to handle updates
+
+          $variantMetaFields = $this->shopify->getVariantMetafields($shopifyVariantId);
+        */
+
+        $variantChanges = $variantData->diff($shopifyVariantAttributes);
+        if ($variantChanges->isNotEmpty()) {
+            $postData["variants"] = array_merge(["id" => $shopifyVariantId], $variantChanges->toArray());
+        }
+
+        return $postData;
+    }
+
+    /**
+     * Build an array with the properly formatted data for a Product
+     *
+     * @param Product $product
+     * @param bool $withImage
+     * @return array
+     */
+    private function formatProductData(Product $product, bool $withImage): array
+    {
+        $productData = [
+            "title" => $product->getName(),
+            "body_html" => $product->getDescription(),
+            "vendor" => Str::ucfirst($product->getBrand()),
+            "product_type" => Str::ucfirst($product->getType())
+            //TODO?
+            // "tags" => "",
+        ];
+        if ($withImage) {
+            $productData["images"] = [
+                [
+                    "src" => $product->getThumbnailUrl(),
+                ]
+            ];
+        }
+        if ($product->getActive()) {
+            $status = "active";
+        } else {
+            $status = $product->getDeletedAt() ? "archived" : "draft";
+        }
+        $productData["status"] = $status;
+
+        return $productData;
+    }
+
+    /**
+     * Create the data required to post a product to Shopify as a variant
+     *
+     * @param Product $product
+     * @param int|null $locationId
+     * @param bool $withMetafields
+     * @return array
+     */
+    private function createVariantData(Product $product, ?int $locationId, bool $withMetafields): array
+    {
+        $variantData = [
+            "compare_at_price" => number_format($product->getPrice(), 2),
+            "price" => number_format($product->getPrice(), 2),
+            "sku" => $product->getSku(),
+            "weight" => $product->getWeight(),
+            "weight_unit" => "lb",
+            "inventory_management" => "shopify",
+            "inventory_quantity" => $product->getStockAvailability(),
+            "requires_shipping" => $product->getIsPhysical(),
+        ];
+        if ($locationId) {
+            $variantData["location_id"] = $locationId;
+        }
+        if ($withMetafields) {
+            // TODO?
+            // refer to https://shopify.dev/docs/apps/custom-data/metafields/types
+            // we can use meta fields for stuff like our product id, etc
+            $variantData["metafields"] = [
+                [
+                    "key" => "_id",
+                    "value" => $product->getId(),
+                    "type" => "number_integer",
+                    "namespace" => "products"
+                ]
+            ];
+        }
+        return $variantData;
+    }
+
+
+    private function createProductDataWithOptions(Product $product)
+    {
+        //    TODO - if the product is one that has options, handle it differently
+    }
+
+    private function updateProductDataWithOptions(Product $product)
+    {
+        //    TODO - if the product is one that has options, handle it differently
     }
 
     /**
