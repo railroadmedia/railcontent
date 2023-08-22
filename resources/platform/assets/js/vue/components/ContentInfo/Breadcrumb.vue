d78cefde72adba9a7654a6d63ba61f3c629927b2
peterminhlee <petereminhlee@gmail.com>
2023-08-22 10:35:06 -0700

    made vue comps

---
+++ resources/platform/assets/js/vue/components/ContentInfo/Breadcrumb.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="tw-mb-6 tw-text-black dark:tw-text-white tw-uppercase tw-text-sm">
+        <template v-if="firstLevelTitle">
+            <a class="tw-text-black dark:tw-text-white tw-text-sm"
+               :href="firstLevelUrl">{{ firstLevelTitle }}</a>
+        </template>
+
+        <template v-if="secondLevelTitle">
+            / <a class="tw-text-black dark:tw-text-white tw-text-sm"
+               :href="secondLevelUrl">{{ secondLevelTitle }}</a>
+        </template>
+        / <b>{{ lastLevelTitle }}</b>
+    </div>
+
+
+</template>
+
+<script setup>
+    const props = defineProps({
+        brand: String,
+        firstLevelUrl: String,
+        firstLevelTitle: String,
+        secondLevelUrl: String,
+        secondLevelTitle: String,
+        lastLevelTitle: String,
+    });
+</script>
