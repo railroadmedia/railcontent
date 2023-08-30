ced569b698dea012585661ae84e3c4fb38a4a746
peterminhlee <petereminhlee@gmail.com>
2023-08-30 10:00:10 -0700

    dummy wrapper

---
+++ resources/platform/assets/js/vue/components/Filter/FilterWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="md:tw-px-8 tw-pt-8">
+        <filter-multi-selection></filter-multi-selection>
+    </div>
+</template>
+
+<script setup>
+    import FilterMultiSelection from './FilterMultiSelection.vue';
+</script>
+
+<style scoped>
+
+</style>
