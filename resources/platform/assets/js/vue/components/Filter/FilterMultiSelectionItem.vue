976213096cd2bda9b92a37e82ce1365fbf9de944
peterminhlee <petereminhlee@gmail.com>
2023-08-30 10:54:22 -0700

    optimisations

--- resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
@@ -5,9 +5,12 @@
             <button class="tw-font-bold lg:tw-hidden" @click="toggleDropdown">&mdash;</button>
         </div>
         <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px]" />
-        <li class="tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0" :class="dropdown ? 'tw-flex' : 'tw-hidden lg:tw-flex'" v-for="item in column.items">
-            {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00]" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="filters[column.category] ? filters[column.category].find((f)=> f.value === item.value) : false" />
-        </li>
+        <div :class="!dropdown ? 'tw-hidden lg:tw-block' : ''">
+            <li class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0"  v-for="item in column.items">
+                {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00]" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="filters[column.category] ? filters[column.category].find((f)=> f.value === item.value) : false" />
+            </li>
+        </div>
+
     </ul>
 </template>
 
