c9f9edf9abedf9db9d2eb5ecf5b26c97e27fce7c
peterminhlee <petereminhlee@gmail.com>
2023-09-01 12:20:47 -0700

    added screen size change

--- resources/platform/assets/js/vue/components/Filter/FilterWrapper.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterWrapper.vue
@@ -1,27 +1,37 @@
 <template>
     <div class="md:tw-px-8 tw-pt-8">
         <filter-controls :is-collapsed="isCollapsed" @on-toggle-collapse="handleToggleCollapse" @on-toggle-collapse-mobile="handleToggleCollapseMobile" />
-        <filter-options v-if="!isCollapsed"></filter-options>
-        <filter-options-modal v-if="!isCollapsedMobile" @onClose="handleToggleCollapseMobile"></filter-options-modal>
+        <filter-options v-if="!isCollapsed && isMobile"></filter-options>
+        <filter-options-modal v-if="!isCollapsed && !isMobile" :is-collapsed-mobile="isCollapsed" @onClose="handleToggleCollapse"></filter-options-modal>
     </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue';
+import {onMounted, onUnmounted, ref} from 'vue';
     import FilterOptions from './FilterOptions.vue';
     import FilterOptionsModal from './FilterOptionsModal.vue';
     import FilterControls from './FilterControls.vue';
 
+    const isMobile = ref(true);
     const isCollapsed = ref(true);
-    const isCollapsedMobile = ref(true);
 
     const handleToggleCollapse = () => {
         isCollapsed.value = !isCollapsed.value;
     };
 
-    const handleToggleCollapseMobile = () => {
-        isCollapsedMobile.value = !isCollapsedMobile.value;
-    };
+    const getScreenSize = () => {
+        if (window.innerWidth > 767){
+            isMobile.value = true;
+        }
+        else {
+            isMobile.value = false;
+        }
+    }
+
+    onMounted(()=>{
+        getScreenSize();
+        window.addEventListener('resize', getScreenSize);
+    })
 </script>
 
 <style scoped>
