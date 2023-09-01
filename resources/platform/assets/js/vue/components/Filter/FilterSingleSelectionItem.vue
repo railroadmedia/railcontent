b116ac782c22e17e031707e4831ad42693b3721d
peterminhlee <petereminhlee@gmail.com>
2023-09-01 08:41:55 -0700

    renamed

--- resources/platform/assets/js/vue/components/Filter/FilterSingleSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterSingleSelectionItem.vue
@@ -2,10 +2,10 @@
     <ul class="lg:tw-w-[252px] tw-text-sm tw-mb-6 lg:tw-mb-0">
         <div class="tw-flex tw-justify-between tw-items-center tw-py-[15px] tw-px-4 md:tw-px-0  tw-bg-[#F9F9F9] dark:tw-bg-[#081825] md:tw-bg-transparent md:dark:tw-bg-transparent">
             <div class="tw-uppercase tw-font-bold">{{ column.category }}</div>
-            <button class="tw-font-bold lg:tw-hidden" @click="toggleDropdown">&mdash;</button>
+            <button class="tw-font-bold lg:tw-hidden" @click="toggleCollapse">&mdash;</button>
         </div>
         <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px]" />
-        <div :class="!dropdown ? 'tw-hidden lg:tw-block' : ''">
+        <div :class="isCollapsed ? 'tw-hidden lg:tw-block' : ''">
             <li class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0"  v-for="item in column.items">
                 {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded-full dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00]" type="radio" @click="$emit('singleSelect', column.category, item)" :checked="selectedValue === item.value" />
             </li>
@@ -28,10 +28,10 @@
       },
   })
 
-  const dropdown = ref(true);
+  const isCollapsed = ref(false);
 
-  const toggleDropdown = () => {
-      dropdown.value = !dropdown.value;
+  const toggleCollapse = () => {
+      isCollapsed.value = !isCollapsed.value;
   }
 </script>
 
