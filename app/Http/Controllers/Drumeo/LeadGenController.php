34bc0f0ae3c1ec4c9d79c832cffbcc35c03eca6f
Trent <trent.harms@gmail.com>
2023-09-07 12:13:59 -0700

    adding controller

--- app/Http/Controllers/Drumeo/LeadGenController.php
+++ app/Http/Controllers/Drumeo/LeadGenController.php
@@ -18,6 +18,12 @@ class LeadGenController extends BaseController
                 return view('drumeo.lead-gen.100-songs.signup', ['theme'=> 'drumeo', 'recaptchaKey'=>config('recaptcha.key')]);
             case 'unlocked':
                 return view('drumeo.lead-gen.100-songs.unlocked');
+            case 'thank-you':
+                return view('drumeo.lead-gen.100-songs.thank-you', ['theme'=> 'drumeo', 'month' => true]);
+            case 'ty-annual':
+                return view('drumeo.lead-gen.100-songs.ty-annual', ['theme'=> 'drumeo', 'month' => true]);
+            case 'ty-monthly':
+                return view('drumeo.lead-gen.100-songs.ty-monthly', ['theme'=> 'drumeo', 'month' => true]);
         }
 
         throw new NotFoundHttpException();
