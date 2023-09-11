b4e6dfedf28a2122e989b0edc3ecdd8b0ff22c1d
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-09-11 16:13:53 -0700

    Minor changes

--- resources/marketing/views/musora/sales/components/custom-image-100songs.blade.php
+++ resources/marketing/views/musora/sales/components/custom-image-100songs.blade.php
@@ -14,8 +14,8 @@
         loading="lazy"
         onload="this.classList.remove('opacity-0')"
     />
-        <div class="absolute inset-0 flex flex-col sm:items-center lg:justify-center md:justify-center md:items-start lg:items-start lg:pl-20">
-            <div class="md:w-1/2 @if($textOnLeft) md:order-1 @endif text-center md:text-left p-8 align-center relative z-10 sm:m-8">
+        <div class="absolute inset-0 flex flex-col my-7 lg:justify-center md:justify-center md:items-start lg:items-start lg:pl-20">
+            <div class="md:w-1/2 @if($textOnLeft) md:order-1 @endif text-center md:text-left px-8 pt-8 align-center relative z-10 sm:m-8">
                 <h5 class="text-sky-600 text-lg font-normal uppercase leading-10 text-left leading-10 lg:text-2xl">{{ $title }}</h5>
                 <h3 class="text-white font-extrabold text-4xl font-extrabold leading-10 text-left pb-2 lg:py-4 lg:text-5xl">{{ $subtitle }}</h3>
                 <p class="text-white text-base font-normal leading-loose text-left lg:text-xl lg:py-4">{{ $description }}</p>
