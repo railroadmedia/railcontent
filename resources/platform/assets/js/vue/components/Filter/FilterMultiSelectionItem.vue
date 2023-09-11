c12554b30f7fc14b824db669338ee79997aadbc8
peterminhlee <petereminhlee@gmail.com>
2023-09-11 16:12:41 -0700

    style updates when selected

--- resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
@@ -8,8 +8,8 @@
             <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px] tw-sticky" />
         </div>
         <div :class="isCollapsed ? 'tw-hidden lg:tw-block' : 'tw-pb-6 lg:tw-pb-0'">
-            <li class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0"  v-for="item in column.items">
-                {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00]" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="filters[column.category] ? filters[column.category].find((f)=> f.value === item.value) : false" />
+            <li v-for="item in column.items" class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0" :class="isSelected(item.value) ? 'tw-font-bold' : ''" >
+                {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="isSelected(item.value)" />
             </li>
         </div>
     </ul>
@@ -33,6 +33,10 @@
 
     const toggleCollapse = () => {
         isCollapsed.value = !isCollapsed.value;
+    }
+
+    const isSelected = (value) => {
+        return props.filters[props.column.category] && props.filters[props.column.category].find((f)=> f.value === value) ? true : false;
     }
 </script>
 
--- resources/platform/assets/js/vue/components/Filter/FilterSingleSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterSingleSelectionItem.vue
@@ -6,8 +6,8 @@
         </div>
         <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px]" />
         <div :class="isCollapsed ? 'tw-hidden lg:tw-block' : ''">
-            <li class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0"  v-for="item in column.items">
-                {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded-full dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00]" type="radio" @click="$emit('singleSelect', column.category, item)" :checked="selectedValue === item.value" />
+            <li v-for="item in column.items" class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0" :class="isSelected(item.value) ? 'tw-font-bold' : ''"  >
+                {{ item.name }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded-full dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer" type="radio" @click="$emit('singleSelect', column.category, item)" :checked="isSelected(item.value)" />
             </li>
         </div>
 
@@ -32,6 +32,10 @@
 
   const toggleCollapse = () => {
       isCollapsed.value = !isCollapsed.value;
+  }
+
+  const isSelected = (value) => {
+      return props.selectedValue === value;
   }
 </script>
 
