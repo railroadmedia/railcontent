97fde447c5f843a4702a824a1bc788f2d4738bb0
Alexandre Severo <alexandre@musora.com>
2023-09-13 16:25:16 -0700

    Push purchases to shopify

---
+++ app/Modules/Ecommerce/Services/ShopifyCustomerService.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Modules\Ecommerce\Services;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Modules\UserManagementSystem\Models\User;
+use Railroad\Ecommerce\Entities\Customer;
+use Railroad\Ecommerce\Repositories\CustomerRepository;
+use Signifly\Shopify\Exceptions\ValidationException;
+use Signifly\Shopify\Shopify;
+
+class ShopifyCustomerService
+{
+    private Shopify $shopify;
+    private CustomerRepository $customerRepository;
+
+    public function __construct(
+        Shopify $shopify,
+        CustomerRepository $customerRepository,
+    ) {
+        $this->shopify = $shopify;
+        $this->customerRepository = $customerRepository;
+    }
+
+    public function createShopifyCustomer(User $user)
+    : int {
+        $customers = $this->getCustomersForUser($user);
+
+        $alreadySyncedUserCustomers = $customers->filter(fn(Customer $customer) => !is_null($customer->getShopifyId()));
+        $isCreating = is_null($user->shopify_id) && $alreadySyncedUserCustomers->isEmpty();
+
+        $postData = $this->createCustomerDataForUser($user, $isCreating);
+
+        try {
+            if ($isCreating) {
+                $customerResource = $this->shopify->createCustomer($postData);
+            } else {
+                $existingCustomerShopifyId =
+                    $user->shopify_id
+                    ??
+                    $alreadySyncedUserCustomers->first()
+                        ->getShopifyId();
+                $customerResource = $this->shopify->updateCustomer($existingCustomerShopifyId, $postData);
+            }
+        } catch (ValidationException $exception) {
+            Log::error(sprintf("Validation failed when sending customer data to Shopify: %s", $exception->getMessage()));
+            Log::error(sprintf("Please investigate for user or customers with email address %s. Attempted customer data: %s",
+                    $user->getEmail(), json_encode($postData)));
+
+            return 0;
+        }
+
+        $user->shopify_id = $customerResource->id;
+        $user->save();
+
+        return $user->shopify_id;
+    }
+
+    /**
+     * Get all customers with the same email address as the given user
+     *
+     * @param User $user
+     * @return Collection
+     */
+    private function getCustomersForUser(User $user)
+    : Collection {
+        $qb = $this->customerRepository->createQueryBuilder('customer');
+        $qb->where($qb->expr()
+            ->eq("customer.email", ":email"))
+            ->setParameter("email", $user->getEmail());
+
+        $q = $qb->getQuery();
+
+        return collect($q->getResult());
+    }
+
+    /**
+     * Create the data to post to Shopify to create a Customer from our User
+     *
+     * DEV NOTE: we don't bother looking at the user's customers because our system doesn't allow for
+     * new Customers to be made after a User already exists with the same email address.
+     *
+     * @param User $user
+     * @param bool $withMetafields
+     * @return array
+     */
+    private function createCustomerDataForUser(User $user, bool $withMetafields)
+    : array {
+        $customerData = [
+            "currency" => "USD",
+            "email" => $user->getEmail(),
+            "first_name" => $user->first_name,
+            "last_name" => $user->last_name,
+            "note" => $user->support_note,
+            // "tags" => "",
+        ];
+
+        if ($withMetafields) {
+            // refer to https://shopify.dev/docs/apps/custom-data/metafields/types
+            // we can use meta fields for stuff like our user id, etc
+            $customerData["metafields"] = [
+                [
+                    "key" => "_id",
+                    "value" => $user->id,
+                    "type" => "number_integer",
+                    "namespace" => "users",
+                ],
+            ];
+        }
+
+        return $customerData;
+    }
+}
--- app/Modules/Ecommerce/Services/ShopifySyncService.php
+++ app/Modules/Ecommerce/Services/ShopifySyncService.php
@@ -7,7 +7,6 @@ use App\Modules\Ecommerce\Models\Product;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Modules\UserManagementSystem\Models\User;
-use Railroad\Railcontent\Repositories\PermissionRepository;
 use Signifly\Shopify\REST\Resources\OrderResource;
 use Signifly\Shopify\Shopify;
 
@@ -15,11 +14,16 @@ class ShopifySyncService
 {
     private Shopify $shopify;
     private PermissionsService $permissionService;
+    private ShopifyCustomerService $shopifyCustomerService;
 
-    public function __construct(Shopify $shopify, PermissionsService $permissionService)
-    {
+    public function __construct(
+        Shopify $shopify,
+        PermissionsService $permissionService,
+        ShopifyCustomerService $shopifyCustomerService
+    ) {
         $this->shopify = $shopify;
         $this->permissionService = $permissionService;
+        $this->shopifyCustomerService = $shopifyCustomerService;
     }
 
     public function syncCustomer($shopifyCustomerId)
@@ -31,14 +35,94 @@ class ShopifySyncService
         $this->permissionService->syncPermissions($userId, $userPermissions);
     }
 
+    /**
+     * @param User $user
+     * @param Product $product
+     * @param float $price
+     * @param float $tax
+     * @return void
+     */
+    public function syncOrder(User $user, Product $product, float $price, float $tax)
+    : void {
+        // STEP 1: Is user sync'ed?
+        $customerShopifyId = $user->shopify_id;
+        if (!$customerShopifyId) {
+            $customerShopifyId = $this->shopifyCustomerService->createShopifyCustomer($user);
+        }
+
+        // STEP 2: create shopify order data
+        $postData = $this->createOrderData($customerShopifyId, $user->email, $product, $price, $tax);
+
+        // STEP 3: push order to shopify
+        $this->shopify->createOrder($postData);
+    }
+
+    /**
+     * Create the data to post to Shopify to create an Order
+     *
+     * @param int $customerShopifyId
+     * @param string $email
+     * @param Product $product
+     * @param float $price
+     * @param float $tax
+     * @return array
+     */
+    private function createOrderData(
+        int $customerShopifyId,
+        string $email,
+        Product $product,
+        float $price,
+        float $tax
+    )
+    : array {
+        return [
+            "customer" => ["id" => $customerShopifyId],
+            "email" => $email,
+            "note" => $product->note,
+            "processed_at" => Carbon::now(),
+            "source_name" => $product->brand,
+            "subtotal_price" => $price,
+            "total_outstanding" => 0,
+            "total_price" => $price,
+            "total_tax" => $tax,
+            "line_items" => $this->createOrderItems($product, $price),
+        ];
+    }
+
+    /**
+     * Create the data required for all Order Items of the Order
+     *
+     * @param Product $product
+     * @param float $price
+     * @return array
+     */
+    private function createOrderItems(Product $product, float $price)
+    : array {
+        return [
+            "fulfillable_quantity" => 1,
+            "fulfillment_service" => "manual",
+            "price" => $price,
+            "quantity" => 1,
+            "requires_shipping" => false,
+            "sku" => $product->sku,
+            "title" => $product->name,
+            "variant_id" => $product->shopify_id,
+            "variant_inventory_management" => "shopify",
+            "vendor" => $product->brand,
+        ];
+    }
+
     private function getUserIdFromShopifyCustomerId($shopifyCustomerId)
     {
-        $user = User::query()->where('shopify_id', '=', $shopifyCustomerId)->first('id');
+        $user =
+            User::query()
+                ->where('shopify_id', '=', $shopifyCustomerId)
+                ->first('id');
         return $user->id ?? null;
     }
 
-    private function getOwnedProducts(int $shopifyCustomerId): array
-    {
+    private function getOwnedProducts(int $shopifyCustomerId)
+    : array {
         $orders = $this->shopify->getCustomerOrders($shopifyCustomerId);
         $ownedProducts = [];
         /** @var OrderResource $order */
@@ -54,15 +138,21 @@ class ShopifySyncService
         return $ownedProducts;
     }
 
-    private function getUserPermissions(array $ownedProducts): array
-    {
-        $permissionsLookup = $this->permissionService->getAll()->keyBy(function ($permission) {
-            return $permission->brand . '_' . $permission->name;
-        });
+    private function getUserPermissions(array $ownedProducts)
+    : array {
+        $permissionsLookup =
+            $this->permissionService->getAll()
+                ->keyBy(function ($permission) {
+                    return $permission->brand . '_' . $permission->name;
+                });
 
         $permissionsToCreate = [];
 
-        $products = Product::query()->whereIn('shopify_id', array_keys($ownedProducts))->get()->keyBy('shopify_id');
+        $products =
+            Product::query()
+                ->whereIn('shopify_id', array_keys($ownedProducts))
+                ->get()
+                ->keyBy('shopify_id');
         foreach ($ownedProducts as $productId => $createdAt) {
             /** @var Product $product */
             $product = $products[$productId] ?? null;
@@ -89,8 +179,8 @@ class ShopifySyncService
                 }
                 $permissionId = $permission['id'];
 
-                if (!array_key_exists($permissionId, $permissionsToCreate)
-                    || $permissionsToCreate[$permissionId]['expiration_date'] < $expirationDate) {
+                if (!array_key_exists($permissionId, $permissionsToCreate) ||
+                    $permissionsToCreate[$permissionId]['expiration_date'] < $expirationDate) {
                     $permissionsToCreate[$permissionId] = [
                         'expiration_date' => $expirationDate,
                         'start_date' => $createdAt,
@@ -100,6 +190,4 @@ class ShopifySyncService
         }
         return $permissionsToCreate;
     }
-
-
 }
