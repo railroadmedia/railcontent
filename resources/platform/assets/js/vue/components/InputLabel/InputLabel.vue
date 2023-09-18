e121acc9b55e6526b5f01d8412f1152f3802a70a
peterminhlee <petereminhlee@gmail.com>
2023-09-18 12:51:27 -0700

    added emit

--- resources/platform/assets/js/vue/components/InputLabel/InputLabel.vue
+++ resources/platform/assets/js/vue/components/InputLabel/InputLabel.vue
@@ -90,7 +90,7 @@ const props = defineProps({
 const maskedValue = ref('');
 const bindedObject = reactive({});
 
-const emit = defineEmits(["onChange", "onFocus"]);
+const emit = defineEmits(["onChange", "onFocus", "onEnter"]);
 
 const onClear = (e) => {
   e.preventDefault();
@@ -124,22 +124,22 @@ onBeforeMount(()=> {
         labelValue
       }}</label>
     <div class="tw-flex tw-relative">
-      <input v-model="maskedValue" 
-             v-maska="bindedObject" 
-             v-on:keypress.enter.prevent="onEnter" 
-             :data-maska="maskaConfig.dataMaska" 
-             :data-maska-tokens="maskaConfig.maskaTokens" 
+      <input v-model="maskedValue"
+             v-maska="bindedObject"
+             v-on:keypress.enter.prevent="onEnter"
+             :data-maska="maskaConfig.dataMaska"
+             :data-maska-tokens="maskaConfig.maskaTokens"
              :placeholder="placeholder" :id="id"
              :class="[getBaseInputStyles(),
                       `${inputOverride ? inputOverride : ''}`,
                       { '!tw-border-red-500 dark:!tw-border-red-500' : error },
                       { 'tw-bg-[#D3D3D3] dark:tw-bg-transparent dark:tw-opacity-20' : disabled  },
                     ]"
-             :name="inputName" 
+             :name="inputName"
              :type="inputType"
-             :disabled="disabled" 
+             :disabled="disabled"
              autocomplete="off"
-             @focus="() => emit('onFocus')" 
+             @focus="() => emit('onFocus')"
       />
       <div v-if="showClearButton"
         :class="`tw-absolute tw-right-0 tw-h-full tw-flex tw-items-center tw-justify-center ${maskedValue.length ? 'tw-flex' : 'tw-hidden'} ${clearButtonOverride}`">
