a79df7d5bbfd59d1d40a4168a3305e491e316304
peterminhlee <petereminhlee@gmail.com>
2023-09-19 10:46:10 -0700

    added clear all

--- resources/platform/assets/js/vue/components/Filter/FilterPills.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterPills.vue
@@ -8,41 +8,34 @@
                 </button>
             </li>
         </template>
+        <li class="tw-flex tw-items-center tw-p-2 tw-rounded tw-border dark:tw-border-[#223F57] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] dark:tw-text-white tw-font-semibold tw-uppercase">
+            Clear all
+            <button class="tw-text-[#000C17] dark:tw-text-white">
+                <XIcon class="tw-h-[20px] tw-w-[20px] tw-ml-1" @click="handleClearFilters" />
+            </button>
+        </li>
     </ul>
 </template>
 
 <script setup>
     import { XIcon } from "@heroicons/vue/solid";
-    import {onMounted, ref} from "vue";
 
     const props = defineProps({
         activeFilters: {
             type: Object,
             default: {},
         },
-        filters: {
-            type: Object,
-            default: {},
-        }
     })
 
-    const emit = defineEmits(['cancelFilter']);
-
-    const appliedFilters = ref({});
+    const emit = defineEmits(['cancelFilter', 'clearFilter']);
 
     const handleCancel = (category, item) => {
         emit('cancelFilter', category, item);
     }
 
-    onMounted(() => {
-        let selectedFilters = {};
-
-        Object.keys(props.filters).forEach((key) => {
-            props.filters[key].map((o)=>{
-
-            })
-        });
-    });
+    const handleClearFilters = () => {
+        emit('clearFilter');
+    }
 </script>
 
 <style scoped>
