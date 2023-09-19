7f13c84a6ca321b5e32efa92dfb0849a6bdc2ed4
peterminhlee <petereminhlee@gmail.com>
2023-09-19 08:42:48 -0700

    name changes

---
+++ resources/platform/assets/js/vue/components/Filter/FilterPills.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul v-if="Object.keys(activeFilters).length" class="tw-flex tw-flex-wrap tw-gap-4 tw-text-sm tw-mb-5 tw-px-4 md:tw-px-0">
+        <template v-for="key in Object.keys(activeFilters)">
+            <li class="tw-flex tw-items-center tw-p-2 tw-rounded tw-border dark:tw-border-[#223F57] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] dark:tw-text-white tw-font-semibold" v-for="filter in activeFilters[key]">
+                {{ filter }}
+                <button class="tw-text-[#000C17] dark:tw-text-white">
+                    <XIcon class="tw-h-[20px] tw-w-[20px] tw-ml-1" @click="handleCancel(key, filter)" />
+                </button>
+            </li>
+        </template>
+    </ul>
+</template>
+
+<script setup>
+    import { XIcon } from "@heroicons/vue/solid";
+    import {onMounted, ref} from "vue";
+
+    const props = defineProps({
+        activeFilters: {
+            type: Object,
+            default: {},
+        },
+        filters: {
+            type: Object,
+            default: {},
+        }
+    })
+
+    const emit = defineEmits(['cancelFilter']);
+
+    const appliedFilters = ref({});
+
+    const handleCancel = (category, item) => {
+        emit('cancelFilter', category, item);
+    }
+
+    onMounted(() => {
+        let selectedFilters = {};
+
+        Object.keys(props.filters).forEach((key) => {
+            props.filters[key].map((o)=>{
+
+            })
+        });
+    });
+</script>
+
+<style scoped>
+
+</style>
--- resources/platform/assets/js/vue/components/Filter/FilterTabs.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterTabs.vue
@@ -3,7 +3,7 @@
         <button
             v-for="option in tabOptions"
             @click="onTabClick(option.value)"
-            class="tw-flex tw-items-center tw-justify-center tw-text-center tw-h-[40px] md:tw-h-[50px] tw-uppercase tw-text-[14px] md:tw-text-[20px] tw-font-bebas-neue tw-rounded-[55px] tw-border hover:tw-bg-[#223F57] hover:tw-text-white tw-font-semibold dark:tw-border-[#445F74] tw-px-4 lg:tw-px-6"
+            class="tw-flex tw-items-center tw-justify-center tw-text-center tw-h-[40px] md:tw-h-[50px] tw-uppercase tw-text-[14px] md:tw-text-[20px] tw-font-bebas-neue tw-rounded-[55px] tw-border hover:dark:tw-bg-[#223F57] hover:tw-text-white tw-font-semibold dark:tw-border-[#445F74] tw-px-4 lg:tw-px-6"
             :class="isActive(option.value) ? `tw-bg-[#28282D] dark:tw-bg-[#445F74] tw-text-white` : `tw-bg-white dark:tw-bg-[#000C17] tw-border-[#CBCBCD] dark:tw-text-white`"
         >
            {{ option.key }}
