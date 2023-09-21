458511d811375cc9a9ff6cc9e3efae85d5370e1b
Alexandre Severo <alexandre@musora.com>
2023-09-21 10:33:53 -0700

    Remove unused imports

--- app/Modules/Ecommerce/Providers/EcommerceServiceProvider.php
+++ app/Modules/Ecommerce/Providers/EcommerceServiceProvider.php
@@ -2,16 +2,6 @@
 
 namespace App\Modules\Ecommerce\Providers;
 
-use App\Modules\Ecommerce\Console\Commands\CheckPurchasesMigratedIntoRevenuecat;
-use App\Modules\Ecommerce\Console\Commands\FixMissingStripeCustomerIds;
-use App\Modules\Ecommerce\Console\Commands\FixMobileSubscriptionsBrand;
-use App\Modules\Ecommerce\Console\Commands\MigratePaypalGatewayToMusora;
-use App\Modules\Ecommerce\Console\Commands\MigrateStripeCustomersToMusora;
-use App\Modules\Ecommerce\Console\Commands\MigrateStripeSubscriptions;
-use App\Modules\Ecommerce\Console\Commands\SyncRevenuecatSubscriptionsToMusora;
-use App\Modules\Ecommerce\Console\Commands\SyncStripePaymentMethods;
-use App\Modules\Ecommerce\Console\Commands\ProcessAppleExpiredSubscriptions;
-use App\Modules\Ecommerce\Console\Commands\UnifySubscriptions;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
