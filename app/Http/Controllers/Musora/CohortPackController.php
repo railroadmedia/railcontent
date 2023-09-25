516a29455f783401f05c39173254c95edec57af5
Alexandre Severo <alexandre@musora.com>
2023-09-25 15:35:35 -0700

    Add new access permissions when enrolling to challenges

--- app/Http/Controllers/Musora/CohortPackController.php
+++ app/Http/Controllers/Musora/CohortPackController.php
@@ -2,24 +2,29 @@
 
 namespace App\Http\Controllers\Musora;
 
+use App\Modules\Ecommerce\Enums\UserAccessPermissionsSourceEnum;
+use App\Modules\Ecommerce\Services\UserAccessPermissionsService;
+use Carbon\Carbon;
+use Doctrine\ORM\ORMException;
+use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Railroad\Ecommerce\Entities\Product;
 use Railroad\Ecommerce\Entities\User;
 use Railroad\Ecommerce\Repositories\ProductRepository;
-use Railroad\Ecommerce\Services\UserProductService;
 
 class CohortPackController
 {
 
     private ProductRepository $productRepository;
-    private UserProductService $userProductService;
+    private UserAccessPermissionsService $userAccessPermissionsService;
 
     public function __construct(
         ProductRepository $productRepository,
-        UserProductService $userProductService
+        UserAccessPermissionsService $userAccessPermissionsService,
     ) {
         $this->productRepository = $productRepository;
-        $this->userProductService = $userProductService;
+        $this->userAccessPermissionsService = $userAccessPermissionsService;
     }
 
     public function registerFor30DayDrummer2(Request $request)
@@ -33,21 +38,32 @@ class CohortPackController
     public function registerNewPianoPlayersStartHere(Request $request)
     {
         $sku = "new-piano-players-start-here";
-        $successMessage = 'Success! You have registered for New Piano Players Start Here. Check your email for the details.';
+        $successMessage =
+            'Success! You have registered for New Piano Players Start Here. Check your email for the details.';
 
         return $this->registerForProductPack($sku, $successMessage);
     }
 
+    /**
+     * @throws ORMException
+     * @throws Exception
+     */
     private function registerForProductPack(string $sku, string $successMessage)
-    {
+    : RedirectResponse {
         if (user()?->isAMember()) {
             $user = new User(user()->id, user()->email);
 
             /** @var Product $product */
             $product = $this->productRepository->bySku($sku);
-            $this->userProductService->assignUserProduct($user, $product, null, 1);
+            $this->userAccessPermissionsService->AddUserAccessPermissionsForProducts(
+                $user->getId(),
+                [$product->getId()],
+                Carbon::now(),
+                UserAccessPermissionsSourceEnum::Challenges
+            );
 
-            return redirect()->back()
+            return redirect()
+                ->back()
                 ->with(
                     'success-message',
                     $successMessage
