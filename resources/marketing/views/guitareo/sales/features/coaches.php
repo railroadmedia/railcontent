f92b2a0ffe68cea3fb768944e9d6137530abc998
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-09-25 15:40:55 -0700

    Updated coaches thumbnails

--- resources/marketing/views/guitareo/sales/features/coaches.php
+++ resources/marketing/views/guitareo/sales/features/coaches.php
@@ -1,42 +1,42 @@
 <?php
     $creativities = [
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/301043-card-thumbnail-maxres-1621412323.png',
-            'title' => 'Solo In An Hour',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-learn-solo.jpg',
+            'title' => 'Learn To Solo',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-5-songwriting-cheatcodes-1640922846.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-cheat-codes.jpg',
             'title' => 'Songwriting Cheat Codes',
             'artist' => 'Rob Scallon',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-1664263514.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-perfect-parts.jpg',
             'title' => 'Creating The Perfect Guitar Part',
             'artist' => 'Mark Lettieri',
         ],
         [
-            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/add-power-to-your-playing-thumb.jpg',
-            'title' => 'Better Guitar Solos',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-creative-songwriting.jpg',
+            'title' => 'Creative Songwriting',
             'artist' => 'Dave Weiner',
         ],
         [
-            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/unlock-your-creativity-thumb.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-unlock-creativity.jpg',
             'title' => 'Unlock Your Creativity',
             'artist' => 'Yvette Young',
         ],
         [
-            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/the-anatomy-of-a-song-thumb.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-song-anotomy.jpg',
             'title' => 'The Anatomy of a Song',
             'artist' => 'Pete Thorn',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/altered-open-tunings.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-altered.jpg',
             'title' => 'Altered & Open Tunings',
             'artist' => 'Don Ross',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/222675-card-thumbnail-maxres-1553776379.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-CreativitySection-looping.jpg',
             'title' => 'Looping',
             'artist' => 'David Becker',
         ],
@@ -44,42 +44,42 @@
 
     $learn = [
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/YT-TechniquesToSoundLikeARockGod-05-1628743345.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-rock.jpg',
             'title' => 'Rock Guitar',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-Card-1646897809.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-surf.jpg',
             'title' => 'Surf Guitar',
             'artist' => 'Kent Shores',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/280679-card-thumbnail-maxres-1609366267.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-campfire.jpg',
             'title' => 'Campfire Chords',
             'artist' => 'Rob Scallon',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/278525-card-thumbnail-maxres-1609945442.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-funk.jpg',
             'title' => 'Funk Essentials',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-1664263514.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-neo-soul.jpg',
             'title' => 'Fusion/Neo-Soul',
             'artist' => 'Mark Lettieri',
         ],
         [
-            'img' => 'https://guitarlessons-com.s3.amazonaws.com/guitar-lessons/eba35aa-bg1-card-scaled@2x.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-bluegrass.jpg',
             'title' => 'Bluegrass',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://guitarlessons-com.s3.amazonaws.com/guitar-lessons/7fb6e2b-road15-scaled@2x.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-fingerstyle.jpg',
             'title' => 'Fingerstyle',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-1651207142.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-StyleSection-shred.jpg',
             'title' => 'Shred Guitar',
             'artist' => 'Dean Lamb',
         ],
@@ -87,42 +87,42 @@
 
     $techniques = [
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-1648717349.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-rhythm.jpg',
             'title' => 'Rhythm & Groove',
             'artist' => 'Sami Ghawi',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/294635-card-thumbnail-maxres-1617276103.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-acoustic.jpg',
             'title' => 'Getting Started On The Acoustic Guitar',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/290985-card-thumbnail-maxres-1614867316.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-electric.jpg',
             'title' => 'Getting Started On The Electric Guitar',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://guitarlessons-com.s3.amazonaws.com/guitar-lessons/212a52c-picking-card-scaled@2x.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-picking.jpg',
             'title' => 'Picking',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/qa-feb22-2022-1644338778.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-legato.jpg',
             'title' => 'Legato',
             'artist' => 'Kent Shores',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/YT-thumb-1920x1080_2-1659614695.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-workouts.jpg',
             'title' => 'Strumming Workouts',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/303027-card-thumbnail-maxres-1621854579.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-bending.jpg',
             'title' => 'Bending & Vibrato',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/222675-card-thumbnail-maxres-1553776379.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-Technique-timing.jpg',
             'title' => 'Timing & Feel',
             'artist' => 'David Becker',
         ],
@@ -130,42 +130,42 @@
 
     $plans = [
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/280599-card-thumbnail-maxres-1609369491.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-quest.jpg',
             'title' => 'GuitarQuest',
             'artist' => 'Rob Scallon',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/GTME-card.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-technique.jpg',
             'title' => 'Guitar Technique Made Easy',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/card-image.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-acoustic.jpg',
             'title' => 'Acoustic Guitar Made Easy',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/206357-card-thumbnail-1583865406.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-recording.jpg',
             'title' => 'The Ultimate Guide To Recording Guitar',
             'artist' => 'Victor Guidera',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/course-card-1646143879.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-solo.jpg',
             'title' => 'Your New Sounds For Soloing',
             'artist' => 'Kent Shores',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/expanding-chord-thumb-1.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-vocabulary.jpg',
             'title' => 'Expanding Your Chord Vocabulary',
             'artist' => 'Asher Kurtz',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/1517528198-1-practice-with-improv.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-improv.jpg',
             'title' => 'Practicing With Improvisation',
             'artist' => 'Jacob Wiens',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/How To Write A Song On Guitar-07-1640780317.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-GoalOrientedPlans-songwriting.jpg',
             'title' => 'How To Write A Song On Guitar',
             'artist' => 'Ayla Tesler-Mabé',
         ],
@@ -173,42 +173,42 @@
 
     $anythingElse = [
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/sweep picking-1651731867.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-sweep.jpg',
             'title' => 'Sweep Picking',
             'artist' => 'Dean Lamb',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/303028-card-thumbnail-maxres-1622191902.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-quickly.jpg',
             'title' => 'How To Quickly Learn A Song',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/294633-card-thumbnail-maxres-1616664547.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-chords.jpg',
             'title' => 'Beginner Guitar Chords',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/caged-thumb.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-caged.jpg',
             'title' => 'The CAGED System',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/298219-card-thumbnail-maxres-1619082725.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-changing.jpg',
             'title' => 'Changing Chords',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/287255-card-thumbnail-maxres-1615194945.png',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-rock-licks.jpg',
             'title' => 'Rock Guitar Licks',
             'artist' => 'Ayla Tesler-Mabé',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/235823-card-thumbnail-maxres-1573750575.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-ear.jpg',
             'title' => 'Learn Songs By Ear',
             'artist' => 'Nate Savage',
         ],
         [
-            'img' => 'https://d1923uyy6spedc.cloudfront.net/shaping-up-the-neck-main-high.jpg',
+            'img' => 'https://d122ay5chh2hr5.cloudfront.net/sales/2023/coaches/Guitareo-AnythingElse-the-neck.jpg',
             'title' => 'Shaping Up The Neck',
             'artist' => 'David Becker',
         ],
