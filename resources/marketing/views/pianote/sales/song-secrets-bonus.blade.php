96d1df9555213dfe45649068a3d217f0be47a3fe
Trent <trent.harms@gmail.com>
2023-09-27 17:18:03 -0700

    fixing offer page

--- resources/marketing/views/pianote/sales/song-secrets-bonus.blade.php
+++ resources/marketing/views/pianote/sales/song-secrets-bonus.blade.php
@@ -149,10 +149,10 @@
         <div class="container mx-auto max-w-4xl">
             <div class="flex flex-wrap sm:flex-nowrap items-center">
                 <div class="flex w-full justify-center sm:justify-start sm:w-1/2 lg:w-auto sm:order-1 lg:pl-5">
-                    <img class="hidden sm:inline-block h-56 sm:h-auto max-w-full sm:max-w-md lg:max-w-full transition-opacity opacity-0"
+                    <img class="hidden sm:inline-block h-56 sm:h-auto max-w-full sm:max-w-sm lg:max-w-full transition-opacity opacity-0"
                             loading="lazy"
                             onload="this.classList.remove('opacity-0')"
-                            src="https://www.musora.com/musora-cdn/image/width=1210,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/bundle-collage2.png"
+                            src="https://www.musora.com/musora-cdn/image/width=1210,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/pianote-bundle.png"
                             alt="{{$theme}} collage image"
                     >
                 </div>
@@ -160,7 +160,7 @@
                     <img class="inline-block sm:hidden mb-4 h-64 sm:h-auto max-w-full sm:max-w-xl lg:max-w-full transition-opacity opacity-0"
                             loading="lazy"
                             onload="this.classList.remove('opacity-0')"
-                            src="https://www.musora.com/musora-cdn/image/width=600,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/bundle-collage-m2.png"
+                            src="https://www.musora.com/musora-cdn/image/width=600,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/pianote-bundle-m.png"
                             alt="{{$theme}} collage image"
                     >
                     <div class="inline-block mx-auto">
@@ -191,9 +191,9 @@
         <h3 class="inline-block font-bebas mx-0 pr-3">* CONGRATULATIONS! *</h3>
         <p class="inline-block text-sm mx-0 leading-tight">You qualify for the discount!</p>
     </a>
-    <section class="py-6 sm:py-8 text-white text-center bg-cover bg-center" style="background:#ac1179 url('https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/lead-gen/song-secrets/order-bg.jpg');">
+    <section class="px-6 py-6 sm:py-8 text-white text-center bg-cover bg-center" style="background:#ac1179 url('https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/lead-gen/song-secrets/order-bg.jpg');">
         <div class="container max-w-4xl mx-auto">
-            <h6 class="leading-tight mb-4"><em><strong>PLUS</strong> get these special bonuses when you join today.</em></h6>
+            <h6 class="leading-tight mb-4"><em><strong>PLUS</strong> get these special<br class="sm:hidden"> bonuses when you join today.</em></h6>
             <div style="font-size:0px">
                 <div class="bonus-wrap relative inline-block align-top mx-auto mb-4  lg:mb-0 px-1 md:px-3  w-1/2 md:w-1/5 ">
                     <div class="flip-div inline-block relative w-full group" style=" padding-bottom: 133%;  perspective: 1000px;">
@@ -227,6 +227,15 @@
                         <div class="text-center w-full h-full absolute" style="transform-style: preserve-3d;">
                             <div x-ref="front" class="border-2 border-musora front absolute z-20 overflow-hidden rounded-xl w-full h-full transition-transform duration-700" style=" backface-visibility: hidden;">
                                 <div class="overflow-hidden rounded-xl h-full w-full bg-black bg-top bg-cover" style="background-image:url(https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/black-friday/unlimited/piano-riffs-and-fills.jpg);"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bonus-wrap relative inline-block align-top mx-auto mb-4  lg:mb-0 px-1 md:px-3  w-1/2 md:w-1/5 ">
+                    <div class="flip-div inline-block relative w-full group" style=" padding-bottom: 133%;  perspective: 1000px;">
+                        <div class="text-center w-full h-full absolute" style="transform-style: preserve-3d;">
+                            <div x-ref="front" class="border-2 border-musora front absolute z-20 overflow-hidden rounded-xl w-full h-full transition-transform duration-700" style=" backface-visibility: hidden;">
+                                <div class="overflow-hidden rounded-xl h-full w-full bg-black bg-top bg-cover" style="background-image:url(https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/music-theory-card.jpg);"></div>
                             </div>
                         </div>
                     </div>
@@ -311,59 +320,54 @@
         'instagram' => '220K',
     ])
 
-    <section class="py-8 sm:py-10 lg:py-12 relative overflow-hidden text-center customize px-4 lg:px-8 relative overflow-hidden" style="background: #f6f8fc;">
+    <section class="py-8 sm:py-10 lg:py-12 relative overflow-hidden text-center customize px-5 lg:px-8 relative overflow-hidden" style="background: #f6f8fc;">
         <div class="container mx-auto max-w-4xl">
             <h3 class="leading-tight"><strong>Take a look at your bonuses…</strong></h3>
             <h6 class="leading-tight text-pianote mt-2 mb-5"><em>Join today and you’ll get all of these.</em></h6>
 
             <div class="flex flex-wrap text-left">
-                <div class="w-full mb-12 flex items-center">
+                <div class="w-full mb-12 sm:flex items-center">
                     <div class="relative flex-shrink-0">
-                        <img class="h-96" src="">
-                        <h3 class="absolute bottom-0 mx-auto rounded-xl bg-white text-pianote px-4 py-2"><s>$39</s> <strong>FREE</strong></h3>
+                        <img class="h-48 sm:h-64 lg:h-72 rounded-xl overflow-hidden" src="https://www.musora.com/musora-cdn/image/width=750,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/chords-scales.jpg">
                     </div>
                     <div class="sm:pl-5 lg:pl-8">
-                        <img class="h-32 mb-3" src="">
+                        <img class="h-14 sm:h-16 lg:h-20 my-3 sm:mt-0 sm:mb-4" src="https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/chords-scales-logo.png">
                         <p class="leading-tight">Our best-selling book is yours FREE. This book is your encyclopedia of piano chords & scales. Arranged by key, you’ll find every major, minor, sus, and 7th chord as well as all the scales you’ll need to play the songs you love without fear.</p>
                     </div>
                 </div>
-                <div class="w-full mb-12 flex items-center">
+                <div class="w-full mb-12 sm:flex items-center">
                     <div class="relative flex-shrink-0 sm:order-1">
-                        <img class="h-96" src="">
-                        <h3 class="absolute bottom-0 mx-auto rounded-xl bg-white text-pianote px-4 py-2"><s>$39</s> <strong>FREE</strong></h3>
+                        <img class="h-48 sm:h-64 lg:h-72 rounded-xl overflow-hidden" src="https://www.musora.com/musora-cdn/image/width=750,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/music-theory-poster.jpg">
                     </div>
                     <div class="sm:pr-5 lg:pr-8">
-                        <img class="h-32 mb-3" src="">
+                        <img class="h-14 sm:h-16 lg:h-20 my-3 sm:mt-0 sm:mb-4" src="https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/music-theory-logo.png">
                         <p class="leading-tight">Upgrade your practice space and master your music theory with this gorgeous poster bundle. Shipped flat so there are no creases and printed in full color on beautiful paper stock, these posters will help you connect the notes on the page to the keys on your piano.</p>
                     </div>
                 </div>
-                <div class="w-full mb-12 flex items-center">
+                <div class="w-full mb-12 sm:flex items-center">
                     <div class="relative flex-shrink-0">
-                        <img class="h-96" src="">
-                        <h3 class="absolute bottom-0 mx-auto rounded-xl bg-white text-pianote px-4 py-2"><s>$97</s> <strong>FREE</strong></h3>
+                        <img class="h-48 sm:h-64 lg:h-72 rounded-xl overflow-hidden" src="https://www.musora.com/musora-cdn/image/width=750,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/nppsh.jpg">
                     </div>
                     <div class="sm:pl-5 lg:pl-8">
-                        <img class="h-32 mb-3" src="">
+                        <img class="h-14 sm:h-16 lg:h-20 my-3 sm:mt-0 sm:mb-4" src="https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/nppsh-logo.png">
                         <p class="leading-tight">Your first 30 days on the piano. This 30-day challenge will help you come back to the keys with confidence and feel excited to play your piano every day. Simply follow along with Lisa for 10 minutes a day. You’ll have lifetime access to this course.</p>
                     </div>
                 </div>
-                <div class="w-full mb-12 flex items-center">
+                <div class="w-full mb-12 sm:flex items-center">
                     <div class="relative flex-shrink-0 sm:order-1">
-                        <img class="h-96" src="">
-                        <h3 class="absolute bottom-0 mx-auto rounded-xl bg-white text-pianote px-4 py-2"><s>$97</s> <strong>FREE</strong></h3>
+                        <img class="h-48 sm:h-64 lg:h-72 rounded-xl overflow-hidden" src="https://www.musora.com/musora-cdn/image/width=750,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/easy-chords.jpg">
                     </div>
                     <div class="sm:pr-5 lg:pr-8">
-                        <img class="h-32 mb-3" src="">
+                        <img class="h-14 sm:h-16 lg:h-20 my-3 sm:mt-0 sm:mb-4" src="https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/easy-chords-logo.png">
                         <p class="leading-tight">Put what you learned in the webinar to use with this 30-day chording challenge. Play with Lisa every day and master your chord progressions and inversions so you can play any lead sheet with ease. All you have to do is pretty play and follow along.</p>
                     </div>
                 </div>
-                <div class="w-full mb-12 flex items-center">
+                <div class="w-full sm:flex items-center">
                     <div class="relative flex-shrink-0">
-                        <img class="h-96" src="">
-                        <h3 class="absolute bottom-0 mx-auto rounded-xl bg-white text-pianote px-4 py-2"><s>$99</s> <strong>FREE</strong></h3>
+                        <img class="h-48 sm:h-64 lg:h-72 rounded-xl overflow-hidden" src="https://www.musora.com/musora-cdn/image/width=750,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/riffs-fills.jpg">
                     </div>
                     <div class="sm:pl-5 lg:pl-8">
-                        <img class="h-32 mb-3" src="">
+                        <img class="h-14 sm:h-16 lg:h-20 my-3 sm:mt-0 sm:mb-4" src="https://www.musora.com/musora-cdn/image/width=460,quality=95/https://d2vyvo0tyx8ig5.cloudfront.net/sales/promos/september/webinar-offer/riffs-fills-logo.png">
                         <p class="leading-tight">Take your chords and make something beautiful. Lisa will show you her favorite riffs and fills to add some emotion and beauty to your playing. These are the fills she uses every day. She’ll show you each one, note for note.</p>
                     </div>
                 </div>
