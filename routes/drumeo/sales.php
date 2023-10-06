4ed84e7c51f92cf52474c7e7079bb390e0e83312
Trent <trent.harms@gmail.com>
2023-10-06 14:36:41 -0700

    adding pad promo page

--- routes/drumeo/sales.php
+++ routes/drumeo/sales.php
@@ -13,7 +13,7 @@ Route::domain('{drumeoDomain}')
     Route::get('/', [SalesController::class, 'home'] );
     Route::get('/new-year', [SalesController::class, 'promo'] );
     Route::get('/lp', [SalesController::class, 'promo'] );
-    Route::get('/promo-bag', [SalesController::class, 'promoBag'] );
+    Route::get('/promo-stand', [SalesController::class, 'promoStand'] );
     Route::get('/beginner', [SalesController::class, 'promo']);
     Route::get('/choose-plan', [SalesController::class, 'choosePlan'] );
     Route::get('/student-only', [SalesController::class, 'promo'] );
