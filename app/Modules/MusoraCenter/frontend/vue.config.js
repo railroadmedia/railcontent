39d5548da8531bd3f04b648a752952a90f86ac2f
Roxana Riza <roxana_r2000@yahoo.com>
2023-10-10 17:13:51 +0300

    Fix icons issue - SRR-84

--- app/Modules/MusoraCenter/frontend/vue.config.js
+++ app/Modules/MusoraCenter/frontend/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: '/dist/',
+    publicPath: 'vendor/musora-center/dist/' ,
     outputDir: process.env.NODE_ENV === 'production'
         ? '../public/dist/'
         : '../public/dist/js',
