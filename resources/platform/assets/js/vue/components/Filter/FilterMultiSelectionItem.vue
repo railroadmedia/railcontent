29acb909e276b96b78848b02a396979224387e1d
peterminhlee <petereminhlee@gmail.com>
2023-10-11 15:13:54 -0700

    key updates

--- resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
@@ -8,8 +8,8 @@
             <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px] tw-sticky" />
         </div>
         <div :class="isCollapsed ? 'tw-hidden lg:tw-block' : 'tw-pb-6 lg:tw-pb-0'">
-            <li v-for="item in column.items" class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0" :class="isSelected(item.key) ? 'tw-font-bold' : ''" >
-                {{ item.value }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="isSelected(item.key)" />
+            <li v-for="item in column.items" class="tw-flex tw-justify-between tw-items-center tw-mb-2 tw-px-4 md:tw-px-0" :class="isSelected(item.value) ? 'tw-font-bold' : ''" >
+                {{ item.key }} <input class="tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer" type="checkbox" @click="$emit('clickColumnItem', column.category, item)" :checked="isSelected(item.value)" />
             </li>
         </div>
     </ul>
@@ -35,8 +35,8 @@
         isCollapsed.value = !isCollapsed.value;
     }
 
-    const isSelected = (key) => {
-        return props.selectedFilters[props.column.category] && props.selectedFilters[props.column.category].find((f)=> f === key) ? true : false;
+    const isSelected = (value) => {
+        return props.selectedFilters[props.column.category] && props.selectedFilters[props.column.category].find((f)=> f === value) ? true : false;
     }
 </script>
 
