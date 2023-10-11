35fc0991246b547e5956e3288e2dc484443e7c71
Trent <trent.harms@gmail.com>
2023-10-11 16:31:17 -0700

    adding new cio triggers

--- config/lead-tracker.php
+++ config/lead-tracker.php
@@ -1023,6 +1023,91 @@ return [
                 'customer_io_event_name' => 'leadtracker_customer_io_event_name',
             ],
         ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'Pianote webinar Joined',
+            'brand' => 'pianote',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'Pianote Webinar Watched',
+            'brand' => 'pianote',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'Pianote Webinar Replay Watched',
+            'brand' => 'pianote',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'Pianote Webinar Missed',
+            'brand' => 'pianote',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'Pianote Webinar Finished',
+            'brand' => 'pianote',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
         //--------------------------------------------------
         // guitareo
         [
