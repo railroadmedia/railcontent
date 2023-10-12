55a38fc9eac86c785e77a4e6c82314bfe2ac9b67
Trent <trent.harms@gmail.com>
2023-10-11 17:09:03 -0700

    chords hacks updates

--- resources/marketing/views/drumeo/lead-gen/partials/free-trial.blade.php
+++ resources/marketing/views/drumeo/lead-gen/partials/free-trial.blade.php
@@ -5,13 +5,19 @@
                 <img class="w-full" src="https://www.musora.com/musora-cdn/image/width=800,quality=95/{{ $mobileImg }}" alt="albums mobile" />
             </div>
             <h3 class="font-extrabold">{{ $header }}</h3>
-            <p class="uppercase text-promo tracking-widest py-3">{!! $subHeader !!}</p>
+            <p class="uppercase text-musora tracking-widest py-3">{!! $subHeader !!}</p>
             <ul class="fa-ul text-left pl-6 mx-auto inline-block">
                 @foreach($benefits as $benefit)
                     <li class="leading-tight mb-3"><i class="fa-li fas fa-check text-{{ $theme }}" aria-hidden="true"></i> {{ $benefit }}</li>
                 @endforeach
             </ul>
-            <a class="w-full sm:w-72 smaller join bg-{{ $theme }} w-full mt-3 mb-10 block mx-auto sm:ml-0" href="/trial">
+            <a class="w-full sm:w-72 smaller join bg-{{ $theme }} w-full mt-3 mb-10 block mx-auto sm:ml-0"
+              @if(!empty($customLink))
+                  href="{{ $customLink }}"
+              @else
+                href="/trial"
+               @endif
+            >
                 start your free trial
             </a>
         </div>
