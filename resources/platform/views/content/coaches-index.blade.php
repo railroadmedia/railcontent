1efacdb318f540f14864f5a3c870ad4acd7423e7
peterminhlee <petereminhlee@gmail.com>
2023-10-13 09:00:52 -0700

    replaced two separate comps with coach collection wrapper

--- resources/platform/views/content/coaches-index.blade.php
+++ resources/platform/views/content/coaches-index.blade.php
@@ -159,7 +159,7 @@
     @endcomponent
 
     <div class="tw-container tw-mx-auto tw-px-4 md:tw-px-8 tw-mb-3">
-        <collection-filter-wrapper
+        <coach-collection-wrapper
             brand="{{ $brand }}"
             content-type="coach"
             :filterable-values="{{ json_encode(['focus','style']) }}"
@@ -174,42 +174,7 @@
                     [ 'value' => 'subscribedCoaches', 'key' => 'Subscribed Coaches' ]
                 ])
             }}"
-        ></collection-filter-wrapper>
-
-        <transition appear name="fade">
-            <content-collection />
-{{--            <content-catalogue--}}
-{{--                    theme-color="{{ $brand }}"--}}
-{{--                    brand="{{ $brand }}"--}}
-{{--                    content-endpoint="/railcontent/content?only_subscribed={{$onlySubscribedCoaches}}"--}}
-{{--                    catalogue-type="coaches-grid"--}}
-{{--                    limit="{{ $limitOverride ?? 18 }}"--}}
-{{--                    :infinite-scroll="true"--}}
-{{--                    :statuses="{{ json_encode(['published', 'scheduled']) }}"--}}
-{{--                    :filterable-values="{{ json_encode(['focus','style']) }}"--}}
-{{--                    :included-types="{{ json_encode(['instructor']) }}"--}}
-{{--                    :required-fields="{{json_encode(['is_coach,1'])}}"--}}
-{{--                    :pre-loaded-content="{{ $coaches->toResponseRawJson() }}"--}}
-{{--                    user-id="{{ auth()->id() }}"--}}
-{{--                    :is-admin="{{ json_encode(user()->isAdmin()) }}"--}}
-{{--                    :use-url-params="true"--}}
-{{--                    :lock-unowned="true"--}}
-{{--                    :show-loading-animation="true"--}}
-{{--                    sort-override="slug"--}}
-{{--                    :six-wide="true"--}}
-{{--                    @if(!empty($showSearch))--}}
-{{--                    :search-bar="true"--}}
-{{--                    :total-results="{{ $limitOverride ?? 18 }}"--}}
-{{--                    :search-endpoint="{{json_encode(url()->route('content.index'))}}"--}}
-{{--                    @endif--}}
-{{--            >--}}
-{{--                @for($i = 0; $i < ($limitOverride ?? 18); $i++)--}}
-{{--                    @include('partials.bladesora.members.skeletons.card-item', [--}}
-{{--                    "cardClass" => 'six-wide',--}}
-{{--                    ])--}}
-{{--                @endfor--}}
-{{--            </content-catalogue>--}}
-        </transition>
+        />
     </div>
 
 @endsection
