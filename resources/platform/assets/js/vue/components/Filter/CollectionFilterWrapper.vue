592ce16b6fdacc7c3c58f4c0f86c033b4d558402
peterminhlee <petereminhlee@gmail.com>
2023-10-13 09:01:50 -0700

    removed mount some mount code related to store

--- resources/platform/assets/js/vue/components/Filter/CollectionFilterWrapper.vue
+++ resources/platform/assets/js/vue/components/Filter/CollectionFilterWrapper.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
         <filter-controls
-            :title="title"
+            :collection-title="collectionTitle"
             :search-term="state.searchTerm"
             :is-collapsed="isCollapsed"
-            :selected-sort="collectionStore.filter.sort"
-            :sort-option-data="sortOptions"
+            :selected-sort="collectionStore.tabData[collectionStore.filter.activeTab] && collectionStore.tabData[collectionStore.filter.activeTab].sort"
             :tab-options="tabOptions"
             :active-tab="collectionStore.filter.activeTab"
             :hide-filter-icon="state.multiSelectColumns.length === 0"
             @on-toggle-collapse="handleToggleCollapse"
             @on-search-submit="handleSearch"
             @on-sort="handleSort"
+            @on-filter-tab-click="(tabClick)"
         >
             <slot name="viewToggleButton"></slot>
         </filter-controls>
@@ -35,7 +35,7 @@
 </template>
 
 <script setup>
-import {onBeforeMount, onMounted, onUnmounted, reactive, ref} from 'vue';
+    import { onMounted, onUnmounted, reactive, ref } from 'vue';
     import { useCollectionStore } from "../../../stores/collection";
     import ContentHelpers from '../../vuesora/assets/js/helper-functions/content.js';
     import FilterOptions from './FilterOptions.vue';
@@ -43,49 +43,17 @@ import {onBeforeMount, onMounted, onUnmounted, reactive, ref} from 'vue';
     import FilterControls from './FilterControls.vue';
 
     const props = defineProps({
-        brand:{
+        collectionTitle: {
             type: String,
-            default: () => "drumeo",
-        },
-        contentEndpoint: {
-            type: String,
-            default: () => "/railcontent/content",
-        },
-        contentType: {
-            type: String,
-            default: () => '',
+            default: '',
         },
         filterableValues: {
             type: Array,
             default: () => [],
         },
-        includedFields: {
-            type: Array,
-            default: () => [],
-        },
-        includedTypes: {
-            type: Array,
-            default: () => [],
-        },
-        includeFutureScheduledContentOnly: {
-            type: Boolean,
-            default: () => false,
-        },
-        infiniteScroll: {
-            type: Boolean,
-            default: () => false,
-        },
-        limit: {
-            type: String,
-            default: () => "10",
-        },
         preLoadedContent: {
             type: Object,
             default: () => ({}),
-        },
-        selectedSort: {
-            type: String,
-            default: '',
         },
         singleSelectColumns: {
             type: Array,
@@ -109,33 +77,9 @@ import {onBeforeMount, onMounted, onUnmounted, reactive, ref} from 'vue';
                 },
             ],
         },
-        sortOptions: {
-            type: Array,
-            default: [],
-        },
-        statuses: {
-            type: Array,
-            default: () => ["published"],
-        },
         tabOptions: {
             type: Array,
             default: [],
-        },
-        title: {
-            type: String,
-            default: '',
-        },
-        totalResults: {
-            type: [String, Number],
-            default: () => 0,
-        },
-        requiredFields: {
-            type: Array,
-            default: () => [],
-        },
-        requiredUserStates: {
-            type: Array,
-            default: () => [],
         },
     });
 
@@ -168,8 +112,7 @@ import {onBeforeMount, onMounted, onUnmounted, reactive, ref} from 'vue';
     }
 
     const handleFilterChange = (selection) => {
-        state.selectedFilters = selection;
-        state.page = 1;
+
     }
 
     const getFilterColumns = () => {
@@ -183,44 +126,20 @@ import {onBeforeMount, onMounted, onUnmounted, reactive, ref} from 'vue';
         state.multiSelectColumns = filters;
     }
 
-    const request_params = () => {
-        return {
-            required_fields: props.requiredFields,
-            statuses: props.statuses,
-            required_user_states: props.requiredUserStates,
-            [props.contentType === 'coach' ? 'term' : 'title']: state.searchTerm,
-            included_types: props.includedTypes,
-            include_future_scheduled_content_only: props.includeFutureScheduledContentOnly,
-            included_fields: props.included_fields || [],
-            limit: props.limit,
-        };
-    }
-
     const handleSort = (item) => {
         collectionStore.sortData(item);
         collectionStore.setParams();
     }
 
-    onBeforeMount(()=>{
-        collectionStore.getParams();
-    })
+    const tabClick = (tab) => {
+        collectionStore.switchTab(tab);
+    }
 
     onMounted(()=>{
         getScreenSize();
         window.addEventListener('resize', getScreenSize);
 
         getFilterColumns();
-
-        collectionStore.setDefaults({
-            brand: props.brand,
-            data: props.preLoadedContent?.data || [],
-            filter: {
-                activeTab: props.tabOptions ? props.tabOptions[0].value : '',
-                params: {...request_params()},
-                term: '',
-            },
-            totalPages: props.preLoadedContent ? Math.ceil(props.preLoadedContent.meta.totalResults / props.limit) : 0,
-        })
     })
 
     onUnmounted(()=>{
