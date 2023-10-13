5d9cb673f8be4b1ceb0222c58c663f4395b2c67f
peterminhlee <petereminhlee@gmail.com>
2023-10-13 09:02:18 -0700

    emit tab click

--- resources/platform/assets/js/vue/components/Filter/FilterTabs.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterTabs.vue
@@ -2,7 +2,7 @@
     <div class="tw-flex tw-flex-wrap tw-gap-[4px] md:tw-gap-[10px] tw-text-sm">
         <button
             v-for="option in tabOptions"
-            @click="collectionStore.switchTab(option.value)"
+            @click="onTabClick(option.value)"
             class="tw-flex tw-items-center tw-justify-center tw-text-center tw-h-[40px] md:tw-h-[50px] tw-uppercase tw-text-[14px] md:tw-text-[20px] tw-font-bebas-neue tw-rounded-[55px] tw-border tw-font-semibold dark:tw-border-[#445F74] tw-px-4 lg:tw-px-6"
             :class="isActive(option.value) ? `tw-bg-[#28282D] dark:tw-bg-[#445F74] tw-text-white` : `hover:tw-bg-[#E7E7E8] hover:dark:tw-bg-[#223F57] hover:dark:tw-text-white tw-bg-white dark:tw-bg-[#000C17] tw-border-[#CBCBCD] dark:tw-text-white`"
         >
@@ -12,8 +12,6 @@
 </template>
 
 <script setup>
-    import {useCollectionStore} from "../../../stores/collection";
-
     const props = defineProps({
         tabOptions: {
             type: Array,
@@ -25,8 +23,6 @@
         },
     });
     const emit = defineEmits(['onTabClick']);
-
-    const collectionStore = useCollectionStore();
 
     const isActive = (value) => {
         return value === props.activeTab;
