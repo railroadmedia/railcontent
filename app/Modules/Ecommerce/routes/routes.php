cb68297a14f29c817492d56a2e3923d1c0ff220d
Caleb Favor <calebfavor@gmail.com>
2023-10-22 05:16:29 -0700

    Fixing broken routes for ecom

--- app/Modules/Ecommerce/routes/routes.php
+++ app/Modules/Ecommerce/routes/routes.php
@@ -6,24 +6,6 @@ use App\Modules\Ecommerce\Controllers\UserAccessPermissionsController;
 use Illuminate\Support\Facades\Route;
 use Railroad\Ecommerce\Controllers\AppleStoreKitController;
 
-<<<<<<< HEAD
-Route::prefix(config('ecommerce.route_prefix'))->group(function () {
-    Route::post(
-        'apple/webhook/notification/v1',
-        AppleStoreKitController::class . '@processNotification'
-    );
-
-    Route::post(
-        'revenuecat/webhook/notification',
-        \App\Modules\Ecommerce\Controllers\RevenueCatController::class . '@processNotification'
-    );
-
-    Route::post(
-        'revenuecat/restore',
-        \App\Modules\Ecommerce\Controllers\RevenueCatController::class . '@syncSubscriber'
-    )->middleware([\Modules\UserManagementSystem\Middleware\AuthenticateIfAvailable::class]);
-});
-=======
 Route::prefix(config('ecommerce.route_prefix'))
     ->group(function () {
         Route::post(
