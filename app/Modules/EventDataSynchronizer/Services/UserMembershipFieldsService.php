64883a7d9fd0eca004257dcf608939b242510e13
robwillems <robert@musora.com>
2023-10-25 16:20:05 -0700

    Remove debugging

--- app/Modules/EventDataSynchronizer/Services/UserMembershipFieldsService.php
+++ app/Modules/EventDataSynchronizer/Services/UserMembershipFieldsService.php
@@ -89,15 +89,6 @@ class UserMembershipFieldsService
     public function syncUserAccess(UserAccessPermissionsCollection $userAccessPermissions): bool
     {
         $userId = $userAccessPermissions->getUserId();
-        Log::debug("syncUserAccess $userId");
-        Log::debug(
-            print_r(
-                $userAccessPermissions->getCollection()->map(function ($item) {
-                    return "$item->id $item->permission_id $item->start_time";
-                })->toArray(),
-                true
-            )
-        );
 
         //This code is difficult to understand and should be thoroughly understood before changing it
         //membership expiration date comes from both plus and basic permissions combined
