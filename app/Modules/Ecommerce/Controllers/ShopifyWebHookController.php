8969fe77d8c18a45cf55d833534039d58c47d207
Alexandre Severo <alexandre@musora.com>
2023-10-27 16:52:29 -0700

    Trigger OrderPlaced event job

--- app/Modules/Ecommerce/Controllers/ShopifyWebHookController.php
+++ app/Modules/Ecommerce/Controllers/ShopifyWebHookController.php
@@ -3,6 +3,8 @@
 namespace App\Modules\Ecommerce\Controllers;
 
 use App\Modules\Ecommerce\Services\ShopifySyncService;
+use App\Modules\EventDataSynchronizer\Jobs\CustomerIoSyncOrderPlaced;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
@@ -41,8 +43,7 @@ class ShopifyWebHookController extends Controller
             $shopifyCustomerId = $request->get('customer')['id'];
             $email = $request->get('customer')['email'];
             Log::debug("Shopify customer id: $shopifyCustomerId email: $email");
-            $this->shopifySyncService->syncCustomer($shopifyCustomerId, $email);
-            event(new OrderPlaced($request->all()));
+            dispatch(new CustomerIoSyncOrderPlaced($request->all()))->delay(Carbon::now()->addSeconds(3));
         } catch (\Exception $e) { //Catch exception to prevent shopify from retrying the webhook
             Log::error($e->getMessage());
             Log::error($e->getTraceAsString());
