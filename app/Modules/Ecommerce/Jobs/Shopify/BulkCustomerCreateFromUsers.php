0909c5c80e3770b3e271ebdc8d258bfa9c83ccd6
Brandon Kerr <brandonk@musora.com>
2023-10-30 16:35:21 -0400

    Improved functionality for creating Shopify metafields. Improve support for Users, allowing for updates

--- app/Modules/Ecommerce/Jobs/Shopify/BulkCustomerCreateFromUsers.php
+++ app/Modules/Ecommerce/Jobs/Shopify/BulkCustomerCreateFromUsers.php
@@ -172,22 +172,7 @@ class BulkCustomerCreateFromUsers implements ShouldQueue
             // "tags" => "",
         ];
 
-        // refer to https://shopify.dev/docs/apps/custom-data/metafields/types
-        // we can use meta fields for stuff like our user id, etc
-        $customerData["metafields"] = [
-            [
-                "key" => ShopifyMetafieldKey::Id->value,
-                "value" => (string)$user->id,
-                "type" => ShopifyMetafieldTypes::integer->value,
-                "namespace" => ShopifyMetafieldNamespace::Model_Users->value
-            ],
-            [
-                "key" => ShopifyMetafieldKey::IsMusoraAccountSetUp->value,
-                "value" => "true",
-                "type" => ShopifyMetafieldTypes::boolean->value,
-                "namespace" => ShopifyMetafieldNamespace::Musora->value
-            ]
-        ];
+        $customerData["metafields"] = $user->getMetafieldsForShopify();
 
         $customerData["addresses"] = $this->createAddressesData($user, $userCustomers);
 
--- app/Modules/Ecommerce/Jobs/Shopify/SyncUsersToShopify.php
+++ app/Modules/Ecommerce/Jobs/Shopify/SyncUsersToShopify.php
@@ -17,6 +17,7 @@ use App\Modules\Ecommerce\Jobs\Shopify\Traits\SyncsToShopify;
 use Carbon\Carbon;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Exception\ORMException;
+use Exception;
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -369,10 +370,10 @@ class SyncUsersToShopify implements ShouldQueue
      * new Customers to be made after a User already exists with the same email address.
      *
      * @param  User  $user
-     * @param  bool  $withMetafields
+     * @param  bool  $isCreating
      * @return array
      */
-    private function createCustomerDataForUser(User $user, bool $withMetafields): array
+    private function createCustomerDataForUser(User $user, bool $isCreating): array
     {
         $customerData = [
             "currency" => "USD",
@@ -384,23 +385,25 @@ class SyncUsersToShopify implements ShouldQueue
             // "tags" => "",
         ];
 
-        if ($withMetafields) {
-            // refer to https://shopify.dev/docs/apps/custom-data/metafields/types
-            // we can use meta fields for stuff like our user id, etc
-            $customerData["metafields"] = [
-                [
-                    "key" => ShopifyMetafieldKey::Id->value,
-                    "value" => (string)$user->id,
-                    "type" => ShopifyMetafieldTypes::integer->value,
-                    "namespace" => ShopifyMetafieldNamespace::Model_Users->value
-                ],
-                [
-                    "key" => ShopifyMetafieldKey::IsMusoraAccountSetUp->value,
-                    "value" => "true",
-                    "type" => ShopifyMetafieldTypes::boolean->value,
-                    "namespace" => ShopifyMetafieldNamespace::Musora->value
-                ]
-            ];
+        if ($isCreating) {
+            $customerData["metafields"] = $user->getMetafieldsForShopify();
+        } else {
+            try {
+                $newMetafields = $user->getNewMetafieldsForShopify();
+                $this->handleRateLimit();
+                if (!empty($newMetafields)) {
+                    $customerData["metafields"] = $newMetafields;
+                }
+            } catch (Exception $exception) {
+                $this->logError(
+                    sprintf(
+                        "%s: Failed to find new metafields to send to Shopify for updating user with email address %s: %s",
+                        $this->getClassName(),
+                        $user->email,
+                        $exception->getMessage()
+                    )
+                );
+            }
         }
 
         return $customerData;
