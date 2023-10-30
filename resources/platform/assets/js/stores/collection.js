41d87b771e655fd5c7244bd6848c835ddfc3a0e8
peterminhlee <petereminhlee@gmail.com>
2023-10-30 08:19:42 -0700

    optimisations

--- resources/platform/assets/js/stores/collection.js
+++ resources/platform/assets/js/stores/collection.js
@@ -21,17 +21,17 @@ export const useCollectionStore = defineStore({
         }
     },
     actions: {
-        applyFilter (category, item, clear= false) {
-            if (clear) this.resetFilterFields();
-            else this.updateFilterFields(category, item);
-
+        applyFilter (category, item) {
+            this.updateFilterFields(category, item);
             this.setAllTabsToFilterNotApplied();
             this.setActiveTabToFilterApplied();
             this.getData();
         },
 
         clearFilter () {
-            this.applyFilter(null, null, true);
+            this.resetFilterFields();
+            this.setAllTabsToFilterNotApplied();
+            this.setActiveTabToFilterApplied();
             this.getData();
         },
 
