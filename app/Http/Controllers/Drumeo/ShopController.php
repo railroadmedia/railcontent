37ec00bb048f8ffadecc28c35b6eeda2d5700d92
peterminhlee <petereminhlee@gmail.com>
2023-10-30 12:46:10 -0700

    added featured

--- app/Http/Controllers/Drumeo/ShopController.php
+++ app/Http/Controllers/Drumeo/ShopController.php
@@ -45,6 +45,8 @@ class ShopController extends BaseController
             return $value->productType->name === 'Hoodies';
         });
 
+        $featured = $products->whereIn('id', [95, 97, 98, 100]);
+
         return view('drumeo.drumshop.shop', [
             'allProducts' => $products,
             'lessons' => $lessons,
@@ -53,7 +55,8 @@ class ShopController extends BaseController
             'shirts' => $shirts,
             'hoodies' => $hoodies,
             'theme' => 'drumeo',
-            'category' => $request->category
+            'category' => $request->category,
+            'featured' => $featured
         ]);
     }
 
