f618473addbc8f7d4ef52552f37c6c48fa535798
Trent <trent.harms@gmail.com>
2023-11-01 11:28:47 -0700

    trashing 5 for 3 bundles

--- routes/pianote/shop.php
+++ routes/pianote/shop.php
@@ -29,7 +29,6 @@ Route::domain('{pianoteDomain}')
                 Route::get('/the-power-of-chords-giveaway', [SalesController::class, 'PowerOfChordsGiveaway'] );
                 Route::get('/worship-piano', [SalesController::class, 'worshippiano'] );
                 Route::get('/beautiful-beginner-bundle', [SalesController::class, 'beautifulBeginnerBundle'] );
-                Route::get('/5-for-3-bundle', [SalesController::class, 'fiveforthreeBundle'] );
                 Route::get('/metronome', [SalesController::class, 'metronome'] );
             }
         );
