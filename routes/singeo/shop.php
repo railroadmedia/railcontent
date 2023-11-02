ff557a8e061522c71a97175ae87bf4c9eec99465
Trent <trent.harms@gmail.com>
2023-11-02 11:49:07 -0700

    moving 2 routes

--- routes/singeo/shop.php
+++ routes/singeo/shop.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Singeo\ShopController;
+use App\Http\Controllers\Singeo\SalesController;
 
 Route::domain('{singeoDomain}')
     ->middleware(['web_public'])
     ->group(function () {
         Route::group(['prefix' => 'shop' ],
             function () {
+                Route::get('/singing-starter-kit', [SalesController::class, 'singingstarterkit']);
+                Route::get('/beautiful-harmonies', [SalesController::class, 'beautifulharmonies']);
             }
         );
 
