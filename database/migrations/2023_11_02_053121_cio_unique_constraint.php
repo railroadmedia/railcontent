e59d79ddb7276182dc99257d39e5bbd52a57e834
robwillems <robert@musora.com>
2023-11-01 22:35:57 -0700

    Change cio unique index to include the workspace id

---
+++ database/migrations/2023_11_02_053121_cio_unique_constraint.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer_io_customers', function (Blueprint $table) {
+            $table->dropUnique('customer_io_customers_uuid_unique');
+            $table->unique(['workspace_id', 'uuid'], 'customer_io_customers_uuid_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    { Schema::table('customer_io_customers', function (Blueprint $table) {
+        $table->dropUnique('customer_io_customers_uuid_unique');
+        $table->unique('uuid', 'customer_io_customers_uuid_unique');
+    });
+    }
+};
