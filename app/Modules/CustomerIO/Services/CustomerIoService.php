6fac1f4718d93501f613d48fe67bd7c13adf850c
robwillems <robert@musora.com>
2023-11-05 22:10:47 -0800

    Fix customer io shopify issue

--- app/Modules/CustomerIO/Services/CustomerIoService.php
+++ app/Modules/CustomerIO/Services/CustomerIoService.php
@@ -171,7 +171,7 @@ class CustomerIoService
 
         // uuid (this is what is used inside customer.io
         if (empty($id)) {
-            if ($userId) {
+            if ($userId && config('shopify.enabled')) {
                 $customer->uuid = $userId;
             } else {
                 $customer->generateUUID();
