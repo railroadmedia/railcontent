d4eda753782cd09c05a68a0a7400650f4b55a6b8
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-11-06 15:50:23 -0800

    Updated button style

--- resources/marketing/views/drumeo/pages/easy-rudiments-playlist.blade.php
+++ resources/marketing/views/drumeo/pages/easy-rudiments-playlist.blade.php
@@ -39,7 +39,7 @@
                     <div class="flex justify-center">
                         <a href="https://music.apple.com/ca/artist/drumeo/1707098843" target="_blank"
                             x-data="{ isHovered: false }">
-                            <button class="text-xl sm:text-2xl rounded-full w-40 sm:w-48 md:w-72 h-12 sm:h-16 mx-2"
+                            <button class="text-xl sm:text-2xl rounded-full w-40 sm:w-56 md:w-72 h-12 sm:h-16 mx-2"
                                 style="background: #2EBD59" @mouseenter="isHovered = true" @mouseleave="isHovered = false"
                                 :style="{ filter: isHovered ? 'brightness(120%)' : 'brightness(100%)' }">
                                 <i class="fas fa-brands fa-spotify text-white"></i> <strong> Spotify </strong>
@@ -48,7 +48,7 @@
 
                         <a href="https://open.spotify.com/artist/7pxX1NadzHWfzSXyAPO2ha" target="_blank"
                             x-data="{ isHovered: false }">
-                            <button class="bg-black text-xl sm:text-2xl uppercase rounded-full w-40 sm:w-48 md:w-72 h-12 sm:h-16 mx-2"
+                            <button class="bg-black text-xl sm:text-2xl uppercase rounded-full w-40 sm:w-56 md:w-72 h-12 sm:h-16 mx-2"
                                 style="background: #000000" @mouseenter="isHovered = true" @mouseleave="isHovered = false"
                                 :style="{ 'background-color': isHovered ? '#323232' : '#000000' }">
                                 <i class="fas fa-brands fa-apple"></i> <strong> Music </strong>
