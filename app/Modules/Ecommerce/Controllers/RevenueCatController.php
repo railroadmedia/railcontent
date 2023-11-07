c292c28112c81a4a516786c64d98c60e087d8997
Alexandre Severo <alexandre.severo@me.com>
2023-11-07 12:36:38 -0800

    Update app/Modules/Ecommerce/Controllers/RevenueCatController.php
    Co-authored-by: Brandon <brandonk@musora.com>

--- app/Modules/Ecommerce/Controllers/RevenueCatController.php
+++ app/Modules/Ecommerce/Controllers/RevenueCatController.php
@@ -398,7 +398,7 @@ class RevenueCatController extends Controller
                     $processedAt = Carbon::createFromTimestampMs($data['event']['purchased_at_ms']);
                     $expiredAt = Carbon::createFromTimestampMs($data['event']['expiration_at_ms']);
                     if ($data['event']['environment'] == self::SANDBOX_ENVIRONMENT) {
-                        UserAccessPermissionsService::$timeMinutes = round($expiredAt->diffInSeconds($processedAt)/60);
+                        UserAccessPermissionsService::$timeMinutes = $expiredAt->diffInMinutes($processedAt);
                     }
                     $orderId = $this->shopifySyncService->getCustomerOrderIdByProcessedAtDate($user->shopify_id, $processedAt);
                     if ($orderId) {
