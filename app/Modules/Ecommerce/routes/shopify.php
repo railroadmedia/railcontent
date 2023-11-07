8c8d9f2e6a4374f74570ce96a02f099752ee0693
robwillems <robert@musora.com>
2023-11-07 01:15:39 -0800

    Try to fix webhook routes

--- app/Modules/Ecommerce/routes/shopify.php
+++ app/Modules/Ecommerce/routes/shopify.php
@@ -5,19 +5,17 @@ use App\Modules\Ecommerce\Controllers\ShopifyWebHookController;
 use App\Modules\Ecommerce\Middleware\ShopifyWebhookVerify;
 use Illuminate\Support\Facades\Route;
 
+Route::post('ecommerce/shopify/webhook/order/update', [ShopifyWebHookController::class, 'orderUpdated'])
+    ->middleware(ShopifyWebhookVerify::class)
+    ->name('shopify.webhook.order.update');
+Route::post('ecommerce/shopify/webhook/order/create', [ShopifyWebHookController::class, 'orderCreated'])
+    ->middleware(ShopifyWebhookVerify::class)
+    ->name('shopify.webhook.order.create');
+Route::post('ecommerce/shopify/webhook/refunds/create', [ShopifyWebHookController::class, 'refundCreated'])
+    ->middleware(ShopifyWebhookVerify::class)
+    ->name('shopify.webhook.refund.create');
+
 Route::prefix('ecommerce/shopify')->group(function () {
-    Route::prefix('webhook')
-        ->middleware(ShopifyWebhookVerify::class)
-        ->group(function () {
-            Route::post('order/update', [ShopifyWebHookController::class, 'orderUpdated'])
-                ->name('shopify.webhook.order.update');
-
-            Route::post('order/create', [ShopifyWebHookController::class, 'orderCreated'])
-                ->name('shopify.webhook.order.create');
-
-            Route::post('refunds/create', [ShopifyWebHookController::class, 'refundCreated'])
-                ->name('shopify.webhook.refund.create');
-        });
 
     Route::prefix('cart')
         ->group(function () {
@@ -35,6 +33,9 @@ Route::prefix('ecommerce/shopify')->group(function () {
 
 Route::middleware(['web_public'])
     ->group(function () {
-        Route::get('/order/{brand?}', [\App\Modules\Ecommerce\Controllers\ShopifyCartAPIController::class, 'redirectToShopifyOrderForm'])
+        Route::get(
+            '/order/{brand?}',
+            [\App\Modules\Ecommerce\Controllers\ShopifyCartAPIController::class, 'redirectToShopifyOrderForm']
+        )
             ->name('redirect-to-shopify-order-form');
     });
