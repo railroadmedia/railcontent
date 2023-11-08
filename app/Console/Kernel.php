d92a5864b315eab9164513849fea88a56937bf27
robwillems <robert@musora.com>
2023-11-08 07:26:42 -0800

    Disable subscription processing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -97,12 +97,12 @@ class Kernel extends ConsoleKernel
             "0 */6 * * *"
         );//4am, 10am, 4pm, 10pm PST
 
-        $schedule->command('ecommerce:renewalDueSubscriptions 200')->cron(
-            "15 */2 * * *"
-        );
-        $schedule->command('ecommerce:ProcessAppleExpiredSubscriptionsQueued')->cron(
-            "30 */8 * * *"
-        ); // every 8 hours: 12am, 8am, 4pm PST
+//        $schedule->command('ecommerce:renewalDueSubscriptions 200')->cron(
+//            "15 */2 * * *"
+//        );
+//        $schedule->command('ecommerce:ProcessAppleExpiredSubscriptionsQueued')->cron(
+//            "30 */8 * * *"
+//        ); // every 8 hours: 12am, 8am, 4pm PST
 
         $schedule->command('mentors:verify')->daily(); //4pm
         //temporary measure to assign mentors until ecommerce is integrated with MWP
