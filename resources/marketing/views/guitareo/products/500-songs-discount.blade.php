acf5fdf573d9834fb6a95c7d6e64b531ea7beed4
Trent <trent.harms@gmail.com>
2023-11-08 10:16:57 -0800

    fixing discounts

--- resources/marketing/views/guitareo/products/500-songs-discount.blade.php
+++ resources/marketing/views/guitareo/products/500-songs-discount.blade.php
@@ -3,7 +3,7 @@
 @php $productPrice = 9 @endphp
 
 @section('order-link')
-    {{ url()->route('shopping-cart.add-to-cart', ['products' => ['500-songs-in-5-days-guitareo' => 1], 'redirect' => '/order', 'promo-code' => 'special-discount']) }}
+    {{ url()->route('shopping-cart.add-to-cart', ['products' => ['500-songs-in-5-days-guitareo' => 1], 'redirect' => '/order', 'promo-code' => 'lead-discount2']) }}
 @endsection
 
 @section('product-json')
