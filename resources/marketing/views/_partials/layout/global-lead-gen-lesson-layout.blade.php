af0d1bc561792c6cf56b6078e2db3d971b3d157e
Trent <trent.harms@gmail.com>
2023-11-08 15:51:02 -0800

    fixing free book

--- resources/marketing/views/_partials/layout/global-lead-gen-lesson-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-lead-gen-lesson-layout.blade.php
@@ -236,7 +236,7 @@
             'benefits' => ['FREE Chords & Scales book', 'The perfect step-by-step curriculum', 'Beginner-friendly song tutorials', 'Live support from REAL teachers'],
             'img' => 'https://pianote.s3.amazonaws.com/products/30-day-blues-piano/collage-lessons.png',
             'mobileImg' => 'https://pianote.s3.amazonaws.com/products/30-day-blues-piano/collage-lessons-m.png',
-            'customLink' => '/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-TRIAL-7-DAY-ANNUAL]=1&products[piano-chords-and-scales-guide]=1&promo-code=annual-trial&redirect=/order&locked=true',
+            'customLink' => '/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-TRIAL-7-DAY-ANNUAL]=1&products[piano-chords-and-scales-guide]=1&promo-code=trial-book&redirect=/order&locked=true',
         ])
     @endif
 
