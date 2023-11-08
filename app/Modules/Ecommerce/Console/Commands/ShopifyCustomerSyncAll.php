ca491ead08511692bea6740cb4c8c40291214263
robwillems <robert@musora.com>
2023-11-08 10:48:19 -0800

    Add option to rebuild permissions on sync

--- app/Modules/Ecommerce/Console/Commands/ShopifyCustomerSyncAll.php
+++ app/Modules/Ecommerce/Console/Commands/ShopifyCustomerSyncAll.php
@@ -7,14 +7,15 @@ use App\Modules\Ecommerce\Jobs\ShopifyCustomersSyncAllJob;
 
 class ShopifyCustomerSyncAll extends Command
 {
-    protected $signature = 'ecommerce:ShopifyCustomerSyncAll {startId?} {endId?}';
+    protected $signature = 'ecommerce:ShopifyCustomerSyncAll {startId} {endId} {--rebuildPermissions}';
 
     public function handle()
     {
         $startId = $this->argument('startId') ?? null;
         $endId = $this->argument('endId') ?? null;
-        $this->runBatchQuery(function (int $skip, int $take) use ($startId, $endId) {
-            return new ShopifyCustomersSyncAllJob($skip, $take, $startId, $endId);
+        $isRebuildingPermissions = $this->option('rebuildPermissions');
+        $this->runBatchQuery(function (int $skip, int $take) use ($isRebuildingPermissions, $startId, $endId) {
+            return new ShopifyCustomersSyncAllJob($skip, $take, $startId, $endId, $isRebuildingPermissions);
         }, chunks: 100, queue: 'command');
     }
 }
