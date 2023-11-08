dd7a45a0e6c54932a413fdc70ad54f94f8cd4cca
robwillems <robert@musora.com>
2023-11-07 17:18:39 -0800

    Handle membership expiration for users with 100% revoked membership permissions.

--- app/Providers/MusoraApiUserProvider.php
+++ app/Providers/MusoraApiUserProvider.php
@@ -130,7 +130,7 @@ class MusoraApiUserProvider implements UserProviderInterface
 
         return [
             'isEdge' => $user->isAMember(),
-            'isEdgeExpired' => $user->isAnExpiredMember(),
+            'isEdgeExpired' => !$user->membership_expiration_date || $user->isAnExpiredMember(),
             'edgeExpirationDate' => $user->membership_expiration_date,
             'isPackOnlyOwner' => $user->isPackOnlyOwner(),
             'isAppleAppSubscriber' => $isAppleAppSubscriber,
