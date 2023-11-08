b001b193aec20ca392f0b0d9628da3edf6fdc462
calebfavor <calebfavor@gmail.com>
2023-11-08 07:51:48 -0800

    Fixing onboarding not redirecting to login if user is already setup.

--- app/Modules/UserManagementSystem/Controllers/OnboardingController.php
+++ app/Modules/UserManagementSystem/Controllers/OnboardingController.php
@@ -42,12 +42,13 @@ class OnboardingController extends Controller
             throw new AuthorizationException('Invalid verification_token.', 403);
         }
 
-        if (user() && user()->getEmail() == $email && user()->doesRequirePasswordUpdate()) {
+        if (user() && user()->getEmail() == $email && !user()->doesRequirePasswordUpdate()) {
             return redirect()->to('/members');
         }
 
         $user = User::query()->where('email', $email)->first() ?? null;
-        if ($user && $user->doesRequirePasswordUpdate()) {
+
+        if ($user && !$user->doesRequirePasswordUpdate()) {
             return redirect()->route('login', ['email' => $email]);
         }
 
