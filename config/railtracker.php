e3e34764ed963d4a659bb1d9faa1237a35852868
robwillems <robert@musora.com>
2023-11-09 14:10:18 -0800

    Revert "Enable media-playback-session login to find issue"
    This reverts commit 2087f30454499a2cccdfa079789d41de2c4a3e8d.

--- config/railtracker.php
+++ config/railtracker.php
@@ -37,7 +37,7 @@ return [
     'exclusion_regex_paths'=> [
         '/members\/live\-poll/',
         '/members\/user\-video\-session\/store/',
-        //'/railtracker\/media\-playback\-sessions*/',
+        '/railtracker\/media\-playback\-sessions*/',
         '/musora\-api\/v1\/media*/',
         '/members\/live\/are\-we\-live\-poll*/',
         '/menu\-api*/',
