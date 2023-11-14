04ca94ce2702e9543042c60108981167b35d2d4f
peterminhlee <petereminhlee@gmail.com>
2023-11-14 09:58:34 -0800

    added carousel

--- app/Http/Controllers/Platform/WorkoutsPageController.php
+++ app/Http/Controllers/Platform/WorkoutsPageController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers\Platform;
 
+use App\Modules\Content\Services\CarouselService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 
 class WorkoutsPageController extends BaseController
 {
+    private CarouselService $carouselService;
+
+    public function __construct(CarouselService $carouselService){
+        $this->carouselService = $carouselService;
+    }
+
     public function showWorkoutsPage(Request $request)
     {
-        return view('pages.workouts');
+        $carousel = $this->carouselService->getCarouselSlides()->where('is_featured', 1);
+
+        return view('pages.workouts', [
+            'carousel' => $carousel,
+        ]);
     }
 }
--- resources/platform/views/pages/workouts.blade.php
+++ resources/platform/views/pages/workouts.blade.php
@@ -9,6 +9,7 @@
 
         <section>
             <h1>Featured Challenges</h1>
+            <header-carousel :preloaded-carousel="{{ $carousel }}" />
         <section>
         <br>
         <section>
