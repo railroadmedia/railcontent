96f06a322bef9d1b40479a42a8044af10c21ace3
Trent <trent.harms@gmail.com>
2023-11-16 13:14:25 -0800

    adding 1/3 split option

--- resources/marketing/views/_partials/layout/holiday/bundle-tiles.blade.php
+++ resources/marketing/views/_partials/layout/holiday/bundle-tiles.blade.php
@@ -3,8 +3,8 @@
         <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-badge-percent text-{{ $brand }} mr-1"></i> {!!  $header  !!}</strong></h5>
         <div class="flex flex-wrap mb-5 sm:mb-10">
             @foreach(array_filter($bundles, function($val){if($val['visible'] === 1){ return 1; }}) as $key => $bundle)
-                <a href="{{ $bundle['slug'] }}" class="w-full @if(empty($bundle['full'])) sm:w-1/2 @endif mx-auto p-1 sm:p-2">
-                    <div class="flex items-center @if(empty($bundle['full'])) justify-center @endif w-full overflow-hidden relative text-white rounded-xl px-5 lg:px-16 xl:pl-20 py-5 sm:py-7 lg:py-8"
+                <a href="{{ $bundle['slug'] }}" class="w-full @if(!empty($bundle['specialW'])) lg:w-2/3 @elseif(!empty($bundle['specialW2'])) lg:w-1/3 @elseif(!empty($bundle['full'])) @else sm:w-1/2 @endif mx-auto p-1 sm:p-2">
+                    <div class="flex items-center @if(empty($bundle['full']) && empty($bundle['specialW'])) justify-center @else lg:pl-16 xl:pl-20 @endif w-full overflow-hidden relative text-white rounded-xl px-5 lg:px-10 py-5 sm:py-7 lg:py-8"
                         @if(empty($bundle['img'])) style="background:linear-gradient(to bottom, {{ $bundle['bgColor'] }});" @endif >
                         <div class="relative z-10 inline-block w-full sm:w-auto text-center mx-0">
                             <img class="h-16 sm:h-24 sm:h-28" src="{{ $bundle['logo'] }}"><br>
