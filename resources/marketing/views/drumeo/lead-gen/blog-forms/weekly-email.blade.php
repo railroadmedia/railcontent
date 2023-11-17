629c1dd6f6b8233e7f213c84dd07edddb48feb3e
Trent <trent.harms@gmail.com>
2023-11-17 13:06:47 -0800

    test

--- resources/marketing/views/drumeo/lead-gen/blog-forms/weekly-email.blade.php
+++ resources/marketing/views/drumeo/lead-gen/blog-forms/weekly-email.blade.php
@@ -6,10 +6,14 @@
 
 @section('form')
     @include("drumeo.lead-gen.partials.sign-up-form", [
-                    "recaptchaKey" => $recaptchaKey,
-                            "formName" => 'Blog Signup',
+        "recaptchaKey" => $recaptchaKey,
+        "formName" => 'Blog Signup',
         "formId" => "Drumeo - Engagement - Trigger - Blog Signup - WebForm",
         "buttonText" => "Join The Drum Club &raquo;",
         "minimalForm" => true,
     ])
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
+    @include("drumeo.lead-gen.partials.impact-email-sign-up-tracker")
 @endsection
