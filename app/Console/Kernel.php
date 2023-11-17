cff2251ec33aa902f1ddc7115f40a5248d1373d6
Roxana Riza <roxana_r2000@yahoo.com>
2023-11-17 16:25:27 +0200

    New command to seed user progress on workouts - for devs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ use App\Console\Commands\RemoveRailTrackerData;
 use App\Console\Commands\RemoveTemporarySongsAccessForLifetimeMembersJanuary2023;
 use App\Console\Commands\RepairUserProgressOn30DD;
 use App\Console\Commands\RepairUserProgressOnNPPSH;
+use App\Console\Commands\SeedUserProgress;
 use App\Console\Commands\SoftDeleteOldGuitareoSongs;
 use App\Console\Commands\SoftDeleteOldSingeoSongs;
 use App\Console\Commands\PopulateNewRolesAndPermissionsTables;
@@ -75,6 +76,7 @@ class Kernel extends ConsoleKernel
         MigrateOldGuitareoDeletedSongs::class,
         RemoveRailTrackerData::class,
         SyncUsersToCIO::class,
+        SeedUserProgress::class,
     ];
 
     /**
