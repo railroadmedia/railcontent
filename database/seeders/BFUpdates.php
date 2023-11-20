f7b22495682f746e99ee3e44e4f054758540872e
Trent <trent.harms@gmail.com>
2023-11-20 14:59:10 -0800

    fixing prices

--- database/seeders/BFUpdates.php
+++ database/seeders/BFUpdates.php
@@ -25,7 +25,7 @@ class BFUpdates extends Seeder
         Product::where('brand_id', 1)->where('name', 'Beyond Beginner Drumming')->update(['discounted_price' => 50]);
         Product::where('brand_id', 1)->where('name', '30 Day Drummer - Season 3')->update(['discounted_price' => 50]);
         Product::where('brand_id', 1)->where('name', '30 Day Chops')->update(['discounted_price' => 50]);
-        Product::where('brand_id', 1)->where('name', 'Electrify Your Drumming')->update(['discounted_price' => 77]);
+        Product::where('brand_id', 1)->where('name', 'Electrify Your Drumming')->update(['discounted_price' => 79]);
         Product::where('brand_id', 1)->where('name', 'Learn Songs Faster')->update(['discounted_price' => 5]);
         Product::where('brand_id', 1)->where('name', 'Successful Drumming')->update(['discounted_price' => 47]);
         Product::where('brand_id', 1)->where('name', 'New Drummers Start Here')->update(['discounted_price' => 1]);
@@ -39,21 +39,21 @@ class BFUpdates extends Seeder
         Product::where('brand_id', 1)->where('name', 'The Grid')->update(['discounted_price' => 12]);
         Product::where('brand_id', 1)->where('name', 'Beyond The Chops')->update(['discounted_price' => 12]);
 
-        Product::where('brand_id', 1)->where('name', 'Vater Drumeo 5A Drumsticks')->update(['discounted_price' => 9.65]);
-        Product::where('brand_id', 1)->where('name', 'Drumeo QuietPad')->update(['discounted_price' => 29.99]);
+        Product::where('brand_id', 1)->where('name', 'Vater Drumeo 5A Drumsticks')->update(['discounted_price' => 9.71]);
+        Product::where('brand_id', 1)->where('name', 'Drumeo QuietPad')->update(['discounted_price' => 30]);
         Product::where('brand_id', 1)->where('name', 'The P4 Practice Pad')->update(['discounted_price' => 59]);
         Product::where('brand_id', 1)->where('name', 'Drumeo QuietKick')->update(['discounted_price' => 59]);
         Product::where('brand_id', 1)->where('name', 'Drumeo EarDRUM In-Ear Monitors')->update(['discounted_price' => 112]);
         Product::where('brand_id', 1)->where('name', 'Drumeo Comfort Cover')->update(['discounted_price' => 112]);
         Product::where('brand_id', 1)->where('name', 'Drumeo Tone Control Kit')->update(['discounted_price' => 59]);
-        Product::where('brand_id', 1)->where('name', 'The Best Beginner Drum Book')->update(['discounted_price' => 22.50]);
-        Product::where('brand_id', 1)->where('name', 'The Drummer’s Toolbox')->update(['discounted_price' => 22.50]);
+        Product::where('brand_id', 1)->where('name', 'The Best Beginner Drum Book')->update(['discounted_price' => 22.49]);
+        Product::where('brand_id', 1)->where('name', 'The Drummer’s Toolbox')->update(['discounted_price' => 22.49]);
         Product::where('brand_id', 1)->where('name', 'Drumeo Drum Key')->update(['discounted_price' => 11.25]);
         Product::where('brand_id', 1)->where('name', 'Drumeo StickBag')->update(['discounted_price' => 110]);
         Product::where('brand_id', 1)->where('name', 'Drumeo PadStand')->update(['discounted_price' => 59]);
-        Product::where('brand_id', 1)->where('name', 'Easy Rudiments')->update(['discounted_price' => 22.50]);
+        Product::where('brand_id', 1)->where('name', 'Easy Rudiments')->update(['discounted_price' => 22.49]);
 
-        Product::where('brand_id', 1)->where('product_type_id', 4)->update(['discounted_price' => 20]);
+        Product::where('brand_id', 1)->where('product_type_id', 4)->update(['discounted_price' => 21]);
         Product::where('brand_id', 1)->where('product_type_id', 5)->update(['discounted_price' => 39]);
         Product::where('brand_id', 1)->where('product_type_id', 7)->update(['discounted_price' => 39]);
 
@@ -86,8 +86,8 @@ class BFUpdates extends Seeder
         Product::where('brand_id', 2)->where('name', 'The Pianote Metronome')->update(['discounted_price' => 59]);
         Product::where('brand_id', 2)->where('name', 'The Prestige Flamed Maple Metronome')->update(['discounted_price' => 299]);
 
-        Product::where('brand_id', 2)->where('product_type_id', 3)->update(['discounted_price' => 17]);
-        Product::where('brand_id', 2)->where('product_type_id', 4)->update(['discounted_price' => 20]);
+        Product::where('brand_id', 2)->where('product_type_id', 3)->update(['discounted_price' => 17.40]);
+        Product::where('brand_id', 2)->where('product_type_id', 4)->update(['discounted_price' => 21]);
         Product::where('brand_id', 2)->where('product_type_id', 5)->update(['discounted_price' => 39]);
         Product::where('brand_id', 2)->where('product_type_id', 7)->update(['discounted_price' => 39]);
 //        todo socks price
@@ -101,18 +101,18 @@ class BFUpdates extends Seeder
         Product::where('brand_id', 3)->where('name', '500 Songs In 5 Days')->update(['discounted_price' => 97]);
 
         Product::where('brand_id', 3)->where('name', "Chords & Scales Poster")->update(['discounted_price' => 9]);
-        Product::where('brand_id', 3)->where('name', "Guitarist's Survival Kit")->update(['discounted_price' => 67]);
+        Product::where('brand_id', 3)->where('name', "Guitarist's Survival Kit")->update(['discounted_price' => 66.75]);
         Product::where('brand_id', 3)->where('name', "Guitareo Survival Guide")->update(['discounted_price' => 14.25]);
 
         //SINGEO UPDATES
         Product::where('brand_id', 4)->where('name', 'The Singing Starter Kit')->update(['discounted_price' => 50]);
         Product::where('brand_id', 4)->where('name', 'The Essential Guide to Beautiful Harmonies')->update(['discounted_price' => 50]);
 
-        Product::where('brand_id', 4)->where('name', 'Do Re Mi Tumbler')->update(['discounted_price' => 21.70]);
+        Product::where('brand_id', 4)->where('name', 'Do Re Mi Tumbler')->update(['discounted_price' => 21.75]);
         Product::where('brand_id', 4)->where('name', 'Rockstar Mug')->update(['discounted_price' => 9]);
         Product::where('brand_id', 4)->where('name', 'Vowel Practice Poster')->update(['discounted_price' => 9]);
 
-        Product::where('brand_id', 4)->where('product_type_id', 4)->update(['discounted_price' => 20]);
+        Product::where('brand_id', 4)->where('product_type_id', 4)->update(['discounted_price' => 21]);
         Product::where('brand_id', 4)->where('product_type_id', 5)->update(['discounted_price' => 39]);
         Product::where('brand_id', 4)->where('product_type_id', 7)->update(['discounted_price' => 39]);
     }
