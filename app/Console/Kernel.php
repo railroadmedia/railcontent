0778ad75f7f1e929b4cba5fb262b0122ef3be895
calebfavor <calebfavor@gmail.com>
2023-11-22 15:43:06 -0800

    Adding command to sync product stock levels to our system.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ use App\Console\Commands\RepairUserProgressStartedOn;
 use App\Console\Commands\RepairVimeoDurations;
 use App\Console\Commands\SeedLiveAndScheduledContent;
 use App\Console\Commands\SeedUserContentData;
+use App\Console\Commands\SyncShopifyProductInventoryToProductsTable;
 use App\Console\Commands\SyncUsersToCIO;
 use App\Console\Commands\TestLessonsDescriptionUrls;
 use App\Console\Commands\MembershipFieldsSync;
@@ -77,6 +78,7 @@ class Kernel extends ConsoleKernel
         RemoveRailTrackerData::class,
         SyncUsersToCIO::class,
         AssignUnassignedHelpScoutCustomersToMentors::class,
+        SyncShopifyProductInventoryToProductsTable::class,
     ];
 
     /**
