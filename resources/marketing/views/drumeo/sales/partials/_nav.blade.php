44b1971344a5188300dde229f98f7a2cffba1354
Trent <trent.harms@gmail.com>
2023-11-21 19:42:01 -0800

    hiding pricing

--- resources/marketing/views/drumeo/sales/partials/_nav.blade.php
+++ resources/marketing/views/drumeo/sales/partials/_nav.blade.php
@@ -50,7 +50,7 @@
                 <div class="edge-wrap show-for-medium">
                     <span class="cursor-pointer features @if(strpos(url()->full(), 'method') || strpos(url()->full(), 'songs') || strpos(url()->full(), 'coaches')) active @endif">Features <i class="fa-solid fa-caret-down"></i></span>
                     <span class="cursor-pointer instruments">Instruments <i class="fa-solid fa-caret-down"></i></span>
-                    <a class=" @if(strpos(url()->full(), 'choose-plan')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/choose-plan" >Pricing</a>
+{{--                    <a class=" @if(strpos(url()->full(), 'choose-plan')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/choose-plan" >Pricing</a>--}}
                     @if(Carbon\Carbon::create(2023, 11, 27, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
                         <a style="color: #fe006b;" class="@if(strpos(url()->full(), 'drumshop')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/drumshop" ><div class="hidden lg:inline">Black Friday</div> Deals</a>
                     @else
@@ -161,14 +161,11 @@
                 "linkIcon" => ''
             ])
         </div>
-
-
-
-        @include('drumeo.sales.partials._nav-link', [
-            "linkName" => "Pricing",
-            "linkIcon" => "fas fa-money-bill-wave",
-            "linkUrl" => "/choose-plan",
-        ])
+{{--        @include('drumeo.sales.partials._nav-link', [--}}
+{{--            "linkName" => "Pricing",--}}
+{{--            "linkIcon" => "fas fa-money-bill-wave",--}}
+{{--            "linkUrl" => "/choose-plan",--}}
+{{--        ])--}}
         @include('drumeo.sales.partials._nav-link', [
             "linkName" => "Shop",
             "linkIcon" => "fas fa-tag",
--- resources/marketing/views/pianote/sales/partials/_nav.blade.php
+++ resources/marketing/views/pianote/sales/partials/_nav.blade.php
@@ -42,7 +42,7 @@
                 <div class="edge-wrap show-for-medium">
                     <span class="cursor-pointer features @if(strpos(url()->full(), 'method') || strpos(url()->full(), 'songs') || strpos(url()->full(), 'coaches')) active @endif">Features <i class="fa-solid fa-caret-down"></i></span>
                     <span class="cursor-pointer instruments">Instruments <i class="fa-solid fa-caret-down"></i></span>
-                    <a class=" @if(strpos(url()->full(), 'choose-plan')) active @endif" href="{{ get_legacy_brand_base_url('pianote') }}/choose-plan" >Pricing</a>
+{{--                    <a class=" @if(strpos(url()->full(), 'choose-plan')) active @endif" href="{{ get_legacy_brand_base_url('pianote') }}/choose-plan" >Pricing</a>--}}
                     @if(Carbon\Carbon::create(2023, 11, 27, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
                         <a style="color: #fe006b;" class="@if(strpos(url()->full(), 'shop')) active @endif" href="{{ get_legacy_brand_base_url('pianote') }}/shop" ><div class="hidden lg:inline">Black Friday</div> Deals</a>
                     @else
@@ -153,11 +153,11 @@
                 "linkIcon" => ''
             ])
         </div>
-        @include('drumeo.sales.partials._nav-link', [
-            "linkName" => "Pricing",
-            "linkIcon" => "fas fa-money-bill-wave",
-            "linkUrl" => "/choose-plan",
-        ])
+{{--        @include('drumeo.sales.partials._nav-link', [--}}
+{{--            "linkName" => "Pricing",--}}
+{{--            "linkIcon" => "fas fa-money-bill-wave",--}}
+{{--            "linkUrl" => "/choose-plan",--}}
+{{--        ])--}}
         @include('drumeo.sales.partials._nav-link', [
             "linkName" => "Shop",
             "linkIcon" => "fas fa-tag",
