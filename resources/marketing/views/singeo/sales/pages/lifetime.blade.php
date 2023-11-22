4d6691f6b979ddb38f6169cf9bd83d75e962ed60
Trent <trent.harms@gmail.com>
2023-11-22 13:33:43 -0800

    typos

--- resources/marketing/views/singeo/sales/pages/lifetime.blade.php
+++ resources/marketing/views/singeo/sales/pages/lifetime.blade.php
@@ -3,7 +3,7 @@
 @section('global-head')
     <title>Lifetime Membership To Singeo | Singeo</title>
     <meta property="og:title" content="Lifetime Membership To Singeo">
-    <meta property="og:url" content="https://www.pianote.com/{{ Request::path() }}">
+    <meta property="og:url" content="https://www.singeo.com/{{ Request::path() }}">
 
     <meta name="description" content="Two, maybe three times per year you get the chance to become a Singeo Lifetime Member.">
     <meta property="og:description" content="Two, maybe three times per year you get the chance to become a Singeo Lifetime Member.">
@@ -119,7 +119,7 @@
     <section class="text-center px-3 sm:px-5 py-10 md:py-14 lg:py-16 px-2 md:px-4 relative overflow-hidden">
         <div class="container mx-auto max-w-4xl z-10 relative">
             <h2 class="leading-tight mb-2"><strong>The Lifetime Advantage</strong></h2>
-            <p class="leading-tight"><em>Pay once. Play forever. Get unlimited piano lessons for <br class="hidden sm:inline lg:hidden"> the price of 5 years of access to Singeo ($1200 total).</em></p>
+            <p class="leading-tight"><em>Pay once. Play forever. Get unlimited singing lessons for <br class="hidden sm:inline lg:hidden"> the price of 5 years of access to Singeo ($1200 total).</em></p>
             <img class="hidden sm:inline-block w-full max-w-3xl mb-10" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/singeo/promos/november/bundles/timeline.png">
             <img class="sm:hidden inline-block w-full max-w-3xl mb-10" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/filters:quality(95)/marketing/singeo/promos/november/bundles/timeline-m.png">
             <div class="flex flex-wrap sm:flex-nowrap items-start lg:items-center justify-center">
