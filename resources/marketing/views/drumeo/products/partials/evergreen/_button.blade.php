189268c15c509e9ae0476d541ef62f1463786e9f
Trent <trent.harms@gmail.com>
2023-11-23 11:07:18 -0800

    removing new tab

--- resources/marketing/views/drumeo/products/partials/evergreen/_button.blade.php
+++ resources/marketing/views/drumeo/products/partials/evergreen/_button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $link }}" target="_blank" x-data="{ isHovered: false }">
+<a href="{{ $link }}" x-data="{ isHovered: false }">
         <button class="text-xl md:text-2xl bg-{{$brand}} uppercase rounded-full w-full sm:w-full md:w-full lg:w-full xl:w-full h-12 md:h-14 transition duration-300 ease-in-out {{ $buttonClass }}"
             :style="'background-color: ' + (isHovered ? 'lighten($brand, 10%)' : 'bg-{{$brand}}') + '; box-shadow: ' + (isHovered ? '0 0 7px rgba(0, 0, 0, 0.35)' : 'none') + '; filter: brightness(' + (isHovered ? '115%' : '100%') + ')'" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
             @if (!empty($iconClass) && $iconPosition === 'left')
