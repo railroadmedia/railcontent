d2390235e84e68683069c70828b889bc3f1d1fa4
Trent <trent.harms@gmail.com>
2023-11-25 12:32:06 -0800

    fixing typo

--- resources/marketing/views/singeo/shop/shop.blade.php
+++ resources/marketing/views/singeo/shop/shop.blade.php
@@ -95,7 +95,7 @@
         <div id="lessons" class="anchor"></div>
         <section class="grid-view category-section" data-category="lessons" x-show="filter === 'lessons' || filter === 'all'">
             <div class="container">
-                <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-video text-{{ $brand }} mr-1"></i> Piano Lessons</strong></h5>
+                <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-video text-{{ $brand }} mr-1"></i> Singing Lessons</strong></h5>
                 <div class="fixed-cards grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-4 text-left">
 
 {{--                    @include('musora.shop._shop-card-alt', [--}}
