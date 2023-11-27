ee81f78e3591c6ce81146fc862830c4de41e8d32
Trent <trent.harms@gmail.com>
2023-11-27 14:25:32 -0800

    prepping xmas swap

--- resources/marketing/views/drumeo/sales/partials/_nav.blade.php
+++ resources/marketing/views/drumeo/sales/partials/_nav.blade.php
@@ -51,10 +51,10 @@
                     <span class="cursor-pointer features @if(strpos(url()->full(), 'method') || strpos(url()->full(), 'songs') || strpos(url()->full(), 'coaches')) active @endif">Features <i class="fa-solid fa-caret-down"></i></span>
                     <span class="cursor-pointer instruments">Instruments <i class="fa-solid fa-caret-down"></i></span>
 {{--                    <a class=" @if(strpos(url()->full(), 'choose-plan')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/choose-plan" >Pricing</a>--}}
-                    @if(Carbon\Carbon::create(2023, 11, 27, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
-                        <a style="color: #fe006b;" class="@if(strpos(url()->full(), 'drumshop')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/drumshop" ><div class="hidden lg:inline">Black Friday</div> Deals</a>
-                    @else
+                    @if(Carbon\Carbon::create(2023, 11, 28, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
                         <a style="color: #23bcff;" class="@if(strpos(url()->full(), 'drumshop')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/drumshop" ><div class="hidden lg:inline">Cyber Monday</div> Deals</a>
+                    @else
+                        <a style="color: #b30c15;" class="@if(strpos(url()->full(), 'drumshop')) active @endif" href="{{ get_legacy_brand_base_url('drumeo') }}/drumshop" ><div class="hidden lg:inline">Holiday</div> Deals</a>
                     @endif
                     <a class="" href="{{ get_legacy_brand_base_url('drumeo') }}/beat" >Blog</a>
                 </div>
@@ -166,15 +166,16 @@
 {{--            "linkIcon" => "fas fa-money-bill-wave",--}}
 {{--            "linkUrl" => "/choose-plan",--}}
 {{--        ])--}}
-        @if(Carbon\Carbon::create(2023, 11, 27, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
+        @if(Carbon\Carbon::create(2023, 11, 28, 0, 0, 0, 'America/Vancouver') > Carbon\Carbon::now())
             @include('drumeo.sales.partials._nav-link', [
-                "linkName" => "Black Friday Deals",
+                "linkName" => "Cyber Monday Deals",
                 "linkIcon" => "fas fa-tag text-promo",
                 "linkUrl" => "/drumshop",
             ])
         @else
             @include('drumeo.sales.partials._nav-link', [
-                "linkName" => "Cyber Monday Deals",
+                "linkName" => "Holiday
+                 Deals",
                 "linkIcon" => "fas fa-tag text-promo",
                 "linkUrl" => "/drumshop",
             ])
