c431ae0aa2c9e5ca1133eb5e49fe9d11e91980a9
Trent <trent.harms@gmail.com>
2023-11-28 14:34:05 -0800

    prepping 30 day merch

--- resources/marketing/views/drumeo/drumshop/shop.blade.php
+++ resources/marketing/views/drumeo/drumshop/shop.blade.php
@@ -314,6 +314,37 @@
 
 
         {{--   XMAS     --}}
+        <div id="30dd" class="anchor"></div>
+{{--        <section class="grid-view category-section" data-category="shirts" x-show="filter === 'clothing' || filter === 'all'">--}}
+{{--            <div class="container">--}}
+{{--                <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-shirt text-{{ $brand }} mr-1"></i> 30-Day Drummer Merch</strong></h5>--}}
+{{--                <div class="fixed-cards grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-4 text-left">--}}
+{{--                    @foreach($thiryDD as $key => $thiryDDItem)--}}
+{{--                            @include('musora.shop._shop-card-alt', [--}}
+{{--                                "itemURL" => '/drumshop/'.str_replace( array('Drumeo-', 'Pianote-', 'Guitareo-', 'Singeo-'), '', $thiryDDItem->slug ),--}}
+{{--                                "sku" => $thiryDDItem->sku === 'drumeo' ? null : $thiryDDItem->sku,--}}
+{{--                                "badgeText" => $thiryDDItem->badge_text,--}}
+{{--                                "thumbnail" => $thiryDDItem->thumbnail,--}}
+{{--                                "packLogo" => $thiryDDItem->thumbnail_logo,--}}
+{{--                                "title" => $thiryDDItem->name,--}}
+{{--                                "packAuthor" => $thiryDDItem->instructor_name,--}}
+{{--                                "cardDescription" => $thiryDDItem->short_desc,--}}
+{{--                                "fullPrice" => $thiryDDItem->price,--}}
+{{--                                "price" => $thiryDDItem->discounted_price,--}}
+{{--                                "category" => strtolower($thiryDDItem->productType->name),--}}
+{{--                                "buttonText" => $thiryDDItem->sku === 'drumeo' || $thiryDDItem->sku === 'pianote' || $thiryDDItem->sku === 'singeo' || $thiryDDItem->sku === 'guitareo' ? 'see the deal' : null,--}}
+{{--                                "soldOut" => $thiryDDItem->sold_out,--}}
+{{--                                "includedEdge" => $thiryDDItem->included_edge,--}}
+{{--                                "sizes" => $thiryDDItem->sizes,--}}
+{{--                                'FCP' => $key < 4 ? true : null,--}}
+{{--                                "size_case_sensitive" => $thiryDDItem->size_case_sensitive,--}}
+{{--                            ])--}}
+{{--                    @endforeach--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </section>--}}
+
+        {{--   XMAS     --}}
         <div id="xmas" class="anchor"></div>
         <section class="grid-view category-section" data-category="shirts" x-show="filter === 'clothing' || filter === 'all'">
             <div class="container">
