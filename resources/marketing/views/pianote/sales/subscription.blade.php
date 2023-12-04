21ed9c3467fc20d8da587138b634a7ea94fdc931
Trent <trent.harms@gmail.com>
2023-12-04 11:43:14 -0800

    removing where trial explanation shows up

--- resources/marketing/views/pianote/sales/subscription.blade.php
+++ resources/marketing/views/pianote/sales/subscription.blade.php
@@ -376,9 +376,6 @@
         <li class="leading-tight text-coaches max-w-xs mx-0"><i class="fa-li fas fa-check"></i> <strong>PLUS</strong> voice, guitar, and drums lessons with full access to all Musora communities.</li>',
         'image' => 'marketing/pianote/membership/homepage/2023/pianote-collage.png',
         ])
-        @include('musora.sales.components.trial-explanation', [
-            'instrument' => 'piano',
-        ])
 
     @endif
 
