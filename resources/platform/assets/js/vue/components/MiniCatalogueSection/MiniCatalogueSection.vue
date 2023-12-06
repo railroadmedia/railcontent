bbfa7a9883ae584449908cfbf6aa834a0482362c
peterminhlee <petereminhlee@gmail.com>
2023-12-06 15:08:16 -0800

    header/see all text updates

--- resources/platform/assets/js/vue/components/MiniCatalogueSection/MiniCatalogueSection.vue
+++ resources/platform/assets/js/vue/components/MiniCatalogueSection/MiniCatalogueSection.vue
@@ -5,10 +5,10 @@
             <div class="tw-flex tw-items-center tw-mb-4 tw-w-full tw-justify-between tw-px-4 lg:tw-px-0">
                 <a :href="seeAllUrl"
                     class="tw-text-[#00101D] dark:tw-text-white tw-pb-1 tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
-                    <h2 class="tw-font-bold tw-text-2xl tw-leading-none lg:tw-leading-none lg:tw-text-3xl">{{ title }}</h2>
+                    <h2 class="tw-font-bold tw-text-xl tw-leading-none md:tw-leading-none md:tw-text-2xl">{{ title }}</h2>
                 </a>
                 <a :href="seeAllUrl" :aria-label="seeAllAriaLabel"
-                    class="tw-text-base xl:tw-text-lg xl:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
+                    class="tw-text-sm md:tw-text-base md:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
                     See All
                 </a>
             </div>
