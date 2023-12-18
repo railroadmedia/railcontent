ed0d388e5fd6f76402fee4c6c760b15493e7a876
peterminhlee <petereminhlee@gmail.com>
2023-12-18 11:42:32 -0800

    omit text on mobile

--- resources/platform/assets/js/vue/Views/Workouts.vue
+++ resources/platform/assets/js/vue/Views/Workouts.vue
@@ -13,7 +13,7 @@
                     <musora-icon @click="openModal('challenge')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>
                 </div>
                 <a :href="`/${brand}/workouts/challenges`" class="tw-text-sm lg:tw-text-base xl:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
-                    See All Challenges
+                    See All <span class="tw-hidden sm:tw-inline">Challenges</span>
                 </a>
             </div>
             <hr class="tw-border-[#65656b40] dark:tw-border-[#223F57]" />
