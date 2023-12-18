60f1faecb9ed680770546ce2ac33f9b0daff98a6
peterminhlee <petereminhlee@gmail.com>
2023-12-18 14:41:32 -0800

    text size updates

--- resources/platform/assets/js/vue/components/GroupedCard/GroupedCard.vue
+++ resources/platform/assets/js/vue/components/GroupedCard/GroupedCard.vue
@@ -4,7 +4,7 @@
         <a :href="item.web_url_path+'?included_types[]=Workout'" class="tw-flex tw-items-center tw-text-[#00101D] dark:tw-text-white hover:tw-underline" style="text-underline-offset: 6px;">
             <img class="tw-rounded-full tw-w-20 tw-h-20 tw-border-2 tw-border-white tw-border-solid tw-mr-[10px]" :src="`https://www.musora.com/musora-cdn/image/width=200/${item.coach_profile_image}`" :alt="`${parsedData.name} Image`" />
             <div>
-                <h3 class="tw-font-bold">{{ parsedData.name }}</h3>
+                <h3 class="tw-font-bold tw-text-lg md:tw-text-xl">{{ parsedData.name }}</h3>
                 <div class="tw-font-semibold">{{ item.all_lessons_count }} Workouts</div>
             </div>
         </a>
