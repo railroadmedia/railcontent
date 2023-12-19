383ef272383bf6365d14257347e0853671867ff1
Miguel Fondeur <miguelfondeur@gmail.com>
2023-12-19 19:21:04 +0000

    add console.log for beta

--- resources/platform/assets/js/vue/components/Catalogue/CatalogueCard.vue
+++ resources/platform/assets/js/vue/components/Catalogue/CatalogueCard.vue
@@ -424,7 +424,7 @@ const closeDropdownOnScroll = () => {
 };
 
 onMounted(() => {
-    //console.log('item', props.item)
+    console.log('item', props.item)
     const contentContainer = document.getElementById('content-container');
     contentContainer.addEventListener('scroll', closeDropdownOnScroll);
 });
