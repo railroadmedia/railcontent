ff7dc9d194cce23e7b0bb3fe7fa4b42069d2caff
Trent <trent.harms@gmail.com>
2023-12-19 11:03:36 -0800

    removing stock count on poster trial

--- resources/marketing/views/pianote/sales/posters-trial.blade.php
+++ resources/marketing/views/pianote/sales/posters-trial.blade.php
@@ -155,8 +155,8 @@
     <section class="text-center px-5 sm:px-6 py-12 sm:py-14 lg:py-20 text-white relative" style="background: linear-gradient(to bottom, #f41a30, #79080b);">
         <div class="container max-w-5xl mx-auto">
             <h2><strong>Unlimited piano lessons + 2 FREE posters. </strong></h2>
-            <p class="leading-tight text-musora mt-1"><strong>You’ll get free Chords & Scales Posters when you try Pianote for 7 days ($18 value)</strong></p>
-            <h6 class="inline-block mx-auto rounded-md text-black bg-musora py-2 px-4 mt-4 sm:mb-10"><strong>ONLY <s class="opacity-50">2000</s> @if(!empty($products['poster-chords']->getPublicStockCount())) {{ $products['poster-chords']->getPublicStockCount() }} @endif LEFT</strong></h6>
+            <p class="leading-tight text-musora mt-1 sm:mb-10"><strong>You’ll get free Chords & Scales Posters when you try Pianote for 7 days ($18 value)</strong></p>
+{{--            <h6 class="inline-block mx-auto rounded-md text-black bg-musora py-2 px-4 mt-4 sm:mb-10"><strong>ONLY <s class="opacity-50">2000</s> @if(!empty($products['poster-chords']->getPublicStockCount())) {{ $products['poster-chords']->getPublicStockCount() }} @endif LEFT</strong></h6>--}}
             <img class="my-5 h-40 inline sm:hidden"
                     src="https://d21q7xesnoiieh.cloudfront.net/fit-in/620x0/filters:quality(95)/marketing/pianote/promos/september/pianote-trial-sept-promo-collage-m.png"
                     alt="learn playing image"
@@ -334,8 +334,11 @@
                     <br>
                     <h2 class="leading-tight mt-4 sm:mt-5 mb-2"><strong>Try Pianote for 7 days & get<br class="hidden sm:inline"> FREE Chords & Scales posters.</strong></h2>
 
-                    <p class="leading-tight mt-4 sm:mt-5 mb-2"><span class="text-musora">Click below to start your free 7-day trial. Your annual membership will<br class="hidden sm:inline"> continue on {{ Carbon\Carbon::now()->addDays(7)->format('F jS') }} (after your free trial).</span>
-                        <em>Only <s class="opacity-50">2000</s> <strong> @if(!empty($products['poster-chords']->getPublicStockCount())) {{ $products['poster-chords']->getPublicStockCount() }} @endif </strong> posters left!</em></p>
+                    <p class="leading-tight mt-4 sm:mt-5 mb-2">
+                        <span class="text-musora">Click below to start your free 7-day trial. Your annual membership<br class="hidden sm:inline">
+                            will continue on {{ Carbon\Carbon::now()->addDays(7)->format('F jS') }} (after your free trial).</span>
+{{--                        <em>Only <s class="opacity-50">2000</s> <strong> @if(!empty($products['poster-chords']->getPublicStockCount())) {{ $products['poster-chords']->getPublicStockCount() }} @endif </strong> posters left!</em>--}}
+                    </p>
 
                     <h3 class="leading-tight mt-4 sm:mt-5 mb-2"><strong>Free for 7 days</strong></h3>
                     <p class="leading-tight opacity-70 text-sm"><em>Then billed at $240 per year. Save 33%.</em></p>
