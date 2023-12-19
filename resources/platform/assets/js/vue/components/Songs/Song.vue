bcbc01cb2449b12733ec4b890432d7de58ca3116
peterminhlee <petereminhlee@gmail.com>
2023-12-19 11:57:45 -0800

    header updates

--- resources/platform/assets/js/vue/components/Songs/Song.vue
+++ resources/platform/assets/js/vue/components/Songs/Song.vue
@@ -201,7 +201,7 @@ const handleCloseSoundslice = () => {
 
                     <div class="tw-flex flex-column tw-w-full song-details">
                         <div>
-                            <h1 class="text-black font-bold item-title heading dark:tw-text-white">{{ songTitle }}</h1>
+                            <h1 class="text-black font-bold item-title heading dark:tw-text-white tw-text-xl md:tw-text-2xl">{{ songTitle }}</h1>
                             <p class="text-grey-3 tw-text-lg dark:tw-text-[#9EC0DC] tw-text-[#3F3F46] mt-1 tw-mb-3">
                                 {{ songArtist }} -
                                 {{ songAlbum }} -
