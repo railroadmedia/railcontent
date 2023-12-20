936ea6b5e932eba9c2ea4355e5095ed5e382b148
peterminhlee <petereminhlee@gmail.com>
2023-12-20 14:45:13 -0800

    removed console

--- resources/platform/assets/js/vue/components/Catalogue/CollectionResults.vue
+++ resources/platform/assets/js/vue/components/Catalogue/CollectionResults.vue
@@ -53,7 +53,6 @@ import {onMounted, onUnmounted, onUpdated} from "vue";
         const scroll_position = scrollEl.scrollTop + scrollEl.offsetHeight;
         const scroll_buffer = scrollEl.scrollHeight * 0.8;
 
-        console.log(scroll_position >= scroll_buffer, scroll_position, scroll_buffer);
         if (scroll_position >= scroll_buffer && props.currentPage < props.totalPages) {
             emit('onLoadMore');
         }
