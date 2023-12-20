9f89508d7edd976151a180cfd8edcb6b822021bd
Miguel Fondeur <miguelfondeur@gmail.com>
2023-12-20 17:34:06 +0000

    add some bottom margin to continue section

--- resources/platform/assets/js/vue/components/MiniCatalogueSection/MiniCatalogueSection.vue
+++ resources/platform/assets/js/vue/components/MiniCatalogueSection/MiniCatalogueSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="tw-flex tw-flex-row tw-mb-[14px] lg:tw-mb-[6px]">
+    <section class="tw-flex tw-flex-row tw-mb-[14px] lg:tw-mb-4">
         <div class="tw-flex tw-flex-col tw-w-full">
             <!-- Section Title -->
             <div class="tw-flex tw-items-center tw-mb-4 tw-w-full tw-justify-between tw-px-4 lg:tw-px-0">
