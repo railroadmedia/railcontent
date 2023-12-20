92592489638e1b5bbca439a63ff82c7ac656aca8
Miguel Fondeur <miguelfondeur@gmail.com>
2023-12-20 01:47:11 +0000

    fix chapter bug for vimeo

--- resources/platform/assets/js/vue/Views/WorkoutsPlayback.vue
+++ resources/platform/assets/js/vue/Views/WorkoutsPlayback.vue
@@ -407,7 +407,7 @@ const getBrandSpecificParams = () => {
 };
 
 const openSlice = (title, index, startAt, loop) => {
-    mediaElementVueInstance.value.stopVideo(); //Pause Video when soundslice opens
+    if(videoProps.videoType === 'youtube') mediaElementVueInstance.value.stopVideo();
     soundsliceTitle.value = title;
     chapterStartTime.value = startAt;
     chapterEndTime.value = formattedChapters.value.length === index ? props.videoProps.totalDuration : formattedChapters.value[index].time;
