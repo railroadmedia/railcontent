84d25669d2594d8d69b13eb7d7c189d1d7105d3c
peterminhlee <petereminhlee@gmail.com>
2023-12-20 09:53:57 -0800

    pills sticky to top

--- resources/platform/assets/js/vue/components/Filter/FilterPills.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterPills.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul v-if="showPills" class="tw-flex tw-flex-wrap tw-gap-4 tw-text-sm tw-mb-5">
+    <ul v-if="showPills" class="tw-flex tw-flex-wrap tw-gap-4 tw-text-sm tw-mb-2 xl:tw-mb-0 tw-px-4 lg:tw-px-0 tw-sticky tw-top-0 tw-z-30 dark:tw-bg-[#000C17] tw-bg-[#F9F9F9] tw-py-3 xl:tw-py-5">
         <li class="tw-flex tw-items-center tw-p-2 tw-rounded tw-border dark:tw-border-[#223F57] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] dark:tw-text-white tw-font-semibold" v-for="pill in pills">
             {{ pill.item.key }}
             <button class="tw-text-[#000C17] dark:tw-text-white">
