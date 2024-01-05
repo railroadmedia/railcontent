458d63aa3ef77db96702dcf4e42a2e701fbff058
calebfavor <calebfavor@gmail.com>
2024-01-05 16:46:48 +0000

    sort value hook

--- resources/platform/assets/js/vue/vuesora/views/play-alongs/PlayAlongsFilters.vue
+++ resources/platform/assets/js/vue/vuesora/views/play-alongs/PlayAlongsFilters.vue
@@ -233,7 +233,8 @@ export default {
         },
 
         handleContentSort(event) {
-            this.$emit('handleContentSort', event);
+            const value = event?.target?.value;
+            this.$emit('handleContentSort', value);
         },
     },
 };
