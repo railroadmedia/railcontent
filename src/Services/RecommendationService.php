434c1f5e3176ce11b3700601d747713c75701452
adrian-musora <adrian@musora.com>
2024-01-10 12:40:07 -0800

    Add hugging face implementation

--- src/Services/RecommendationService.php
+++ src/Services/RecommendationService.php
@@ -4,25 +4,32 @@ namespace Railroad\Railcontent\Services;
 
 use Exception;
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Facades\Http;
 use PDO;
 use Railroad\Railcontent\Enums\RecommenderSection;
+
+enum AccessMethod {
+    case PDO;
+    case DB;
+    case HUGGINGFACE;
+}
 
 class RecommendationService
 {
 
-    public bool $usePDO;
+    public AccessMethod $accessMethod;
 
     public function __construct(
         private DatabaseManager $databaseManager,
     ) {
-        $this->usePDO = true;
+        $this->accessMethod = AccessMethod::HUGGINGFACE;
         $this->invalidConfigurations = [
             'pianote' => [RecommenderSection::Course],
             'singeo' => [RecommenderSection::Course],
         ];
     }
 
-    public function getRawTable()
+    public function getRawTable() : array
     {
         $results = [];
         $sth = $this->createConnection()->query(
@@ -39,27 +46,25 @@ class RecommendationService
         if ($this->hasNoResults($brand, $section)) {
             return [];
         }
-        if ($this->usePDO) {
-            return  $this->getFilteredRecommendationsUsingPDO($userID, $brand, $section);
-        } else {
-            return  $this->getFilteredRecommendationsUsingDBHandler($userID, $brand, $section);
-        }
+        return match($this->accessMethod)
+        {
+            AccessMethod::PDO => $this->getFilteredRecommendationsUsingPDO($userID, $brand, $section),
+            AccessMethod::DB => $this->getFilteredRecommendationsUsingDBHandler($userID, $brand, $section),
+            AccessMethod::HUGGINGFACE => $this->getFilteredRecommendationsUsingHuggingFace([$userID], $brand, $section),
+        };
     }
 
-    private function hasNoResults($brand, RecommenderSection $section): bool
+    private function hasNoResults($brand, $section): bool
     {
         return in_array($section, $this->invalidConfigurations[$brand]);
     }
 
-    private function getFilteredRecommendationsUsingPDO($userID, $brand, RecommenderSection $section)
+    private function getFilteredRecommendationsUsingPDO($userID, $brand, RecommenderSection $section) : array
     {
         $connection = $this->databaseManager->connection('snowflake_pdo');
+        $query = "CALL RECSYS.RECOMMENDATIONS.GET_FILTERED_RECOMMENDATIONS('$userID', '$brand', '$section->value')";
         try {
-            $result = $connection->select(
-                'CALL RECSYS.RECOMMENDATIONS.GET_FILTERED_RECOMMENDATIONS(?,?,?)',
-                array($userID, $brand, $section->value)
-
-            );
+            $result = $connection->select($query);
             $contentIDs = json_decode($result[0]->GET_FILTERED_RECOMMENDATIONS);
         } catch (Exception $e) {
             error_log($e);
@@ -68,7 +73,24 @@ class RecommendationService
         return $contentIDs;
     }
 
-    private function getFilteredRecommendationsUsingDBHandler($userID, $brand, RecommenderSection $section)
+    public function getBulkFilterRecommendations($userIDs, $brand, RecommenderSection $section) : array
+    {
+
+        $connection = $this->databaseManager->connection('snowflake_pdo');
+        $content = [];
+        $idString = implode(',', $userIDs);
+        $query = "CALL RECSYS.RECOMMENDATIONS.GET_BATCH_FILTERED_RECOMMENDATIONS([$idString], '$brand', '$section->value')";
+        try {
+            $result = $connection->select($query);
+            $content = json_decode($result[0]->GET_BATCH_FILTERED_RECOMMENDATIONS);
+        } catch (Exception $e) {
+            error_log($e);
+        }
+        return $content;
+
+    }
+
+    private function getFilteredRecommendationsUsingDBHandler($userID, $brand, RecommenderSection $section) : array
     {
         $databaseHandler = $this->createConnection();
         $results = [];
@@ -80,7 +102,7 @@ class RecommendationService
         return $results;
     }
 
-    private function createConnection()
+    private function createConnection() : PDO
     {
         $account = env('DB_SNOWFLAKE_ACCOUNT');
         $user = env('DB_SNOWFLAKE_USER_NAME');
@@ -90,12 +112,26 @@ class RecommendationService
         return $dbh;
     }
 
-    private function unpackRecommendationRow($row)
+    private function unpackRecommendationRow($row) : array
     {
         return [ 'userID' => $row[0],
-                 'contentID' => $row[1],
-                 'rank' => $row[2],
+            'contentID' => $row[1],
+            'rank' => $row[2],
+        ];
+    }
 
-                ];
+    private function getFilteredRecommendationsUsingHuggingFace($userIDs, $brand, RecommenderSection $section)
+    {
+        $url = env('HUGGINGFACE_URL');
+        $authToken = env('HUGGINGFACE_TOKEN');
+        $data = [
+            'user_ids' => $userIDs,
+            'brand' => $brand,
+            'section' => $section->value
+        ];
+
+        $response = Http::withToken($authToken)->post($url, $data);
+        $content = $response->json();
+        return $content;
     }
 }
