aa27126c49c09901d3890c12293ded75fdf7fadb
Trent <trent.harms@gmail.com>
2024-01-10 12:04:50 -0800

    fixing date

--- resources/marketing/views/pianote/lead-gen/giveaway.blade.php
+++ resources/marketing/views/pianote/lead-gen/giveaway.blade.php
@@ -158,7 +158,7 @@
                 </div>
             </div>
             <div class="inline-block italic text-pianote py-4 px-6" style="background:#111F29;">
-                The winner will be announced during a LIVE event on January 29th!
+                The winner will be announced during a LIVE event on January 30th!
             </div>
         </div>
     </section>
