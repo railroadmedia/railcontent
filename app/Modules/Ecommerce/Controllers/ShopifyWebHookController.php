472e02927ee40aa8fd544a0b533a3b2c18d9ca77
robwillems <robert@musora.com>
2024-01-11 23:47:14 -0800

    Add one second delay to trigger the job

--- app/Modules/Ecommerce/Controllers/ShopifyWebHookController.php
+++ app/Modules/Ecommerce/Controllers/ShopifyWebHookController.php
@@ -34,7 +34,8 @@ class ShopifyWebHookController extends Controller
             $shopifyCustomerId = $request->get('customer')['id'];
             $email = $request->get('customer')['email'];
             Log::debug("Shopify order updated webhook received:$shopifyCustomerId $email");
-            dispatch(new ShopifySyncCustomerJob($shopifyCustomerId, $email));
+            dispatch(new ShopifySyncCustomerJob($shopifyCustomerId, $email))
+                ->delay(Carbon::now()->addSeconds(1));
         } catch (\Exception $e) { //Catch exception to prevent shopify from retrying the webhook
             Log::error($e->getMessage());
             Log::error($e->getTraceAsString());
