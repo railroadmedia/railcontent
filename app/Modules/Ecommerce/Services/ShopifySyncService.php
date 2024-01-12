3cd2ee8e4bccf9444943f0a4c95b4f8e2456fa0b
robwillems <robert@musora.com>
2024-01-11 23:36:37 -0800

    Improve logging context, changed shopify order update to use a job

--- app/Modules/Ecommerce/Services/ShopifySyncService.php
+++ app/Modules/Ecommerce/Services/ShopifySyncService.php
@@ -77,6 +77,7 @@ class ShopifySyncService
         bool $skipEventSync = false,
         bool $removeDeletedOrderPermissions = false,
     ): void {
+        Log::debug("Shopify syncCustomer: $shopifyCustomerId $email");
         if (!$shopifyCustomerId) {
             $user = $this->userService->getByEmailOrNull($email);
             if ($user) {
