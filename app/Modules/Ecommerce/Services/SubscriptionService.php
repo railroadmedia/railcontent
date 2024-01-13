c849c030a9e785381d8dab0f6ad97f67a51ea537
robwillems <robert@musora.com>
2024-01-13 13:41:10 -0800

    Remove failing debug message

--- app/Modules/Ecommerce/Services/SubscriptionService.php
+++ app/Modules/Ecommerce/Services/SubscriptionService.php
@@ -102,7 +102,6 @@ class SubscriptionService
             // BR-1243: safety check for null nextChargeScheduledAt
             if (is_null($mostRecentActiveSubscription->nextChargeScheduledAt)) {
                 $diff = 0;
-                Log::debug('BR-1243: null nextChargeScheduledAt', $mostRecentActiveSubscription);
             } else {
                 $diff = abs(
                     $mostRecentActiveSubscription->nextChargeScheduledAt->startOfDay()->diffInDays(
