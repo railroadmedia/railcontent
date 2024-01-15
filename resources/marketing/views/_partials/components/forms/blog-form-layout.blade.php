f1e7636d64e584d60d160855ee3923a0926c6cfc
Trent <trent.harms@gmail.com>
2024-01-15 15:22:29 -0800

    removing font weight

--- resources/marketing/views/_partials/components/forms/blog-form-layout.blade.php
+++ resources/marketing/views/_partials/components/forms/blog-form-layout.blade.php
@@ -101,7 +101,7 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com/" />
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&family=Roboto+Condensed:wght@300;400;700&family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&family=Roboto+Condensed:wght@400;700&family=Bebas+Neue&display=swap" rel="stylesheet">
 
     {!! \App\Analytics\Tracker::trackPageView() !!}
 
