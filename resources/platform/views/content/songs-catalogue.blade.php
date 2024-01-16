be3c79154ee7cd105d052624886e11ec3adc1a1f
peterminhlee <petereminhlee@gmail.com>
2024-01-16 08:31:57 -0800

    prop updates

--- resources/platform/views/content/songs-catalogue.blade.php
+++ resources/platform/views/content/songs-catalogue.blade.php
@@ -81,7 +81,7 @@
             <collection-wrapper
                 :pre-loaded-content="{{ $listLessons }}"
                 collection-type="song"
-                :tabs="{{ json_encode($catalogueMeta['tabs'] ?? []) }}"
+                :tab-options="{{ json_encode($catalogueMeta['tabs'] ?? []) }}"
                 :filterable-values="{{ json_encode($catalogueMeta['allowableFilters']) }}"
             ></collection-wrapper>
 {{--            <content-catalogue dusk="content-catalogue" brand="{{ $brand }}" theme-color="{{ $brand }}"--}}
