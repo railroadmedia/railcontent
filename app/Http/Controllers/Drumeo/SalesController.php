646d4145c8a308b8de12037c67a6140c72e19c3c
Trent <trent.harms@gmail.com>
2024-01-19 15:58:49 -0800

    adding stock flipper

--- app/Http/Controllers/Drumeo/SalesController.php
+++ app/Http/Controllers/Drumeo/SalesController.php
@@ -114,7 +114,7 @@ class SalesController extends BaseController
     }
     public function promo()
     {
-        return view('drumeo.sales.subscription', ['theme' => 'drumeo', 'promoVersion' => 'true', 'promoPage' => 'true']);
+        return view('drumeo.sales.subscription', ['theme' => 'drumeo', 'promoVersion' => 'true', 'promoPage' => 'true', 'recaptchaKey'=>config('recaptcha.key')]);
     }
     public function promoEG()
     {
