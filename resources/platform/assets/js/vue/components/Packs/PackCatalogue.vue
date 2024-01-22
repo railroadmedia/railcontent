6981c47672bb84e06cbefeeef4182bdd35e6b98b
peterminhlee <petereminhlee@gmail.com>
2024-01-22 10:28:46 -0800

    vue componentized pack page

---
+++ resources/platform/assets/js/vue/components/Packs/PackCatalogue.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tw-container tw-mt-[30px] tw-mx-auto">
+        <div class="tw-flex tw-flex-col">
+          <PackCatalogueItem v-for="item in content" :pack="item" />
+        </div>
+    </div>
+</template>
+<script setup>
+import PackCatalogueItem from "./PackCatalogueItem";
+
+const props = defineProps({
+    content: {
+        type: Array,
+        default: [],
+    },
+})
+</script>
