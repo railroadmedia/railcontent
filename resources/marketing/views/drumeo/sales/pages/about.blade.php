833a9a0238260fee70631f70a74145ab08316d6a
Trent <trent.harms@gmail.com>
2024-01-25 13:07:06 -0800

    removing more jquery/foundation

--- resources/marketing/views/drumeo/sales/pages/about.blade.php
+++ resources/marketing/views/drumeo/sales/pages/about.blade.php
@@ -426,15 +426,11 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
-    <script type="text/javascript" src="{{ asset('/marketing/js/modal.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('/marketing/js/modal-autoplay.js') }}"></script>
 
     <script async type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
     <script async type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/unveilhooks/ls.unveilhooks.min.js"></script>
     <script>
         $(document).ready(function () {
-            $(document).foundation();
-
             $('.flip-div.teacher').click(function (e) {
                 $(this).toggleClass('flipped');
             });
