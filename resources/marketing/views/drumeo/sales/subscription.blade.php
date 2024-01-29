e8d04e3347d9a8e05b21a6a5f8b6864b4172d385
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2024-01-29 15:13:43 -0800

    Delete lazysizes scripts on homepages

--- resources/marketing/views/drumeo/sales/subscription.blade.php
+++ resources/marketing/views/drumeo/sales/subscription.blade.php
@@ -813,8 +813,5 @@
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/songs-toggler.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
-
-    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/plugins/unveilhooks/ls.unveilhooks.min.js"></script> --}}
     @yield('scripts')
 @stop
