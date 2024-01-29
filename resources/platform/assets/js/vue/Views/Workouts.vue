a5e02c2411aa08d904f4a7440a540f4470675c50
peterminhlee <petereminhlee@gmail.com>
2024-01-29 09:21:57 -0800

    tooltip updates

--- resources/platform/assets/js/vue/Views/Workouts.vue
+++ resources/platform/assets/js/vue/Views/Workouts.vue
@@ -9,7 +9,14 @@
                 <div class="tw-flex tw-items-start">
                     <a :href="`/${brand}/workouts/challenges`" class="tw-text-[#00101D] dark:tw-text-white tw-pb-1 tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current tw-font-bold tw-text-xl md:tw-text-2xl tw-mr-2">Featured Challenges</a>
 <!--                    <musora-icon @click="openVideo('//player.vimeo.com/video/785314424?autoplay=1')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>-->
-                    <musora-icon @click="openModal('challenge')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>
+                    <div class="tw-relative tw-group">
+                        <musora-icon @click="openModal('challenge')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>
+                        <div class="tw-hidden group-hover:lg:tw-block tw-absolute tw-left-full tw-top-0 tw-border tw-border-[#444447] tw-bg-[#232327] tw-text-white tw-p-5 tw-z-50 tw-min-w-[350px] tw-w-full">
+                            <h1 class="tw-text-lg tw-font-extrabold tw-mb-2">What is a Challenge?</h1>
+                            <p>{{ infoText['challenge']['content'] }}</p>
+                        </div>
+                    </div>
+
                 </div>
                 <a :href="`/${brand}/workouts/challenges`" class="tw-text-sm lg:tw-text-base xl:tw-leading-none tw-uppercase tw-leading-none tw-font-bebas-neue tw-text-[#00101D] dark:tw-text-white tw-border-b tw-border-transparent tw-transition-all hover:tw-border-current">
                     See All <span class="tw-hidden sm:tw-inline">Challenges</span>
@@ -28,7 +35,13 @@
                 <div class="tw-flex tw-items-start">
                     <div class="tw-text-[#00101D] dark:tw-text-white tw-font-bold tw-text-xl md:tw-text-2xl tw-mr-2">Workouts</div>
 <!--                    <musora-icon @click="openVideo('//player.vimeo.com/video/785314388?autoplay=1')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>-->
-                    <musora-icon @click="openModal('workout')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>
+                    <div class="tw-relative tw-group">
+                        <musora-icon @click="openModal('workout')" icon-name="info" class="tw-inline-block dark:tw-text-[#80A0B9] tw-w-[27px] tw-h-[27px] tw-cursor-pointer"></musora-icon>
+                        <div class="tw-hidden group-hover:lg:tw-block tw-absolute tw-left-full tw-top-0 tw-border tw-border-[#444447] tw-bg-[#232327] tw-text-white tw-p-5 tw-z-50 tw-min-w-[350px] tw-w-full">
+                            <h1 class="tw-text-lg tw-font-extrabold tw-mb-2">What is a Workout?</h1>
+                            <p>{{ infoText['workout']['content'] }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <hr class="tw-border-[#65656b40] dark:tw-border-[#223F57]" />
