cdc406b412583e972f8e9a8efdb2609fbef4a46f
robwillems <robert@musora.com>
2024-01-30 13:55:46 -0800

    Add new product service functions

--- app/Modules/Ecommerce/Services/ProductService.php
+++ app/Modules/Ecommerce/Services/ProductService.php
@@ -2,20 +2,21 @@
 
 namespace App\Modules\Ecommerce\Services;
 
+use App\Modules\Ecommerce\Models\AccessCode;
 use App\Modules\Ecommerce\Models\Product;
 use Illuminate\Support\Collection;
 
 class ProductService
 {
 
-    public function getProductsByShopifyIds(array $shopifyProductIds) : Collection
+    public function getProductsByShopifyIds(array $shopifyProductIds): Collection
     {
         return Product::query()
             ->whereIn('shopify_id', $shopifyProductIds)
             ->get();
     }
 
-    public function getProductsBySkus(array $skus) : Collection
+    public function getProductsBySkus(array $skus): Collection
     {
         return Product::query()
             ->whereIn('sku', $skus)
@@ -37,5 +38,25 @@ class ProductService
     public function getBySku(string $sku): ?Product
     {
         return Product::query()->where('sku', '=', $sku)->first();
+    }
+
+    public function getByAccessCode(AccessCode $accessCode): Collection
+    {
+        $productIds = unserialize($accessCode->product_ids);
+        return Product::query()
+            ->whereIn('id', $productIds)
+            ->get();
+    }
+
+    public function getAll(): Collection
+    {
+        return Product::all();
+    }
+
+    public function getByBrand(string $brand): Collection
+    {
+        return Product::query()
+            ->where('brand', '=', $brand)
+            ->get();
     }
 }
