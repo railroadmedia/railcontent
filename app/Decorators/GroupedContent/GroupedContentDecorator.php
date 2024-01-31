a67677333e823cfded09ace494097961bca5c478
Roxana Riza <roxana_r2000@yahoo.com>
2024-01-31 17:00:00 +0200

    FSU-137: Provide number of plays on Songs group by artist

--- app/Decorators/GroupedContent/GroupedContentDecorator.php
+++ app/Decorators/GroupedContent/GroupedContentDecorator.php
@@ -3,13 +3,19 @@
 namespace App\Decorators\GroupedContent;
 
 use App\Decorators\Content\ModeDecoratorBase;
-use App\Decorators\Content\UrlDecorator;
-use Railroad\Railcontent\Decorators\Entity\ContentEntityDecorator;
+use App\Maps\PrimaryURLSlugToContentTypeMap;
+use Railroad\Railcontent\Services\UserContentProgressService;
 use Railroad\Railcontent\Support\Collection;
 
 class GroupedContentDecorator extends ModeDecoratorBase
 {
+    protected UserContentProgressService $userContentProgressService;
 
+    public function __construct(
+        UserContentProgressService $userContentProgressService,
+    ) {
+        $this->userContentProgressService = $userContentProgressService;
+    }
     public function decorate(Collection $contents)
     {
         $contentsOfType = $contents->whereIn('type', ['style', 'artist', 'instructor']);
@@ -19,23 +25,28 @@ class GroupedContentDecorator extends ModeDecoratorBase
         }
 
         foreach ($contents as $index => $content) {
+            $lessonType =  array_flip(PrimaryURLSlugToContentTypeMap::$map)[$content['content_type']] ?? '';
             if ($content['type'] == 'artist') {
                 $contents[$index]['url'] = url()->route('platform.content.artist.show', [
                     'brand' => brand(),
-                    'slug' => $content['artist'],
+                    'slug' => urlencode(urlencode($content['artist'])),
                 ]);
+                $contents[$index]['total_plays'] = $this->userContentProgressService->countByArtistTypesUserProgress(
+                    ['song'],
+                    $content['artist']
+                );
             } elseif ($content['type'] == 'style') {
                 $contents[$index]['url'] = url()->route('platform.content.genre.show', [
                     'brand' => brand(),
-                    'genre' => urlencode($content['grouped_by_field']),
-                    'contentTypeName' => $content['content_type'],
+                    'genre' => urlencode(urlencode($content['grouped_by_field'])),
+                    'contentTypeName' => $lessonType,
                 ]);
             } else {
                 $contents[$index]['url'] = url()->route('platform.content.coach.show', [
-                    'brand' => brand(),
-                    'firstContentSlug' => $content['slug'],
-                    'firstContentId' => $content['id'],
-                ]).'?included_types[]='.$content['content_type'];
+                        'brand' => brand(),
+                        'firstContentSlug' => $content['slug'],
+                        'firstContentId' => $content['id'],
+                    ]).'?included_types[]='.$content['content_type'];
             }
         }
 
