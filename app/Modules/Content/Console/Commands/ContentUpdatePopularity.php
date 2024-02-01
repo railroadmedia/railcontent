5add8137d7a5fc664a00d5cbdd0640d29a66e1f8
robwillems <robert@musora.com>
2024-01-31 23:17:05 -0800

    Command clean up

--- app/Modules/Content/Console/Commands/ContentUpdatePopularity.php
+++ app/Modules/Content/Console/Commands/ContentUpdatePopularity.php
@@ -4,16 +4,9 @@ namespace App\Modules\Content\Console\Commands;
 
 use App\Console\Commands\Infrastructure\Command;
 use App\Modules\Content\Models\Content;
-use App\Modules\Content\Models\ContentUserProgress;
 use Carbon\Carbon;
 use DB;
 use Illuminate\Database\DatabaseManager;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Schema;
-use Railroad\Railcontent\Services\ContentService;
-use Railroad\Railcontent\Services\ElasticService;
 
 class ContentUpdatePopularity extends Command
 {
@@ -22,33 +15,16 @@ class ContentUpdatePopularity extends Command
 
     protected $description = 'Updates the content popularity column';
 
-    public function handle(DatabaseManager $databaseManager)
+    public function handle()
     {
-        $this->withExecutionTime(function () use ($databaseManager) {
-            //use startOfDay for consistency when testing
-            $startDate = Carbon::now()->subDays(30)->startOfDay()->toDateTimeString();
-
-            if (!Schema::connection(config('railcontent.database_connection_name'))->hasColumn(
-                config('railcontent.table_prefix') . 'content',
-                'popularity_old'
-            )) {
-                Schema::connection(config('railcontent.database_connection_name'))
-                    ->table(config('railcontent.table_prefix') . 'content', function (Blueprint $table) {
-                        $table->integer('popularity_old')->nullable(true);
-                    });
-            }
-
-            $this->updatePopularity($startDate, $databaseManager);
-
-            Schema::connection(config('railcontent.database_connection_name'))
-                ->table(config('railcontent.table_prefix') . 'content', function (Blueprint $table) {
-                    $table->dropColumn('popularity_old');
-                });
+        $this->withExecutionTime(function () {
+            $this->updatePopularity();
         });
     }
 
-    public function updatePopularity(string $startDate, DatabaseManager $databaseManager): void
+    public function updatePopularity(): void
     {
+        $startDate = Carbon::now()->subDays(30)->startOfDay()->toDateTimeString();
         $max = Content::query()->max('id');
         $chunk = 1000;
         $minId = 1;
