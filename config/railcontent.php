9b9a2ca412ca8bea8fcb8c15d9e7ca315511e293
robwillems <robert@musora.com>
2024-02-06 13:50:30 -0800

    Update cohort permission ids

--- config/railcontent.php
+++ config/railcontent.php
@@ -2030,6 +2030,6 @@ return [
     'playlist_items_limit' => 300,
     'search_in_playlist_items_name' => false,
 
-    'cohort_permisssion_ids' => [81, 65,96,97,98,100,101],
+    'cohort_permisssion_ids' => [81, 65,96,97,98,100,101,102],
     'enable_recsys' => false,
 ];
