ced5015e306155f8c24215a3c5713ce504a860a1
Roxana Riza <roxana_r2000@yahoo.com>
2024-02-06 18:29:23 +0200

    Fix coach-stream URL

--- app/Maps/PrimaryURLSlugToContentTypeMap.php
+++ app/Maps/PrimaryURLSlugToContentTypeMap.php
@@ -52,6 +52,5 @@ class PrimaryURLSlugToContentTypeMap
         'song-tutorials' => 'song-tutorial',
         'drum-fest-international-2022' => 'drum-fest-international-2022',
         'workouts' => 'workout',
-        'coach-stream' => 'coach-stream',
     ];
 }
