baf7242e85bbd15b2afb8886ae7b86cd8991274c
peterminhlee <petereminhlee@gmail.com>
2024-02-06 09:22:46 -0800

    pack components

---
+++ resources/platform/assets/js/vue/components/Packs/PackCatalogue.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tw-container tw-mt-[30px] tw-mx-auto tw-px-4 lg:tw-px-0">
+        <div class="tw-flex tw-flex-col">
+          <PackCatalogueItem v-for="item in content" :pack="item" />
+        </div>
+    </div>
+</template>
+<script setup>
+import { onMounted } from 'vue';
+import PackCatalogueItem from "./PackCatalogueItem";
+
+const props = defineProps({
+    content: {
+        type: Array,
+        default: [],
+    },
+})
+
+onMounted(() => {
+    console.log('content', props.content);
+})
+</script>
