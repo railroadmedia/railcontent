3ed76acb5afc45bb13ac87d6ad13209776c6c09b
Yalung Tang <yalungtang@gmail.com>
2024-02-08 16:53:54 -0300

    home changes

--- resources/platform/assets/js/vue/Views/Home.vue
+++ resources/platform/assets/js/vue/Views/Home.vue
@@ -14,7 +14,7 @@
             title="Continue"
             seeAllAriaLabel="See All Lessons In Progress"
             :seeAllUrl="continueUrl"
-            :preLoadedContent="startedContent.data"
+            :preLoadedContent="startedContent"
             :isMiniView="true"
             :show-dropdown="true"
         />
