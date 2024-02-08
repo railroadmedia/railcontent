e980849c5c3e1f27d4610f0fc12f798ac3baf603
peterminhlee <petereminhlee@gmail.com>
2024-02-08 14:34:37 -0800

    carousel image fix with cdn

--- resources/platform/assets/js/vue/components/HeaderCarousel/CarouselSlide.vue
+++ resources/platform/assets/js/vue/components/HeaderCarousel/CarouselSlide.vue
@@ -141,7 +141,7 @@ const skillLevelColor = computed(() => {
         <!-- Background Image -->
         <div class="tw-absolute tw-inset-0 tw-bg-cover tw-bg-top z-10">
             <picture>
-                <source media="(min-width:1280px)" :srcset="`https://www.musora.com/musora-cdn/image/width=2190,quality=100/${desktopImg}`">
+                <source media="(min-width:1280px)" :srcset="`https://www.musora.com/musora-cdn/image/width=3000,quality=100/${desktopImg}`">
                 <source media="(min-width:768px)" :srcset="`https://www.musora.com/musora-cdn/image/width=1200,quality=100/${tabletImg}`">
                 <img
                     class="tw-w-full tw-h-full tw-object-cover tw-object-top tw-z-50"
