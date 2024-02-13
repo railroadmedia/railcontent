ad8234bdf6e0ac077d1c2bfd7e637c37c63f219a
Roxana Riza <roxana_r2000@yahoo.com>
2024-02-13 15:17:34 +0200

    Display all packs after clear search input on packs index page

--- resources/platform/views/content/packs/packs-index.blade.php
+++ resources/platform/views/content/packs/packs-index.blade.php
@@ -46,6 +46,7 @@
             :filterable-values="{{ json_encode($catalogueMeta['allowableFilters'] ?? []) }}"
             default-sorts="-progress"
             :hide-sort-icon="true"
+            limit = -1
             search-placeholder="Search all packs..."
         ></collection-wrapper>
     </div>
