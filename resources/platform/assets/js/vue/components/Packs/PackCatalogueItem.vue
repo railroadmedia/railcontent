a557f0ed67a9e8effa3eb1c024b2178cc28cc40d
peterminhlee <petereminhlee@gmail.com>
2024-02-14 11:25:14 -0800

    logo style updates

--- resources/platform/assets/js/vue/components/Packs/PackCatalogueItem.vue
+++ resources/platform/assets/js/vue/components/Packs/PackCatalogueItem.vue
@@ -14,7 +14,8 @@
             <!-- Logo -->
             <div :class="`tw-absolute tw-w-full tw-p-[10px] tw-pt-[30px] tw-flex tw-justify-center`" style="background:linear-gradient(to bottom, transparent 0%, #000 100%);" :style="`bottom: ${hasStarted ? '6px' : '0'}`">
                 <img
-                    class="tw-max-h-[70px] sm:tw-max-h-[40px] lg:tw-max-h-[50px] xl:tw-max-h-[70px]"
+                    class="tw-max-h-[70px] sm:tw-max-h-[40px]"
+                    :class="`${logoStyle ? logoStyle : 'lg:tw-max-h-[50px] xl:tw-max-h-[70px]'}`"
                     :src="`https://www.musora.com/musora-cdn/image/width=280,height=280,quality=95/${logo}`"
                     loading="lazy"
                     onload="this.classList.remove('tw-opacity-0')"
@@ -194,4 +195,16 @@ const addToPlaylist = () => {
 
     window.openplaylistmodal({ modalType: 'addItem', content });
 }
+
+const logoStyle = computed(() => {
+    if(title.value === 'Electrify Your Drumming' || title.value === 'Beyond Beginner Drumming' || title.value === 'Anatomy of a Drum Solo' || title.value === 'Creative Control' || title.value === 'Getting Started on the Piano' || title.value === 'Easy Chords' || title.value === 'The Power of Chords' || tile.value === 'The Beginner’s Guide To Classical Piano' || title.value === 'The Beginner’s Guide To PLaying Beautiful Piano' || title.value === 'Beginner Guitar System' || title.value === 'Rhythm & Groove') {
+        return 'lg:tw-max-h-[50px]';
+    }
+
+    if(title.value.includes('Rock Drumming Mastercard') || title.value === 'New Drummers Start Here' || title.value.includes('Drum Technique Made Easy') || title.value.includes('Independence Made Easy') || title.value === 'The Ultimate Guide To Recording Drums' || title.value === 'De-Stupefy Your Left Hand' || title.value === 'Blues Guitar Blueprint' || title.value === 'Guitar Quest' || title.value === 'The Ultimate Guide To Recording Guitar'){
+        return 'lg:tw-max-h-[40px]';
+    }
+
+
+})
 </script>
