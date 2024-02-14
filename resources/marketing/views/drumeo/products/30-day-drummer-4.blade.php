0e14e51b25d6b6e84aad73b6dbd5cb2d21ba60aa
Trent <trent.harms@gmail.com>
2024-02-14 10:27:38 -0800

    prepping 30DD-4

--- resources/marketing/views/drumeo/products/30-day-drummer-4.blade.php
+++ resources/marketing/views/drumeo/products/30-day-drummer-4.blade.php
@@ -827,12 +827,17 @@
 
             <div class="flex flex-wrap items-center mt-7 sm:mt-10">
                 <div class="flex flex-wrap sm:flex-nowrap items-center text-left w-full max-w-3xl mx-auto xl:w-7/12">
-                    <a href="/ecommerce/add-to-cart?products[30-day-drummer-4]=1&locked=true" class="px-5 sm:px-7 lg:px-9 py-7 sm:py-9 mb-7 sm:mb-0 rounded-xl shadow-lg w-full sm:w-1/2" style="background-color:#d4eaff;">
+                    <a href="/ecommerce/add-to-cart?products[30-day-drummer-4]=1&products[Drumeo-VaterSticks]=1&products[drumeo_access_30-days]=1&locked=true" class="px-5 sm:px-7 lg:px-9 py-7 sm:py-9 mb-7 sm:mb-0 rounded-xl shadow-lg w-full sm:w-1/2" style="background-color:#d4eaff;">
                         <h3><strong>30-Day Drummer</strong></h3>
-                        <p class="text-sm mt-2 mb-5">Just The Course (SAVE 61%)</p>
+                        <p class="text-sm mt-2 mb-5">Just The Course + 2 Bonuses worth $42.94.</p>
                         <h2 class="inline-block"><s class="opacity-60">$127</s> <strong class="text-4xl">${{ 50 }}</strong></h2> <p class="inline-block text-xs">one time payment.</p><br>
-                        <div class="join blue smaller mt-4">ENROLL NOW</div>
+                        <div class="join blue smaller my-4">ENROLL NOW</div>
+                        <ul class="list-disc ml-10">
+                            <li class="text-sm relaxed"><span class="text-drumeo">Free</span> Drumsticks</li>
+                            <li class="text-sm relaxed"><span class="text-drumeo">Free</span> 1-Month Drumeo Access</li>
+                        </ul>
                         <hr class="w-full my-5" style="border-color:#b2cae1">
+
                         <p class="leading-loose text-sm"><strong>Key Features</strong><br>
                             <i class="fas fa-check text-drumeo mr-1"></i> Runs February 26 to March 25<br>
                             <i class="fas fa-check text-drumeo mr-1"></i> 24 Guided Workouts<br>
@@ -840,7 +845,7 @@
                             <i class="fas fa-check text-drumeo mr-1"></i> Lifetime Course Access<br>
                             <i class="fas fa-check text-drumeo mr-1"></i> 90-Day Money Back Guarantee</p>
                     </a>
-                    <a href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[30-day-drummer-4]=1&products[quietpad]=1&products[Drumeo-VaterSticks]=1&products[BeginnerBook]=1&locked=true" class="px-5 sm:px-7 lg:px-9 py-7 sm:py-11 sm:-ml-5 z-10 relative rounded-xl bg-white shadow-lg w-full sm:w-1/2">
+                    <a href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[30-day-drummer-4]=1&products[quietpad]=1&products[padstand]=1&products[Drumeo-VaterSticks]=1&products[BeginnerBook]=1&locked=true" class="px-5 sm:px-7 lg:px-9 py-7 sm:py-11 sm:-ml-5 z-10 relative rounded-xl bg-white shadow-lg w-full sm:w-1/2">
                         <h3><strong>Unlimited Lessons</strong></h3>
                         <p class="text-sm mt-2 mb-5">1 Year of Drumeo + 5 Bonuses worth $231.89.</p>
                         <h2 class="inline-block"><strong class="text-4xl">$20</strong>/mo</h2> <p class="inline-block text-xs">Billed annually at $240/yr.</p><br>
@@ -849,6 +854,7 @@
                             <li class="text-sm leading-relaxed">Drumeo Annual Membership</li>
                             <li class="text-sm leading-relaxed"><span class="text-drumeo">Free</span> 30-Day Drummer</li>
                             <li class="text-sm leading-relaxed"><span class="text-drumeo">Free</span> QuietPad Practice Pad</li>
+                            <li class="text-sm leading-relaxed"><span class="text-drumeo">Free</span> PadStand</li>
                             <li class="text-sm leading-relaxed"><span class="text-drumeo">Free</span> 5A Drumsticks</li>
                             <li class="text-sm leading-relaxed"><span class="text-drumeo">Free</span> Best Beginner Drum Book</li>
                         </ul>
