2ae17edc6caa45a13f4acfd2266c098f22e2cfa4
peterminhlee <petereminhlee@gmail.com>
2024-02-16 08:27:29 -0800

    skeleton for song cards

--- resources/platform/assets/js/vue/components/SkeletonLoader/SkeletonSongCard.vue
+++ resources/platform/assets/js/vue/components/SkeletonLoader/SkeletonSongCard.vue
@@ -1,41 +1,49 @@
 <template>
-    <div class="tw-animate-pulse tw-snap-center tw-flex tw-flex-col tw-group" :class="[
-        forceListView || breakToListView ? 'tw-py-3 tw-w-full' : `tw-w-[267px] lg:tw-w-1/4 2xl:tw-w-1/5 tw-mb-4 lg:tw-mb-6 tw-shrink-0 tw-pr-[8px] xl:tw-pr-[12px] 3xl:tw-pr-[18px]`,
-        { 'tw-py-3 tw-w-full @3xl/breakToList:tw-py-0 @3xl/breakToList:tw-w-1/4 @3xl/breakToList:tw-mb-6 @3xl/breakToList:tw-pr-[8px] @4xl/breakToList:tw-pr-[12px] @5xl/breakToList:tw-w-1/5 @6xl/breakToList:tw-pr-[18px]': breakToListView }
-    ]">
-        <div class="tw-flex" :class="[
-            forceListView || breakToListView ? 'tw-flex-row tw-items-center' : 'tw-flex-col',
-            { '@3xl/breakToList:tw-flex-col': breakToListView }
-        ]">
-            <!-- Thumbnail Image -->
-            <div class="tw-flex tw-flex-col tw-items-start tw-p-0 tw-isolate tw-h-[120px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-[8px]"
-                :class="[
-                    forceListView || breakToListView ? 'tw-w-1/3 @3xl/breakToList:tw-w-full' : 'tw-w-full']">
+    <div class="tw-animate-pulse tw-snap-center tw-flex tw-flex-col tw-group tw-shrink-0 sm:tw-pr-[12px]"
+         :class="`sm:tw-w-1/4 lg:tw-w-1/5 xl:tw-w-1/7 tw-mb-4 lg:tw-mb-6 ${isGroupedView ? 'tw-w-[145px] sm:tw-w-[170px] tw-mr-[12px] lg:tw-mr-0' : 'tw-w-full'}`">
+        <div class="tw-flex tw-items-center" :class="`${isGroupedView ? 'tw-flex-col' : 'tw-flex-row sm:tw-flex-col'}`">
+            <!-- Thumbnail Section -->
+            <div class="tw-no-underline tw-flex tw-flex-col tw-aspect-square tw-mr-[5px] sm:tw-mr-0" :class="[
+                { 'tw-w-full': isGroupedView },
+                { 'tw-w-[65px] sm:tw-w-full tw-flex-shrink-0': !isGroupedView },
+            ]">
+                <div :class="`tw-relative tw-overflow-hidden ${isGroupedView ? 'tw-rounded-[9px]' : 'tw-rounded-[5px] sm:tw-rounded-[9px]'} tw-bg-white dark:tw-bg-[#0E2031] tw-aspect-square`"></div>
             </div>
-            <!-- Text Section -->
-            <div class="tw-flex tw-flex-col"
-                :class="[
-                    forceListView || breakToListView ? 'tw-w-2/3 tw-ml-2 @3xl/breakToList:tw-w-full @3xl/breakToList:tw-ml-0' : 'tw-w-full tw-ml-0']">
-                <div class="tw-flex tw-items-start tw-p-0 tw-gap-[12px] tw-w-full tw-h-[16px] tw-my-1">
-                    <div class="tw-w-9/10 tw-w-full tw-h-[16px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-[40px]">
+            <!-- Description Section -->
+            <div class="tw-flex tw-w-full tw-justify-between">
+                <div class="tw-w-full tw-flex tw-flex-wrap lg:tw-block">
+                    <div class="card-info tw-flex-auto tw-flex-col tw-rounded-lg tw-pt-2" :class="isGroupedView ? 'tw-flex' : 'tw-hidden sm:tw-flex'">
+                        <div class="tw-flex tw-flex-col">
+                            <!-- Artist Name -->
+                            <div class="tw-mb-1 tw-w-2/3 tw-h-[17px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full"></div>
+                            <!-- Song Title -->
+                            <div class="tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full tw-w-11/12 tw-h-[18px] tw-mb-1"></div>
+                        </div>
+                        <!-- Difficulty Label -->
+                        <div class="tw-w-2/3 tw-h-[17px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full"></div>
                     </div>
-                    <div class="tw-w-1/10 tw-w-[16px] tw-h-[16px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-[40px]">
+                    <div
+                       class="card-info tw-flex-auto tw-flex-col tw-rounded-lg tw-h-full tw-justify-center"
+                       :class="isGroupedView ? 'tw-hidden' : 'tw-flex sm:tw-hidden'">
+                        <div class="tw-flex tw-flex-col">
+                            <!-- Song Title -->
+                            <div class="tw-mb-1 tw-w-3/4 tw-h-[18px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full"></div>
+                        </div>
+                        <!-- Difficulty Label, Artist Name -->
+                        <div class="tw-w-1/2 tw-h-[18px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full"></div>
                     </div>
                 </div>
-                <div class="tw-w-full tw-h-[16px] tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-[40px]"></div>
+                <!-- Add to Playlist -->
+                <div :class="`tw-bg-[#F2F2F2] dark:tw-bg-[#002039] tw-rounded-full sm:tw-mt-2 ${isGroupedView ? 'tw-h-[14px] tw-w-[14px] lg:tw-h-[32px] lg:tw-w-[32px]' : 'tw-h-[25px] tw-w-[30px]'}`"></div>
             </div>
         </div>
     </div>
 </template>
 <script setup>
 const props = defineProps({
-    forceListView: {
+    isGroupedView: {
         type: Boolean,
-        default: false,
+        default: () => false,
     },
-    breakToListView: {
-        type: Boolean,
-        default: false,
-    }
-})
+});
 </script>
