c7024c9e98320d8d10a485ce81577c66e41f02d9
Ahmed El Shantaly <ahmed.elshantaly@nuvoair.com>
2024-02-21 13:02:56 -0500

    uppercase header info data

--- resources/platform/assets/js/vue/components/PageHeader/PageHeader.vue
+++ resources/platform/assets/js/vue/components/PageHeader/PageHeader.vue
@@ -32,7 +32,7 @@
       <div :class="ctasInsideHero ? 'sm:tw-hidden' : ''" class="tw-flex tw-justify-between tw-items-center tw-w-full">
         <div v-if="isSongs"
           class="tw-flex tw-items-center tw-text-base tw-font-bold tw-text-[#002039] dark:tw-text-[#9EC0DC]">
-          <div class="tw-flex tw-items-center" v-for="(item, index) in formattedInfoData" :key="item">
+          <div class="tw-flex tw-items-center tw-uppercase" v-for="(item, index) in formattedInfoData" :key="item">
             <span>{{ item }}</span>
             <span v-if="index !== formattedInfoData.length - 1"
               class="tw-inline-block tw-h-[7px] tw-w-[7px] tw-mx-2 tw-rounded-full tw-bg-[#002039] dark:tw-bg-[#9EC0DC]"></span>
