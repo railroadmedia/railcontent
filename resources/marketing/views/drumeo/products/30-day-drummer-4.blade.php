0eb8129a0b51d74d3a114f4d8c2f82e44c6ce656
Trent <trent.harms@gmail.com>
2024-02-21 14:50:35 -0800

    page updates

--- resources/marketing/views/drumeo/products/30-day-drummer-4.blade.php
+++ resources/marketing/views/drumeo/products/30-day-drummer-4.blade.php
@@ -441,19 +441,19 @@
                                  [
                                      'season' => '3',
                                      'name' => 'Kenyatta ',
-                                     'comment' => '❤️ This was so absolutely lovely and a super fun drum lesson experience. Domino, thank you so much for putting this together for us with the Drumeo team.',
+                                     'comment' => '<strong>❤️ This was so absolutely lovely and a super fun drum lesson experience</strong>. Domino, thank you so much for putting this together for us with the Drumeo team.',
                                      'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/8021832C-21B0-459E-8CE1-1F315451C8AC-1692660890-593257.jpg',
                                  ],
                                  [
                                      'season' => '3',
                                      'name' => 'Anthony D ',
-                                     'comment' => 'I just wanted to thank you for your work on this program. I found an instrument I really enjoy playing because of you. You really make the workouts fun and motivating to do.',
+                                     'comment' => 'I just wanted to thank you for your work on this program. <strong>I found an instrument I really enjoy playing because of you</strong>. You really make the workouts fun and motivating to do.',
                                      'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/user-profile-picture-1678298286-409099.jpg',
                                  ],
                                  [
                                      'season' => '3',
                                      'name' => 'Doramu ',
-                                     'comment' => 'What a journey! I started this course on a friend\'s electric kit and finished it on my first ever acoustic drum kit. These workouts have been so much fun and the Drumeo community is so wonderfully encouraging.',
+                                     'comment' => 'What a journey! <strong>I started this course on a friend\'s electric kit and finished it on my first ever acoustic drum kit</strong>. These workouts have been so much fun and the Drumeo community is so wonderfully encouraging.',
                                      'img' => 'https://d3fzm1tzeyr5n3.cloudfront.net/profile_picture_url/user-profile-picture-1694598558-612038.jpg',
                                  ],
                              ];
