7022198779d42373bbd88de74cb7812178ccfba2
peterminhlee <petereminhlee@gmail.com>
2024-02-28 12:59:28 -0800

    only alphabetic soting on group view for now

--- resources/platform/assets/js/vue/components/CollectionWrapper/CollectionWrapper.vue
+++ resources/platform/assets/js/vue/components/CollectionWrapper/CollectionWrapper.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <CollectionFilterWrapper
-            :showBackButton="showBackButton" :parentUrl="parentUrl" :active-tab="getActiveTab" :hide-sort-icon="hideSortIcon" :loading="loading" :selected-filters="getSelectedFilters" :selected-progress="filter.progress" :selected-sort="getSelectedSort" :search-term="getSearchTerm" :search-placeholder="searchPlaceholder" :tab-options="tabOptionData" :multi-select-columns="filterColumns" :sort-options="sortOptions"
+            :showBackButton="showBackButton" :parentUrl="parentUrl" :active-tab="getActiveTab" :hide-sort-icon="hideSortIcon" :loading="loading" :selected-filters="getSelectedFilters" :selected-progress="filter.progress" :selected-sort="getSelectedSort" :search-term="getSearchTerm" :search-placeholder="searchPlaceholder" :tab-options="tabOptionData" :multi-select-columns="filterColumns" :sort-options="getSortOptions"
             @on-clear-filter="handleClearFilter" @on-filter-change="handleFilterChange" @on-search-change="handleSearchChange" @on-sort-change="handleSortChange" @on-tab-change="handleTabChange" @on-progress-change="handleProgressChange"
         />
 
@@ -342,6 +342,16 @@ const getTotalPages = computed(() => {
 
 const getActiveTab = computed(() => {
     return Array.isArray(filter.value.activeTab) ? JSON.stringify(filter.value.activeTab) : filter.value.activeTab;
+})
+
+const getSortOptions = computed(() => {
+    return showGroupBy.value ?
+        [
+            { value: 'slug', name: 'Name: A to Z', icon: 'sort-name-asc', },
+            { value: '-slug', name: 'Name: Z to A', icon: 'sort-name-desc', },
+        ]
+        : props.sortOptions
+
 })
 
 const handleClearFilter = () => {
