bc25686320e65c99117d169ecd315fe7a1e00f2c
Roxana Riza <roxana_r2000@yahoo.com>
2024-02-29 11:21:00 +0200

    Add option to preview filters options count by style

--- app/Modules/Content/Console/Commands/Filters20242.php
+++ app/Modules/Content/Console/Commands/Filters20242.php
@@ -7,26 +7,42 @@ use App\Modules\Content\Models\Content;
 use Exception;
 use Railroad\Railcontent\Services\ContentService;
 
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
+use Symfony\Component\Console\Helper\TableSeparator;
+
 class Filters20242 extends Command
 {
-    protected $signature = 'filters:import {brand=drumeo} {startIndex=0} {endIndex=-1}';
+    protected $signature = 'filters:import {brand=drumeo} {--withImport}';
     protected $description = 'Import filters data from CSV file';
 
 
     public function handle(
         ContentService $contentService
     ) {
-        $startIndex = $this->argument('startIndex');
-        $endIndex = $this->argument('endIndex');
+        $startIndex = 0;
+        $endIndex = -1;
         $brand = $this->argument('brand');
+        $import = $this->option('withImport');
+
+        $table = new Table($this->output);
+
+        // Set the table headers.
+        $table->setHeaders([
+            'Content Type', 'Style','Number'
+        ]);
+
+
+        // Render the table to the output.
 
         [$csv, $headersRow] = $this->getCSV($startIndex, $endIndex, $brand);
 
-        $contentIds = [];
+        $results = [];
 
         $this->withProgressBar(
             $csv,
-            function ($row) use ($headersRow, $contentService, &$contentIds, $brand) {
+            function ($row) use ($headersRow, $contentService, &$results, $brand, $import) {
                 $data = $this->getData($row, $headersRow);
 
                 $contentId = $this->getValue($data, $headersRow, 'id');
@@ -35,13 +51,15 @@ class Filters20242 extends Command
 
                 $content = Content::query()
                     ->where('type', '=', $contentType)
-                    ->where('title', '=', $contentTitle)
                     ->where('id', '=', $contentId)
                     ->first();
 
                 if (!$content) {
                     $this->info('Not exists  '.$contentId.'    '.$contentTitle);
                 } else {
+                    $style = $this->getValue($data, $headersRow, 'Genre');
+                    $results[$contentType][$style] = ($results[$contentType][$style] ?? 0) + 1;
+                    if ($import) {
                     if($brand == 'drumeo-Rudiments') {
                         $content->setTopic($this->getValue($data, $headersRow, 'Topic'));
                         $content->setGear($this->getValue($data, $headersRow, 'Gear'));
@@ -56,9 +74,25 @@ class Filters20242 extends Command
                         $content->setLifestyle($this->getValue($data, $headersRow, 'Lifestyle-2'));
                         $content->setStyle($this->getValue($data, $headersRow, 'Genre'));
                     }
+                    }
                 }
             }
         );
+
+        $separator = new TableSeparator();
+        $roows = [];
+        foreach ($results as $key=>$result) {
+            $roows[] = $separator;
+            foreach($result as $key2=>$row) {
+                $roows = array_merge($roows, [
+                    [$key,   $key2,     $row],
+                ]) ;
+            }
+        }
+
+        // Set the contents of the table.
+        $table->setRows($roows);
+        $table->render();
 
         $this->info('Done.');
     }
