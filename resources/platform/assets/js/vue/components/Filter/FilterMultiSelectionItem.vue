7597f0b401d3c2bf2c6e96ac54a5cf1ebc6f2820
peterminhlee <petereminhlee@gmail.com>
2024-02-29 09:28:31 -0800

    value updates on filter column

--- resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
+++ resources/platform/assets/js/vue/components/Filter/FilterMultiSelectionItem.vue
@@ -8,10 +8,10 @@
             <hr class="tw-border-[rgba(101, 101, 107, 0.25)] dark:tw-border-[#223F57] tw-mb-[15px] tw-sticky" />
         </div>
         <div :class="isCollapsed ? 'tw-hidden lg:tw-block' : 'tw-pb-6 lg:tw-pb-0'">
-            <li v-for="item in column.items" class="tw-mb-2 tw-px-4 lg:tw-px-0" :class="isSelected(column.category, item.value) ? 'tw-font-bold' : ''" >
-                <button class="tw-flex tw-justify-between lg:tw-justify-start tw-items-center tw-w-full" @click="$emit('clickColumnItem', `${column.category},${item.value}`)">
+            <li v-for="item in column.items" class="tw-mb-2 tw-px-4 lg:tw-px-0" :class="isSelected(category, item.value) ? 'tw-font-bold' : ''" >
+                <button class="tw-flex tw-justify-between lg:tw-justify-start tw-items-center tw-w-full" @click="$emit('clickColumnItem', `${category},${item.value}`)">
                     <span class="lg:tw-order-1 tw-text-left">{{ item.key }}</span>
-                    <input class="tw-border-[2px] dark:tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer lg:tw-mr-[14px]" type="checkbox" :checked="isSelected(column.category, item.value)" />
+                    <input class="tw-border-[2px] dark:tw-border tw-border-[#D1D5DB] dark:tw-border-[#445F74] tw-rounded dark:tw-bg-[#002039] checked:tw-bg-[#FFAE00] checked:dark:tw-bg-[#FFAE00] tw-cursor-pointer lg:tw-mr-[14px]" type="checkbox" :checked="isSelected(category, item.value)" />
                 </button>
             </li>
         </div>
