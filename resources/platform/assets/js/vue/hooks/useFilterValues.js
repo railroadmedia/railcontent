11cb312700fbd1d563319900e3bf1eb7284859b7
peterminhlee <petereminhlee@gmail.com>
2024-02-29 08:02:50 -0800

    split columns with more than 10 items

--- resources/platform/assets/js/vue/hooks/useFilterValues.js
+++ resources/platform/assets/js/vue/hooks/useFilterValues.js
@@ -1,7 +1,7 @@
 export function useFilterValues() {
     const getFilterValues = (object) => {
         const formattedFilters = {};
-        const numberFormat = /\s\(\d{1,}\)/;
+        const numberFormat = /\s\(\d+\)/;
 
         Object.keys(object).forEach((key) => {
             if(object[key].length > 0){
@@ -19,10 +19,21 @@ export function useFilterValues() {
 
         if(Object.keys(formattedFilters).length > 0){
             for(const key of Object.keys(formattedFilters)){
-                filters.push({
-                    category: key,
-                    items: formattedFilters[key]
-                });
+                if(formattedFilters[key].length > 10){
+                    const quotient = Math.ceil(formattedFilters[key].length/10);
+
+                    for(let i = 0; i < quotient; i++){
+                        filters.push({
+                            category: key + `${i + 1}`,
+                            items: formattedFilters[key].slice(1*i*10, (i+1)*10-1)
+                        });
+                    }
+                } else {
+                    filters.push({
+                        category: key,
+                        items: formattedFilters[key]
+                    });
+                }
             }
         }
 
