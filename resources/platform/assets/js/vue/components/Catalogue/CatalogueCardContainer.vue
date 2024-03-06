07b3007a857ff90d82146df1d9d3da75d6310e71
peterminhlee <petereminhlee@gmail.com>
2024-03-06 14:01:32 -0800

    indentation fix

--- resources/platform/assets/js/vue/components/Catalogue/CatalogueCardContainer.vue
+++ resources/platform/assets/js/vue/components/Catalogue/CatalogueCardContainer.vue
@@ -5,10 +5,10 @@
             <div :class="`
                     tw-px-4 lg:tw-px-0 tw-no-scrollbar
                     ${isMiniView && willScroll ? 'tw-grid tw-auto-rows-min tw-pb-[8px] tw-grid-flow-row tw-auto-cols-min lg:tw-auto-cols-auto tw-grid-cols-4 lg:tw-grid-cols-2 xl:tw-grid-cols-3 2xl:tw-grid-cols-4 4xl:tw-grid-cols-5 lg:tw-w-auto tw-gap-[5px] tw-overflow-x-auto tw-min-w-max lg:tw-min-w-full' : ''}
-                                            ${!isMiniView && willScroll ? 'tw-flex lg:tw-overflow-x-clip tw-flex-nowrap' : ''}
-                                            ${!isMiniView && willScroll && !collectionStoreLoading ? 'tw-overflow-x-scroll' : ''}
-                                            ${!isMiniView && !willScroll ? 'tw-flex tw-flex-wrap' : ''}
-                                            ${breakToListView ? 'tw-@container/breakToList' : ''}
+                    ${!isMiniView && willScroll ? 'tw-flex lg:tw-overflow-x-clip tw-flex-nowrap ' : ''}
+                    ${!isMiniView && willScroll && !collectionStoreLoading ? 'tw-overflow-x-scroll' : ''}
+                    ${!isMiniView && !willScroll ? 'tw-flex tw-flex-wrap' : ''}
+                    ${!isMiniView ? 'lg:tw-grid lg:tw-grid-cols-4 2xl:tw-grid-cols-5 lg:tw-gap-4 2xl:tw-gap-5' : ''}
                 `">
                 <!-- Skeleton Loader -->
                 <template v-if="showSkeletonLoader">
