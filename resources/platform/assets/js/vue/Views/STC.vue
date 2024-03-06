eb5aefc64c6e0a8938ead484986cdc8cd7c021f4
peterminhlee <petereminhlee@gmail.com>
2024-03-06 14:52:47 -0800

    language selection back to default when nothing selected

--- resources/platform/assets/js/vue/Views/STC.vue
+++ resources/platform/assets/js/vue/Views/STC.vue
@@ -84,8 +84,8 @@
                             class="tw-bg-transparent tw-rounded-full tw-absolute tw-w-full tw-h-full tw-min-h-[24px]"
                             :class="selectedLanguages.length > 0 ? 'tw-rounded-lg' : 'tw-rounded-full'"
                             @click="languageContainerClick" @change="(event) => selectLanguage(event.target.value)">
-                            <option value="" class="bg-white text-black">Select</option>
-                            <option v-for="language in languages" class="bg-white text-black" :value="language['value']">{{ language['value'] }}</option>
+                            <option value="" :selected="selectedLanguages.length === 0" class="tw-bg-white tw-text-black">Select</option>
+                            <option v-for="language in languages" class="tw-bg-white tw-text-black" :value="language['value']">{{ language['value'] }}</option>
                         </select>
                         <div class="tw-max-w-[85%] tw-py-2 tw-px-3">
                             <div class="tw-flex tw-flex-wrap tw-gap-2 tw-z-20 tw-items-center tw-min-h-[26px] tw-bg-[#F9F9F9] dark:tw-bg-[#000C18] tw-relative tw-rounded-full">
