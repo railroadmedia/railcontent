62a74a4781af36f36c37a73c91b6b17c8a82ad79
robwillems <robert@musora.com>
2024-03-06 09:13:05 -0800

    Add discount code default to handle failing manual case

--- app/Modules/Ecommerce/Services/EventTrackingService.php
+++ app/Modules/Ecommerce/Services/EventTrackingService.php
@@ -147,12 +147,12 @@ class EventTrackingService
 
         $refundLineItemsAmount = $refundLineItems
             ->pluck('subtotal_set')
-            ->map(fn ($a) => floatval($a['presentment_money']['amount']))
+            ->map(fn($a) => floatval($a['presentment_money']['amount']))
             ->sum();
 
         $orderAdjustmentsAmount = collect($refund['order_adjustments'])
             ->pluck('amount_set')
-            ->map(fn ($a) => floatval($a['presentment_money']['amount']) * -1)
+            ->map(fn($a) => floatval($a['presentment_money']['amount']) * -1)
             ->sum();
 
         $lineItems = $refundLineItems->pluck('line_item')->toArray();
@@ -174,7 +174,8 @@ class EventTrackingService
             ->map(function (array $discount) {
                 return match ($discount['type']) {
                     'automatic' => $discount['title'],
-                    'discount_code' => $discount['code']
+                    'discount_code' => $discount['code'],
+                    default => ''
                 };
             })->toArray();
     }
