503aef45a59c81433406c304b862ea53c9c05efc
peterminhlee <petereminhlee@gmail.com>
2024-03-08 14:18:19 -0800

    move classes to class object

--- resources/platform/assets/js/vue/components/Catalogue/CatalogueCard.vue
+++ resources/platform/assets/js/vue/components/Catalogue/CatalogueCard.vue
@@ -8,7 +8,6 @@
         :class="[
             class_object,
             forceListView || breakToListView ? 'tw-py-3 tw-w-full' : `${isSingleItem ? 'tw-w-full tw-px-2 sm:mb-4' : `${fullWidthOnMobile ? 'tw-w-full md:tw-w-auto' : 'tw-w-[267px] tw-mr-3 lg:tw-mr-0'} lg:tw-w-auto tw-shrink-0 `}`,
-            { 'lg:[&:nth-child(n+5)]:tw-hidden 2xl:[&:nth-child(n+5)]:tw-flex 2xl:[&:nth-child(n+6)]:tw-hidden': isSingleRow }
         ]">
         <div class="tw-flex" :class="[
             forceListView || breakToListView ? 'tw-flex-row tw-items-center' : 'tw-flex-col',
@@ -351,6 +350,7 @@ const class_object = computed(() => ({
     completed: props.item.completed,
     'dark:tw-border-[#223F57] tw-inline': props.forceListView,
     'lg:tw-py-0': props.breakToListView,
+    'lg:[&:nth-child(n+5)]:tw-hidden 2xl:[&:nth-child(n+5)]:tw-flex 2xl:[&:nth-child(n+6)]:tw-hidden': props.isSingleRow,
 }));
 
 const is_added = computed(() => props.item.is_added_to_primary_playlist);
