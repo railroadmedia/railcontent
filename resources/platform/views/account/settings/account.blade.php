4931cab27ad90d2f9dfcc7a9cef69adec7542322
peterminhlee <petereminhlee@gmail.com>
2024-03-11 13:30:56 -0700

    remove iframe container if catches any errors

--- resources/platform/views/account/settings/account.blade.php
+++ resources/platform/views/account/settings/account.blade.php
@@ -38,14 +38,19 @@
             '{{ config('shopify.storefront.access_token') }}',
             '{{ $shopifyCustomerAccessToken }}'
         )
-            .then(session => {
-                console.log('session');
-                console.log(session);
-                recharge.customer.getCustomerPortalAccess(session).then(portal => {
-                    console.log(portal);
-                    document.getElementById('rcPortal').src = portal.portal_url.replace('schedule', 'subscriptions');
-                })
-            }).catch(error => {
+        .then(session => {
+            console.log('session');
+            console.log(session);
+            recharge.customer.getCustomerPortalAccess(session)
+            .then(portal => {
+                console.log(portal);
+                document.getElementById('rcPortal').src = portal.portal_url.replace('schedule', 'subscriptions');
+            })
+            .catch(error => {
+                document.getElementById('rcPortalContainer').remove();
+            })
+
+        }).catch(error => {
             console.log(error);
         });
     </script>
@@ -115,7 +120,7 @@
                 {{-- ================================= Subscription Info Section ================================= --}}
                 {{-- ============================================================================================= --}}
 
-                <div class="tw-flex tw-flex-col pa-3">
+                <div class="tw-flex tw-flex-col pa-3" id="rcPortalContainer">
                     {{-- TEMPORARY MESSAGE --}}
                     {{-- <div class="tw-full tw-flex tw-p-4 tw-mb-4 tw-rounded-lg tw-bg-red-100 tw-text-red-800 tw-font-semibold">
                         <p>
