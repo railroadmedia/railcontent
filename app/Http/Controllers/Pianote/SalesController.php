48067b3f88daf4821e5beb1684e84f64ea2494e1
Alexandre Severo <alexandre@musora.com>
2024-03-11 12:02:32 -0700

    Add scaffolding for Spotify access code generation

--- app/Http/Controllers/Pianote/SalesController.php
+++ app/Http/Controllers/Pianote/SalesController.php
@@ -7,6 +7,7 @@ use App\Modules\Ecommerce\Services\UserAccessPermissionsService;
 use App\Modules\EventDataSynchronizer\Jobs\CustomerIoSendTransactionalEmail;
 use App\Modules\EventDataSynchronizer\Jobs\CustomerIoTriggerEvent;
 use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -336,6 +337,45 @@ class SalesController extends BaseController
                 $request->get('email'),
                 null,
                 'pianote_onboarding_roland-trial',
+                null
+            ))
+                ->onConnection(config('event-data-synchronizer.customer_io_queue_connection_name', 'database'))
+                ->onQueue(config('event-data-synchronizer.customer_io_queue_name', 'customer_io'))
+                ->delay(Carbon::now()->addSeconds(10))
+        );
+
+        return response()->json(['success' => true]);
+    }
+
+    public function claimSpotify(Request $request): JsonResponse
+    {
+        // create access code
+        $accessCode = $this->accessCodeService->generateAccessCode(
+            [491],
+            'musora',
+            'spotify-thinkific-30-day-promo'
+        );
+
+        // create the customer and send the email
+        dispatch(
+            (new CustomerIoSendTransactionalEmail(
+                'musora',
+                2,
+                $request->get('email'),
+                ['access_code' => strtoupper($this->accessCodeService->hyphenateCode($accessCode->code))]
+            ))
+                ->onConnection(config('event-data-synchronizer.customer_io_queue_connection_name', 'database'))
+                ->onQueue(config('event-data-synchronizer.customer_io_queue_name', 'customer_io'))
+                ->delay(Carbon::now()->addSeconds(3))
+        );
+
+        // dispatch the event
+        dispatch(
+            (new CustomerIoTriggerEvent(
+                'musora',
+                $request->get('email'),
+                null,
+                'musora_onboarding_spotify-trial',
                 null
             ))
                 ->onConnection(config('event-data-synchronizer.customer_io_queue_connection_name', 'database'))
