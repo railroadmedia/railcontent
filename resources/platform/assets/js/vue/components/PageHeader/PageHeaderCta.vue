6b1058c5c5172f0ad820628594b1cf723f8ed17e
Ahmed El Shantaly <ahmed.elshantaly@nuvoair.com>
2024-03-13 08:51:19 -0400

    Header base components updates

--- resources/platform/assets/js/vue/components/PageHeader/PageHeaderCta.vue
+++ resources/platform/assets/js/vue/components/PageHeader/PageHeaderCta.vue
@@ -1,22 +1,26 @@
 <template>
     <a v-if="url" :href="url"
-        class="tw-btn-secondary tw-text-[#00101D] dark:tw-text-white ml-1 tw-mb-0 tw-p-0 tw-min-h-0 tw-w-[32px] tw-h-[32px] hover:tw-border-[#000C17] hover:tw-bg-[#000C17] hover:dark:tw-bg-white hover:tw-text-white hover:dark:tw-text-[#000C17]"
-        :class="[text ? 'sm:tw-px-6 sm:tw-w-auto sm:tw-h-auto' : 'md:tw-w-[40px] md:tw-h-[40px]']">
+        class="tw-btn-secondary tw-text-[#00101D] dark:tw-text-white tw-mb-0 tw-min-h-0 hover:tw-border-[#000C17] hover:tw-bg-[#000C17] hover:dark:tw-bg-white hover:tw-text-white hover:dark:tw-text-[#000C17]"
+        :class="buttonConditionalClasses">
         <MusoraIcon v-if="musoraIconName" :icon-name="musoraIconName" class="tw-w-6 tw-h-6" />
         <i v-else-if="faIconClass" class="fas" :class="[faIconClass]"></i>
-        <span v-if="text" :class="{ 'tw-hidden sm:tw-block': hasIcon, 'ml-1': hasIcon }">{{ text }}</span>
+        <span v-if="text" :class="textSpanClass">
+            {{ text }}
+        </span>
     </a>
     <button v-else @click.prevent="emitClick"
-        class="tw-btn-secondary tw-text-[#00101D] dark:tw-text-white ml-1 tw-mb-0 tw-p-0 tw-min-h-0 tw-w-[32px] tw-h-[32px] hover:tw-border-[#000C17] hover:tw-bg-[#000C17] hover:dark:tw-bg-white hover:tw-text-white hover:dark:tw-text-[#000C17]"
-        :class="[text ? 'sm:tw-px-6 sm:tw-py-1 sm:tw-w-auto sm:tw-h-auto' : 'md:tw-w-[40px] md:tw-h-[40px]']">
+        class="tw-btn-secondary tw-text-[#00101D] dark:tw-text-white tw-mb-0 tw-p-0 tw-min-h-0 hover:tw-border-[#000C17] hover:tw-bg-[#000C17] hover:dark:tw-bg-white hover:tw-text-white hover:dark:tw-text-[#000C17]"
+        :class="buttonConditionalClasses">
         <MusoraIcon v-if="musoraIconName" :icon-name="musoraIconName" class="tw-w-6 tw-h-6" />
         <i v-else-if="faIconClass" class="fas" :class="[faIconClass]"></i>
-        <span v-if="text" :class="{ 'tw-hidden sm:tw-block': hasIcon, 'ml-1': hasIcon }">{{ text }}</span>
+        <span v-if="text" :class="textSpanClass">
+            {{ text }}
+        </span>
     </button>
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits, computed } from 'vue';
 import MusoraIcon from '../MusoraIcons/MusoraIcon.vue';
 import ContentService from '../../vuesora/assets/js/services/content';
 
@@ -27,11 +31,53 @@ const props = defineProps({
     url: String,
     contentFunction: String,
     payload: Object,
+    showTextMobileHideDesktop: Boolean,
+    showAllAlways: Boolean,
 });
 
 const hasIcon = () => {
     return props.faIconClass || props.musoraIconName;
 };
+
+const textSpanClass = computed(() => {
+    const classes = [];
+    if (hasIcon) {
+        if (!props.showAllAlways) {
+            if (props.showTextMobileHideDesktop) {
+                classes.push('sm:tw-hidden');
+            }
+            else {
+                classes.push('tw-hidden sm:tw-block');
+            }
+        }
+        classes.push('ml-1');
+    }
+    return classes
+});
+
+const buttonConditionalClasses = computed(() => {
+    const classes = [];
+    if (props.text && hasIcon) {
+        if (!props.showAllAlways) {
+            if (props.showTextMobileHideDesktop) {
+                classes.push('tw-px-6 tw-py-1 tw-w-auto tw-h-auto sm:tw-p-0 sm:tw-w-[32px] sm:tw-h-[32px] md:tw-w-[40px] md:tw-h-[40px]');
+            }
+            else {
+                classes.push('tw-p-0 tw-w-[32px] tw-h-[32px] sm:tw-px-6 sm:tw-py-1 sm:tw-w-auto sm:tw-h-auto md:tw-h-[40px]');
+            }
+        }
+        else {
+            classes.push('tw-px-6 tw-py-1 tw-w-auto tw-h-auto md:tw-h-[40px]');
+        }
+    }
+    else if (hasIcon) {
+        classes.push('tw-p-0 tw-w-[32px] tw-h-[32px] md:tw-w-[40px] md:tw-h-[40px]')
+    }
+    else if (props.text) {
+        classes.push('tw-px-6 tw-py-1 tw-w-auto tw-h-auto')
+    }
+    return classes
+});
 
 const emit = defineEmits(['click']);
 
--- resources/platform/assets/js/vue/components/PageHeader/PageHeaderHero.vue
+++ resources/platform/assets/js/vue/components/PageHeader/PageHeaderHero.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="tw-flex tw-items-center">
     <template v-if="iconName">
-      <musora-icon :icon-name="iconName" class="tw-w-[35px] tw-h-[35px] dark:tw-text-white" />
+      <musora-icon :icon-name="iconName" class="tw-hidden sm:tw-block tw-w-[35px] tw-h-[35px] dark:tw-text-white tw-mr-2" />
     </template>
     <template v-else-if="heroImg">
-      <div class="tw-flex-none tw-w-[80px] sm:tw-w-[150px] sm:tw-max-w-[150px] flex flex-column">
+      <div class="tw-flex-none tw-w-[80px] sm:tw-w-[150px] sm:tw-max-w-[150px] tw-flex tw-flex-col tw-mr-2">
         <div class="square">
           <img class="rounded inset-border" :src="heroImg">
         </div>
       </div>
     </template>
 
-    <div class="tw-flex tw-flex-col pl-2 tw-self-stretch tw-mr-1 tw-w-full">
-      <div class="tw-h-full tw-flex tw-flex-col tw-items-start" :class="{ 'tw-justify-end': !additionalImgSrc }">
+    <div class="tw-flex tw-flex-col tw-self-stretch tw-mr-1 tw-w-full">
+      <div class="tw-h-full tw-flex tw-flex-col tw-items-start" :class="[!hasCtas ? 'tw-justify-center' : !additionalImgSrc ? 'tw-justify-end' : ''
+    ]">
         <div class="tw-flex">
           <template v-if="additionalImgSrc">
             <img :src="additionalImgSrc" class="tw-max-w-[200px] tw-h-[60px] sm:tw-max-w-[460px] sm:tw-h-[86px]">
@@ -33,10 +34,8 @@
                   <XIcon class="tw-w-[26px] tw-h-[26px] md:tw-w-[48px] md:tw-h-[48px]" />
                 </button>
                 <div
-                  class="tw-rounded-lg dark:tw-border dark:tw-border-[#223F57] dark:tw-text-white tw-bg-white dark:tw-bg-[#081825] tw-text-center tw-p-6 sm:tw-p-[30px] tw-max-w-[600px] tw-mx-4 sm:tw-mx-0">
+                  class="dark:tw-text-white tw-text-center tw-p-6 sm:tw-p-[30px] tw-max-w-[600px] tw-mx-4 sm:tw-mx-0 tw-h-full">
                   <slot name="header-info"></slot>
-                  <button @click="closeModal"
-                    class="tw-mt-3 tw-btn-primary tw-border-[#000C17] dark:tw-border-white tw-text-[#000C17] dark:tw-text-white dark:tw-bg-[#00101D] hover:tw-bg-[#00101D] hover:tw-text-white dark:hover:tw-bg-white dark:hover:tw-text-[#00101D]">Close</button>
                 </div>
               </ModalRenderer>
             </div>
@@ -57,7 +56,8 @@
             </div>
           </div>
         </div>
-        <PageHeaderRowInfo v-if="infoData" class="sm:tw-mt-1" :class="{ 'tw-mb-2': $slots['ctas'] }" :infoData="infoData" />
+        <PageHeaderRowInfo v-if="infoData" class="sm:tw-mt-1" :class="{ 'tw-mb-2': $slots['ctas'] }"
+          :infoData="infoData" />
       </div>
       <div class="tw-flex">
         <slot name="ctas"></slot>
@@ -80,6 +80,7 @@ const props = defineProps({
   infoData: Array,
   additionalImgSrc: String,
   secondaryCtaText: String,
+  hasCtas: Boolean,
 })
 
 const isModalOpen = ref(false);
