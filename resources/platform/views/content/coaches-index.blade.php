7c487d8702ac81587078d9812a5cd8f29a25d5e6
Ahmed El Shantaly <ahmed.elshantaly@nuvoair.com>
2024-03-13 08:54:37 -0400

    packs, coaches, shows, and live page headers

--- resources/platform/views/content/coaches-index.blade.php
+++ resources/platform/views/content/coaches-index.blade.php
@@ -1,3 +1,16 @@
+@php
+    $headerDescription = "";
+    if ($brand === "drumeo") {
+        $headerDescription = "Your drumming journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best drummers in the world!";
+    } elseif ($brand === "pianote") {
+        $headerDescription = "Your piano journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best pianists in the world!";
+    } elseif ($brand === "guitareo") {
+        $headerDescription = "Tackle your next guitar goal with bite-sized courses from many of the world's best guitarists.";
+    } elseif ($brand === "singeo") {
+        $headerDescription = "Your singing journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best singers and vocal coaches in the world!";
+    }
+@endphp
+
 @extends('partials.layout')
 
 @section('meta')
@@ -6,32 +19,12 @@
 
 @section('content')
 
-    @component('partials.bladesora.members.components.header-banner', [
-        'hideUser' => true,
-        'backgroundImage' => 'https://d3fzm1tzeyr5n3.cloudfront.net/headers/'.$brand.'-header.jpg',
-        'brand' => '{{ $brand }}'
-    ])
-        @slot('content')
-            <div class="tw-flex tw-flex-col tw-pr-1">
-                <h1 class="heading tw-text-white tw-flex tw-flex-row tw-justify-start tw-mb-2">
-                    <musora-icon icon-name="whistle-filled" class="tw-w-[33px] tw-mr-2 tw-text-{{ $brand }}"></musora-icon>
-                    Coaches
-                </h1>
-
-                <p class="text-white body tw-max-w-[960px]">
-                    @if($brand === "drumeo")
-                        Your drumming journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best drummers in the world!
-                    @elseif($brand === "pianote")
-                        Your piano journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best pianists in the world!
-                    @elseif($brand === "guitareo")
-                        Tackle your next guitar goal with bite-sized courses from many of the world's best guitarists.
-                    @elseif($brand === "singeo")
-                        Your singing journey is unique. You need personalized coaching that helps you reach your goals. Learn from some of the best singers and vocal coaches in the world!
-                    @endif
-                </p>
-            </div>
-        @endslot
-    @endcomponent
+    <page-header
+        title="Coaches"
+        icon-name="whistle-filled"
+        description="{{ $headerDescription }}"
+    >
+    </page-header>
 
     @if( !empty($coachEvent) )
         <div class=" tw-container tw-mx-auto tw-px-4 md:tw-px-8 tw-mt-4">
