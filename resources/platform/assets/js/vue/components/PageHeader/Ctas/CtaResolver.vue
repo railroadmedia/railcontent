91b6df8212ac8e761bda79747b3da58224474421
Ahmed El Shantaly <ahmed.elshantaly@nuvoair.com>
2024-03-13 08:53:00 -0400

    some more adjustments to header base components

---
+++ resources/platform/assets/js/vue/components/PageHeader/Ctas/CtaResolver.vue
@@ -0,0 +1,32 @@
+<template>
+    <component v-for="(cta, index) in ctas" :key="index" :is="resolveComponent(cta.type)" v-bind="cta.props" />
+</template>
+
+<script setup>
+import SongRequest from '../../Songs/SongRequest.vue';
+import ResetProgressCta from './ResetProgressCta.vue';
+import DownloadResourcesCta from './DownloadResourcesCta.vue';
+import PageHeaderCta from '../PageHeaderCta.vue';
+import TimezoneSelectCta from './TimezoneSelectCta.vue'
+import PreviewLessonCta from './PreviewLessonCta.vue';
+import WhereToBeginCta from './WhereToBeginCta.vue';
+
+const componentMap = {
+    SongRequest,
+    ResetProgressCta,
+    DownloadResourcesCta,
+    PageHeaderCta,
+    TimezoneSelectCta,
+    PreviewLessonCta,
+    WhereToBeginCta,
+};
+
+const resolveComponent = (type) => componentMap[type];
+
+const props = defineProps({
+    ctas: {
+        type: Array,
+        default: () => [],
+    },
+});
+</script>
