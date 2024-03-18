414ed7632b3e819079a4b4f31fad711d12f1e70e
Yalung Tang <yalungtang@gmail.com>
2024-03-18 18:12:43 -0300

    removed fivewide sixwide

--- resources/platform/assets/js/vue/vuesora/views/catalogues/ContentCatalogue.vue
+++ resources/platform/assets/js/vue/vuesora/views/catalogues/ContentCatalogue.vue
@@ -64,7 +64,7 @@
     <grid-catalogue v-if="catalogueType === 'grid'" :content="content"
       :brand="brand" :theme-color="themeColor" :use-theme-color="useThemeColor" :user-id="userId" :is-admin="isAdmin"
       :lock-unowned="lockUnowned" :force-wide-thumbs="forceWideThumbs" :content-type-override="contentTypeOverride"
-      :six-wide="sixWide" :five-wide="fiveWide" :show-my-list-action="showMyListAction" :display-inline="displayInline" :is-single-item="isSingleItem" :full-width-on-Mobile="fullWidthOnMobile"
+      :show-my-list-action="showMyListAction" :display-inline="displayInline" :is-single-item="isSingleItem" :full-width-on-Mobile="fullWidthOnMobile"
       @addToList="addToListEventHandler" />
 
     <routines-catalogue v-if="catalogueType === 'routines'" :content="content" :brand="brand" :theme-color="themeColor"
