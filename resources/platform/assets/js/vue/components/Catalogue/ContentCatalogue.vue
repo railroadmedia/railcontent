a062d7b7bff2ba2c362befb87d1b70c54e7dbf3a
Yalung Tang <yalungtang@gmail.com>
2024-03-18 18:13:24 -0300

    removed fivewide sixwide

--- resources/platform/assets/js/vue/components/Catalogue/ContentCatalogue.vue
+++ resources/platform/assets/js/vue/components/Catalogue/ContentCatalogue.vue
@@ -204,10 +204,6 @@ export default {
             type: Boolean,
             default: () => false,
         },
-        fiveWide: {
-            type: Boolean,
-            default: () => false,
-        },
         displayInline: {
             type: Boolean,
             default: () => false,
