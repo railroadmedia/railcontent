d61c53f33448c730f67c0cc53d032dfeba86b522
Brandon Kerr <brandonk@musora.com>
2024-03-19 13:47:22 -0400

    Schedule every other Monday

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -120,7 +120,9 @@ class Kernel extends ConsoleKernel
         $schedule->command('SyncShopifyProductInventoryToProductsTable')->everyFiveMinutes();
 
         $schedule->command('ecommerce:report-cancelled-subscriptions celina@drumeo.com --cc=karissa@musora.com')
-            ->twiceMonthly();
+            ->mondays()->when(function () {
+                return now()->weekOfYear % 2 == 0;
+            })->at('08:01'); // every other Monday at 12:01am PST
     }
 
     /**
