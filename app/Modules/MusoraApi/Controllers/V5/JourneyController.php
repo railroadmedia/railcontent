fa0ce0de73859884beec72639c1e48d6cab2d3df
Alexandre Severo <alexandre@musora.com>
2024-03-20 12:43:16 -0700

    Add logging to JourneyController

--- app/Modules/MusoraApi/Controllers/V5/JourneyController.php
+++ app/Modules/MusoraApi/Controllers/V5/JourneyController.php
@@ -5,6 +5,7 @@ namespace App\Modules\MusoraApi\Controllers\V5;
 use App\Modules\MusoraApi\Services\V5\FiltersJourneyService;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class JourneyController extends Controller
 {
@@ -15,6 +16,8 @@ class JourneyController extends Controller
     public function track(Request $request, string $event): void
     {
         $validated = $request->validate(config('journeys.v5.schema.' . $event));
+
+        Log::info('JourneyController::track() - event: ' . $event . ' - validated: ' . json_encode($validated));
 
         match ($event) {
             'filter-applied' => $this->filtersJourneyService->trackFilterApplied($validated),
