c3cce2c7e692a98a7f1d00e7c960bba23cb9a4a9
Awatanka <natalia.sokolova.ca@gmail.com>
2024-03-26 08:42:33 -0700

    Add banner to homepage

--- resources/marketing/views/drumeo/sales/subscription.blade.php
+++ resources/marketing/views/drumeo/sales/subscription.blade.php
@@ -300,6 +300,17 @@
             }
         }
 
+        @media (max-width: 763px) {
+         .sticky-anniversary {
+        top: 40px;
+            }
+        }
+        @media (min-width: 764px) {
+            .sticky-anniversary {
+                top: 56px;
+            }
+        }
+
     </style>
 @stop
 
@@ -339,6 +350,17 @@
 
     @hasSection('top-bar')
         @yield('top-bar')
+    @endif
+   
+    @if(Request::path() == '/')
+    <a href="/anniversary"
+        class="sticky-anniversary sticky z-[100] flex items-center justify-center transition-none" style="background: #CFDDF9;">
+        <img class="h-8 sm:h-10 mr-3" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/230x0/filters:quality(95)/marketing/drumeo/promos/march/12-anniversary-sticky-logo.webp" alt="Drumeo Anniversary Logo" />
+        <p class="inline-block text-xs mx-0 leading-tight">
+            Celebrate <strong>12 years of Drumeo </strong> with
+            <br>12 FREE bonuses 🥳 <span class="text-drumeo uppercase">CLICK HERE</span>
+        </p>
+    </a>
     @endif
 
     @php
@@ -439,6 +461,7 @@
     @hasSection('promo-banner')
         @yield('promo-banner')
     @endif
+
 
     @php
         $gridItems = $drumeo['gridItems'];
