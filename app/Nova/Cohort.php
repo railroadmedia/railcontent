79cb9e7a8b0b570da7c548aa1231b8f7da7ad81e
peterminhlee <petereminhlee@gmail.com>
2024-03-26 11:52:07 -0700

    made product image required

--- app/Nova/Cohort.php
+++ app/Nova/Cohort.php
@@ -409,7 +409,7 @@ class Cohort extends Resource
                 ->dependsOn(
                     ['is_product'],
                     function (Image $field, NovaRequest $request, FormData $formData) {
-                        if ($formData->is_product) $field->show();
+                        if ($formData->is_product) $field->show()->rules(['required']);
                     }
                 ),
             Text::make('Get Product Badge', 'get_product_badge')->hideFromIndex()
