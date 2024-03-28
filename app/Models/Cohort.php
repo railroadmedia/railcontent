6f58ef4f77169bcfa3bf41ac9d907a0942ecbbcd
peterminhlee <petereminhlee@gmail.com>
2024-03-28 13:38:02 -0700

    product image save updates

--- app/Models/Cohort.php
+++ app/Models/Cohort.php
@@ -127,6 +127,10 @@ class Cohort extends Model
             if (gettype($model['demo_mobile_center_image_url']) === 'object') {
                 $model['demo_mobile_center_image_url'] = 'https://d1fyshwdvi6fth.cloudfront.net/' . $model->brand->name . '/cohorts/' . $uuid . '-' . str_replace(' ', '+', $model['demo_mobile_center_image_url']->getClientOriginalName());
             }
+
+            if (gettype($model['product_image']) === 'object') {
+                $model['demo_mobile_center_image_url'] = 'https://d1fyshwdvi6fth.cloudfront.net/' . $model->brand->name . '/cohorts/' . $uuid . '-' . str_replace(' ', '+', $model['demo_mobile_center_image_url']->getClientOriginalName());
+            }
         });
     }
 
