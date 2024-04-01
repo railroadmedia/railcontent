ad2f0816f8fafe429044fc340b3caeb2c0f93aa2
Roxana Riza <roxana_r2000@yahoo.com>
2024-04-01 16:56:25 +0300

    Revert verification if user has purchased the product already

--- app/Http/Controllers/Platform/CohortPackController.php
+++ app/Http/Controllers/Platform/CohortPackController.php
@@ -6,7 +6,6 @@ use App\Modules\Content\Services\CohortService;
 use App\Modules\Content\Services\ContentPermissionsService;
 use App\Modules\Ecommerce\Enums\UserAccessPermissionsSourceEnum;
 use App\Modules\Ecommerce\Services\ProductService;
-use App\Modules\Ecommerce\Services\ShopifySyncService;
 use App\Modules\Ecommerce\Services\UserAccessPermissionsService;
 use App\Modules\Ecommerce\Services\UserProductService;
 use Carbon\Carbon;
@@ -29,20 +28,17 @@ class CohortPackController
 
     private UserAccessPermissionsService $userAccessPermissionsService;
     private ProductService $productService;
-    private ShopifySyncService $shopifySyncService;
 
     public function __construct(
         CohortService $cohortService,
         ContentService $contentService,
         UserAccessPermissionsService $userAccessPermissionsService,
-        ProductService $productService,
-        ShopifySyncService $shopifySyncService
+        ProductService $productService
     ) {
         $this->cohortService = $cohortService;
         $this->contentService = $contentService;
         $this->userAccessPermissionsService = $userAccessPermissionsService;
         $this->productService = $productService;
-        $this->shopifySyncService = $shopifySyncService;
     }
 
     /**
@@ -96,15 +92,6 @@ class CohortPackController
         }
         $cohort->lists = $lists;
 
-
-        if($cohort['is_product']) {
-            $shopifyCustomerId = $this->shopifySyncService->getShopifyCustomer(\user()->email)['id'];
-            $sku = 'singeo-the-foiled-water-bottle';
-            $ownedProducts = $this->shopifySyncService->getOwnedProducts($shopifyCustomerId);
-            $hasPurchasedTheProduct =
-                ($ownedProducts->where('sku', '=', $sku)
-                    ->where('order.processedAt', '>', $cohort['enrollment_start_date']));
-        }
         return view('content.cohort-template', [
             'hasProduct' => $hasProduct,
             'nPackOwners' => $nPackOwners,
@@ -113,8 +100,7 @@ class CohortPackController
             'cohort' => $cohort,
             'enrollmentClosed' => $enrollmentClosed,
             'homeUrl' => url()->route('platform.home', ['brand' => brand()]),
-            'purchased' => $purchased,
-            'hasPurchasedTheProduct' => $hasPurchasedTheProduct
+            'purchased' => $purchased
         ]);
     }
 
