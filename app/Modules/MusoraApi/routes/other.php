d460502aabac7f72b4f60a965f0a637ec4b24af6
Alexandre Severo <alexandre@musora.com>
2024-04-03 15:36:27 -0700

    Fix route paths

--- app/Modules/MusoraApi/routes/other.php
+++ app/Modules/MusoraApi/routes/other.php
@@ -6,13 +6,15 @@ Route::as('musora-api.')
     ->prefix('/musora-api')
     ->middleware('web_or_api_authenticated')
     ->group(function () {
-        Route::match(['post', 'put'], 'picture/upload', [PictureUploadController::class, 'uploadPicture'])
+        Route::match(['post', 'put'], '/v5/picture/upload', [PictureUploadController::class, 'uploadPicture'])
             ->middleware('api_version:v5')
             ->name('v5.picture.upload');
-        Route::match(['post', 'put'], 'picture/upload-from-s3', [PictureUploadController::class, 'uploadPictureFromS3'])
+        Route::match(['post', 'put'],
+            '/v5/picture/upload-from-s3',
+            [PictureUploadController::class, 'uploadPictureFromS3'])
             ->middleware('api_version:v5')
             ->name('v5.picture.upload-from-s3');
-        Route::delete('picture/delete', [PictureUploadController::class, 'deletePictureFromS3'])
+        Route::delete('/v5/picture/delete', [PictureUploadController::class, 'deletePictureFromS3'])
             ->middleware('api_version:v5')
             ->name('v5.picture.delete');
     });
