0b3ed605e10ba80c16b1450e59a49b9a4121b19c
Alexandre Severo <alexandre@musora.com>
2024-04-03 15:34:56 -0700

    API routes to upload and delete pictures to S3

---
+++ app/Modules/MusoraApi/routes/other.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Modules\MusoraApi\Controllers\V5\PictureUploadController;
+
+Route::as('musora-api.')
+    ->prefix('/musora-api')
+    ->middleware('web_or_api_authenticated')
+    ->group(function () {
+        Route::match(['post', 'put'], 'picture/upload', [PictureUploadController::class, 'uploadPicture'])
+            ->middleware('api_version:v5')
+            ->name('v5.picture.upload');
+        Route::match(['post', 'put'], 'picture/upload-from-s3', [PictureUploadController::class, 'uploadPictureFromS3'])
+            ->middleware('api_version:v5')
+            ->name('v5.picture.upload-from-s3');
+        Route::delete('picture/delete', [PictureUploadController::class, 'deletePictureFromS3'])
+            ->middleware('api_version:v5')
+            ->name('v5.picture.delete');
+    });
+
