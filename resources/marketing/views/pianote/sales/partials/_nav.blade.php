b99eda98d5fb832d06591f1aa5531acf04bcf26b
Trent <trent.harms@gmail.com>
2024-04-05 14:09:13 -0700

    pointing FAQ link to musora

--- resources/marketing/views/pianote/sales/partials/_nav.blade.php
+++ resources/marketing/views/pianote/sales/partials/_nav.blade.php
@@ -240,7 +240,7 @@
         ])
         @include('drumeo.sales.partials._secondary-nav-link', [
             "linkName" => "<i class='fas fa-fw fa-question'></i>&nbsp; FAQs",
-            "linkUrl" => "https://help.pianote.com/",
+            "linkUrl" => "https://help.musora.com/",
             "externalLink" => false
         ])
         @include('drumeo.sales.partials._secondary-nav-link', [
