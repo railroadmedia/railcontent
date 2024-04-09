a049da7bfe6255748def775d228b5e3a3c68e51d
peterminhlee <petereminhlee@gmail.com>
2024-04-09 15:12:17 -0700

    added delete account modal

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -45,7 +45,8 @@ import ContentInfo from './vue/components/ContentInfo/ContentInfo';
 import CollectionFilterWrapper from './vue/components/Filter/CollectionFilterWrapper';
 import CollectionWrapper from './vue/components/CollectionWrapper/CollectionWrapper';
 import ChildCollectionPage from './vue/components/CollectionPage/ChildCollectionPage';
-import PageHeader from './vue/components/PageHeader/PageHeader'
+import PageHeader from './vue/components/PageHeader/PageHeader';
+import DeleteAccountModal from './vue/components/Modal/DeleteAccountModal';
 
 //Vuesora Assets
 import Forms from './vue/vuesora/assets/js/classes/forms';
@@ -292,6 +293,7 @@ app.component('AppContainer', AppContainer)
     .component('Playlist', Playlist)
     .component('Referral', Referral)
     .component('Stc', Stc)
+    .component('DeleteAccountModal', DeleteAccountModal)
     // .component('PackCatalogue', PackCatalogue)
     .component('LessonHistory', LessonHistory)
 
