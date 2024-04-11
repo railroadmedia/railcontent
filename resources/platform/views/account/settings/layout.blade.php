7a578455722c64d7fd0001f36ed3e774b46b26c6
peterminhlee <petereminhlee@gmail.com>
2024-04-11 11:32:01 -0700

    made tabs scrollable on mobile

--- resources/platform/views/account/settings/layout.blade.php
+++ resources/platform/views/account/settings/layout.blade.php
@@ -148,7 +148,7 @@
     <div class="tw-w-full tw-max-w-[1703px] tw-mx-auto tw-px-4 md:tw-px-8 tw-pt-8 tw-pb-14">
         <div class="tw-flex tw-flex-row">
             <div class="tw-flex tw-flex-col grow">
-                <div class="tw-flex tw-flex-row tw-gap-[4px] md:tw-gap-[10px] tw-px-[30px]">
+                <div class="tw-flex tw-overflow-x-auto tw-no-scrollbar tw-gap-[4px] md:tw-gap-[10px] tw-px-[30px]">
                     @foreach($sections as $index => $section)
                         <a href="{{ $section['url'] }}" class="tw-btn-primary tw-flex tw-items-center tw-justify-center tw-text-center tw-border tw-text-[#000C17] dark:tw-text-white dark:tw-border-[#445F74] tw-px-4 lg:tw-px-6 tw-mb-0 tw-leading-[1px]
                             {{ $section['active'] ? 'tw-bg-[#28282D] dark:tw-bg-[#445F74] tw-text-white' : 'hover:tw-bg-[#E7E7E8] hover:dark:tw-bg-[#223F57] hover:dark:tw-text-white tw-bg-white dark:tw-bg-[#000C17] tw-border-[#CBCBCD] dark:tw-text-white' }}">
