58460a0370d64ede62aff196b55e6ec55ac95ab0
Robert Willems <108689774+robwillems@users.noreply.github.com>
2024-04-18 09:53:17 -0700

    Update app/Modules/Ecommerce/ApiGateways/ShopifyGateway.php
    Co-authored-by: Brandon <brandonk@musora.com>

--- app/Modules/Ecommerce/ApiGateways/ShopifyGateway.php
+++ app/Modules/Ecommerce/ApiGateways/ShopifyGateway.php
@@ -156,6 +156,7 @@ class ShopifyGateway
      *
      * @param Carbon $startDate
      * @param Carbon $endDate
+     * @param  int  $limit  The page limit for this query. Must be 1-250.
      * @param string $additionalFilter Additional query filter, e.g. " AND status:closed".
      *                       See https://shopify.dev/docs/api/usage/search-syntax
      * @param string $additionalFields Additional fields to include in result, e.g. ", processedAt".
