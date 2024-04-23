2e03c975408113261da71fd3470f97c84b71acc7
Adrian <150092463+adrian-musora@users.noreply.github.com>
2024-04-23 12:57:32 -0700

    BE-60 - FeatureFlag - add userid artisan commands, add dockblocks (#901)
    Update FeatureFlag artisan commands to include documentation. 
    Add commands to add or remove users from branches/features.

--- app/Modules/FeatureFlagging/tests/Feature/Services/FeatureFlagCRUDTest.php
+++ app/Modules/FeatureFlagging/tests/Feature/Services/FeatureFlagCRUDTest.php
@@ -2,8 +2,13 @@
 
 namespace App\Modules\FeatureFlagging\tests\Feature\Services;
 
+use App\Modules\FeatureFlagging\Models\Branch;
+use App\Modules\FeatureFlagging\Models\Experiment;
+use App\Modules\FeatureFlagging\Models\Feature;
 use App\Modules\FeatureFlagging\Services\FeatureFlagService;
 use App\Modules\FeatureFlagging\Facades\FeatureFlagging;
+use Google\Exception;
+use Illuminate\Support\Facades\Bus;
 use Modules\UserManagementSystem\Models\User;
 use Tests\TestCase;
 
@@ -17,18 +22,22 @@ class FeatureFlagCRUDTest extends TestCase
     {
         parent::setUp();
         $this->ffService= app(FeatureFlagService::class);
+
     }
 
     public function test_crud_feature()
     {
-        $feature = $this->ffService->addFeature(
-            $this->faker->name,
-        );
+        $name = $this->faker->unique()->name;
+        $this->artisan("featureFlag:addFeature '$name'")
+            ->assertSuccessful();
+        $feature = Feature::whereName($name)->first();
         $this->assertDatabaseHas('features_features',
-                                [ 'id' => $feature->id,
-                                    'name' => $feature->name
+                                [
+                                    'id' => $feature->id,
+                                    'name' => $name
                                 ]);
         $newDescription = $this->faker->sentence(3);
+
         $this->ffService->editFeature($feature->id, ['description' => $newDescription]);
         $feature->refresh();
         $this->assertEquals($feature->description, $newDescription);
@@ -83,7 +92,6 @@ class FeatureFlagCRUDTest extends TestCase
             priority: 1,
             allow_filter: 'admin',
             weight: 10);
-
         // ----------- CREATE ----------------- //
         $this->assertDatabaseHas('features_branches', [
             'id' => $branch1->id,
@@ -165,6 +173,7 @@ class FeatureFlagCRUDTest extends TestCase
                 'default_value' => $experiment->default_value,
                 'enabled' => true]);
         // ----------- EDIT ----------------- //
+
         $this->ffService->setExperimentEnabled($experiment->id, false);
         $experiment->refresh();
         $this->assertDatabaseHas('features_experiments', [
@@ -172,7 +181,8 @@ class FeatureFlagCRUDTest extends TestCase
             'name' => $experiment->name,
             'default_value' => $experiment->default_value,
             'enabled' => false]);
-        $this->ffService->setExperimentEnabled($experiment->id, true);
+        $name = $experiment->name;
+        $this->artisan("featureFlag:toggleExperiment '$name' 1")->assertSuccessful();
         $experiment->refresh();
         // ----------- DELETE WITH CHILDREN ----------------- //
         $branch1 = $this->ffService->addBranch(
@@ -186,6 +196,7 @@ class FeatureFlagCRUDTest extends TestCase
             'content' => $branch1->content,
             'experiment_id' => $branch1->experiment_id]);
         $user = User::factory()->create();
+        Bus::fake();
         $content = FeatureFlagging::branch($experiment->name, $user);
         $this->assertEquals($content, $branch1->content);
         $this->assertDatabaseHas('features_tracking', [
@@ -207,4 +218,50 @@ class FeatureFlagCRUDTest extends TestCase
             'experiment_id' => $experiment->id,
             'branch_id' => $branch1->id]);
     }
+
+    public function test_add_remove_users_from_branch()
+    {
+        $experiment = $this->ffService->addExperiment(
+            $this->faker->name,
+            default_value: $this->faker->words(2, true));
+        $branch1 = $this->ffService->addBranch(
+            $this->faker->name,
+            $this->faker->words(2, true),
+            $experiment->id,
+            weight: 1);
+        $name = $branch1->name;
+        $this->artisan("featureFlag:addRemoveUsersToBranch '$name' 1,2,3 --add")
+            ->assertSuccessful();
+        $branch1->refresh();
+        $this->assertEquals($branch1->userid_list, '1,2,3');
+        $this->artisan("featureFlag:addRemoveUsersToBranch '$name' 4,5 --add")
+            ->assertSuccessful();
+        $branch1->refresh();
+        $this->assertEquals($branch1->userid_list, '1,2,3,4,5');
+        $this->artisan("featureFlag:addRemoveUsersToBranch '$name' 3,4 --remove")
+            ->assertSuccessful();
+        $branch1->refresh();
+        $this->assertEquals($branch1->userid_list, '1,2,5');
+    }
+
+    public function test_add_remove_users_from_feature()
+    {
+        $name = $this->faker->unique()->name;
+        $this->artisan("featureFlag:addFeature '$name'")
+            ->assertSuccessful();
+        $feature = Feature::whereName($name)->first();
+        $this->artisan("featureFlag:addRemoveUsersToFeature '$name' 1,2,3 --add")
+            ->assertSuccessful();
+        $feature->refresh();
+        $this->assertEquals($feature->userid_list, '1,2,3');
+        $this->artisan("featureFlag:addRemoveUsersToFeature '$name' 4,5 --add")
+            ->assertSuccessful();
+        $feature->refresh();
+        $this->assertEquals($feature->userid_list, '1,2,3,4,5');
+        $this->artisan("featureFlag:addRemoveUsersToFeature '$name' 3,4 --remove")
+            ->assertSuccessful();
+        $feature->refresh();
+        $this->assertEquals($feature->userid_list, '1,2,5');
+    }
+
 }
