8a775752e4edd64a77670d6e337d251c6b2db541
robwillems <robert@musora.com>
2024-04-24 14:49:14 -0700

    Add limit option to control batch size

--- app/Modules/Ecommerce/Console/Commands/FixMobileTransactions.php
+++ app/Modules/Ecommerce/Console/Commands/FixMobileTransactions.php
@@ -11,14 +11,16 @@ class FixMobileTransactions extends Command
 {
     protected $signature = 'ecommerce:fixMobileTransactions
                             {startDate : The ISO 8601 date time for all Shopify orders to get where the created_at at or after. e.g. 2023-10-13T17:00:25+00:00}
-                            {endDate : The ISO 8601 date time for all Shopify orders to get where the created_at at or before. e.g. 2023-10-13T17:30:14+00:00}';
+                            {endDate : The ISO 8601 date time for all Shopify orders to get where the created_at at or before. e.g. 2023-10-13T17:30:14+00:00}
+                            {--limit= : The number of orders to process per batch.}';
 
     public function handle()
     {
         $startDate = Carbon::parse($this->argument('startDate'));
         $endDate = Carbon::parse($this->argument('endDate'));
+        $limit = $this->option('limit');
         Bus::batch([
-            new FixMobileTransactionsJob($startDate, $endDate)
+            new FixMobileTransactionsJob($startDate, $endDate, $limit)
         ])->onQueue('command')->dispatch();
     }
 }
