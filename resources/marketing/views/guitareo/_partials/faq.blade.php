c699c4e83eec0b66c09266ea81270c2f267f75d8
Trent <trent.harms@gmail.com>
2024-04-29 16:04:47 -0700

    method page updates

--- resources/marketing/views/guitareo/_partials/faq.blade.php
+++ resources/marketing/views/guitareo/_partials/faq.blade.php
@@ -1,28 +1,48 @@
 @php
     $faqs = [
         [
+        "title" => "What is Guitareo?",
+        "desc" => 'Guitareo is your personal guitar mentor online, offering a comprehensive learning experience with a step-by-step curriculum. Whether you are a beginner picking up the guitar for the first time or an advanced player looking to refine your skills, Guitareo provides tailored lessons to suit your level. With a supportive global community, direct access to real teachers, and over a thousand popular songs to learn, Guitareo is designed to make your musical journey enjoyable and successful. From reading tabs and mastering chords to exploring new styles and creating your own music, Guitareo is a full spectrum platform that grows with you as you progress on your guitar adventure​.',
+        ],
+        [
+        "title" => "What makes Guitareo the perfect choice for someone who's never touched a guitar before?",
+        "desc" => 'For beginners, Guitareo stands out with its foundational lessons that cater specifically to those picking up a guitar for the first time. With lessons like "Getting Started on the Acoustic Guitar" and "Beginner Guitar Chords," you’ll find a nurturing environment that encourages and grows your natural talent.',
+        ],
+        [
+        "title" => "What's the first milestone I can look forward to with Guitareo?",
+        "desc" => 'As a Guitareo student, your first thrilling milestone will be mastering the basics of rhythm. You\'ll quickly learn essential strumming patterns that will enable you to turn chords into actual music. Expect to have this solid foundation within your first lessons, paving the way for your guitar journey.',
+        ],
+        [
+        "title" => "Do I need any musical background to succeed with Guitareo?",
+        "desc" => 'Not at all! Guitareo welcomes music lovers with open arms, no matter their starting point. You’ll be guided through a step-by-step curriculum designed to build your skills from the ground up. With a mix of theory and practical lessons, your lack of musical background won\'t hold you back.',
+        ],
+        [
+        "title" => "Can Guitareo help me with finger placement and strumming techniques?",
+        "desc" => 'Yes, Guitareo covers the essentials of finger placement and strumming techniques right from the start. You\'ll get detailed guidance on forming chords, developing a clean strum, and transitioning smoothly between chords. Plus, with exercises and challenges, you\'ll get plenty of practice.',
+        ],
+        [
+        "title" => "What kind of support does Guitareo offer when I hit a roadblock?",
+        "desc" => 'Whenever you face a challenge, Guitareo\'s community and instructors are there to support you through live Q&A sessions, direct feedback, and a treasure trove of resources. You\'re never alone on your guitar-playing journey with such an accessible and helpful community.',
+        ],
+        [
+        "title" => "How does Guitareo keep me motivated during the learning process?",
+        "desc" => 'Your motivation is fueled by Guitareo\'s engaging lesson structure, which includes practice challenges, the ability to learn iconic songs, and a platform that celebrates each step forward. Plus, you\'ll join a community of fellow students where you can share progress and inspiration.',
+        ],
+        [
+        "title" => "How does Guitareo adapt to my pace of learning?",
+        "desc" => 'Your pace sets the pace at Guitareo. With an on-demand lesson structure, you can repeat lessons as needed or skip ahead when you feel ready. Plus, the platform\'s interactive tools allow you to practice at the speed that\'s comfortable for you, ensuring that you learn at a pace that suits your lifestyle and abilities.',
+        ],
+        [
+        "title" => "Does Guitareo offer resources for seasoned guitarists to advance further?",
+        "desc" => 'Absolutely. Guitareo understands that mastery is an ongoing journey. That’s why we offer artist courses and specialized lessons aimed at experienced guitarists. Dive into advanced topics like soloing techniques, complex chord variations, and genre-specific styles to elevate your play. With access to lessons from Grammy Award winners and chart-topping artists, you can refine your skills, learn new ones, and stay inspired. Each course is designed to challenge and expand your musical vocabulary, ensuring there\'s always something new to discover, no matter how advanced you are',
+        ],
+        [
         "title" => "Do I get free access to Drumeo, Singeo, and Pianote with my Guitareo Membership?",
         "desc" => "Absolutely! As a Guitareo member, you'll receive free access to Pianote, Singeo, and Drumeo as well. Elevate your musical journey with us by exploring additional instruments and expanding your musical horizons!",
         ],
         [
-            "title" => "What is Guitareo?",
-            "desc" => 'Guitareo is an online platform that offers an organized guitar lesson curriculum, artist courses on popular topics, 1000+ songs transcribed note-for-note, and a supportive global community of students and teachers.',
-        ],
-        [
-            "title" => "Is Guitareo good for beginners?",
-            "desc" => 'Yes! You’ll always know what to work on with step-by-step video lessons – plus have fun applying your new skills to your favorite songs. And if you ever need help, you’ll have unlimited personal support through live Q&A sessions, student reviews, and a helpful community.',
-        ],
-        [
-            "title" => "Does Guitareo have anything for advanced guitarists?",
-            "desc" => 'Guitareo is the perfect companion for advanced guitarists, giving you access to artist courses so you can gain insights and inspiration from Grammy Award winners, chart-topping performers, and trending musicians – with a growing library of artist courses on a variety of topics.',
-        ],
-        [
             "title" => "Am I too old to start guitar lessons?",
             "desc" => 'You’re never too old to learn guitar. Guitareo has a community of students of all ages, from all around the world. Whether you’re 40, 50, 60, 70, or beyond – you’ll connect with aspiring guitarists just like you who are learning and applying their skills to music.',
-        ],
-        [
-            "title" => "Do I need to be tech-savvy to learn through your app?",
-            "desc" => 'Not at all! Technology is here to make your life easier, and Guitareo is designed to help you find lessons and songs easily. And if you ever get stuck, you can contact our Student Experience team by phone or email for prompt and helpful support.',
         ],
     ]
 @endphp
--- resources/marketing/views/singeo/sales/features/method.blade.php
+++ resources/marketing/views/singeo/sales/features/method.blade.php
@@ -1,5 +1,6 @@
 @php
-    require_once(resource_path('marketing/views/singeo/sales/features/methods.php'))
+    require_once(resource_path('marketing/views/singeo/sales/features/methods.php'));
+    require_once(resource_path('marketing/views/singeo/_partials/homepage-data.php'));
 @endphp
 
 @extends('singeo.sales.features.features-layout')
@@ -17,12 +18,16 @@
 
 @section('header', 'Your singing goals start here.')
 
-@section('desc', 'Step-by-step lessons to understand your voice, strengthen it, and sing with confidence!')
+@section('desc')
+    Have you always wanted to learn how to sing but don’t know where to start?<br>
+    <strong>Get step-by-step lessons that will take you from shower singer to star performer.</strong>
+@endsection
 
 @section('page-body')
-    <section class="py-12 md:py-20">
+    <section class="py-10 sm:py-14 lg:py-20 relative overflow-hidden text-center px-3 lg:px-5">
         <div class="container mx-auto max-w-5xl px-6">
-            <h3 class="font-extrabold text-center mb-10 leading-snug">Your clear path to <br>singing like you’ve always wanted.</h3>
+            <h3 class="font-extrabold max-w-2xl mb-3 leading-snug">Your clear path, frustration-free guide to learning to sing like you always imagined!</h3>
+            <p class="max-w-2xl leading-tight mb-10">Our 10-level curriculum is designed to elevate you from a total beginner to a performer. Each lesson is crafted to build your confidence, improve your range, find your true natural voice, and teach you to sing like you’ve always wanted to.</p>
             @foreach($methods as $key => $method)
                 @include('_partials.components.question-dropdown', [
                     'num' => $key+1,
@@ -35,4 +40,17 @@
             @endforeach
         </div>
     </section>
+
+    @php
+        $testimonials = $singeo['testimonials'];
+        $youtube = convertNumber(Prices::$singeoYoutubeSubsc);
+        $facebook = convertNumber(Prices::$singeoFacebookLikes);
+        $instagram = convertNumber(Prices::$singeoInstagramFollowers);
+    @endphp
+    @include('musora.sales.components.testimonials-section', [
+        'header' => 'singers',
+        'youtubeLink' => 'https://www.youtube.com/singeoofficial/',
+        'facebookLink' => 'https://facebook.com/singeoofficial/',
+        'instagramLink' => 'https://instagram.com/singeoofficial/',
+    ])
 @stop
