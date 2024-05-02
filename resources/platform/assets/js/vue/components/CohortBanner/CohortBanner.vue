f55a13580acef7138c320937a592825658689cc3
Yalung Tang <yalungtang@gmail.com>
2024-05-02 14:52:49 -0300

    cohort banner fix

--- resources/platform/assets/js/vue/components/CohortBanner/CohortBanner.vue
+++ resources/platform/assets/js/vue/components/CohortBanner/CohortBanner.vue
@@ -3,7 +3,10 @@ import { ref, onBeforeMount, onMounted } from 'vue';
 import { XIcon } from "@heroicons/vue/solid";
 import Button from "../Button/Button.vue";
 import userJourney from '../../../services/userJourney';
+import { useUserStore } from '../../../stores/user';
 import {getCookie, setCookie} from "../../vuesora/assets/js/functions/cookies";
+
+const userStore = useUserStore();
 
 const props = defineProps({
     preloadedBanner: {
