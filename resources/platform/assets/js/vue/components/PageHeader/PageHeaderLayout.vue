6191e52d5d94c156e1f9ccc45bc32270ebf2d791
Miguel Fondeur <miguelfondeur@gmail.com>
2024-05-03 20:37:39 +0000

    re-add z-index fix and make header slots wrap

--- resources/platform/assets/js/vue/components/PageHeader/PageHeaderLayout.vue
+++ resources/platform/assets/js/vue/components/PageHeader/PageHeaderLayout.vue
@@ -1,13 +1,12 @@
 <template>
     <header class="tw-px-4 md:tw-px-8">
         <div class="tw-py-5 tw-flex tw-flex-col tw-border-b tw-border-[#E4E4E7] dark:tw-border-[#223457]">
-            <div class="tw-flex tw-justify-between tw-items-center"> 
-                <div class="tw-flex-grow">
+            <div class="tw-flex tw-items-center tw-flex-wrap"> 
+                <div class="tw-flex-grow tw-mr-auto">
                     <slot name="top-left"></slot>
                 </div>
                 <!-- only use this div if top-right is used -->
-
-                <div v-if="$slots['top-right']" class="tw-flex tw-flex-grow tw-justify-end sm:tw-self-end tw-items-end">
+                <div v-if="$slots['top-right']" class="tw-flex tw-py-1">
                     <slot name="top-right"></slot>
                 </div>
             </div>
