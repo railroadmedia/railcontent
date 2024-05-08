2fec3ef2a9712df4b7063ccd1870df398b717278
Roxana Riza <roxana_r2000@yahoo.com>
2024-05-08 16:04:44 +0300

    Set new products for Apple

--- config/ecommerce.php
+++ config/ecommerce.php
@@ -450,6 +450,8 @@ return [
         //musora products
         'musora_app_monthly_member' => 'musora-monthly-recurring-membership',
         'musora_app_1_year_member' => 'musora-annual-recurring-membership',
+        'musora_annual_subscription_plus' => 'musora-annual-recurring-membership',
+        'musora_monthly_subscription_plus' => 'musora-monthly-recurring-membership',
 //        'musora_app_monthly_member' => 'musora-monthly-recurring-7-day-trial-membership',
 //        'musora_app_1_year_member' => 'musora-annual-recurring-7-day-trial-membership',
     ],
@@ -535,6 +537,9 @@ return [
         //musora products
         'musora_app_monthly_member' => 'musora-monthly-recurring-7-day-trial-membership',
         'musora_app_1_year_member' => 'musora-annual-recurring-7-day-trial-membership',
+        'musora_annual_subscription_plus' => 'musora-annual-recurring-7-day-trial-membership',
+        'musora_monthly_subscription_plus' => 'musora-monthly-recurring-7-day-trial-membership',
+
     ],
     'google_store_products_map_trial' => [
         // live products
