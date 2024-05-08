172568ed61b3f9a40afb4d909b7ec8de91c35651
Miguel Fondeur <miguelfondeur@gmail.com>
2024-05-08 15:08:26 +0000

    remove unused bladesora

--- resources/platform/views/account/lesson-history.blade.php
+++ resources/platform/views/account/lesson-history.blade.php
@@ -20,28 +20,4 @@
             :show-reset-progress="{{ json_encode(true) }}"
         @endif
     ></lesson-history>
-
-{{--                <content-catalogue--}}
-{{--                    catalogue-type="list"--}}
-{{--                    brand="{{ $brand }}"--}}
-{{--                    theme-color="{{ $brand }}"--}}
-{{--                    limit="20"--}}
-{{--                    :included-types="{{ json_encode($allowedTypes) }}"--}}
-{{--                    :use-theme-color="true"--}}
-{{--                    :pre-loaded-content="{{ $listLessons }}"--}}
-{{--                    user-id="{{ auth()->id() }}"--}}
-{{--                    :is-playlists="true"--}}
-{{--                    :paginate="true"--}}
-{{--                    no-results-message="{{ $noResultsMessage }}"--}}
-{{--                    no-results-icon="happy"--}}
-{{--                    :destroy-on-list-removal="true"--}}
-{{--                    initial-page="{{ $initialPage }}"--}}
-{{--                    :force-wide-thumbs="true"--}}
-{{--                    :lock-unowned="true"--}}
-{{--                    :is-admin="<?php echo e(json_encode(user()->isAdmin())); ?>"--}}
-{{--                    @if($resetProgress)--}}
-{{--                    :reset-progress="true"--}}
-{{--                    @endif--}}
-{{--                    :show-loading-animation="true"--}}
-{{--                />--}}
 @endsection
