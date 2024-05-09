75b6cee90b9f311d711f4e8e561957f735089990
Miguel Fondeur <miguelfondeur@gmail.com>
2024-05-09 18:50:01 +0000

    foundations button fix

--- resources/platform/views/content/overview.blade.php
+++ resources/platform/views/content/overview.blade.php
@@ -85,8 +85,13 @@
         ];
     }
     elseif ($parentContent->fetch('type') === 'learning-path') {
+        
         $headerData['iconName'] = 'method';
-        $headerData['title'] = 'Method';
+        if(!Str::contains(request()->path(), 'foundations-2019')) {
+            $headerData['title'] = 'Method';
+        } else {
+            $headerData['title'] = 'Foundations';
+        }
         $headerData['description'] = $parentContent->fetch('data.description');
         $headerData['progress'] = $parentContent->fetch('progress_percent', 0);
         $headerData['progressLabelText'] = $progressLabelText;
@@ -108,9 +113,19 @@
                 ],
             ];
         }
-        if ($brand === 'drumeo' || $brand === 'pianote') {
+        if ($brand === 'drumeo' || $brand === 'pianote' && !Str::contains(request()->path(), 'foundations-2019')) {
             $headerData['ctas'][] = [
                 'type' => 'WhereToBeginCta',
+            ];
+        }
+        if (Str::contains(request()->path(), 'foundations-2019')) {
+            $headerData['ctas'][] = [
+                'type' => 'PageHeaderCta',
+                'props' => [
+                    'text' => 'FOUNDATIONS BOOK RESOURCES',
+                    'url' => '/pianote/resources',
+                    'showAllAlways' => true
+                ]
             ];
         }
     }
