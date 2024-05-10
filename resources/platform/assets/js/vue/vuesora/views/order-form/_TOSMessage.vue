0eacee5f71a8cc37082fedb488f1b0fafcf96d47
Miguel Fondeur <miguelfondeur@gmail.com>
2024-05-10 18:30:15 +0000

    fix footer, coach event, notifications, re-add coach-footer

--- resources/platform/assets/js/vue/vuesora/views/order-form/_TOSMessage.vue
+++ resources/platform/assets/js/vue/vuesora/views/order-form/_TOSMessage.vue
@@ -12,7 +12,7 @@
 
             <!-- If New Recurring Subscription -->
             <template v-if="isNewRecurringSub">
-                By completing your order you agree to our <a class="tiny" href="/terms-of-service">Terms Of Service</a>,
+                By completing your order you agree to our <a class="tiny" href="/terms">Terms Of Service</a>,
                 <a class="tiny" href="/privacy-policy">Privacy Statement</a>, and that <span class="tw-capitalize">{{ brand }}</span> will automatically continue your
                 membership and charge the membership fee (currently ${{ subPrice }}) to your payment plan on
                 a {{ subInterval }} basis until you cancel. You can cancel your subscription at any time by going
@@ -23,7 +23,7 @@
             <!-- Else If Lifetime -->
             <template v-else-if="isNewLifeTimeSub">
                 <span v-if="isZeroDollarOrder" class="text-error">Your credit card will not be charged for this transaction.<br></span>
-                By completing your order you agree to our <a class="tiny" href="/terms-of-service">Terms Of Service</a>,
+                By completing your order you agree to our <a class="tiny" href="/terms">Terms Of Service</a>,
                 <a class="tiny" href="/privacy-policy">Privacy Statement</a>, and that <span class="tw-capitalize">{{ brand }}</span> will give you lifetime access to this
                 service. You have a 90-day guarantee on your purchase to make sure you’re happy with your lessons experience.
             </template>
@@ -31,7 +31,7 @@
             <!-- Else If New Trial NOT Recurring -->
             <template v-else-if="isNewTrial">
                 <span v-if="isZeroDollarOrder" class="text-error">Your credit card will not be charged for this transaction.<br></span>
-                By completing your order you agree to our <a class="tiny" href="/terms-of-service">Terms Of Service</a>
+                By completing your order you agree to our <a class="tiny" href="/terms">Terms Of Service</a>
                 and <a class="tiny" href="/privacy-policy">Privacy Statement</a>. As a trial user, you will have access for {{ trialDuration }} days. After
                 that, you will have the option to continue your membership at your own discretion
                 by choosing one of our membership service agreements.
@@ -40,7 +40,7 @@
             <!-- Else If Pack -->
             <template v-else-if="isPack">
                 <span v-if="isZeroDollarOrder" class="text-error">Your credit card will not be charged for this transaction.<br></span>
-                By completing your order you agree to our <a class="tiny" href="/terms-of-service">Terms Of Service</a>,
+                By completing your order you agree to our <a class="tiny" href="/terms">Terms Of Service</a>,
                 <a class="tiny" href="/privacy-policy">Privacy Statement</a>, and that <span class="tw-capitalize">{{ brand }}</span> will give you lifetime access to this
                 digital product(s). You have a 90-day guarantee on your purchase to make sure you’re happy with your lessons experience.
             </template>
