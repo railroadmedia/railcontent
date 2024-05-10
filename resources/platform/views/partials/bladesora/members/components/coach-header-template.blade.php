32994cb5680356de4b2b34f8b6a4c7dd9b65be9d
Miguel Fondeur <miguelfondeur@gmail.com>
2024-05-10 20:09:03 +0000

    delete unused Vuesora files

---
+++ resources/platform/views/partials/bladesora/members/components/coach-header-template.blade.php
@@ -0,0 +1,96 @@
+@section('scripts')
+    @if(isset($queuedScripts))
+        {{ $queuedScripts }}
+    @endif
+@endsection
+
+@php
+$colors = [
+    'drumeo' => [
+        'solidBg' => '#00101d',
+        'bgRgbStart' => 'rgba(0, 16, 29, 0)',
+        'bgRgbEnd' => '',
+        'subtitle' => '#7E9AB1',
+    ],
+    'guitareo' => [
+        'solidBg' => '#101817',
+        'bgRgbStart' => 'rgba(0, 16, 29, 0)',
+        'bgRgbEnd' => '',
+        'subtitle' => '#717179',
+    ],
+    'singeo' => [
+        'solidBg' => '#18131C',
+        'bgRgbStart' => 'rgba(0, 16, 29, 0)',
+        'bgRgbEnd' => '',
+        'subtitle' => '#717179',
+    ],
+    'pianote' => [
+        'solidBg' => '#201617',
+        'bgRgbStart' => 'rgba(0, 16, 29, 0)',
+        'bgRgbEnd' => '',
+        'subtitle' => '#717179',
+    ],
+];
+$brandColors = $colors[$brand];
+@endphp
+
+<section class="tw-grid xs:tw-grid-cols-1 sm:tw-grid-cols-1 md:tw-grid-cols-1 lg:tw-grid-cols-2 tw-gap-0 tw-relative">
+    <div class="lg:tw-hidden tw-bg-top tw-bg-cover tw-text-white tw-min-h-[25vh]"
+        style="
+            background-color: {{ $colors["$brand"]['solidBg'] }};
+            background-image: url(https://www.musora.com/musora-cdn/image/quality=95/{{ $backgroundImage }});">
+        <div style="width: 100%; height: 100%; background: linear-gradient(180deg,{{ $colors["$brand"]['bgRgbStart'] }} 50%,{{ $colors["$brand"]['solidBg'] }});"></div>
+    </div>
+    <div class="tw-text-white" style="background-color: {{ $brandColors['solidBg'] }}">
+        <div class="tw-mt-24 md:tw-mt-0 tw-text-center lg:tw-text-left md:tw-py-16 lg:tw-py-10 tw-py-12 md:tw-px-14 tw-px-6">
+            <div class="tw-top-5 tw-left-5 lg:tw-top-0 lg:tw-left-0 tw-absolute lg:tw-relative lg:tw-mb-7">
+                <a href="javascript:history.back()" class="tw-inline-flex tw-items-center tw-justify-center tw-shrink-0 tw-w-[40px] tw-h-[40px] tw-border-[2px] tw-border-white tw-rounded-full hover:tw-bg-white tw-text-white hover:tw-text-[#000C17]"><i class="fas fa-arrow-left" aria-hidden="true"></i></a>
+            </div>
+            <div>
+                @if (isset($topSubtitle))
+                    <h2 class="tw-uppercase  tw-mb-2 tw-font-bold tw-text-lg" style="color: {{ $brandColors['subtitle'] }}; font-size: 14px; line-height: 24px;">
+                        {{ $topSubtitle }}
+                    </h2>
+                @endif
+                <h1 class="tw-mb-3 tw-uppercase tw-font-normal tw-text-[40px] tw-leading-none">
+                    {{ $title }}
+                </h1>
+                @if (isset($bottomSubtitle))
+                    <h2 class="tw-uppercase tw-mb-2 tw-font-bold tw-text-lg" style="color: {{ $brandColors['subtitle'] }}; font-size: 14px; line-height: 24px;">
+                        {{ $bottomSubtitle }}
+                    </h2>
+                @endif
+                <h3 class="tw-mb-6 tw-font-normal tw-text-base">
+                    {{ $shortBio }}
+                </h3>
+                @if(implode(' ',$fullName) !== 'Students Of Guitareo')
+                    <div class="tw-flex tw-flex-col lg:tw-flex-row tw-items-center xl:tw-items-start tw-flex-wrap xl:tw-flex-nowrap">
+                        {{ $actions }}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="tw-hidden lg:tw-flex tw-bg-top tw-bg-cover tw-text-white"
+         style="
+            background-color: {{ $colors["$brand"]['solidBg'] }};
+            background-image: url(https://www.musora.com/musora-cdn/image/quality=95/{{ !empty($backgroundImage) ? $backgroundImage : 'https://d3fzm1tzeyr5n3.cloudfront.net/headers/'.$brand.'-header.jpg' }});">
+        <div style="width: 100%; height: 100%; background: linear-gradient(268deg,{{ $colors["$brand"]['bgRgbStart'] }} 50%,{{ $colors["$brand"]['solidBg'] }});"></div>
+    </div>
+</section>
+
+@if ($vimeoVideo != null)
+    <!-- Video Modal -->
+    <div id="coach-trailer-modal" class="modal vimeo-embedded-player">
+        <div class="flex flex-column corners-3">
+            <div class="tw-w-full tw-relative" style="padding-bottom: 56.25%;">
+                <iframe class="tw-absolute tw-w-full tw-h-full reset-on-close"
+                    src="https://player.vimeo.com/video/{{$vimeoVideo}}?title=0&byline=0&portrait=0"
+                    frameborder="0"
+                    allow="autoplay; fullscreen;"
+                    >
+                </iframe>
+            </div>
+        </div>
+    </div>
+@endif
