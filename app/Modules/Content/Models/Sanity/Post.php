a16dc20d4d4458794ab500ba3d6c0aab195e8215
Brandon Kerr <brandonk@musora.com>
2024-05-13 15:44:12 -0400

    Define model structures

---
+++ app/Modules/Content/Models/Sanity/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Modules\Content\Models\Sanity;
+
+use App\Modules\Content\Models\Sanity\Enums\FieldType;
+use App\Modules\Content\Models\Sanity\Structure\Field;
+
+/**
+ * Defines the schema structure for a Post document type in Sanity.
+ *
+ * @property string $type
+ * @property string $name
+ * @property string $title
+ * @property ?string $icon
+ * @property array<Field> $fields
+ */
+class Post extends BaseSanityModel
+{
+    public function __construct()
+    {
+        $fields = [
+            new Field(FieldType::String, 'title', 'Title'),
+        ];
+        parent::__construct('post', 'Post', $fields);
+    }
+}
