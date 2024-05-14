d057fc1554ef379930008ebf02d49aa7f9bad742
Trent <trent.harms@gmail.com>
2024-05-14 11:49:56 -0700

    adding name input to 3 more lead gens

--- app/Modules/CustomerIO/config/customer-io.php
+++ app/Modules/CustomerIO/config/customer-io.php
@@ -370,12 +370,18 @@ return [
                 ],
             ],
             'Getting Started On The Piano' => [
-                'custom_attributes' => [],
+                'custom_attributes' => [
+                    'first_name' => 'required|string'
+                ],
                 'events' => [
                     'pianote_prospect_getting-started',
                 ],
                 'accounts_to_sync' => [
                     'pianote',
+                ],
+                'attributes' => [
+                    'first_name' => 'First Name',
+                    'email' => 'Email'
                 ],
             ],
             'Getting Started On The Piano - Facebook' => [
@@ -388,12 +394,18 @@ return [
                 ],
             ],
             'Sight Reading Made Simple' => [
-                'custom_attributes' => [],
+                'custom_attributes' => [
+                    'first_name' => 'required|string'
+                ],
                 'events' => [
                     'pianote_prospect_sight-reading-made-simple',
                 ],
                 'accounts_to_sync' => [
                     'pianote',
+                ],
+                'attributes' => [
+                    'first_name' => 'First Name',
+                    'email' => 'Email'
                 ],
             ],
             'Learn 3 Songs On Piano' => [
@@ -442,12 +454,18 @@ return [
                 ],
             ],
             'Digital Chords And Scales' => [
-                'custom_attributes' => [],
+                'custom_attributes' => [
+                    'first_name' => 'required|string'
+                ],
                 'events' => [
                     'pianote_prospect_digital-chords-and-scales',
                 ],
                 'accounts_to_sync' => [
                     'pianote',
+                ],
+                'attributes' => [
+                    'first_name' => 'First Name',
+                    'email' => 'Email'
                 ],
             ],
             'Blog Signup' => [
