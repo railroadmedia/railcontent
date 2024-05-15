2e5b995cda7d41f7410a14eb6eea9ee92ac1f9ef
Alexandre Severo <alexandre@musora.com>
2024-05-15 16:19:27 -0700

    then handler

--- resources/marketing/views/_partials/components/forms/redeem-form-script.blade.php
+++ resources/marketing/views/_partials/components/forms/redeem-form-script.blade.php
@@ -8,7 +8,7 @@
                 passwordCheck: '',
             },
 
-            submitRedeem(event){
+            async submitRedeem(event) {
                 this.errors = {
                     code: '',
                     email: '',
@@ -23,38 +23,38 @@
 
                 //validation
                 const access_code = form.access_code.value;
-                if(!access_code.replaceAll(' ', '') || access_code.length < 24){
+                if (!access_code.replaceAll(' ', '') || access_code.length < 24) {
                     this.errors.code = 'Code is not valid.';
                 }
 
 
                 const email = form.email.value;
                 const emailFormat = /^\w+([\.-^+]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                if(!email.match(emailFormat)){
+                if (!email.match(emailFormat)) {
                     this.errors.email = 'Email is not a valid email address.';
                 }
 
                 const password = form.password.value;
-                if(password.length < 8){
+                if (password.length < 8) {
                     this.errors.password = 'Password must be at least 8 characters long.';
                 }
 
-                @if(empty($existingMember) || !$existingMember)
+            @if (empty($existingMember) || !$existingMember)
                     const passwordCheck = form.password_confirmation.value;
-                    if(!passwordCheck){
-                        this.errors.passwordCheck = 'Password must be confirmed.';
-                    } else if(password !== passwordCheck) {
-                        this.errors.passwordCheck = 'Passwords do not match.';
-                    }
-                @endif
+                if (!passwordCheck) {
+                    this.errors.passwordCheck = 'Password must be confirmed.';
+                } else if (password !== passwordCheck) {
+                    this.errors.passwordCheck = 'Passwords do not match.';
+                }
+            @endif
 
                 Object.keys(this.errors).forEach(key => {
-                    if(this.errors[key]){
+                    if (this.errors[key]) {
                         isValid = false;
                     }
                 })
 
-                if(isValid){
+                if (isValid) {
                     const data = new FormData(form);
 
                     fetch('{{ $api }}', {
@@ -65,9 +65,13 @@
                         body: JSON.stringify({
                             ...Object.fromEntries(data)
                         })
-                    }).catch(error =>{
+                    }).then(async res => {
+                        const body = await res.json();
+                        console.log({body})
+                    }).catch(error => {
                         console.log(error)
                     })
+
                     // form.submit();
                 }
             }
