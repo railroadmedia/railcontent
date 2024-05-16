10bbd4668f13011019dce873cdcb4e99e694510f
adrian-musora <adrian@musora.com>
2024-05-16 15:32:20 -0700

    QOL updates

---
+++ app/Modules/Content/Models/Sanity/Structure/BrandField.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Content\Models\Sanity\Structure;
+
+use App\Modules\Brand\Enums\Brand;
+use App\Modules\Content\Models\Sanity\Enums\FieldType;
+use App\Modules\Content\Models\Sanity\Structure\Field;
+use App\Modules\Content\Models\Sanity\Structure\Group;
+
+/**
+ * A field of a Sanity CMS document used for Brand Values
+ */
+class BrandField extends Field
+{
+    /**
+     * @param  Group|array<Group>|null  $group
+     */
+    public function __construct(
+        public Group|array|null $group = null,
+    ) {
+        parent::__construct(FieldType::String, 'brand', options:['list' => array_column(Brand::cases(), 'value')], group: $this->group);
+    }
+
+}
