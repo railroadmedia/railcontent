6887ccbc5f517615f9fabd9db5a0a6a077d7bc57
peterminhlee <petereminhlee@gmail.com>
2024-05-20 15:50:45 -0700

    misplaced container fix

--- resources/platform/assets/js/vue/Pages/CoachIndex.vue
+++ resources/platform/assets/js/vue/Pages/CoachIndex.vue
@@ -8,75 +8,75 @@
             icon-name="whistle"
             :description="headerDescription"
         />
-        <!-- Coach Event -->
-        <div v-if="hasCoachEvent" class="tw-w-full tw-mx-auto 3xl:tw-max-w-screen-3xl 4xl:tw-max-w-screen-4xl tw-px-4 md:tw-px-8 tw-mt-4">
-            <CoachEvent
-                :preloaded-content="coachEvent"
-                :current-date-string="currentDateString"
-                :subscription-calendar-id="subscriptionCalendarId"
-                :youtube-event-id="youtubeEventId"
-                :time-cutoff-minutes="timeCutoffMinutes"
-            />
-        </div>
-        <div class="tw-w-full tw-mx-auto 3xl:tw-max-w-screen-3xl 4xl:tw-max-w-screen-4xl">
-            <!-- Featured Coach -->
-            <div class="tw-mb-[30px]">
-                <div class="tw-flex tw-flex-row">
-                    <div class="tw-flex tw-flex-col tw-flex-grow">
-                        <div class="tw-text-[#00101D] dark:tw-text-white tw-pb-1">
-                            <h2 class="tw-font-bold tw-text-xl md:tw-text-2xl tw-mb-[15px]">
-                                Featured Coach
-                            </h2>
-                        </div>
-                        <StaticHeader
-                            v-if="featuredCoachLength === 1"
-                            v-for="formattedCoach in formattedFeaturedCoaches"
-                            :top-subtitle="formattedCoach.subtitle"
-                            title-classes="tw-text-[#FAA300]"
-                            :title="formattedCoach.title"
-                            :cta-text="formattedCoach.primary_cta_text"
-                            :description="formattedCoach.description"
-                            :cta-url="formattedCoach.primary_cta_url"
-                            :img="formattedCoach.img"
-                        />
-                        <HeaderCarousel v-else-if="featuredCoachLength > 1" :preloaded-carousel="formattedFeaturedCoaches" />
+    </div>
+    <!-- Coach Event -->
+    <div v-if="hasCoachEvent" class="tw-w-full tw-mx-auto 3xl:tw-max-w-screen-3xl 4xl:tw-max-w-screen-4xl tw-px-4 md:tw-px-8 tw-mt-4">
+        <CoachEvent
+            :preloaded-content="coachEvent"
+            :current-date-string="currentDateString"
+            :subscription-calendar-id="subscriptionCalendarId"
+            :youtube-event-id="youtubeEventId"
+            :time-cutoff-minutes="timeCutoffMinutes"
+        />
+    </div>
+    <div class="tw-w-full tw-mx-auto 3xl:tw-max-w-screen-3xl 4xl:tw-max-w-screen-4xl">
+        <!-- Featured Coach -->
+        <div class="tw-mb-[30px]">
+            <div class="tw-flex tw-flex-row">
+                <div class="tw-flex tw-flex-col tw-flex-grow">
+                    <div class="tw-text-[#00101D] dark:tw-text-white tw-pb-1">
+                        <h2 class="tw-font-bold tw-text-xl md:tw-text-2xl tw-mb-[15px]">
+                            Featured Coach
+                        </h2>
                     </div>
+                    <StaticHeader
+                        v-if="featuredCoachLength === 1"
+                        v-for="formattedCoach in formattedFeaturedCoaches"
+                        :top-subtitle="formattedCoach.subtitle"
+                        title-classes="tw-text-[#FAA300]"
+                        :title="formattedCoach.title"
+                        :cta-text="formattedCoach.primary_cta_text"
+                        :description="formattedCoach.description"
+                        :cta-url="formattedCoach.primary_cta_url"
+                        :img="formattedCoach.img"
+                    />
+                    <HeaderCarousel v-else-if="featuredCoachLength > 1" :preloaded-carousel="formattedFeaturedCoaches" />
                 </div>
             </div>
-            <!-- Latest Featured Lessons -->
-            <div class="tw-mb-[30px]">
-                <MiniCatalogueSection
-                    title="Latest Featured Lessons"
-                    :pre-loaded-content="latestLessons"
-                />
-            </div>
-            <!-- From Subscribed Coaches -->
-            <div class="tw-mb-[30px]">
-                <MiniCatalogueSection
-                    title="From Subscribed Coaches"
-                    :see-all-url="`/${brand}/lessons/subscribed`"
-                    seeAllAriaLabel="See All From Subscribed Coaches"
-                    :pre-loaded-content="followedLessons"
-                />
-            </div>
-            <!-- Upcoming Coaches -->
-            <UpcomingCoach v-if="hasUpcomingCoaches" :upcoming-coaches="upcomingCoaches" />
-            <!-- Active Coaches -->
-            <ActiveCoach v-if="hasActiveCoaches" :active-coaches="activeCoaches"  />
-
-            <CollectionWrapper
-                :collection-type="collectionType"
-                :filterable-values="collectionFilterableValues"
-                :included-types="collectionIncludedTypes"
-                :limit="collectionLimit"
-                :pre-loaded-content="collectionData"
-                :required-fields="collectionRequiredFields"
-                :statuses="collectionStatuses"
-                :tab-options="collectionTabOptions"
-                :default-sort="collectionDefaultSort"
-                :show-reset-progress="collectionShowResetProgress"
+        </div>
+        <!-- Latest Featured Lessons -->
+        <div class="tw-mb-[30px]">
+            <MiniCatalogueSection
+                title="Latest Featured Lessons"
+                :pre-loaded-content="latestLessons"
             />
         </div>
+        <!-- From Subscribed Coaches -->
+        <div class="tw-mb-[30px]">
+            <MiniCatalogueSection
+                title="From Subscribed Coaches"
+                :see-all-url="`/${brand}/lessons/subscribed`"
+                seeAllAriaLabel="See All From Subscribed Coaches"
+                :pre-loaded-content="followedLessons"
+            />
+        </div>
+        <!-- Upcoming Coaches -->
+        <UpcomingCoach v-if="hasUpcomingCoaches" :upcoming-coaches="upcomingCoaches" />
+        <!-- Active Coaches -->
+        <ActiveCoach v-if="hasActiveCoaches" :active-coaches="activeCoaches"  />
+
+        <CollectionWrapper
+            :collection-type="collectionType"
+            :filterable-values="collectionFilterableValues"
+            :included-types="collectionIncludedTypes"
+            :limit="collectionLimit"
+            :pre-loaded-content="collectionData"
+            :required-fields="collectionRequiredFields"
+            :statuses="collectionStatuses"
+            :tab-options="collectionTabOptions"
+            :default-sort="collectionDefaultSort"
+            :show-reset-progress="collectionShowResetProgress"
+        />
     </div>
 </template>
 <script setup>
