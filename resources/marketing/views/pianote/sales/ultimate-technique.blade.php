16d5e24d3fb9fa29157155c0809cbc37bc885240
Trent <trent.harms@gmail.com>
2024-05-22 12:40:43 -0700

    fixing guarantee section

--- resources/marketing/views/pianote/sales/ultimate-technique.blade.php
+++ resources/marketing/views/pianote/sales/ultimate-technique.blade.php
@@ -467,33 +467,26 @@
         'instagramLink' => 'https://instagram.com/pianoteofficial/',
     ])
 
-    @php
-        $logo =
-            'https://d21q7xesnoiieh.cloudfront.net/fit-in/400x0/filters:quality(95)/marketing/pianote/products/30-day-better-technique/better-technique-guarantee.webp';
-        $guaranteeText = "We’re so confident that you’ll LOVE the improvements to your piano technique after Jordan’s course, that we’re giving you THREE times as long to put it to the test.
-                        <br><br>
-                        <strong>The course is 30 days, but you’ll have 90 days to try it risk-free.</strong>
-                        <br><br>
-                        That means you’ll have enough time to go through every lesson and play with Jordan - THREE times. And if -- after you’ve put in the work -- you don’t see real improvements to your technique... 
-                        <br><br>
-                        If your fingers don’t feel stronger and your hands aren’t more coordinated…
-                        <br><br>
-                        If you don’t enjoy playing the piano more than you did before you started…
-                        <br><br>
-                        Contact <u id='email'>support@pianote.com</u> within those 90 days and get a refund.";
-        $guaranteeHeader = "<strong>The 90-Day “Better <br class='inline sm:hidden'> Technique” Guarantee</strong>";
-    @endphp
-
     <div class="h-5 sm:h-10 -mt-5 sm:-mt-10 relative z-10"
         style="background: linear-gradient(to bottom right, transparent calc(50% - 1px), transparent, #00101D calc(50% + 1px));">
     </div>
     <section class="text-center text-white px-5 sm:px-6 py-10 sm:py-14 lg:py-20 relative z-10"
-        style="background-color:#00101D; border: 1px solid #00101D">
-        <div class="container max-w-6xl mx-auto">
+        style="background-color:#00101D;">
+        <div class="container max-w-5xl mx-auto">
+            @php
+                $logo =
+                    'https://d21q7xesnoiieh.cloudfront.net/fit-in/400x0/filters:quality(95)/marketing/pianote/products/30-day-better-technique/better-technique-guarantee.webp';
+                $guaranteeText = "We’re so confident that you’ll LOVE your time with Pianote, that we’re giving you another 90 days to put it to the test.
+                    <br><br>
+                    It’s the longest guarantee out there. And it’s enough time to know if this is for you.
+                    <br><br>
+                    If you’re not happy (for any reason), simply contact support@pianote.com within those 90 days for a refund.
+                    <br><br>
+                    That’s the Better Technique Guarantee™ ";
+                $guaranteeHeader = "<strong>The 90-Day “Better <br class='inline sm:hidden'> Technique” Guarantee</strong>";
+            @endphp
             @include('pianote._partials._guarantee-section', [
-                'containerWidth' => 'max-w-6xl',
-                'imageUrl' =>
-                    'https://d21q7xesnoiieh.cloudfront.net/fit-in/770x0/filters:quality(95)/marketing/pianote/products/30-day-better-technique/guarantee-collage.webp',
+                'imageUrl' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/770x0/filters:quality(95)/marketing/pianote/products/30-day-better-technique/guarantee-collage.webp',
             ])
         </div>
     </section>
