2bdbfebe366c82badf70002fd0c3e255e45426f4
Alexandre Severo <alexandre.severo@me.com>
2024-05-29 09:55:33 -0700

    refactor(BE-196): remap platform property (#1046)
    * Change to match new user-agent
    * unit tests
    
    ---------
    
    Co-authored-by: Alexandre Severo <alexandre@musora.com>

--- app/Modules/EventTracking/Avo/AvoHelper.php
+++ app/Modules/EventTracking/Avo/AvoHelper.php
@@ -9,16 +9,16 @@ class AvoHelper
     /**
      * @return string
      */
-    public static function getRequestPlatform(): string
+    public static function getRequestPlatform(?string $userAgent = ''): string
     {
-        $userAgent = strtolower(request()?->userAgent()) ?? '';
+        $userAgent = strtolower($userAgent);
 
-        if (str_contains($userAgent, 'drumeo')) {
-            return 'drumeo-app';
-        } elseif (str_contains($userAgent, 'pianote')) {
-            return 'pianote-app';
-        } elseif (str_contains($userAgent, 'musora')) {
+        if (str_contains($userAgent, 'musora/2')) {
             return 'musora-app';
+        } elseif (str_contains($userAgent, 'pianote')) {
+            return 'pianote-app-legacy';
+        } elseif (str_contains($userAgent, 'musora')) {
+            return 'musora-app-legacy';
         }
 
         return 'web';
@@ -27,14 +27,16 @@ class AvoHelper
     /**
      * @return string
      */
-    public static function getRequestOS(): string
+    public static function getRequestOS(?string $userAgent = ''): string
     {
-        $userAgent = strtolower(request()?->userAgent()) ?? '';
+        $userAgent = strtolower($userAgent);
 
-        if (str_contains($userAgent, 'ios')
+        if (
+            str_contains($userAgent, 'ios')
             || str_contains($userAgent, 'iphone')
             || str_contains($userAgent, 'ipad')
-            || str_contains($userAgent, 'cfnetwork')) {
+            || str_contains($userAgent, 'cfnetwork')
+        ) {
             return 'ios';
         } elseif (str_contains($userAgent, 'android')) {
             return 'android';
@@ -49,13 +51,13 @@ class AvoHelper
      */
     public static function defaultEventProperties(array $properties = [], User $user = null): array
     {
+        $userAgent = request()?->userAgent();
         $defaultProperties = [
             'user_id_' => $user ? strval($user->id) : userIdString(),
-            'platform' => self::getRequestPlatform(),
-            'os' => self::getRequestOS()
+            'platform' => self::getRequestPlatform($userAgent),
+            'os' => self::getRequestOS($userAgent)
         ];
 
         return array_merge($defaultProperties, $properties);
     }
-
 }
