cc1a284d6bc04538a2eb01ac4cac08d4645cd12f
Roxana Riza <roxana_r2000@yahoo.com>
2024-05-29 17:12:39 +0300

    Add models

---
+++ app/Modules/Content/Models/Sanity/Resource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Modules\Content\Models\Sanity;
+
+use App\Modules\Content\Models\Sanity\Enums\FieldType;
+use App\Modules\Content\Models\Sanity\Structure\Field;
+
+/**
+ * Defines the schema structure for a Resource document type in Sanity.
+ *
+ * @property string $type
+ * @property string $name
+ * @property string $title
+ * @property ?string $icon
+ * @property array<Field> $fields
+ */
+class Resource extends BaseSanityModel
+{
+    public function __construct()
+    {
+        $fields = [
+            new Field(FieldType::String, 'title'),
+            new Field(FieldType::URL, 'url'),
+        ];
+        $preview = ['select' => ['title' => 'title', 'subtitle' => 'url']];
+        parent::__construct('resource', 'Resource', $fields, preview: $preview);
+    }
+}
