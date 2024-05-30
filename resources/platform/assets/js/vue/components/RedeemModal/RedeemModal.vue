c17c4238561218607389b336af6d029bb94cc1ab
peterminhlee <petereminhlee@gmail.com>
2024-05-30 10:04:46 -0700

    password check refix

--- resources/platform/assets/js/vue/components/RedeemModal/RedeemModal.vue
+++ resources/platform/assets/js/vue/components/RedeemModal/RedeemModal.vue
@@ -168,17 +168,17 @@ const submitForm = async (event) => {
         if (password.length < 8) {
             errors.value.password = 'Password must be at least 8 characters long.';
         }
-    }
 
-    if (isNewAccount.value) {
-        const passwordCheck = form.password_confirmation.value;
-        if (!passwordCheck) {
-            errors.value.passwordCheck = 'Password must be confirmed.';
-        } else if (password !== passwordCheck) {
-            errors.value.passwordCheck = 'Passwords do not match.';
+        if (isNewAccount.value) {
+            const passwordCheck = form.password_confirmation.value;
+            if (!passwordCheck) {
+                errors.value.passwordCheck = 'Password must be confirmed.';
+            } else if (password !== passwordCheck) {
+                errors.value.passwordCheck = 'Passwords do not match.';
+            }
         }
     }
-
+    
     Object.keys(errors.value).forEach(key => {
         if (errors.value[key]) {
             isLoading.value = false;
