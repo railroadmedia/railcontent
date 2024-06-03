7289378284a7860d53fc2a8047d66fefa885e4e5
Brandon Kerr <brandonk@musora.com>
2024-06-03 15:49:25 -0400

    Add API token config

--- app/Modules/Content/config/sanity-cms.php
+++ app/Modules/Content/config/sanity-cms.php
@@ -4,5 +4,6 @@ return [
 
     'project_id' => env('SANITY_CMS_PROJECT_ID', '4032r8py'),
     'dataset' => env('SANITY_CMS_DATASET', 'development'),
+    'api_token' => env('SANITY_API_TOKEN'),
 
 ];
