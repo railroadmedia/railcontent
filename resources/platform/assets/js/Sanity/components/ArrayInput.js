e50fbf7653a1a06df3e068985224b0e9575db802
Roxana Riza <roxana_r2000@yahoo.com>
2024-06-05 16:47:57 +0300

    Update total_xp and child_count when song soundslice change

--- resources/platform/assets/js/Sanity/components/ArrayInput.js
+++ resources/platform/assets/js/Sanity/components/ArrayInput.js
@@ -1,30 +1,79 @@
 import React from 'react';
-import {useCallback} from 'react'
-import {Grid, Stack, Button} from '@sanity/ui'
-import {AddIcon} from '@sanity/icons'
-import {ArrayOfObjectsInputProps, useFormValue} from 'sanity'
-import {useDocumentPane} from 'sanity/desk'
+import {useCallback, useState} from 'react'
+import {Grid, Stack, Button, Box, Flex} from '@sanity/ui'
+import {ArrayOfObjectsInputProps, useClient, useFormValue, set, unset} from 'sanity'
+
 
 
 const ArrayInput  = React.forwardRef((props, ref) => {
-    const {value, path} = props
-    const {onChange} = useDocumentPane()
-    const docId = String(useFormValue(["_id"]));
-    console.log("handleChange ArrayInput   doc id:: ", docId, 'props:::: ',props)
+    const {renderDefault, elementProps, onChange, value = '',  path} = props
+    const sanityClient = useClient({apiVersion: '2023-01-01'});
+    const docId = useFormValue(["_id"]);
+    const entries = useFormValue(['soundslice']) ?? [];
+    const childrenCount = useFormValue(['child_count']) ?? 0;
+    const totalXP = useFormValue(['total_xp']);
+    const xp = useFormValue(['xp']) ?? 150;
 
-    const handleChange = useCallback(
-        (event) => {
-            console.log('handleChange event', event,  docId);
+//     const handleChange = useCallback(
+//         (event) => {
+//
+//             console.log('event in soundslice array::::: ', event.parent.parent,'  document::::: ', event.target);
+//             const nextValue = event.currentTarget.value
+//             //change <otherField> to the field you'd like to patch
+//             //client.patch(id).set({otherField: nextValue}).commit()
+//             console.log('entries', entries);
+//             const number = entries.length;
+//
+//             if(number !== childrenCount) {
+//                 console.log("handleChange ArrayInput   soundslices ", number, 'childrenCount:::',childrenCount)
+//                 sanityClient
+//                     .patch(docId)
+//                     .set({
+//                         child_count: number,
+//                     })
+//                     .commit()
+//             }
+//
+//
+//             const calculatedXP = 25*number + xp;
+//             if(calculatedXP !== totalXP) {
+//                 console.log("handleChange ArrayInput   soundslices ", entries, entries.length, 'totalXP:::',calculatedXP)
+//                 sanityClient
+//                     .patch(docId)
+//                     .set({
+//                         total_xp: calculatedXP,
+//                     })
+//                     .commit()
+//             }
+//
+//             onChange(nextValue ? set(nextValue) : unset())
+//         },
+//         [onChange],
+//     )
+    console.log('entries', entries);
+    const number = entries.length;
 
-            onChange(event.target.value ? set(event.target.value) : unset());
-        },
-        [onChange]
-    )
+    if(number !== childrenCount) {
+        console.log("handleChange ArrayInput   soundslices ", number, 'childrenCount:::',childrenCount)
+        sanityClient
+            .patch(docId)
+            .set({
+                child_count: number,
+            })
+            .commit()
+    }
 
-    return (
-        <Stack space={3}>
-            {props.renderDefault(props)}
-        </Stack>
-    )
+    const calculatedXP = 25*number + xp;
+    if(calculatedXP !== totalXP) {
+        console.log("handleChange ArrayInput   soundslices ", entries, entries.length, 'totalXP:::',calculatedXP)
+        sanityClient
+            .patch(docId)
+            .set({
+                total_xp: calculatedXP,
+            })
+            .commit()
+    }
+
+    return renderDefault({ ...props, elementProps: {...elementProps}})
 });
 export default ArrayInput;
