bae469abddd6e057a526c85d3ccb4e50c628ca44
peterminhlee <petereminhlee@gmail.com>
2024-06-05 18:22:57 -0700

    pack overview bundle page

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -35,6 +35,7 @@ import StudentFocus from './vue/Pages/StudentFocus';
 import Shows from './vue/Pages/Shows';
 import GuitareoLessons from './vue/Pages/GuitareoLessons';
 import PackOverview from './vue/Pages/PackOverview';
+import PackOverviewBundles from './vue/Pages/PackOverviewBundles';
 
 //App Components
 import AppContainer from './vue/apps/AppContainer.vue';
@@ -283,6 +284,7 @@ app.component('AppContainer', AppContainer)
     .component('Shows', Shows)
     .component('GuitareoLessons', GuitareoLessons)
     .component('PackOverview', PackOverview)
+    .component('PackOverviewBundles', PackOverviewBundles)
 
     .component('PlaylistPlayback', defineAsyncComponent(() =>
         import(
