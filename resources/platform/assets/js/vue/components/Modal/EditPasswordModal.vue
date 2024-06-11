dd8f35388037f74e9ff6158136e8ee31f2952366
Miguel Fondeur <miguelfondeur@gmail.com>
2024-06-11 15:29:19 +0000

    fix style in modals

--- resources/platform/assets/js/vue/components/Modal/EditPasswordModal.vue
+++ resources/platform/assets/js/vue/components/Modal/EditPasswordModal.vue
@@ -56,7 +56,7 @@
                 </div>
                 <div class="tw-flex tw-w-full tw-justify-end tw-mb-[20px] tw-flex-wrap sm:tw-flex-nowrap tw-gap-2 sm:tw-gap-0">
                     <MuButton
-                        class="tw-flex tw-w-full tw-justify-end tw-mb-[20px] tw-flex-wrap sm:tw-flex-nowrap tw-gap-2 sm:tw-gap-0"
+                        class="tw-w-full sm:tw-w-auto sm:tw-mx-1 dark:tw-bg-white tw-bg-black dark:tw-text-[#00101D] tw-text-white"
                         type="submit"
                         :disabled="formData.new_password_confirmation !== formData.new_password"
                         :processing="formProcessing"
