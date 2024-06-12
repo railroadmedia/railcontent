0af5f6d4d78c9e14289fc9c73ceb931b83ef0ae4
peterminhlee <petereminhlee@gmail.com>
2024-06-12 10:34:35 -0700

    routine comps

---
+++ resources/platform/assets/js/vue/components/Catalogue/RoutineCatalogue.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="tw-flex tw-flex-row  tw-flex-wrap -tw-mx-[10px]">
+        <RoutineCard
+            v-for="(item, i) in content"
+            :key="'grid' + item.id"
+            :item="item"
+            @addToList="addToList"
+            @showRoutineSoundSlice="showRoutineSoundSlice"
+        />
+
+        <SoundSlice
+            :user-id="userId"
+            :theme-color="themeColor"
+            :soundslice-slug="state.soundSliceSlug"
+            :contentId="state.routineId"
+        >
+            <template v-slot:soundsliceControls>
+                <SoundSliceControls
+                    :title="`${state.routineTitle}`"
+                    :disable-next="true"
+                    :disable-prev="true"
+                    @onClose="soundSliceClosed"
+                />
+            </template>
+        </SoundSlice>
+<!--        <SoundSlice-->
+<!--            :sound-slice-slug="state.soundSliceSlug"-->
+<!--            :theme-color="themeColor"-->
+<!--            :title="state.routineTitle"-->
+<!--            :content-id="state.routineId"-->
+<!--            :user-id="userId"-->
+<!--            @soundSliceClosed="soundSliceClosed"-->
+<!--        />-->
+    </div>
+</template>
+<script setup>
+import { reactive } from "vue";
+import RoutineCard from './RoutineCard'
+import SoundSlice from '../SoundSlice/SoundSlice';
+import SoundSliceControls from '../SoundSlice/SoundSliceControls';
+
+import useUserCatalogueEvents from "../../hooks/useUserCatalogueEvents";
+
+const props = defineProps({
+    content: {
+        type: Array,
+        default: () => [],
+    },
+    themeColor: {
+        type: String,
+        default: () => 'drumeo',
+    },
+    userId: {
+        type: String,
+        default: () => '',
+    },
+})
+
+const state = reactive({
+    routineId: '',
+    routineTitle: '',
+    soundSliceSlug: '',
+})
+
+const showRoutineSoundSlice = ({ soundSliceSlug, title, routineId }) => {
+    state.soundSliceSlug = soundSliceSlug;
+    state.routineTitle = title;
+    state.routineId = routineId;
+    console.log('open')
+}
+
+const soundSliceClosed = () => {
+    state.soundSliceSlug = '';
+    state.routineTitle = '';
+    state.routineId = '';
+}
+
+const { addToList } = useUserCatalogueEvents({ ...props });
+</script>
