4602b88ab0247bacc03afde121d1d925151549e5
Roxana Riza <roxana_r2000@yahoo.com>
2024-06-12 20:24:07 +0300

    Update custom components

--- resources/sanitystudio/src/components/ArrayInput.js
+++ resources/sanitystudio/src/components/ArrayInput.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import {useCallback} from 'react'
-import {useClient, useFormValue, set} from 'sanity'
-import {randomKey} from '@sanity/util/content'
+import React, { useEffect } from 'react';
+import { useClient, useFormValue, set } from 'sanity';
 
-const ArrayInput  = React.forwardRef((props, ref) => {
-    const {renderDefault, elementProps, value = '',  onChange} = props
-    const sanityClient = useClient({apiVersion: '2023-01-01'});
+const ArrayInput = React.forwardRef((props, ref) => {
+    const { renderDefault, elementProps } = props;
+    const sanityClient = useClient({ apiVersion: '2023-01-01' });
     const docId = useFormValue(["_id"]);
-    const entries = useFormValue(['soundslice']) ?? [];
-    const childrenCount = useFormValue(['child_count']) ?? 0;
+    const entries = useFormValue(['soundslice']) || [];
+    const childrenCount = useFormValue(['child_count']) || 0;
     const totalXP = useFormValue(['total_xp']);
-    const xp = useFormValue(['xp']) ?? 150;
+    const xp = useFormValue(['xp']) || 150;
 
-    const number = entries.length;
-    if(number !== childrenCount) {
-        sanityClient
-            .patch(docId)
-            .set({
-                child_count: number,
-            })
-            .commit()
-    }
+    useEffect(() => {
+        const updateDocument = async () => {
+            try {
+                const number = entries.length;
+                if (number !== childrenCount) {
+                    await sanityClient
+                        .patch(docId)
+                        .set({ child_count: number })
+                        .commit();
+                }
 
-    const calculatedXP = 25*number + xp;
-    if(calculatedXP !== totalXP) {
-        sanityClient
-            .patch(docId)
-            .set({
-                total_xp: calculatedXP,
-            })
-            .commit()
-    }
+                const calculatedXP = 25 * number + xp;
+                if (calculatedXP !== totalXP) {
+                    await sanityClient
+                        .patch(docId)
+                        .set({ total_xp: calculatedXP })
+                        .commit();
+                }
+            } catch (error) {
+                console.error('Error updating document:', error);
+                // Handle error here
+            }
+        };
 
-    return renderDefault({ ...props, elementProps: {...elementProps}})
+        updateDocument();
+    }, [docId, entries, childrenCount, totalXP, xp, sanityClient]);
+
+    return renderDefault({ ...props, elementProps: { ...elementProps } });
 });
+
 export default ArrayInput;
