d8e1b1f9eea22d8a8251923e082cc15b5b8ad513
peterminhlee <petereminhlee@gmail.com>
2024-06-13 12:18:40 -0700

    added alpine x mask

--- resources/marketing/views/_partials/layout/global-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-layout.blade.php
@@ -19,8 +19,10 @@
         {!! \App\Analytics\Tracker::trackPageView() !!}
 
         {!! \App\Analytics\Tracker::headBottom() !!}
+        <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.x.x/dist/cdn.min.js"></script>
         <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
         <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.12.0/cdn.min.js"></script>
+
     </head>
 
     <body class="flex flex-col w-full min-h-screen pt-[40px] md:pt-[56px] @yield('body-class')"
