4b4755559bf46be2e429d2935a03237e4f1c606d
peterminhlee <petereminhlee@gmail.com>
2024-06-13 10:53:39 -0700

    style updates on code input

--- resources/marketing/views/musora/pages/redeem/_redeem-form.blade.php
+++ resources/marketing/views/musora/pages/redeem/_redeem-form.blade.php
@@ -17,7 +17,7 @@
     @endif
     <div class="flex flex-wrap">
         <p class="w-full input-describer">Code</p>
-        <input class="default-form-field" x-bind:class="{ 'border border-[#EF4444] bg-[#FECACA] text-black mb-1.5': errors.access_code }" type="text" name="access_code" maxlength="24" placeholder="XXXXXXXXXXXXXXXXXXXXXXXX" value="" x-bind:disabled="loading" />
+        <input class="default-form-field text-center uppercase" x-bind:class="{ 'border border-[#EF4444] bg-[#FECACA] text-black mb-1.5': errors.access_code }" type="text" name="access_code" maxlength="24" placeholder="XXXX - XXXX - XXXX - XXXX - XXXX - XXXX" value="" x-bind:disabled="loading" />
         <span class="text-xs text-[#EF4444]" x-show="errors.access_code" x-text="errors.access_code"></span>
     </div>
     @if(empty($existing))
