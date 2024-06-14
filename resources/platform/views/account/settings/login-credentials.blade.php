4a7d7029fa0de338e8c72c3633866f0bff9d5838
Miguel Fondeur <miguelfondeur@gmail.com>
2024-06-14 18:09:05 +0000

    clean up

--- resources/platform/views/account/settings/login-credentials.blade.php
+++ resources/platform/views/account/settings/login-credentials.blade.php
@@ -4,55 +4,6 @@
     <title>Login Credentials | Musora</title>
 @endsection
 
-{{-- @section('edit-forms')
-    <div id="editForm" class="tw-flex tw-flex-col">
-        
-        <div class="tw-flex tw-flex-row pa-3 tw-pb-0 tw-flex-auto">
-            <h1 class="tw-text-2xl tw-font-bold tw-text-[#00101D] dark:tw-text-white">Login Credentials</h1>
-        </div>
-
-        <div class="tw-flex tw-flex-row pa-3 tw-flex-auto tw-border-b tw-border-gray-300 dark:tw-border-[#223F57]">
-            @include('partials.bladesora.members.account.settings.login-credentials.email-form', [
-                'brand' => brand(),
-                'otherBrands' => 'Drumeo, Pianote, and Guitareo',
-                'action' =>  route('user_management_system.email-change.request'),
-                'method' => 'post',
-                'emailInput' => [
-                    'inputErrors' => $errors->get('email'),
-                    'inputValue' => user()->email,
-                    'inputName' => 'email',
-                ],
-                'emailPasswordInput' => [
-                    'inputErrors' => $errors->get('current_password'),
-                    'inputValue' => '',
-                    'inputName' => 'user_password'
-                ],
-                'showLegacyUserNameMessage' => false
-           ])
-        </div>
-
-        <div class="tw-flex tw-flex-row pa-3 tw-flex-auto body">
-            @include('partials.bladesora.members.account.settings.login-credentials.password-form', [
-                'brand' => '{{ $brand }}',
-                'otherBrands' => 'Drumeo, Pianote, and Guitareo',
-                'action' => route('user_management_system.password.update'),
-                'method' => 'patch',
-                'currentPasswordInput' => [
-                    'inputErrors' => $errors->get('current_password'),
-                ],
-                'newPasswordInput' => [
-                    'inputErrors' => $errors->get('new_password'),
-                    'inputName' => 'new_password',
-                ],
-                'newPasswordConfirmationInput' => [
-                    'inputErrors' => $errors->get('new_password_confirmation'),
-                    'inputName' => 'new_password_confirmation',
-                ],
-            ])
-        </div>
-    </div>
-@endsection --}}
-
 @section('content')
     {{-- Login Credentials Page Component --}}
     <login-credentials></login-credentials>
