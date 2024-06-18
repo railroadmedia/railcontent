01f6597f56d34d182eb59cd7d8ca1cd8053fe2e7
Brandon <brandonk@musora.com>
2024-06-18 13:06:30 -0400

    Create endpoint to get whether a song is liked by a user (#1117)
    * Setting up controller and route for checking if a user has liked a piece of content
    
    * cleanup
    
    * redis hack for test routes
    
    * Remove UserActivitySyncMiddleware for tests
    
    * Remove UserActivitySyncMiddleware from all tests by default

---
+++ app/Modules/Content/routes/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Modules\Content\Controllers\ContentMetadataController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('content')
+    ->middleware('web_or_api_authenticated')
+    ->group(function () {
+        Route::get(
+            'is_liked_by_user/{content}/{user?}',
+            [ContentMetadataController::class, 'isLikedByUser']
+        )->name('content.is_liked_by_user');
+    });
