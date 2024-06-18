b91999e60fb4f840a42f92ef71f7a19e147edac8
Trent <trent.harms@gmail.com>
2024-06-18 16:42:13 -0700

    wip

--- resources/marketing/views/musora/_partials/homepage-data.php
+++ resources/marketing/views/musora/_partials/homepage-data.php
@@ -12,28 +12,31 @@ $musora = [
 
     "packs" => [
         [
-            "image" => "marketing/musora/membership/homepage/2024/packs/30DBP.webp",
-        ],
-        [
-            "image" => "marketing/musora/membership/homepage/2024/packs/30DC.webp",
-        ],
-        [
             "image" => "marketing/musora/membership/homepage/2024/packs/30DD.webp",
-        ],
-        [
-            "image" => "marketing/musora/membership/homepage/2024/packs/30DI.webp",
-        ],
-        [
-            "image" => "marketing/musora/membership/homepage/2024/packs/30DTBS.webp",
         ],
         [
             "image" => "marketing/musora/membership/homepage/2024/packs/30TBT.webp",
         ],
         [
+            "image" => "marketing/musora/membership/homepage/2024/packs/AGSH.webp",
+        ],
+        [
+            "image" => "marketing/musora/membership/homepage/2024/packs/SBI30D.webp",
+        ],
+        [
+            "image" => "marketing/musora/membership/homepage/2024/packs/30DC.webp",
+        ],
+        [
+            "image" => "marketing/musora/membership/homepage/2024/packs/NPPSH.webp",
+        ],
+        [
+            "image" => "marketing/musora/membership/homepage/2024/packs/30DTBS.webp",
+        ],
+        [
             "image" => "marketing/musora/membership/homepage/2024/packs/5DTTYV.webp",
         ],
         [
-            "image" => "marketing/musora/membership/homepage/2024/packs/AGSH.webp",
+            "image" => "marketing/musora/membership/homepage/2024/packs/30DI.webp",
         ],
         [
             "image" => "marketing/musora/membership/homepage/2024/packs/EC.webp",
@@ -42,10 +45,7 @@ $musora = [
             "image" => "marketing/musora/membership/homepage/2024/packs/GI.webp",
         ],
         [
-            "image" => "marketing/musora/membership/homepage/2024/packs/NPPSH.webp",
-        ],
-        [
-            "image" => "marketing/musora/membership/homepage/2024/packs/SBI30D.webp",
+            "image" => "marketing/musora/membership/homepage/2024/packs/30DBP.webp",
         ],
     ],
     "gridItems" => [
--- resources/marketing/views/musora/sales/components/order-section-collage.blade.php
+++ resources/marketing/views/musora/sales/components/order-section-collage.blade.php
@@ -1,4 +1,4 @@
-<section class="py-10 sm:py-20 lg:py-24 relative overflow-hidden text-white text-center customize px-4 lg:px-8 relative overflow-hidden"
+<section class="py-10 sm:py-16 lg:py-20 relative overflow-hidden text-white text-center customize px-4 lg:px-8 relative overflow-hidden"
     @if(!empty($bgColor))
         style="background: {{ $bgColor }};"
     @else
@@ -75,6 +75,14 @@
 
                         </a>
                         <p class="text-xs"><em> Pay nothing for @if(!empty($month)) 30 @else 7 @endif days, <br class="lg:hidden">then $20/month billed annually.</em></p>
+                        @if(!empty($theme) && $theme == 'musora')
+                            <div class="flex justify-center sm:justify-start mt-4 sm:mt-5">
+                                <img style="padding-bottom:2px;" class="h-5 sm:h-6 inline-block mr-2 sm:mr-4" src="https://www.musora.com/musora-cdn/image/quality=95,width=250,metadata=none/https://dpwjbsxqtam5n.cloudfront.net/logos/logo-blue.png" alt="logo">
+                                <img style="padding-top:2px;" class="h-5 sm:h-6 inline-block mr-2 sm:mr-4" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/250x0/filters:quality(95)/marketing/pianote/membership/homepage/2023/pianote-logo-red.png" alt="logo">
+                                <img style="padding-top:2px;" class="h-5 sm:h-6 inline-block mr-2 sm:mr-4" src="https://www.musora.com/musora-cdn/image/width=200,quality=95/https://d122ay5chh2hr5.cloudfront.net/sales/guitareo-logo-green.png" alt="logo">
+                                <img style="padding-top:2px;" class="h-5 sm:h-6 inline-block" src="https://www.musora.com/musora-cdn/image/width=200,quality=95/https://d21xeg6s76swyd.cloudfront.net/sales/2021/singeo-logo.png" alt="logo">
+                            </div>
+                        @endif
                     </div>
                 @endif
             </div>
