860ed1bce27860bc8d67dd57aa721410ba5d10a8
Miguel Fondeur <miguelfondeur@gmail.com>
2024-06-19 19:17:28 +0000

    account for empty resources

--- resources/platform/assets/js/vue/vuesora/components/VideoResources/ContentLessonActionButtons.vue
+++ resources/platform/assets/js/vue/vuesora/components/VideoResources/ContentLessonActionButtons.vue
@@ -26,7 +26,7 @@
         </div>
 
         <!-- Resources -->
-        <div v-if="resources.length > 0" class="tw-flex tw-mr-2 relative">
+        <div v-if="resources?.length > 0" class="tw-flex tw-mr-2 relative">
             <button
                 class="open-resources tw-font-bebas-neue tw-uppercase tw-py-1 tw-px-2 tw-text-sm tw-rounded-full tw-text-[#000C17] dark:tw-text-white tw-bg-[#EDEDED] dark:tw-bg-[#0E2031] hover:tw-bg-[#00000026] hover:dark:tw-bg-[#223F57]/90 dark:tw-border dark:tw-border-[#223F57]/40"
                 title="Download Resources" @click="resourceDropdown = !resourceDropdown">
