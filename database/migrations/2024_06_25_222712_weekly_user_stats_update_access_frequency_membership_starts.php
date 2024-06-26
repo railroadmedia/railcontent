59dec4cf6babdc4c06fa46904adea0288bb9ba50
Brandon Kerr <brandonk@musora.com>
2024-06-26 17:07:14 -0400

    Apply default value to weekly_user_statistics table

--- database/migrations/2024_06_25_222712_weekly_user_stats_update_access_frequency_membership_starts.php
+++ database/migrations/2024_06_25_222712_weekly_user_stats_update_access_frequency_membership_starts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class () extends Migration {
     /**
      * Run the migrations.
      *
@@ -28,6 +28,7 @@ return new class extends Migration {
                 'other',
                 'trial'
             ])
+                ->default('other')
                 ->after('access_type')
                 ->index();
 
