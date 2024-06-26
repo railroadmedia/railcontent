a4a309a6346d97ff45a481e2d66c0fdee562a5ee
calebfavor <calebfavor@gmail.com>
2024-06-26 13:03:32 -0700

    Adding started column to weekly users stats. Fixing up some logic issues.

---
+++ database/migrations/2024_06_25_222712_weekly_user_stats_update_access_frequency_membership_starts.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('weekly_user_statistics', function (Blueprint $table) {
+            $table->dropColumn('access_frequency');
+        });
+
+        Schema::table('weekly_user_statistics', function (Blueprint $table) {
+            $table->enum('access_frequency', [
+                '1 month',
+                '2 month',
+                '3 month',
+                '6 month',
+                '5 year',
+                '1 year',
+                'lifetime',
+                'other',
+                'trial'
+            ])
+                ->after('access_type')
+                ->index();
+
+            $table->boolean('started')->after('expired')->index()->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('weekly_user_statistics', function (Blueprint $table) {
+            $table->dropColumn('access_frequency');
+        });
+
+        Schema::table('weekly_user_statistics', function (Blueprint $table) {
+            $table->enum('access_frequency', ['monthly', 'yearly', 'lifetime', 'other', 'trial'])
+                ->after('access_type')
+                ->index();
+
+            $table->dropColumn('started');
+        });
+    }
+};
