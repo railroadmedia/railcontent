fa564680bb2a166945e24bb09d9d345fc421e246
Trent <trent.harms@gmail.com>
2024-07-03 13:11:10 -0700

    adding preferences page

---
+++ resources/marketing/views/drumeo/lead-gen/pages/sm101preferences.blade.php
@@ -0,0 +1,78 @@
+@extends('drumeo._partials.global-layout')
+
+@section('global-head')
+    <title>Updated! | Drumeo</title>
+    <meta property="og:image" content="https://dpwjbsxqtam5n.cloudfront.net/sales/2023/share-image-drumeo.jpg" style="display: none;">
+    @include('_partials.layout._fonts')
+    @include('_partials.layout._tailwindcdn')
+    <link href="{{ asset('/marketing/css/tailwind-helpers.css') }}" rel="stylesheet">
+    <link href="{{ asset('/marketing/parcel/drumeo/navigation-sales.css') }}" rel="stylesheet">
+    <style>
+
+        p {
+            font:400 15px/1.4em "Open Sans", sans-serif;
+            margin:15px auto;
+        }
+
+        @media (min-width:40em) {
+            p {
+                font-size:19px;
+                margin:20px auto;
+            }
+        }
+
+        @media (min-width:64em) {
+            p {
+                font-size:23px;
+            }
+        }
+
+        h2 {
+            font:700 30px/1em "Bebas Neue", sans-serif;
+            text-transform:uppercase;
+            color:#0b76db;
+        }
+
+        @media (min-width:40em) {
+            h2 {
+                font-size:40px;
+            }
+        }
+
+        @media (min-width:64em) {
+            h2 {
+                font-size:50px;
+            }
+        }
+
+        p em {
+            line-height:1.4em;
+            max-width:550px;
+            display:inline-block;
+            font-size:12px;
+        }
+
+        @media (min-width:40em) {
+            p em {
+                font-size:14px;
+            }
+        }
+    </style>
+@stop
+
+@section('global-body')
+    @include("drumeo.sales.partials._nav")
+    <div class="text-center container mx-auto max-w-5xl py-48 sm:py-72">
+        <h2>YOU'RE ENROLLED!</h2>
+        <img class="rounded-xl w-full my-4" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/drumeo/lead-gen/pages/SheetMusic101-01.jpg">
+        <p><strong>Sheet Music 101 starts on July 18th!</strong><br>
+            We'll notify you when the lessons drop! Click below for more info.</p>
+        <a class="join blue smaller" href="https://www.musora.com/drumeo/forums">LEARN MORE</a>
+    </div>
+    @include("drumeo.sales.partials._footer", [
+            "minimal" => true
+        ])
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}"></script>
+@stop
