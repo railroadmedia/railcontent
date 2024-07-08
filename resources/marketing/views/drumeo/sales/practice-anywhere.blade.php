50365e00f69972c939dc82fd5aa96d078cf48f4a
Awatanka <natalia.sokolova.ca@gmail.com>
2024-07-08 15:10:44 -0700

    Updates to bundle pages

--- resources/marketing/views/drumeo/sales/practice-anywhere.blade.php
+++ resources/marketing/views/drumeo/sales/practice-anywhere.blade.php
@@ -18,11 +18,24 @@
 
 @section('top-bar')
 
+@php
+    $originalPrice = 240;
+    $discountedPrice = 180; 
+    $savePercentage = round((($originalPrice - $discountedPrice) / $originalPrice) * 100);
+@endphp
+
     @php
+      $bubble1 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/130x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-04.webp';
+        $bubble2 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/350x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-01.webp';
+        $bubble3 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/380x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-06.webp';
+        $bubble6 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/340x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-03.webp';
+        $bubble7 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/370x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-02.webp';
+        $bubble8 = 'https://d21q7xesnoiieh.cloudfront.net/fit-in/130x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-header-items-05.webp';
+
         $bubbles =  [
              [
                  'src' => $bubble1,
-                 'classes' => 'h-10 sm:h-14 lg:h-16 top-[53%] sm:top-[53%] left-[4%] sm:left-[4%]',
+                 'classes' => 'h-16 sm:h-24 lg:h-28 top-[53%] sm:top-[53%] left-[4%] sm:left-[4%] -rotate-12',
              ],
              [
                  'src' => $bubble2,
@@ -30,27 +43,19 @@
              ],
              [
                  'src' => $bubble3,
-                 'classes' => 'h-32 sm:h-40 lg:h-52 top-[84%] sm:top-[81%] left-[9%] sm:left-[18%]',
-             ],
-             [
-                 'src' => $bubble4,
-                 'classes' => 'h-10 sm:h-12 lg:h-16 top-[13%] sm:top-[13%] left-[31%] sm:left-[31%]',
-             ],
-             [
-                 'src' => $bubble5,
-                 'classes' => 'h-10 sm:h-12 lg:h-16 top-[8%] sm:top-[8%] left-[58%] sm:left-[58%]',
+                 'classes' => 'h-6 md:h-8 top-[91%] sm:top-[81%] left-[9%] sm:left-[18%] -rotate-45',
              ],
              [
                  'src' => $bubble6,
-                 'classes' => 'h-28 sm:h-32 lg:h-48 top-[88%] sm:top-[88%] left-[90%] sm:left-[78%]',
+                 'classes' => 'h-20 sm:h-28 lg:h-40 top-[88%] sm:top-[88%] left-[90%] sm:left-[78%] -rotate-12',
              ],
              [
                  'src' => $bubble7,
-                 'classes' => 'h-28 sm:h-36 lg:h-52 top-[13%] sm:top-[18%] left-[93%] sm:left-[87%]',
+                 'classes' => 'h-28 sm:h-36 lg:h-52 top-[13%] sm:top-[18%] left-[93%] sm:left-[87%] rotate-12',
              ],
              [
                  'src' => $bubble8,
-                 'classes' => 'h-12 sm:h-14 lg:h-16 top-[63%] sm:top-[63%] left-[99%] sm:left-[99%]',
+                 'classes' => 'h-16 sm:h-24 lg:h-28 top-[63%] sm:top-[63%] left-[89%] sm:left-[89%] rotate-12',
              ]
          ];
          $slides = $drumeo['slides'];
@@ -58,10 +63,10 @@
     <header class="text-center px-5 sm:px-6 py-28 sm:py-48 relative overflow-hidden text-white"
         style="background:linear-gradient(45deg, #3418E1, #FF005C);">
         <div class="container max-w-6xl mx-auto relative z-20">
-            <img class="h-20 sm:h-28 mb-3 sm:mb-4" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/510x0/filters:quality(95)/marketing/drumeo/promos/may/logo.webp">
+            <img class="h-20 sm:h-32 mb-3 sm:mb-4" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/700x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/drumeo-summer-logo.webp">
             <br>
             <h1 class="relative w-auto inline-block text-3xl sm:text-5xl lg:text-6xl mb-7 sm:mb-10 font-black font-lexend leading-none sm:leading-none lg:leading-none uppercase">
-                <span class="text-drumeo">EVERYTHING</span> YOU NEED<br class="sm:hidden"> TO <br class="hidden sm:inline"> <span class="relative inline-block">LEARN THE DRUMS.<svg class="absolute left-0 right-0 bottom-0 w-full h-4 sm:h-7" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 524 22" fill="none" style="transform: translate(0, 100%);"><path d="M1.99978 10.6328C84.053 4.08508 302.889 -3.20824 521.809 20" stroke="#0B76DB" stroke-width="3" stroke-linecap="round"></path><path d="M2.17373 15.0541C83.9528 7.29382 302.406 -3.51921 521.988 15.3111" stroke="#0B76DB" stroke-width="3" stroke-linecap="round"></path></svg></span>
+                EVERYTHING YOU NEED<br class="sm:hidden"> TO <br class="hidden sm:inline"> <span class="relative inline-block">LEARN THE DRUMS.<svg class="absolute left-0 right-0 bottom-0 w-full h-4 sm:h-7" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 524 22" fill="none" style="transform: translate(0, 100%);"><path d="M1.99978 10.6328C84.053 4.08508 302.889 -3.20824 521.809 20" stroke="#0B76DB" stroke-width="3" stroke-linecap="round"></path><path d="M2.17373 15.0541C83.9528 7.29382 302.406 -3.51921 521.988 15.3111" stroke="#0B76DB" stroke-width="3" stroke-linecap="round"></path></svg></span>
             </h1>
 
             <p class="text-sm leading-normal sm:tracking-widest mb-5 lg:mb-7">
@@ -71,17 +76,17 @@
                 <i class="fas fa-check lg:ml-5 text-drumeo"></i> DRUMSTICKS
                 <i class="fas fa-check ml-3 sm:ml-5 text-drumeo"></i> 2 DIGITAL PACKS
             </p>
-            <h2 class="leading-tight mt-4 mb-1">
-                <s class="opacity-50">$240</s> <strong>$180</strong>
+             <h2 class="leading-tight my-4">
+                <s class="opacity-50">${{$originalPrice}}</s> <strong>${{$discountedPrice}}</strong>            
+                <span class="mb-4 sm:mb-6 text-3xl">Save {{$savePercentage}}% </span>
+            </span>
             </h2>
-            <p class="mb-4 sm:mb-6"><strong class="text-drumeo">Save 25%</strong> for your first year. Renews at $240/yr.
-            </p>
             <div class="flex flex-wrap justify-center max-w-xs sm:max-w-full mx-auto px-5 sm:px-0">
 
 
                 <a class="sm:mx-0.5 w-full sm:w-56 join drumeo smaller sm:order-1 mb-2 sm:mb-0 anchor-slide"
                     href="#customize-anchor" aria-label="Customize anchor"
-                >SEE YOUR DEAL <i class="fas fa-arrow-right" style="line-height: 0;" aria-hidden="true"></i></a>
+                >GET STARTED </a>
                 <div class="sm:mx-0.5 w-full sm:w-56 join outline white smaller autoplay-video" x-on:click="trailer = true;">WATCH THE TRAILER</div>
             </div>
 
@@ -151,7 +156,7 @@
         style="background:linear-gradient(45deg, #3418E1, #FF005C);">
         <div class="container mx-auto relative z-50  max-w-4xl ">
             <div class="w-full mb-6">
-                <img class="h-20 sm:h-28" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/510x0/filters:quality(95)/marketing/drumeo/promos/may/logo.webp">
+                <img class="h-20 sm:h-28" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/700x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/practice-anywhere-logo.webp">
                 <br>
                 <h3 class="leading-tight mt-4 sm:mt-5 mb-2">The perfect bundle to get you started on the drums.</h3>
             </div>
@@ -159,57 +164,46 @@
             @php
                 $bonuses = [
                     [
-                    'image' => 'marketing/drumeo/promos/may/card-annual.png',
-                    'title' => 'Drumeo StickBag',
+                    'image' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/420x0/filters:quality(95)/marketing/drumeo/promos/may/card-annual.png',
+                    'title' => 'Drumeo',
                     'description' => '1 year of unlimited drum lessons',
                     'price' => 240,
-                    'customText' => '$200',
+                    'customText' => '$180',
+                    'customSubText' => 'true',
                     ],
                     [
-                    'image' => 'marketing/drumeo/promos/may/card-stick-bag.png',
-                    'title' => 'Drumeo StickBag',
-                    'description' => 'A StickBag you’ll want to show your friends.',
-                    'price' => floatval($productPrices['stickbag']->price),
+                    'image' => 'https://d1fyshwdvi6fth.cloudfront.net/Drumeo/Bundle-images/784dedb6-1b4b-4ce6-9940-a0e520c9d1b8-padstand-card.jpg',
+                    'title' => 'Drumeo PadStand',
+                    'description' => 'Practice anywhere with perfect height and ergonomics.',
+                    'price' => floatval($productPrices['padstand']->price),
                     'shipping' => true,
                     ],
                     [
-                    'image' => 'marketing/drumeo/membership/homepage/2024/drumsticks.webp',
+                    'image' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/420x0/filters:quality(95)/marketing/drumeo/membership/homepage/2024/drumsticks.webp',
                     'title' => 'Drumeo Drumsticks',
                     'description' => 'Drumeo 5A Drumsticks by Vater — made with hickory and extra moisture to last longer.',
                     'price' => floatval($productPrices['Drumeo-VaterSticks']->price),
                     'shipping' => true,
                     ],
                     [
-                    'image' => 'marketing/drumeo/promos/may/card-drum-key.png',
-                    'title' => 'Drumeo Drum Key',
-                    'description' => 'The Drumeo DrumKey sits perfectly in your hand with an ergonomic design and flared handle to help you tighten the most stubborn lugs.',
-                    'price' => floatval($productPrices['Drumeo-Key']->price),
+                        'image' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/420x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/quietpad-card.webp',
+                        'title' => 'QuietPad',
+                        'description' => 'Practice anywhere with two full-size playing surfaces.',
+                        'price' => floatval($productPrices['quietpad']->price),
+                        'shipping' => true,
+                    ],
+                    [
+                    'image' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/420x0/filters:quality(95)/marketing/drumeo/promos/summer-sale/30d-drummer.jpg',
+                    'title' => '30-Day Drummer',
+                    'description' => '30-Day Drummer gives you guided play-along workouts every day for thirty days.',
+                    'price' => floatval($productPrices['30-day-drummer-4']->price),
+                    ],
+                    [
+                    'image' => 'https://d1fyshwdvi6fth.cloudfront.net/Drumeo/Bundle-images/eaa92a5e-9269-4d51-873b-bc0124540a40-30d-chops-thumb.png',
+                    'title' => '30-Day Chops',
+                    'description' => '30-Day Chops is the first-ever course that teaches you tasty linear drum chops one note at a time.',
+                    'price' => floatval($productPrices['30-day-chops']->price),
                     'shipping' => true,
-                    ],
-                    [
-                    'image' => 'marketing/drumeo/promos/may/card-easy-rudiments.png',
-                    'title' => 'Easy Rudiments Book',
-                    'description' => 'The 15 Rudiments You Actually Need To Know (And How To Learn Them Quickly)',
-                    'price' => floatval($productPrices['easy-rudiments-book']->price),
-                    'shipping' => true,
-                    ],
-                    [
-                    'image' => 'marketing/drumeo/membership/homepage/2024/rdm.webp',
-                    'title' => 'Rock Drumming Masterclass',
-                    'description' => 'Todd Sucherman’s 26-week masterclass to help you improve your rock drumming.',
-                    'price' => floatval($productPrices['rock-drumming-masterclass-pack']->price),
-                    ],
-                    [
-                    'image' => 'marketing/drumeo/membership/homepage/2024/dtme.webp',
-                    'title' => 'Drum Technique Made Easy',
-                    'description' => 'Bruce Becker’s 26-week masterclass to improve your hand & foot technique.',
-                    'price' => floatval($productPrices['drum-technique-made-easy-pack']->price),
-                    ],
-                    [
-                    'image' => 'marketing/drumeo/membership/homepage/2024/ime.webp',
-                    'title' => 'Independence Made Easy',
-                    'description' => 'Jared Falk’s 26-week masterclass to unlock your musicality and freedom on the drums.',
-                    'price' => floatval($productPrices['independence-made-easy-pack']->price),
                     ],
                 ];
             @endphp
@@ -247,9 +241,9 @@
                                         :class="{'opacity-0': !lazyLoad, 'opacity-100': lazyLoad}"
                                         x-intersect.once="lazyLoad = true">
                                         <picture class="absolute inset-0 w-full h-full object-cover">
-                                            <source srcset="https://d21q7xesnoiieh.cloudfront.net/fit-in/980x0/filters:quality(95)/{{ $bonus['image'] }}"
+                                            <source srcset="{{ $bonus['image'] }}"
                                                 media="(min-width: 640px)">
-                                            <img src="https://d21q7xesnoiieh.cloudfront.net/fit-in/420x0/filters:quality(95)/{{ $bonus['image'] }}"
+                                            <img src="{{ $bonus['image'] }}"
                                                 alt="Bonus Image"
                                                 class="w-full h-full object-cover opacity-0 transition-opacity"
                                                 loading="lazy"
@@ -279,14 +273,16 @@
                                     <s class="opacity-40">${{ $bonus['price'] }}</s>
                                 @endif
                                 @if(!empty($bonus['customText']))
-                                    <strong class="text-drumeo">{{ $bonus['customText'] }}</strong>
+                                    <strong class="text-musora">{{ $bonus['customText'] }}</strong>
                                 @else
-                                    <strong class="text-drumeo">FREE</strong>
+                                    <strong class="text-musora">FREE</strong>
                                 @endif
                                 <br>
-                                <em>
+                               <em>
                                     @if(!empty($bonus['shipping']))
                                         Free Shipping
+                                    @elseif(!empty($bonus['customSubText']))
+                                        Save {{$savePercentage}}%
                                     @else
                                         Online Access
                                     @endif
@@ -296,15 +292,18 @@
                     </div>
                 @endforeach
             </div>
-            <h2 class="leading-tight mt-6 mb-1"><s class="opacity-50">$240</s> <strong>$180</strong></h2>
-            <p class="mb-4 sm:mb-6"><strong class="text-drumeo">Save 25%</strong> for your first year. Renews at $240/yr.
+              <h2 class="leading-tight mt-6 mb-1">
+                    <s class="opacity-50">${{$originalPrice}}</s> <strong>${{$discountedPrice}}</strong> 
+                </h2>
+             <p class="mb-4 sm:mb-6"><strong class="text-musora">Save {{$savePercentage}}%</strong> for your first year. Renews at $240/yr.</p>
             </p>
-            <a role="link" aria-label=" Get Started" class="join  drumeo  mb-4 md:mb-5 w-full max-w-xs md:max-w-lg lg:max-w-3xl" style="padding: 20px 10px;" href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[stickbag]=1&products[Drumeo-VaterSticks]=1&products[easy-rudiments-book]=1&products[drum-technique-made-easy-pack]=1&products[rock-drumming-masterclass-pack]=1&products[independence-made-easy-pack]=1&locked=true&promo-code=special">
-                GET Started »
+            <a role="link" aria-label=" Get Started" class="join  drumeo  mb-4 md:mb-5 w-full max-w-xs md:max-w-lg lg:max-w-3xl" style="padding: 20px 10px;" 
+            href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[quietpad]=1&products[padstand]=1&products[Drumeo-VaterSticks]=1&products[30-day-drummer-3]=1&products[30-day-chops]=1&promo-code=summersalepromo&locked=true">
+                GET Started
             </a>
             <br>
-            <a role="link" class="inline-block mt-2" aria-label="Start a monthly membership" href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[drum-technique-made-easy-pack]=1&products[rock-drumming-masterclass-pack]=1&products[independence-made-easy-pack]=1&locked=true&promo-code=special">
-                <p><u><em>Trying to avoid VAT fees on physical items? Click here to just grab<br class="hidden sm:inline">  your discounted membership + 3 free digital lesson packs.</em></u></p></a>
+            <a role="link" class="inline-block mt-2" aria-label="Start a monthly membership" href="/ecommerce/add-to-cart?products[DLM-1-year]=1&products[30-day-drummer-3]=1&products[30-day-chops]=1&promo-code=summersalepromo&locked=true">
+                <p><u><em><strong>Trying to avoid VAT fees on physical items?</strong> Click here to just grab<br class="hidden sm:inline">  your discounted membership + 3 free digital lesson packs.</em></u></p></a>
         </div>
     </section>
 @endsection
@@ -314,7 +313,7 @@
     'countdownDate' => '2024-06-01 00:00:00',
     'promoVersion' => false
     ])
-    <script type="application/javascript">
+    {{-- <script type="application/javascript">
         document.addEventListener('DOMContentLoaded', function () {
             var stickyBar = document.querySelector('.promo-banner');
             window.addEventListener('scroll', function () {
@@ -331,7 +330,7 @@
                 }
             });
         });
-    </script>
+    </script> --}}
 @endsection
 
 
