a66de438818b6861494a53c68978a05f77f5783d
Awatanka <natalia.sokolova.ca@gmail.com>
2024-07-10 20:55:02 -0700

    Test

--- resources/marketing/views/_partials/components/shop/promo-shop-header.blade.php
+++ resources/marketing/views/_partials/components/shop/promo-shop-header.blade.php
@@ -37,19 +37,19 @@
                     <div class="h-12 mx-4 bg-[#00101D]" style="width:2px;"></div>
                     <div class="flex">
                         <div class="mr-4 sm:mr-6" x-show="timeLeft > 0 && day > 0">
-                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="day">00</div>
+                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="day">0</div>
                             <div class="text-xs font-semibold" x-text="dayText">DAYS</div>
                         </div>
                         <div class="mr-4 sm:mr-6" x-show="timeLeft > 0 && hour > 0">
-                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="hour">00</div>
+                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="hour">0</div>
                             <div class="text-xs font-semibold" x-text="hourText">HRS</div>
                         </div>
                         <div class="mr-4 sm:mr-6" x-show="timeLeft > 0">
-                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="minute">00</div>
+                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="minute">0</div>
                             <div class="text-xs font-semibold" x-text="minuteText">MIN</div>
                         </div>
                         <div x-show="timeLeft > 0">
-                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="second">00</div>
+                            <div class="text-2xl sm:text-3xl font-extrabold" x-text="second">0</div>
                             <div class="text-xs font-semibold" x-text="secondText">SEC</div>
                         </div>
                         <span x-cloak x-show="timeLeft < 0">A Limited Time Left!</span>
