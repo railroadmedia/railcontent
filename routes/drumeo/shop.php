627def4fa7793a365e36f5832cebaeee6062177a
Trent <trent.harms@gmail.com>
2024-07-11 11:17:33 -0700

    moving quietpad page

--- routes/drumeo/shop.php
+++ routes/drumeo/shop.php
@@ -27,7 +27,6 @@ Route::domain('{drumeoDomain}')->middleware(['web_public'])->group(function () {
             'new-drummers',
             'padstand',
             'quietpad',
-            'quietpad-colorburst',
             'quietkick',
             'rock-drumming-masterclass',
             'successful-drumming',
