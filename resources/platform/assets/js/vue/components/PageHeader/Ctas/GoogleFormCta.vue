1f431601998e9fc310184fc8e54e1a0aef85be08
peterminhlee <petereminhlee@gmail.com>
2024-07-12 07:43:10 -0700

    modal updates

--- resources/platform/assets/js/vue/components/PageHeader/Ctas/GoogleFormCta.vue
+++ resources/platform/assets/js/vue/components/PageHeader/Ctas/GoogleFormCta.vue
@@ -1,14 +1,16 @@
 <template>
     <PageHeaderCta v-bind="$attrs" :text="text" :faIconClass="faIconClass" showAllAlways iconPosition="right"
         @click="handleOpen" />
-    <InfoModal v-if="modalOpen" :modalId="modalId"
-        :classOverride="'tw-bg-white dark:tw-bg-[#081825] tw-border tw-border-[#445F74] dark:tw-border-[#445F74] tw-max-w-[654px]'"
-        :selfContained="true" :showOverlay="true" @onClose="handleClose">
-        <div class="tw-text-[#00101D] tw-flex tw-flex-col tw-rounded-[10px] tw-shadow tw-p-[30px] tw-relative tw-px-5 tw-pt-8"
-            style="background: white !important;">
-            <div class="tw-flex tw-flex-row tw-mb-[30px]">
-                <h1 class="subheading">Student Review Application</h1>
-            </div>
+    <InfoModal
+        v-if="modalOpen"
+        :modalId="modalId"
+        :classOverride="'tw-max-w-[654px]'"
+        :selfContained="true"
+        :showOverlay="true"
+        title="Student Review Application"
+        @onClose="handleClose"
+    >
+        <div class="tw-text-[#00101D] tw-flex tw-flex-col tw-rounded-[10px] tw-shadow tw-p-[30px] tw-relative tw-px-5 tw-pt-8 tw-bg-white">
             <div class="flex flex-column tw-w-full">
                 <StudentReviewForm :url="iframeSrc" height="600" />
             </div>
