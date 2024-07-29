bb5b3ded9801982ad1de5eda4561f20715df1d67
Awatanka <natalia.sokolova.ca@gmail.com>
2024-07-29 16:44:00 -0700

    Cta to leadgen better technique

--- resources/marketing/views/_partials/layout/global-lead-gen-lesson-layout.blade.php
+++ resources/marketing/views/_partials/layout/global-lead-gen-lesson-layout.blade.php
@@ -240,6 +240,18 @@
         ])
     @endif
 
+     @if($theme === 'pianote' && $leadgen->slug == 'piano-technique-essentials/lessons')
+        @include('drumeo.lead-gen.partials._learn-more', [
+            'bg' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/1800x0/filters:quality(95)/marketing/pianote/lead-gen/technique-essentials/sign-up-bg.webp',
+            'logo' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/marketing/pianote/lead-gen/technique-essentials/30DTBT-logo-light.webp',
+            'header' => 'What would happen if you learned piano from <br class="hidden md:block"> <strong>the best keyboardist in the world?</strong>',
+            'subheader' => 'Improve your piano technique with 30 days of guided lessons from Jordan Rudess.',
+            'btnLink' => '/shop/30-days-to-better-technique',
+            'btnText' => 'LEARN MORE',
+            'logoStyle' => 'h-24 md:h-36',
+        ])
+    @endif
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/marketing/js/modal.js') }}"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
