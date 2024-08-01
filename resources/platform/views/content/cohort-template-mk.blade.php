468604256130482e9ea2db9dedebab03a6cea8a6
Trent <trent.harms@gmail.com>
2024-08-01 11:30:56 -0700

    tweaking layouts

--- resources/platform/views/content/cohort-template-mk.blade.php
+++ resources/platform/views/content/cohort-template-mk.blade.php
@@ -6,9 +6,17 @@
 
 @section('content')
     @if($cohort['slug'] == '30-day-double-bass')
-        @include('drumeo.products.30-day-double-bass', [
-            'platformVersion' => true
-        ])
+        <link rel="stylesheet" href="{{ mix('marketing/css/app.css') }}">
+        <link href="{{ asset('/marketing/css/tailwind-helpers.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('/marketing/css/animate.css') }}">
+        <link href="{{ asset('/marketing/parcel/drumeo/30dd.css') }}" rel="stylesheet">
+        <div class="bg-white text-black">
+            @include('drumeo.products._30D-double-bass', [
+                'theme' => 'drumeo',
+                'platformVersion' => true
+            ])
+
+        </div>
     @endif
     @include('partials._railanalytics-brand-tracking-iframe')
 @endsection
