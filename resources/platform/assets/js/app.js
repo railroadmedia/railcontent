3af61809ec5c34df7b5586e0c0c50853d12bc917
Miguel Fondeur <miguelfondeur@gmail.com>
2024-08-06 15:18:27 +0000

    update

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -19,7 +19,8 @@ import AccountDetails from './Components/_Pages/Settings/AccountDetails.vue';
 import Artists from './Components/_Pages/Artists.vue';
 import ChildCatalog from './Components/_Pages/ChildCatalog.vue';
 import Cohort from './Components/_Pages/Cohort';
-import Home from './Components/_Pages/Home.vue';
+import Home from './Components/_Pages/Home/Home.vue';
+import HomePageLoader from './Components/_Pages/Home/HomePageLoader.vue';
 import LessonHistory from './Components/_Pages/LessonHistory';
 import LessonPlayback from './Components/_Pages/LessonPlayback';
 import Playlist from './Components/_Pages/Playlist';
@@ -181,6 +182,7 @@ app.component('AppContainer', AppContainer)
     .component('PageLoader', PageLoader)
     .component('PageContainer', PageContainer)
     .component('Home', Home)
+    .component('HomePageLoader', HomePageLoader)
     .component('HeaderCarousel', HeaderCarousel)
     .component('StaticHeader', StaticHeader)
     .component('CoachEvent', CoachEvent)
--- resources/platform/views/home/index.blade.php
+++ resources/platform/views/home/index.blade.php
@@ -5,44 +5,52 @@
 @endsection
 
 @section('content')
-    <home
-        :is-pack-only="false"
-        account-url="{{ user()->getDashboardUrl() }}"
-        calendar-id="{{ $calendarId }}"
-        :carousel="{{ json_encode($carousel) }}"
-        :coach-event="{{ $coachEvent }}"
-        :cohort-banner="{{ $cohortBanner }}"
-        content-endpoint="/railcontent/content"
-        continue-url="{{ url()->route('platform.lesson-history.in-progress') }}"
-        current-date="{{ $currentDate }}"
-        event-coach-profile-url="{{ $eventCoachProfileUrl }}"
-        :exists-cohort-banner="{{ $existsCohortBanner ? 'true' : 'false' }}"
-        :has-started-content="{{ $hasStartedLessons ? 'true' : 'false' }}"
-        :has-started-lessons="{{ $hasStartedLessons ? 'true' : 'false' }}"
-        :has-upcoming-events="{{ $hasUpcomingEvents ? 'true' : 'false' }}"
-        @if(count($hotForumTopics) > 0) 
-            :conversation-data="{{ json_encode($hotForumTopics) }}"
-        @endif
-        :is-a-member="{{ user()->isAMember() ? 'true' : 'false' }}"
-        :new-content="{{ $newContentJson }}"
-        new-content-url="{{ url()->route('platform.new-lessons') }}"
-        next-learning-path-level="{{ user()->getMethodLevel() }}"
-        :next-learning-path-progress-percent="{{ $nextLearningPathProgressPercent }}"
-        :recommended-content="{{ $recommendedContentJson }}"
-        recommended-content-url="{{ url()->route('platform.recommended-lessons') }}"
-        :started-content="{{ $startedContentJson }}"
-        :time-cutoff-minutes="{{ $timeCutoffMinutes }}"
-        :upcoming-events="{{ $upcomingEvents }}"
-        upcoming-url="{{ '/'.$brand.'/live' }}"
-        :users-list="{{ json_encode($usersList->results())  }}"
-        :user-metrics="{{ json_encode($userMetrics) }}"A
-        :workouts-content="{{ $workoutsContentJson }}"
-        workouts-content-url="{{ url()->route('platform.workouts') }}"
-        youtube-id="{{ $youtubeId }}"
-        :learning-paths="{{ json_encode($trialSection) }}"
-        :display-trial-section="{{ $displayTrialSection ? 'true' : 'false' }}"
-    ></home>
 
-@include('partials._railanalytics-brand-tracking-iframe')
+    <home-page-loader>
+        <template #loading>
+            <song-skeleton></song-skeleton>
+        </template>
+        <template #page="{ pageData }">
+            <home
+                :is-pack-only="false"
+                account-url="{{ user()->getDashboardUrl() }}"
+                calendar-id="{{ $calendarId }}"
+                :carousel="{{ json_encode($carousel) }}"
+                :coach-event="{{ $coachEvent }}"
+                :cohort-banner="{{ $cohortBanner }}"
+                content-endpoint="/railcontent/content"
+                continue-url="{{ url()->route('platform.lesson-history.in-progress') }}"
+                current-date="{{ $currentDate }}"
+                event-coach-profile-url="{{ $eventCoachProfileUrl }}"
+                :exists-cohort-banner="{{ $existsCohortBanner ? 'true' : 'false' }}"
+                :has-started-content="{{ $hasStartedLessons ? 'true' : 'false' }}"
+                :has-started-lessons="{{ $hasStartedLessons ? 'true' : 'false' }}"
+                :has-upcoming-events="{{ $hasUpcomingEvents ? 'true' : 'false' }}"
+                @if(count($hotForumTopics) > 0) 
+                    :conversation-data="{{ json_encode($hotForumTopics) }}"
+                @endif
+                :is-a-member="{{ user()->isAMember() ? 'true' : 'false' }}"
+                :new-content="{{ $newContentJson }}"
+                new-content-url="{{ url()->route('platform.new-lessons') }}"
+                next-learning-path-level="{{ user()->getMethodLevel() }}"
+                :next-learning-path-progress-percent="{{ $nextLearningPathProgressPercent }}"
+                :recommended-content="{{ $recommendedContentJson }}"
+                recommended-content-url="{{ url()->route('platform.recommended-lessons') }}"
+                :started-content="{{ $startedContentJson }}"
+                :time-cutoff-minutes="{{ $timeCutoffMinutes }}"
+                :upcoming-events="{{ $upcomingEvents }}"
+                upcoming-url="{{ '/'.$brand.'/live' }}"
+                :users-list="{{ json_encode($usersList->results())  }}"
+                :user-metrics="{{ json_encode($userMetrics) }}"A
+                :workouts-content="{{ $workoutsContentJson }}"
+                workouts-content-url="{{ url()->route('platform.workouts') }}"
+                youtube-id="{{ $youtubeId }}"
+                :learning-paths="{{ json_encode($trialSection) }}"
+                :display-trial-section="{{ $displayTrialSection ? 'true' : 'false' }}"
+            ></home>
+        </template>
+    </home-page-loader>
+
+    @include('partials._railanalytics-brand-tracking-iframe')
 
 @endsection
