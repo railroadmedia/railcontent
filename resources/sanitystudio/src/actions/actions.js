35665186537533e3f60d2f153646435ca7ff945a
Roxana Riza <roxana_r2000@yahoo.com>
2024-08-07 17:02:51 +0300

    BE-271 - Update chid_count when a new child(lesson) is added

--- resources/sanitystudio/src/actions/actions.js
+++ resources/sanitystudio/src/actions/actions.js
@@ -2,33 +2,18 @@ import {useDocumentOperation} from 'sanity'
 
 export function CreateImprovedAction(originalPublishAction, token, context) {
     const BetterAction = (props) => {
-        const { document, getClient } = context;
         const originalResult = originalPublishAction(props)
         // eslint-disable-next-line
         const {patch, publish} = useDocumentOperation(props.id, props.type)
         const sanityConfig = window.sanityConfig.find(item => item.name == 'publishing-workspace');
         const url = sanityConfig.appUrl + `/admin/last-content`;
-        const client = getClient({ apiVersion: '2022-12-07' });
-        console.log('roxana publish action before check::::');
+
         return {
             ...originalResult,
             onHandle: async () => {
-                // Sync railcontent with new content and sync railcontent_id and web_url_path
-                if (props.parent) {
-                    const params = {
-                        id: props.parent._ref,
-                    };
-                    // Construct the query based on the presence of the brand field
-                    let query = `*[_id == $id]`;
-//                     if (document.parent) {
-//                         params.parent = document.parent._ref;
-//                         query = `*[_type == $type && parent._ref == $parent && slug.current == $slug && !(_id in [$id, $draft_id])]`;
-//                     } else if (document.brand) {
-//                         params.brand = document.brand;
-//                         query = `*[_type == $type && brand == $brand && slug.current == $slug && !(_id in [$id, $draft_id])]`;
-//                     }
-                    const documents = await client.fetch(query, params);
-                    console.log('roxana publish action::::', props, documents);
+                if (("child" in props.draft) && props.draft.child.length != props.draft.child_count) {
+                    props.draft.child_count = props.draft.child.length;
+                    patch.execute([{set: {child_count: props.draft.child.length}}])
                 }
                 fetch(url, {
                     method:  'POST',
