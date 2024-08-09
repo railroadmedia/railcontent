16fa913842c309c4aa302519e9a5f7fbb7caa875
peterminhlee <petereminhlee@gmail.com>
2024-08-09 12:42:33 -0700

    Draft label comp

---
+++ resources/platform/assets/js/Components/_Units/DraftLabel/DraftLabel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        class="tw-text-white tw-absolute tw-z-20 tw-font-extrabold tw-text-xl tw-rounded-xl tw-bg-red-700 tw-px-6 tw-py-1"
+        :class="position ? position : 'tw-right-2 md:tw-right-6 tw-top-2 md:tw-top-6'"
+    >
+        DRAFT
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    position: {
+        type:String,
+        default: '',
+    }
+})
+</script>
