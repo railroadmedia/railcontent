14e8e0c6ee1e3e942ffee61e7c4d035e26a783ab
peterminhlee <petereminhlee@gmail.com>
2024-08-13 12:07:46 -0700

    fix

--- resources/platform/assets/js/Components/_Pages/LessonPlayback.vue
+++ resources/platform/assets/js/Components/_Pages/LessonPlayback.vue
@@ -401,7 +401,6 @@ const thumbnailUrl = computed(() => {
 })
 
 const showDraftLabel = computed(() => {
-    return !noAccess && Date.now() > DateTime.fromSQL(props.lessonData.published_on, { zone: 'UTC' }).toFormat('x');
+    return !noAccess.value && Date.now() < DateTime.fromSQL(props.lessonData.published_on, { zone: 'UTC' }).toFormat('x');
 })
-
 </script>
