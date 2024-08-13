d49811903f387ac9fe9365ffbbd2173118830810
peterminhlee <petereminhlee@gmail.com>
2024-08-13 11:04:41 -0700

    fix

--- resources/platform/assets/js/Components/_Pages/WorkoutsPlayback.vue
+++ resources/platform/assets/js/Components/_Pages/WorkoutsPlayback.vue
@@ -376,6 +376,6 @@ const noAccess = computed(() => {
 })
 
 const showDraftLabel = computed(() => {
-    return !noAccess && Date.now() > DateTime.fromSQL(props.lessonData.published_on, { zone: 'UTC' }).toFormat('x');
+    return !noAccess.value && Date.now() > DateTime.fromSQL(props.lessonData.published_on, { zone: 'UTC' }).toFormat('x');
 })
 </script>
