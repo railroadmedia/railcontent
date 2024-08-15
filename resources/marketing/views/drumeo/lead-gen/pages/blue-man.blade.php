1c828e169ddb761933b8d3e31b7a9cddcfb6502b
Trent <trent.harms@gmail.com>
2024-08-15 15:27:02 -0700

    quick tweaks

--- resources/marketing/views/drumeo/lead-gen/pages/blue-man.blade.php
+++ resources/marketing/views/drumeo/lead-gen/pages/blue-man.blade.php
@@ -28,18 +28,20 @@
 
     <section class="py-12 sm:py-14 lg:py-20 px-4 text-center" style="background: linear-gradient(0deg, #FFF2D4, #3EC8FF);">
         <div class="container max-w-5xl mx-auto">
-            <img style="height:550px;" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp">
-            <p class="leading-tight text-drumeo tracking-widest mt-7">Exclusive drumeo member offer</p>
+            <img style="height:500px;" src="https://placehold.co/630x500/EEE/31343C">
+{{--            <img style="height:550px;" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp">--}}
+            <p class="uppercase leading-tight text-drumeo tracking-widest mt-7">Exclusive drumeo member offer</p>
             <h2 class="leading-tight font-black my-2">SAVE 30% on tickets in 4 US cities</h2>
             <p class="leading-tight mb-7">Available August 26 to October 29.</p>
             <a class="anchor-slide join drumeo smaller" href="#final">Select Your City</a>
         </div>
     </section>
     <section class="px-4 sm:px-6 py-10 sm:py-16 lg:py-20">
-        <div class="container mx-auto max-w-5xl">
-            <div class="flex flex-wrap sm:flex-nowrap">
-                <img class="w-full sm:w-auto h-auto sm:h-96 sm:rounded-xl" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp">
-                <div class="px-6 sm:pr-0 sm:pl-7 lg:pl-14 mt-7 sm:mt-0 sm:order-1">
+        <div class="container mx-auto max-w-4xl">
+            <div class="flex flex-wrap sm:flex-nowrap items-center">
+                <img class="w-full sm:w-auto h-auto sm:h-96 sm:rounded-xl" src="https://placehold.co/580x600/EEE/31343C">
+{{--                <img class="w-full sm:w-auto h-auto sm:h-96 sm:rounded-xl" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp">--}}
+                <div class="px-6 sm:pr-0 sm:pl-7 lg:pl-10 mt-7 sm:mt-0 sm:order-1">
                     <h3 class="leading-tight mb-4"><strong>Drums, world-class performances, the color blue…</strong></h3>
                     <p class="leading-normal">The Blue Man Group and Drumeo have too much in common to NOT combine forces. 
                         <br><br>
@@ -56,7 +58,7 @@
     <div id="final" class="anchor"></div>
     <section class="px-5 sm:px-6 py-10 sm:py-14 lg:py-20" style="background-color:#FFF8EB;">
         <div class="max-w-5xl mx-auto">
-            <h3 class="mb-4 text-center mb-2 md:mb-3"><strong>Select a city to see your show</strong></h3>
+            <h2 class="text-center mb-7 sm:mb-10"><strong>Select a city to see your show</strong></h2>
             <div
                 x-data="{
                     init() {
@@ -81,31 +83,31 @@
                     },
                 }"
             >
-                <div x-ref="splide" class="splide sm:mb-9">
+                <div x-ref="splide" class="splide mb-10">
                     <div class="splide__track">
-                        <ul class="splide__list items-start" style="padding-top: 60px !important;">
+                        <ul class="splide__list items-start">
                             <li class="splide__slide px-2">
                                 <a href="">
-                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')"
-                                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />
+                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://placehold.co/450x450/EEE/31343C" alt="city" />
+{{--                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />--}}
                                 </a>
                             </li>
                             <li class="splide__slide px-2">
                                 <a href="">
-                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')"
-                                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />
+                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://placehold.co/450x450/EEE/31343C" alt="city" />
+{{--                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />--}}
                                 </a>
                             </li>
                             <li class="splide__slide px-2">
                                 <a href="">
-                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')"
-                                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />
+                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://placehold.co/450x450/EEE/31343C" alt="city" />
+{{--                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />--}}
                                 </a>
                             </li>
                             <li class="splide__slide px-2">
                                 <a href="">
-                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')"
-                                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />
+                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://placehold.co/450x450/EEE/31343C" alt="city" />
+{{--                                    <img class="rounded-xl transition-opacity opacity-0" loading="lazy" onload="this.classList.remove('opacity-0')" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/drumeo/lead-gen/double-bass-101/header.webp" alt="city" />--}}
                                 </a>
                             </li>
                         </ul>
