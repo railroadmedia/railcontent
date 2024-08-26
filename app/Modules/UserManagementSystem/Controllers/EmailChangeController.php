6b685b41791554b97b6eac72436d7ece96f39d84
Alexandre Severo <alexandre.severo@me.com>
2024-08-26 08:34:05 -0700

    feat(BE-195): Remove UUID usage for CustomerIO (#1053)
    * feat(BE-195): Remove UUID usage for CustomerIO
    
    * Clean up CIO gateway
    
    * clean up typing
    
    * Clean up
    
    * don't trigger unnecessary updates
    
    * Update app/Modules/CustomerIO/Services/CustomerIoService.php
    
    Co-authored-by: Brandon <brandonk@musora.com>
    
    * Refactor solution to make it synchronous
    
    * Update app/Modules/UserManagementSystem/tests/Feature/Controllers/EmailChangeControllerTest.php
    
    Co-authored-by: Brandon <brandonk@musora.com>
    
    * Revert back to UserUpdated and handle it manually
    
    * Address PR comments
    
    * Fix test
    
    * fix email sync
    
    * fix email update
    
    * update cio id from musora-center
    
    * fix dependencies
    
    * 2.4.1
    
    * update musora center
    
    * only update user if there's data
    
    * fix auth check
    
    * fix access permissions
    
    * musoracenter to user userupdated event
    
    * update tests
    
    ---------
    
    Co-authored-by: Alexandre Severo <alexandre@musora.com>
    Co-authored-by: Brandon <brandonk@musora.com>

--- app/Modules/UserManagementSystem/Controllers/EmailChangeController.php
+++ app/Modules/UserManagementSystem/Controllers/EmailChangeController.php
@@ -4,7 +4,6 @@ namespace Modules\UserManagementSystem\Controllers;
 
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Validation\Rule;
-use Modules\UserManagementSystem\Events\User\UserUpdated;
 use Carbon\Carbon;
 use Illuminate\Contracts\Hashing\Hasher;
 use Illuminate\Http\RedirectResponse;
@@ -17,26 +16,19 @@ use Illuminate\Notifications\AnonymousNotifiable;
 use Modules\UserManagementSystem\Models\EmailChange;
 use Modules\UserManagementSystem\Models\User;
 use Modules\UserManagementSystem\Events\EmailChangeRequest;
-use Modules\UserManagementSystem\Notifications\EmailChange as EmailChangeNotification;
+use Modules\UserManagementSystem\Events\User\UserUpdated;
 
 class EmailChangeController extends Controller
 {
     use ValidatesRequests;
 
     /**
-     * @var Hasher
-     */
-    private $hasher;
-
-    /**
      * EmailChangeController constructor.
      *
      * @param Hasher $hasher
      */
-    public function __construct(
-        Hasher $hasher,
-    ) {
-        $this->hasher = $hasher;
+    public function __construct(private Hasher $hasher)
+    {
     }
 
     /**
@@ -139,11 +131,11 @@ class EmailChangeController extends Controller
 
         return $request->has('redirect') ?
             redirect()
-                ->away($request->get('redirect'))
-                ->with($message) :
+            ->away($request->get('redirect'))
+            ->with($message) :
             redirect()
-                ->back()
-                ->with($message);
+            ->back()
+            ->with($message);
     }
 
     /**
@@ -184,22 +176,23 @@ class EmailChangeController extends Controller
             return redirect()->back()->withErrors(['error-message' => 'Token is invalid']);
         }
 
-        if (Carbon::parse(
-            $emailChange->updated_at
+        if (
+            Carbon::parse(
+                $emailChange->updated_at
                     ->format('Y-m-d H:i:s')
-        ) <
+            ) <
             Carbon::now()
-                ->subHours(config('user_management_system.email_change_token_ttl'))) {
+            ->subHours(config('user_management_system.email_change_token_ttl'))
+        ) {
             // todo: error message does not appear
             return redirect()
                 ->back()
                 ->withErrors(['error-message' => 'Your email reset code has expired.']);
-
         }
 
         $user = User::find($emailChange->user_id);
 
-        $oldUser = clone($user);
+        $oldUser = clone ($user);
 
         $user->email = $emailChange->email;
         $user->save();
@@ -215,16 +208,16 @@ class EmailChangeController extends Controller
 
         return $request->has('redirect_to') ?
             redirect()
-                ->away($request->get('redirect'))
-                ->with($message) :
+            ->away($request->get('redirect'))
+            ->with($message) :
             redirect()
-                ->to(
-                    route('platform.profile.settings.login-credentials', [
-                        'userId' => $user->id,
-                        'brand' => $emailChange->brand
-                    ])
-                )
-                ->with($message);
+            ->to(
+                route('platform.profile.settings.login-credentials', [
+                    'userId' => $user->id,
+                    'brand' => $emailChange->brand
+                ])
+            )
+            ->with($message);
     }
 
     /**
