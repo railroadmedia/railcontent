4a487865e0796ba2a8314a8a4f0d77e809e5109a
Trent <trent.harms@gmail.com>
2024-08-26 11:38:16 -0700

    adding new bundle

--- app/Http/Controllers/Pianote/SalesController.php
+++ app/Http/Controllers/Pianote/SalesController.php
@@ -50,6 +50,10 @@ class SalesController extends BaseController
     {
         return view('pianote.sales.ultimate-practice', ['theme' => 'pianote', 'smallPromoBanner' => 'true',]);
     }
+    public function backToSchool()
+    {
+        return view('pianote.sales.back-to-school', ['theme' => 'pianote', 'smallPromoBanner' => 'true',]);
+    }
     public function promoEG()
     {
         return view('pianote.sales.subscription', ['theme' => 'pianote', 'promoVersion' => 'true', 'evergreenVersion' => 'true']);
