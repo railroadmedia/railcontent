574c648054745d9ba7d49b05cff70b976c85d389
calebfavor <calebfavor@gmail.com>
2024-08-27 16:10:25 -0700

    Fixing up syntax error.

--- app/Modules/Content/Models/Content.php
+++ app/Modules/Content/Models/Content.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Modules\Content\Builders\ContentBuilder;
 use App\Modules\Content\database\factories\ContentFactory;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
