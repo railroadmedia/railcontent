cc558ea9c29e00135cb61bdfc321947a9a1bf4cb
Trent <trent.harms@gmail.com>
2024-08-28 10:51:14 -0700

    adding second playlist signup

--- config/lead-tracker.php
+++ config/lead-tracker.php
@@ -1793,5 +1793,22 @@ return [
                 'customer_io_event_name' => 'leadtracker_customer_io_event_name',
             ],
         ],
+        [
+            'path' => '/customer-io/submit-email-form-rc',
+            'method' => 'post',
+            'form_name' => 'The Playlist - Musora Newsletter',
+            'brand' => 'musora',
+
+            'input_data_map' => [
+                'email' => 'email',
+                'form_name' => 'form_name',
+                'utm_source' => 'leadtracker_utm_source',
+                'utm_medium' => 'leadtracker_utm_medium',
+                'utm_campaign' => 'leadtracker_utm_campaign',
+                'utm_term' => 'leadtracker_utm_term',
+                'customer_io_customer_id' => 'leadtracker_customer_io_customer_id',
+                'customer_io_event_name' => 'leadtracker_customer_io_event_name',
+            ],
+        ],
     ],
 ];
