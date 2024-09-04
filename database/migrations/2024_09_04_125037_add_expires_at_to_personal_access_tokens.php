06e48833164b79c2ab478ef97258017e1bd11cf9
Brandon Kerr <brandonk@musora.com>
2024-09-04 12:40:38 -0400

    Add rollback for personal_access_tokens expires_at

--- database/migrations/2024_09_04_125037_add_expires_at_to_personal_access_tokens.php
+++ database/migrations/2024_09_04_125037_add_expires_at_to_personal_access_tokens.php
@@ -21,7 +21,7 @@ return new class () extends Migration {
     public function down(): void
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
-            //
+            $table->dropColumn('expires_at');
         });
     }
 };
