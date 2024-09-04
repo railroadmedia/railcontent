0e149bc03e375159e835878430ce0218f143a5d6
Trent <trent.harms@gmail.com>
2024-09-04 10:24:41 -0700

    adding separate route

--- routes/drumeo/sales.php
+++ routes/drumeo/sales.php
@@ -55,6 +55,7 @@ Route::domain('{drumeoDomain}')
         Route::get('/trial-key', [SalesController::class, 'trialKey']);
         Route::get('/practice-anywhere', [SalesController::class, 'practiceAnywhere']);
         Route::get('/guitarcenter', [SalesController::class, 'guitarcenter']);
+        Route::get('/GuitarCenter', [SalesController::class, 'guitarcenter']);
         Route::get('/welcome-back-discount', [SalesController::class, 'welcomeBackDiscount']);
         Route::get('/trial', [SalesController::class, 'trial']);
         Route::get('/trial2', [SalesController::class, 'trial']);
