9896b01e8d7c09e4a86e9258e8206dc33309cef2
peterminhlee <petereminhlee@gmail.com>
2024-09-12 07:37:08 -0700

    href updates

--- resources/platform/assets/js/Components/_Collections/GroupedCard/GroupedCard.vue
+++ resources/platform/assets/js/Components/_Collections/GroupedCard/GroupedCard.vue
@@ -3,7 +3,7 @@
     <SkeletonLoader v-if="collectionStoreLoading || isLoading" type="card-group-header" />
     <!--  Instructor Thumbnail  -->
     <div v-else class="tw-flex tw-justify-between tw-items-center tw-mb-4">
-        <a :href="item.url"
+        <a :href="item.web_url_path"
             class="tw-flex tw-items-center tw-text-[#00101D] dark:tw-text-white hover:tw-underline"
             style="text-underline-offset: 6px;">
             <img v-if="thumb" class="tw-rounded-full tw-w-20 tw-h-20 tw-border-2 tw-border-white tw-border-solid tw-mr-[10px]"
@@ -17,7 +17,7 @@
 
             </div>
         </a>
-        <a :href="item.url"
+        <a :href="item.web_url_path"
             class="tw-text-[#00101D] dark:tw-text-white tw-uppercase tw-font-bebas-neue xl:tw-text-lg hover:tw-underline"
             style="text-underline-offset: 6px;">
             See All
