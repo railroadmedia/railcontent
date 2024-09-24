722ad863d798f308c36b75de3c583f135465733c
Trent <trent.harms@gmail.com>
2024-09-24 13:05:37 -0700

    fixing typo

--- resources/marketing/views/drumeo/sales/trials/blue-man-trial.blade.php
+++ resources/marketing/views/drumeo/sales/trials/blue-man-trial.blade.php
@@ -15,7 +15,7 @@
                 <img class="avatar mx-auto mb-3 sm:mb-0 w-48 md:w-52 lg:w-72" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/600x0/filters:quality(95)/marketing/drumeo/lead-gen/blue-man/logo2.png">
                 <div class="md:text-left px-2 md:pl-7 lg:pl-10">
                     <h2 class="uppercase"><strong>Blue Man Group: Unlimited Drum Lessons For 30 Days</strong></h2>
-                    <h6 class="leading-normal my-3 md:my-5">Drumeo and Blue Man Group have teamed up to help you learn the world's greatest instrument. For a limited time, you can grab your first 30 days of drum lessons FREE. Inside Drumeo, you'll find all the lessons, songs and support you need to get starts or take your drumming to the next level. Scroll down to redeem your offer.</h6>
+                    <h6 class="leading-normal my-3 md:my-5">Drumeo and Blue Man Group have teamed up to help you learn the world's greatest instrument. For a limited time, you can grab your first 30 days of drum lessons FREE. Inside Drumeo, you'll find all the lessons, songs and support you need to get started or take your drumming to the next level. Scroll down to redeem your offer.</h6>
                     <a href="/choose-your-trial-month" class="join smaller musora">START MY FREE TRIAL <i class="fas fa-arrow-right" style="line-height: 0;" aria-hidden="true"></i></a>
                 </div>
             </div>
