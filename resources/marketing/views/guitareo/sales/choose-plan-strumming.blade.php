c8dea00c81729a3731457020f5c7151ffc58947b
Trent <trent.harms@gmail.com>
2024-09-26 15:23:59 -0700

    fixing capitals

--- resources/marketing/views/guitareo/sales/choose-plan-strumming.blade.php
+++ resources/marketing/views/guitareo/sales/choose-plan-strumming.blade.php
@@ -43,11 +43,11 @@
      @include('musora.sales.components.card-selection-section', [
             "altHeadline" => '
             <h3 class="leading-tight">Start A Free Trial To Continue <strong>30 Days To Better<br class="hidden sm:inline"> Strumming</strong> & Get <strong>Unlimited Access</strong> To Guitareo</h3>
-
-<p class="mt-2 md:mt-4 mb-5 text-left mx-auto w-auto inline-block">1. Select Your Membership Type To Start A Free Trial<br>
-2. Complete The Checkout (You won’t be charged until after your trial & you can cancel at any time)<br>
-3. Jump back into 30 Days to Better Strumming and explore all of Guitero’s courses, workouts,<br class="hidden sm:inline"> challenges, song breakdowns & more! </p>
-            ',
+<p class="mt-2 md:mt-4 mb-5 text-left mx-auto w-auto inline-block">
+1. Select your membership type to start a free trial.<br>
+2. Complete the checkout (you won’t be charged until after your trial & you can cancel at any time)<br>
+3. Jump back into 30 Days to better strumming and explore all of Guitero’s courses, workouts,<br class="hidden sm:inline"> challenges, song breakdowns & more!
+</p>',
             "plusLogo" => "https://d122ay5chh2hr5.cloudfront.net/sales/2023/guitareo-plus-logo-light.svg",
             "logo" => "https://d21q7xesnoiieh.cloudfront.net/fit-in/400x0/filters:quality(95)/marketing/guitareo/membership/homepage/webp-format/guitareo-logo.webp",
             "songs" => "500+ popular songs.",
