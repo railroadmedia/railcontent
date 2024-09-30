a3558251ba13dc0b41e509fb8b087874a889941c
Roxana Riza <roxana_r2000@yahoo.com>
2024-09-30 10:36:09 +0300

    New show type: Odd Times With Aaron Edgar

---
+++ app/Modules/Content/Models/Sanity/Shows/OddTimes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Modules\Content\Models\Sanity\Shows;
+
+use App\Modules\Content\Models\Sanity\Enums\FieldType;
+use App\Modules\Content\Models\Sanity\Enums\VideoType;
+use App\Modules\Content\Models\Sanity\LessonTemplate;
+use App\Modules\Content\Models\Sanity\Structure\Field;
+use App\Modules\Content\Models\Sanity\Structure\Group;
+use App\Modules\Content\Models\Sanity\Structure\ListItemPreview;
+use App\Modules\Content\Models\Sanity\Structure\Reference;
+use Modules\Content\Models\Sanity\Structure\Block;
+use Modules\Content\Models\Sanity\Structure\BrandField;
+use Modules\Content\Models\Sanity\Structure\ListObject;
+
+/**
+ * Defines the schema structure for an Odd Times with Aaron Edgar document type in Sanity.
+ *
+ * @property string       $type
+ * @property string       $name
+ * @property string       $title
+ * @property ?string      $icon
+ * @property array<Field> $fields
+ */
+class OddTimes extends LessonTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(self::getName(), 'Odd Times With Aaron Edgar');
+    }
+    public static function getName(): string
+    {
+        return 'odd-times';
+    }
+}
