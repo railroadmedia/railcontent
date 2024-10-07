d839800e07474220dcac34c51fb955d152fead3a
peterminhlee <petereminhlee@gmail.com>
2024-10-07 07:28:20 -0700

    added text color

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeLeaveModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeLeaveModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <InfoModal :selfContained="true" class-override="tw-max-w-[533px] tw-w-full" title="Are you sure you want o leave 30-Day Drummer?">
+    <InfoModal :selfContained="true" class-override="tw-max-w-[533px] tw-w-full dark:tw-text-white" title="Are you sure you want o leave 30-Day Drummer?">
         <p class="tw-mb-5">Leaving 30-Day drummer will delete your progress.</p>
         <div class="tw-flex tw-justify-end">
             <MuButton>Leave Challenge</MuButton>
