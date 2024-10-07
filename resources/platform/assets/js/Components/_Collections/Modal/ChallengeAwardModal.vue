e70b70f662c3a5d6b8feba702e583dabbff35876
peterminhlee <petereminhlee@gmail.com>
2024-10-07 07:29:33 -0700

    added lottie styles

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
@@ -2,7 +2,7 @@
     <ModalRenderer :black-background="true" :show-x-icon="true">
         <div class="tw-max-w-[470px] tw-w-full tw-rounded-[10px] tw-px-4 sm:tw-px-14 tw-py-10 tw-relative tw-border dark:tw-border-[rgba(255,255,255,0.09)] tw-mx-4 sm:tw-mx-0">
             <!-- Animation -->
-            <Vue3Lottie v-if="!hideAnimation" class="tw-w-[800px] tw-h-[800px] tw-absolute tw-top-1/2 tw-left-1/2 -tw-translate-x-1/2 -tw-translate-y-1/2 tw-z-[5]" :animation-link="animations[brand]" width="100%" height="100%" :loop="false" />
+            <Vue3Lottie v-if="!hideAnimation" class="tw-w-[calc(100% + 200px)] sm:tw-w-[800px] tw-h-[800px] tw-absolute tw-top-1/2 tw-left-1/2 -tw-translate-x-1/2 -tw-translate-y-1/2 tw-z-[5]" :animation-link="animations[brand]" width="100%" height="100%" :loop="false" />
 
             <!-- BG image -->
             <img class="tw-absolute tw-w-full tw-h-full tw-left-0 tw-top-0 tw-hidden dark:tw-block tw-z-0" src="https://www.musora.com/musora-cdn/image/width=500,quality=95/https://d3fzm1tzeyr5n3.cloudfront.net/challenges/award-dark-bg.png" />
