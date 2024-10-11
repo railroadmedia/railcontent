720fe05495d62514870a31506f167f5c1cce7bcd
peterminhlee <petereminhlee@gmail.com>
2024-10-11 13:07:26 -0700

    unlock challenge cta for header

---
+++ resources/platform/assets/js/Components/_Collections/PageHeader/Ctas/UnlockChallengeCta.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <PageHeaderCta
+            v-bind="$attrs"
+            text="Unlock"
+            showAllAlways
+            @click="handleOpen"
+        />
+        <ChallengeActionModal v-if="modalOpen" modal-type="unlock" @close-modal="handleClose" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PageHeaderCta from '@collections/PageHeader/PageHeaderCta';
+import ChallengeActionModal from '@collections/Modal/ChallengeActionModal';
+
+const props = defineProps({
+    text: String,
+    emailLogo: String,
+    emailRecipient: String,
+});
+
+const modalOpen = ref(false);
+
+const handleOpen = () => {
+    modalOpen.value = true;
+};
+
+const handleClose = () => {
+    modalOpen.value = false;
+};
+</script>
