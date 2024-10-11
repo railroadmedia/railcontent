5c1a96f4d8f640984e357abf5983799b39a34ae5
peterminhlee <petereminhlee@gmail.com>
2024-10-11 08:44:30 -0700

    integration with BE

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeNotificationModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeNotificationModal.vue
@@ -1,15 +1,18 @@
 <template>
-    <InfoModal :selfContained="true" class-override="tw-max-w-[510px] tw-w-full">
+    <InfoModal :selfContained="true" class-override="tw-max-w-[510px] tw-w-full" @onClose="emit('modalClose')">
         <div class="tw-flex tw-flex-col tw-justify-center -tw-mt-[50px] dark:tw-text-white tw-text-center">
-            <img class="tw-h-20 tw-mb-5" src="https://www.musora.com/musora-cdn/image/width=300,quality=95/https://d1923uyy6spedc.cloudfront.net/30DayDrummer-Logos-07-1702425574.svg" />
+            <!-- Dark mode Logo -->
+            <img class="tw-h-20 tw-mb-5 tw-hidden dark:tw-block" :src="`https://www.musora.com/musora-cdn/image/width=300,quality=95/${challenge.dark_mode_logo}}`" />
+            <!-- Light mode Logo -->
+            <img class="tw-h-20 tw-mb-5 dark:tw-hidden" :src="`https://www.musora.com/musora-cdn/image/width=300,quality=95/${challenge.light_mode_logo}`" />
 
             <!-- Step 1 -->
             <template v-if="step === 1">
                 <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">You're enrolled in 30-Day Drummer!</h1>
-                <p class="tw-mb-5">Next, select your notification frequency for this Challenges.</p>
+                <p class="tw-mb-5 tw-text-left">Next, choose if you would like to receive practice reminders for this Challenge (make sure you download the app to get push notifications).</p>
                 <ul class="tw-text-left">
-                    <li class="tw-mb-5"><input class="tw-mr-[10px]" type="radio" value="daily" v-model="selectedFrequency" /> <label>Receive daily reminders to practice</label></li>
-                    <li><input class="tw-mr-[10px]" type="radio" value="no" v-model="selectedFrequency" /> <label>Receive no reminders to practice</label></li>
+                    <li class="tw-mb-5"><input class="tw-mr-[10px]" type="radio" value="true" v-model="selectedFrequency" /> <label>Yes, send me practice reminders!</label></li>
+                    <li><input class="tw-mr-[10px]" type="radio" value="false" v-model="selectedFrequency" /> <label>No thanks, I’m all good!</label></li>
                 </ul>
                 <div class="tw-flex tw-justify-end tw-mt-[30px]">
                     <MuButton :disabled="!selectedFrequency" @click="handleNext">Next</MuButton>
@@ -20,10 +23,10 @@
             <template v-if="step === 2 && challengeType === 'solo'">
                 <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">Choose Your Start Date</h1>
                 <div class="tw-mx-auto">
-                    <Datepicker v-model="selectedDate" :start-date="selectedDate" inline :enable-time-picker="false" :action-row="{ showCancel: false, showSelect: false, showPreview: false }" position="center" @internal-model-change="handleDateChange" />
+                    <Datepicker :start-date="selectedDate" inline :enable-time-picker="false" :action-row="{ showCancel: false, showSelect: false, showPreview: false }" position="center" @internal-model-change="handleDateChange" />
                 </div>
                 <div class="tw-flex tw-justify-end tw-mt-[30px]">
-                    <MuButton :disabled="!selectedDate" @click="handleStartNow">Start Now</MuButton>
+                    <MuButton :disabled="!selectedDate" @click="setStartDate">{{ setStartDateButtonText }}</MuButton>
                 </div>
             </template>
 
@@ -32,11 +35,10 @@
                 <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">30-Day Drummer starts on August 20</h1>
                 <div class="tw-mb-[10px] tw-flex tw-justify-center">
                     <!-- Avatars -->
-                    <div class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-relative tw-overflow-hidden"></div>
-                    <div class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-relative tw-overflow-hidden -tw-ml-3"></div>
-                    <div class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-relative tw-overflow-hidden -tw-ml-3"></div>
+                    <div v-for="(avatar, index) in challengeData.entity" class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-overflow-hidden tw-bg-cover tw-bg-center" :class="index !== 0 ? '-tw-ml-3' : ''" :style="`background-image: url('${avatar.profile_picture_url}')`"></div>
+                    <div class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-overflow-hidden tw-bg-cover tw-bg-center -tw-ml-3 tw-transition-all tw-duration-1000" :class="slideIn ? '' : 'tw-absolute tw-opacity-0 tw-translate-x-10'" :style="`background-image: url('${userProfilePictureUrl}')`"></div>
                 </div>
-                <p>You’ve joined <span class="tw-font-bold">Stidger, Poco Askew, Dr Mojo,</span> and <span class="tw-font-bold">683</span> other drummers who have already enrolled!</p>
+                <p class="tw-text-left">You’ve joined <span class="tw-font-bold">{{ userNames }}</span> and <span class="tw-font-bold">{{ challengeData.total }}</span> other drummers who have already enrolled!</p>
                 <div class="tw-flex tw-justify-end tw-mt-[30px]">
                     <MuButton variant="secondary" class="tw-mr-[9px]">View Challenge</MuButton>
                     <MuButton>Go Home</MuButton>
@@ -46,43 +48,106 @@
     </InfoModal>
 </template>
 <script setup>
-import { ref } from 'vue';
-import InfoModal from '@collections/Modal/InfoModal';
-import MuButton from '@units/Button/MuButton';
+import { ref, computed } from 'vue';
+import axios from 'axios';
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
+import { useUserStore } from "@stores/user";
+import { storeToRefs } from "pinia/dist/pinia";
+
+import InfoModal from '@collections/Modal/InfoModal';
+import MuButton from '@units/Button/MuButton';
 
 const props = defineProps({
     challengeType: {
         type: String,
         default: 'community',
-    }
+    },
+    challenge: {
+      type: Object,
+      default: () => {},
+    },
+    defaultStep: {
+        type: Number,
+        default: 1,
+    },
 });
 
-const selectedFrequency = ref('daily');
-const step = ref(1);
+const emit = defineEmits(['modalClose']);
+
+const userStore = useUserStore();
+const { userProfilePictureUrl } = storeToRefs(userStore);
+
+const selectedFrequency = ref(true);
+const step = ref(props.defaultStep);
 const selectedDate = ref(new Date(Date.now()));
+const slideIn = ref(false);
+const challengeData = ref({
+    entity:[],
+    total: 0,
+});
+
+const userNames = computed(() => {
+    const names = [];
+    challengeData.value.entity.forEach((user) => {
+        names.push(user.display_name);
+    });
+
+    return names.join(', ');
+})
+
+const setStartDateButtonText = computed(() => {
+    const today = new Date(Date.now());
+    if(today.getDate() === selectedDate.value.getDate() && today.getMonth() === selectedDate.value.getMonth() && today.getFullYear() === selectedDate.value.getFullYear()){
+        return 'Start Now';
+    } else {
+        return `Start on ${months[selectedDate.value.getMonth()]} ${selectedDate.value.getDate()}`
+    }
+})
 
 const handleFrequencyChange = (val) => {
     selectedFrequency.value = val;
 };
 
-const handleNext = () => {
-    step.value = 1;
+const handleNext = async () => {
+    if(props.challengeType === 'community'){
+        try {
+            const data = await axios.get(`/challenges/${props.challenge.content_id}`);
+            challengeData.value = data.data;
+            step.value = 2;
+            setTimeout(() => {
+                slideIn.value = true;
+            },1500)
+        }
+        catch(e) {
+            window.shownotification({
+                icon: 'error',
+                text: 'Woops! Something wrong happened, please try again later.'
+            })
+        }
+    } else {
+        step.value = 2;
+    }
 };
 
 const handleDateChange = (date) => {
     if(date){
-        const day = date.getDate();
-        const month = date.getMonth();
-        const year = date.getFullYear();
-
-        console.log(year, month, day);
+        selectedDate.value = date;
     }
 }
 
-const handleStartNow = () => {
-
+const setStartDate = async () => {
+    try {
+        const startDate = await axios.post(`/challenges/set_start_date/${props.challenge.content_id}`, {
+            start_date: selectedDate.value,
+        });
+    }
+    catch(e) {
+        window.shownotification({
+            icon: 'error',
+            text: 'Woops! Something wrong happened, please try again later.'
+        })
+    }
 };
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
