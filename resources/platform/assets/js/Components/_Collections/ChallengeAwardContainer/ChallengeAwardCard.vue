a9c3cc620761d37e179d6eeebbdcfa9724e13299
peterminhlee <petereminhlee@gmail.com>
2024-10-17 09:34:54 -0700

    Challenge award section

---
+++ resources/platform/assets/js/Components/_Collections/ChallengeAwardContainer/ChallengeAwardCard.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="tw-flex tw-flex-col tw-items-center dark:tw-text-white">
+        <img class="tw-w-full" src="https://d3fzm1tzeyr5n3.cloudfront.net/challenges/test_award.png" />
+        <p class="tw-font-bold">30-Day Drummer</p>
+        <p>August 2024</p>
+    </div>
+</template>
---
+++ resources/platform/assets/js/Components/_Collections/ChallengeAwardContainer/ChallengeAwardContainer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="tw-grid tw-grid-cols-6 tw-gap-4">
+        <ChallengeAwardCard />
+    </div>
+</template>
+<script setup>
+import ChallengeAwardCard from '@collections/ChallengeAwardContainer/ChallengeAwardCard';
+</script>
