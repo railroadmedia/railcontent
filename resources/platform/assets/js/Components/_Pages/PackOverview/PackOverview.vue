56ba5cf1b254fb7f756c0868c3c8e7548b90e2d0
Miguel Fondeur <miguelfondeur@gmail.com>
2024-10-18 21:23:56 +0000

    fix pack bundles :

--- resources/platform/assets/js/Components/_Pages/PackOverview/PackOverview.vue
+++ resources/platform/assets/js/Components/_Pages/PackOverview/PackOverview.vue
@@ -96,6 +96,7 @@ onMounted( async () => {
         data.value = PackData.value;
         //Header Data
         header.value = PackData.value.header;
+        console.log('header', header.value)
 
         platformStore.setLoadingState(PackLoading.value);
 })
