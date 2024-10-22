8d015ff30dc7bd4262812c594c356f8a74357483
robwillems <robert@musora.com>
2024-10-22 15:21:42 -0700

    Add user likes routes

--- app/Modules/Content/routes/routes.php
+++ app/Modules/Content/routes/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Modules\Content\Controllers\ChallengesMetaDataController;
+use App\Modules\Content\Controllers\ContentLikesControllerUser;
 use App\Modules\Content\Controllers\ContentMetadataController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,7 +38,21 @@ Route::prefix('content')
             [ContentMetadataController::class, 'getUserPermissions']
         )->name('content.user-permissions');
 
+        //Content User Likes
+        Route::get(
+            'user/likes/all',
+            [ContentLikesControllerUser::class, 'all']
+        )->name('content.user.likes.all');
 
+        Route::post(
+            'user/likes/like/{contentId}',
+            [ContentLikesControllerUser::class, 'like']
+        )->name('content.user.like');
+
+        Route::post(
+            'user/likes/unlike/{contentId}',
+            [ContentLikesControllerUser::class, 'unLike']
+        )->name('content.user.unlike');
 
     });
 
