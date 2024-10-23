b1fb1e65cad1a195610edf2502fcb36c8026a12e
Trent <trent.harms@gmail.com>
2024-10-23 11:24:53 -0700

    changing route

--- routes/drumeo/sales.php
+++ routes/drumeo/sales.php
@@ -50,8 +50,8 @@ Route::domain('{drumeoDomain}')
         Route::get('/alesis-strata-core/existing', [SalesController::class, 'alesisStrataCoreExisting']);
         Route::get('/alesis-nitro-pro', [SalesController::class, 'alesisNitroPro']);
         Route::get('/alesis-nitro-pro/existing', [SalesController::class, 'alesisNitroProExisting']);
-        Route::get('/yamaha', [SalesController::class, 'yamaha']);
-        Route::get('/yamaha/existing', [SalesController::class, 'yamahaExisting']);
+        Route::get('/yamaha-ead10', [SalesController::class, 'yamaha']);
+        Route::get('/yamaha-ead10/existing', [SalesController::class, 'yamahaExisting']);
 
         Route::get('/june', [SalesController::class, 'trial']);
         Route::get('/trial-key', [SalesController::class, 'trialKey']);
