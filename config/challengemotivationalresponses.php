52f6cef4add24f796cbba1e57030f2284df22e53
Adrian <150092463+adrian-musora@users.noreply.github.com>
2024-10-28 13:41:47 -0700

    TCH-66 TCH-39 - Complete Lesson Endpoint and handle milestone and rest day calculations (#1534)

---
+++ config/challengemotivationalresponses.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    5=> [
+        'drumeo'=> 'https=>//lottie.host/74bf25dd-376e-4ee4-b2d7-24c1a4bfaa13/jrmL6ilgZX.json',
+        'pianote' => 'https=>//lottie.host/b275c479-3c38-42e3-b358-2c26f9c32549/dPIjijPGMP.json',
+        'guitareo' => 'https=>//lottie.host/4208bdd3-4cf6-46b0-957d-008710822923/snrFxpZJVF.json',
+        'singeo' => 'https=>//lottie.host/99218eda-d69a-4988-93c1-cb78850ec5db/1M05ONEo5W.json',
+        'text' => 'Nice!'
+    ],
+    10=> [
+        'drumeo'=> 'https=>//lottie.host/78a3fc85-e9ef-404e-ac70-05b4413fdec2/15jLWPqTYS.json',
+        'pianote' => 'https=>//lottie.host/ee335b2a-903f-4295-bb9c-d2997f17d0e5/pNKuRuthZU.json',
+        'guitareo' => 'https=>//lottie.host/82ea88e3-73d8-483c-97c1-7fbba85d83ba/jdCFOWRePT.json',
+        'singeo' => 'https=>//lottie.host/b42a1be1-c1d4-4a6a-ba05-cf7c3ac9e328/F6yuKriIQf.json',
+        'text' => 'Great!'
+    ],
+    15=> [
+        'drumeo'=> 'https=>//lottie.host/45d3a312-3d96-4827-89f1-e522d0086a65/i5udgslkg8.json',
+        'pianote' => 'https=>//lottie.host/f08f0569-2db9-477b-8fa3-03d80f65174e/R1S7P1JebZ.json',
+        'guitareo' => 'https=>//lottie.host/705ce138-374d-4074-a621-39e2e3bc99f9/8eIYRyn4Uv.json',
+        'singeo' => 'https=>//lottie.host/2f7b671e-2b5b-438d-98a0-b995dba40f53/8nKexdbSyB.json',
+        'text' => 'Fantastic!'
+    ],
+    20=> [
+        'drumeo'=> 'https=>//lottie.host/74bf25dd-376e-4ee4-b2d7-24c1a4bfaa13/jrmL6ilgZX.json',
+        'pianote' => 'https=>//lottie.host/b275c479-3c38-42e3-b358-2c26f9c32549/dPIjijPGMP.json',
+        'guitareo' => 'https=>//lottie.host/4208bdd3-4cf6-46b0-957d-008710822923/snrFxpZJVF.json',
+        'singeo' => 'https=>//lottie.host/99218eda-d69a-4988-93c1-cb78850ec5db/1M05ONEo5W.json',
+        'text' => 'Well Done!'
+    ],
+    25=> [
+        'drumeo'=> 'https=>//lottie.host/78a3fc85-e9ef-404e-ac70-05b4413fdec2/15jLWPqTYS.json',
+        'pianote' => 'https=>//lottie.host/ee335b2a-903f-4295-bb9c-d2997f17d0e5/pNKuRuthZU.json',
+        'guitareo' => 'https=>//lottie.host/82ea88e3-73d8-483c-97c1-7fbba85d83ba/jdCFOWRePT.json',
+        'singeo' => 'https=>//lottie.host/b42a1be1-c1d4-4a6a-ba05-cf7c3ac9e328/F6yuKriIQf.json',
+        'text' => 'Well Done!'
+    ],
+    'complete' => [
+        'drumeo'=> 'https=>//lottie.host/c0f917ca-73c4-4b4c-8d2e-53a415d7ae09/jrNUPzGgwb.json',
+        'pianote' => 'https=>//lottie.host/3287ce92-0770-4ab3-95d3-3f74f10d4ca0/sEt84SWvHf.json',
+        'guitareo' => 'https=>//lottie.host/70c2c2f6-2325-4350-baa9-f703710c4ca3/FH8b9EZJkC.json',
+        'singeo' => 'https=>//lottie.host/d2c57df2-f221-4054-a5db-22f12e1a659a/kTBSCOj8tW.json',
+    ]
+];
