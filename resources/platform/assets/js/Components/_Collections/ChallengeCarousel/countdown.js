e7ae926f6f0986a54c692fff951867c6999b2fae
peterminhlee <petereminhlee@gmail.com>
2024-10-28 11:28:47 -0700

    optimisations

--- resources/platform/assets/js/Components/_Collections/ChallengeCarousel/countdown.js
+++ resources/platform/assets/js/Components/_Collections/ChallengeCarousel/countdown.js
@@ -1,5 +1,4 @@
 export const countdown = (targetDate) => {
-
         const now = new Date().getTime();
         const distance = new Date(targetDate).getTime() - now;
 
@@ -8,12 +7,8 @@ export const countdown = (targetDate) => {
         }
 
         // Calculate remaining time
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let hours =  Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let hours =  Math.floor(distance / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        if(days > 0) {
-            hours = hours + (days * 24);
-        }
 
         return `${String(hours).padStart(2, '0')} : ${String(minutes).padStart(2, '0')}`;
 }
