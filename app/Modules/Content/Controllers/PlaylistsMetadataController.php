6a9ab12b1ed642ef14f91b4ffb8333f3d864c046
Roxana Riza <roxana_r2000@yahoo.com>
2024-10-28 11:11:37 +0200

    Set content_id in playlist items id field

--- app/Modules/Content/Controllers/PlaylistsMetadataController.php
+++ app/Modules/Content/Controllers/PlaylistsMetadataController.php
@@ -293,6 +293,7 @@ class PlaylistsMetadataController extends Controller
             $item['duration'] = $sanityInfo ?  $sanityInfo['length_in_seconds']: null;
             $item['playlist_item_name'] = $sanityInfo ?  $item['playlist_item_name']: $item['content_name'];
             $item['user_playlist_item_id'] = $item['id'];
+            $item['id'] = $item['content_id'];
             return array_merge(
                 $item->toArray(),
                 $sanityInfo ? $sanityInfo : []
