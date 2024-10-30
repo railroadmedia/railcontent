4e661261bb70da4dfffc447b2f06fac88698bbb7
Awatanka <natalia.sokolova.ca@gmail.com>
2024-10-30 12:51:19 -0700

    Removed saasquatch from page controller

--- app/Http/Controllers/Platform/ReferralPagesController.php
+++ app/Http/Controllers/Platform/ReferralPagesController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers\Platform;
 
 use App\Http\Controllers\BaseController;
-use App\Modules\EventTracking\Avo\AvoHelper;
 use App\Modules\Referral\Services\ReferralService;
-use Avo;
-use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ReferralPagesController extends BaseController
 {
@@ -24,26 +20,6 @@ class ReferralPagesController extends BaseController
 
     public function inviteAFriend(Request $request): Factory|View|Application
     {
-        $user = user();
-        $brand = brand();
-        $referrer = $this->referralService->getOrCreateReferrer(
-            $user->id,
-            config('referral.saasquatch_referral_program_id.' . $brand),
-            $brand
-        );
-
-        try {
-            Avo::referral_page_viewed(
-                AvoHelper::defaultEventProperties(
-                    ['referral_code' => $referrer->referral_code, 'brand' => $brand],
-                    $user
-                )
-            );
-        } catch (Exception $e) {
-            // Do not block user flow if event tracking fails
-            Log::error($e->getMessage());
-        }
-
         return view('referral.invite-friend');
     }
 }
