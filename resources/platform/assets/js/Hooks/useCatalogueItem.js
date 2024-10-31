859cf9724dfb5d8c7c0be9c640a0134b0d1084f4
peterminhlee <petereminhlee@gmail.com>
2024-10-31 12:39:57 -0700

    removed spacing

--- resources/platform/assets/js/Hooks/useCatalogueItem.js
+++ resources/platform/assets/js/Hooks/useCatalogueItem.js
@@ -24,7 +24,6 @@ export default function useCatalogueItem(props) {
                 return true;
             }
 
-
             if(props.item.is_locked){
                 return !props.item.is_locked;
             } else if(props.item.quarter_published && props.item.status === 'draft'){
