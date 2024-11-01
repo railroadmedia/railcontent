270959338d03580efcb0c9dd9d4cd76f032c137b
Trent <trent.harms@gmail.com>
2024-11-01 10:44:30 -0700

    quick fixes

--- resources/marketing/views/drumeo/products/headphones.blade.php
+++ resources/marketing/views/drumeo/products/headphones.blade.php
@@ -557,7 +557,7 @@
         'vimeo' => true,
     ]) --}}
 
-    @include("pianote.sales.partials._footer")
+    @include("drumeo.sales.partials._footer")
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('/marketing/parcel/drumeo/navigation-sales.js') }}" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js"></script>
