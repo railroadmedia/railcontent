20f92c2dd79bb53a86b85aa4003e1209182f7da5
Adrian <150092463+adrian-musora@users.noreply.github.com>
2024-11-04 15:06:47 -0800

    Update resources/platform/views/content/lesson.blade.php

--- resources/platform/views/content/lesson.blade.php
+++ resources/platform/views/content/lesson.blade.php
@@ -304,7 +304,7 @@
         @if(!empty($lessonContent->fetch('soundslice_slug')))
             :soundslice-slug={{ json_encode($lessonContent->fetch('soundslice_slug')) }}
         @endif
-        :qa-video="{{ $hasQAVideo }}"
+        :qa-video="{{ $hasQAVideo }}" 
         :video-props="{{ json_encode($videoProps) }}" {{-- Mostly used for user data --}}
         :video-resources="{{ json_encode($videoResources) }}" {{-- Mostly used for user data --}}
         {{-- :content-description="{{ json_encode($lessonContent->fetch('data.description', null)) }}" --}}
