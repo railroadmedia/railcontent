b36cbed167c2b193794872f97985a85da437bb9d
robwillems <robert@musora.com>
2024-11-05 15:38:26 -0800

    Add default logging back

--- config/logging.php
+++ config/logging.php
@@ -1,12 +1,11 @@
 <?php
 
 use Monolog\Processor\PsrLogMessageProcessor;
-use Monolog\Handler\NullHandler;
-use Monolog\Handler\SocketHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
 
 return [
+    'default' => env('LOG_CHANNEL', 'stack'),
 
     'channels' => [
         'stack' => [
@@ -24,6 +23,17 @@ return [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
                 'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+            ],
+            'processors' => [PsrLogMessageProcessor::class],
+        ],
+
+        'stderr' => [
+            'driver' => 'monolog',
+            'level' => env('LOG_LEVEL', 'debug'),
+            'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
+            'with' => [
+                'stream' => 'php://stderr',
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
