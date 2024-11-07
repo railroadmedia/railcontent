e5daba576bfd32f4818210e5a60b7a424bd3b07c
robwillems <robert@musora.com>
2024-11-07 11:46:02 -0800

    Add new user permissions endpoint

---
+++ app/Modules/Content/Controllers/UserPermissionsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Modules\Content\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Arr;
+use Railroad\Railcontent\Services\UserPermissionsService;
+
+class UserPermissionsController extends Controller
+{
+    public function __construct(
+        private readonly UserPermissionsService $userPermissionsService
+    ) {
+    }
+
+    public function getUserPermissionData(): JsonResponse
+    {
+        $permissions = $this->userPermissionsService->getUserPermissions(user()->id);
+        $data = [
+            "permissions" => Arr::pluck($permissions, 'permission_id'),
+            "isAdmin" => user()->isAdmin()
+        ];
+        return response()->json($data);
+    }
+}
