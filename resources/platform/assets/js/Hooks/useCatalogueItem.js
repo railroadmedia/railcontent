53b74eae4a54dfb4e51f428052a72a4f6f4c5d86
Miguel Fondeur <miguelfondeur@gmail.com>
2024-11-07 00:52:08 +0000

    fix pack image

--- resources/platform/assets/js/Hooks/useCatalogueItem.js
+++ resources/platform/assets/js/Hooks/useCatalogueItem.js
@@ -90,7 +90,6 @@ export default function useCatalogueItem(props) {
         if (shows.indexOf(type) !== -1) {
             type = 'show';
         }
-        console.log("type", type)
         return new ContentModel(type, {
             brand: userStore.brand,
             post: props.item,
