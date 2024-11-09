126e80bff372ba8ae89528e36a39d5ae83aa0475
Adrian <150092463+adrian-musora@users.noreply.github.com>
2024-11-08 16:38:23 -0800

    TCH-72 - Short_name and index : TCH-89 - Update Milestone logic for challenges 11-29 days. Add update milestone tests.  (#1586)

--- app/Modules/Content/Services/ChallengesService.php
+++ app/Modules/Content/Services/ChallengesService.php
@@ -137,16 +137,21 @@ class ChallengesService
 
         if (!is_null($progressData) && $progressData->is_active) {
             $today = Carbon::now()->startOfDay();
+            $day = 0;
             foreach($challengeLessons as $index => $lesson) {
                 $unlockDate = $progressData->lessons_meta_data[$index]['unlock_date'];
                 $unlockDate = Carbon::parse($unlockDate);
                 $challengeLessons[$index]['unlock_date'] = $unlockDate->toISOString();
                 $challengeLessons[$index]['is_locked'] = $progressData->is_locked && $unlockDate > $today;
                 $challengeLessons[$index]['completed'] = $progressData->lessons_meta_data[$index]['completed'];
-                // TODO https://musora.atlassian.net/browse/TCH-72
-                // handle index and short name
-                $challengeLessons[$index]['index'] = $index;
-                $challengeLessons[$index]['short_name'] = "Day {$index}";
+                if (!$lesson['is_always_unlocked_for_challenge']) {
+                    $day += 1;
+                    $challengeLessons[$index]['index'] = $day;
+                    $challengeLessons[$index]['short_name'] = "Day {$day}";
+                } else {
+                    $challengeLessons[$index]['index'] = '';
+                    $challengeLessons[$index]['short_name'] = $lesson['title'];
+                }
             }
 
             $firstIncompleteLesson = $this->getFirstIncompleteLesson($challengeLessons, $progressData);
@@ -222,22 +227,17 @@ class ChallengesService
      */
     private function getFirstIncompleteLesson($challengeLessons, $progressData)
     {
-        $firstIncompleteLesson = null;
-        $now = Carbon::now()->startOfDay();
-        foreach ($challengeLessons as $index => $lesson) {
+        foreach ($challengeLessons as $lesson) {
             foreach ($progressData->lessons_meta_data as $userProgressLesson) {
                 if ($lesson['id'] == $userProgressLesson['content_id']) {
-                    if (is_null($firstIncompleteLesson) && !$userProgressLesson['completed']) {
-                        $firstIncompleteLesson = $lesson;
+                    if (!$userProgressLesson['completed']) {
+                        return $lesson;
                     }
-                    $unlockDate = Carbon::parse($userProgressLesson['unlock_date']);
-                    $challengeLessons[$index]['unlock_date'] = $unlockDate->toISOString();
-                    $challengeLessons[$index]['is_locked'] = $unlockDate >= $now;
                     break;
                 }
             }
         }
-        return $firstIncompleteLesson;
+        return null;
     }
 
     /**
@@ -283,8 +283,11 @@ class ChallengesService
         $sanityDocument = $this->sanityGateway->getChallengeChildAndParentData($lessonId);
         $challenge = $sanityDocument['parent'];
         $userProgress = ChallengeUserProgress::whereChallengeIdAndUser($challenge['id'], $userId);
+        $wasChallengeCompleted = $userProgress->areAllLessonsCompleted();
         $lessonsProgress = $userProgress->updateLessonsProgress($lessonId, isCompleted: true);
+        $userProgress = ChallengeUserProgress::whereChallengeIdAndUser($challenge['id'], $userId);
         $isChallengeCompleted = $userProgress->areAllLessonsCompleted();
+
         $lessonData = $this->getCurrentLessonData($lessonId, $userId, isLesson: true);
         $active = true;
         $motivationalText = [];
@@ -292,7 +295,7 @@ class ChallengesService
             $active = false;
         } else if ($lessonsProgress['is_milestone']) {
             $milestone = $isChallengeCompleted ? 'complete' : $lessonData['user_data']['current_streak'];
-            $motivationalTextConfig = config('challengemotivationalresponses')[$milestone];
+            $motivationalTextConfig = config('challengemotivationalresponses')[$milestone] ?? config('challengemotivationalresponses')['default'];
             $motivationalText = [
                 'lottie_url' => $motivationalTextConfig[brand()],
                 'milestone' => $milestone,
@@ -312,6 +315,9 @@ class ChallengesService
             ];
         }
         $userData = $userProgress->getCompiledMetadata();
+        if (!$wasChallengeCompleted && $isChallengeCompleted) {
+            $this->completeChallenge($challenge['id'], $userId);
+        }
         $challengeData = array_intersect_key($lessonData['lesson'], array_flip(['challenge_dark_mode_logo_url', 'challenge_light_mode_logo_url', 'challenge_logo_image_url', 'index', 'short_name']));
         return [
             'show_modal' => $active,
@@ -337,7 +343,7 @@ class ChallengesService
         $bestMinutesPracticed = max($userProgress->getMinutesPracticed(), $userProgress->completed_time_practiced);
         $userProgress->completed_time_practiced = $bestMinutesPracticed;
         $userProgress->completed_best_streak = $bestStreak;
-        $userProgress->last_completed_date = $today;
+        $userProgress->last_completed_date = $today->toISOString();
         $userProgress->is_active = false;
         $userProgress->save();
     }
