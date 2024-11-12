9e7166e5f1de3370e7753f400e67a765c7b7c66d
peterminhlee <petereminhlee@gmail.com>
2024-11-12 13:44:40 -0800

    merge fix

--- resources/platform/assets/js/Hooks/pages/useOverviewPageData.js
+++ resources/platform/assets/js/Hooks/pages/useOverviewPageData.js
@@ -1,6 +1,6 @@
 // hooks/useOverviewPageData.js
 import { ref } from 'vue';
-import { fetchMethod, getProgressPercentage, fetchMethodChildren, fetchFoundation, fetchCompletedState, fetchMethod, getProgressPercentage, fetchMethodChildren, fetchFoundation, getProgressPercentageByIds, fetchUserChallengeProgress } from 'musora-content-services';
+import { fetchMethod, fetchMethodChildren, fetchFoundation, fetchCompletedState, getProgressPercentage, fetchUserChallengeProgress } from 'musora-content-services';
 
 import { useUserStore } from "@stores/user";
 import { useBuildHeader } from '@hooks/useBuildHeader';
@@ -30,7 +30,7 @@ export async function useOverviewPageData(contentType, parentType) {
                 };
             }
         } else {
-<			if (contentType === "learning-path-level") {
+		if (contentType === "learning-path-level") {
 	            const result = await fetchMethod(userStore.brand, `${userStore.brand}-method`);
 	            if (result) {
 	                result.levels = result.levels.map((level, index) => ({
