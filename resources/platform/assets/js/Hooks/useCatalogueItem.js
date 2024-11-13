d7df424cb3d2e0defe6879711fe307a340beb343
Miguel Fondeur <miguelfondeur@gmail.com>
2024-11-13 22:42:24 +0000

    update progress values

--- resources/platform/assets/js/Hooks/useCatalogueItem.js
+++ resources/platform/assets/js/Hooks/useCatalogueItem.js
@@ -3,12 +3,21 @@ import { computed } from 'vue';
 import ContentHelpers from "@vuesora/assets/js/helper-functions/content.js";
 import ContentModel from '@vuesora/assets/js/models/_model.js';
 import { useUserStore } from "@stores/user.js";
+import { getProgressPercentage } from 'musora-content-services';
 
 export default function useCatalogueItem(props) {
     const userStore = useUserStore();
 
     const is_added = computed(() => props.item.is_added_to_primary_playlist);
-    const progress_percent = computed(() => props.item.progress_percent);
+    
+    let progress_percent = 0;
+    //Progress Percentage
+    getProgressPercentage(props.item.id).then( value => {
+        progress_percent = value;
+    }).catch( error => {
+        console.log('error fetching progress', error)
+    })
+
     const noAccess = computed(() => {
             if (userStore.isAdmin) {
                 return false;
