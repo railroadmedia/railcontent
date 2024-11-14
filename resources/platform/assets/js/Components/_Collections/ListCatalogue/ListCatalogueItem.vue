66efc477d06e4e0d08ac8d2d362c196ee71e1cc1
Miguel Fondeur <miguelfondeur@gmail.com>
2024-11-14 16:49:04 +0000

    remove completed/progress code from the useSongPageData.js

--- resources/platform/assets/js/Components/_Collections/ListCatalogue/ListCatalogueItem.vue
+++ resources/platform/assets/js/Components/_Collections/ListCatalogue/ListCatalogueItem.vue
@@ -247,10 +247,10 @@
             <!-- STARTED OR COMPLETED -->
             <div v-else class="body tw-inline-flex tw-h-full tw-items-center">
                 <i
-                    v-if="item.started || item.completed"
+                    v-if="item.started || lesson_completed"
                    class="fas flex-center rounded dark:hover:tw-text-white hover:tw-text-[#00101D]"
                    :class="[
-                            item.completed ? completedIcon : 'fa-adjust',
+                            lesson_completed ? completedIcon : 'fa-adjust',
                             themeTextClass,
                     ]"></i>
 
@@ -265,7 +265,7 @@
 </template>
 
 <script setup>
-import {computed, ref} from "vue";
+import {computed, onBeforeMount, ref} from "vue";
 import { storeToRefs } from "pinia/dist/pinia";
 import { usePlatformStore } from "../../../Stores/platform";
 import { useUserStore } from "@stores/user";
@@ -274,6 +274,7 @@ import useThemeClasses from "@hooks/useThemeClasses";
 import useUserCatalogueEvents from "@hooks/useUserCatalogueEvents";
 import { useResetProgress } from "@hooks/useResetProgress";
 import DifficultyLabel from '@units/DifficultyLabel/DifficultyLabel';
+import { contentStatusCompleted } from 'musora-content-services';
 
 const props = defineProps({
     isCoach: {
@@ -384,7 +385,9 @@ const {
 const { addToList, addEvent } = useUserCatalogueEvents({ ...props });
 const { resetProgress } = useResetProgress();
 
+//Ref
 const resetIcon = ref('fas fa-redo-alt fa-flip-horizontal');
+const lesson_completed = ref(false);
 
 //Computed
 const branchPathBG = computed(() => {
@@ -398,7 +401,7 @@ const branchPathText = computed(() => {
 const class_object = computed(() => {
     return {
         active: props.active,
-        completed: props.item.completed,
+        completed: lesson_completed.value,
         "content-overview pv-2": props.overview,
         "content-table-row pv-1": !props.overview,
         'tw-flex-nowrap': props.isNextLesson,
@@ -475,5 +478,13 @@ const handleReset = () => {
 const openUpgradeModal = () => {
     noAccess.value && platformStore.openMembershipUpgradeModal();
 }
+
+onBeforeMount( () => {
+    contentStatusCompleted(props.item.id).then( value => {
+        lesson_completed.value = value;
+    }).catch( error => {
+        console.log('error gettin completed value', error);
+    })
+})
 </script>
 
--- resources/platform/assets/js/Hooks/pages/useSongPageData.js
+++ resources/platform/assets/js/Hooks/pages/useSongPageData.js
@@ -1,54 +1,34 @@
-// hooks/useSongPageData.js
 import { ref } from 'vue';
 import { fetchSongById, fetchRelatedSongs } from 'musora-content-services';
-import { fetchCompletedState, fetchAllCompletedStates } from 'musora-content-services';
 
-export async function useSongPageData(contentId, brand, userId, token) {
+export async function useSongPageData(contentId, brand) {
   const data = ref(null);
   const error = ref(null);
   const isLoading = ref(true);
 
   try {
-    const [songResponse, relatedSongsResponse, currentSongCompleteResponse] = await Promise.all([
+    const results = await Promise.allSettled([
       fetchSongById(contentId),
-      fetchRelatedSongs(brand, contentId),
-      fetchCompletedState(contentId)
+      fetchRelatedSongs(brand, contentId)
     ]);
 
+    const songResponse = results[0].status === "fulfilled" ? results[0].value : null;
+    const relatedSongsResponse = results[1].status === "fulfilled" ? results[1].value : null;
+
     data.value = songResponse;
+    console.log('songResponse', songResponse)
 
     if (relatedSongsResponse) {
+      console.log('relatedSongs', relatedSongsResponse);
       data.value.relatedLessons = relatedSongsResponse.entity;
     }
 
-    if (currentSongCompleteResponse) {
-      data.value.completed = currentSongCompleteResponse.state !== "not started";
-      data.value.progress_percent = currentSongCompleteResponse.percent.toString();
-    }
 
-    if (data.value.relatedLessons) {
-      const relatedLessonIds = data.value.relatedLessons.map(lesson => lesson.id);
-      const relatedLessonsCompletionStates = await fetchAllCompletedStates(userId, relatedLessonIds, token);
-      if (relatedLessonsCompletionStates) {
-        data.value.relatedLessons = data.value.relatedLessons.map(lesson => {
-          const lessonCompletionState = relatedLessonsCompletionStates[lesson.id];
-          if (lessonCompletionState) {
-            return {
-              ...lesson,
-              completed: lessonCompletionState.state !== "not started",
-              lesson_progress: lessonCompletionState.percent.toString()
-            };
-          }
-          return lesson;
-        });
-      }
-    }
   } catch (err) {
     error.value = err;
   } finally {
     isLoading.value = false;
   }
 
-  //console.log('Song data:', data.value);
   return { data, error, isLoading };
 }
