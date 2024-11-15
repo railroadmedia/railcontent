03b00dcacfd54d934d7ede675181a40ff0a1239e
Trent <trent.harms@gmail.com>
2024-11-15 11:14:24 -0800

    testing

--- app/Modules/CustomerIO/config/customer-io.php
+++ app/Modules/CustomerIO/config/customer-io.php
@@ -97,6 +97,21 @@ return [
                     'email' => 'Email'
                 ],
             ],
+            'Cymbal Giveaway' => [
+                'custom_attributes' => [
+                    'first_name' => 'nullable|string',
+                ],
+                'events' => [
+                    'drumeo_prospect_cymbal-giveaway',
+                ],
+                'accounts_to_sync' => [
+                    'drumeo',
+                ],
+                'attributes' => [
+                    'first_name' => 'First Name',
+                    'email' => 'Email'
+                ],
+            ],
             'Free Music Lessons For Life' => [
                 'custom_attributes' => [
                     'first_name' => 'nullable|string',
