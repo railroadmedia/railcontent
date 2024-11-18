29b09ea0ccf42922a7cd1e71cef8c901585b0bb6
peterminhlee <petereminhlee@gmail.com>
2024-11-18 09:46:58 -0800

    added on close

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <component v-if="!isCertificateOpen" :is="isModal ? compMap.ModalRenderer : 'div'" :black-background="true" :show-x-icon="true" >
+    <component v-if="!isCertificateOpen" :is="isModal ? compMap.ModalRenderer : 'div'" :black-background="true" :show-x-icon="true" @on-close="emit('closeModal')" >
         <div class="tw-max-w-[470px] tw-w-full tw-rounded-[10px] tw-px-4 sm:tw-px-14 tw-py-10 tw-relative tw-border dark:tw-border-[rgba(255,255,255,0.09)] tw-mx-4 sm:tw-mx-0">
             <!-- Animation -->
             <Vue3Lottie v-if="!hideAnimation && !isModal" class="tw-w-[calc(100% + 200px)] sm:tw-w-[800px] tw-h-[800px] tw-absolute tw-top-1/2 tw-left-1/2 -tw-translate-x-1/2 -tw-translate-y-1/2 tw-z-[5]" :animation-link="animations[brand]" width="100%" height="100%" :loop="false" />
@@ -25,7 +25,7 @@
                     <MuButton @click="openShareModal"><musora-icon icon-name="share" class="tw-h-6 tw-mr-1 -tw-mt-1 " /> Share</MuButton>
                 </div>
                 <div v-if="!openFromAwards" class="tw-text-center">
-                    <a :href="`/${brand}/challenges`" class="tw-uppercase tw-underline tw-font-bold tw-font-bebas-neue dark:tw-text-white tw-z-[3] tw-relative">Return to Challenges</a> 
+                    <a :href="`/${brand}/challenges`" class="tw-uppercase tw-underline tw-font-bold tw-font-bebas-neue dark:tw-text-white tw-z-[3] tw-relative">Return to Challenges</a>
                 </div>
             </div>
         </div>
