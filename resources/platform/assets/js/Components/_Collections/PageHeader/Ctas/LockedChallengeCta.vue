97f1ce3b1ca0b982c98eab6f6c30c9cb19663c0c
peterminhlee <petereminhlee@gmail.com>
2024-11-19 09:19:56 -0800

    added lockedChallengeCta

---
+++ resources/platform/assets/js/Components/_Collections/PageHeader/Ctas/LockedChallengeCta.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <PageHeaderCta
+            v-bind="$attrs"
+            :text="text"
+            showAllAlways
+            @click="handleOpen"
+        />
+        <ChallengeActionModal v-if="modalOpen" modal-type="unlock" @close-modal="handleClose" :challenge="lessonData" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PageHeaderCta from '@collections/PageHeader/PageHeaderCta';
+import ChallengeActionModal from '@collections/Modal/ChallengeActionModal';
+
+const props = defineProps({
+    text: String,
+    nextLesson: {
+        type: Object,
+        default: () => {},
+    },
+});
+
+const modalOpen = ref(false);
+
+const handleOpen = () => {
+    modalOpen.value = true;
+};
+
+const handleClose = () => {
+    modalOpen.value = false;
+};
+</script>
