e1bca602e5765de4bfaccce63c2222a8f4cfad75
Yalung Tang <yalungtang@gmail.com>
2024-11-21 20:38:42 -0300

    homepage updates index blade

--- resources/platform/views/home/index.blade.php
+++ resources/platform/views/home/index.blade.php
@@ -10,17 +10,21 @@
         account-url="{{ user()->getDashboardUrl() }}"
         calendar-id="{{ $calendarId }}"
         :carousel="{{ json_encode($carousel) }}"
+        :coach-event="{{ $coachEvent }}"
         :cohort-banner="{{ $cohortBanner }}"
         content-endpoint="/railcontent/content"
         continue-url="{{ url()->route('platform.lesson-history.in-progress') }}"
         current-date="{{ $currentDate }}"
+        event-coach-profile-url="{{ $eventCoachProfileUrl }}"
         :exists-cohort-banner="{{ $existsCohortBanner ? 'true' : 'false' }}"
         :has-started-content="{{ $hasStartedLessons ? 'true' : 'false' }}"
         :has-started-lessons="{{ $hasStartedLessons ? 'true' : 'false' }}"
+        :has-upcoming-events="{{ $hasUpcomingEvents ? 'true' : 'false' }}"
         @if(count($hotForumTopics) > 0)
             :conversation-data="{{ json_encode($hotForumTopics) }}"
         @endif
         :is-a-member="{{ user()->isAMember() ? 'true' : 'false' }}"
+        :new-content="{{ $newContentJson }}"
         new-content-url="{{ url()->route('platform.new-lessons') }}"
         next-learning-path-level="{{ user()->getMethodLevel() }}"
         :next-learning-path-progress-percent="{{ $nextLearningPathProgressPercent }}"
@@ -28,14 +32,21 @@
         recommended-content-url="{{ url()->route('platform.recommended-lessons') }}"
         :started-content="{{ $startedContentJson }}"
         :time-cutoff-minutes="{{ $timeCutoffMinutes }}"
+        :upcoming-events="{{ $upcomingEvents }}"
+        upcoming-url="{{ '/'.$brand.'/live' }}"
         :users-list="{{ json_encode($usersList->results())  }}"
-        :user-metrics="{{ json_encode($userMetrics) }}"A
+        :user-metrics="{{ json_encode($userMetrics) }}"
+        :workouts-content="{{ $workoutsContentJson }}"
         workouts-content-url="{{ url()->route('platform.workouts') }}"
         youtube-id="{{ $youtubeId }}"
         :learning-paths="{{ json_encode($trialSection) }}"
         :display-trial-section="{{ $displayTrialSection ? 'true' : 'false' }}"
+        :trial-section-redesign="{{ $trialSectionRedesign }}"
+        :is-first-access="{{ $isFirstAccess ? 'true' : 'false' }}"
+        :explore-tasks="{{ json_encode($exploreTasks) }}"
+        :is-v2-user="{{ json_encode($homepageV2) }}"
     ></home>
 
-    @include('partials._railanalytics-brand-tracking-iframe')
+@include('partials._railanalytics-brand-tracking-iframe')
 
 @endsection
