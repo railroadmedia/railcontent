1ea297fc74bf11758a54c02d6bcdab4f51b066c0
Trent <trent.harms@gmail.com>
2024-11-20 18:45:21 -0800

    testing forms

--- resources/marketing/views/guitareo/sales/subscription.blade.php
+++ resources/marketing/views/guitareo/sales/subscription.blade.php
@@ -357,7 +357,7 @@
             <div class="container max-w-4xl mx-auto">
                 <div class="flex flex-wrap items-center justify-center">
                     <div class="w-full sm:w-7/12 mb-2 sm:mb-0">
-                        <img class="h-7 sm:h-8 lg:h-10" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/2000x0/filters:quality(95)/marketing/drumeo/promos/november/bf-logo-alt.png">
+                        <img class="h-7 sm:h-8 lg:h-10" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/870x0/filters:quality(95)/marketing/drumeo/promos/november/bf-logo-alt.png">
                         <h5 class="leading-tight font-bold text-musora tracking-widest mt-2"><strong>STARTS ON NOVEMBER 26TH</strong></h5>
                     </div>
                     <div class="w-full sm:w-5/12">
