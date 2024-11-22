32c1430ac857c2491e0bf93e0b82d45293c19a2c
Yalung Tang <yalungtang@gmail.com>
2024-11-22 10:27:06 -0300

    WIP merge

--- resources/platform/assets/js/Components/_Pages/Login.vue
+++ resources/platform/assets/js/Components/_Pages/Login.vue
@@ -14,13 +14,11 @@ const userStore = useUserStore();
 const notification = useNotificationStore();
 
 const props = defineProps({
-  loginurl: {
+  redirectUrl: {
     type: String,
+    default: ''
   },
   reseturl: {
-    type: String,
-  },
-  joinurl: {
     type: String,
   },
   usecsrftoken: {
@@ -86,6 +84,10 @@ const changeConfirmationScreen = (confirmationType) => {
   }
 };
 
+function deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
+
 const handleButtonClick = () => {
   isLoading.value = true;
   localStorage.setItem("lastEmailUsed", emailInput.value);
@@ -93,10 +95,15 @@ const handleButtonClick = () => {
   axios.post('/user-management-system/login', {
     email: emailInput.value,
     password: passwordInput.value,
-    redirect_to: new URLSearchParams(window.location.search).get('redirect_to')
+    redirect_to: new URLSearchParams(window.location.search).get('redirect_to') ?? props.redirectUrl
   })
     .then((response) => {
       if (response.status === 200) {
+          deleteCookie(`hideOnboardingBanner_drumeo`);
+          deleteCookie(`hideOnboardingBanner_guitareo`);
+          deleteCookie(`hideOnboardingBanner_singeo`);
+          deleteCookie(`hideOnboardingBanner_pianote`);
+          
           const redirectTo = response.data.redirect_to;
           if (redirectTo) {
             window.location.href = redirectTo;
--- resources/platform/assets/js/Components/_Pages/Settings/AccountDetails.vue
+++ resources/platform/assets/js/Components/_Pages/Settings/AccountDetails.vue
@@ -11,7 +11,7 @@
                 :ctas="[{
                     type: 'PageHeaderPrimaryCta',
                     props: {
-                        text: `${userCompletedAccount ? 'Update Your Account' : 'Complete Your Account'}`,
+                        text: `${showOnboardingBanner ? 'Update Your Account' : 'Complete Your Account'}`,
                         url: `/onboarding?brand=${brand}`,
                         showAllAlways: true,
                     }
@@ -136,7 +136,7 @@ const {
     userDisplayName,
     userProfilePictureUrl,
     userCreatedYear,
-    userCompletedAccount,
+    showOnboardingBanner,
     userMembershipLevel,
     isLifetimeMember,
     userMembershipExpirationFormatted,
