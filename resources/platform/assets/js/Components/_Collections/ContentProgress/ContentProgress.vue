a98c45f288abadda952e1515ceafe3a040bf7b03
Miguel Fondeur <miguelfondeur@gmail.com>
2024-11-25 16:52:17 +0000

    update user progress functions

--- resources/platform/assets/js/Components/_Collections/ContentProgress/ContentProgress.vue
+++ resources/platform/assets/js/Components/_Collections/ContentProgress/ContentProgress.vue
@@ -7,16 +7,16 @@
                 <h3 :class="`display ${brandTextColor} nowrap`">{{ labelText }}</h3>
             </div>
             <div class="flex flex-column">
-                <div :class="`flex flex-row trophy-progress-bar tw-h-[18px] sm:tw-h-[50px] mr-2 bg-${brand} bg-darken ${isCompleted ? 'complete' : ''}`">
+                <div :class="`flex flex-row trophy-progress-bar tw-h-[18px] sm:tw-h-[50px] mr-2 bg-${brand} bg-darken ${completed ? 'complete' : ''}`">
                     <div :class="`flex flex-column trophy-progress-cutoff tw-h-[38px] sm:tw-h-[70px] bg-${brand} inverted relative`">
                         <span :class="`progress-border ba-${brand}-5 border-darken absolute-fill`"></span>
                         <span
-                            :data-current-progress="progress"
+                            :data-current-progress="progressAmount"
                             class="trophy-progress relative bg-white"
-                            :style="{ transform: `translateX(${progress - 100}%)` }"
+                            :style="{ transform: `translateX(${progressAmount - 100}%)` }"
                         >
-                <span :class="`progress-percent body tw-font-bold tw-text-[10px] sm:tw-text-base ${brandTextColor} ${progress > 50 ? '' : 'right'}`">
-                  {{ Math.round(progress) }}%
+                <span :class="`progress-percent body tw-font-bold tw-text-[10px] sm:tw-text-base ${brandTextColor} ${progressAmount > 50 ? '' : 'right'}`">
+                  {{ Math.round(progressAmount) }}%
                 </span>
               </span>
                     </div>
@@ -25,16 +25,16 @@
                             <i :class="`fas fa-trophy ${brandTextColor}`"></i>
                             <span v-if="xpAmount">&nbsp;&nbsp;{{ xpAmount }} XP</span>
                         </div>
-                        <div :class="`white-underlay ba-${brand}-5 ${progress === 100 ? 'visible' : ''} border-darken`"></div>
+                        <div :class="`white-underlay ba-${brand}-5 ${progressAmount === 100 ? 'visible' : ''} border-darken`"></div>
                     </div>
                 </div>
             </div>
             <div class="tw-flex tw-flex-col tw-text-white tw-w-full sm:tw-w-auto tw-justify-center">
                 <a v-if="!showCompleteButton"
-                   :href="isCompleted ? backButton.url : nextLessonUrl"
+                   :href="completed ? backButton.url : nextLessonUrl"
                    class="tw-btn-secondary tw-text-lg tw-mb-0 tw-leading-[0] tw-border-[3px] tw-text-white"
                 >
-            <span v-if="!isCompleted">
+            <span v-if="!completed">
               <span v-if="isStarted">Next Lesson &raquo;</span>
               <span v-else>Start First Lesson</span>
             </span>
@@ -45,26 +45,26 @@
                             :data-brand="brand"
                             :data-content-id="contentId"
                             title="Reset Progress"
-                            @click="resetProgress"
+                            @click="triggerProgressReset"
                     >
               <span class="bg-white inverted tw-text-white tw-px-6 tw-items-center tw-border-none tw-shadow-none tw-flex-col">
                 <i class="fas fa-undo tw-text-white reset tw-mb-0.5 tw-text-lg" aria-hidden="true"></i> Reset
               </span>
                     </button>
                     <button class="btn completeButton tw-text-base tw-max-w-[250px] sm:tw-max-w-none"
-                            :class="isCompleted ? 'is-complete' : ''"
+                            :class="completed ? 'is-complete' : ''"
                             dusk="master-complete-button"
                             title="Mark Lesson as Complete"
                             :data-brand="brand"
                             :data-content-id="contentId"
-                            @click="toggleComplete"
+                            @click="triggerLessonComplete"
                     >
-              <span class="incompleted bg-white inverted tw-text-white tw-px-6 tw-items-center tw-border tw-border-white sm:tw-border-none tw-shadow-none" :class="!isCompleted ? 'tw-flex sm:tw-flex-col tw-h-[35px] sm:tw-h-auto' : 'tw-hidden'">
+              <span class="incompleted bg-white inverted tw-text-white tw-px-6 tw-items-center tw-border tw-border-white sm:tw-border-none tw-shadow-none" :class="!completed ? 'tw-flex sm:tw-flex-col tw-h-[35px] sm:tw-h-auto' : 'tw-hidden'">
                 <div class="tw-border-2 tw-border-white tw-rounded-full tw-px-1 tw-mr-1 sm:tw-mr-0 sm:tw-mb-1.5">
                   <i class="fas fa-check tw-text-[10px] tw-mb-1"></i>
                 </div> Complete
               </span>
-                        <span class="completed tw-text-white tw-px-6 tw-items-center tw-border tw-border-white sm:tw-border-none tw-shadow-none" :class="isCompleted ? 'tw-flex sm:tw-flex-col tw-h-[35px] sm:tw-h-auto' : 'tw-hidden'">
+                        <span class="completed tw-text-white tw-px-6 tw-items-center tw-border tw-border-white sm:tw-border-none tw-shadow-none" :class="completed ? 'tw-flex sm:tw-flex-col tw-h-[35px] sm:tw-h-auto' : 'tw-hidden'">
                 <div class="tw-border-2 tw-border-white tw-bg-white tw-rounded-full tw-px-1 tw-mr-1 sm:tw-mr-0 sm:tw-mb-1.5">
                   <i :class="`fas fa-check tw-text-[10px] tw-mb-1 ${brandTextColor}`"></i>
                 </div> Completed
@@ -78,16 +78,16 @@
 
 <script setup>
 // TODO: We might need to test corner cases if this is implemented in templates different than the LessonPlayback
-import {  computed } from 'vue';
+import {  computed, onBeforeMount, ref } from 'vue';
 import { textColor } from '@constants/brands';
 import { usePlatformStore } from "@stores/platform";
 import { storeToRefs } from "pinia/dist/pinia";
 import SkeletonContentProgress from '../SkeletonLoader/SkeletonContentProgress.vue';
+import { useResetProgress } from '@hooks/useResetProgress';
 
 const props = defineProps({
     labelText: String,
     brand: String,
-    isCompleted: Boolean,
     progress: Number,
     xpAmount: [String, Number],
     isStarted: Boolean,
@@ -97,22 +97,37 @@ const props = defineProps({
     contentId: Number
 });
 
+const { resetProgress } = useResetProgress();
 const platformStore = usePlatformStore();
+
 const { isLoading } = storeToRefs(platformStore);
 
 const brandTextColor = computed(() => {
     return textColor[props.brand];
 });
 
-const resetProgress = () => {
-    // Add logic for resetting progress
-    // console.log('Reset progress');
-    // TODO: Refactor event listeners and attach here
+const triggerProgressReset = () => {
+    resetProgress(props.contentId, '', true); //leave icon arg empty
 }
 
-const toggleComplete = () => {
-    // Add logic for marking lesson as complete/incomplete
-    // console.log('Toggle complete');
-    // TODO: Refactor event listeners and attach here
+//refs
+const progressAmount = ref(0);
+
+const triggerLessonComplete = () => {
+    //Update Locally First?
+    progressAmount.value = 100;
+
+
 }
+
+const completed = computed ( ()=> {
+    return progressAmount.value === 100;
+})
+
+onBeforeMount( ()=> {
+    console.log('progress', props.progress);
+    
+    //Set initial Values
+    progressAmount.value = props.progress;
+})
 </script>
