625b879b8c3f80d7ee3dd14ae6246d26688b668b
Trent <trent.harms@gmail.com>
2024-11-25 16:14:54 -0800

    moving tooltip to tailwind

--- resources/marketing/views/musora/sales/components/guarantee-section.blade.php
+++ resources/marketing/views/musora/sales/components/guarantee-section.blade.php
@@ -40,7 +40,19 @@
             </div>
             <div class="w-full sm:w-1/3 px-2">
                 <h5 class="@if($theme === 'musora') text-black border-black @else text-{{ $theme }} border-{{ $theme }} @endif border-2 rounded-full inline-block @if($theme === 'musora') py-1 @else py-2 @endif px-3 mb-1">3</h5>
-                <h6 class="leading-normal">Change your mind?<br> Get a refund. <div class="ml-2 tool absolute inline-block cursor-pointer" tip="If it’s not for you, simply cancel your membership within 90 days and contact us for a full refund. (If your membership includes any bonuses, your refund will deduct the value of hard goods that were shipped to you.)"  aria-label="Refund Information"><i class="fas fa-info-circle"></i></div></h6>
+                <h6 class="leading-normal relative">
+                    Change your mind?<br> Get a refund.
+                    <div class="ml-2 inline-block cursor-pointer group" aria-label="Refund Information">
+                        <i class="fas fa-info-circle"></i>
+                        <div class="absolute left-1/2 transform -translate-x-1/2 -translate-y-full hidden group-hover:block transition-all duration-300 opacity-0 group-hover:opacity-100 group-hover:max-h-[1000px] max-h-0"
+                        style="    top: 0;">
+                            <div class="absolute left-1/2 transform -translate-x-1/2 bottom-[-14px] border-[7px] border-transparent border-t-white"></div>
+                            <div class="p-2 text-xs text-black bg-white rounded-lg shadow-xl" style="    width: 200px;">
+                                If it’s not for you, simply cancel your membership within 90 days and contact us for a full refund. (If your membership includes any bonuses, your refund will deduct the value of hard goods that were shipped to you.)
+                            </div>
+                        </div>
+                    </div>
+                </h6>
             </div>
         </div>
     </div>
