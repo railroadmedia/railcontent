c0f26997691cb70b36bd3c9f5d2b6aa61c47aa45
Trent <trent.harms@gmail.com>
2024-11-26 11:08:50 -0800

    overriding percent

--- resources/marketing/views/drumeo/shop/shop.blade.php
+++ resources/marketing/views/drumeo/shop/shop.blade.php
@@ -212,15 +212,12 @@
             <div class="container">
                 <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-person-to-door text-{{ $brand }} mr-1"></i> Bundles & Door Crashers</strong></h5>
                 <div style="margin-bottom: 0;" class="fixed-cards grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-4 text-left">
-                    @include('_partials.components.shop.product-card', [
-                        "href" => "/drumshop/practice-bundle",
-                         "price" => 1227.87,
-                         "discounted_price" => 399,
-                         "title" => "The Practice Bundle",
-                         "instructor" => "Save $828.87 on most popular practice essentials + lessons.",
-                         "thumbnailFull" => "https://d21q7xesnoiieh.cloudfront.net/fit-in/740x0/filters:quality(95)/marketing/drumeo/promos/november/2024/practice-bundle.webp",
-                         'soldOut' => false,
-                    ])
+                    <a class="product-wrap relative text-black pb-4 hover:opacity-90 transition-opacity" data-price="399" href="/drumshop/practice-bundle" x-data="{ open: false }">
+                        <div class="overflow-hidden rounded-lg relative bg-cover bg-top mb-3 border border-gray-300" style="padding-bottom: 100%; background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/740x0/filters:quality(95)/marketing/drumeo/promos/november/2024/practice-bundle.webp');"></div>
+                        <p class="leading-tight font-black mb-1">The Practice Bundle</p>
+                        <p class="leading-tight text-sm mb-1">Save $828.87 on most popular practice essentials + lessons.</p>
+                        <p class="leading-tight"><s class="opacity-60">$1227.87</s> <strong class="text-drumeo font-extrabold">$399</strong><span class="ml-1 text-xs bottom-0 font-black text-black rounded-md px-1.5 leading-none py-1 inline-block bg-musora align-bottom">Save 67%</span></p>
+                    </a>
                     @include('_partials.components.shop.product-card', [
                         "href" => "/drumshop/kit",
                          "price" => 1474,
@@ -334,14 +331,12 @@
                     ])
                 @endforeach
                     <div x-cloak x-show="filter === 'accessories'">
-                        @include('_partials.components.shop.product-card', [
-                            "href" => "/drumshop/practice-bundle",
-                             "price" => 1227.87,
-                             "discounted_price" => 399,
-                             "title" => "The Practice Bundle",
-                             "thumbnailFull" => "https://d21q7xesnoiieh.cloudfront.net/fit-in/540x0/filters:quality(95)/marketing/drumeo/promos/november/2024/practice-bundle.webp",
-                             'soldOut' => false,
-                        ])
+                        <a class="product-wrap relative text-black pb-4 hover:opacity-90 transition-opacity" data-price="399" href="/drumshop/practice-bundle" x-data="{ open: false }">
+                            <div class="overflow-hidden rounded-lg relative bg-cover bg-top mb-3 border border-gray-300" style="padding-bottom: 100%; background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/740x0/filters:quality(95)/marketing/drumeo/promos/november/2024/practice-bundle.webp');"></div>
+                            <p class="leading-tight font-black mb-1">The Practice Bundle</p>
+                            <p class="leading-tight text-sm mb-1">Save $828.87 on most popular practice essentials + lessons.</p>
+                            <p class="leading-tight"><s class="opacity-60">$1227.87</s> <strong class="text-drumeo font-extrabold">$399</strong><span class="ml-1 text-xs bottom-0 font-black text-black rounded-md px-1.5 leading-none py-1 inline-block bg-musora align-bottom">Save 67%</span></p>
+                        </a>
                     </div>
                     <div x-cloak x-show="filter === 'accessories'">
                         @include('_partials.components.shop.product-card', [
