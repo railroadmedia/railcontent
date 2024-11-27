d6ac43121116c9c546b23bc76c085b9c99dedfd0
BC <bogdan@meshnet.capital>
2024-11-27 14:39:09 -0800

    fix(TP-377): drop duplicated map difficulty

--- resources/platform/assets/js/Libraries/Vuesora/assets/js/models/song.js
+++ resources/platform/assets/js/Libraries/Vuesora/assets/js/models/song.js
@@ -33,7 +33,6 @@ export default class SongContentModel extends ContentModel {
 
             this.list.column_data = [
                 this.getPostField('style'),
-                // ContentModel.mapDifficulty(this.post), TODO: would display "Intermediate 5" which is not desired
                 this.postPublisedOn,
             ];
         }
