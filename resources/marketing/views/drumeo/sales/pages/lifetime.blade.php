4f37a6ab6cadcae4503111fe9d3075b534bc65bb
Trent <trent.harms@gmail.com>
2024-11-27 10:30:49 -0800

    quick fix

--- resources/marketing/views/drumeo/sales/pages/lifetime.blade.php
+++ resources/marketing/views/drumeo/sales/pages/lifetime.blade.php
@@ -166,7 +166,7 @@
                 @else
                 <a class="join musora w-full sm:max-w-xs md:max-w-lg lg:max-w-xl" style="padding: 15px 10px;"
                     href="/ecommerce/add-to-cart?products[DLM-Lifetime]=1&products[LTM-songs-access-3-years]=1&promo-code=lifetime-3yr-songs&locked=true">GET THE DEAL <i class="fas fa-arrow-right"></i></a>
-                <a class="text-white underline" href="/ecommerce/add-to-cart?products[DLM-Lifetime-3-pay]=1&products[LTM-songs-access-3-years]=1&promo-code=lifetime-3yr-songs&locked=true"><p class="leading-tight text-sm pt-4"><em>Prefer a payment plan? Click here to order with 3 monthly payments.</em></p></a>
+                <a class="text-white underline" href="/ecommerce/add-to-cart?products[DLM-Lifetime-3-pay]=1&products[LTM-songs-access-3-years]=1&promo-code=lifetime-3yr-songs,lifetime-existing-3p&locked=true"><p class="leading-tight text-sm pt-4"><em>Prefer a payment plan? Click here to order with 3 monthly payments.</em></p></a>
                 @endif
                 {{-- @else
                      <span class="join sold-out mt-4 md:mt-5 w-full max-w-xs md:max-w-lg lg:max-w-xl" style="padding: 15px 10px;">SOLD OUT</span>
