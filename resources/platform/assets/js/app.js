0f49c1836100e32abf77f13583e03507d24056a4
peterminhlee <petereminhlee@gmail.com>
2024-11-28 14:47:44 -0800

    debug to false for sanity

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -18,7 +18,7 @@ const config = {
         projectId:'4032r8py',
         dataset:'production',
         version:'2021-06-07',
-        debug: true,
+        debug: false,
         useCachedAPI: true
       },
       railcontentConfig: {
