7642ba754493dcddbd26464703a4c5dd08d576f4
robwillems <robert@musora.com>
2024-11-28 13:29:36 -0800

    Upgrade mcs to handle challenge difficulty group by

--- resources/platform/assets/js/app.js
+++ resources/platform/assets/js/app.js
@@ -18,7 +18,7 @@ const config = {
         projectId:'4032r8py',
         dataset:'production',
         version:'2021-06-07',
-        debug: false,
+        debug: true,
         useCachedAPI: true
       },
       railcontentConfig: {
