a25f69c43eb7bdc0d67235cb0ecc9d0ef5c33ab0
Alexandre Severo <alexandre.severo@me.com>
2024-11-29 11:55:11 -0800

    fix for empty explore tasks

--- app/Modules/UserManagementSystem/Services/ExploreTasksService.php
+++ app/Modules/UserManagementSystem/Services/ExploreTasksService.php
@@ -31,6 +31,11 @@ class ExploreTasksService
     public function uncompletedTasksForUser(User $user, ?int $take = 2): array
     {
         if (boolval(FeatureFlagging::branch('homepage-v2', user()))) {
+            $count = $user->exploreTasks->count();
+            if ($count === 0) {
+                $this->createDefaultExploreTasks($user);
+            }
+
             return $user->exploreTasks()
                 ->uncompleted()
                 ->notExpired()
