9509dbaff787cad8a4e7667fb5fa5e4bfe08b2a8
peterminhlee <petereminhlee@gmail.com>
2024-12-02 08:24:33 -0800

    bug fix on challenge notification modal

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeNotificationModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeNotificationModal.vue
@@ -11,11 +11,11 @@
                 <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">You're enrolled in 30-Day Drummer!</h1>
                 <p class="tw-mb-5 tw-text-left">Next, choose if you would like to receive practice reminders for this Challenge (make sure you download the app to get push notifications).</p>
                 <ul class="tw-text-left">
-                    <li class="tw-mb-5"><input class="tw-mr-[10px]" type="radio" value="true" v-model="selectedFrequency" /> <label>Yes, send me practice reminders!</label></li>
-                    <li><input class="tw-mr-[10px]" type="radio" value="false" v-model="selectedFrequency" /> <label>No thanks, I’m all good!</label></li>
+                    <li class="tw-mb-5"><input class="tw-mr-[10px]" type="radio" :value="true" v-model="selectedFrequency" /> <label>Yes, send me practice reminders!</label></li>
+                    <li><input class="tw-mr-[10px]" type="radio" :value="false" v-model="selectedFrequency" /> <label>No thanks, I’m all good!</label></li>
                 </ul>
                 <div class="tw-flex tw-justify-end tw-mt-[30px]">
-                    <MuButton :disabled="!selectedFrequency" @click="handleNext">Next</MuButton>
+                    <MuButton @click="handleNext">Next</MuButton>
                 </div>
             </template>
 
@@ -32,7 +32,7 @@
 
             <!-- Step 2 (Community) -->
             <template v-if="step === 2 && challengeType === 'community'">
-                <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">30-Day Drummer starts on August 20</h1>
+                <h1 class="tw-mb-3 tw-text-2xl tw-font-bold">{{ challengeTitle }} starts on {{ startDate }}</h1>
                 <div class="tw-mb-[10px] tw-flex tw-justify-center">
                     <!-- Avatars -->
                     <div v-for="(avatar, index) in challengeData.entity" class="tw-w-10 tw-h-10 tw-border tw-border-white tw-rounded-full tw-overflow-hidden tw-bg-cover tw-bg-center" :class="index !== 0 ? '-tw-ml-3' : ''" :style="`background-image: url('${avatar.profile_picture_url}')`"></div>
@@ -96,6 +96,17 @@ const userNames = computed(() => {
     return names.join(', ');
 })
 
+const challengeTitle = computed(() => {
+    //TODO(challenge): updated the field when migrating with MCS
+    return props.challenge.cohort_title;
+})
+
+const startDate = computed(() => {
+    //TODO(challenge): updated the field when migrating with MCS
+    const converted = new Date(props.challenge.cohort_start_date);
+    return `${months[converted.getMonth()]} ${converted.getDate()}`;
+})
+
 const setStartDateButtonText = computed(() => {
     const today = new Date(Date.now());
     if(today.getDate() === selectedDate.value.getDate() && today.getMonth() === selectedDate.value.getMonth() && today.getFullYear() === selectedDate.value.getFullYear()){
@@ -123,8 +134,9 @@ const handleNext = async () => {
                     slideIn.value = true;
                 },1500)
             }
-        } else {
-            step.value = 2;
+            else {
+                step.value = 2;
+            }
         }
     } catch (e) {
         window.shownotification({
