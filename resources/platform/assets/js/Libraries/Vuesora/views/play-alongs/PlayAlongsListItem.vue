c4c3e32b136db75aae2cba139b9e49c40515eb13
robwillems <robert@musora.com>
2024-12-01 23:30:10 -0800

    Fix play along details

--- resources/platform/assets/js/Libraries/Vuesora/views/play-alongs/PlayAlongsListItem.vue
+++ resources/platform/assets/js/Libraries/Vuesora/views/play-alongs/PlayAlongsListItem.vue
@@ -220,18 +220,9 @@ export default {
     },
     computed: {
         mappedData() {
-            let difficultyValue = 'all';
-            if(this.contentModel.post.fields) difficultyValue = this.contentModel.post.fields.find(field => field.key === 'difficulty').value
 
             const contentModel = JSON.parse(JSON.stringify(this.contentModel)) //Create a deep copy to not update reactive prop
-
-            if (Number.isFinite(Number(difficultyValue))) {
-                contentModel.list.difficulty = difficultyValue;
-            }
-            else {
-                contentModel.list.difficulty = 'all';
-            }
-
+            contentModel.list.difficulty = this.contentModel.post.difficulty_string;
             const excludeWords = ['novice', 'beginner', 'intermediate', 'advanced', 'expert', 'all'];
             const filteredColumnData = contentModel.list.column_data.filter(item => item && !excludeWords.some(word => item.toLowerCase().includes(word)));
             contentModel.list.column_data = filteredColumnData
