f415c17c8a5f64771952c9e1475285e0c8cd66ac
peterminhlee <petereminhlee@gmail.com>
2024-12-03 12:27:49 -0800

    Unlock dropdown updates in challenge overview

--- resources/platform/assets/js/Components/_Pages/Overview/Overview.vue
+++ resources/platform/assets/js/Components/_Pages/Overview/Overview.vue
@@ -213,7 +213,7 @@ const OverviewChildData = computed( () => {
 })
 
 const headerDropdown = computed(() => {
-    if(isChallenge.value && !isUnlocked.value && isChallengeEnrolled.value){
+    if(isChallenge.value && !isUnlocked.value && isChallengeEnrolled.value && isChallengeSolo.value){
         return dropdowns['challenges'];
     }
 })
@@ -224,10 +224,13 @@ const isChallenge = computed(() => {
 
 const isChallengeEnrolled = computed(() => {
     return data.value?.user_data?.is_active;
+})z
+
+const isChallengeSolo = computed(() => {
+    return data.value?.lesson?.is_solo;
 })
 
 const generateChallengeCtas = (data) => {
-    console.log('cta', data)
     if(isChallengeEnrolled.value){
         //when next lesson is the first lesson
         if(data.next_lesson.id === data.children[0].id){
