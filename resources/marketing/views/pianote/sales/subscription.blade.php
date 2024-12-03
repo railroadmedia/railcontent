ad4547f0f99bd4bd73d7aa4420a235f6ca0589e3
Awatanka <natalia.sokolova.ca@gmail.com>
2024-12-03 08:37:28 -0800

    Updates to pianote bonus section layout

--- resources/marketing/views/pianote/sales/subscription.blade.php
+++ resources/marketing/views/pianote/sales/subscription.blade.php
@@ -561,12 +561,12 @@
         @endphp
             <div id="customize-anchor"></div>
             @include('drumeo._partials.bf-order-section-bonuses-modal', [
-            'maxWidth' => 'max-w-4xl',
+            'maxWidth' => 'max-w-3xl',
             'bgColor' => 'background-image: url(\'https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/filters:quality(95)/marketing/drumeo/promos/november/2024/xm-bg.webp\'); background-repeat: no-repeat; background-size: cover;',
             'promoLogo' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/filters:quality(95)/marketing/drumeo/promos/november/2024/holiday-bundle/holiday-bundle-logo.svg',
             'logoHeight' => 'h-16 sm:h-20',
             'topImage' => 'marketing/pianote/promos/black-friday/the-book-bundle/bonus-AM.webp',
-            'bonusWidth' => 'w-1/2 md:w-1/3 lg:w-1/4',
+            'bonusWidth' => 'w-1/2 md:w-1/3',
             'promoHeader' => '<h2 class="leading-tight mb-4 sm:mb-5"><strong>Annual membership with a <br class="hidden md:block">free Metronome, Books, and $381 in digital bonuses.</h2>',
             'buttonLink' => '/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-1-YEAR]=1&products[the-holiday-bundle-pianote]=1&promo-code=holiday-bundle-pianote&locked=true',
             'bundle'=> "holiday-pianote",
