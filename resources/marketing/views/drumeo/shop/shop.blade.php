c62b71b670833fe49ca0a5e54169bde2506db71c
Trent <trent.harms@gmail.com>
2024-12-03 11:22:50 -0800

    adding mobile class

--- resources/marketing/views/drumeo/shop/shop.blade.php
+++ resources/marketing/views/drumeo/shop/shop.blade.php
@@ -84,7 +84,7 @@
                 <h5 class="leading-tight mb-4 md:mb-5"><strong><i class="fas fa-fire text-{{ $brand }} mr-1"></i> Featured</strong></h5>
                 <div class="flex flex-wrap mb-5 sm:mb-10">
                     <a href="/drumshop/holiday-bundle" class="w-full mx-auto p-1 sm:p-2 transition-opacity duration-500 hover:opacity-90">
-                        <div class="flex items-center w-full overflow-hidden relative text-white rounded-xl sm:pb-[35%]">
+                        <div class="flex items-center w-full overflow-hidden relative text-white rounded-xl pb-[71%] sm:pb-[35%]">
                             <div class="hidden sm:inline-block absolute inset-0 z-0 bg-cover bg-center" style="background-position:30% 0;background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/2230x0/filters:quality(95)/marketing/drumeo/promos/november/2024/holiday-bundle-full-new.webp');"></div>
                             <div class="inline-block sm:hidden absolute inset-0 z-0 bg-cover bg-top" style="background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/filters:quality(95)/marketing/drumeo/promos/november/2024/holiday-bundle-m-new.webp');"></div>
                         </div>
