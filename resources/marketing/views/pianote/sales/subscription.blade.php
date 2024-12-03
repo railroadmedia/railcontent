93317efd637b7ed694244041d4960d70dd782223
Awatanka <natalia.sokolova.ca@gmail.com>
2024-12-03 10:18:49 -0800

    Text homepage updates

--- resources/marketing/views/pianote/sales/subscription.blade.php
+++ resources/marketing/views/pianote/sales/subscription.blade.php
@@ -462,7 +462,7 @@
         @else
             @include('musora.sales.components.header-section', [
             'promoHeader' => true,
-            'BFheader' => 'books, metronome & 3 digital courses.',
+            'BFheader' => 'Books, metronome & 3 digital courses.',
                 'header' => 'Piano lessons for<br> <span class="relative inline-block">all skill levels<svg class="absolute left-0 right-0 bottom-0 w-full h-4 sm:h-7" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 524 22" fill="none" style="transform: translate(0, 100%);"><path d="M1.99978 10.6328C84.053 4.08508 302.889 -3.20824 521.809 20" stroke="#f61a30" stroke-width="3" stroke-linecap="round"></path><path d="M2.17373 15.0541C83.9528 7.29382 302.406 -3.51921 521.988 15.3111" stroke="#f61a30" stroke-width="3" stroke-linecap="round"></path></svg></span>.',
                 'desc' => 'Learn the piano faster with step-by-step lessons,<br class="hidden sm:inline"> a thousand songs, and unlimited personal support. ',
                 'thumb' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/filters:quality(95)/marketing/pianote/membership/homepage/webp-format/header-thumb2.webp',
@@ -567,7 +567,7 @@
             'logoHeight' => 'h-16 sm:h-20',
             'topImage' => 'marketing/pianote/promos/black-friday/the-book-bundle/bonus-AM.webp',
             'bonusWidth' => 'w-1/2 md:w-1/3',
-            'promoHeader' => '<h2 class="leading-tight mb-4 sm:mb-5"><strong>Annual membership with a <br class="hidden md:block">free Metronome, Books, and $381 in digital bonuses.</h2>',
+            'promoHeader' => '<h2 class="leading-tight mb-4 sm:mb-5"><strong>Get a Pianote Membership with $538 in free bonuses.</h2>',
             'buttonLink' => '/ecommerce/add-to-cart?products[PIANOTE-MEMBERSHIP-1-YEAR]=1&products[the-holiday-bundle-pianote]=1&promo-code=holiday-bundle-pianote&locked=true',
             'bundle'=> "holiday-pianote",
             ])
