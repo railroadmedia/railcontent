d98e5b27f8196184e57cbd64545aeb3b7783e2f8
Trent <trent.harms@gmail.com>
2024-12-03 11:24:31 -0800

    fixing url

--- resources/marketing/views/pianote/shop/shop.blade.php
+++ resources/marketing/views/pianote/shop/shop.blade.php
@@ -87,7 +87,7 @@
                     <a href="/shop/holiday-bundle" class="w-full mx-auto p-1 sm:p-2 transition-opacity duration-500 hover:opacity-90">
                         <div class="flex items-center w-full overflow-hidden relative text-white rounded-xl pb-[71%] sm:pb-[35%]">
                             <div class="hidden sm:inline-block absolute inset-0 z-0 bg-cover bg-center" style="background-position:30% 0;background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/2230x0/filters:quality(95)/marketing/pianote/promos/black-friday/shop/holiday-bundle-full.webp');"></div>
-                            <div class="inline-block sm:hidden absolute inset-0 z-0 bg-cover bg-top" style="background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/filters:quality(95)/marketing/pianote/promos/black-friday/shop/holiday-bundle-full-m.webp');"></div>
+                            <div class="inline-block sm:hidden absolute inset-0 z-0 bg-cover bg-top" style="background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/800x0/filters:quality(95)/marketing/pianote/promos/black-friday/shop/holiday-bundle-m.webp');"></div>
                         </div>
                     </a>
                     <div class="w-full p-2">
