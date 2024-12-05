7731c3abc863c0b814bcbb2429e11ada1c741cab
peterminhlee <petereminhlee@gmail.com>
2024-12-04 20:57:04 -0800

    added skeleton loading in challenge award container

--- resources/platform/assets/js/Components/_Collections/ChallengeAwardContainer/ChallengeAwardContainer.vue
+++ resources/platform/assets/js/Components/_Collections/ChallengeAwardContainer/ChallengeAwardContainer.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="tw-flex tw-flex-nowrap lg:tw-grid tw-grid-cols-4 xl:tw-grid-cols-6 tw-gap-4 tw-no-scrollbar tw-overflow-x-scroll">
-        <ChallengeAwardCard v-for="award in preLoadedContent" :award="award" />
+        <SkeletonChallengeAwardCard v-if="isLoading" v-for="n in 4" :key="n" />
+        <ChallengeAwardCard v-else v-for="award in preLoadedContent" :award="award" />
     </div>
 </template>
 <script setup>
+import { usePlatformStore } from "@stores/platform";
+import { storeToRefs } from "pinia/dist/pinia";
 import ChallengeAwardCard from '@collections/ChallengeAwardContainer/ChallengeAwardCard';
+import SkeletonChallengeAwardCard from '@collections/SkeletonLoader/SkeletonChallengeAwardCard';
 
 const props = defineProps({
     preLoadedContent: {
@@ -12,4 +16,7 @@ const props = defineProps({
         default: () => [],
     },
 })
+
+const platformStore = usePlatformStore();
+const { isLoading } = storeToRefs(platformStore);
 </script>
