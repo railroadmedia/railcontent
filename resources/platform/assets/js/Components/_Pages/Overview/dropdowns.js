b8e9c8a321efd7f430f62b26ff7f8bb24c419c80
peterminhlee <petereminhlee@gmail.com>
2024-12-05 14:37:12 -0800

    dropdown updates in overview

--- resources/platform/assets/js/Components/_Pages/Overview/dropdowns.js
+++ resources/platform/assets/js/Components/_Pages/Overview/dropdowns.js
@@ -1,11 +1,24 @@
 export const dropdowns = {
-    challenges: [
-        {
-            props: {
-                text: 'Unlock',
-                modalType: 'unlock',
-            },
-            type: 'UnlockChallengeCta'
+    challenges: {
+        unlock : [
+            {
+                props: {
+                    text: 'Unlock',
+                    modalType: 'unlock',
+                },
+                type: 'UnlockChallengeCta'
+            }
+        ],
+        unlocked: (url) => {
+            return [
+                {
+                    props: {
+                        text: 'Enroll Now',
+                        url,
+                    },
+                    type: 'PageHeaderCta'
+                }
+            ]
         }
-    ]
+    }
 }
