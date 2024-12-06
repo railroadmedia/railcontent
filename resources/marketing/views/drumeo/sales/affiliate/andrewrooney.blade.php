7a0df7a80fbea044ec16ddb24b4ff3e0588131d2
Trent <trent.harms@gmail.com>
2024-12-06 12:39:24 -0800

    updating affiliates

--- resources/marketing/views/drumeo/sales/affiliate/andrewrooney.blade.php
+++ resources/marketing/views/drumeo/sales/affiliate/andrewrooney.blade.php
@@ -1,9 +1,13 @@
-@extends('drumeo.sales.affiliate.affiliate-layout')
+@extends('drumeo.sales.affiliate.affiliate-layout', [
+     'video' => '953608536',
+     'headline' => 'Hey Hey!',
+     'background' => "#000 url('https://d21q7xesnoiieh.cloudfront.net/fit-in/2000x0/filters:quality(95)/marketing/musora/lead-gen/affiliate/andrew-rooney-header-bg.webp')",
+])
 
 @section('name', 'Andrew Rooney')
 
 @section('url', 'andrew-rooney')
 
 @section('text')
-    "Drumeo is your one-stop resource to fast track your drumming progress. As a full-time player and educator, I could not live without it." <strong class="inline-block">- Andrew Rooney</strong>
+    Thank you so much for clicking the link and starting your musical education with Drumeo. I have partnered with them to offer you 30 days to try it out for free. Click the link to get started and keep choppin’ wood!
 @endsection
