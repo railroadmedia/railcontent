5c05d15601565d0cc2234bb2d5887fa04d6d5253
peterminhlee <petereminhlee@gmail.com>
2024-12-09 21:03:06 -0800

    share feature updates in challenge award modal

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeAwardModal.vue
@@ -22,12 +22,12 @@
 
                 <div class="tw-flex-col sm:tw-flex-row tw-flex tw-justify-center tw-gap-[10px] tw-mb-16 sm:tw-mb-6 tw-z-[3] tw-relative">
                     <MuButton @click="openCertificate">View certificate</MuButton>
-                    <MuButton v-if="isSharable" @click="openShareModal"><musora-icon icon-name="share" class="tw-h-6 tw-mr-1 -tw-mt-1 " /> Share</MuButton>
+<!--                    <MuButton v-if="isSharable" @click="openShareModal"><musora-icon icon-name="share" class="tw-h-6 tw-mr-1 -tw-mt-1 " /> Share</MuButton>-->
                 </div>
                 <div v-if="!openFromAwards" class="tw-text-center">
                     <a :href="`/${brand}/challenges`" class="tw-uppercase tw-underline tw-font-bold tw-font-bebas-neue dark:tw-text-white tw-z-[3] tw-relative">Return to Challenges</a>
                 </div>
-                <div v-if="!isSharable" class="tw-mt-5 tw-text-xs tw-text-center">
+                <div class="tw-mt-5 tw-text-xs tw-text-center">
                     Sharing is currently disabled in your browser. To share your awards with others, download the Musora app today!
                 </div>
             </div>
