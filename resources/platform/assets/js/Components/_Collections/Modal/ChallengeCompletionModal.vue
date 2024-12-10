2c6f6bba45c936ecf7c8517d744aac31fcc133ea
peterminhlee <petereminhlee@gmail.com>
2024-12-10 09:34:46 -0800

    animation updates in challenge completion modal

--- resources/platform/assets/js/Components/_Collections/Modal/ChallengeCompletionModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/ChallengeCompletionModal.vue
@@ -71,7 +71,7 @@
 
                     <!-- Progress Bar -->
                     <div class="tw-absolute tw-left-0 tw-bottom-0 tw-w-full tw-h-5 tw-bg-[#223F57]">
-                        <div :class="`tw-absolute tw-left-0 tw-top-0 tw-h-5 tw-transition-all tw-duration-400 tw-bg-${brand} tw-flex tw-justify-end tw-items-center tw-text-[#E3E3E3] tw-text-[11px] tw-font-bold`" :style="`width:${progress}%`">{{ progress }}%</div>
+                        <div :class="`tw-absolute tw-left-0 tw-top-0 tw-h-5 tw-transition-all tw-duration-700 tw-bg-${brand} tw-flex tw-justify-end tw-items-center tw-text-[#E3E3E3] tw-text-[11px] tw-font-bold`" :style="`width:${progress}%`">{{ progress }}%</div>
                     </div>
                 </div>
                 <div v-if="!isChallengeCompleted" class="tw-flex tw-justify-center tw-w-full">
@@ -98,7 +98,7 @@
                                 <circle cx="150" cy="142" r="120" stroke="currentColor" stroke-width="20" fill="transparent"
                                         :stroke-dasharray="circumference"
                                         :stroke-dashoffset="circumference - progress / 100 * circumference"
-                                        :class="`tw-text-${brand} tw-transition-all tw-duration-400`" />
+                                        :class="`tw-text-${brand} tw-transition-all tw-duration-700`" />
                             </svg>
                             <div class="tw-absolute tw-text-center tw-max-w-[200px]">
                                 <img class="tw-h-14 tw-mx-auto dark:tw-hidden" :src="`https://www.musora.com/cdn-cgi/image/width=300,quality=95/${lightModeLogo}`" alt="Challenge light mode logo" />
@@ -306,7 +306,7 @@ onMounted(() => {
         if(streakBadgeText.value){
             showBadgeAnimation.value = true;
         }
-    }, 2000)
+    }, 1000)
 
     if(isChallengeCompleted.value){
         setTimeout(() => {
