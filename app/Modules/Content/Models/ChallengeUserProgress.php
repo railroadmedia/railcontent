63adbbc64bd5ffda8d0b5e32c4e46c04a6d9b704
robwillems <robert@musora.com>
2024-12-10 18:20:42 -0800

    Change card carousel to remove 24 hour limit to show active challenges

--- app/Modules/Content/Models/ChallengeUserProgress.php
+++ app/Modules/Content/Models/ChallengeUserProgress.php
@@ -84,15 +84,15 @@ class ChallengeUserProgress extends Model
     public function scopeActive(Builder $query): Builder
     {
         return $query
-            ->where('is_active', true)
-            ->where('start_date', '<=',  Carbon::now()->addHours(24));
+            ->where('is_active', true);
+            //->where('start_date', '<=',  Carbon::now()->addHours(24));
     }
 
     public function scopeCurrentlyActive(Builder $query): Builder
     {
         return $query
-            ->where('is_active', true)
-            ->where('start_date', '<=',  Carbon::now()->addHours(24));
+            ->where('is_active', true);
+            //->where('start_date', '<=',  Carbon::now()->addHours(24));
     }
 
     public function scopeSolo(Builder $query): Builder
