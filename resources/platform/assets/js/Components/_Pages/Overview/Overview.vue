b71214133c7fb6bb1202ca924527c5291da2b75b
calebfavor <calebfavor@gmail.com>
2024-12-10 22:55:38 -0800

    Fixing broken challenge index page if the upcoming challenge starts in the future and its a community challenge. Hiding CTA in this case. Making this compontent function for the CTA more fail-safe.

--- resources/platform/assets/js/Components/_Pages/Overview/Overview.vue
+++ resources/platform/assets/js/Components/_Pages/Overview/Overview.vue
@@ -228,10 +228,10 @@ const isChallengeSolo = computed(() => {
 const generateChallengeCtas = (data) => {
     if(isChallengeEnrolled.value){
         //when next lesson is the first lesson
-        if(data.next_lesson.is_first_lesson){
+        if(data.next_lesson?.is_first_lesson){
             let type;
 
-            if(data.next_lesson.is_locked){
+            if(data.next_lesson?.is_locked){
                 type = 'LockedChallengeCta';
             } else {
                 type = 'PageHeaderPrimaryCta';
@@ -249,29 +249,33 @@ const generateChallengeCtas = (data) => {
             ]
         } else {
             // if next lesson is locked
-            if(data.next_lesson.is_locked){
-                return [
-                    {
-                        type: 'PageHeaderPrimaryCta',
-                        props: {
-                            text: `Replay ${data.previous_lesson.title}`,
-                            isPrimary: true,
-                        },
-                        lessonData: data.previous_lesson.web_url_path,
-                    }
-                ]
+            if(data.next_lesson?.is_locked){
+                if (data.previous_lesson?.title) {
+                    return [
+                        {
+                            type: 'PageHeaderPrimaryCta',
+                            props: {
+                                text: `Replay ${data.previous_lesson.title}`,
+                                isPrimary: true,
+                            },
+                            lessonData: data.previous_lesson.web_url_path,
+                        }
+                    ]
+                } else {
+                    return [];
+                }
             }
             // if next lesson is unlocked
             else {
                 let obj;
-                if(!data.previous_lesson.completed){
+                if(!data.previous_lesson?.completed){
                     obj = {
-                        url: data.previous_lesson.web_url_path,
+                        url: data.previous_lesson?.web_url_path,
                         text: 'Continue'
                     }
                 } else {
                     obj = {
-                        url: data.next_lesson.web_url_path,
+                        url: data.next_lesson?.web_url_path,
                         text: 'Next Lesson',
                         faIconClass: 'fas fa-play',
                     }
