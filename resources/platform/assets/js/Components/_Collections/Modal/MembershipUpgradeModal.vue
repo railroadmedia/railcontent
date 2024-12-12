f70a5b89be1aa39df2eba6733622011bdf0a73c7
robwillems <robert@musora.com>
2024-12-11 22:31:35 -0800

    Minor membership upgrade model change

--- resources/platform/assets/js/Components/_Collections/Modal/MembershipUpgradeModal.vue
+++ resources/platform/assets/js/Components/_Collections/Modal/MembershipUpgradeModal.vue
@@ -38,7 +38,7 @@
                         </template>
                     </div>
                     <div class="tw-font-extrabold tw-text-lg lg:tw-text-[22px] tw-mb-1">
-                        ${{ price }} {{ interval }}
+                        {{ price }} {{ interval }}
                     </div>
                     <div v-if="intervalDesc" class="tw-italic tw-text-sm tw-mb-8">
                         {{ intervalDesc }}
@@ -140,12 +140,13 @@ const isYearly = computed(() => {
 
 const price = computed(() => {
     if(isMonthly.value) {
-        return 5;
+        return "$5";
     } else if(isYearly.value) {
-        return 20;
+        return "$20";
     } else if(isLifetimeMember.value){
-        return 40;
+        return "$40";
     }
+    return "";
 })
 
 const interval = computed(() => {
