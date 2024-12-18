46eae9c77d3a3493220b84b9bdd2349b80f6af9e
Trent <trent.harms@gmail.com>
2024-12-17 17:51:45 -0800

    swapping share images

--- resources/marketing/views/pianote/sales/subscription.blade.php
+++ resources/marketing/views/pianote/sales/subscription.blade.php
@@ -16,8 +16,7 @@
     @hasSection('share-image')
         @yield('share-image')
     @else
-        <meta property="og:image" content="https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/filters:quality(95)/marketing/pianote/promos/black-friday/share-image-home.jpg">
-{{--        <meta property="og:image" content="https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/filters:quality(95)/marketing/pianote/membership/homepage/webp-format/share-image-pianote2.webp ">--}}
+        <meta property="og:image" content="https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/filters:quality(95)/marketing/pianote/membership/homepage/webp-format/share-image-pianote2.webp ">
     @endif
 
     @include('_partials.layout._fonts')
