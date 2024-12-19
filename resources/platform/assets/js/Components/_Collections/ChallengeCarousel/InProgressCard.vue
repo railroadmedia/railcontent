c4f1bdb475d34b116ec2e8ac303fd48c29109112
peterminhlee <60075429+peterminhlee@users.noreply.github.com>
2024-12-19 09:17:17 -0800

    logo style updates (#1807)
    Co-authored-by: peterminhlee <petereminhlee@gmail.com>

--- resources/platform/assets/js/Components/_Collections/ChallengeCarousel/InProgressCard.vue
+++ resources/platform/assets/js/Components/_Collections/ChallengeCarousel/InProgressCard.vue
@@ -108,8 +108,8 @@
                 </svg>
                 <div class="tw-absolute tw-text-center tw-flex tw-flex-col tw-items-center">
                     <!-- Challenge Logos -->
-                    <img class="tw-max-w-[155px] tw-max-h-[64px] tw-mb-1 tw-hidden dark:tw-block" :src="`https://www.musora.com/cdn-cgi/image/width=300,quality=95/${challenge.dark_mode_logo_url}`" />
-                    <img class="tw-max-w-[155px] tw-max-h-[64px] tw-mb-1 dark:tw-hidden" :src="`https://www.musora.com/cdn-cgi/image/width=300,quality=95/${challenge.light_mode_logo_url}`" />
+                    <img class="tw-h-14 tw-w-[155px] tw-object-contain tw-object-center tw-mb-1 tw-hidden dark:tw-block" :src="`https://www.musora.com/cdn-cgi/image/width=300,quality=95/${challenge.dark_mode_logo_url}`" />
+                    <img class="tw-h-14 tw-w-[155px] tw-object-contain tw-object-center tw-mb-1 dark:tw-hidden" :src="`https://www.musora.com/cdn-cgi/image/width=300,quality=95/${challenge.light_mode_logo_url}`" />
                     <div class="tw-text-[11px] lg:tw-text-[13px] tw-font-bold tw-max-w-[160px] tw-mt-2" :class="hasMissedLessons ? 'tw-text-[#F61A30]' : ''">{{ actionText }}</div>
                 </div>
             </div>
