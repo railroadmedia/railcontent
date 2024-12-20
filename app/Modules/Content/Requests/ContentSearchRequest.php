51c53518b77a0ac7d8e7cc5204ba1b2b97a86d9d
calebfavor <calebfavor@gmail.com>
2024-12-20 10:10:50 -0800

    Fixing infinite redirect issue with failed search validation. This type of validation should not be done on query params but I am not sure it's worth fixing.

--- app/Modules/Content/Requests/ContentSearchRequest.php
+++ app/Modules/Content/Requests/ContentSearchRequest.php
@@ -34,7 +34,7 @@ class ContentSearchRequest extends FormRequest
             'coach_ids' => ['nullable', 'array'],
             'coach_ids.*' => ['exists:' . Instructor::class],
             'page' => ['nullable', 'integer', 'gte:0'],
-            'limit' => ['nullable', 'integer', 'gte:1', 'required_with:page'],
+            'limit' => ['nullable', 'integer', 'gte:1'],
             'sort' => ['nullable', 'string'],
             'included_types' => ['nullable', 'array'],
             'include_future_scheduled_content_only' => ['nullable', 'boolean'],
