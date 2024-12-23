68d5ef6bac9baf141669905f88c77ebc78a8b5fc
Brandon <brandonk@musora.com>
2024-12-23 10:36:40 -0500

    Fix for the previous and next buttons in playlists (#1818)

--- app/Http/Controllers/Platform/UserPlaylistsController.php
+++ app/Http/Controllers/Platform/UserPlaylistsController.php
@@ -122,12 +122,12 @@ class UserPlaylistsController extends BaseController
         // DEV NOTE: checking for the 'type' is a bit of a workaround to ensure that the data came from Sanity
         throw_if((!$playlistItem || !$playlistItem['type']), new NotFoundHttpException());
 
-        $position = $otherItems->search(function ($item) use ($playlistItemId) {
+        $index = $otherItems->search(function ($item) use ($playlistItemId) {
             return $item['user_playlist_item_id'] == $playlistItemId;
         });
-        $position = $position === false ? 1 : $position + 1;
-        $previousPlaylistItem = $otherItems->get($position - 1);
-        $nextPlaylistItem = $otherItems->get($position + 1);
+        $position = $index === false ? 1 : $index + 1;
+        $previousPlaylistItem = $otherItems->get($index - 1);
+        $nextPlaylistItem = $otherItems->get($index + 1);
         $givenDate = Carbon::parse($playlistItem['published_on']);
         $today = Carbon::now();
         if ($givenDate->lt($today)) {
