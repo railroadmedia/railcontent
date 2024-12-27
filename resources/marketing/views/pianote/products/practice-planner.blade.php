fc685f1aedd776f4c559659c691c14b3cc1a1a94
Awatanka <natalia.sokolova.ca@gmail.com>
2024-12-27 15:27:24 -0800

    Added assets

--- resources/marketing/views/pianote/products/practice-planner.blade.php
+++ resources/marketing/views/pianote/products/practice-planner.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Stay on track and see better results from your practice sessions. Make every practice perfect.">
     <meta property="og:description" content="Stay on track and see better results from your practice sessions. Make every practice perfect.">
     <meta property="og:image"
-        content="https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/marketing/pianote/products/pianote-practice-planner/share-image.jpg"
+        content="https://d21q7xesnoiieh.cloudfront.net/fit-in/1200x0/marketing/pianote/products/practice-planner/share-image.jpg"
         style="display: none;">
     <meta property="og:url" content="https://www.pianote.com/{{ Request::path() }}">
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/focus@3.x.x/dist/cdn.min.js"></script>
@@ -23,13 +23,13 @@
     <style>
 
         header {
-            background-image: url('https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/pianote-practice-planner/header-bg-m.webp');
+            background-image: url('https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/header-bg-m.webp');
             background-size: cover;
         }
 
         @media (min-width: 639px) {
             header {
-                background-image: url('https://d21q7xesnoiieh.cloudfront.net/fit-in/2500x0/filters:quality(95)/marketing/pianote/products/pianote-practice-planner/header-bg.webp');
+                background-image: url('https://d21q7xesnoiieh.cloudfront.net/fit-in/2500x0/filters:quality(95)/marketing/pianote/products/practice-planner/header-bg.webp');
                 background-size: cover;
             }
         }
@@ -54,21 +54,21 @@
         "noBreadcrumb" => true
     ])
 
-        <header class="px-5 sm:px-6 py-12 sm:py-20 lg:py-36 bg-top" style="background-color:#F4F1EC;">
+        <header class="px-5 sm:px-6 py-12 sm:py-20 lg:py-28 bg-top" style="background-color:#F4F1EC;">
         <div class="container max-w-4xl mx-auto">
             <div class="flex flex-wrap sm:flex-nowrap items-center sm:mt-0">
-             <img class="h-24 mx-auto mb-4 sm:hidden" alt="Read Music in 30 Days Logo" fetchpriority="high"
-                    src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/read-music-book/logo.webp">
+             <img class="w-96 mx-auto mb-4 sm:hidden" alt="Logo" fetchpriority="high"
+                    src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/logo-mobile.svg">
                 
                 <div class="w-full sm:w-7/12 text-center lg:text-left sm:order-1 px-10">
                     <img class="w-full max-w-xs lg:max-w-md"
-                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/950x0/filters:quality(95)/marketing/pianote/products/read-music-book/RMI30D-book2.webp"
+                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/pianote/products/practice-planner/planner.webp"
                         alt="Practice Planner Cover">
                 </div>
                 <div class="w-full sm:w-5/12 text-center lg:text-left">
-                    <img class="w-96" alt="logo" fetchpriority="high"
-                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/practice-planner-logo.svg"
-                        alt="Read Music in 30 Days Logo">
+                    <img class="w-96 hidden sm:block" alt="logo" fetchpriority="high"
+                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/logo-desktop.svg"
+                        alt="Logo">
                     <p class="my-4 sm:my-6 text-black">Stay on track and see better results from your practice sessions.</p>
                     <p>Make every practice perfect.</p>
                     <h2 class="mb-4 sm:mb-6">
@@ -95,7 +95,7 @@
         <div class="container mx-auto z-10 relative max-w-5xl">
             @php
             $item = [
-                'img' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/products/practice-planner/planner-piano.png',
+                'img' => 'https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/products/practice-planner/intro-image.webp',
                 'desc' => [
                     'Practice doesn’t make perfect.',
                     '<strong>Perfect practice makes perfect.</strong>',
@@ -133,12 +133,12 @@
         </div>
     </section>
 
-    <section class="text-center text-white pt-10 sm:pt-14 lg:pt-20 bg-cover bg-center" style="background-color:#2a2f34; background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/lead-gen/digital-christmas-songbook/piano-bg.webp');">
+    <section class="text-center text-white pt-10 sm:pt-14 lg:pt-20 bg-cover bg-center" style="background-color:#2a2f34; background-image:url('https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/products/practice-planner/piano-bg.webp');">
         <h3 class="leading-tight"><strong>Take a look inside</strong></h3>
         <p class="leading-normal mt-1 sm:mt-2 mb-4 sm:mb-5"><i class="fa-light fa-arrow-turn-down fa-flip-horizontal mr-1 relative text-[#FCBD50]" style="bottom:-7px"></i> <em>Click to see a sample of how to use your practice planner.</em> <i class="fa-light fa-arrow-turn-down ml-1 relative text-[#FCBD50]" style="bottom:-7px"></i></p>
-        <a target="_blank" href="https://musora-image-processing-cdn.s3.us-east-2.amazonaws.com/marketing/pianote/lead-gen/digital-christmas-songbook/pianote-christmas-songbook-sample.pdf" class="relative">
-            <img class="inline-block lg:hidden w-full transition-all opacity-0" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/lead-gen/digital-christmas-songbook/piano-m.webp" alt="tablet piano" loading="lazy" onload="this.classList.remove('opacity-0')">
-            <img class="hidden lg:inline-block w-full transition-all opacity-0" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/2500x0/filters:quality(95)/marketing/pianote/lead-gen/digital-christmas-songbook/piano.webp" alt="tablet piano" loading="lazy" onload="this.classList.remove('opacity-0')">
+        <a target="_blank" href="" class="relative"> {{-- TODO: Add link here --}}
+            <img class="inline-block lg:hidden w-full transition-all opacity-0" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1500x0/filters:quality(95)/marketing/pianote/products/practice-planner/piano-m.webp" alt="tablet piano" loading="lazy" onload="this.classList.remove('opacity-0')">
+            <img class="hidden lg:inline-block w-full transition-all opacity-0" src="https://d21q7xesnoiieh.cloudfront.net/fit-in/2500x0/filters:quality(95)/marketing/pianote/products/practice-planner/piano.webp" alt="tablet piano" loading="lazy" onload="this.classList.remove('opacity-0')">
         </a>
     </section>
 
@@ -153,49 +153,49 @@
                 </p>
                 <p class="text-center mb-6"><strong>You'll learn:</strong></p>
                 
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-3">
+                               <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-3">
                     @php
                     $cards = [
                         [
-                            'icon' => 'fa-solid fa-message-question',
+                            'icon' => 'icons-01.svg',
                             'text' => 'The <strong>one question</strong> every piano player needs to be able to answer before getting started.'
                         ],
                         [
-                            'icon' => 'fas fa-clock',
+                            'icon' => 'icons-02.svg',
                             'text' => 'What to do if you only have <strong>10 minutes</strong> to practice today.'
                         ],
                         [
-                            'icon' => 'fas fa-bookmark',
+                            'icon' => 'icons-03.svg',
                             'text' => 'The <strong>essential elements</strong> every practice needs to include (don\'t skip these).'
                         ],
                         [
-                            'icon' => 'fas fa-arrow-up',
+                            'icon' => 'icons-04.svg',
                             'text' => 'How to <strong>stay motivated</strong> so you\'ll want to practice more (and get better).'
                         ],
                         [
-                            'icon' => 'fas fa-piano-keyboard',
+                            'icon' => 'icons-06.svg',
                             'text' => 'How to play <strong>any chord</strong> from <strong>any key</strong> with these chord formulas (including 6th, 7th, diminished, and augmented chords).'
                         ],
                         [
-                            'icon' => 'fa-regular fa-bullseye-arrow',
+                            'icon' => 'icons-05.svg',
                             'text' => 'The <strong>4 elements</strong> you need to set achievable goals (and how to follow through).'
                         ],
                         [
-                            'icon' => 'fa-solid fa-xmark-large',
+                            'icon' => 'icons-07.svg',
                             'text' => 'What to do when you <strong>don\'t feel like practicing</strong> (one simple trick).'
                         ],
                         [
-                            'icon' => 'fas fa-forward',
+                            'icon' => 'icons-08.svg',
                             'text' => 'How to <strong>end each practice session on a high note</strong> (so you\'ll want to come back).'
                         ]
                     ];
                     @endphp
-
+                
                     @foreach ($cards as $index => $card)
                         <div class="bg-[#F4F1EC] rounded-lg p-4 shadow-sm flex items-center md:block lg:p-6 lg:min-h-[220px]" 
                             style="box-shadow: 0px 4px 4px 0px #00000040;">
                             <div class="icon-gradient w-12 h-12 flex items-center justify-center mb-4 md:mb-6 md:mr-0 mr-4">
-                                <i class="{{ $card['icon'] }} text-4xl" style="color:#EA7A00;"></i>
+                                <img src="https://d21q7xesnoiieh.cloudfront.net/fit-in/2500x0/filters:quality(95)/marketing/pianote/products/practice-planner/{{ $card['icon'] }}" alt="Icon {{ $index + 1 }}" class="w-11 h-11">
                             </div>
                             <p class="flex-1 text-sm">{!! $card['text'] !!}</p>
                         </div>
@@ -210,16 +210,16 @@
     <section class="px-5 sm:px-6 py-12 sm:py-16 lg:py-20" style="background:#F4F1EC">
         <div class="container max-w-4xl mx-auto">
             <div class="flex flex-wrap sm:flex-nowrap items-center">
-                <img class="h-24 mx-auto mb-4 sm:hidden" alt="Read Music in 30 Days Logo" fetchpriority="high"
-                    src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/read-music-book/logo.webp">
+                <img class="w-96 mx-auto mb-4 sm:hidden" alt="Logo" fetchpriority="high"
+                    src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/logo-mobile.svg">
                 <div class="w-full sm:w-7/12 text-center lg:text-left sm:order-1 px-10">
                     <img class="w-full max-w-xs lg:max-w-md"
-                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/950x0/filters:quality(95)/marketing/pianote/products/read-music-book/RMI30D-book2.webp"
+                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/1000x0/filters:quality(95)/marketing/pianote/products/practice-planner/planner.webp"
                         alt="Practice Planner Cover">
                 </div>
-                <div class="w-full sm:w-5/12 text-center lg:text-left sm:pr-5">
-                    <img class="h-24 hidden sm:block lg:h-28" alt="Read Music in 30 Days Logo" fetchpriority="high"
-                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/read-music-book/logo.webp">
+                <div class="w-full sm:w-5/12 text-center sm:text-left sm:pr-5">
+                    <img class="w-96 hidden sm:block" alt="Logo" fetchpriority="high"
+                        src="https://d21q7xesnoiieh.cloudfront.net/fit-in/850x0/filters:quality(95)/marketing/pianote/products/practice-planner/logo-desktop.svg">
                     <p class="my-4 sm:my-6 text-black">Stay on track and see better results from your practice sessions.</p>
                     <p class="my-4 sm:my-6 text-black">Make every practice perfect.</p>
     
