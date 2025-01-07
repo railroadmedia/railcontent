d4e54fe5f831e65d7404e880bdc1889164562582
Robert Willems <robert@musora.com>
2025-01-07 11:41:24 -0800

    Remove logging

--- app/Modules/RailTracker/Services/BatchService.php
+++ app/Modules/RailTracker/Services/BatchService.php
@@ -32,7 +32,6 @@ class BatchService
     public function storeRequest(RequestVO $requestVO)
     {
         if ($this->connection()) {
-            \Log::info("storeRequest $requestVO->id");
             $setKey = $this->batchKeyPrefix . 'set' . '_' . $requestVO->uuid;
 
             $this->connection()->sadd($setKey, serialize($requestVO));
