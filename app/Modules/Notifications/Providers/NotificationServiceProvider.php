a88a4552445915b184543720fe866bee24df90cd
Roxana Riza <roxana_r2000@yahoo.com>
2025-01-13 16:41:46 +0200

    Fix events

--- app/Modules/Notifications/Providers/NotificationServiceProvider.php
+++ app/Modules/Notifications/Providers/NotificationServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Modules\Notifications\Providers;
 
 use App\Modules\Notifications\Console\Commands\DailySummaryNotifications;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Railroad\Railcontent\Events\CommentCreated;
 use Railroad\Railcontent\Events\CommentLiked;
 use App\Modules\Notifications\Listeners\NotificationListener as NotificationEventListener;
+use Railroad\Railforums\Events\PostCreated;
+use Railroad\Railforums\Events\PostLiked;
 
 class NotificationServiceProvider extends ServiceProvider
 {
