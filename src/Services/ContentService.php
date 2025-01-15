1719d3aaae91b534fe348a4ff524971fcb664aa0
adrianmcneill <adrian@musora.com>
2025-01-15 12:43:36 -0800

    update base method

--- src/Services/ContentService.php
+++ src/Services/ContentService.php
@@ -4,6 +4,7 @@ namespace Railroad\Railcontent\Services;
 
 use App\Modules\Content\ApiGateways\SanityGateway;
 use App\Modules\Content\Models\ContentUserProgress;
+use App\Modules\Content\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Database\Query\JoinClause;
@@ -16,6 +17,7 @@ use Railroad\Railcontent\Decorators\Decorator;
 use Railroad\Railcontent\Decorators\ModeDecoratorBase;
 use Railroad\Railcontent\Entities\ContentEntity;
 use Railroad\Railcontent\Entities\ContentFilterResultsEntity;
+use Railroad\Railcontent\Enums\RecommenderSection;
 use Railroad\Railcontent\Events\ContentCreated;
 use Railroad\Railcontent\Events\ContentDeleted;
 use Railroad\Railcontent\Events\ContentSoftDeleted;
@@ -113,16 +115,45 @@ class ContentService
 
 
     /**
-     * @param string|int $userId -
+     * @param User $user -
      * @param string $brand -
-     * @param array[RecommenderSection] $sections - sections to include in the result
+     * @param string $filter
      * @param int $pageSize -
      * @param int $page -
      * @param array $groupByForLessonsPage - Which RecommenderSection to bundle in a groupby filter
      * @return ContentFilterResultsEntity -
      */
-    public function getRecommendedContent(string|int $userId, string $brand, array $sections=[], int $pageSize=6, int $page=1, array $groupByForLessonsPage = [])
+    public function getRecommendedContent(User $user, string $brand, string $filter='', int $pageSize=6, int $page=1, bool $groupByForLessonsPage = [])
     {
+        $userId = $user->id;
+        $sections = match(strtolower($filter)) {
+            'songs', 'song' => [RecommenderSection::Song],
+            'lessons', 'lesson' => [RecommenderSection::QuickTip, RecommenderSection::Course],
+            'workouts', 'workout' => [RecommenderSection::Workout],
+            default => [],
+        };
+        if (!$sections && $groupByForLessonsPage) {
+            $groupBySections = [
+                'Songs You Might Like' => [RecommenderSection::Song],
+                'Lessons You Might Like' => [RecommenderSection::QuickTip, RecommenderSection::Course],
+                'Workouts You Might Like' => [RecommenderSection::Workout],
+            ];
+        } else {
+            $groupBySections = [];
+        }
+        if(!$user->hasSongsAccess($brand)) {
+            $groupBySections =
+                array_filter($groupBySections, function ($key) {
+                    return $key != 'Songs You Might Like';
+                },
+                    ARRAY_FILTER_USE_KEY);
+
+            $sections = array_values(
+                array_filter($sections, function ($section) {
+                    return $section->name != 'Song';
+                })
+            );
+        }
         $useFastImplementation = config('railcontent.recsys.use_fast_implementation');
         $useCaching = config('railcontent.recsys.use_caching');
 
@@ -177,9 +208,9 @@ class ContentService
             $pageSize,
             $page,
             $userId,
-            $groupByForLessonsPage
+            $groupBySections
         );
-        return $this->getContentFilterResultsFromRecommendations($processedRecommendations, $groupByForLessonsPage);
+        return $this->getContentFilterResultsFromRecommendations($processedRecommendations, $groupBySections);
     }
 
     /**
