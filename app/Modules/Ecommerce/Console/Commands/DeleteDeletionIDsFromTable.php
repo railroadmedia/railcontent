5e0b5cf48eed4c1c37802b75a8352c0e5ae80766
Daniel Austin <daniel@abbytreasures.com>
2025-01-21 13:51:54 -0800

    added artisan command that removes "delete" Product id's

--- app/Modules/Ecommerce/Console/Commands/DeleteDeletionIDsFromTable.php
+++ app/Modules/Ecommerce/Console/Commands/DeleteDeletionIDsFromTable.php
@@ -1,44 +1,35 @@
 <?php
 
-namespace Modules\Ecommerce\Console\Commands;
+namespace App\Modules\Ecommerce\Console\Commands;
 
 use App\Console\Commands\Infrastructure\Command;
 use App\Modules\Ecommerce\Models\Product;
+use App\Modules\Ecommerce\Services\ShopifySyncService;
 
 class DeleteDeletionIDsFromTable extends Command
 {
     //name and signature of console command
-    protected $signature = 'product:delete
-                            {--search= : the IDs of the product to delete. input as numbers seperated by commas.}
-                            {--delete : confirm delete? if not confirmed, will instead display tables.}';
-
-    //
-    protected $name = 'DeleteIDs';
+    protected $signature = 'product:delete-deletion-ids
+                            {--delete : confirm delete these items. if blank, will instead display delete items}';
     //The console command description.
-    protected $description = 'Delete product ids';
-
+    protected $description = 'Searches Product table and deletes all products with "delete"';
 
     //Execute the console command.
-    public function handle(): void
+    public function handle(ShopifySyncService $shopifySyncService): void
     {
         //set up variables for usage
         $query = Product::query();  //establish query in Product model
-        $search = $this->option('search');
-        $array = explode(",", $search); //convert input string to array bcs i cant figure out how to input an array into terminal
-        //$method  = $this->option('method');
-        $confirm = $this->option('delete') == 'true';   //check if you're sure you're deleting
-        //if (!$method) {$method = 'id';} //if method not specified, set as id
+        $confirm = $this->option('delete') == 'true';   //check if you're sure you want to delete
 
-        $query->whereIn('id', $array);
+        //search for items with "delete"
+        $query->whereRaw("name LIKE '%delete%'");
 
-
-        if ($confirm) { //delete
+        if ($confirm) { //delete items selected
             $query->delete();
         }
-        else {  //just show tables
-        $this->table(['id', 'name'], $query->select(['id', 'name'])->get()->toArray());
-
-        //could add in here a message to say there is a searched item alrdy missing...
+        else {  //just show the id's
+            $this->table(['id', 'name'], $query->select(['id', 'name'])->get()->toArray());
+            printf("add '--delete' to confirm delete these items\n");
         }
 
 
