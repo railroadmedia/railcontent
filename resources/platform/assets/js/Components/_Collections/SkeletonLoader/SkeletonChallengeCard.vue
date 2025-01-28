0384ba0c8581ebc3c0a472279d03b5db979eb3b1
peterminhlee <petereminhlee@gmail.com>
2025-01-28 13:45:57 -0800

    refactor: style updates for updated carousel in challenge card

--- resources/platform/assets/js/Components/_Collections/SkeletonLoader/SkeletonChallengeCard.vue
+++ resources/platform/assets/js/Components/_Collections/SkeletonLoader/SkeletonChallengeCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tw-animate-pulse tw-shrink-0" :class="widthStyles">
+    <div class="tw-animate-pulse tw-shrink-0 lg:tw-w-1/4 xl:tw-w-1/5 3xl:tw-w-1/6 tw-pr-1 lg:tw-pr-[15px] tw-mb-1 lg:tw-mb-[15px]" :class="widthStyles">
         <!-- Image -->
         <div class="tw-rounded-lg tw-aspect-[2/3] tw-bg-[#F2F2F2] dark:tw-bg-[#002039]"></div>
         <!-- Name -->
@@ -20,7 +20,9 @@ const props = defineProps({
 
 const widthStyles = computed(() => {
     if(props.isGroupedView){
-        return 'tw-w-[168px] sm:tw-w-[230px] lg:tw-w-auto'
+        return 'tw-w-[172px] sm:tw-w-[234px]';
+    } else {
+        return 'tw-w-1/2 sm:tw-w-1/3';
     }
 })
 </script>
