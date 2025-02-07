10efdb835ba2d69cc4012c4bf9f93a13f20df542
Roxana Riza <roxana_r2000@yahoo.com>
2025-02-07 20:11:52 +0200

    Hotfix: Upload captions/subtitles to AWS    The link received from Vimeo expires after 2 hours

--- app/Modules/Content/Controllers/SanityStudioCMSController.php
+++ app/Modules/Content/Controllers/SanityStudioCMSController.php
@@ -428,6 +428,12 @@ class SanityStudioCMSController extends BaseController
             ConfigService::$brand = 'musora';
             $vimeoTrailerDecorator = app()->make(VimeoTrailerDecorator::class);
             $vimeo = $vimeoTrailerDecorator->decorate($vimeoId);
+            foreach($vimeo['captions'] as $caption){
+                $client = new Client();
+                $response = $client->get($caption['link'], ['stream' => true]);
+                Storage::disk('s3')->put($caption['name'], $response->getBody(), 'public');
+                $caption['link'] = Storage::disk('s3')->url($caption['name']);
+            }
             if ($vimeo) {
                 $video = Vimeo::create(
                     [
