76ad34cf1b3d631f5d35f9febd52802a4cd0ca52
Roxana Riza <roxana_r2000@yahoo.com>
2025-02-21 16:25:20 +0200

    Set default chapter thumbnail images

--- app/Modules/Content/ApiGateways/SanityGateway.php
+++ app/Modules/Content/ApiGateways/SanityGateway.php
@@ -1045,6 +1045,7 @@ class SanityGateway
         $userPermissionIds = $this->getPermissionIds();
         if ($document['type'] == 'challenge' && ($document['lessons'] ?? false)) {
             $this->processNeedsAccessForChildren($document['lessons'], $userPermissionIds, $isAdmin);
+            $this->processChapterThumbnailForChildren($document['lessons']);
         } elseif ($document['type'] == 'challenge-part' && ($document['parent'] ?? false)) {
             $document['parent']['need_access'] = $this->doesUserNeedAccessToContent(
                 $document['parent'],
@@ -1052,6 +1053,7 @@ class SanityGateway
                 $isAdmin
             );
             $this->processNeedsAccessForChildren($document['parent']['lessons'], $userPermissionIds, $isAdmin);
+            $this->processChapterThumbnailForChildren($document['parent']['lessons']);
         }
         $document['need_access'] = $this->doesUserNeedAccessToContent($document, $userPermissionIds, $isAdmin);
     }
@@ -1182,6 +1184,14 @@ class SanityGateway
             $this->userPermissionsCached = \Arr::pluck($userPermissions, 'permission_id');
         }
         return $this->userPermissionsCached;
+    }
+
+    private function processChapterThumbnailForChildren(&$lessons)    {
+        foreach ($lessons as $index => $lesson) {
+            foreach ($lesson['chapters']??[] as $indexC => $chapter) {
+                $lessons[$index]['chapters'][$indexC]['chapter_thumbnail_url'] = "https://musora-web-platform.s3.amazonaws.com/chapters/{$lesson['brand']}/Chapter" . ($indexC + 1) . ".jpg";
+            }
+        }
     }
 
 
